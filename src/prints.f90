461baa7722ff2d0043bb5fa91e0a2160bfef91fa
Wolfgang Dobler <wdobler@gmail.com>
2002-05-04 12:40:13 +0000

    Minor

--- src/prints.f90
+++ src/prints.f90
@@ -1,4 +1,4 @@
-! $Id: prints.f90,v 1.1 2002-05-04 09:11:59 brandenb Exp $
+! $Id: prints.f90,v 1.2 2002-05-04 12:40:13 dobler Exp $
 
 module Print
 
@@ -13,7 +13,8 @@ module Print
 !***********************************************************************
     subroutine prints
 !
-!  reads and registers print parameters relevant for hydro part
+!  reads and registers print parameters gathered from the different
+!  modules and marked in `print.in'
 !
 !   3-may-02/axel: coded
 !
@@ -23,8 +24,8 @@ module Print
 !  this needs to be made more sophisticated of course...
 !
       if(lroot) then
-        write( 6,*) 'it,t,',cname(1:nname)
-        write( 6,*) it-1,t_diag,fname(1:nname)
+        write( 6,*) 'it,t,dt,',cname(1:nname)
+        write( 6,*) it-1,t_diag,dt,fname(1:nname)
         write( 6,*) 
       endif
 !
