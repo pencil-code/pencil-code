4e9d695ed88a77c4e8d7eb1cf6afa3fc7f528468
Axel Brandenburg <brandenb@nordita.org>
2002-08-21 04:15:01 +0000

    the removal of a mean flow does still not work correcly under mpi. nompi is ok.

--- src/hydro.f90
+++ src/hydro.f90
@@ -1,4 +1,4 @@
-! $Id: hydro.f90,v 1.59 2002-08-21 03:29:25 brandenb Exp $
+! $Id: hydro.f90,v 1.60 2002-08-21 04:15:01 brandenb Exp $
 
 module Hydro
 
@@ -72,7 +72,7 @@ module Hydro
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: hydro.f90,v 1.59 2002-08-21 03:29:25 brandenb Exp $")
+           "$Id: hydro.f90,v 1.60 2002-08-21 04:15:01 brandenb Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -513,6 +513,7 @@ module Hydro
 !
       real, dimension (mx,my,mz,mvar) :: f,df
       real, dimension(nx) :: ux
+      real, dimension(1) :: fsum_tmp,fsum
       real :: tau_damp_uxm1
       real, save :: uxm=0.,ux_sum=0.
 !
@@ -523,12 +524,16 @@ module Hydro
 !  needs to be broadcasted.
 !
       if(lfirstpoint) then
+        fsum_tmp(1)=ux_sum
+        call mpireduce_max(fsum_tmp,fsum,1)
         if(lroot) then
           uxm=ux_sum/(nw*ncpus)
           call mpibcast_real(uxm,1)
+print*,'root: uxm=',uxm
         endif
       endif
 !
+print*,'iproc,uxm=',iproc,uxm
       ux=f(l1:l2,m,n,iux)
       call sum_mn(ux,ux_sum)
       tau_damp_uxm1=1./tau_damp_uxm
@@ -549,6 +554,7 @@ module Hydro
 !
       real, dimension (mx,my,mz,mvar) :: f,df
       real, dimension(nx) :: uy
+      real, dimension(1) :: fsum_tmp,fsum
       real :: tau_damp_uym1
       real, save :: uym=0.,uy_sum=0.
 !
@@ -559,12 +565,16 @@ module Hydro
 !  needs to be broadcasted.
 !
       if(lfirstpoint) then
+        fsum_tmp(1)=uy_sum
+        call mpireduce_max(fsum_tmp,fsum,1)
         if(lroot) then
-          uym=uy_sum/(nw*ncpus)
+          uym=fsum(1)/(nw*ncpus)
           call mpibcast_real(uym,1)
+print*,'root: uym=',uym
         endif
       endif
 !
+print*,'iproc,uym=',iproc,uym
       uy=f(l1:l2,m,n,iuy)
       call sum_mn(uy,uy_sum)
       tau_damp_uym1=1./tau_damp_uym
