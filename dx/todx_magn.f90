d8f74509281d1b8b6dd9e1c2b592b27b1ac4d467
Vladimir Pariev <vpariev@pas.rochester.edu>
2002-10-02 03:52:37 +0000

    Generalized todx_magn.f90, which creates input for DX. Now it can run automatically for any combination of modules in the main code. todx_magn.f90 now reads tmp/index.pro file. Also, modified hydro.net to include passive scalar import.

--- dx/todx_magn.f90
+++ dx/todx_magn.f90
@@ -6,8 +6,6 @@
 ! directory of the run. Compile todx_magn.f90 and run it. 
 ! .net files should be linked to run directory (from pencil_modular/dx).
 ! Then, Data Explorer is ready to be started from run directory.
-! This todx routine is better to be integrated into the main code - to
-! do in the future.
 
 ! Include grid sizes
       include 'src/cparam.local'
@@ -32,7 +30,22 @@
       real, dimension(mx,my,mz,3) :: bbb
       real :: x00,y00,z00,t,dx,dy,dz
       integer :: i,j,k,n
-      integer :: iux,iuy,iuz,ilnrho,ient,iax,iay,iaz
+      integer :: iux,iuy,iuz,ilnrho,ient,iax,iay,iaz,ilncc
+      integer :: i_t,i_it,i_dt,i_dtc,nname,i_u2m,i_um2,i_o2m
+      integer :: i_oum,i_urms,i_umax,i_orms,i_omax,i_uxm
+      integer :: i_uym,i_uzm,iuu,i_eth,i_ekin,i_rhom
+      integer :: i_ssm,i_abm,i_jbm,i_b2m,i_bm2,i_j2m,i_jm2
+      integer :: iaa,nnamez,i_bxmz,i_bymz,i_bzmz,i_bmx,i_bmy
+      integer :: i_bmz,nnamexy,i_bxmxy,i_bymxy,i_bzmxy,i_frms
+      integer :: i_fmax,i_Erad_rms,i_Erad_max,i_Egas_rms
+      integer :: i_Egas_max,ie,ifx,ify,ifz,i_ccm,i_ccmax,i_rhoccm
+
+! Read file of integer parameters, index.pro
+      include 'tmp/index.pro'
+      
+      print*,'iux=',iux,' iuy=',iuy,' iuz=',iuz,' ilnrho=',  &
+       ilnrho,' ient=',ient,' iax=',iax,' iay=', iay,' iaz=', &
+       iaz,' ilncc=',ilncc
 
 ! Read snapshot file
       open(unit=17, file='./tmp/proc0/var.dat', form='unformatted')
@@ -52,33 +65,16 @@
       print*,'mvar=',mvar
       
 ! Write out velocity field for Dx
-      iux=1
-      iuy=2
-      iuz=3
+      if(iux.ne.0) then
+         if(iuy-iux.ne.1.or.iuz-iuy.ne.1) then
+            print*,'iux,iuy,iuz are not in order, exiting'
+            stop
+         else   
       open(unit=18, file='./tmp/uuu_dx.bin', form='unformatted')
       write(18) ((((real(f(i,j,k,n)),n=iux,iuz),k=nghost+1,mz-nghost), &
        j=nghost+1,my-nghost),i=nghost+1,mx-nghost)
       close(18)
 
-! Write out ln density field for Dx
-      if(mvar.ge.4) then
-      ilnrho=4
-      open(unit=19, file='./tmp/lnrho_dx.bin', form='unformatted')
-      write(19) (((real(f(i,j,k,ilnrho)),k=nghost+1,mz-nghost), &
-       j=nghost+1,my-nghost),i=nghost+1,mx-nghost)
-      close(19)
-      endif
-
-! Write out entropy field for Dx
-      if(mvar.ge.5) then
-      ient=5
-      open(unit=20, file='./tmp/ss_dx.bin', form='unformatted')
-      write(20) (((real(f(i,j,k,ient)),k=nghost+1,mz-nghost),  &
-       j=nghost+1,my-nghost),i=nghost+1,mx-nghost)
-      close(20)
-      endif
-
-! Create .dx scripts describing the data in Data Explorer format
       open(unit=21, file='./tmp/uuu.dx')
       write(21,*) 'object 1 class gridpositions counts ', mx-2*nghost, &
         my-2*nghost, mz-2*nghost
@@ -104,7 +100,20 @@
       write(21,*) 'end'
       close(21)
 
-      if(mvar.ge.4) then         
+      print*,'velocity field has been written out'
+
+      endif
+      endif
+
+
+! Write out ln density field for Dx
+      if(ilnrho.ne.0) then
+
+      open(unit=19, file='./tmp/lnrho_dx.bin', form='unformatted')
+      write(19) (((real(f(i,j,k,ilnrho)),k=nghost+1,mz-nghost), &
+       j=nghost+1,my-nghost),i=nghost+1,mx-nghost)
+      close(19)
+
       open(unit=22, file='./tmp/lnrho.dx')
       write(22,*) 'object 1 class gridpositions counts ', mx-2*nghost,  &
         my-2*nghost, mz-2*nghost
@@ -129,9 +138,20 @@
       write(22,*) 'component "data" value 3'
       write(22,*) 'end'
       close(22)
+
+      print*,'density field has been written out'
+
       endif
 
-      if(mvar.ge.5) then
+
+! Write out entropy field for Dx
+      if(ient.ne.0) then
+
+      open(unit=20, file='./tmp/ss_dx.bin', form='unformatted')
+      write(20) (((real(f(i,j,k,ient)),k=nghost+1,mz-nghost),  &
+       j=nghost+1,my-nghost),i=nghost+1,mx-nghost)
+      close(20)
+
       open(unit=23, file='./tmp/ss.dx')
       write(23,*) 'object 1 class gridpositions counts ', mx-2*nghost, &
         my-2*nghost, mz-2*nghost
@@ -156,16 +176,19 @@
       write(23,*) 'component "data" value 3'
       write(23,*) 'end'
       close(23)
+
+      print*,'entropy field has been written out'
+
       endif
 
 
+! Write out vector potential and magnetic field for Dx
+      if(iax.ne.0) then
+         if(iay-iax.ne.1.or.iaz-iay.ne.1) then
+            print*,'iax,iay,iaz are not in order, exiting'
+            stop
+         else   
 
-      if(mvar.ge.6) then
-! Process magnetic part if mvar>5: no kinematic dynamo yet, needs to be added
-! Write out vector potential for Dx
-      iax=6
-      iay=7
-      iaz=8
       open(unit=18, file='./tmp/aaa_dx.bin', form='unformatted')
       write(18) ((((real(f(i,j,k,n)),n=iax,iaz),k=nghost+1,mz-nghost), &
         j=nghost+1,my-nghost),i=nghost+1,mx-nghost)
@@ -242,9 +265,47 @@
       write(25,*) 'end'
       close(25)
 
+      print*,'vector potential and magnetic field have been written out'
 
       endif
+      endif
 
+! Write out passive scalar field for Dx
+      if(ilncc.ne.0) then
+
+      open(unit=19, file='./tmp/lncc_dx.bin', form='unformatted')
+      write(19) (((real(f(i,j,k,ilncc)),k=nghost+1,mz-nghost), &
+       j=nghost+1,my-nghost),i=nghost+1,mx-nghost)
+      close(19)
+
+      open(unit=22, file='./tmp/lncc.dx')
+      write(22,*) 'object 1 class gridpositions counts ', mx-2*nghost,  &
+        my-2*nghost, mz-2*nghost
+      write(22,*) 'origin  ', x00, y00, z00 
+      write(22,*) 'delta  ', dx,'  0   ', '   0    '
+      write(22,*) 'delta  ', '  0  ',dy,'  0  '
+      write(22,*) 'delta','  0  ','  0  ',dz
+      write(22,*) '  '
+      write(22,*) 'object 2 class gridconnections counts ', mx-2*nghost,  &
+        my-2*nghost, mz-2*nghost
+      write(22,*) '  '
+      write(22,*) 'object 3 class array type float rank 0'
+      write(22,*) 'items ', (mx-2*nghost)*(my-2*nghost)*(mz-2*nghost),  &
+        ' lsb binary'
+      write(22,*) 'data file "lncc_dx.bin", 4'
+      write(22,*) '  '
+      write(22,*) 'attribute "dep" string "positions" '
+      write(22,*) '  '
+      write(22,*) 'object "regular positions regular connections" class field'
+      write(22,*) 'component "positions" value 1'
+      write(22,*) 'component "connections" value 2'
+      write(22,*) 'component "data" value 3'
+      write(22,*) 'end'
+      close(22)
+
+      print*,'passive scalar field has been written out'
+
+      endif
 
       stop
       end
