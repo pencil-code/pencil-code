7bb9c35973650dee162e1fb5db71afb180fd08ea
Antony J. Mee <eemynotna@gmail.com>
2003-01-13 19:00:57 +0000

    Had fixed the wsnap problem earlier (I think) but hadn't checked it in...
    1) Undone Axel's change to wsnaps.f90
    
    The code compiled (on my Linux box) if I used struct_func.f90 but not
    if I used nostruct_func.f90.  On closer examination, in struct_func
    then variabl parameter had unspecified length:
    
      character (len=*) :: variabl
    
    yet in nostruct_func it had length 4:
    
      character (len=4) :: variabl
    
    2) I have changes nostruct_func.f90 to match struct_func.f90 where
       variabl has unspecified length, hence the compiler doesn't fail
       on type checking.

--- src/nostruct_func.f90
+++ src/nostruct_func.f90
@@ -1,4 +1,4 @@
-! $Id: nostruct_func.f90,v 1.3 2003-01-10 14:00:27 nilshau Exp $
+! $Id: nostruct_func.f90,v 1.4 2003-01-13 19:00:57 mee Exp $
 !
 module  struct_func
   !
@@ -12,7 +12,7 @@ module  struct_func
     subroutine structure(f,variabl)
 !
   real, dimension (mx,my,mz,mvar) :: f
-  character (len=4) :: variabl
+  character (len=*) :: variabl
   !
   if(ip<=15) print*,'Use POWER=power_spectrum in Makefile.local'
   if(ip<=15) print*,'Use STRUCT_FUNC  = struct_func in Makefile.local'
