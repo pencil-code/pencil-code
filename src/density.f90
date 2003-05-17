74650dead8b5600353368bc266a870067fac9592
Anders Johansen <anders@astro.lu.se>
2003-05-17 13:37:37 +0000

    Added call to subroutine enthblob in Initcond_spec module.

--- src/density.f90
+++ src/density.f90
@@ -1,4 +1,4 @@
-! $Id: density.f90,v 1.81 2003-05-05 18:48:51 brandenb Exp $
+! $Id: density.f90,v 1.82 2003-05-17 13:37:37 anders Exp $
 
 !  This module is used both for the initial condition and during run time.
 !  It contains dlnrho_dt and init_lnrho, among other auxiliary routines.
@@ -69,7 +69,7 @@ module Density
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: density.f90,v 1.81 2003-05-05 18:48:51 brandenb Exp $")
+           "$Id: density.f90,v 1.82 2003-05-17 13:37:37 anders Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -300,6 +300,13 @@ module Density
         !  planet solution of Chavanis (2000)
         !
         call kepvor(f,xx,yy,zz,b_ell,q_ell,gamma,cs20,hh0)
+
+
+      case('enthblob')
+        !
+        !  attempt to produce vortex from enthalpy blob
+        !
+        call enthblob(f,xx,yy,zz,b_ell,q_ell,gamma,cs20,hh0)
 
 
       case('Ferriere'); call ferriere(f)
