cd27bf25aa43076a3a07b3db025e20bbca144266
Anders Johansen <anders@astro.lu.se>
2003-05-17 13:36:57 +0000

    Added subroutine enthblob.

--- src/noinitcond_spec.f90
+++ src/noinitcond_spec.f90
@@ -1,4 +1,4 @@
-!  $Id: noinitcond_spec.f90,v 1.4 2003-04-05 21:39:56 brandenb Exp $
+!  $Id: noinitcond_spec.f90,v 1.5 2003-05-17 13:36:57 anders Exp $
 !
 !  Substitute routines for vortex_solve.f90
 !
@@ -20,5 +20,17 @@ module Initcond_spec
       real :: gamma,cs20
       if(ip==0) print*,f,xx,yy,zz,b_ell,q_ell,gamma,cs20,hh0 ! compiler quiet
     endsubroutine kepvor
+!***********************************************************************
+    subroutine enthblob(f,xx,yy,zz,b_ell,q_ell,gamma,cs20,hh0)
+!
+      use Cdata
+      use General
+
+      real, dimension (mx,my,mz,mvar) :: f
+      real, dimension (mx,my,mz) :: xx,yy,zz
+      real :: q_ell, b_ell, hh0
+      real :: gamma,cs20
+      if(ip==0) print*,f,xx,yy,zz,b_ell,q_ell,gamma,cs20,hh0 ! compiler quiet
+    endsubroutine enthblob
 
 end module Initcond_spec
