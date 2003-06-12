db04ea6ea84f90bef9a8101153e9aa26db5f6064
Antony J. Mee <eemynotna@gmail.com>
2003-06-12 02:03:56 +0000

    An idea... (for Wolfgang's entertainment?)
    This is a first draft of a distributed I/O module using MPI to do the actual
    I/O.  Why?
    
    I suffer a huge bottleneck when writing var files
    CPU activity is negligable and in some cases - with zero or little local
    diskspace - it is network bandwidth causing the problem eg. copson with
    RAID drives mounted with NFS over Myrinet.
    
    So why not let the processors get on with the calculation?
    
    Well, most MPI implementations are not threadsafe so one cannot implement
    their own, nonblocking I/O routines safely.
    
    BUT the MPI standard contains nonblocking I/O routines so why not use them?
    
    Writing large contiguous chunks of ram to 'per processor' files should still
    be quickest.  But write it while the next df is being calculated.
    
    Perhaps a further option exists: if we have sufficient RAM we could provide
    our own buffer (or partial buffer) so writing may continue in the background
    until the next write operation. eg. if we provide a 20% buffer, the write need
    only be 80% complete by the time f is evolved.
    
    Stage 1:     Implement distributed IO using MPI I/O and MPI_COMM_SELF
    (test/debug) communication group -> data/proc*/var.dat
    
    Stage 2:     Move to nonblocking calls with a state machine that will
                 call MPI_WAIT for the file I/O only when absolutely necessary.
    
    Ok, 3am... Good night all!

---
+++ src/io_mpidist.f90
@@ -0,0 +1,540 @@
+! $Id: io_mpidist.f90,v 1.1 2003-06-12 02:03:56 mee Exp $
+
+!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!   io_mpidist.f90   !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!
+
+!!!  IO via MPI2 (i.e. all process write to separate files, e.g.
+!!!  data/proc0/var.dat
+!!!  WITH NON-BLOCKING WRITES
+!!!  12-jun-03/tony: started  (based on Wolfgang's io-mpio.f90 code)
+
+module Io
+
+  use Cdata
+
+  implicit none
+ 
+  interface output              ! Overload the `output' function
+    module procedure output_vect
+    module procedure output_scal
+  endinterface
+
+  interface output_pencil        ! Overload the `output_pencil' function
+    module procedure output_pencil_vect
+    module procedure output_pencil_scal
+  endinterface
+
+  !
+  ! Interface to external C function(s).
+  ! Need to have two different C functions in order to have F90
+  ! interfaces, since a pencil can be either a 1-d or a 2-d array.
+  !
+!   interface output_penciled_vect_c
+!     subroutine output_penciled_vect_c(filename,pencil,&
+!                                       ndim,i,iy,iz,t, &
+!                                       nx,ny,nz,nghost,fnlen)
+!       use Cdata, only: mx
+!       real,dimension(mx,*) :: pencil
+!       real :: t
+!       integer :: ndim,i,iy,iz,nx,ny,nz,nghost,fnlen
+!       character (len=*) :: filename
+!     endsubroutine output_penciled_vect_c
+!   endinterface
+!   !
+!   interface output_penciled_scal_c
+!     subroutine output_penciled_scal_c(filename,pencil,&
+!                                       ndim,i,iy,iz,t, &
+!                                       nx,ny,nz,nghost,fnlen)
+!       use Cdata, only: mx
+!       real,dimension(mx) :: pencil
+!       real :: t
+!       integer :: ndim,i,iy,iz,nx,ny,nz,nghost,fnlen
+!       character (len=*) :: filename
+!     endsubroutine output_penciled_scal_c
+!   endinterface
+  !
+  !  Still not possible with the NAG compiler (`No specific match for
+  !  reference to generic OUTPUT_PENCILED_SCAL_C')
+  !
+  external output_penciled_scal_c
+  external output_penciled_vect_c
+!
+  include 'mpif.h'
+!  include 'mpiof.h'
+!
+  integer :: lun_output=91
+
+  integer, dimension(MPI_STATUS_SIZE) :: status
+! LAM-MPI does not know the MPI2 constant MPI_OFFSET_KIND, 8 is hopfully OK
+!  integer(kind=MPI_OFFSET_KIND) :: dist_zero=0
+  integer(kind=8) :: dist_zero=0
+  integer :: io_filetype_s,io_memtype_s,io_filetype_v,io_memtype_v
+  integer :: fhandle,ierr
+  logical :: io_initialized=.false.
+
+  logical :: lbusy_var = .false.
+
+contains
+
+!***********************************************************************
+    subroutine register_io()
+!
+!  define MPI data types needed for MPI-IO
+!  closely follwing Gropp et al. `Using MPI-2'
+!
+!  20-sep-02/wolf: coded
+!  25-oct-02/axel: removed assignment of datadir; now set in cdata.f90
+!
+      use General
+      use Cdata
+      use Sub
+      use Mpicomm, only: lroot,stop_it
+!
+      logical, save :: first=.true.
+!
+!      if (.not. first) call stop_it('register_io called twice')
+      first = .false.
+!
+!  identify version number
+!
+      if (lroot) call cvs_id("$Id: io_mpidist.f90,v 1.1 2003-06-12 02:03:56 mee Exp $")
+!
+      io_initialized=.true.
+!
+!  initialize datadir and directory_snap (where var.dat and VAR# go)
+!  -- may be overwritten in *.in parameter file
+!
+      directory_snap = ''
+!
+    endsubroutine register_io
+!
+!ajwm need and initialize_io ???
+!
+!
+!***********************************************************************
+    subroutine directory_names()
+!
+!  Set up the directory names:
+!  set directory name for the output (one subdirectory for each processor)
+!  if datadir_snap (where var.dat, VAR# go) is empty, initialize to datadir
+!
+!  02-oct-2002/wolf: coded
+!
+      use Cdata, only: datadir,directory,directory_snap
+      use Mpicomm, only: iproc
+      use General, only: chn, safe_character_assign
+!
+      character (len=5) :: chproc=''
+!
+      call chn(iproc,chproc)
+      call safe_character_assign(directory, trim(datadir)//'/proc'//chproc)
+!
+!  check whether directory_snap contains `/proc0' -- if so, revert to the
+!  default name.
+!  Rationale: if directory_snap was not explicitly set in start.in, it
+!  will be written to param.nml as 'data/proc0', but this should in fact
+!  be data/procN on processor N.
+!
+      if ((directory_snap == '') .or. (index(directory_snap,'proc0')>0)) then
+        directory_snap = directory
+      endif
+!
+    endsubroutine directory_names
+!***********************************************************************
+    subroutine commit_io_type_vect(nn,a,mode)
+!
+!  For a new value of nn, commit MPI types needed for output_vect(). If
+!  called with the same value of nn as the previous time, do nothing.
+!  12-jun-03/tony: coded
+!
+!      use Cdata
+!
+      integer, dimension(9) :: blen, datatype
+      integer (kind=MPI_ADDRESS_KIND), dimension(9) :: disp
+      integer,save :: lastnn=-1, lastmode ! value of nn at previous call
+      real, dimension (mx,my,mz,nn) :: a
+      integer :: nn, mode, mode_vars
+      integer (kind=MPI_ADDRESS_KIND) :: a_addr, var_addr
+      integer (kind=MPI_ADDRESS_KIND), save :: lasta_addr=-1
+
+!               a    t   x   y   z dx dy dz deltay
+
+!
+      call MPI_GET_ADDRESS(a,a_addr,ierr)
+
+      if (nn /= lastnn .or. a_addr/=lasta_addr .or. mode.ne.lastmode) then
+        if (lastnn > 0) then
+          ! free old types, so we can re-use them
+          call MPI_TYPE_FREE(io_memtype_v, ierr)
+          call MPI_TYPE_FREE(io_filetype_v, ierr)
+        endif
+  
+        mode_vars=8
+        if (lshear) mode_vars=9
+        if (mode.eq.0) mode_vars=1
+
+        blen     = (/ mw*nn, 1, mx, my, mz, 1, 1, 1,     1/)
+        datatype(:) = MPI_REAL
+        disp(1)=0
+        call MPI_GET_ADDRESS(t,var_addr,ierr)
+        disp(2)=var_addr - a_addr
+        call MPI_GET_ADDRESS(x,var_addr,ierr)
+        disp(3)=var_addr - a_addr
+        call MPI_GET_ADDRESS(y,var_addr,ierr)
+        disp(4)=var_addr - a_addr
+        call MPI_GET_ADDRESS(z,var_addr,ierr)
+        disp(5)=var_addr - a_addr
+        call MPI_GET_ADDRESS(dx,var_addr,ierr)
+        disp(6)=var_addr - a_addr
+        call MPI_GET_ADDRESS(dy,var_addr,ierr)
+        disp(7)=var_addr - a_addr
+        call MPI_GET_ADDRESS(dz,var_addr,ierr)
+        disp(8)=var_addr - a_addr
+        call MPI_GET_ADDRESS(deltay,var_addr,ierr)
+        disp(9)=var_addr - a_addr
+        
+!
+        call MPI_TYPE_CREATE_STRUCT(mode_vars, blen(1:mode_vars), disp(1:mode_vars), &
+                                    datatype(1:mode_vars), io_memtype_v, ierr)
+        call MPI_TYPE_COMMIT(io_memtype_v,ierr)
+
+        call MPI_TYPE_CONTIGUOUS(sum(blen(1:mode_vars)),MPI_REAL,io_filetype_v,ierr)
+        call MPI_TYPE_COMMIT(io_filetype_v,ierr)
+
+        lasta_addr=a_addr
+      endif
+!
+    endsubroutine commit_io_type_vect
+!***********************************************************************
+    subroutine commit_io_type_scal(nn,a,mode)
+!
+!  For a new value of nn, commit MPI types needed for output_vect(). If
+!  called with the same value of nn as the previous time, do nothing.
+!  12-jun-03/tony: coded
+!
+!      use Cdata
+!
+      integer, dimension(9) :: blen, datatype
+      integer (kind=MPI_ADDRESS_KIND), dimension(9) :: disp
+      integer,save :: lastnn=-1, lastmode ! value of nn at previous call
+      real, dimension (mx,my,mz,nn) :: a
+      integer :: nn, mode, mode_vars
+      integer (kind=MPI_ADDRESS_KIND) :: a_addr, var_addr
+      integer (kind=MPI_ADDRESS_KIND), save :: lasta_addr=-1
+
+!               a    t   x   y   z dx dy dz deltay
+
+!
+      call MPI_GET_ADDRESS(a,a_addr,ierr)
+
+      if (nn /= lastnn .or. a_addr/=lasta_addr .or. mode.ne.lastmode) then
+        if (lastnn > 0) then
+          ! free old types, so we can re-use them
+          call MPI_TYPE_FREE(io_memtype_s, ierr)
+          call MPI_TYPE_FREE(io_filetype_s, ierr)
+        endif
+  
+        mode_vars=8
+        if (lshear) mode_vars=9
+        if (mode.eq.0) mode_vars=1
+
+        blen     = (/ mw*nn, 1, mx, my, mz, 1, 1, 1,     1/)
+        datatype(:) = MPI_REAL
+        disp(1)=0
+        call MPI_GET_ADDRESS(t,var_addr,ierr)
+        disp(2)=var_addr - a_addr
+        call MPI_GET_ADDRESS(x,var_addr,ierr)
+        disp(3)=var_addr - a_addr
+        call MPI_GET_ADDRESS(y,var_addr,ierr)
+        disp(4)=var_addr - a_addr
+        call MPI_GET_ADDRESS(z,var_addr,ierr)
+        disp(5)=var_addr - a_addr
+        call MPI_GET_ADDRESS(dx,var_addr,ierr)
+        disp(6)=var_addr - a_addr
+        call MPI_GET_ADDRESS(dy,var_addr,ierr)
+        disp(7)=var_addr - a_addr
+        call MPI_GET_ADDRESS(dz,var_addr,ierr)
+        disp(8)=var_addr - a_addr
+        call MPI_GET_ADDRESS(deltay,var_addr,ierr)
+        disp(9)=var_addr - a_addr
+        
+!
+        call MPI_TYPE_CREATE_STRUCT(mode_vars, blen(1:mode_vars), disp(1:mode_vars), &
+                                    datatype(1:mode_vars), io_memtype_s, ierr)
+        call MPI_TYPE_COMMIT(io_memtype_s,ierr)
+
+        call MPI_TYPE_CONTIGUOUS(sum(blen(1:mode_vars)),MPI_REAL,io_filetype_s,ierr)
+        call MPI_TYPE_COMMIT(io_filetype_s,ierr)
+
+        lasta_addr=a_addr
+      endif
+!
+    endsubroutine commit_io_type_scal
+!***********************************************************************
+    subroutine input(file,a,nn,mode)
+!
+!  read snapshot file, possibly with mesh and time (if mode=1)
+!  11-apr-97/axel: coded
+!
+      use Cdata
+      use Mpicomm, only: lroot,stop_it
+!
+      character (len=*) :: file
+      integer :: nn,mode                  ,i
+      real, dimension (mx,my,mz,nn) :: a
+!
+      if (ip<=8) print*,'INPUT: mx,my,mz,nn=',mx,my,mz,nn
+      if (.not. io_initialized) &
+           call stop_it("INPUT: Need to call init_io first")
+!
+      call commit_io_type_vect(nn,a,mode)
+!
+!  open file and set view (specify which file positions we can access)
+!
+      call MPI_FILE_OPEN(MPI_COMM_SELF, file, &
+               MPI_MODE_RDONLY, &
+               MPI_INFO_NULL, fhandle, ierr)
+      call MPI_FILE_SET_VIEW(fhandle, dist_zero, io_filetype_v, io_filetype_v, &
+               "native", MPI_INFO_NULL, ierr)
+!
+!  read data
+!
+      call MPI_FILE_READ(fhandle, a, 1, io_memtype_v, status, ierr)
+      call MPI_FILE_CLOSE(fhandle, ierr)
+      
+!
+    endsubroutine input
+!***********************************************************************
+    subroutine output_vect(file,a,nn,noclose)
+!
+!  write snapshot file; currently without ghost zones and any meta data
+!  like time, etc.
+!    Looks like we nee to commit the MPI type anew each time we are called,
+!  since nn may vary.
+!  20-sep-02/wolf: coded
+!
+      use Cdata
+      use Mpicomm, only: lroot,stop_it
+!
+      logical, intent(in), optional :: noclose
+      integer :: nn
+      real, dimension (mx,my,mz,nn) :: a
+      character (len=*) :: file
+!
+      if ((ip<=8) .and. lroot) print*,'OUTPUT_VECTOR: nn =', nn
+      if (.not. io_initialized) &
+           call stop_it("OUTPUT: Need to call init_io first")
+!
+      call commit_io_type_vect(nn,a,1)
+      !
+      !  open file and set view (specify which file positions we can access)
+      !
+      call MPI_FILE_OPEN(MPI_COMM_SELF, file, &
+               ior(MPI_MODE_CREATE,MPI_MODE_WRONLY), &
+               MPI_INFO_NULL, fhandle, ierr)
+      call MPI_FILE_SET_VIEW(fhandle, dist_zero, MPI_REAL, io_filetype_v, &
+               "native", MPI_INFO_NULL, ierr)
+      !
+      !  write data
+      !
+      call MPI_FILE_WRITE(fhandle, a, 1, io_memtype_v, status, ierr)
+      call MPI_FILE_CLOSE(fhandle, ierr)
+!
+    endsubroutine output_vect
+!***********************************************************************
+    subroutine output_scal(file,a,nn,noclose)
+!
+!  write snapshot file; currently without ghost zones and any meta data
+!  like time, etc.
+!  20-sep-02/wolf: coded
+!
+      use Cdata
+      use Mpicomm, only: lroot,stop_it
+!
+      logical, intent(in), optional :: noclose
+      real, dimension (mx,my,mz) :: a
+      integer :: nn
+      character (len=*) :: file
+
+      if ((ip<=8) .and. lroot) print*,'OUTPUT_SCALAR'
+      if (.not. io_initialized) &
+           call stop_it("OUTPUT: Need to call init_io first")
+      if (nn /= 1) call stop_it("OUTPUT called with scalar field, but nn/=1")
+!
+!  open file and set view (specify which file positions we can access)
+!
+      call commit_io_type_scal(nn,a,1)
+
+      call MPI_FILE_OPEN(MPI_COMM_SELF, file, &
+               ior(MPI_MODE_CREATE,MPI_MODE_WRONLY), &
+               MPI_INFO_NULL, fhandle, ierr)
+      call MPI_FILE_SET_VIEW(fhandle, dist_zero, io_filetype_s, io_filetype_s, &
+               "native", MPI_INFO_NULL, ierr)
+!
+!  write data
+!
+      call MPI_FILE_WRITE(fhandle, a, 1, io_memtype_s, status, ierr)
+      call MPI_FILE_CLOSE(fhandle, ierr)
+!
+    endsubroutine output_scal
+!***********************************************************************
+    subroutine output_auxiliary(lun_output,nn1,nn2,a)
+!
+!  write auxiliary information into snapshot file
+!  26-may-03/axel: adapted from output_vect
+!
+      use Cdata
+!
+      integer :: lun_output,nn1,nn2
+      real, dimension (mx,my,mz,nn1+nn2) :: a
+      logical :: lauxiliary
+!
+      if (lroot) print*,'ERROR: OUTPUT AUXILLIARY NOT IMPLEMENTED FOR IO_MPIDIST.F90'
+!
+!  determine whether we want to write auxiliary output
+!  (currently we always do this provided maux>0)
+!
+!      lauxiliary=(maux>0)
+!      if(lauxiliary) write(lun_output) a(:,:,:,nn1+1:nn1+1+nn2)
+!
+    endsubroutine output_auxiliary
+!***********************************************************************
+    subroutine output_pencil_vect(file,a,ndim)
+!
+!  Write snapshot file of penciled vector data (for debugging).
+!  Wrapper to the C routine output_penciled_vect_c.
+!
+!  15-feb-02/wolf: coded
+!
+      use Cdata
+      use Mpicomm, only: imn,mm,nn
+!
+      integer :: ndim
+      real, dimension (nx,ndim) :: a
+      character (len=*) :: file
+!
+      if (ip<9.and.lroot.and.imn==1) &
+           print*,'output_pencil_vect('//file//'): ndim=',ndim
+!
+      if (headt .and. (imn==1)) print*, &
+           'OUTPUT_PENCIL: Writing to ', trim(file), &
+           ' for debugging -- this may slow things down'
+!
+       call output_penciled_vect_c(file, a, ndim, &
+                                   imn, mm(imn), nn(imn), t, &
+                                   nx, ny, nz, nghost, len(file))
+!
+    endsubroutine output_pencil_vect
+!***********************************************************************
+    subroutine output_pencil_scal(file,a,ndim)
+!
+!  Write snapshot file of penciled scalar data (for debugging).
+!  Wrapper to the C routine output_penciled_scal_c.
+!
+!  15-feb-02/wolf: coded
+!
+      use Cdata
+      use Mpicomm, only: imn,mm,nn,lroot,stop_it
+
+!
+      integer :: ndim
+      real, dimension (nx) :: a
+      character (len=*) :: file
+!
+      if ((ip<=8) .and. lroot .and. imn==1) &
+           print*,'output_pencil_scal('//file//')'
+!
+      if (ndim /= 1) &
+           call stop_it("OUTPUT called with scalar field, but ndim/=1")
+!
+      if (headt .and. (imn==1)) print*, &
+           'OUTPUT_PENCIL: Writing to ', trim(file), &
+           ' for debugging -- this may slow things down'
+!
+      call output_penciled_scal_c(file, a, ndim, &
+                                  imn, mm(imn), nn(imn), t, &
+                                  nx, ny, nz, nghost, len(file))
+!
+    endsubroutine output_pencil_scal
+!***********************************************************************
+    subroutine outpus(file,a,nn)
+!
+!  write snapshot file, always write mesh and time, could add other things
+!  11-oct-98/axel: adapted
+!
+      use Cdata
+      use Mpicomm, only: lroot,stop_it
+!
+      integer :: nn
+      character (len=*) :: file
+      real, dimension (mx,my,mz,nn) :: a
+!
+      call stop_it("OUTPUS doesn't work with io_mpio yet -- but wasn't used anyway")
+!
+      open(1,file=file,form='unformatted')
+      write(1) a(l1:l2,m1:m2,n1:n2,:)
+      write(1) t,x,y,z,dx,dy,dz,deltay
+      close(1)
+    endsubroutine outpus
+!***********************************************************************
+    subroutine wgrid (file)
+!
+!  Write processor-local part of grid coordinates.
+!  21-jan-02/wolf: coded
+!
+      use Cdata, only: t,x,y,z,dx,dy,dz
+!
+      character (len=*) :: file
+!
+      open(1,FILE=file,FORM='unformatted')
+      write(1) t,x,y,z,dx,dy,dz
+      write(1) dx,dy,dz
+!
+    endsubroutine wgrid
+!***********************************************************************
+    subroutine rgrid (file)
+!
+!  Read processor-local part of grid coordinates.
+!  21-jan-02/wolf: coded
+!
+      use Cdata
+!
+      real :: tdummy
+      character (len=*) :: file
+!
+      open(1,FILE=file,FORM='unformatted')
+      read(1) tdummy,x,y,z,dx,dy,dz
+      read(1) dx,dy,dz
+!
+      dxmin = minval( (/dx,dy,dz/), MASK=((/nxgrid,nygrid,nzgrid/) /= 1) )
+      dxmax = maxval( (/dx,dy,dz/), MASK=((/nxgrid,nygrid,nzgrid/) /= 1) )
+
+      Lx=dx*nx*nprocx
+      Ly=dy*ny*nprocy
+      Lz=dz*nz*nprocz
+!
+      if (ip<=4) print*
+      if (ip<=4) print*,'dx,dy,dz=',dx,dy,dz
+      if (ip<=4) print*,'dxmin,dxmax=',dxmin,dxmax
+!
+    endsubroutine rgrid
+!***********************************************************************
+    subroutine wtime(file,tau)
+!
+      real :: tau
+      character (len=*) :: file
+!
+      if (.false.) print*,tau,file
+    endsubroutine wtime
+!***********************************************************************
+    subroutine rtime(file,tau)
+!
+      real :: tau
+      character (len=*) :: file
+!
+      if (.false.) print*,tau,file
+    endsubroutine rtime
+!***********************************************************************
+
+endmodule Io
