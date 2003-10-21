f3832df5347675735841da2ff9ae26943e610f2f
David McMillan <david.mcmillan@newcastle.ac.uk>
2003-10-21 11:30:46 +0000

    Wondering about the checks on cs2bot and cs2top, which are done for all cases, even those that do not require the variables (lines 439-445).

--- src/density.f90
+++ src/density.f90
@@ -1,4 +1,4 @@
-! $Id: density.f90,v 1.126 2003-10-20 16:27:20 dobler Exp $
+! $Id: density.f90,v 1.127 2003-10-21 11:30:46 mcmillan Exp $
 
 !  This module is used both for the initial condition and during run time.
 !  It contains dlnrho_dt and init_lnrho, among other auxiliary routines.
@@ -81,7 +81,7 @@ module Density
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: density.f90,v 1.126 2003-10-20 16:27:20 dobler Exp $")
+           "$Id: density.f90,v 1.127 2003-10-21 11:30:46 mcmillan Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -436,6 +436,9 @@ module Density
       endif
 !
 !  check that cs2bot,cs2top are ok
+!  dave:
+!  these variables are not always used, but always checked; 
+!  is there a way to limit this check to the appropriate cases?
 !
       if(lroot) print*,'init_lnrho: cs2bot,cs2top=',cs2bot,cs2top
       if(lroot) print*, &
