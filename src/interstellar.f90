645c9e9168253dad7cdd1f695c8a0c6a45cd534b
Graeme R. Sarson <g.r.sarson@ncl.ac.uk>
2003-10-23 16:47:26 +0000

    Split the writing of headers for sn_series.dat into 2 lines; the ifc compiler was fussy about the limit of 132 characters per line.

--- src/interstellar.f90
+++ src/interstellar.f90
@@ -1,4 +1,4 @@
-! $Id: interstellar.f90,v 1.60 2003-10-20 17:39:21 mee Exp $
+! $Id: interstellar.f90,v 1.61 2003-10-23 16:47:26 ngrs Exp $
 
 !  This modules contains the routines for SNe-driven ISM simulations.
 !  Still in development. 
@@ -115,7 +115,7 @@ module Interstellar
 !  identify version number
 !
       if (lroot) call cvs_id( &
-           "$Id: interstellar.f90,v 1.60 2003-10-20 17:39:21 mee Exp $")
+           "$Id: interstellar.f90,v 1.61 2003-10-23 16:47:26 ngrs Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -202,7 +202,8 @@ module Interstellar
       if (lroot.and. (.not. lstart)) then
          open(1,file=trim(datadir)//'/sn_series.dat',position='append')
          write(1,'("# ",A)')  &
-          '--it-----t----------itype_SN---iproc_SN------x_SN-----------y_SN-----------z_SN-----------rho_SN---------EE_SN-----l_SN--m_SN--n_SN-----'
+          '--it-----t----------itype_SN---iproc_SN------x_SN-----------y_SN-----------z_SN-----------rho_SN---------EE_SN&
+          &-----l_SN--m_SN--n_SN-----'
          close(1)
       endif
 !
