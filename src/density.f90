403094d2d8b7a8124eb90ee8ea8e3be3cfa1acc8
Anders Johansen <anders@astro.lu.se>
2003-11-18 16:24:17 +0000

    Removed some obsolete parameters from the 'baroclinic' call.

--- src/density.f90
+++ src/density.f90
@@ -1,4 +1,4 @@
-! $Id: density.f90,v 1.136 2003-11-16 15:28:59 ajohan Exp $
+! $Id: density.f90,v 1.137 2003-11-18 16:24:17 ajohan Exp $
 
 !  This module is used both for the initial condition and during run time.
 !  It contains dlnrho_dt and init_lnrho, among other auxiliary routines.
@@ -29,14 +29,13 @@ module Density
   real :: radius_lnrho=.5,kx_lnrho=1.,ky_lnrho=1.,kz_lnrho=1.
   real :: eps_planet=.5
   real :: b_ell=1., q_ell=5., hh0=0., rbound=1.
-  real :: TT_til0,alpha_TT,dlnrhobdx
+  real :: dlnrhobdx,dssdz
   real :: mpoly=1.5
   real :: mpoly0=1.5,mpoly1=1.5,mpoly2=1.5
   real, dimension(3) :: gradlnrho0=(/0.,0.,0./)
-  integer:: isothtop=0,hires_q_z
+  integer:: isothtop=0
   logical :: lupw_lnrho=.false.
   character (len=labellen) :: initlnrho='nothing', initlnrho2='nothing'
-  character (len=labellen) :: t_fct_type='nothing'
   complex :: coeflnrho=0.
 
   namelist /density_init_pars/ &
@@ -46,7 +45,7 @@ module Density
        b_ell,q_ell,hh0,rbound, &
        mpoly, &
        kx_lnrho,ky_lnrho,kz_lnrho,amplrho,coeflnrho, &
-       t_fct_type,TT_til0,alpha_TT,dlnrhobdx,hires_q_z
+       dlnrhobdx,dssdz
 
   namelist /density_run_pars/ &
        cs0,rho0,gamma,cdiffrho,diffrho,diffrho_shock,gradlnrho0, &
@@ -85,7 +84,7 @@ module Density
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: density.f90,v 1.136 2003-11-16 15:28:59 ajohan Exp $")
+           "$Id: density.f90,v 1.137 2003-11-18 16:24:17 ajohan Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -408,8 +407,7 @@ module Density
         !
         !  Baroclinic initial condition
         !
-        call baroclinic(f,xx,yy,zz,gamma,t_fct_type,hires_q_z, &
-            TT_til0,alpha_TT,rho0,dlnrhobdx)
+        call baroclinic(f,xx,yy,zz,gamma,rho0,dlnrhobdx,dssdz,cs20)
 
       
       case('Ferriere'); if(lroot) print*,'init_lnrho: Ferriere set in entropy'
