57596ab1b59f0cab7f61b359ac785986323ea3c1
Anders Johansen <anders@astro.lu.se>
2003-11-19 15:50:44 +0000

    Some new input parameters to baroclinic (co1_ss,co2_ss). Removed obsolete parameter dssdz.

--- src/density.f90
+++ src/density.f90
@@ -1,4 +1,4 @@
-! $Id: density.f90,v 1.137 2003-11-18 16:24:17 ajohan Exp $
+! $Id: density.f90,v 1.138 2003-11-19 15:50:44 ajohan Exp $
 
 !  This module is used both for the initial condition and during run time.
 !  It contains dlnrho_dt and init_lnrho, among other auxiliary routines.
@@ -29,7 +29,7 @@ module Density
   real :: radius_lnrho=.5,kx_lnrho=1.,ky_lnrho=1.,kz_lnrho=1.
   real :: eps_planet=.5
   real :: b_ell=1., q_ell=5., hh0=0., rbound=1.
-  real :: dlnrhobdx,dssdz
+  real :: dlnrhobdx,co1_ss,co2_ss
   real :: mpoly=1.5
   real :: mpoly0=1.5,mpoly1=1.5,mpoly2=1.5
   real, dimension(3) :: gradlnrho0=(/0.,0.,0./)
@@ -45,7 +45,7 @@ module Density
        b_ell,q_ell,hh0,rbound, &
        mpoly, &
        kx_lnrho,ky_lnrho,kz_lnrho,amplrho,coeflnrho, &
-       dlnrhobdx,dssdz
+       dlnrhobdx,co1_ss,co2_ss
 
   namelist /density_run_pars/ &
        cs0,rho0,gamma,cdiffrho,diffrho,diffrho_shock,gradlnrho0, &
@@ -84,7 +84,7 @@ module Density
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: density.f90,v 1.137 2003-11-18 16:24:17 ajohan Exp $")
+           "$Id: density.f90,v 1.138 2003-11-19 15:50:44 ajohan Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -407,7 +407,7 @@ module Density
         !
         !  Baroclinic initial condition
         !
-        call baroclinic(f,xx,yy,zz,gamma,rho0,dlnrhobdx,dssdz,cs20)
+        call baroclinic(f,xx,yy,zz,gamma,rho0,dlnrhobdx,co1_ss,co2_ss,cs20)
 
       
       case('Ferriere'); if(lroot) print*,'init_lnrho: Ferriere set in entropy'
