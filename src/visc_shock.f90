62e416f372a9b24c0d165dca712669d46315a2c5
Tobias Heinemann <tobias.heinemann@gmail.com>
2003-11-28 15:50:25 +0000

    don't call max_mn_name(...,i_dtnu,...) twice in calc_viscous_force

--- src/visc_shock.f90
+++ src/visc_shock.f90
@@ -1,4 +1,4 @@
-! $Id: visc_shock.f90,v 1.43 2003-11-28 09:56:28 theine Exp $
+! $Id: visc_shock.f90,v 1.44 2003-11-28 15:50:25 theine Exp $
 
 !  This modules implements viscous heating and diffusion terms
 !  here for shock viscosity nu_total = nu + nu_shock*dx*smooth(max5(-(div u)))) 
@@ -68,7 +68,7 @@ module Viscosity
 !  identify version number
 !
       if (lroot) call cvs_id( &
-           "$Id: visc_shock.f90,v 1.43 2003-11-28 09:56:28 theine Exp $")
+           "$Id: visc_shock.f90,v 1.44 2003-11-28 15:50:25 theine Exp $")
 !
 ! Check we aren't registering too many auxiliary variables
 !
@@ -568,7 +568,7 @@ module Viscosity
 !  set viscous time step
 !
       if (ldiagnos.and.i_dtnu/=0) then
-        call max_mn_name(spread(nu,1,nx)/dxmin**2,i_dtnu,l_dt=.true.)
+        call max_mn_name(spread(maxeffectivenu,1,nx)/dxmin**2,i_dtnu,l_dt=.true.)
       endif
 !
 !  shock viscosity
@@ -587,9 +587,6 @@ module Viscosity
             call multsv_add_mn(fvisc,nu_shock*divu,gshock,tmp)
             fvisc = tmp + 2*nu*sglnrho+nu*(del2u+1./3.*graddivu)
             maxdiffus=amax1(maxdiffus,maxeffectivenu)
-           if (ldiagnos.and.i_dtnu/=0) then
-             call max_mn_name(spread(maxeffectivenu,1,nx)/dxmin**2,i_dtnu,l_dt=.true.)
-           endif
          else
             if(lfirstpoint) &
                  print*,"ldensity better be .true. for ivisc='nu-const'"
