912995c9926fc6750b953ddd2fe70620539e9c8c
Andrew P. Snodin <andrew.snodin@ncl.ac.uk>
2003-12-04 23:23:37 +0000

    changed iecr to ilnecr removed duplicate initialisation cc_pdf=.false

--- src/cdata.f90
+++ src/cdata.f90
@@ -1,4 +1,4 @@
-! $Id: cdata.f90,v 1.193 2003-12-04 09:03:38 brandenb Exp $
+! $Id: cdata.f90,v 1.194 2003-12-04 23:23:37 snod Exp $
 
 module Cdata
 
@@ -60,7 +60,7 @@ module Cdata
   integer :: iuud=0,iudx=0,iudy=0,iudz=0,ilnrhod=0,igg=0,igx=0,igy=0,igz=0
   integer :: iaa=0,iax=0,iay=0,iaz=0
   integer :: ie=0,iff=0,ifx=0,ify=0,ifz=0,idd=0,ishock=0,iyH=0,ihyper3=0
-  integer :: iecr=0
+  integer :: ilnecr=0
   integer :: iQrad=0,iSrad=0,ikappa=0,ilnTT=0
   integer :: nt=1000000,it1=10
   integer :: it=1,itsub,ix,iy,iz,iz2
@@ -143,7 +143,6 @@ module Cdata
   logical :: test_nonblocking=.false.,onedall=.false.
   logical :: lsfu=.false.,lsfb=.false.,lsfz1=.false.,lsfz2=.false.
   logical :: lpdfu=.false.,lpdfb=.false.,lpdfz1=.false.,lpdfz2=.false.
-  logical :: cc_pdf=.false.
 !  logical, dimension(mvar + maux) :: lsnap ! flag which variables should be written
                                              ! to the snapshots
 
