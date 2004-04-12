41cc4a9fe5ead8be51b9ebf9f56098f3a83fdcad
Anders Johansen <anders@astro.lu.se>
2004-04-12 13:17:44 +0000

    Must declare nu_turb here to avoid trouble with DUSTDENSITY and NOENTROPY at the same time.

--- src/noentropy.f90
+++ src/noentropy.f90
@@ -1,4 +1,4 @@
-! $Id: noentropy.f90,v 1.57 2004-04-11 05:33:59 brandenb Exp $
+! $Id: noentropy.f90,v 1.58 2004-04-12 13:17:44 ajohan Exp $
 
 !** AUTOMATIC CPARAM.INC GENERATION ****************************
 ! Declare (for generation of cparam.inc) the number of f array
@@ -29,6 +29,7 @@ module Entropy
   real :: hcond0=0.,hcond1=impossible,chi=impossible
   real :: Fbot=impossible,FbotKbot=impossible,Kbot=impossible
   real :: Ftop=impossible,FtopKtop=impossible
+  real :: nu_turb=0.
   logical :: lmultilayer=.true.
   logical :: lcalc_heatcond_constchi=.false.
 
@@ -57,7 +58,7 @@ module Entropy
 !  identify version number
 !
       if (lroot) call cvs_id( &
-           "$Id: noentropy.f90,v 1.57 2004-04-11 05:33:59 brandenb Exp $")
+           "$Id: noentropy.f90,v 1.58 2004-04-12 13:17:44 ajohan Exp $")
 !
     endsubroutine register_entropy
 !***********************************************************************
