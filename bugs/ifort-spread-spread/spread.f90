7eab13792efe3d9201be104503a77d3f2b74ff60
Axel Brandenburg <brandenb@nordita.org>
2004-04-28 16:41:59 +0000

    Automatically generate spread_N.f90 files, based on template file spread.f90

---
+++ bugs/ifort-spread-spread/spread.f90
@@ -0,0 +1,15 @@
+program test
+
+  integer, parameter        :: mx=<N>, my=2, mz=2
+  real, dimension(mx,my,mz) :: f
+  real, dimension(mx)       :: fx=1.0
+
+  write(0,*) 'spread_<N>: Starting'
+
+  f(:,:,:) = f(:,:,:) + spread(spread(fx,2,my),3,mz)
+
+  write(0,*) 'spread_<N>: <f> = ', sum(f)/mx/my/mz 
+  write(0,*) 'spread_<N>: Done.' 
+  write(0,*)
+
+endprogram test
