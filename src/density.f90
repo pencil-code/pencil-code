926bfda26033a6670aafeb780623177c06596f2f
Wolfgang Dobler <wdobler@gmail.com>
2004-05-18 15:38:24 +0000

    Removed calls to initialize_gravity, since that is called in register.f90 already. Auto-tests do not show any anomaly.

--- src/density.f90
+++ src/density.f90
@@ -1,4 +1,4 @@
-! $Id: density.f90,v 1.160 2004-04-30 09:30:49 ajohan Exp $
+! $Id: density.f90,v 1.161 2004-05-18 15:38:24 dobler Exp $
 
 !  This module is used both for the initial condition and during run time.
 !  It contains dlnrho_dt and init_lnrho, among other auxiliary routines.
@@ -90,7 +90,7 @@ module Density
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: density.f90,v 1.160 2004-04-30 09:30:49 ajohan Exp $")
+           "$Id: density.f90,v 1.161 2004-05-18 15:38:24 dobler Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -267,7 +267,7 @@ module Density
           if (lroot) print*, &
                'init_lnrho: radial density stratification (assumes s=const)'
 !ajwm - here's the init call that needs sorting!
-          call initialize_gravity()     ! get coefficients cpot(1:5)
+!          call initialize_gravity()     ! get coefficients cpot(1:5)
 
           call potential(xx,yy,zz,pot,POT0=pot0) ! gravity potential
           call output(trim(directory)//'/pot.dat',pot,1)
@@ -293,7 +293,7 @@ module Density
         if (lgravr) then
           if (lroot) print*, &
                'init_lnrho: isentropic star with isothermal atmosphere'
-          call initialize_gravity()     ! get coefficients cpot(1:5)
+!          call initialize_gravity()     ! get coefficients cpot(1:5)
           call potential(xx,yy,zz,pot,POT0=pot0) ! gravity potential
           call output(trim(directory)//'/pot.dat',pot,1)
           !
