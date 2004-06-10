a67fd01ec168888a2e46783a5b56cb55e538d07a
Wolfgang Dobler <wdobler@gmail.com>
2004-06-10 00:46:49 +0000

    Added get_global,set_global routines

--- src/global_rr.f90
+++ src/global_rr.f90
@@ -1,4 +1,4 @@
-! $Id: global_rr.f90,v 1.3 2002-06-01 02:56:21 brandenb Exp $
+! $Id: global_rr.f90,v 1.4 2004-06-10 00:46:49 dobler Exp $
 
 module Global
 
@@ -14,10 +14,84 @@ module Global
 
   implicit none
 
+  interface set_global
+    module procedure set_global_vect
+    module procedure set_global_scal
+  endinterface
+
+  interface get_global
+    module procedure get_global_vect
+    module procedure get_global_scal
+  endinterface
+
   real, dimension (mx,my,mz) :: rr
 
   contains
 
+!***********************************************************************
+    subroutine set_global_vect(var,m,n,label)
+!
+!  set (m-n)-pencil of global vector variable identified by LABEL
+!  [dummy routine]
+!
+      use Mpicomm, only: stop_it
+!
+      real, dimension(nx,3) :: var
+      integer :: m,n
+      character (len=*) :: label
+!
+      call stop_it("GLOBAL_RR: set_global_vect not implemented")
+      if (ip == 0) print*, var(1,1),m,n,label ! keep compiler quiet
+!
+    endsubroutine set_global_vect
+!***********************************************************************
+    subroutine set_global_scal(var,m,n,label)
+!
+!  set (m,n)-pencil of the global scalar variable identified by LABEL
+!  [dummy routine]
+!
+      use Mpicomm, only: stop_it
+!
+      real, dimension(nx) :: var
+      integer :: m,n
+      character (len=*) :: label
+!
+      call stop_it("GLOBAL_RR: set_global_scal not implemented")
+      if (ip == 0) print*, var(1),m,n,label ! keep compiler quiet
+!
+    endsubroutine set_global_scal
+!***********************************************************************
+    subroutine get_global_vect(var,m,n,label)
+!
+!  get (m,n)-pencil of the global vector variable identified by LABEL
+!  [dummy routine]
+!
+      use Mpicomm, only: stop_it
+!
+      real, dimension(nx,3) :: var
+      integer :: m,n
+      character (len=*) :: label
+!
+      call stop_it("GLOBAL_RR: get_global_vect not implemented")
+      if (ip == 0) print*, var(1,1),m,n,label ! keep compiler quiet
+!
+    endsubroutine get_global_vect
+!***********************************************************************
+    subroutine get_global_scal(var,m,n,label)
+!
+!  set (m,n)-pencil of the global scalar variable identified by LABEL
+!  [dummy routine]
+!
+      use Mpicomm, only: stop_it
+!
+      real, dimension(nx) :: var
+      integer :: m,n
+      character (len=*) :: label
+!
+      call stop_it("GLOBAL_RR: get_global_scla not implemented")
+      if (ip == 0) print*, var(1),m,n,label ! keep compiler quiet
+!
+    endsubroutine get_global_scal
 !***********************************************************************
     subroutine wglobal()
 !
