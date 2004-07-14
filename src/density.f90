fdfa97fad0846b045002d622ae3f1cc9012f0957
Wolfgang Dobler <wdobler@gmail.com>
2004-07-14 14:38:25 +0000

    Flagged that the cdiffrho --> diffrho stuff is not working with RELOAD. In addition, this involves xyzmin and gives ludicrous numbers for me in 2d.
    A general question about shock viscosity:
    Why is it that one should give diffrho, rather than cdiffrhor?
    Or in terms of viscosity: why on Earth does one have to specify the
    dimensional nu_shock, rather than some non-dimensional c_nu_shock?
    nu_shock needs to be adjusted each time one changes the resolution,
    while c_nu_shock would remain the same in many cases.

--- src/density.f90
+++ src/density.f90
@@ -1,4 +1,4 @@
-! $Id: density.f90,v 1.173 2004-07-05 22:19:49 theine Exp $
+! $Id: density.f90,v 1.174 2004-07-14 14:38:25 dobler Exp $
 
 !  This module is used both for the initial condition and during run time.
 !  It contains dlnrho_dt and init_lnrho, among other auxiliary routines.
@@ -90,7 +90,7 @@ module Density
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: density.f90,v 1.173 2004-07-05 22:19:49 theine Exp $")
+           "$Id: density.f90,v 1.174 2004-07-14 14:38:25 dobler Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -126,6 +126,10 @@ module Density
       logical :: lstarting
 !
       if(diffrho==0.) then
+        !
+        !  FIXME:
+        !  This will not work with RELOAD if cdiffrho is changed in run.in
+        !
         diffrho=cdiffrho*dxmin*cs0
       endif
 !
