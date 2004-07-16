0f485aa6c2b5229119fd08ab8017d64231d4e068
Nils Erland L. Haugen <nilshau@ntnu.no>
2004-07-16 13:42:49 +0000

    Added module that will handle the correct smagorinsky viscosity. The simplified smagorinsky viscosity is still in visc_const. 2S.gradnu_smag is still not implemented though so currently ivisc=smagorinsky and ivisc=smagorinsky_simplified is the same.

---
+++ src/visc_smagorinsky.f90
@@ -0,0 +1,358 @@
+! $Id: visc_smagorinsky.f90,v 1.1 2004-07-16 13:42:49 nilshau Exp $
+
+!  This modules implements viscous heating and diffusion terms
+!  here smagorinsky viscosity
+
+!** AUTOMATIC CPARAM.INC GENERATION ****************************
+! Declare (for generation of cparam.inc) the number of f array
+! variables and auxiliary variables added by this module
+!
+! MVAR CONTRIBUTION 0
+! MAUX CONTRIBUTION 1
+!
+!***************************************************************
+
+module Viscosity
+
+  use Cparam
+  use Cdata
+  use Density
+
+  implicit none
+
+  character (len=labellen) :: ivisc='smagorinsky'
+  integer :: i_dtnu=0
+  real :: maxeffectivenu,nu_mol,C_smag=0.17
+  logical :: lvisc_first=.false.
+
+  ! input parameters
+  integer :: dummy
+  namelist /viscosity_init_pars/ dummy
+
+  ! run parameters
+  namelist /viscosity_run_pars/ nu, lvisc_first,ivisc,c_smag
+ 
+  ! other variables (needs to be consistent with reset list below)
+  !integer :: i_nu_LES=0
+
+  contains
+
+!***********************************************************************
+    subroutine register_viscosity()
+!
+!  19-nov-02/tony: coded
+!  16-july-04/nils: adapted from visc_shock
+!
+      use Cdata
+      use Mpicomm
+      use Sub
+!
+      logical, save :: first=.true.
+!
+      if (.not. first) call stop_it('register_viscosity called twice')
+      first = .false.
+!
+      lviscosity = .true.
+      lvisc_LES  = .true.
+      lvisc_smagorinsky = .true.
+!
+      ismagorinsky = mvar + naux + 1
+      naux = naux + 1
+!
+      if ((ip<=8) .and. lroot) then
+        print*, 'register_viscosity: smagorinsky viscosity nvar = ', nvar
+        print*, 'ismagorinsky = ', ismagorinsky
+      endif
+!
+!  identify version number
+!
+      if (lroot) call cvs_id( &
+           "$Id: visc_smagorinsky.f90,v 1.1 2004-07-16 13:42:49 nilshau Exp $")
+!
+! Check we aren't registering too many auxiliary variables
+!
+      if (naux > maux) then
+        if (lroot) write(0,*) 'naux = ', naux, ', maux = ', maux
+        call stop_it('register_viscosity: naux > maux')
+      endif
+!
+!  Writing files for use with IDL
+!
+      if (naux < maux) aux_var(aux_count)=',smagorinsky $'
+      if (naux == maux) aux_var(aux_count)=',smagorinsky'
+      aux_count=aux_count+1
+      if (lroot) write(15,*) 'smagorinsky = fltarr(mx,my,mz)*one'
+!
+    endsubroutine register_viscosity
+!***********************************************************************
+    subroutine initialize_viscosity()
+!
+!  20-nov-02/tony: coded
+!  16-july-04/nils: adapted from visc_shock
+!
+       use Cdata
+!
+        if (headtt.and.lroot) print*,'viscosity: nu=',nu
+        if (headtt.and.lroot) print*,'viscosity: c_smag=',c_smag
+
+    endsubroutine initialize_viscosity
+!*******************************************************************
+    subroutine rprint_viscosity(lreset,lwrite)
+!
+!  Writes ismagorinsky to index.pro file
+!
+!  16-july-04/nils: adapted from visc_shock
+!
+      use Cdata
+      use Sub
+! 
+      logical :: lreset
+      logical, optional :: lwrite
+      integer :: iname
+!
+!  reset everything in case of reset
+!  (this needs to be consistent with what is defined above!)
+!
+      if (lreset) then
+        i_nu_LES=0
+      endif
+!
+!  iname runs through all possible names that may be listed in print.in
+!
+      if(lroot.and.ip<14) print*,'rprint_ionization: run through parse list'
+      do iname=1,nname
+        call parse_name(iname,cname(iname),cform(iname),'nu_LES',i_nu_LES)
+      enddo
+!
+!  write column where which viscosity variable is stored
+!
+      if (present(lwrite)) then
+        if (lwrite) then
+          write(3,*) 'i_nu_LES=',i_nu_LES
+        endif
+      endif
+!   
+      if(ip==0) print*,lreset  !(to keep compiler quiet)
+    endsubroutine rprint_viscosity
+
+!***********************************************************************
+    subroutine calc_viscosity(f)
+!
+!  calculate nu_smag for full domain
+!
+!  16-july-04/nils: coded
+!
+      use IO
+      use Mpicomm
+!
+      real, dimension (mx,my,mz,mvar+maux) :: f
+      real, dimension (mx,my,mz,2) :: tmp
+      real, dimension (nx,ny,nz) :: sij2
+      integer :: i,j
+!
+
+
+      sij2=0
+      do i=1,2
+         do j=i+1,3
+!
+! find uij and uji (stored in tmp in order to save space)
+!
+            call der_2nd_nof(f(:,:,:,iux+i-1),tmp(:,:,:,1),j) ! uij
+            call der_2nd_nof(f(:,:,:,iux+j-1),tmp(:,:,:,2),i) ! uji
+!
+! find off diagonal part of sij2 for full array
+!
+            sij2=sij2+tmp(l1:l2,m1:m2,n1:n2,1)*tmp(l1:l2,m1:m2,n1:n2,2) &
+                 +0.5*tmp(l1:l2,m1:m2,n1:n2,1)**2 &
+                 +0.5*tmp(l1:l2,m1:m2,n1:n2,2)**2 
+         enddo
+      enddo
+!
+!  add diagonal part of sij2 for full array
+!
+      tmp(:,:,:,1)=0
+      call der_2nd_nof(f(:,:,:,iux-1+1),tmp(:,:,:,1),1)
+      sij2=sij2+tmp(l1:l2,m1:m2,n1:n2,1)**2
+      call der_2nd_nof(f(:,:,:,iux-1+2),tmp(:,:,:,1),2)
+      sij2=sij2+tmp(l1:l2,m1:m2,n1:n2,1)**2
+      call der_2nd_nof(f(:,:,:,iux-1+3),tmp(:,:,:,1),3)
+      sij2=sij2+tmp(l1:l2,m1:m2,n1:n2,1)**2
+!
+      f(l1:l2,m1:m2,n1:n2,ismagorinsky)=(C_smag*dxmax)**2.*sqrt(2*sij2)
+      nu_LES=sum(f(l1:l2,m1:m2,n1:n2,ismagorinsky))
+!
+    endsubroutine calc_viscosity
+
+!***********************************************************************
+    subroutine der_2nd_nof(var,tmp,j)
+!
+!  24-nov-03/nils: coded
+!
+      use Cdata
+!
+      real, dimension (mx,my,mz) :: var
+      real, dimension (mx,my,mz) :: tmp
+      integer :: j
+!
+      intent (in) :: var,j
+      intent (out) :: tmp
+!
+      tmp=0.
+
+      if (j==1 .and. nxgrid/=1) then
+          tmp(     1,:,:) = (- 3.*var(1,:,:) &
+                            + 4.*var(2,:,:) &
+                            - 1.*var(3,:,:))/(2.*dx) 
+          tmp(2:mx-1,:,:) = (- 1.*var(1:mx-2,:,:) &
+                            + 1.*var(3:mx  ,:,:))/(2.*dx) 
+          tmp(    mx,:,:) = (+ 1.*var(mx-2,:,:) &
+                            - 4.*var(mx-1,:,:) &
+                            + 3.*var(mx  ,:,:))/(2.*dx) 
+      endif
+!
+      if (j==2 .and. nygrid/=1) then
+          tmp(:,     1,:) = (- 3.*var(:,1,:) &
+                            + 4.*var(:,2,:) &
+                            - 1.*var(:,3,:))/(2.*dy) 
+          tmp(:,2:my-1,:) = (- 1.*var(:,1:my-2,:) &
+                            + 1.*var(:,3:my  ,:))/(2.*dy) 
+          tmp(:,    my,:) = (+ 1.*var(:,my-2,:) &
+                            - 4.*var(:,my-1,:) &
+                            + 3.*var(:,my  ,:))/(2.*dy) 
+      endif
+!
+      if (j==3 .and. nzgrid/=1) then
+          tmp(:,:,     1) = (- 3.*var(:,:,1) &
+                            + 4.*var(:,:,2) &
+                            - 1.*var(:,:,3))/(2.*dz) 
+          tmp(:,:,2:mz-1) = (- 1.*var(:,:,1:mz-2) &
+                            + 1.*var(:,:,3:mz  ))/(2.*dz) 
+          tmp(:,:,    mz) = (+ 1.*var(:,:,mz-2) &
+                            - 4.*var(:,:,mz-1) &
+                            + 3.*var(:,:,mz  ))/(2.*dz) 
+      endif
+!
+    end subroutine der_2nd_nof
+
+!!***********************************************************************
+    subroutine calc_viscous_heat(f,df,glnrho,divu,rho1,cs2,TT1,shock)
+!
+!  calculate viscous heating term for right hand side of entropy equation
+!
+!  20-nov-02/tony: coded
+!
+      use Cdata
+      use Mpicomm
+      use Sub
+
+      real, dimension (mx,my,mz,mvar+maux) :: f
+      real, dimension (mx,my,mz,mvar) :: df
+      real, dimension (nx)   :: rho1,TT1,cs2
+      real, dimension (nx)   :: sij2, divu,shock
+      real, dimension (nx,3) :: glnrho
+!
+!  traceless strain matrix squared
+!
+      call multm2_mn(sij,sij2)
+!
+      select case(ivisc)
+       case default
+         if (headtt) print*,'no heating: ivisc=',ivisc
+      endselect
+      if(ip==0) print*,f,cs2,divu,glnrho,shock  !(keep compiler quiet)
+    endsubroutine calc_viscous_heat
+
+!***********************************************************************
+    subroutine calc_viscous_force(f,df,glnrho,divu,rho1,shock,gshock)
+!
+!  calculate viscous heating term for right hand side of entropy equation
+!
+!   16-jul-04/nils: coded
+!
+      use Cdata
+      use Mpicomm
+      use Sub
+
+      real, dimension (mx,my,mz,mvar+maux) :: f
+      real, dimension (mx,my,mz,mvar) :: df
+      real, dimension (nx,3) :: glnrho,del2u,del6u,graddivu,fvisc,sglnrho,gshock
+      real, dimension (nx,3) :: nusglnrho,tmp1,tmp2
+      real, dimension (nx) :: murho1,rho1,divu,shock,SS12,sij2
+      integer :: i
+
+      intent (in) :: f, glnrho, rho1
+      intent (out) :: df
+!
+!  viscosity operator
+!  rho1 is pre-calculated in equ
+!
+      select case (ivisc)
+
+        case ('smagorinsky')
+!AB: the 2S.gradnu_smag is currently not implemented.
+!AB: Since this involves out-of-pencil derivatives, we may want
+!AB: to have this term optional (smagorinsky_simplified).
+!AB: Maybe the damage from smagorinsky_simplified is minor.
+!NILS: We then need something like the visc_shock or visc_hyper
+!NILS: modules where we allocate some auxilliary chunks of memory.
+!NILS: I guess it would be a good idea to call such a module for example 
+!NILS: visc_LES
+!NILS: such that we can use that module for possible other large eddy 
+!NILS: simulations aswell?
+!AB: visc_LES sounds good, although I would wait with renaming modules.
+!AB: Some LES may require more memory, and should be in a separate module.
+!AB: Thus, visc_smagorinsky is ok for the time being.
+!AB: But we can still keep the smagorinsky_simplified in visc_const.
+          !
+          !  viscous force: nu_smag*(del2u+graddivu/3+2S.glnrho)+2S.gradnu_smag
+          !  where nu_smag=(C_smag*dxmax)**2*sqrt(SS)
+          !
+          if (headtt) print*,'viscous force: Smagorinsky'
+          if(ldensity) then
+            call del2v_etc(f,iuu,del2u,GRADDIV=graddivu)
+            call multmv_mn(sij,glnrho,sglnrho)
+            call multsv_mn(f(l1:l2,m,n,ismagorinsky),sglnrho,nusglnrho)
+            tmp1=del2u+1./3.*graddivu
+            call multsv_mn(f(l1:l2,m,n,ismagorinsky),tmp1,tmp2)
+            fvisc=2*nusglnrho+tmp2
+            diffus_nu=max(diffus_nu,f(l1:l2,m,n,ismagorinsky)*dxyz_2)
+            !
+            ! Add ordinary viscosity if nu /= 0
+            !
+            if (nu /= 0.) then
+            !
+            !  viscous force: nu*(del2u+graddivu/3+2S.glnrho)
+            !  -- the correct expression for nu=const
+            !
+               fvisc=fvisc+2*nu*sglnrho+nu*(del2u+1./3.*graddivu)
+               diffus_nu=max(diffus_nu,nu*dxyz_2)
+            endif
+         else
+            if(lfirstpoint) &
+                 print*,"ldensity better be .true. for ivisc='smagorinsky'"
+         endif
+      case ('smagorinsky_simplified')
+         if (lroot) print*, 'for ivisc=smagorinsky_simplified use visc_const'
+         call stop_it('calc_viscous_forcing')
+      case default
+         !
+         !  Catch unknown values
+         !
+         if (lroot) print*, 'No such such value for ivisc: ', trim(ivisc)
+         call stop_it('calc_viscous_forcing')
+         
+      endselect
+      df(l1:l2,m,n,iux:iuz)=df(l1:l2,m,n,iux:iuz)+fvisc
+!
+!  set viscous time step
+!
+      if (ldiagnos.and.i_dtnu/=0) then
+        call max_mn_name(diffus_nu/cdtv,i_dtnu,l_dt=.true.)
+      endif
+!
+      if(ip==0) print*,divu,shock,gshock  !(keep compiler quiet)
+    end subroutine calc_viscous_force
+!***********************************************************************
+
+endmodule Viscosity
