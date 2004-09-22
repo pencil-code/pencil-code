8139ed585b8e36070db2ebf951970f43dc9bce19
Anders Johansen <anders@astro.lu.se>
2004-09-22 13:08:07 +0000

    Only calculate gnd once.

--- src/dustdensity.f90
+++ src/dustdensity.f90
@@ -1,4 +1,4 @@
-! $Id: dustdensity.f90,v 1.124 2004-09-22 13:06:14 ajohan Exp $
+! $Id: dustdensity.f90,v 1.125 2004-09-22 13:08:07 ajohan Exp $
 
 !  This module is used both for the initial condition and during run time.
 !  It contains dndrhod_dt and init_nd, among other auxiliary routines.
@@ -119,7 +119,7 @@ module Dustdensity
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: dustdensity.f90,v 1.124 2004-09-22 13:06:14 ajohan Exp $")
+           "$Id: dustdensity.f90,v 1.125 2004-09-22 13:08:07 ajohan Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -430,7 +430,6 @@ module Dustdensity
 !  Diffusion terms from dnd/dt = div(D*grad(nd))
 !
             call del2(f,ind(k),del2nd)
-            call grad(f,ind(k),gnd(:,:,k))
             if (ldustdensity_log) then
               call dot2_mn(gnd(:,:,k),gnd2)
               fdiffd = nd_diff_arr(:,k)*(del2nd + gnd2)
@@ -445,7 +444,6 @@ module Dustdensity
             call del2(f,ind(k),del2nd)
             call grad(f,ilnrho,glnrho)
             call del2(f,ilnrho,del2lnrho)
-            call grad(f,ind(k),gnd(:,:,k))
             call dot_mn(gnd(:,:,k),glnrho,gndglnrho)
             if (ldustdensity_log) then
               call dot2_mn(gnd(:,:,k),gnd2)
