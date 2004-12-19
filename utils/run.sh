4d4595fd221c218134e550a166fba23651cd8376
Wolfgang Dobler <wdobler@gmail.com>
2004-12-19 22:28:08 +0000

    Catch USR1, USR2 signals and do controlled job shutdown when they arrive.
    With Gridengine (or other queuing systems that notify a job before
    shutting it down), submit your job with
      qsub -notify run.sh
    and perl_monitor will touch RELOAD when the job's time is over.
    If notification is not configured by the administrator, use
      qsub -h_rt=48:00:00 -s_rt=47:55:00 run.sh
    which will give the code 5 minutes time to write the data and finish.
    
    Thus I don't need to estimate a good number of time steps in
    advance. PBS unfortunately does not seem to support this.

---
+++ utils/run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#                       run.csh
+#                      ---------
+#   Run ./run.csh, but catch USR1 and USR2 signals (csh apparently cannot
+#   do that) and make the code STOP in time. This assumes that SIGUSR1/2 is
+#   sent ahead of SIGKILL as is the case with Gridengine's -notify option
+#   (if the queue is configured accordingly) or with Gridengine's s_rt
+#   resource.
+
+# Join stderr and stout:
+#$ -j y -o run.log
+#@$-eo
+#@$-o run.log
+#
+# Work in submit directory (SGE):
+#$ -cwd
+
+# Spawn Perl script that writes STOP file when it receives USR1 or USR2 signal
+./perl_monitor &
+
+# Protect run.csh (and thus in particular mpirun and run.x subprocesses)
+# from signals. The code will stop when it sees the STOP file.
+trap '' usr1
+trap '' usr2
+
+
+echo "run.sh: starting run.csh"
+./run.csh
+echo "run.sh: Done"
+
+
