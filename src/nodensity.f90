807d097fb10ae9f9e002bbb3fd15ca2966569fd2
Antony J. Mee <eemynotna@gmail.com>
2005-07-01 03:20:28 +0000

    Another missing use statement

--- src/nodensity.f90
+++ src/nodensity.f90
@@ -1,4 +1,4 @@
-! $Id: nodensity.f90,v 1.37 2005-07-01 02:56:08 mee Exp $
+! $Id: nodensity.f90,v 1.38 2005-07-01 03:20:28 mee Exp $
 
 !** AUTOMATIC CPARAM.INC GENERATION ****************************
 ! Declare (for generation of cparam.inc) the number of f array
@@ -15,6 +15,7 @@
 module Density
 
   use Cparam
+  use Messages
   use EquationOfState, only: cs0,cs20,lnrho0,rho0,lcalc_cp, &
                              gamma,gamma1,cs2top,cs2bot
 
@@ -54,7 +55,7 @@ module Density
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: nodensity.f90,v 1.37 2005-07-01 02:56:08 mee Exp $")
+           "$Id: nodensity.f90,v 1.38 2005-07-01 03:20:28 mee Exp $")
 !
 !ajwm Necessary? added incase
 !      gamma=1.
