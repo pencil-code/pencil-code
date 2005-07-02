06b49f0056e3ae970d5aa8006d9b2a9e20c4ff21
Anders Johansen <anders@astro.lu.se>
2005-07-02 14:23:33 +0000

    Global module for keeping the particle number density only.

--- src/mpicomm.f90
+++ src/mpicomm.f90
@@ -1,4 +1,4 @@
-! $Id: mpicomm.f90,v 1.136 2005-07-02 12:12:55 ajohan Exp $
+! $Id: mpicomm.f90,v 1.137 2005-07-02 14:23:33 ajohan Exp $
 
 !!!!!!!!!!!!!!!!!!!!!
 !!!  mpicomm.f90  !!!
@@ -1622,9 +1622,9 @@ module Mpicomm
     function mpiwtime()
 !
       double precision :: mpiwtime
-      double precision :: MPI_WTIME   ! definition needed for mpicomm_ to work
+      double precision :: PMPI_WTIME   ! definition needed for mpicomm_ to work
 !
-      mpiwtime = MPI_WTIME()
+      mpiwtime = PMPI_WTIME()
     endfunction mpiwtime
 !***********************************************************************
     function mpiwtick()
