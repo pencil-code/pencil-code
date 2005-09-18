4596aaf9a5032010b48b2c3b3c29ac3e0edc2d77
Anders Johansen <anders@astro.lu.se>
2005-09-18 12:35:59 +0000

    Corrected typo in upwinding of dust advection.

--- src/dustdensity.f90
+++ src/dustdensity.f90
@@ -1,4 +1,4 @@
-! $Id: dustdensity.f90,v 1.137 2005-09-18 12:30:10 ajohan Exp $
+! $Id: dustdensity.f90,v 1.138 2005-09-18 12:35:59 ajohan Exp $
 
 !  This module is used both for the initial condition and during run time.
 !  It contains dndrhod_dt and init_nd, among other auxiliary routines.
@@ -135,7 +135,7 @@ module Dustdensity
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: dustdensity.f90,v 1.137 2005-09-18 12:30:10 ajohan Exp $")
+           "$Id: dustdensity.f90,v 1.138 2005-09-18 12:35:59 ajohan Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -719,12 +719,8 @@ module Dustdensity
         endif
 ! udglnnd
         if (lpencil(i_udglnnd)) then
-          if (lupw_ndmdmi) then
-            call u_dot_gradf(f,ind(k),p%uud(:,:,k),p%glnnd(:,:,k),p%udglnnd, &
-                upwind=.true.)
-          else
-            call dot_mn(p%uud(:,:,k),p%glnnd,p%udglnnd)
-          endif
+          call u_dot_gradf(f,ind(k),p%glnnd(:,:,k),p%uud(:,:,k),p%udglnnd, &
+              UPWIND=lupw_ndmdmi)
         endif
 ! md
         if (lpencil(i_md)) then
