4ef38b0385e80f33b651a019fa37a547cceb499a
Anders Johansen <anders@astro.lu.se>
2005-10-09 15:16:14 +0000

    Minor typography.

--- src/density.f90
+++ src/density.f90
@@ -1,4 +1,4 @@
-! $Id: density.f90,v 1.204 2005-10-02 11:26:04 ajohan Exp $
+! $Id: density.f90,v 1.205 2005-10-09 15:16:14 ajohan Exp $
 
 !  This module is used both for the initial condition and during run time.
 !  It contains dlnrho_dt and init_lnrho, among other auxiliary routines.
@@ -110,7 +110,7 @@ module Density
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: density.f90,v 1.204 2005-10-02 11:26:04 ajohan Exp $")
+           "$Id: density.f90,v 1.205 2005-10-09 15:16:14 ajohan Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -1223,14 +1223,14 @@ module Density
 !  Calculate density diagnostics
 !
       if (ldiagnos) then
-        if (idiag_rhom/=0)  call sum_mn_name(p%rho,idiag_rhom)
+        if (idiag_rhom/=0)    call sum_mn_name(p%rho,idiag_rhom)
         if (idiag_rhomin/=0) &
             call max_mn_name(-p%rho,idiag_rhomin,lneg=.true.)      
-        if (idiag_rhomax/=0) call max_mn_name(p%rho,idiag_rhomax)
-        if (idiag_rho2m/=0) call sum_mn_name(p%rho**2,idiag_rho2m)
+        if (idiag_rhomax/=0)  call max_mn_name(p%rho,idiag_rhomax)
+        if (idiag_rho2m/=0)   call sum_mn_name(p%rho**2,idiag_rho2m)
         if (idiag_lnrho2m/=0) call sum_mn_name(p%lnrho**2,idiag_lnrho2m)
-        if (idiag_rhomz/=0) call xysum_mn_name_z(p%rho,idiag_rhomz)
-        if (idiag_rhomx/=0) call yzsum_mn_name_x(p%rho,idiag_rhomx)
+        if (idiag_rhomz/=0)   call xysum_mn_name_z(p%rho,idiag_rhomz)
+        if (idiag_rhomx/=0)   call yzsum_mn_name_x(p%rho,idiag_rhomx)
         if (idiag_dtd/=0) &
             call max_mn_name(diffus_diffrho/cdtv,idiag_dtd,l_dt=.true.)
       endif
