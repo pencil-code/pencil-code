5602f3e0bb67b337dacc9c62ea74feb0141b18cf
Antony J. Mee <eemynotna@gmail.com>
2006-07-18 19:25:42 +0000

    * Neutron star stuff moved to neutron_star module

--- src/hydro.f90
+++ src/hydro.f90
@@ -1,4 +1,4 @@
-! $Id: hydro.f90,v 1.269 2006-07-06 11:13:59 joishi Exp $
+! $Id: hydro.f90,v 1.270 2006-07-18 19:25:42 mee Exp $
 !
 !  This module takes care of everything related to velocity
 !
@@ -154,7 +154,7 @@ module Hydro
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: hydro.f90,v 1.269 2006-07-06 11:13:59 joishi Exp $")
+           "$Id: hydro.f90,v 1.270 2006-07-18 19:25:42 mee Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -535,12 +535,6 @@ module Hydro
           f(:,:,:,iux) = -1/(2*Omega)*1/gamma*cs20*beta_glnrho_scaled(2)
           f(:,:,:,iuy) = 1/(2*Omega)*1/gamma*cs20*beta_glnrho_scaled(1)
   
-        case('step_xz')
-          call velocity_step(f)
-
-        case('kep_disk')
-          call velocity_kep_disk(f,zz)
-
         case default
           !
           !  Catch unknown values
@@ -1758,130 +1752,5 @@ module Hydro
 !
       first = .false.
     endsubroutine calc_mflow
-!***********************************************************************
-    subroutine velocity_step(f)
-!Natalia
-!Initialization of velocity in a case of the step-like distribution
-
-      use Cdata
-
-      real, dimension (mx,my,mz,mvar+maux) :: f
-      integer :: step_width, step_length
-      real ::  H_disk_min, L_disk_min, hdisk, ldisk
-    
-      hdisk=H_disk 
-      ldisk=L_disk
-    
-      H_disk_min=Lxyz(1)/(nxgrid-1)
-      L_disk_min=Lxyz(3)/(nzgrid-1)
-
-      if (H_disk .GT. Lxyz(1)-H_disk_min) hdisk=Lxyz(1)
-      if (H_disk .LT. H_disk_min) hdisk=0.
-
-      if (L_disk .GT. Lxyz(3)-L_disk_min) ldisk=Lxyz(3)
-      if (L_disk .LT. L_disk_min) ldisk=0.
-
-
-       
-      step_width=nint((nxgrid-1)*hdisk/Lxyz(1))
-      step_length=nint((nzgrid-1)*(Lxyz(3)-ldisk)/Lxyz(3))
-
-
-      if (hdisk .EQ. Lxyz(1) .AND. ldisk .EQ. Lxyz(3))  then
-        f(:,:,:,iuz)=uu_left
-        f(:,:,:,iuy)=uy_left
-      end if
-
-      if (hdisk .EQ. 0. .AND. ldisk .EQ. 0.) f(:,:,:,iuy)=uy_right
-      if (hdisk .EQ. 0. .OR. ldisk .EQ. 0.) f(:,:,:,iuy)=uy_right
-      
-       
-      if (hdisk .EQ. Lxyz(1) .AND. ldisk .LT. Lxyz(3)) then
-        f(:,:,step_length+3+1:mz,iuz)=uu_left
-        f(:,:,step_length+3+1:mz,iuy)=uy_left
-        f(:,:,1:step_length+3,iuy)=uy_right
-      endif
-
-      if (hdisk .LT. Lxyz(1) .AND. ldisk .EQ. Lxyz(3)) then
-        f(1:step_width+3,:,:,iuz)=uu_left
-        f(1:step_width+3,:,:,iuy)=uy_left
-        f(step_width+3+1:mx,:,:,iuy)=uy_right
-      endif
-
-
-      if (hdisk .GT. 0.  .AND. hdisk .LT. Lxyz(1) ) then
-        if (ldisk .GT. 0.  .AND. ldisk .LT. Lxyz(3)) then
-          f(1:step_width+3,:,step_length+3+1:mz,iuz)=uu_left
-          f(1:step_width+3,:,step_length+3+1:mz,iuy)=uy_left
-          f(step_width+3+1:mx,:,step_length+3+1:mz,iuy)=uy_right
-          f(:,:,1:step_length+3,iuy)=uy_right
-        end if
-      end if
-
-    endsubroutine  velocity_step
-!***********************************************************************
-    subroutine velocity_kep_disk(f,zz)
-!Natalia
-!Initialization of velocity in a case of the step-like distribution
-
-      use Cdata
-
-      real, dimension (mx,my,mz,mvar+maux) :: f
-      real, dimension (mx,my,mz) :: zz
-      integer :: step_length, decel_zone
-      real ::   L_disk_min,  ldisk,   ll
-
-      decel_zone=ac_dc_size+4
-       
-
-      ll=Lxyz(3)-L_disk
-      ldisk=L_disk
-
-   if (nzgrid .GT. 1) then
-      L_disk_min=Lxyz(3)/(nzgrid-1)
-
-      if (L_disk .GT. Lxyz(3)-L_disk_min) ldisk=Lxyz(3)
-      if (L_disk .LT. L_disk_min) ldisk=0.
-     
-      step_length=nint((nzgrid-1)*ll/Lxyz(3))
-
-      if (ldisk .LT. L_disk_min) then
-        f(:,:,:,iuz)=uu_left
-        f(:,:,:,iuy)=(zz-R_star)/ll*sqrt(M_star/(ll+R_star))
-      endif
-
-      if (ldisk .GE. Lxyz(3)) then
-        f(:,:,:,iuz)=uu_left
-        f(:,:,:,iuy)=sqrt(M_star/zz)
-      endif
-!
-      if (ldisk .GT. 0.  .AND. ldisk .LT. Lxyz(3)) then
-
-        f(:,:,:,iuz)=uu_left
-       if (ldecelerat_zone .AND. decel_zone .LT. nzgrid) then 
- 
-        f(:,:,step_length+3+1:mz,iuy)=sqrt(M_star/zz(:,:,step_length+3+1:mz))
-
-        f(:,:,decel_zone+1:step_length+3,iuy)= &
-           (zz(:,:,decel_zone+1:step_length+3)-R_star-(decel_zone-4)*L_disk_min) &
-           /(ll-(decel_zone-4)*L_disk_min)*sqrt(M_star/(ll+R_star))
-        f(:,:,1:decel_zone,iuy)=0.
-
-       else
-         f(:,:,step_length+3+1:mz,iuy)=sqrt(M_star/zz(:,:,step_length+3+1:mz))
-         f(:,:,1:step_length+3,iuy)= &
-           (zz(:,:,1:step_length+3)-R_star)/ll*sqrt(M_star/(ll+R_star))
-       end if
-
-      end if
-   else
-    f(:,:,:,iux)=uu_left
-    f(:,:,:,iuz)=uu_left
-    f(:,:,:,iuy)=sqrt(M_star/xyz0(3))
-   endif
-
-
-    endsubroutine  
-
 !***********************************************************************
 endmodule Hydro
