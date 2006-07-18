c7e3db7f9949404482a3760cab1f330e1e732bb0
Antony J. Mee <eemynotna@gmail.com>
2006-07-18 19:28:33 +0000

    * Moved neutron star things to the neutron star module

--- src/param_io.f90
+++ src/param_io.f90
@@ -1,4 +1,4 @@
-! $Id: param_io.f90,v 1.249 2006-07-17 11:37:31 mee Exp $ 
+! $Id: param_io.f90,v 1.250 2006-07-18 19:28:33 mee Exp $ 
 
 module Param_IO
 
@@ -66,9 +66,9 @@ module Param_IO
        mu0,force_lower_bound,force_upper_bound, &
        fbcx1,fbcx2,fbcy1,fbcy2,fbcz1,fbcz2,fbcz1_1,fbcz1_2,fbcz2_1,fbcz2_2, &
        xyz_step,xi_step_frac,xi_step_width, &
-       lcylindrical,init_loops,H_disk, H_disk_point, L_disk,R_star,M_star,T_star,accretion_flux, & 
-       lextrapolate_bot_density,ltop_velocity_kep,laccelerat_zone, &
-       ldecelerat_zone,lsurface_zone,lnstar_entropy,lnstar_T_const,lnstar_1D, T_disk
+       lcylindrical,init_loops
+ !      lextrapolate_bot_density,ltop_velocity_kep,laccelerat_zone, &
+ !      ldecelerat_zone,lsurface_zone,lnstar_entropy,lnstar_T_const,lnstar_1D, T_disk
   namelist /run_pars/ &
        cvsid,ip,nt,it1,dt,cdt,ddt,cdtv,cdts,cdtr,isave,itorder, &
        dsnap,d2davg,dvid,dtmin,dspec,tmax,iwig,awig,ialive, max_walltime, &
@@ -97,9 +97,7 @@ module Param_IO
        lpoint,mpoint,npoint, &
        lrescaling, lcylindrical, &
        ipencil_swap,lpencil_requested_swap,lpencil_diagnos_swap, &
-       lpencil_check,lpencil_check_diagnos_opti,lpencil_init, H_disk, H_disk_point, &
-       L_disk, R_star, M_star, T_star, accretion_flux, lnstar_entropy, &
-       lnstar_T_const,lnstar_1D
+       lpencil_check,lpencil_check_diagnos_opti,lpencil_init
   contains
 
 !***********************************************************************
