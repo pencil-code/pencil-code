ed29dd57e1d4f886e4cda125277eee474d82e3a2
Antony J. Mee <eemynotna@gmail.com>
2006-07-28 22:10:12 +0000

    * Removed FFT_LIB variable which is not needed, only need to set FOURIER   * Explicit dependencies for Fourier modules to allow different     libs to be pulled in for each one.   * Explicit dependencies for external "source" libraries (fft,fftpack)   * Removed redundant dummy routines.   * Made Makefile.src ready for fourier_fftw2/3 to be plugged in to     demonstrate why things need to be done this way.

--- src/nofft_lib.f
+++
@@ -1,9 +0,0 @@
-!  $Id: nofft_lib.f,v 1.1 2006-07-28 20:41:34 ajohan Exp $
-!
-!  Dummy routine, to avoid never seeing the compiler warnings from fftpack.
-!
-!***********************************************************************
-      SUBROUTINE CFFTF ()
-!
-      END
-!***********************************************************************
