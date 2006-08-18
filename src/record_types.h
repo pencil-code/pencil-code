e88aed5012cfc079c985a3890e9c9a2b12e595bb
Antony J. Mee <eemynotna@gmail.com>
2006-08-18 12:45:05 +0000

    * New persistent record types for some interstellar SNR data

--- src/record_types.h
+++ src/record_types.h
@@ -16,4 +16,8 @@ integer, parameter :: id_record_T_NEXT_SN    = 250
 integer, parameter :: id_record_FORCING_LOCATION = 270 
 integer, parameter :: id_record_FORCING_TSFORCE  = 271 
 !
+! Other
+!
+integer, parameter :: id_record_INTERSTELLAR_SN_TOGGLE = 1001
+integer, parameter :: id_record_INTERSTELLAR_SNRS = 1002
 !
