0275c091d9e76f09ec08470fda5c1f393946a114
Tobias Heinemann <tobias.heinemann@gmail.com>
2006-08-23 13:54:54 +0000

    Made get_slices_chiral public. I'm still not able to compile 1d-tests/riemann_bfield without manual intervention. Missing Makefile dependency?

--- src/chiral.h
+++ src/chiral.h
@@ -5,7 +5,7 @@
 
   public :: read_chiral_init_pars, write_chiral_init_pars
   public :: read_chiral_run_pars,  write_chiral_run_pars
-  public :: rprint_chiral
+  public :: rprint_chiral, get_slices_chiral
   public :: init_chiral, dXY_chiral_dt
   public :: pencil_criteria_chiral, pencil_interdep_chiral
   public :: calc_pencils_chiral
