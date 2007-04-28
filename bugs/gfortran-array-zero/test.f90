32dc3c008a7a2cbb62d1fa2adadd4a59f47beb5b
Wolfgang Dobler <wdobler@gmail.com>
2007-04-28 08:10:37 +0000

    Added simple program to reproduce gfortran bug. This bug (which is a bit scary) makes the SVN version of gfortran fail one auto-test (2d-tests/globaldisc) -- the other 24 tests succeed.

---
+++ bugs/gfortran-array-zero/test.f90
@@ -0,0 +1,32 @@
+!!!!!!!!!!!!!!!!!!!!
+!!!   test.f90   !!!
+!!!!!!!!!!!!!!!!!!!!
+
+!!!  Author: wd (Wolfgang.Dobler <at> ucalgary.ca)
+!!!  Date:   28-Apr-2007
+!!!
+!!!  Description:
+!!!   Demonstrates a compiler bug in gfortran 4.3.0 20070426 (experimental):
+!!!   Program will print
+!!!       1.000000       1.000000       1.000000       1.000000    
+!!!       0.000000       0.000000       0.000000       0.000000    
+!!!   instead of
+!!!       1.000000       1.000000       1.000000       1.000000    
+!!!       0.000000       1.000000       0.000000       1.000000    
+
+program Test
+
+  implicit none
+  real, dimension (2,2) :: smooth_factor
+
+  smooth_factor = 1.
+  write(0,*) smooth_factor
+
+  ! Either of the following lines goes wrong:
+  smooth_factor(1,:) = 0.
+  !smooth_factor(:,1) = 0.
+  write(0,*) smooth_factor
+
+endprogram
+
+!!! End of file test.f90
