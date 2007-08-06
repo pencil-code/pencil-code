c3a7e7c509e9404c24f1bf0d7b0286d16791e2ce
Dhrubaditya Mitra <dhruba.mitra@gmail.com>
2007-08-06 10:36:05 +0000

    forgot to check in the latest version. Should cure the auto-tests.

--- src/nogsl.c
+++ src/nogsl.c
@@ -1,6 +1,11 @@
-/* nogsl function */
+/* Wrapper C functions for the gsl library */
 
-void nogsl(void){}
-
+void  sp_besselj_l_(float* y, int*l, float* x){ }
+/* ------------------------------------------ */
+void  sp_bessely_l_(float *y, int*l, float* x){ }
+/* ------------------------------------------ */
+void  sp_harm_real_(float *y, int *l, int *m, float *theta, float *phi){ }
+/* -------------------------------------------- */
+void  sp_harm_imag_(float *y, int *l, int *m, float *theta, float *phi){ }
 
 
