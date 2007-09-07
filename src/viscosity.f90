e07c1638ad8667f07b63d024568db76b43b49c8e
Boris Dintrans <boris.dintrans@irap.omp.eu>
2007-09-07 09:25:48 +0000

    included the pretend_TT case in the viscous heat calculation Boris.

--- src/viscosity.f90
+++ src/viscosity.f90
@@ -1,4 +1,4 @@
-! $Id: viscosity.f90,v 1.78 2007-08-30 17:24:59 dhruba Exp $
+! $Id: viscosity.f90,v 1.79 2007-09-07 09:25:48 dintrans Exp $
 
 !  This modules implements viscous heating and diffusion terms
 !  here for cases 1) nu constant, 2) mu = rho.nu 3) constant and
@@ -102,7 +102,7 @@ module Viscosity
 !  identify version number
 !
       if (lroot) call cvs_id( &
-           "$Id: viscosity.f90,v 1.78 2007-08-30 17:24:59 dhruba Exp $")
+           "$Id: viscosity.f90,v 1.79 2007-09-07 09:25:48 dintrans Exp $")
 
       ivisc(1)='nu-const'
 !
@@ -861,7 +861,11 @@ module Viscosity
             df(l1:l2,m,n,iss) = df(l1:l2,m,n,iss) + p%TT1*p%visc_heat
          endif
       else if (ltemperature) then
-        df(l1:l2,m,n,ilnTT) = df(l1:l2,m,n,ilnTT) + p%cv1*p%TT1*p%visc_heat
+        if (pretend_TT) then
+          df(l1:l2,m,n,ilnTT) = df(l1:l2,m,n,ilnTT) + p%cv1*p%visc_heat
+        else
+          df(l1:l2,m,n,ilnTT) = df(l1:l2,m,n,ilnTT) + p%cv1*p%TT1*p%visc_heat
+        endif
       endif
 !
 !  calculate maximum heating (for time step constraint), so it is
