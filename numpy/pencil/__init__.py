440fdecf4333bbfd1a604ca0685aa4c46af649c5
Jeffrey S. Oishi <joishi@gmail.com>
2007-11-19 13:59:53 +0000

    * added __init__.py for simple imports...forgot this earlier. thanks, Thomas.

---
+++ numpy/pencil/__init__.py
@@ -0,0 +1,14 @@
+#import numpy as N
+#import scipy as S
+
+from pencil.files.ts import *
+from pencil.files.dim import *
+from pencil.files.param import *
+from pencil.files.grid import read_grid
+from pencil.files.var import read_var
+from pencil.files.slices import *
+from pencil.files.xyaver import *
+from pencil.files.yzaver import *
+from pencil.files.yaver import *
+from pencil.math.derivatives import *
+from pencil.math.vector_multiplication import *
