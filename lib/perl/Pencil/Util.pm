4b6a34d2072ad134ae11f64b11cb03831893fd56
Wolfgang Dobler <wdobler@gmail.com>
2008-02-07 22:59:15 +0000

    beginning Perl utlity library for the Pencil Code. Currently provides a routine for trying to import a Perl module, or giving useful advice if the import fails.

---
+++ lib/perl/Pencil/Util.pm
@@ -0,0 +1,57 @@
+#
+#                            Util.pm
+#                            -------
+#
+# Description:
+#   Some utility routines we repeatedly need from Perl
+# Author: wd (wdobler#cpan:org =~ tr/:#/.@/)
+# $Date: 2008-02-07 22:59:15 $
+# $Revision: 1.1 $
+#
+# This file is part of the Pencil Code and licensed under the GNU Public
+# License version 3 or later; see $PENCIL_HOME/license/GNU_public_license.txt.
+#
+
+package Pencil::Util;
+
+use warnings;
+use strict;
+use Carp;
+use vars qw($VERSION);
+
+##use critic
+
+$VERSION = '0.1';
+
+# ---------------------------------------------------------------------- #
+
+sub use_pencil_perl_module {
+# Try to use a module from the Pencil Code lib/perl directory and give
+# useful advice if the module cannot be found.
+#
+    my $module = shift;
+
+    my $modulefile = $module;
+    $modulefile =~ s{::}{/}g;
+    $modulefile .= '.pm';
+
+    eval "use $module";
+    my $msg = $@;
+    if ($msg) {               # Error loading module -- give useful advice
+        my $error = join("\n", $msg);
+        if ($error =~ m|Can't locate $modulefile in \@INC|) {
+            croak "Perl: Can't locate $modulefile in \@INC\n"
+                . "You need to check out the Pencil library directory"
+                . " (including lib/perl):\n"
+                . "  (cd \$PENCIL_HOME; cvs up -dA lib)\n";
+        } else {                # unknown error message
+            croak "$@\n";
+        }
+    } else {
+        return 1;               # everything was fine
+    }
+}
+
+# ---------------------------------------------------------------------- #
+
+# End of file
