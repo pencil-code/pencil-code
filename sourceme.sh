584e7022bb993bb41dde8dbe9a72eb7f1fcf8f3e
Thomas Gastine <gastine@mps.mpg.de>
2008-02-18 13:36:18 +0000

    Modification in sourceme.sh for the PYTHONPATH variable.
    Thomas

--- sourceme.sh
+++ sourceme.sh
@@ -58,7 +58,11 @@ if [ -z $_sourceme ]; then	# called for the first time?
 #    #  Set Perl module path
 #    PERL5LIB="${PENCIL_HOME}/perl${PERL5LIB:+:$PERL5LIB}"
     #   Set PYTHONPATH
-    PYTHONPATH=$PYTHONPATH:$PENCIL_HOME/numpy
+    if [ -z $PYTHONPATH ]; then
+       PYTHONPATH="$PENCIL_HOME/numpy"
+    else ;
+       PYTHONPATH="$PYTHONPATH:$PENCIL_HOME/numpy"
+    fi
     # Remember that sourceme has been successfully run
     _sourceme="set"
 
