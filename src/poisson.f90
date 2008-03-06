e149dbe34f4056a0da3b8c53497ae6cc0b0f74f0
Wladimir Lyra <wladimir.lyra@gmail.com>
2008-03-06 22:56:28 +0000

    moved inverse_laplacian_fft_cyl to poisson_cyl.f90

--- src/poisson.f90
+++ src/poisson.f90
@@ -1,4 +1,4 @@
-! $Id: poisson.f90,v 1.45 2008-03-03 00:53:20 wlyra Exp $
+! $Id: poisson.f90,v 1.46 2008-03-06 22:56:28 wlyra Exp $
 
 !
 !  This module solves the Poisson equation
@@ -89,16 +89,16 @@ module Poisson
 !
       intent(inout) :: phi
 !
+      if (lcylindrical_coords) then 
+        if (lroot) print*,'You are using cylindrical coordinates. '//&
+             'Use poisson_cyl.f90 instead'
+        call fatal_error("inverse_laplacian","")
+      endif
+!
       if (lsemispectral) then
-!
         call inverse_laplacian_semispectral(phi)
-!
       else
-        if (lcylindrical_coords) then
-          call inverse_laplacian_fft_cyl(phi)
-        else
           call inverse_laplacian_fft(phi)
-        endif
       endif
 !
     endsubroutine inverse_laplacian
@@ -119,7 +119,7 @@ module Poisson
 !  Identify version.
 !
       if (lroot .and. ip<10) call cvs_id( &
-        "$Id: poisson.f90,v 1.45 2008-03-03 00:53:20 wlyra Exp $")
+        "$Id: poisson.f90,v 1.46 2008-03-06 22:56:28 wlyra Exp $")
 !
 !  The right-hand-side of the Poisson equation is purely real.
 !
@@ -212,279 +212,6 @@ module Poisson
 !
     endsubroutine inverse_laplacian_fft
 !***********************************************************************
-    subroutine inverse_laplacian_fft_cyl(phi)
-!
-!  Solve the 2D Poisson equation in cylindrical coordinates
-!  by transforming to a periodic cartesian grid before 
-!  Fourier transforming. 
-!
-!  This method works only for 2D. The frequent broadcast of 
-!  a big array gave problems at the PIA cluster in Heidelberg
-!  after some thousands of time-steps. The problem was probably
-!  due to memory leaking. No problem occured at the UPPMAX
-!  cluster in Uppsala. So beware that using this broadcasting
-!  extravaganza subroutine might not work in all clusters. 
-!
-!  01-12-07/wlad: coded
-!  28-02-08/wlad: merged the serial and mpi versions
-!
-      use Mpicomm
-!
-      real, dimension (nx,ny,nz)  :: phi
-      real, dimension (2*nx,2*ny) :: nphi,nb1
-!
-      real, dimension(nx)     :: rad
-      real, dimension(ny)     :: tht
-      real, dimension(nygrid) :: theta_serial
-!
-      real, dimension(2*nx)     :: xc,kkx_fft
-      real, dimension(2*ny)     :: yc
-      real, dimension(2*nygrid) :: kky_fft,yserial
-!
-! For communication
-!
-      real, dimension (nx,ny)         :: cross_proc
-      real, dimension (2*nx,2*ny)     :: cross_proc_big
-      real, dimension (nx,nygrid)     :: cross
-      real, dimension (2*nx,2*nygrid) :: crossbig
-!
-! Cheap stuff
-!
-      real    :: x0,xn,y0,yn,dxc,dyc,dxc1,dyc1,Lxn,Lyn
-      real    :: r0,rn,theta0,theta1,dr,dth,dr1,dth1
-      real    :: theta,radius,k2,xp,yp
-      real    :: delr,delp,fr,fp,delx,dely,fx,fy
-      real    :: p1,p2,p3,p4,interp_pot
-!
-      integer :: ix1,ix2,iy1,iy2,ir1,ir2,ip1,ip2
-      integer :: i,j,ikx,iky,ir,im,ido,iup
-      integer :: nr,nth,nnx,nny,nnghost
-      integer :: nrgrid,nthgrid,nnxgrid,nnygrid
-!
-      if (nx/=nygrid) &
-           call fatal_error("","currently only works for nx=nygrid")
-      if (nzgrid/=1)  &
-           call fatal_error("","currently only works for 2D simulations")
-!
-! Keep the notation consistent.
-!
-      rad=x(l1:l2)     ; tht=y(m1:m2)
-      nr =nx           ; nth=ny
-      r0=rad(1)        ; theta0=xyz0(2)+.5*dth 
-      rn=rad(nr)       ; theta1=xyz1(2)-.5*dth
-      nrgrid=nxgrid    ; nthgrid=nygrid
-      dr=dx            ; dth=dy
-      dr1=1./dr        ; dth1=1./dth
-!
-! Expanded cartesian axes
-!
-      nnx=2*nx         ; nny=2*ny
-      xn=2*rad(nr)     ; x0=-xn
-      yn=xn            ; y0=-yn
-      nnxgrid=2*nxgrid ; nnygrid=2*nygrid
-!
-      do i=1,nnx
-        xc(i)=1.*(i-1)        /(nnxgrid-1)*(xn-x0)+x0
-      enddo
-      do m=1,nny
-        yc(m)=1.*(m-1+ipy*nny)/(nnygrid-1)*(yn-y0)+y0
-      enddo      
-! 
-      dxc=xc(2)-xc(1)  ; dyc=dxc
-      dxc1=1/dxc       ; dyc1=1/dyc
-!
-! Now transform to Cartesian grid
-!
-      nnghost=npoint-nghost
-!
-      if (lmpicomm) then
-!
-! All processors send its density array to the root processor
-!
-        if (.not.lroot) then
-          call mpisend_real(phi(:,:,nnghost),(/nx,ny/),root,111)
-        else
-          cross_proc=phi(:,:,nnghost)
-!
-! The root processor receives all arrays and
-! stores them in a single big array of dimension
-! nx*nygrid
-!
-          do j=0,ncpus-1
-            if (j/=0) call mpirecv_real(cross_proc,(/nx,ny/),j,111)
-            ido= j  * ny + 1
-            iup=(j+1)*ny
-            cross(:,ido:iup)=cross_proc
-          enddo
-        endif
-!
-! Broadcast the density field to all processors
-!
-        call mpibcast_real(cross,(/nx,nygrid/))
-!
-      else
-!
-! For serial runs, ny=nygrid, so just copy the density
-!
-        cross(:,1:ny)=phi(:,1:ny,nnghost)
-!
-      endif
-!
-! Need the serial theta later in order to compute the
-! azimuthal displacement in parallel
-!
-      do i=1,nygrid
-        theta_serial(i)=1.*(i-1)/(nthgrid-1)*(theta1-theta0)+theta0
-      enddo
-!
-! Now transform the grid just like we would do in a serial run
-!
-      do m=1,nny
-        do i=1,nnx
-          radius=sqrt(xc(i)**2+yc(m)**2)
-          if ((radius.ge.r0).and.(radius.le.rn)) then
-            ir1=floor((radius-r0)*dr1 ) +1;ir2=ir1+1
-            delr=radius-rad(ir1)
-!
-! this should never happen, but is here for warning
-!
-            if (ir1.lt.1 ) call fatal_error("","cyl2cart: ir1<1")
-            if (ir2.gt.nr) call fatal_error("","cyl2cart: ir2>nr")
-!
-            theta=atan2(yc(m),xc(i))
-            ip1=floor((theta - theta0)*dth1)+1;ip2=ip1+1
-            if (ip1==0) then
-              ip1=nthgrid
-              delp=theta-theta_serial(ip1) + 2*pi
-            else
-              delp=theta-theta_serial(ip1)
-            endif
-            if (ip2==nthgrid+1) ip2=1
-!
-! Bilinear interpolation
-!
-            !p1=phi(ir1,ip1,nnghost);p2=phi(ir2,ip1,nnghost)
-            !p3=phi(ir1,ip2,nnghost);p4=phi(ir2,ip2,nnghost)
-            p1=cross(ir1,ip1);p2=cross(ir2,ip1)
-            p3=cross(ir1,ip2);p4=cross(ir2,ip2)
-!
-            fr=delr*dr1
-            fp=delp*dth1
-!
-            nphi(i,m)=fr*fp*(p1-p2-p3+p4) + fr*(p2-p1) + fp*(p3-p1) + p1
-          else
-            nphi(i,m)=0.
-          endif
-        enddo
-      enddo
-!
-!  The right-hand-side of the Poisson equation is purely real.
-!
-      nb1=0.0
-!
-!  Forward transform (to k-space).
-!
-      call fourier_transform_xy_xy_other(nphi,nb1)
-!
-!  Solve Poisson equation
-!
-      Lxn=2*xc(nnx);Lyn=Lxn
-!
-      kkx_fft=cshift((/(i-(nnxgrid+1)/2,i=0,nnxgrid-1)/),+(nnxgrid+1)/2)*2*pi/Lxn
-      kky_fft=cshift((/(i-(nnygrid+1)/2,i=0,nnygrid-1)/),+(nnygrid+1)/2)*2*pi/Lyn
-!
-      do iky=1,nny
-        do ikx=1,nnx
-          if ((kkx_fft(ikx)==0.0) .and. (kky_fft(iky+ipy*nny)==0.0)) then
-            nphi(ikx,iky) = 0.0
-            nb1(ikx,iky) = 0.0
-          else
-            if (.not.lrazor_thin) then
-              call fatal_error("","3d case not implemented yet")
-!
-!  Razor-thin approximation. Here we solve the equation
-!    del2Phi=4*pi*G*Sigma(x,y)*delta(z)
-!  The solution at scale k=(kx,ky) is
-!    Phi(x,y,z)=-(2*pi*G/|k|)*Sigma(x,y)*exp[i*(kx*x+ky*y)-|k|*|z|]
-!
-            else
-              k2 = (kkx_fft(ikx)**2+kky_fft(iky+ipy*nny)**2)
-              nphi(ikx,iky) = -.5*nphi(ikx,iky) / sqrt(k2)
-              nb1(ikx,iky)  = -.5*nb1(ikx,iky)  / sqrt(k2)
-            endif
-          endif
-!
-!  Limit |k| < kmax
-!
-          if (kmax>0.0) then
-            if (sqrt(k2)>=kmax) then
-              nphi(ikx,iky) = 0.
-              nb1(ikx,iky) = 0.
-            endif
-          endif
-        enddo
-      enddo
-!
-!  Inverse transform (to real space).
-!
-      call fourier_transform_xy_xy_other(nphi,nb1,linv=.true.)
-!
-      if (lmpicomm) then
-        if (.not.lroot) then
-          call mpisend_real(nphi,(/nnx,nny/),root,222)
-        else
-          cross_proc_big=nphi
-          !the root processor receives all arrays and
-          !stores them in a single big array of dimension
-          !nx*nygrid
-          do j=0,ncpus-1
-            if (j/=0) call mpirecv_real(cross_proc_big,(/nnx,nny/),j,222)
-            ido= j   *nny+1
-            iup=(j+1)*nny
-            crossbig(:,ido:iup)=cross_proc_big
-          enddo
-        endif
-        call mpibcast_real(crossbig,(/nnx,nnygrid/))
-      else
-        crossbig(:,1:ny)=nphi(:,1:ny)
-      endif
-!
-!  Convert back to cylindrical
-!
-      yserial=xc
-      do ip=1,Nth
-        do ir=1,Nr
-!
-          xp=rad(ir)*cos(tht(ip))
-          yp=rad(ir)*sin(tht(ip))
-!
-          ix1 = floor((xp-x0)*dxc1)+1 ; ix2 = ix1+1
-          iy1 = floor((yp-y0)*dyc1)+1 ; iy2 = iy1+1
-!
-          if (ix1 .lt.  1)      call fatal_error("","ix1 lt 1")
-          if (iy1 .lt.  1)      call fatal_error("","iy1 lt 1")
-          if (ix2 .gt. nnxgrid) call fatal_error("","ix2 gt nnxgrid")
-          if (iy2 .gt. nnygrid) call fatal_error("","iy2 gt nnygrid")
-!
-          delx=xp-     xc(ix1);fx=delx*dxc1
-          dely=yp-yserial(iy1);fy=dely*dyc1
-!
-! Bilinear interpolation
-!
-          p1=crossbig(ix1,iy1);p2=crossbig(ix2,iy1)
-          p3=crossbig(ix1,iy2);p4=crossbig(ix2,iy2)
-!
-          interp_pot=fx*fy*(p1-p2-p3+p4) + fx*(p2-p1) + fy*(p3-p1) + p1
-!
-          do n=1,nz
-            phi(ir,ip,n)=interp_pot
-          enddo
-!
-        enddo
-      enddo
-!
-    endsubroutine inverse_laplacian_fft_cyl
-!***********************************************************************
     subroutine inverse_laplacian_semispectral(phi)
 !
 !  Solve the Poisson equation by Fourier transforming in the xy-plane and
@@ -507,7 +234,7 @@ module Poisson
 !  identify version
 !
       if (lroot .and. ip<10) call cvs_id( &
-        "$Id: poisson.f90,v 1.45 2008-03-03 00:53:20 wlyra Exp $")
+        "$Id: poisson.f90,v 1.46 2008-03-06 22:56:28 wlyra Exp $")
 !
 !  The right-hand-side of the Poisson equation is purely real.
 !
