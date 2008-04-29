5bf381d4e201df858fa9ae7a76ab673f089f9790
Natalia Babkovskaia <nbabkovskaia@gmail.com>
2008-04-29 11:10:41 +0000

    chem_stream.f90

--- src/special/chem_stream.f90
+++ src/special/chem_stream.f90
@@ -1,4 +1,4 @@
-! $Id: chem_stream.f90,v 1.18 2008-04-28 16:05:40 nbabkovs Exp $
+! $Id: chem_stream.f90,v 1.19 2008-04-29 11:10:41 nbabkovs Exp $
 !
 !  This module incorporates all the modules used for Natalia's
 !  neutron star -- disk coupling simulations (referred to as nstar)
@@ -139,11 +139,11 @@ module Special
 !
 !
 !  identify CVS version information (if checked in to a CVS repository!)
-!  CVS should automatically update everything between $Id: chem_stream.f90,v 1.18 2008-04-28 16:05:40 nbabkovs Exp $
+!  CVS should automatically update everything between $Id: chem_stream.f90,v 1.19 2008-04-29 11:10:41 nbabkovs Exp $
 !  when the file in committed to a CVS repository.
 !
       if (lroot) call cvs_id( &
-           "$Id: chem_stream.f90,v 1.18 2008-04-28 16:05:40 nbabkovs Exp $")
+           "$Id: chem_stream.f90,v 1.19 2008-04-29 11:10:41 nbabkovs Exp $")
 !
 !
 !  Perform some sanity checks (may be meaningless if certain things haven't
@@ -219,7 +219,7 @@ module Special
          case('bomb')
             call bomb_field(f,xx,yy)
          case('default')
-          if(lroot) print*,'init_special: Default neutron star setup'
+          if(lroot) print*,'init_special: Default  setup'
      !     call density_init(f,xx,zz)
      !     call entropy_init(f,xx,zz)
      !     call velocity_init(f,zz)
@@ -514,6 +514,10 @@ module Special
            call bc_equil_x(f,-1, bc)
          case (iBC_X_BOT)
            call bc_equil_x(f,-1, bc)
+         case (iBC_Y_TOP)
+           call bc_equil_y(f,-1, bc)
+         case (iBC_Y_BOT)
+           call bc_equil_y(f,-1, bc)
          endselect
          bc%done=.true.
 
@@ -793,6 +797,13 @@ module Special
     if (bc%location==iBC_X_BOT) then
       ! bottom boundary
 
+      if (vr==1) then
+           do i=0,nghost
+            f(l1-i,:,:,vr)=value1
+           enddo 
+      endif
+
+
       if (vr==4) then
            do i=0,nghost
             f(l1-i,:,:,4)=f(l1,:,:,4)
@@ -807,12 +818,19 @@ module Special
 
        if (vr >= ichemspec(1)) then
          do i=0,nghost; 
-             f(l1-i,:,:,vr)=f(l1,:,:,vr)
+             f(l1-i,:,:,vr)=2.*f(l1,:,:,vr)-f(l1+i,:,:,vr)
           enddo
        endif
 
       elseif (bc%location==iBC_X_TOP) then
       ! top boundary
+
+      if (vr==1) then
+           do i=0,nghost
+            f(l2+i,:,:,vr)=value1
+           enddo 
+      endif
+
 
       if (vr==4) then
            do i=0,nghost
@@ -828,13 +846,15 @@ module Special
 
        if (vr >= ichemspec(1)) then
          do i=0,nghost; 
-             f(l2+i,:,:,vr)=f(l2,:,:,vr)
+             f(l2+i,:,:,vr)=2.*f(l2,:,:,vr)-f(l2-i,:,:,vr)
           enddo
        endif
 
     !   do i=1,nghost
      !     f(l2+i,:,:,vr)=f(l2,:,:,vr)!2*f(l2,:,:,vr)!+sgn*f(l2-i,:,:,vr);
      !   enddo
+
+
       else
         print*, "bc_BL_x: ", bc%location, " should be `top(", &
                         iBC_X_TOP,")' or `bot(",iBC_X_BOT,")'"
@@ -842,6 +862,93 @@ module Special
 !
     endsubroutine bc_equil_x
 !***********************************************************************
+ subroutine bc_equil_y(f,sgn,bc)
+!
+! Natalia
+!
+    use Cdata
+!
+      real, dimension (mx,my,mz,mvar+maux) :: f
+      real, dimension (my, nchemspec-1) :: mask
+      integer :: sgn
+      type (boundary_condition) :: bc
+      integer :: i,i1,j,vr,k
+      real :: value1, value2
+
+      vr=bc%ivar
+
+      value1=bc%value1
+      value2=bc%value2
+
+    if (bc%location==iBC_Y_BOT) then
+      ! bottom boundary
+
+      if (vr==1) then
+           do i=0,nghost
+            f(:,m1-i,:,vr)=value1
+           enddo 
+      endif
+
+
+      if (vr==4) then
+           do i=0,nghost
+            f(:,m1-i,:,vr)=f(:,m1,:,vr)
+           enddo 
+      endif
+
+      if (vr==5) then
+          do i=0,nghost
+            f(:,m1-i,:,vr)=2.*f(:,m1,:,vr)-f(:,m1+i,:,vr)
+          enddo 
+      endif
+
+       if (vr >= ichemspec(1)) then
+         do i=0,nghost; 
+             f(:,m1-i,:,vr)=2.*f(:,m1,:,vr)-f(:,m1+i,:,vr)
+          enddo
+       endif
+
+      elseif (bc%location==iBC_Y_TOP) then
+      ! top boundary
+
+      if (vr==1) then
+           do i=0,nghost
+            f(:,m2+i,:,vr)=value1
+           enddo 
+      endif
+
+
+      if (vr==4) then
+           do i=0,nghost
+            f(:,m2+i,:,vr)=f(:,m2,:,vr)
+           enddo 
+      endif
+
+      if (vr==5) then
+          do i=0,nghost
+            f(:,m2+i,:,vr)=2.*f(:,m2,:,vr)-f(:,m2-i,:,vr)
+          enddo 
+      endif
+
+       if (vr >= ichemspec(1)) then
+         do i=0,nghost; 
+             f(:,m2+i,:,vr)=2.*f(:,m2,:,vr)-f(:,m2-i,:,vr)
+          enddo
+       endif
+
+    !   do i=1,nghost
+     !     f(l2+i,:,:,vr)=f(l2,:,:,vr)!2*f(l2,:,:,vr)!+sgn*f(l2-i,:,:,vr);
+     !   enddo
+
+
+      else
+        print*, "bc_BL_y: ", bc%location, " should be `top(", &
+                        iBC_X_TOP,")' or `bot(",iBC_X_BOT,")'"
+      endif
+!
+    endsubroutine bc_equil_y
+!**********************************************************************
+
   subroutine spec_input(f,sgn,bc)
    !
 ! Natalia
@@ -943,7 +1050,7 @@ module Special
 
            enddo 
        else
-         do i=0,nghost;   f(l1-i,:,:,vr)=log(value1); enddo 
+         do i=0,nghost;   f(l1-i,:,:,5)=log(value1); enddo 
 
          do i=0,nghost
            pres(l1-i,:,:)=pres(l1-i+1,:,:)-divtau(l1-i,:,:,1)*dx
@@ -981,7 +1088,7 @@ module Special
             f(l2+i,:,:,5)=f(l2,:,:,5)
            enddo 
        else
-         do i=0,nghost;   f(l2+i,:,:,vr)=log(value1); enddo 
+         do i=0,nghost;   f(l2+i,:,:,5)=log(value1); enddo 
 
          do i=0,nghost
            pres(l2+i,:,:)=pres(l2+i-1,:,:)+divtau(l2+i,:,:,1)*dx
