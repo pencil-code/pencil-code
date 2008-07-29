85b5d71c01a4a2f83267571dc0f2c25b7f675ddf
Anders Johansen <anders@astro.lu.se>
2008-07-29 07:53:44 +0000

    For constant solids-to-gas ratio, take into account that density is either logarithmic or non-logarithmic.

--- src/dustdensity.f90
+++ src/dustdensity.f90
@@ -1,4 +1,4 @@
-! $Id: dustdensity.f90,v 1.178 2008-06-17 15:34:08 ajohan Exp $
+! $Id: dustdensity.f90,v 1.179 2008-07-29 07:53:44 ajohan Exp $
 
 !  This module is used both for the initial condition and during run time.
 !  It contains dndrhod_dt and init_nd, among other auxiliary routines.
@@ -145,7 +145,7 @@ module Dustdensity
 !  identify version number (generated automatically by CVS)
 !
       if (lroot) call cvs_id( &
-           "$Id: dustdensity.f90,v 1.178 2008-06-17 15:34:08 ajohan Exp $")
+           "$Id: dustdensity.f90,v 1.179 2008-07-29 07:53:44 ajohan Exp $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
@@ -375,7 +375,11 @@ module Dustdensity
 
         case('const_epsd')
           do k=1,ndustspec
-            f(:,:,:,ind(k)) = eps_dtog*exp(f(:,:,:,ilnrho))/(md(k)*unit_md)
+            if (ldensity_nolog) then
+              f(:,:,:,ind(k)) = eps_dtog*f(:,:,:,ilnrho)/(md(k)*unit_md)
+            else
+              f(:,:,:,ind(k)) = eps_dtog*exp(f(:,:,:,ilnrho))/(md(k)*unit_md)
+            endif
           enddo
         case('const_epsd_global')
           do l=1,mx
