9545f97ae7196e4d6ac457f6e3180387c9aeb1d3
Anders Johansen <anders@astro.lu.se>
2008-10-13 16:07:55 +0000

    Made single and double underscore versions of all subroutines, to avoid annoying issue with g95 that has existed for a very long time.

--- src/nogsl.c
+++ src/nogsl.c
@@ -5,7 +5,23 @@ void FTNIZE(sp_besselj_l)
      (REAL* y, FINT*l, REAL* x) {
 }
 /* ------------------------------------------ */
+void FTNIZE(sp_besselj_l_)
+     (REAL* y, FINT*l, REAL* x) {
+}
+/* ------------------------------------------ */
+void FTNIZE(sp_besselj_l__)
+     (REAL* y, FINT*l, REAL* x) {
+}
+/* ------------------------------------------ */
 void FTNIZE(sp_bessely_l)
+     (REAL *y, FINT*l, REAL* x) {
+}
+/* ------------------------------------------ */
+void FTNIZE(sp_bessely_l_)
+     (REAL *y, FINT*l, REAL* x) {
+}
+/* ------------------------------------------ */
+void FTNIZE(sp_bessely_l__)
      (REAL *y, FINT*l, REAL* x) {
 }
 /* ------------------------------------------ */
@@ -13,19 +29,58 @@ void FTNIZE(sp_bessel_jnu)
      (REAL* y, REAL*nu, REAL* x) {
 }
 /* ------------------------------------------ */
+void FTNIZE(sp_bessel_jnu_)
+     (REAL* y, REAL*nu, REAL* x) {
+}
+/* ------------------------------------------ */
+void FTNIZE(sp_bessel_jnu__)
+     (REAL* y, REAL*nu, REAL* x) {
+}
+/* ------------------------------------------ */
 void FTNIZE(sp_bessel_ynu)
+     (REAL *y, REAL*nu, REAL* x) {
+}
+/* ------------------------------------------ */
+void FTNIZE(sp_bessel_ynu_)
+     (REAL *y, REAL*nu, REAL* x) {
+}
+/* ------------------------------------------ */
+void FTNIZE(sp_bessel_ynu__)
      (REAL *y, REAL*nu, REAL* x) {
 }
 /* ------------------------------------------ */
 void FTNIZE(sp_harm_real)
      (REAL *y, FINT *l, FINT *m, REAL *theta, REAL *phi) {
 }
+/* ------------------------------------------ */
+void FTNIZE(sp_harm_real_)
+     (REAL *y, FINT *l, FINT *m, REAL *theta, REAL *phi) {
+}
+/* ------------------------------------------ */
+void FTNIZE(sp_harm_real__)
+     (REAL *y, FINT *l, FINT *m, REAL *theta, REAL *phi) {
+}
 /* -------------------------------------------- */
 void FTNIZE(sp_harm_imag)
+     (REAL *y, FINT *l, FINT *m, REAL *theta, REAL *phi) {
+}
+/* -------------------------------------------- */
+void FTNIZE(sp_harm_imag_)
+     (REAL *y, FINT *l, FINT *m, REAL *theta, REAL *phi) {
+}
+/* -------------------------------------------- */
+void FTNIZE(sp_harm_imag__)
      (REAL *y, FINT *l, FINT *m, REAL *theta, REAL *phi) {
 }
 /* ------------------------------------------ */
 void FTNIZE(legendre_pl)
      (REAL *y, FINT *l, REAL *theta) {
 }
-
+/* ------------------------------------------ */
+void FTNIZE(legendre_pl_)
+     (REAL *y, FINT *l, REAL *theta) {
+}
+/* ------------------------------------------ */
+void FTNIZE(legendre_pl__)
+     (REAL *y, FINT *l, REAL *theta) {
+}
