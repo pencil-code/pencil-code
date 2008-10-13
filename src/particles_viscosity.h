81a58c29727767e0f3f473d2b9acc2c9313ef8c2
Anders Johansen <anders@astro.lu.se>
2008-10-13 12:40:36 +0000

    Added header file.

---
+++ src/particles_viscosity.h
@@ -0,0 +1,11 @@
+private
+
+public :: register_particles_viscosity
+public :: initialize_particles_viscosity
+public :: calc_particles_viscosity
+public :: calc_particles_viscous_force
+public :: read_particles_visc_init_pars
+public :: write_particles_visc_init_pars
+public :: read_particles_visc_run_pars
+public :: write_particles_visc_run_pars
+public :: rprint_particles_viscosity
