68962266ffba104bd6f1a6364f877507bf2f7ef4
Anders Johansen <anders@astro.lu.se>
2009-05-09 17:28:37 +0000

    This file is not used any more.

--- src/planet.h
+++
@@ -1,6 +0,0 @@
-!  -*-f90-*-  (for emacs)    vim:set filetype=fortran:  (for vim)
-  private
-
-  public :: calc_pencils_planet
-  public :: pencil_criteria_planet
-  public :: pencil_interdep_planet
