68f3907517645dd219e3a26c1b5378986de29f52
Anders Johansen <anders@astro.lu.se>
2009-05-26 09:06:59 +0000

    Suggest to use ispecial and ispecaux for variable and aux variable.

--- src/nospecial.f90
+++ src/nospecial.f90
@@ -6,28 +6,28 @@
 !  The module provides a set of standard hooks into the Pencil-Code and
 !  currently allows the following customizations:
 !
-!   Description                                     | Relevant function call
+!  Description                                     | Relevant function call
 !  ---------------------------------------------------------------------------
-!   Special variable registration                   | register_special
-!     (pre parameter read)                          |
-!   Special variable initialization                 | initialize_special
-!     (post parameter read)                         |
-!                                                   |
-!   Special initial condition                       | init_special
-!    this is called last so may be used to modify   |
-!    the mvar variables declared by this module     |
-!    or optionally modify any of the other f array  |
-!    variables.  The latter, however, should be     |
-!    avoided where ever possible.                   |
-!                                                   |
-!   Special term in the mass (density) equation     | special_calc_density
-!   Special term in the momentum (hydro) equation   | special_calc_hydro
-!   Special term in the entropy equation            | special_calc_entropy
-!   Special term in the induction (magnetic)        | special_calc_magnetic
-!      equation                                     |
-!                                                   |
-!   Special equation                                | dspecial_dt
-!     NOT IMPLEMENTED FULLY YET - HOOKS NOT PLACED INTO THE PENCIL-CODE
+!  Special variable registration                   | register_special
+!    (pre parameter read)                          |
+!  Special variable initialization                 | initialize_special
+!    (post parameter read)                         |
+!                                                  |
+!  Special initial condition                       | init_special
+!   this is called last so may be used to modify   |
+!   the mvar variables declared by this module     |
+!   or optionally modify any of the other f array  |
+!   variables.  The latter, however, should be     |
+!   avoided where ever possible.                   |
+!                                                  |
+!  Special term in the mass (density) equation     | special_calc_density
+!  Special term in the momentum (hydro) equation   | special_calc_hydro
+!  Special term in the entropy equation            | special_calc_entropy
+!  Special term in the induction (magnetic)        | special_calc_magnetic
+!     equation                                     |
+!                                                  |
+!  Special equation                                | dspecial_dt
+!    NOT IMPLEMENTED FULLY YET - HOOKS NOT PLACED INTO THE PENCIL-CODE
 !
 !** AUTOMATIC CPARAM.INC GENERATION ****************************
 ! Declare (for generation of cparam.inc) the number of f array
@@ -44,7 +44,7 @@
 ! --------------------
 !
 ! Change the line above to
-!    lspecial = .true.
+!   lspecial = .true.
 ! to enable use of special hooks.
 !
 ! The rest of this file may be used as a template for your own
@@ -64,7 +64,7 @@
 ! use your additional physics.  Add a line with all the module
 ! selections to say something like:
 !
-!    SPECIAL=special/geo_kws
+!   SPECIAL=special/geo_kws
 !
 ! Where geo_kws it replaced by the filename of your new module
 ! upto and not including the .f90
@@ -86,23 +86,27 @@ module Special
 !
 ! Declare index of new variables in f array (if any).
 !
-!!   integer :: iSPECIAL_VARIABLE_INDEX=0
+!!   integer :: ispecial=0
+!!   integer :: ispecaux=0
 !
 !! Diagnostic variables (needs to be consistent with reset list below).
 !
-!!   integer :: i_POSSIBLEDIAGNOSTIC=0
+!!   integer :: idiag_POSSIBLEDIAGNOSTIC=0
 !
   contains
 !***********************************************************************
     subroutine register_special()
 !
-!  Configure pre-initialised (i.e. before parameter read) variables
-!  which should be know to be able to evaluate
+!  Set up indices for variables in special modules.
 !
 !  6-oct-03/tony: coded
 !
       if (lroot) call svn_id( &
            "$Id$")
+!
+!!      call farray_register_pde('special',ispecial)
+!!      call farray_register_auxiliary('specaux',ispecaux)
+!!      call farray_register_auxiliary('specaux',ispecaux,communicated=.true.)
 !
     endsubroutine register_special
 !***********************************************************************
@@ -139,7 +143,6 @@ module Special
       real, dimension (mx,my,mz,mfarray) :: f
 !
       intent(inout) :: f
-
 !!
 !!  SAMPLE IMPLEMENTATION
 !!
@@ -147,9 +150,6 @@ module Special
 !!        case('nothing'); if (lroot) print*,'init_special: nothing'
 !!        case('zero', '0'); f(:,:,:,iSPECIAL_VARIABLE_INDEX) = 0.
 !!        case default
-!!          !
-!!          !  Catch unknown values
-!!          !
 !!          if (lroot) print*,'init_special: No such value for initspecial: ', trim(initspecial)
 !!          call stop_it("")
 !!      endselect
@@ -180,10 +180,10 @@ module Special
 !***********************************************************************
     subroutine calc_pencils_special(f,p)
 !
-!  Calculate Hydro pencils.
+!  Calculate Special pencils.
 !  Most basic pencils should come first, as others may depend on them.
 !
-!   24-nov-04/tony: coded
+!  24-nov-04/tony: coded
 !
       real, dimension (mx,my,mz,mfarray) :: f
       type (pencil_case) :: p
@@ -208,29 +208,28 @@ module Special
 !  several precalculated Pencils of information are passed if for
 !  efficiency.
 !
-!   06-oct-03/tony: coded
+!  06-oct-03/tony: coded
 !
       use Mpicomm
 !
       real, dimension (mx,my,mz,mfarray) :: f
       real, dimension (mx,my,mz,mvar) :: df
       type (pencil_case) :: p
-
 !
       intent(in) :: f,p
       intent(inout) :: df
 !
-!  identify module and boundary conditions
+!  Identify module and boundary conditions.
 !
       if (headtt.or.ldebug) print*,'dspecial_dt: SOLVE dSPECIAL_dt'
-!!      if (headtt) call identify_bcs('ss',iss)
+!!      if (headtt) call identify_bcs('special',ispecial)
 !
 !!
 !! SAMPLE DIAGNOSTIC IMPLEMENTATION
 !!
 !!      if (ldiagnos) then
-!!        if (i_SPECIAL_DIAGNOSTIC/=0) then
-!!          call sum_mn_name(SOME MATHEMATICAL EXPRESSION,i_SPECIAL_DIAGNOSTIC)
+!!        if (idiag_SPECIAL_DIAGNOSTIC/=0) then
+!!          call sum_mn_name(MATHEMATICAL EXPRESSION,idiag_SPECIAL_DIAGNOSTIC)
 !!! see also integrate_mn_name
 !!        endif
 !!      endif
@@ -245,8 +244,8 @@ module Special
       integer, intent(in) :: unit
       integer, intent(inout), optional :: iostat
 !
-      if (present(iostat)) call keep_compiler_quiet(iostat)
       call keep_compiler_quiet(unit)
+      if (present(iostat)) call keep_compiler_quiet(iostat)
 !
     endsubroutine read_special_init_pars
 !***********************************************************************
@@ -263,8 +262,8 @@ module Special
       integer, intent(in) :: unit
       integer, intent(inout), optional :: iostat
 !
-      if (present(iostat)) call keep_compiler_quiet(iostat)
       call keep_compiler_quiet(unit)
+      if (present(iostat)) call keep_compiler_quiet(iostat)
 !
     endsubroutine read_special_run_pars
 !***********************************************************************
@@ -278,13 +277,10 @@ module Special
 !***********************************************************************
     subroutine rprint_special(lreset,lwrite)
 !
-!  reads and registers print parameters relevant to special
+!  Reads and registers print parameters relevant to special.
 !
-!   06-oct-03/tony: coded
+!  06-oct-03/tony: coded
 !
-!!
-!!!   SAMPLE IMPLEMENTATION
-!!
 !!      integer :: iname
       logical :: lreset,lwr
       logical, optional :: lwrite
@@ -326,7 +322,7 @@ module Special
 !***********************************************************************
     subroutine calc_lspecial_pars(f)
 !
-!  dummy routine
+!  Dummy routine.
 !
 !  15-jan-08/axel: coded
 !
@@ -339,25 +335,22 @@ module Special
 !***********************************************************************
     subroutine special_calc_density(f,df,p)
 !
-!   calculate a additional 'special' term on the right hand side of the
-!   continuity equation.
+!  Calculate a additional 'special' term on the right hand side of the
+!  continuity equation.
 !
-!   Some precalculated pencils of data are passed in for efficiency
-!   others may be calculated directly from the f array
+!  Some precalculated pencils of data are passed in for efficiency
+!  others may be calculated directly from the f array
 !
-!   06-oct-03/tony: coded
+!  06-oct-03/tony: coded
 !
       real, dimension (mx,my,mz,mfarray), intent(in) :: f
       real, dimension (mx,my,mz,mvar), intent(inout) :: df
       type (pencil_case), intent(in) :: p
-
 !!
 !!  SAMPLE IMPLEMENTATION
 !!     (remember one must ALWAYS add to df)
 !!
-!!
 !!  df(l1:l2,m,n,ilnrho) = df(l1:l2,m,n,ilnrho) + SOME NEW TERM
-!!
 !!
       call keep_compiler_quiet(f,df)
       call keep_compiler_quiet(p)
@@ -366,13 +359,13 @@ module Special
 !***********************************************************************
     subroutine special_calc_hydro(f,df,p)
 !
-!   calculate a additional 'special' term on the right hand side of the
-!   momentum equation.
+!  Calculate a additional 'special' term on the right hand side of the
+!  momentum equation.
 !
-!   Some precalculated pencils of data are passed in for efficiency
-!   others may be calculated directly from the f array
+!  Some precalculated pencils of data are passed in for efficiency
+!  others may be calculated directly from the f array.
 !
-!   06-oct-03/tony: coded
+!  06-oct-03/tony: coded
 !
       real, dimension (mx,my,mz,mfarray), intent(in) :: f
       real, dimension (mx,my,mz,mvar), intent(inout) :: df
@@ -381,11 +374,9 @@ module Special
 !!  SAMPLE IMPLEMENTATION
 !!     (remember one must ALWAYS add to df)
 !!
-!!
 !!  df(l1:l2,m,n,iux) = df(l1:l2,m,n,iux) + SOME NEW TERM
 !!  df(l1:l2,m,n,iuy) = df(l1:l2,m,n,iuy) + SOME NEW TERM
 !!  df(l1:l2,m,n,iuz) = df(l1:l2,m,n,iuz) + SOME NEW TERM
-!!
 !!
       call keep_compiler_quiet(f,df)
       call keep_compiler_quiet(p)
@@ -394,13 +385,13 @@ module Special
 !***********************************************************************
     subroutine special_calc_magnetic(f,df,p)
 !
-!   calculate a additional 'special' term on the right hand side of the
-!   induction equation.
+!  Calculate a additional 'special' term on the right hand side of the
+!  induction equation.
 !
-!   Some precalculated pencils of data are passed in for efficiency
-!   others may be calculated directly from the f array
+!  Some precalculated pencils of data are passed in for efficiency
+!  others may be calculated directly from the f array.
 !
-!   06-oct-03/tony: coded
+!  06-oct-03/tony: coded
 !
       real, dimension (mx,my,mz,mfarray), intent(in) :: f
       real, dimension (mx,my,mz,mvar), intent(inout) :: df
@@ -408,7 +399,6 @@ module Special
 !!
 !!  SAMPLE IMPLEMENTATION
 !!     (remember one must ALWAYS add to df)
-!!
 !!
 !!  df(l1:l2,m,n,iux) = df(l1:l2,m,n,iux) + SOME NEW TERM
 !!  df(l1:l2,m,n,iuy) = df(l1:l2,m,n,iuy) + SOME NEW TERM
@@ -421,13 +411,13 @@ module Special
 !!***********************************************************************
     subroutine special_calc_entropy(f,df,p)
 !
-!   calculate a additional 'special' term on the right hand side of the
-!   entropy equation.
+!  Calculate a additional 'special' term on the right hand side of the
+!  entropy equation.
 !
-!   Some precalculated pencils of data are passed in for efficiency
-!   others may be calculated directly from the f array
+!  Some precalculated pencils of data are passed in for efficiency
+!  others may be calculated directly from the f array
 !
-!   06-oct-03/tony: coded
+!  06-oct-03/tony: coded
 !
       real, dimension (mx,my,mz,mfarray), intent(in) :: f
       real, dimension (mx,my,mz,mvar), intent(inout) :: df
@@ -436,9 +426,7 @@ module Special
 !!  SAMPLE IMPLEMENTATION
 !!     (remember one must ALWAYS add to df)
 !!
-!!
 !!  df(l1:l2,m,n,ient) = df(l1:l2,m,n,ient) + SOME NEW TERM
-!!
 !!
       call keep_compiler_quiet(f,df)
       call keep_compiler_quiet(p)
@@ -447,10 +435,10 @@ module Special
 !***********************************************************************
     subroutine special_after_timestep(f,df,dt_)
 !
-!   Possibility to modify the f and df after df is updated
-!   Used for the fargo shift, for instance.
+!  Possibility to modify the f and df after df is updated
+!  Used for the fargo shift, for instance.
 !
-!   27-nov-08/wlad: coded
+!  27-nov-08/wlad: coded
 !
       real, dimension(mx,my,mz,mfarray) :: f
       real, dimension(mx,my,mz,mvar) :: df
@@ -463,10 +451,10 @@ module Special
 !********************************************************************
     subroutine special_calc_particles(fp)
 !
-!   Called before the loop, in case some particle value is needed 
-!   for the special density/hydro/magnetic/entropy
+!  Called before the loop, in case some particle value is needed 
+!  for the special density/hydro/magnetic/entropy.
 !
-!   20-nov-08/wlad: coded
+!  20-nov-08/wlad: coded
 !
       real, dimension (:,:), intent(in) :: fp
 !
@@ -476,10 +464,10 @@ module Special
 !***********************************************************************
     subroutine special_calc_particles_nbody(fsp)
 !
-!   Called before the loop, in case some massive particles value 
-!   is needed for the special density/hydro/magnetic/entropy
+!  Called before the loop, in case some massive particles value 
+!  is needed for the special density/hydro/magnetic/entropy.
 !
-!   20-nov-08/wlad: coded
+!  20-nov-08/wlad: coded
 !
       real, dimension (:,:), intent(in) :: fsp
 !
@@ -489,10 +477,10 @@ module Special
 !***********************************************************************
     subroutine special_boundconds(f,bc)
 !
-!   Some precalculated pencils of data are passed in for efficiency
-!   others may be calculated directly from the f array
+!  Some precalculated pencils of data are passed in for efficiency
+!  others may be calculated directly from the f array.
 !
-!   06-oct-03/tony: coded
+!  06-oct-03/tony: coded
 !
       real, dimension (mx,my,mz,mfarray), intent(in) :: f
       type (boundary_condition) :: bc
@@ -504,13 +492,13 @@ module Special
 !***********************************************************************
     subroutine special_before_boundary(f)
 !
-!   Possibility to modify the f array before the boundaries are
-!   communicated.
+!  Possibility to modify the f array before the boundaries are
+!  communicated.
 !
-!   Some precalculated pencils of data are passed in for efficiency
-!   others may be calculated directly from the f array
+!  Some precalculated pencils of data are passed in for efficiency
+!  others may be calculated directly from the f array
 !
-!   06-jul-06/tony: coded
+!  06-jul-06/tony: coded
 !
       real, dimension (mx,my,mz,mfarray), intent(in) :: f
 !
