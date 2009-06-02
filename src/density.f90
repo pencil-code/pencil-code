3fc7d698dd86da31376d7aa82a67d868a50c80e9
Boris Dintrans <boris.dintrans@irap.omp.eu>
2009-06-02 15:16:01 +0000

    print on the screen the blob informations when asking the blob_hs setup

--- src/density.f90
+++ src/density.f90
@@ -420,6 +420,9 @@ module Density
         case('blob')
           call blob(ampllnrho(j),f,ilnrho,radius_lnrho(j),xblob,yblob,zblob)
         case('blob_hs')
+          print*, 'init_lnrho: put a blob in hydrostatic equilibrium: &
+          radius_lnrho, ampllnrho, position=',radius_lnrho(j), &
+          ampllnrho(j), xblob, yblob, zblob
           call blob(ampllnrho(j),f,ilnrho,radius_lnrho(j),xblob,yblob,zblob)
           call blob(-ampllnrho(j),f,iss,radius_lnrho(j),xblob,yblob,zblob)
         case('isothermal'); call isothermal_density(f)
