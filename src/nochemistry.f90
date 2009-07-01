2a0571c76ca461ae7cf31f1d5db7f522ec248532
Anders Johansen <anders@astro.lu.se>
2009-07-01 05:26:31 +0000

    Added dummy routine jacobn.

--- src/nochemistry.f90
+++ src/nochemistry.f90
@@ -140,106 +140,66 @@ module Chemistry
 !********************************************************************
     subroutine bc_nscbc_subin_x(f,df,topbot,val)
 !
-!   nscbc case 
-!   subsonic inflow boundary conditions
-!   now it is 2D case (should be corrected for 3D case)
-!    ux,uy,T are fixed, drho  is calculated 
-!
-!   16-nov-08/natalia: coded.
-!
-      use MpiComm, only: stop_it
-      use EquationOfState, only: cs0, cs20
-      use Deriv, only: der_onesided_4_slice, der_pencil
-!      use Chemistry
-!
       real, dimension (mx,my,mz,mfarray) :: f
       real, dimension (mx,my,mz,mvar) :: df
       character (len=3) :: topbot
       real, dimension (mcom), optional :: val
 !
-      intent(inout) :: f
-      intent(out) :: df
+      call keep_compiler_quiet(f)
+      call keep_compiler_quiet(df)
+      call keep_compiler_quiet(topbot)
+      call keep_compiler_quiet(val)
 !
-
-     call keep_compiler_quiet(f)
-     call keep_compiler_quiet(df)
-
     endsubroutine bc_nscbc_subin_x
 !********************************************************************
-  subroutine bc_nscbc_nref_subout_x(f,df,topbot)
-!
-!   nscbc case 
-!   subsonic non-reflecting outflow boundary conditions
-!   now it is 2D case (should be corrected for 3D case)
-!    drho. dT, dux, duy  are calculated, p_inf can be 
-!   fixed (if nscbc_sigma <>0)
-!
-!   16-nov-08/natalia: coded.
-!
-      use MpiComm, only: stop_it
-      !use EquationOfState, only: cs0, cs20
-      use Deriv, only: der_onesided_4_slice,der_pencil
+    subroutine bc_nscbc_nref_subout_x(f,df,topbot)
 !
       real, dimension (mx,my,mz,mfarray) :: f
       real, dimension (mx,my,mz,mvar) :: df
       character (len=3) :: topbot
-
-     
-     call keep_compiler_quiet(f)
-     call keep_compiler_quiet(df)
+!     
+      call keep_compiler_quiet(f)
+      call keep_compiler_quiet(df)
+      call keep_compiler_quiet(topbot)
 !
     endsubroutine bc_nscbc_nref_subout_x
 !***********************************************************************
- subroutine bc_nscbc_nref_subout_y(f,df,topbot)
-!
-!   nscbc case 
-!   subsonic non-reflecting outflow boundary conditions
-!   now it is 2D case (should be corrected for 3D case)
-!    drho. dT, dux, duy  are calculated, p_inf can be 
-!   fixed (if nscbc_sigma <>0)
-!
-!   16-nov-08/natalia: coded.
-!
-      use MpiComm, only: stop_it
-      !use EquationOfState, only: cs0, cs20
-      use Deriv, only: der_onesided_4_slice,der_pencil
+    subroutine bc_nscbc_nref_subout_y(f,df,topbot)
 !
       real, dimension (mx,my,mz,mfarray) :: f
       real, dimension (mx,my,mz,mvar) :: df
       character (len=3) :: topbot
-
-     
-     call keep_compiler_quiet(f)
-     call keep_compiler_quiet(df)
+!     
+      call keep_compiler_quiet(f)
+      call keep_compiler_quiet(df)
+      call keep_compiler_quiet(topbot)
 !
     endsubroutine bc_nscbc_nref_subout_y
 !***********************************************************************
- subroutine bc_nscbc_nref_subout_z(f,df,topbot)
-!
-!   nscbc case 
-!   subsonic non-reflecting outflow boundary conditions
-!   now it is 2D case (should be corrected for 3D case)
-!    drho. dT, dux, duy  are calculated, p_inf can be 
-!   fixed (if nscbc_sigma <>0)
-!
-!   16-nov-08/natalia: coded.
-!
-      use MpiComm, only: stop_it
-      !use EquationOfState, only: cs0, cs20
-      use Deriv, only: der_onesided_4_slice,der_pencil
+    subroutine bc_nscbc_nref_subout_z(f,df,topbot)
 !
       real, dimension (mx,my,mz,mfarray) :: f
       real, dimension (mx,my,mz,mvar) :: df
       character (len=3) :: topbot
-
-     
-     call keep_compiler_quiet(f)
-     call keep_compiler_quiet(df)
+!     
+      call keep_compiler_quiet(f)
+      call keep_compiler_quiet(df)
+      call keep_compiler_quiet(topbot)
 !
     endsubroutine bc_nscbc_nref_subout_z
 !***********************************************************************
-  subroutine chemistry_clean_up()
+    subroutine chemistry_clean_up()
 !
-  endsubroutine chemistry_clean_up
-!********************************************************************
+    endsubroutine chemistry_clean_up
+!***********************************************************************
+    subroutine jacobn(f,jacob)
+!
+      real, dimension(mx,my,mz,mfarray) :: f
+      real, dimension(mx,my,mz,nchemspec,nchemspec) :: jacob
+!
+      call keep_compiler_quiet(f)
+      call keep_compiler_quiet(jacob(1,1,1,1,1))
+!
+    endsubroutine jacobn
+!***********************************************************************
 endmodule Chemistry
