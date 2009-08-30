bc4476314d64f58ec973532b0b1bc6caebfaf7c2
Sven Bingert <sven.bingert@gwdg.de>
2009-08-30 19:21:10 +0000

    long int for mw and nwgrid, nw was already but nwgrid is even bigger

--- src/cparam.f90
+++ src/cparam.f90
@@ -34,7 +34,7 @@ module Cparam
   integer, parameter :: mx=nx+2*nghost,l1=1+nghost,l2=mx-nghost
   integer, parameter :: my=ny+2*nghost,m1=1+nghost,m2=my-nghost
   integer, parameter :: mz=nz+2*nghost,n1=1+nghost,n2=mz-nghost
-  integer, parameter :: mw=mx*my*mz,nwgrid=nxgrid*nygrid*nzgrid
+  integer(KIND=ikind8), parameter :: mw=mx*my*mz,nwgrid=nxgrid*nygrid*nzgrid
 !
   integer, parameter :: l1i=l1+nghost-1,l2i=l2-nghost+1
   integer, parameter :: m1i=m1+nghost-1,m2i=m2-nghost+1
