a7a87ef76f436edc93cea69b234b26d93a6f89d9
Anders Johansen <anders@astro.lu.se>
2009-09-16 13:12:45 +0000

    In order to add random particle velocity initial conditions, we need to keep random number first in variable 'r' and then add to fp.
    This probably changes the random particle velocity initial condition samples -
    I'll check now.

--- src/particles_dust.f90
+++ src/particles_dust.f90
@@ -978,43 +978,37 @@ k_loop:   do while (.not. (k>npar_loc))
           if (lroot) print*, 'init_particles: Random particle velocities; '// &
               'delta_vp0=', delta_vp0
           do k=1,npar_loc
-            call random_number_wrapper(fp(k,ivpx))
-            call random_number_wrapper(fp(k,ivpy))
-            call random_number_wrapper(fp(k,ivpz))
+            call random_number_wrapper(r)
+            fp(1:npar_loc,ivpx) = fp(1:npar_loc,ivpx) + delta_vp0*(2*r-1)
+            call random_number_wrapper(r)
+            fp(1:npar_loc,ivpy) = fp(1:npar_loc,ivpy) + delta_vp0*(2*r-1)
+            call random_number_wrapper(r)
+            fp(1:npar_loc,ivpz) = fp(1:npar_loc,ivpz) + delta_vp0*(2*r-1)
           enddo
-          fp(1:npar_loc,ivpx) = fp(1:npar_loc,ivpx) - &
-              delta_vp0 + fp(1:npar_loc,ivpx)*2*delta_vp0
-          fp(1:npar_loc,ivpy) = fp(1:npar_loc,ivpy) - &
-              delta_vp0 + fp(1:npar_loc,ivpy)*2*delta_vp0
-          fp(1:npar_loc,ivpz) = fp(1:npar_loc,ivpz) - &
-              delta_vp0 + fp(1:npar_loc,ivpz)*2*delta_vp0
 
         case ('random-x')
           if (lroot) print*, 'init_particles: Random particle x-velocity; '// &
               'delta_vp0=', delta_vp0
           do k=1,npar_loc
-            call random_number_wrapper(fp(k,ivpx))
+            call random_number_wrapper(r)
+            fp(k,ivpx) = fp(k,ivpx) + delta_vp0*(2*r-1)
           enddo
-          fp(1:npar_loc,ivpx) = fp(1:npar_loc,ivpx) - &
-              delta_vp0 + fp(1:npar_loc,ivpx)*2*delta_vp0
 
         case ('random-y')
           if (lroot) print*, 'init_particles: Random particle y-velocity; '// &
               'delta_vp0=', delta_vp0
           do k=1,npar_loc
-            call random_number_wrapper(fp(k,ivpy))
+            call random_number_wrapper(r)
+            fp(k,ivpy) = fp(k,ivpy) + delta_vp0*(2*r-1)
           enddo
-          fp(1:npar_loc,ivpy) = fp(1:npar_loc,ivpy) - &
-              delta_vp0 + fp(1:npar_loc,ivpy)*2*delta_vp0
 
         case ('random-z')
           if (lroot) print*, 'init_particles: Random particle z-velocity; '// &
               'delta_vp0=', delta_vp0
           do k=1,npar_loc
-            call random_number_wrapper(fp(k,ivpz))
+            call random_number_wrapper(r)
+            fp(k,ivpz) = fp(k,ivpz) + delta_vp0*(2*r-1)
           enddo
-          fp(1:npar_loc,ivpz) = fp(1:npar_loc,ivpz) - &
-              delta_vp0 + fp(1:npar_loc,ivpz)*2*delta_vp0
 
         case ('average-to-zero')
           call mpireduce_sum(sum(fp(1:npar_loc,ivpx)),vpx_sum)
