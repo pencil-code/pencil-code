ace7ba14a7fb3fb9cc044358734e23bb459529a6
Wladimir Lyra <wladimir.lyra@gmail.com>
2009-09-24 09:26:18 +0000

    add 2D baroclinic sample

---
+++ samples/2d-tests/baroclinic/initial_condition/baroclinic_init.f90
@@ -0,0 +1,284 @@
+! $Id: baroclinic_init.f90,v 1.4 2009-09-24 09:08:03 wlyra Exp $
+!
+!** AUTOMATIC CPARAM.INC GENERATION ****************************
+! Declare (for generation of cparam.inc) the number of f array
+! variables and auxiliary variables added by this module
+!
+! CPARAM logical, parameter :: linitial_condition = .true.
+!
+!***************************************************************
+!
+!
+module InitialCondition
+!
+  use Cdata
+  use Cparam
+  use Messages
+  use Sub, only: keep_compiler_quiet
+!
+  implicit none
+!
+  include '../initial_condition.h'
+!
+  real :: rho_rms=0.05
+  integer :: xmodes=10,ymodes=10,zmodes=0
+  logical :: lunstratified=.false.
+
+  namelist /initial_condition_pars/ &
+      xmodes,ymodes,zmodes,rho_rms,lunstratified
+!
+  contains
+!***********************************************************************
+    subroutine register_initial_condition()
+!
+!  Register variables associated with this module; likely none.
+!
+!  07-may-09/wlad: coded
+!
+      if (lroot) call svn_id( &
+           "$Id: baroclinic_init.f90,v 1.4 2009-09-24 09:08:03 wlyra Exp $")
+!
+    endsubroutine register_initial_condition
+!***********************************************************************
+    subroutine initialize_initial_condition(f)
+!
+!  Initialize any module variables which are parameter dependent.
+!
+!  07-may-09/wlad: coded
+!
+      real, dimension (mx,my,mz,mfarray) :: f
+!
+      call keep_compiler_quiet(f)
+!
+    endsubroutine initialize_initial_condition
+!***********************************************************************
+    subroutine initial_condition_uu(f)
+!
+!  Initialize the velocity field.
+!
+!  07-may-09/wlad: coded
+!
+      real, dimension (mx,my,mz,mfarray), intent(inout) :: f
+!
+!  SAMPLE IMPLEMENTATION
+!
+      call keep_compiler_quiet(f)
+!
+    endsubroutine initial_condition_uu
+!***********************************************************************
+    subroutine initial_condition_lnrho(f)
+!
+!  Initialize logarithmic density. init_lnrho will take care of
+!  converting it to linear density if you use ldensity_nolog.
+!
+!  07-may-09/wlad: coded
+!
+      use General, only: random_number_wrapper
+      use Mpicomm
+!
+      real, dimension (mx,my,mz,mfarray), intent(inout) :: f
+      real, dimension (nx,ny,nz) :: lump_of_sines
+      real :: Lx,Ly,Lz,d0,phase,xi,yi,zi,rhorms,nw1
+      real :: fmeantmp_rho2,fmeantmp_rho
+      real :: fmean_rho2,fmean_rho
+      real :: unnormalized_rho_rms
+      real :: normalization_factor
+      integer :: i,mm,nn,ll,nw,icount,mm1,ll1,nn1,itmp,irho
+!
+      Lx=Lxyz(1) ; Ly=Lxyz(2) ; Lz=Lxyz(3)
+!
+      d0=0.2*Lx
+!
+      if (lroot) print*,'domain size=',Lx,Ly,Lz
+!
+!  save the original linear density 
+!  on the free spot of shock, which isn't set yet
+!
+      if (lshock) then
+        itmp=ishock
+        f(l1:l2,m1:m2,n1:n2,itmp)=f(l1:l2,m1:m2,n1:n2,ilnrho)
+      else 
+        if (lroot) then
+          print*,'This is a baroclinic run. You expect '
+          print*,'many vortices to form in this run, do you not?'
+          print*,'These beasts usually get supersonic, which is '
+          print*,'actually what prevents them from growing too much,'
+          print*,'as then they shock and dissipate. Yet, you are'
+          print*,'NOT using shock viscosity. I recommend you to stop'
+          print*,'and switch SHOCK=shock_highorder in src/Makefile.local'
+        endif
+        call fatal_error("","")     
+      endif
+!
+!  Work with LINEAR density. As this is start time, there is no confusion
+!  linear-log. All initial conditions are to be coded in log. We will 
+!  construct the density here as linear, then pass to log
+!
+      irho=ilnrho
+!
+!  Have to set it to something first, otherwise the sines 
+!  can lead to negative densities.
+!
+      f(l1:l2,m1:m2,n1:n2,irho)=1.
+      lump_of_sines=0.
+!      
+      do ll=-xmodes,xmodes ; do mm=0,ymodes ; do nn=-zmodes,zmodes
+!
+        if (lroot) call random_number_wrapper(phase)
+        call mpibcast_real(phase,1)
+!
+        do i=1,nx ; do m=1,ny ; do n=1,nz
+          ll1=i+l1-1 ; xi=x(ll1)
+          mm1=m+m1-1 ; yi=y(mm1)
+          nn1=n+n1-1 ; zi=z(nn1)
+!
+          lump_of_sines(i,m,n)=lump_of_sines(i,m,n) + &
+             sin(2*pi*(ll*xi/Lx + mm*yi/Ly + nn*zi/Lz+ phase))
+!
+        enddo;enddo;enddo !end grid loop
+      enddo;enddo;enddo !end modes loop
+!
+!  Now construct the density and fill in the normalization 
+!  constants needed to get a rms equal to the input rho_rms.
+!
+      fmeantmp_rho2=0.
+      fmeantmp_rho=0.
+      nw1=1./(nxgrid*nygrid*nzgrid*1.0)
+!      
+      do n=1,nz
+        nn1=n+n1-1
+        do m=1,ny
+          mm1=m+m1-1
+          do i=1,nx
+            ll1=i+l1-1 ; xi=x(ll1)
+            f(ll1,mm1,nn1,irho)=f(ll1,mm1,nn1,irho) + &
+                lump_of_sines(i,m,n)*exp(-(.5*xi/d0)**2)
+          enddo
+          fmeantmp_rho2=fmeantmp_rho2+nw1*sum(f(l1:l2,mm1,nn1,irho)**2)
+          fmeantmp_rho =fmeantmp_rho +nw1*sum(f(l1:l2,mm1,nn1,irho))
+        enddo
+      enddo !end grid loop
+!
+!  Sum the normalization constants over processors, and perform
+!  the normalization.
+!
+      call mpireduce_sum(fmeantmp_rho2,fmean_rho2)
+      call mpireduce_sum(fmeantmp_rho,fmean_rho)
+      call mpibcast_real(fmean_rho2,1)
+      call mpibcast_real(fmean_rho,1)
+!
+      unnormalized_rho_rms=sqrt(fmean_rho2-fmean_rho**2)
+      normalization_factor=rho_rms/unnormalized_rho_rms
+!
+!  Assumes rho0=1.
+!
+      f(l1:l2,m1:m2,n1:n2,irho)=1.+&
+          normalization_factor*(f(l1:l2,m1:m2,n1:n2,irho)-1.)
+!
+      if (lroot) then 
+        print*,'max density (linear): ',maxval(f(l1:l2,m1:m2,n1:n2,irho))
+        print*,'min density (linear): ',minval(f(l1:l2,m1:m2,n1:n2,irho))
+        print*,'rms density (linear): ',&
+            unnormalized_rho_rms*normalization_factor
+      endif
+!
+!  convert to log before finishing. 
+!
+      f(l1:l2,m1:m2,n1:n2,ilnrho)=&
+          f(l1:l2,m1:m2,n1:n2,itmp)+log(f(l1:l2,m1:m2,n1:n2,irho))
+!
+!  keep the original stratification in the ishock slot since it 
+!  will be needed when setting the entropy
+!
+    endsubroutine initial_condition_lnrho
+!***********************************************************************
+    subroutine initial_condition_ss(f)
+!
+!  Initialize entropy.
+!
+!  07-may-09/wlad: coded
+!
+      use EquationOfState, only: gamma,gamma_m1,gamma_inv,cs20,rho0,lnrho0
+
+      real, dimension (mx,my,mz,mfarray), intent(inout) :: f
+      real, dimension (nx) :: lnrho,lnTT,TT,rho
+      real :: cp,cv,cp1,lnTT0,pp0,TT0
+      integer :: irho
+!
+!  Get the density and use a constant pressure entropy condition
+!
+      cp=1.
+      cp1=1/cp
+      cv=gamma_inv*cp
+!
+      TT0 = cs20*cp1/gamma_m1 ; lnTT0=log(TT0)
+      pp0=(cp-cv)*TT0*rho0
+!
+      irho=ilnrho
+!
+      do m=m1,m2;do n=n1,n2
+!
+        if (nzgrid==1.or.&
+            (nzgrid/=1.and.lunstratified)) then  
+          !unstratified case, everything is fresh
+          rho = f(l1:l2,m,n,ilnrho)
+          TT=(pp0/((cp-cv)*rho))/TT0
+        else
+          !stratified case, there's a previous 
+          !density stratification, stored in ishock
+          rho = f(l1:l2,m,n,ilnrho)/exp(f(l1:l2,m,n,ishock))
+          TT=1/rho !now this is just the temperature perturbation
+        endif
+!
+        lnTT = log(TT)
+        lnrho=log(rho)
+!
+!  assumes cp=1
+!  
+        f(l1:l2,m,n,iss)=f(l1:l2,m,n,iss) + &
+            cv*(lnTT-gamma_m1*lnrho)
+!
+      enddo;enddo
+!
+!  Revert the original variable (ishock) to zero.
+!
+      f(:,:,:,ishock)=0.
+!
+    endsubroutine initial_condition_ss
+!********************************************************************
+    subroutine read_initial_condition_pars(unit,iostat)
+!
+!  07-may-09/wlad: coded
+!
+      integer, intent(in) :: unit
+      integer, intent(inout), optional :: iostat
+!
+      if (present(iostat)) then
+        read(unit,NML=initial_condition_pars,ERR=99, IOSTAT=iostat)
+      else
+        read(unit,NML=initial_condition_pars,ERR=99)
+      endif
+!
+99    return
+!
+    endsubroutine read_initial_condition_pars
+!***********************************************************************
+    subroutine write_initial_condition_pars(unit)
+!     
+      integer, intent(in) :: unit
+!
+      write(unit,NML=initial_condition_pars)
+!
+    endsubroutine write_initial_condition_pars
+!***********************************************************************
+!
+!********************************************************************
+!************        DO NOT DELETE THE FOLLOWING       **************
+!********************************************************************
+!**  This is an automatically generated include file that creates  **
+!**  copies dummy routines from noinitial_condition.f90 for any    **
+!**  InitialCondition routines not implemented in this file        **
+!**                                                                **
+    include '../initial_condition_dummies.inc'
+!********************************************************************
+endmodule InitialCondition
---
+++ samples/2d-tests/baroclinic/src/special/baroclinic_run.f90
@@ -0,0 +1,466 @@
+! $Id: baroclinic_run.f90,v 1.5 2009-08-29 02:21:37 wlyra Exp $
+
+!** AUTOMATIC CPARAM.INC GENERATION ****************************
+! Declare (for generation of cparam.inc) the number of f array
+! variables and auxiliary variables added by this module
+!
+! CPARAM logical, parameter :: lspecial = .true.
+!
+! MVAR CONTRIBUTION 0
+! MAUX CONTRIBUTION 0
+!
+!***************************************************************
+
+!-------------------------------------------------------------------
+!
+! HOW TO USE THIS FILE
+! --------------------
+!
+! The rest of this file may be used as a template for your own
+! special module.  Lines which are double commented are intended
+! as examples of code.  Simply fill out the prototypes for the 
+! features you want to use.
+!
+! Save the file with a meaningful name, eg. geo_kws.f90 and place
+! it in the $PENCIL_HOME/src/special directory.  This path has
+! been created to allow users ot optionally check their contributions
+! in to the Pencil-Code CVS repository.  This may be useful if you
+! are working on/using the additional physics with somebodyelse or 
+! may require some assistance from one of the main Pencil-Code team.
+!
+! To use your additional physics code edit the Makefile.local in
+! the src directory under the run directory in which you wish to
+! use your additional physics.  Add a line with all the module 
+! selections to say something like:
+!
+!    SPECIAL=special/nstar
+!
+! Where nstar it replaced by the filename of your new module
+! upto and not including the .f90
+!
+!--------------------------------------------------------------------
+
+!
+!  This file adds the terms to the shearing box equations that 
+!  come from an underlying large scale pressure gradient. If the pressure
+!  falls like p=p0/(r/R)**beta, then a Taylor expansion around R leads to
+!  p=p0*(1-beta/R0*x), since r=R0+x. This extra term enters on the equations
+!  as 
+!
+!     d(ux)/dt = usual terms + beta*p0/(rho*R0) - beta*p0/(rho0*R0) 
+!     d(EE)/dt = usual terms + beta*E0*ux/R0
+!
+!  The last term on the RHS of the momentum equation is because 
+!  the underlying pressure gradient also leads to a further reduction 
+!  of the rotational velocity. Having only the second term implemented 
+!  would lead to a large scale wind as the momentum equation tries to 
+!  adjust to the new rotational speed. 
+!  
+!  10-apr-09/wlad: coded
+!
+module Special
+!
+  use Cdata
+  use Cparam
+  use Messages
+  use Sub, only: keep_compiler_quiet
+!
+  implicit none
+!
+  include '../special.h'
+! Global arrays
+  real :: dummy,rho01,cs201,gammam11,Bshear=0.,p0,p01,nw1
+  real :: cv1
+  logical :: lunstratified=.false.,lstratification=.true.
+  logical :: lstatic_stratification=.false.
+  real, dimension (nx) :: strat
+  real, dimension (nz) :: rtime_strat
+!
+  namelist /special_init_pars/ dummy
+!   
+  namelist /special_run_pars/ Bshear,lunstratified,&
+      lstatic_stratification
+!
+  integer :: idiag_pstratm=0,idiag_pstratmax=0,idiag_pstratmin=0
+!
+  contains
+
+!***********************************************************************
+    subroutine register_special()
+!
+!  Configure pre-initialised (i.e. before parameter read) variables 
+!  which should be know to be able to evaluate
+!
+!  14-jul-09/wlad: coded
+!
+      use Cdata
+!
+      if (lroot) call svn_id( &
+           "$Id: baroclinic_run.f90,v 1.5 2009-08-29 02:21:37 wlyra Exp $")
+!
+    endsubroutine register_special
+!***********************************************************************
+    subroutine initialize_special(f)
+!
+!  called by run.f90 after reading parameters, but before the time loop
+!
+!  14-jul-09/wlad: coded
+!
+      use Cdata 
+      use Mpicomm
+      use EquationOfState, only: rho0,gamma_m1,cs20,gamma_inv,get_cp1,gamma
+!
+      real, dimension (mx,my,mz,mvar+maux) :: f
+      real :: cp1
+!
+!  Define if we want stratification
+!
+      if (nzgrid==1.or.&
+         (nzgrid/=1.and.lunstratified)) lstratification=.false.
+!
+!  Shortcuts for inverse variables
+!
+      rho01=1./rho0
+      cs201=1./cs20
+      gammam11=1./gamma_m1
+!
+!  Shortcut for the initial pressure
+!
+      p0 =rho0*cs20*gamma_inv
+      p01=1./p0
+!
+!  Shortcut for normalization nxgrid*nygrid 
+!
+      nw1=1./(nxgrid*nygrid)
+!
+!  Get cv1
+!
+        call get_cp1(cp1)
+        cv1=gamma*cp1
+!
+    endsubroutine initialize_special
+!***********************************************************************
+    subroutine pencil_criteria_special()
+! 
+!  All pencils that this special module depends on are specified here.
+! 
+!  18-07-06/wlad: coded
+!
+      lpenc_requested(i_rho1)=.true.
+      lpenc_requested(i_uu)=.true.
+!
+      if (lentropy.or.&
+          (ltemperature.and.(.not.ltemperature_nolog))) &
+          lpenc_requested(i_TT1)=.true.
+!
+      if (ltemperature.or.&
+          (lentropy.and.pretend_lnTT)) &
+          lpenc_requested(i_cv1)=.true.
+!
+    endsubroutine pencil_criteria_special
+!***********************************************************************
+    subroutine calc_pencils_special(f,p)
+!
+!   14-jul-09/wlad: coded
+!
+      use Mpicomm
+      use Gravity, only: potential
+      use EquationOfState, only: gamma,cs20
+!
+      real, dimension(mx,my,mz,mfarray) :: f
+      type (pencil_case) :: p
+      real, dimension (nx) :: pot
+      integer :: i
+!
+!  Calculate the stratification pencil
+!
+      if (lstratification) then 
+!
+!  Choose between static (exp{-z^2/2H^2}) or 
+!  time-varying stratification (calculated in 
+!  special_before_boundary. 
+!
+        if (lstatic_stratification) then 
+          call potential(x(l1:l2),y(m),z(n),pot=pot)
+          strat=exp(-gamma*pot*cs201)
+        else
+          do i=1,nx;strat(i)=rtime_strat(n-n1+1);enddo
+        endif
+      else !no stratification  
+        strat=1.
+      endif
+!
+    endsubroutine calc_pencils_special
+!***********************************************************************
+    subroutine dspecial_dt(f,df,p)
+!
+      use Diagnostics
+!
+      real, dimension (mx,my,mz,mvar+maux) :: f
+      real, dimension (mx,my,mz,mvar) :: df
+      type (pencil_case) :: p
+!
+      intent(in) :: f,p
+      intent(inout) :: df
+!
+!  identify module and boundary conditions
+!
+      if (headtt.or.ldebug) print*,'dspecial_dt: SOLVE dSPECIAL_dt'
+!
+      if (ldiagnos) then
+        if (idiag_pstratm/=0) &
+             call sum_mn_name(strat,idiag_pstratm)
+        if (idiag_pstratmax/=0) &
+             call max_mn_name(strat,idiag_pstratmax)
+        if (idiag_pstratmin/=0) &
+             call max_mn_name(-strat,idiag_pstratmin,lneg=.true.)
+      endif
+!
+      call keep_compiler_quiet(f)
+      call keep_compiler_quiet(df)
+      call keep_compiler_quiet(p)
+!
+    endsubroutine dspecial_dt
+!***********************************************************************
+    subroutine special_before_boundary(f)
+!
+!  This subroutine calculates the mean stratification of the 
+!  pressure. This is used on the baroclinic terms on the 
+!  radial velocity and the entropy equation. 
+!
+!   dux/dt = ... -1/rho* dp/dx
+!
+!  where p = p0 (1-beta*x/R) * f(z), with z being the 
+!  stratification function. The initial case is of course 
+!  f(z)=exp[-z^2/(2H^2)], and that is good for most cases. 
+!  (since the RHS of the vertical equation doesn't change in 
+!  time, Omega^2*z). But just for the sake of completeness, 
+!  this is a way to have a stratification function f(z,t) that
+!  also changes in time. The stratification is this case is 
+!  computed as the mean pressure at each level xy.
+!
+      use Mpicomm
+      use Sub
+      use EquationOfState, only: gamma_inv,gamma_m1,&
+          cs20,lnrho0,rho0
+      use Gravity
+!
+      real, dimension (mx,my,mz,mfarray), intent(inout) :: f
+      real, dimension (nx) :: cs2,rho,lnrho,ss,pot,strat
+      real, dimension (nx,nz) :: pp_tmp,pp_sumy
+      real, dimension (nz) :: pp_sum
+      integer :: nn,i,itsub
+      character (len=5) :: sit
+!
+      if (lstratification.and.&
+          (.not.lstatic_stratification)) then 
+!
+!  Calculate mean pressure stratification at each xy-level
+!        
+        pp_tmp=0.
+!
+        do n=n1,n2
+          do m=m1,m2
+!
+!  Get density and sound speed
+!
+            if (ldensity_nolog) then 
+              rho=f(l1:l2,m,n,ilnrho)
+              lnrho=log(rho)
+            else
+              lnrho=f(l1:l2,m,n,ilnrho)
+              rho=exp(lnrho)
+            endif
+!
+            if (ltemperature.or.pretend_lnTT) then 
+              call fatal_error("lazy wlad didn't want to code entropy","")
+            else
+              ss=f(l1:l2,m,n,iss)
+            endif
+            cs2=cs20*exp(cv1*ss+gamma_m1*(lnrho-lnrho0))
+!            
+!  Average (sum) the pressure of all y-grid cells. The result
+!  is xz dependent.
+!
+            nn=n-n1+1
+            pp_tmp(:,nn) = pp_tmp(:,nn)+rho*cs2*gamma_inv
+!
+          enddo
+        enddo
+!
+!  Now perform the y-sum over all y-processors (that's
+!  why lsumy=T). The result is still xz dependent.
+!
+        !call mpiallreduce_sum(pp_tmp,pp_sumy,(/nx,nz/),LSUMY=.true.)
+        call mpiallreduce_sum(pp_tmp,pp_sumy,(/nx,nz/),idir=2)
+!
+!  Now sum in x. The result is z-dependent.
+!
+        pp_sum=0.
+        do n=1,nz;do i=1,nx
+          pp_sum(n)=pp_sum(n)+pp_sumy(i,n)
+        enddo;enddo
+!
+!  Now divide the sum by the number of grid cells to get 
+!  the average (pp_sum*nw1). And normalize by the midplane
+!  initial pressure p0=rho0*cs0^2/gamma
+!
+        rtime_strat=pp_sum*nw1*p01
+!
+      endif
+
+    endsubroutine special_before_boundary
+!***********************************************************************
+    subroutine read_special_init_pars(unit,iostat)
+      integer, intent(in) :: unit
+      integer, intent(inout), optional :: iostat
+!
+      if (present(iostat)) then
+        read(unit,NML=special_init_pars,ERR=99, IOSTAT=iostat)
+      else
+        read(unit,NML=special_init_pars,ERR=99)
+      endif
+!
+99    return
+    endsubroutine read_special_init_pars
+!***********************************************************************
+    subroutine write_special_init_pars(unit)
+      integer, intent(in) :: unit
+
+      write(unit,NML=special_init_pars)
+
+    endsubroutine write_special_init_pars
+!***********************************************************************
+    subroutine read_special_run_pars(unit,iostat)
+      integer, intent(in) :: unit
+      integer, intent(inout), optional :: iostat
+!
+      if (present(iostat)) then
+        read(unit,NML=special_run_pars,ERR=99, IOSTAT=iostat)
+      else
+        read(unit,NML=special_run_pars,ERR=99)
+      endif
+!
+99    return
+endsubroutine read_special_run_pars
+!***********************************************************************
+    subroutine write_special_run_pars(unit)
+      integer, intent(in) :: unit
+!
+      write(unit,NML=special_run_pars)
+!
+    endsubroutine write_special_run_pars
+!***********************************************************************
+    subroutine rprint_special(lreset,lwrite)
+!
+      use Diagnostics
+!
+!  reads and registers print parameters relevant to special
+!
+!   14-jul-09/wlad: coded
+!
+      integer :: iname
+      logical :: lreset,lwr
+      logical, optional :: lwrite
+!
+!  Write information to index.pro
+!
+      if (lreset) then
+        idiag_pstratm=0;idiag_pstratmax=0;idiag_pstratmin=0 
+      endif
+!
+      do iname=1,nname
+        call parse_name(iname,cname(iname),cform(iname),'pstratm',idiag_pstratm)
+        call parse_name(iname,cname(iname),cform(iname),'pstratmax',idiag_pstratmax)
+        call parse_name(iname,cname(iname),cform(iname),'pstratmin',idiag_pstratmin)
+      enddo
+!
+      if (lwr) then
+        write(3,*) 'i_pstratm=',idiag_pstratm
+        write(3,*) 'i_pstratmax=',idiag_pstratmax
+        write(3,*) 'i_pstratmin=',idiag_pstratmin
+      endif
+!
+    endsubroutine rprint_special
+!***********************************************************************
+    subroutine special_calc_hydro(f,df,p)
+!
+!   calculate a additional 'special' term on the right hand side of the 
+!   momentum equation.
+!
+!   Some precalculated pencils of data are passed in for efficiency
+!   others may be calculated directly from the f array
+!
+      use Cdata
+      use Messages, only: fatal_error
+      use EquationOfState, only : rho0
+!      
+      real, dimension (mx,my,mz,mvar+maux), intent(in) :: f
+      real, dimension (mx,my,mz,mvar), intent(inout) :: df
+      type (pencil_case), intent(in) :: p
+      real, dimension (nx) :: rho1
+!
+!  Modified momentum equation
+!
+      rho1=p%rho1*strat
+!      
+      df(l1:l2,m,n,iux)= df(l1:l2,m,n,iux)+Bshear*p0*(rho1-rho01)
+!
+    endsubroutine special_calc_hydro
+!***********************************************************************
+    subroutine special_calc_entropy(f,df,p)
+!
+!   calculate a additional 'special' term on the right hand side of the 
+!   entropy equation.
+!
+!   Some precalculated pencils of data are passed in for efficiency
+!   others may be calculated directly from the f array
+!
+!   14-jul-09/wlad: coded
+!
+      use Cdata
+      use Messages, only: fatal_error
+!
+      real, dimension (mx,my,mz,mvar+maux), intent(in) :: f
+      real, dimension (mx,my,mz,mvar), intent(inout) :: df
+      type (pencil_case), intent(in) :: p
+      real, dimension (nx) :: rhs
+!
+!  Right hand side on the energy equation - background energy gradient
+!
+      rhs=Bshear*p0*p%uu(:,1)*gammam11*strat
+!
+      if (lentropy) then 
+        if (pretend_lnTT) then 
+          df(l1:l2,m,n,iss)=df(l1:l2,m,n,iss) + p%cv1*p%rho1*p%TT1*rhs
+        else
+          df(l1:l2,m,n,iss)=df(l1:l2,m,n,iss) + p%rho1*p%TT1*rhs
+        endif
+      else if (ltemperature) then 
+        if (ltemperature_nolog) then 
+          df(l1:l2,m,n,ilnTT)=df(l1:l2,m,n,ilnTT) + p%cv1*p%rho1*rhs
+        else
+          df(l1:l2,m,n,ilnTT)=df(l1:l2,m,n,ilnTT) + p%cv1*p%rho1*p%TT1*rhs
+        endif
+      else
+        print*,"You want to use a global baroclinic term but    "
+        print*,"you are NOT solving the energy equation. Better "
+        print*,"stop and check."
+        call fatal_error("global_baroclinic","")
+      endif
+!
+    endsubroutine special_calc_entropy
+!***********************************************************************
+!********************************************************************
+!
+!********************************************************************
+!************        DO NOT DELETE THE FOLLOWING       **************
+!********************************************************************
+!**  This is an automatically generated include file that creates  **
+!**  copies dummy routines from nospecial.f90 for any Special      **
+!**  routines not implemented in this file                         **
+!**                                                                **
+    include '../special_dummies.inc'
+!********************************************************************
+endmodule Special
+
