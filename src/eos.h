6d320beeb1bf73c30c50ba0d6a8b07193ebc6744
Piyali Chatterjee <mppiyali@gmail.com>
2009-10-16 09:23:06 +0000

    public ipp_ss

--- src/eos.h
+++ src/eos.h
@@ -5,6 +5,7 @@
   public :: eoscalc,pressure_gradient,temperature_gradient,get_cp1
   public :: temperature_laplacian,get_ptlaw
   public :: ilnrho_ss, ilnrho_lnTT, ilnrho_pp, ilnrho_ee, ilnrho_TT
+  public :: ipp_ss
   public :: eosperturb
   public :: get_soundspeed
   public :: getmu
