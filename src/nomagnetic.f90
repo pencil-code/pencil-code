2d414a3dc3395c886cf94dc29529e97fe3a84429
Sven Bingert <sven.bingert@gwdg.de>
2009-11-23 14:19:40 +0000

    minor, just to make the code conform

--- src/nomagnetic.f90
+++ src/nomagnetic.f90
@@ -296,7 +296,7 @@ module Magnetic
       type (slice_data) :: slices
 !
       call keep_compiler_quiet(f)
-      call keep_compiler_quiet(slices)
+      call keep_compiler_quiet(slices%ready)
 !
     endsubroutine get_slices_magnetic
 !***********************************************************************
