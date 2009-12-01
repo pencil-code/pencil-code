bf6aff27ddf026453d4c4545efa5de5e11a4ef8b
Sven Bingert <sven.bingert@gwdg.de>
2009-12-01 05:59:01 +0000

    just to keep coding conform

--- src/nopolymer.f90
+++ src/nopolymer.f90
@@ -159,7 +159,7 @@ module Polymer
       type (slice_data) :: slices
 !
       call keep_compiler_quiet(f)
-      call keep_compiler_quiet(slices)
+      call keep_compiler_quiet(slices%ready)
 !
     endsubroutine get_slices_polymer
 !***********************************************************************
