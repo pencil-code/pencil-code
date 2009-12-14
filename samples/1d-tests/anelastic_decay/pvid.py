ae8ae79419869afcfb1320bd0dce8d3e0695e624
Boris Dintrans <boris.dintrans@irap.omp.eu>
2009-12-14 10:48:02 +0000

    - improvement of the python subroutine to plot the time evolution of the velocity and pressure - svn propset Id

--- samples/1d-tests/anelastic_decay/pvid.py
+++ samples/1d-tests/anelastic_decay/pvid.py
@@ -5,8 +5,11 @@
 import numpy as N
 import pylab as P
 import pencil as pc
+from os import system
 
-f, t = pc.read_slices(field='uu1', proc=0, extension='xy')
+system('cat video.in')
+field=raw_input('which field? ')
+f, t = pc.read_slices(field=field, proc=0, extension='xy')
 
 dim=pc.read_dim()
 grid=pc.read_grid(trim=True)
@@ -18,7 +21,7 @@ P.subplot(211)
 line, = P.plot(grid.x, ff[0, :])
 P.xlim(grid.x[0], grid.x[-1])
 P.ylim(ymin=ff.min(), ymax=ff.max())
-P.title('velocity')
+P.title(field)
 st=P.figtext(0.2, 0.85, 't=%.1f'%t[0])
 
 for i in range(1, nt):
