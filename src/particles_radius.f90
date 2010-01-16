b4cdd8bd60967fb5b2b90739a7878347534820dd
Anders Johansen <anders@astro.lu.se>
2010-01-16 18:22:57 +0000

    - Condensation/evaporation in separate subroutine - Proper time-step implemented (hopefully)

--- src/particles_radius.f90
+++ src/particles_radius.f90
@@ -26,17 +26,25 @@ module Particles_radius
   real :: vthresh_sweepup=-1.0, deltavp12_floor=0.0
   real, dimension (ninit) :: ap0=0.0
   real :: tstart_sweepup_par=0.0, cdtps=0.2, cdtpc=0.2
-  real :: tstart_condensation_par=0.0, apcrit=0.0
+  real :: tstart_condensation_par=0.0, dt_min_evap=0.0
+  real :: apmin=0.0, latent_heat_SI=2.257e6, alpha_cond=1.0, alpha_cond1=1.0
+  real :: diffusion_coefficient=1.0, diffusion_coefficient1=1.0
+  real :: tau_damp_evap=0.0, tau_damp_evap1=0.0
   logical :: lsweepup_par=.true., lcondensation_par=.false.
   character (len=labellen), dimension(ninit) :: initap='nothing'
+  character (len=labellen) :: condensation_coefficient_type='constant'
 !
   namelist /particles_radius_init_pars/ &
-      initap, ap0, rhops, vthresh_sweepup, deltavp12_floor, apcrit, &
-      lsweepup_par, lcondensation_par, tstart_sweepup_par, cdtps
+      initap, ap0, rhops, vthresh_sweepup, deltavp12_floor, dt_min_evap, &
+      lsweepup_par, lcondensation_par, tstart_sweepup_par, cdtps, apmin, &
+      condensation_coefficient_type, alpha_cond, diffusion_coefficient, &
+      tau_damp_evap
 !
   namelist /particles_radius_run_pars/ &
-      rhops, vthresh_sweepup, deltavp12_floor, apcrit, &
-      lsweepup_par, lcondensation_par, tstart_sweepup_par, cdtps
+      rhops, vthresh_sweepup, deltavp12_floor, dt_min_evap, &
+      lsweepup_par, lcondensation_par, tstart_sweepup_par, cdtps, apmin, &
+      condensation_coefficient_type, alpha_cond, diffusion_coefficient, &
+      tau_damp_evap
 !
   integer :: idiag_apm=0, idiag_ap2m=0, idiag_apmin=0, idiag_apmax=0
   integer :: idiag_dvp12m=0, idiag_dtsweepp=0
@@ -91,6 +99,12 @@ module Particles_radius
             'mass per dust grain mp_tilde=', mp_tilde
       endif
 !
+!  Inverse coefficients.
+!
+      alpha_cond1=1/alpha_cond
+      diffusion_coefficient1=1/diffusion_coefficient
+      if (tau_damp_evap/=0.0) tau_damp_evap1=1/tau_damp_evap
+!
       call keep_compiler_quiet(f)
       call keep_compiler_quiet(lstarting)
 !
@@ -144,9 +158,26 @@ module Particles_radius
 !
 !  21-nov-06/anders: coded
 !
-      lpenc_requested(i_uu)=.true.
-      lpenc_requested(i_rho)=.true.
-      lpenc_requested(i_cc)=.true.
+      if (lsweepup_par) then
+        lpenc_requested(i_uu)=.true.
+        lpenc_requested(i_rho)=.true.
+        lpenc_requested(i_cc)=.true.
+      endif
+      if (lcondensation_par) then
+        lpenc_requested(i_cs2)=.true.
+        lpenc_requested(i_TT1)=.true.
+        lpenc_requested(i_ppvap)=.true.
+        lpenc_requested(i_rho)=.true.
+        lpenc_requested(i_rho1)=.true.
+        lpenc_requested(i_cc)=.true.
+        lpenc_requested(i_cc1)=.true.
+        lpenc_requested(i_np)=.true.
+        lpenc_requested(i_rhop)=.true.
+        if (ltemperature) then
+          lpenc_requested(i_cv1)=.true.
+          lpenc_requested(i_TT1)=.true.
+        endif
+      endif
 !
     endsubroutine pencil_criteria_par_radius
 !***********************************************************************
@@ -164,38 +195,66 @@ module Particles_radius
       real, dimension (mpar_loc,mpvar) :: fp, dfp
       type (pencil_case) :: p
       integer, dimension (mpar_loc,3) :: ineargrid
-!
-      real, dimension (nx) :: dt1_sweepup, dt1_condensation, cs
-      real :: deltavp, np_tilde, ppsat, dapdt, supsatratio1
-      integer :: k, ix0
-      logical :: lheader, lfirstcall=.true.
+      logical :: lfirstcall=.true., lheader
 !
       intent (in) :: f, fp
       intent (out) :: dfp
 !
 !  Print out header information in first time step.
 !
-      lheader=lfirstcall .and. lroot
+      lheader=lfirstcall.and.lroot.and.(.not.lpencil_check_at_work)
 !
-!  Identify module and boundary conditions.
+!  Identify module.
 !
-      if (lheader) print*,'dap_dt_pencil: Calculate dap_dt'
+      if (lheader) print*,'dap_dt_pencil: Calculate dap/dt'
+!
+      if (lsweepup_par) &
+          call dap_dt_sweepup_pencil(f,df,fp,dfp,p,ineargrid)
+      if (lcondensation_par) &
+          call dap_dt_condensation_pencil(f,df,fp,dfp,p,ineargrid)
+!
+      lfirstcall=.false.
+!
+    endsubroutine dap_dt_pencil
+!***********************************************************************
+    subroutine dap_dt_sweepup_pencil(f,df,fp,dfp,p,ineargrid)
 !
 !  Increase in particle radius due to sweep-up of small grains in the gas.
 !
-      if (lsweepup_par .and. t>=tstart_sweepup_par) then
+!  22-aug-05/anders: coded
+!
+      use Diagnostics
+      use Particles_number
+!
+      real, dimension (mx,my,mz,mfarray) :: f
+      real, dimension (mx,my,mz,mvar) :: df
+      real, dimension (mpar_loc,mpvar) :: fp, dfp
+      type (pencil_case) :: p
+      integer, dimension (mpar_loc,3) :: ineargrid
+!
+      real, dimension (nx) :: dt1_sweepup
+      real :: deltavp, np_tilde
+      integer :: k, ix0, ix
+!
+      intent (in) :: f, fp
+      intent (out) :: dfp
+!
+!  Increase in particle radius due to sweep-up of small grains in the gas.
+!
+      if (t>=tstart_sweepup_par) then
 !
         if (lfirst.and.ldt) dt1_sweepup=0.0
 !
         if (npar_imn(imn)/=0) then
           do k=k1_imn(imn),k2_imn(imn)
             ix0=ineargrid(k,1)
+            ix=ix0-nghost
 !  No interpolation needed here.
 !  Relative speed.
             deltavp=sqrt( &
-                (fp(k,ivpx)-p%uu(ix0-nghost,1))**2 + &
-                (fp(k,ivpy)-p%uu(ix0-nghost,2))**2 + &
-                (fp(k,ivpz)-p%uu(ix0-nghost,3))**2 )
+                (fp(k,ivpx)-p%uu(ix,1))**2 + &
+                (fp(k,ivpy)-p%uu(ix,2))**2 + &
+                (fp(k,ivpz)-p%uu(ix,3))**2 )
             if (deltavp12_floor/=0.0) &
                 deltavp=sqrt(deltavp**2+deltavp12_floor**2)
 !  Allow boulders to sweep up small grains if relative velocity not too high.
@@ -205,15 +264,14 @@ module Particles_radius
                     'must have passive scalar module for sweep-up')
               endif
 !  Radius increase due to sweep-up.
-              dfp(k,iap) = dfp(k,iap) + &
-                  0.25*deltavp*p%cc(ix0-nghost)*p%rho(ix0-nghost)/rhops
+              dfp(k,iap) = dfp(k,iap) + 0.25*deltavp*p%cc(ix)*p%rho(ix)*rhops1
 !
 !  Deplete gas of small grains.
 !
               call get_nptilde(fp,k,np_tilde)
               if (lpscalar_nolog) then
                 df(ix0,m,n,icc) = df(ix0,m,n,icc) - &
-                    np_tilde*pi*fp(k,iap)**2*deltavp*p%cc(ix0-nghost)
+                    np_tilde*pi*fp(k,iap)**2*deltavp*p%cc(ix)
               else
                 df(ix0,m,n,ilncc) = df(ix0,m,n,ilncc) - &
                     np_tilde*pi*fp(k,iap)**2*deltavp
@@ -222,7 +280,7 @@ module Particles_radius
 !  Time-step contribution of sweep-up.
 !
               if (lfirst.and.ldt) then
-                dt1_sweepup(ix0-nghost) = dt1_sweepup(ix0-nghost) + &
+                dt1_sweepup(ix) = dt1_sweepup(ix) + &
                     np_tilde*pi*fp(k,iap)**2*deltavp
               endif
 !
@@ -243,67 +301,154 @@ module Particles_radius
                 call max_mn_name(dt1_sweepup,idiag_dtsweepp,l_dt=.true.)
           endif
 !
-      endif  ! if (lsweepup_par)
+      endif
 !
-!  Increase in particle radius due to condensation of monomers from the gas.
+    endsubroutine dap_dt_sweepup_pencil
+!***********************************************************************
+    subroutine dap_dt_condensation_pencil(f,df,fp,dfp,p,ineargrid)
 !
-      if (lcondensation_par .and. t>=tstart_condensation_par) then
+!  Change in particle radius due to condensation of monomers from the gas and
+!  evaporation of solid/liquid droplets.
 !
-        if (lfirst.and.ldt) dt1_sweepup=0.0
+!  15-jan-10/anders: coded
+!
+      use Diagnostics
+      use Particles_number
+!
+      real, dimension (mx,my,mz,mfarray) :: f
+      real, dimension (mx,my,mz,mvar) :: df
+      real, dimension (mpar_loc,mpvar) :: fp, dfp
+      type (pencil_case) :: p
+      integer, dimension (mpar_loc,3) :: ineargrid
+!
+      real, dimension (nx) :: ap_equi, vth, dt1_condensation, rhovap
+      real, dimension (nx) :: total_surface_area, ppsat, supsatratio1
+      real, dimension (nx) :: rhocond_tot, rhosat, np_total
+      real :: dapdt, drhocdt, np_tilde, alpha_cond_par, fdamp_evap
+      integer :: k, ix, ix0
+!
+      intent (in) :: f, fp
+      intent (out) :: dfp
+!
+!  Change in particle radius due to condensation and evaporation.
+!
+      if (t>=tstart_condensation_par) then
 !
         if (npar_imn(imn)/=0) then
-          cs=sqrt(p%cs2)   ! Should actually be thermal speed of vapor
+          rhovap=p%cc*p%rho
+          vth=sqrt(p%cs2)   ! Should actually be thermal speed of vapor
+          ppsat=6.035e11*exp(-5938*p%TT1)  ! Valid for water
+          supsatratio1=ppsat/p%ppvap
+          rhosat=supsatratio1*rhovap
+          rhocond_tot=p%rhop+rhovap
+          if (lfirst.and.ldt) then
+            np_total=0.0
+            total_surface_area=0.0
+            dt1_condensation=0.0
+          endif
           do k=k1_imn(imn),k2_imn(imn)
             ix0=ineargrid(k,1)
+            ix=ix0-nghost
             if (.not. lpscalar) then
               call fatal_error('dap_dt', &
                   'must have passive scalar module for condensation')
             endif
-            ppsat=6.035e12*exp(-5938*p%TT1(ix0-nghost))  ! Valid for water
-            supsatratio1=ppsat/p%ppvap(ix0-nghost)
+!
+!  The condensation/evaporation mass flux is
+!
+!    F = vth*rhovap*alpha
+!
+!  where vth is the thermal speed, rhopvap is the mass density of vapor,
+!  and alpha \in [0,1] is the condensation coefficient. Various authors argue
+!  for different choices of alpha. Following Barkstrom 1978 we take
+!
+!    alpha = 1/(vth*ap/D + 1/alpha0)
+!
+!  where D is the diffusion coefficient of vapor and ap the particle radius.
+!  Small particles, with ap<D/(alpha0*vth), have alpha=alpha0, while the
+!  condenation coefficient decreases linearly with particle size for larger
+!  particles. Barkstrom 1978 use alpha0=0.04.
+!
+            select case (condensation_coefficient_type)
+              case ('constant')
+                alpha_cond_par=alpha_cond
+              case ('size-dependent')
+                alpha_cond_par=1/(vth(ix)*fp(k,iap)*diffusion_coefficient1+ &
+                    alpha_cond1)
+              case default
+                if (lroot) print*, 'dap_dt_condensation_pencil: '// &
+                    'invalid condensation coefficient type'
+                call fatal_error('dap_dt_condensation_pencil','')
+            endselect
 !
 !  Radius increase by condensation or decrease by evaporation.
 !
-            dapdt=cs(ix0-nghost)*p%cc(ix0-nghost)*p%rho(ix0-nghost)/rhops* &
-                (1.0-supsatratio1)
-            if (dapdt<0.0.and.apcrit/=0.0) dapdt=dapdt/(1+apcrit/fp(k,iap))
+            dapdt=0.25*vth(ix)*rhovap(ix)*rhops1*(1.0-supsatratio1(ix))* &
+                alpha_cond_par
+!
+!  Do not allow particles to become smaller than a minimum radius.
+!
+            if (rhocond_tot(ix)<rhosat(ix)) then
+              fdamp_evap=0.5*(tanh((fp(k,iap)-2*apmin)/(0.1*apmin))+1.0)
+              dapdt=fdamp_evap*dapdt+(1.0-fdamp_evap)*tau_damp_evap1* &
+                  (fp(k,iap)-apmin)
+              if (lfirst .and. ldt) then
+                dt1_condensation(ix)=max(dt1_condensation(ix), &
+                    0.25*vth(ix)*(rhosat(ix)-rhocond_tot(ix))* &
+                    rhops1*(1/fp(k,iap))*alpha_cond*fdamp_evap+ &
+                    (1.0-fdamp_evap)*tau_damp_evap1)
+              endif
+            endif
+!
             dfp(k,iap)=dfp(k,iap)+dapdt
 !
 !  Deplete gas of small grains.
 !
             call get_nptilde(fp,k,np_tilde)
+            if (lfirst.and.ldt) np_total(ix)=np_total(ix)+np_tilde
+            drhocdt=-dapdt*(4/3.0)*pi*fp(k,iap)**2*rhops*np_tilde
             if (lpscalar_nolog) then
-              df(ix0,m,n,icc) = df(ix0,m,n,icc) - dapdt* &
-                  (4/3.0)*pi*fp(k,iap)**2*rhops*p%rho1(ix0-nghost)*np_tilde
+              df(ix0,m,n,icc)  =df(ix0,m,n,icc)   + p%rho1(ix)*drhocdt
             else
-              df(ix0,m,n,ilncc) = df(ix0,m,n,ilncc) - dapdt* &
-                  (4/3.0)*pi*fp(k,iap)**2*rhops*p%rho1(ix0-nghost)*np_tilde* &
-                  p%cc1(ix0-nghost)
+              df(ix0,m,n,ilncc)=df(ix0,m,n,ilncc) + p%rho1(ix)*drhocdt*p%cc1(ix)
             endif
 !
-!  Time-step contribution of condensation.
+!  Release latent heat to gas / remove heat from gas.
 !
-            if (lfirst.and.ldt) then
-              dt1_condensation(ix0-nghost) = dt1_condensation(ix0-nghost) + &
-                  np_tilde*pi*fp(k,iap)**2*deltavp
-            endif
+!            if (ltemperature) then
+!              df(ix0,m,n,ilnTT)=df(ix0,m,n,ilnTT) - &
+!                  latent_heat_SI*p%rho1(ix)*p%TT1(ix)*p%cv1(ix)*drhocdt
+!            endif
+            if (lfirst.and.ldt) total_surface_area(ix)=total_surface_area(ix)+ &
+                4*pi*fp(k,iap)**2*np_tilde*alpha_cond_par
           enddo
-        endif
 !
 !  Time-step contribution of condensation.
 !
           if (lfirst.and.ldt) then
-            dt1_condensation=dt1_condensation/cdtpc
-            dt1_max=max(dt1_max,dt1_condensation)
+            ap_equi=((p%rhop+(1.0-supsatratio1)*rhovap)/ &
+                (4.0/3.0*pi*rhops*np_tilde*p%np))**(1.0/3.0)
+            do ix=1,nx
+              if (rhocond_tot(ix)>rhosat(ix)) then
+                dt1_condensation(ix) = max(total_surface_area(ix)*vth(ix), &
+                    pi*vth(ix)*np_total(ix)*ap_equi(ix)**2*alpha_cond)
+              endif
+            enddo
           endif
+        endif
 !
-      endif  ! if (lcondensation_par)
+!  Time-step contribution of condensation.
 !
-      lfirstcall=.false.
+        if (lfirst.and.ldt) then
+          dt1_condensation=dt1_condensation/cdtpc
+          dt1_max=max(dt1_max,dt1_condensation)
+        endif
+!
+      endif
 !
       call keep_compiler_quiet(f)
 !
-    endsubroutine dap_dt_pencil
+    endsubroutine dap_dt_condensation_pencil
 !***********************************************************************
     subroutine dap_dt(f,df,fp,dfp,ineargrid)
 !
