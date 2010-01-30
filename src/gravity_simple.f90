3af998d63b244c898ae3b87b383f45b16f3f49fc
Anders Johansen <anders@astro.lu.se>
2010-01-30 11:28:54 +0000

    Tidied up variable list and name list.

--- src/gravity_simple.f90
+++ src/gravity_simple.f90
@@ -39,26 +39,26 @@ module Gravity
     module procedure acceleration_point
   endinterface
 !
+  double precision, parameter :: g_B_cgs=6.172d20, g_D_cgs=3.086d21
+  double precision :: g_B, g_D
   real, dimension(mx) :: gravx_xpencil=0.0,potx_xpencil=0.0
   real, dimension(my) :: gravy_ypencil=0.0,poty_ypencil=0.0
   real, dimension(mz) :: gravz_zpencil=0.0,potz_zpencil=0.0
   real, dimension(mx) :: xdep=0.0
   real, dimension(mz) :: zdep=0.0
-  real :: gravx=0.0,gravy=0.0,gravz=0.0
-  real :: kx_gg=1.0,ky_gg=1.0,kz_gg=1.0,gravz_const=1.0,reduced_top=1.0
-  real :: xgrav=impossible,ygrav=impossible,zgrav=impossible
-  real :: xinfty=0.0,yinfty=0.0,zinfty=0.0
-  real :: dgravx=0.0,pot_ratio=1.0
-  real :: z1=0.0,z2=1.0,zref=0.0,qgshear=1.5
-  real :: nu_epicycle=1.0,nu_epicycle2=1.0
-  real :: nux_epicycle=0.0,nux_epicycle2=0.0
-  real :: r0_pot=0.0    ! peak radius for smoothed potential
-  real :: g_A, g_C
   real, parameter :: g_A_cgs=4.4e-9, g_C_cgs=1.7e-9
-  double precision :: g_B, g_D
-  double precision, parameter :: g_B_cgs=6.172D20, g_D_cgs=3.086D21
+  real :: gravx=0.0, gravy=0.0, gravz=0.0
+  real :: kx_gg=1.0, ky_gg=1.0, kz_gg=1.0, gravz_const=1.0, reduced_top=1.0
+  real :: xgrav=impossible, ygrav=impossible, zgrav=impossible
+  real :: xinfty=0.0, yinfty=0.0, zinfty=0.0
+  real :: dgravx=0.0, pot_ratio=1.0
+  real :: z1=0.0, z2=1.0, zref=0.0, qgshear=1.5
+  real :: nu_epicycle=1.0, nu_epicycle2=1.0
+  real :: nux_epicycle=0.0, nux_epicycle2=0.0
+  real :: r0_pot=0.0
+  real :: g_A, g_C
   real :: cs0hs=0.0, H0hs=0.0
-  integer :: n_pot=10  ! exponent for smoothed potential
+  integer :: n_pot=10
   character (len=labellen) :: gravx_profile='zero',gravy_profile='zero'
   character (len=labellen) :: gravz_profile='zero'
 !
@@ -67,31 +67,28 @@ module Gravity
   logical :: lnumerical_equilibrium=.false.
   logical :: lxyzdependence=.false.
   logical :: lboussinesq=.false.
-  real :: g0=0.
-  real :: lnrho_bot=0.,lnrho_top=0.,ss_bot=0.,ss_top=0.
-  real :: kappa_x1=0.,kappa_x2=0.,kappa_z1=0.,kappa_z2=0.
-  real :: grav_tilt=0.
+  real :: g0=0.0
+  real :: lnrho_bot=0.0, lnrho_top=0.0, ss_bot=0.0, ss_top=0.0
+  real :: kappa_x1=0.0, kappa_x2=0.0, kappa_z1=0.0, kappa_z2=0.0
+  real :: grav_tilt=0.0
   character (len=labellen) :: grav_profile='const'
 !
   namelist /grav_init_pars/ &
-       gravx_profile,gravy_profile,gravz_profile,gravx,gravy,gravz, &
-       xgrav,ygrav,zgrav,kx_gg,ky_gg,kz_gg,dgravx,pot_ratio, &
-       nux_epicycle,nu_epicycle, &
-       z1,z2,zref,lnrho_bot,lnrho_top,ss_bot,ss_top, &
-       lgravx_gas,lgravx_dust,lgravy_gas,lgravy_dust,lgravz_gas,lgravz_dust, &
-       xinfty,yinfty,zinfty, &
-       lxyzdependence,kappa_x1,kappa_x2,kappa_z1,kappa_z2, &
-       reduced_top,lboussinesq,grav_profile,n_pot, &
-       cs0hs,H0hs,grav_tilt
+      gravx_profile, gravy_profile, gravz_profile, gravx, gravy, gravz, &
+      xgrav, ygrav, zgrav, kx_gg, ky_gg, kz_gg, dgravx, pot_ratio, &
+      nux_epicycle, nu_epicycle, z1, z2, zref, lnrho_bot, lnrho_top, ss_bot, &
+      ss_top, lgravx_gas, lgravx_dust, lgravy_gas, lgravy_dust, lgravz_gas, &
+      lgravz_dust, xinfty, yinfty, zinfty, lxyzdependence, kappa_x1, &
+      kappa_x2, kappa_z1, kappa_z2, reduced_top, lboussinesq, grav_profile, &
+      n_pot, cs0hs, H0hs, grav_tilt
 !
   namelist /grav_run_pars/ &
-       gravx_profile,gravy_profile,gravz_profile,gravx,gravy,gravz, &
-       xgrav,ygrav,zgrav,kx_gg,ky_gg,kz_gg,dgravx,pot_ratio, &
-       nux_epicycle,nu_epicycle, &
-       lgravx_gas,lgravx_dust,lgravy_gas,lgravy_dust,lgravz_gas,lgravz_dust, &
-       xinfty,yinfty,zinfty, &
-       lxyzdependence,kappa_x1,kappa_x2,kappa_z1,kappa_z2, &
-       zref,reduced_top,lboussinesq,grav_profile,n_pot,grav_tilt
+      gravx_profile, gravy_profile, gravz_profile, gravx, gravy, gravz, &
+      xgrav, ygrav, zgrav, kx_gg, ky_gg, kz_gg, dgravx, pot_ratio, &
+      nux_epicycle, nu_epicycle, lgravx_gas, lgravx_dust, lgravy_gas, &
+      lgravy_dust, lgravz_gas, lgravz_dust, xinfty, yinfty, zinfty, &
+      lxyzdependence, kappa_x1, kappa_x2, kappa_z1, kappa_z2, zref, &
+      reduced_top, lboussinesq, grav_profile, n_pot, grav_tilt
 !
   integer :: idiag_epot=0
 !
@@ -109,7 +106,7 @@ module Gravity
 !  Identify version number (generated automatically by SVN)
 !
       if (lroot) call svn_id( &
-           "$Id$")
+          '$Id$')
 !
 !  Set lgrav.
 !
@@ -656,8 +653,8 @@ module Gravity
 !
 !  Calculate acceleration from master pencils defined in initialize_gravity
 !
-      if (size(gg,2) /= 3) then
-        call fatal_error("acceleration_penc","Expecting a 3-vector pencil.")
+      if (size(gg,2)/=3) then
+        call fatal_error('acceleration_penc','Expecting a 3-vector pencil')
       endif
 !
 !  Note: the following would not yet work if lxyzdependence is set to true.
@@ -670,7 +667,7 @@ module Gravity
         gg(:,1) = gravx_xpencil
         !ABlater: gg(:,1) = gravx_xpencil*zdep
       case default
-        call fatal_error("acceleration_penc","Wrong pencil size.")
+        call fatal_error('acceleration_penc','Wrong pencil size.')
       endselect
 !
       gg(:,2) = gravy_ypencil(m)
@@ -690,7 +687,7 @@ module Gravity
 !
 !  Calculate acceleration from master pencils defined in initialize_gravity
 !
-      call fatal_error("acceleration_penc_1D","Not implemented")
+      call fatal_error('acceleration_penc_1D','Not implemented')
 !
       call keep_compiler_quiet(gr)
 !
