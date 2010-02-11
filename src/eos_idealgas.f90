ae1611c8ab1278f882bb8243c8d02ec407826cf4
Raphaël Plasson <rplasson@gmail.com>
2010-02-11 15:41:55 +0000

    Added ! PENCILS PROVIDED TT_2; TT_3; TT_4 for being able to compile eos_idealgas...

--- src/eos_idealgas.f90
+++ src/eos_idealgas.f90
@@ -11,6 +11,7 @@
 !
 ! PENCILS PROVIDED ss; gss(3); ee; pp; lnTT; cs2; cp; cp1; cp1tilde
 ! PENCILS PROVIDED glnTT(3); TT; TT1; gTT(3); yH; hss(3,3); hlnTT(3,3)
+! PENCILS PROVIDED TT_2; TT_3; TT_4
 ! PENCILS PROVIDED del2ss; del6ss; del2lnTT; cv1; del6lnTT; gamma
 ! PENCILS PROVIDED del2TT; del6TT; glnmumol(3); ppvap; csvap2
 !
