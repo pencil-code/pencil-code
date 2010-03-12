c5fdb7fe27e3cf26d847b5d526127451f5936a65
Boris Dintrans <boris.dintrans@irap.omp.eu>
2010-03-12 23:20:16 +0000

    rewrote this python subroutine in a more general way

--- python/pencil/files/power.py
+++ python/pencil/files/power.py
@@ -1,26 +1,36 @@
 # $Id$
 
 import numpy as N
+from pencil.files.dim import read_dim
 
 def read_power(file):
-
+    """ 
+    29-apr-2009/dintrans: coded
+    t,dat=read_power(name_power_file)
+    Read a power spectra file like 'data/poweru.dat'
+    """ 
     infile = open(file, 'r')
     lines = infile.readlines()
     infile.close()
-
+#
+#  find the number of blocks (t,power) that should be read
+#
+    dim=read_dim()
+    nblock=len(lines)/(dim.nxgrid/2/8+1)
+#
     infile = open(file, 'r')
     t=N.zeros(1, dtype='Float32')
     data=N.zeros(1, dtype='Float32')
-    for i in range(0, len(lines), 2):
-      st=infile.readline()
-      t=N.append(t, float(st))
-      st=infile.readline()
-      data=N.append(data, N.asarray(st.split()).astype('f'))
+    for i in range(nblock):
+        st=infile.readline()
+        t=N.append(t, float(st))
+        for ii in range(dim.nxgrid/2/8):
+            st=infile.readline()
+            data=N.append(data, N.asarray(st.split()).astype('f'))
     infile.close()
 
     t=t[1:] ; data=data[1:]
-    nt=len(t)
-    nk=len(data)/nt
+    nt=len(t) ; nk=len(data)/nt
     data=data.reshape(nt, nk)
     return t, data
 
