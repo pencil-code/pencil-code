419e2445d629be661a2613ffdf61e4ed9501c103
Natalia Babkovskaia <nbabkovskaia@gmail.com>
2010-05-11 08:35:51 +0000

    changed the initial T profile on the blob case

--- src/chemistry.f90
+++ src/chemistry.f90
@@ -1008,12 +1008,15 @@ module Chemistry
 !
        Rad=(Rad)**0.5
 !
-         if (Rad<0.2) then
+       !  if (Rad<0.2) then
 !          f(j1,j2,j3,ilnTT)=log(init_TT1+(init_TT2-init_TT1)*((0.06-Rad)/0.06)**2)
-           f(j1,j2,j3,ilnTT)=log(init_TT1)+log(3.5)*((0.2-Rad)/0.2)**2
-         else
-          f(j1,j2,j3,ilnTT)=log(init_TT1)
-         endif
+          ! f(j1,j2,j3,ilnTT)=log(init_TT1)+log(3.5)*((0.2-Rad)/0.2)**2
+           f(j1,j2,j3,ilnTT)=log((init_TT2-init_TT1)*exp(-(Rad/0.04)**2)+init_TT1)    
+       !  else
+       !   f(j1,j2,j3,ilnTT)=log(init_TT1)
+       !  endif
+
+         ! f(j1,j2,j3,ilnTT)=log((init_TT2-init_TT1)*exp(-((0.2-Rad)/0.2)**2)+init_TT1) 
           mu1(j1,j2,j3)=f(j1,j2,j3,i_H2)/(2.*mH2)+f(j1,j2,j3,i_O2)/(2.*mO2) &
               +f(j1,j2,j3,i_H2O)/(2.*mH2+mO2)+f(j1,j2,j3,i_N2)/(2.*mN2)
 !
