414d92721141cc2f2ce39d00620026c0bb697c76
Sven Bingert <sven.bingert@gwdg.de>
2010-05-28 06:57:53 +0000

    fixed last nights autotest by putting lffree into nodensity. I don't know what it is for exactly but one needed to put this variable into the list of shared variables. Right know lffree=FALSE for the nodensity module

--- src/nodensity.f90
+++ src/nodensity.f90
@@ -25,6 +25,7 @@ module Density
   implicit none
 !
   logical :: lcalc_glnrhomean=.false.,lupw_lnrho=.false.
+  logical :: lffree =.false.
   real, dimension (nz,3) :: glnrhomz
 
   include 'density.h'
@@ -46,6 +47,7 @@ module Density
 !  24-nov-02/tony: coded
 !
       use EquationOfState, only: select_eos_variable
+      use SharedVariables, only: put_shared_variable
 !
       real, dimension (mx,my,mz,mfarray) :: f
       logical :: lstarting
@@ -54,6 +56,8 @@ module Density
 !  variable => isochoric (constant density).
 !
       call select_eos_variable('lnrho',-1)
+!
+      call put_shared_variable('lffree',lffree)
 !
       call keep_compiler_quiet(f)
       call keep_compiler_quiet(lstarting)
