5e0650a3c242f2b440a716af7850b32098d1c055
Wladimir Lyra <wladimir.lyra@gmail.com>
2010-06-15 23:03:29 +0000

    Make gaussian-noise-rprof compatible with cylindrical and spherical coordinates.

--- src/initcond.f90
+++ src/initcond.f90
@@ -3372,13 +3372,13 @@ module Initcond
 !
 !  18-apr-04/wolf: coded
 !
-      use Sub, only: cubic_step
+      use Sub, only: cubic_step, get_radial_distance
 !
       real :: ampl
       real, dimension (mx,my,mz,mfarray) :: f
       integer :: i1,i2
 !
-      real, dimension (mx) :: prof, rr, r, p, tmp
+      real, dimension (mx) :: prof, rr, r, p, tmp, rr_cyl, rr_sph
       real :: dr
       integer :: i
 !
@@ -3387,10 +3387,12 @@ module Initcond
 !
 !  set up profile
 !
+      dr = r_ext-max(0.,r_int)
+!
       do n=1,mz; do m=1,my
-        rr=sqrt(x(:)**2+y(m)**2+z(n)**2)
-        dr = r_ext-max(0.,r_int)
-        prof = 1 - cubic_step(rr,r_ext,0.25*dr,SHIFT=-1.)
+        call get_radial_distance(rr_cyl,rr_sph)
+        if (lcylindrical_coords.or.lcylinder_in_a_box) rr=rr_cyl
+        if (lspherical_coords  .or.lsphere_in_a_box)   rr=rr_sph
         prof = 1 - cubic_step(rr,r_ext,0.25*dr,SHIFT=-1.)
         if (r_int>0.) then
           prof = prof*cubic_step(rr,r_int,0.25*dr,SHIFT=1.)
