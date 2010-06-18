c624d2d0ac70ef82ee0a06344477f29cfdf67e2a
Natalia Babkovskaia <nbabkovskaia@gmail.com>
2010-06-18 09:23:48 +0000

    commented the dust part in the lcloud case

--- src/chemistry.f90
+++ src/chemistry.f90
@@ -2276,14 +2276,17 @@ module Chemistry
 !  Atmosphere case
 !
       if (lcloud) then
-        df(l1:l2,m,n,ichemspec(index_H2O))=df(l1:l2,m,n,ichemspec(index_H2O)) &
-              - p%ccondens
-        df(l1:l2,m,n,ilnTT) = df(l1:l2,m,n,ilnTT) &
-              + 2.5e10/1.005e7*p%ccondens*p%TT1
+!
+! commented for the testing. Sould be used later
+!
+!          df(l1:l2,m,n,ichemspec(index_H2O))=df(l1:l2,m,n,ichemspec(index_H2O)) &
+!              - p%ccondens
+!          df(l1:l2,m,n,ilnTT) = df(l1:l2,m,n,ilnTT) &
+!              + 2.5e10/1.005e7*p%ccondens*p%TT1
 
         do i=1,mx
-            if ((f(i,m,n,ind(index_H2O))+df(i,m,n,ind(index_H2O))*dt)>=1. ) &
-              df(i,m,n,ind(index_H2O))=0.
+            if ((f(i,m,n,ichemspec(index_H2O))+df(i,m,n,ichemspec(index_H2O))*dt)>=1. ) &
+              df(i,m,n,ichemspec(index_H2O))=0.
         enddo
       endif
 !
