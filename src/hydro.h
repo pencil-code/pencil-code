1a83f44e262b1c269c0d25e3218cddbacbb280cc
Anders Johansen <anders@astro.lu.se>
2010-07-08 20:17:31 +0000

    Added some missing space separation.
    I do not see any reason why uumz, guumz, lcalc_uumean, uumxy, lcalc_uumeanxy
    should be public. Why not use shared variables instead?

--- src/hydro.h
+++ src/hydro.h
@@ -11,9 +11,9 @@
   public :: time_integrals_hydro
   public :: pencil_criteria_hydro, pencil_interdep_hydro
   public :: calc_mflow, remove_mean_momenta, impose_velocity_ceiling
-  public :: uumz,guumz,lcalc_uumean,lupw_uu
-  public :: uumxy,lcalc_uumeanxy
+  public :: uumz, guumz, lcalc_uumean, lupw_uu
+  public :: uumxy, lcalc_uumeanxy
   public :: lforcing_cont_uu, ampl_fcont_uu
   public :: hydro_clean_up
   public :: traceless_strain, coriolis_cartesian
-  public:: kinematic_random_phase
+  public :: kinematic_random_phase
