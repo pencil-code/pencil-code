84eb9c935f5a5b261c12bae37a363113764ed5ba
Gustavo Guerrero <tavo.buk@gmail.com>
2010-08-06 08:04:04 +0000

    Correction to some previous changes.

--- src/entropy.f90
+++ src/entropy.f90
@@ -669,7 +669,7 @@ module Entropy
       if (lhcond_global) then
         call farray_register_auxiliary("hcond",iglobal_hcond)
         call farray_register_auxiliary("glhc",iglobal_glhc,vector=3)
-        if (coord_system=='spherical')then
+        if (coord_system=='spherical' .or. lconvection_gravx)then
           do q=n1,n2
             do m=m1,m2
               call read_hcond(hcond,glhc)
@@ -682,33 +682,21 @@ module Entropy
           hcondxbot=hcond(1)
           hcondxtop=hcond(nx)
         else
-          if (lconvection_gravx) then
-            do q=n1,n2; do m=m1,m2
-              call read_hcond(hcond,glhc)
-              f(l1:l2,m,q,iglobal_hcond)=hcond
-              f(l1:l2,m,q,iglobal_glhc:iglobal_glhc+2)=glhc
-            enddo; enddo
-            FbotKbot=Fbot/hcond(1)
-            FtopKtop=Ftop/hcond(nx)
-            hcondxbot=hcond(1)
-            hcondxtop=hcond(nx)
-          else
-            do n=n1,n2; do m=m1,m2
-              if (lgravz) then
-                p%z_mn=spread(z(n),1,nx)
-              else
+            do n=n1,n2
+              do m=m1,m2
+               if (lgravz) then
+                 p%z_mn=spread(z(n),1,nx)
+               else
                 p%r_mn=sqrt(x(l1:l2)**2+y(m)**2+z(n)**2)
               endif
               call heatcond(hcond,p)
               call gradloghcond(glhc,p)
               f(l1:l2,m,n,iglobal_hcond)=hcond
               f(l1:l2,m,n,iglobal_glhc:iglobal_glhc+2)=glhc
-            enddo; enddo
-            FbotKbot=Fbot/hcond(1)
-            FtopKtop=Ftop/hcond(nx)
-            hcondxbot=hcond(1)
-            hcondxtop=hcond(nx)
-          endif
+            enddo
+          enddo
+          hcondxbot=hcond(1)
+          hcondxtop=hcond(nx)
         endif
       endif
 !
