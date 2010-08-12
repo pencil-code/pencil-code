13cc6411088833067fe311af3eabed383e7e1479
Frederick Gent <fred.gent.ncl@gmail.com>
2010-08-12 14:23:07 +0000

    density.f90: Added Gressel-hs reference to init_lnrho for consistency. As per Galactic-hs density and entropy are initialized in entropy.f90

--- src/density.f90
+++ src/density.f90
@@ -852,6 +852,9 @@ module Density
         case ('Galactic-hs')
           call information('init_lnrho', &
               'Galactic hydrostatic equilibrium setup done in entropy')
+        case ('Gressel-hs')
+          call information('init_lnrho', &
+              'Gressel hydrostatic equilibrium setup done in interstellar')
         case ('geo-kws')
 !
 !  Radial hydrostatic profile in shell region only.
