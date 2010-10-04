40ea78ff559cab5dc2529c9fc1b55188b029ffca
Sven Bingert <sven.bingert@gwdg.de>
2010-10-04 14:26:03 +0000

    fixed setting of lnrho0 by the initialize in the solar_corona module

--- src/special/solar_corona.f90
+++ src/special/solar_corona.f90
@@ -410,11 +410,13 @@ module Special
       endif
 !
       ! set lnrho0 to the lower boundary value
-      if ((lnrho0 /= 0.0) .and. (lnrho0 /= lnrho_init_z(n1))) then
-        if (lroot) print *,'setup_profiles: WARNING: lnrho0 set to ', lnrho0
-        call warning ("setup_profiles", "overriding lnrho0 setting")
+      if (lread_lnrho) then
+        if ((lnrho0 /= 0.0) .and. (lnrho0 /= lnrho_init_z(n1))) then
+          if (lroot) print *,'setup_profiles: WARNING: lnrho0 set to ', lnrho0, lnrho_init_z(n1)
+          call warning ("setup_profiles", "overriding lnrho0 setting")
+          lnrho0 = lnrho_init_z(n1)
+        endif
       endif
-      lnrho0 = lnrho_init_z(n1)
 !
     endsubroutine setup_profiles
 !***********************************************************************
