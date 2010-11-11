12de518b2ea57e4cfee378a30a5e90b9f560569e
Sven Bingert <sven.bingert@gwdg.de>
2010-11-11 09:17:41 +0000

    Axel, fixed your problem with power_spectra
    -  if (lwrite_krms) then
    +!  if (lwrite_krms) then
         call mpireduce_sum(k2m,k2m_sum,nk)
         call mpireduce_sum(nks,nks_sum,nk)
    -  endif
    +!  endif
    
    For root-proc this variable was set false after writing but the other procs still had lwrite_krms=true
    
    Another idea would be to distribute lwrite_krms every call of that subroutine.
    
    But this depends on  the performance. Is it faster to call mpireduce even it is not needed or to send lwrite_krms to each proc.
    
    Sven

--- src/power_spectrum.f90
+++ src/power_spectrum.f90
@@ -501,10 +501,10 @@ module  power_spectrum
 !
 !  compute krms only once
 !
-  if (lwrite_krms) then
+!  if (lwrite_krms) then
     call mpireduce_sum(k2m,k2m_sum,nk)
     call mpireduce_sum(nks,nks_sum,nk)
-  endif
+!  endif
   !
   !  on root processor, write global result to file
   !  multiply by 1/2, so \int E(k) dk = (1/2) <u^2>
