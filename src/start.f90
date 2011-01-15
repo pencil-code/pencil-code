73d2c36308a9cd7470441d890d95dad7ed13f00e
Anders Johansen <anders@astro.lu.se>
2011-01-15 14:26:52 +0000

    Corrected some typos. Sven, please check.

--- src/start.f90
+++ src/start.f90
@@ -103,19 +103,20 @@ program start
 !
   call mpicomm_init
 !
-!  Check if parallelization and choosen grid numbers make sense.
+!  Check if parallelization and chosen grid numbers make sense.
+!
   if ((nprocx>1.and.nxgrid==1).or. &
       (nprocy>1.and.nygrid==1).or. &
       (nprocz>1.and.nzgrid==1)) then
     call fatal_error('start', &
-        'parallelization in a dimension with ngrid>1 does not work')
+        'parallelization in a dimension with ngrid==1 does not work')
   endif
   if (mod(nxgrid,nprocx)/=0.or. &
       mod(nygrid,nprocy)/=0.or. &
       mod(nzgrid,nprocz)/=0) then
     call fatal_error('start', &
         'in each dimension the number of grid points has to be '// &
-        'devidable by the number of processors.')
+        'dividable by the number of processors')
   endif
 !
 !  Identify version.
