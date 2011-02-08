09f445d68cc8457c1b5887b574b347e2935bab0b
Axel Brandenburg <brandenb@nordita.org>
2011-02-08 22:16:22 +0000

    added divu2mz diagnostics

--- src/hydro.f90
+++ src/hydro.f90
@@ -266,6 +266,7 @@ module Hydro
   integer :: idiag_uzmy=0       ! DIAG_DOC: $\left< u_z \right>_{y}$
   integer :: idiag_u2mz=0       ! DIAG_DOC: $\left< \Uv^2 \right>_{xy}$
   integer :: idiag_o2mz=0       ! DIAG_DOC: $\left< \Wv^2 \right>_{xy}$
+  integer :: idiag_divu2mz=0    ! DIAG_DOC: $\left<(\nabla\cdot\uv)^2\right>_{xy}$
   integer :: idiag_omumz=0      ! DIAG_DOC: $\left<\left<\Wv\right>_{xy}
                                 ! DIAG_DOC:   \cdot\left<\Uv\right>_{xy}
                                 ! DIAG_DOC:   \right>$ \quad($xy$-averaged
@@ -1381,6 +1382,7 @@ module Hydro
           lpenc_diagnos(i_oo)=.true.
       if (idiag_orms/=0 .or. idiag_omax/=0 .or. idiag_o2m/=0 .or. &
           idiag_ormsh/=0 .or. idiag_o2mz/=0 )  lpenc_diagnos(i_o2)=.true.
+      if (idiag_divu2m) lpenc_diagnos(i_divu)=.true.
       if (idiag_oum/=0 .or. idiag_oumx/=0.or.idiag_oumy/=0.or.idiag_oumz/=0 .or. &
            idiag_oumh/=0) lpenc_diagnos(i_ou)=.true.
       if (idiag_Marms/=0 .or. idiag_Mamax/=0) lpenc_diagnos(i_Ma2)=.true.
@@ -1964,6 +1966,7 @@ module Hydro
 !
         if (idiag_u2mz/=0)  call xysum_mn_name_z(p%u2,idiag_u2mz)
         if (idiag_o2mz/=0)  call xysum_mn_name_z(p%o2,idiag_o2mz)
+        if (idiag_divu2mz/=0)  call xysum_mn_name_z(p%divu**2,idiag_divu2mz)
 !
 !  Mean momenta.
 !
@@ -3763,6 +3766,7 @@ module Hydro
             'ekinmz',idiag_ekinmz)
         call parse_name(inamez,cnamez(inamez),cformz(inamez),'u2mz',idiag_u2mz)
         call parse_name(inamez,cnamez(inamez),cformz(inamez),'o2mz',idiag_o2mz)
+        call parse_name(inamez,cnamez(inamez),cformz(inamez),'divu2mz',idiag_divu2mz)
         call parse_name(inamez,cnamez(inamez),cformz(inamez),'oumz',idiag_oumz)
         call parse_name(inamez,cnamez(inamez),cformz(inamez), &
             'uguxmz',idiag_uguxmz)
