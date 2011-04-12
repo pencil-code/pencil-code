bff3ef059c53ec399c3697d91fb8dcc218305e53
Simon Candelaresi <iomsn1@gmail.com>
2011-04-12 11:42:17 +0000

    Extensive modifications and some additions. It is now possible to plot 2d vector fields upon the colormap. It is now possible to use all kwargs for imshow. Changed the gui, which should not give any warnings anymore.

--- python/pencil/files/animate_interactive.py
+++ python/pencil/files/animate_interactive.py
@@ -1,8 +1,8 @@
-# $Id: animate_interactive.py,v 1.5 2010-07-02 18:54:58 iomsn Exp $
+# $Id: animate_interactive.py iomsn Exp $
 #
-# read 3D array and assemble an animation
+# Assemble a 2D animation from a 3D array.
 #
-# Author: Simon Candelaresi (iomsn@physto.se). 
+# Author: Simon Candelaresi (iomsn@physto.se, iomsn1@googlemail.com).
 # 
 #
 
@@ -10,47 +10,114 @@ import numpy as np
 import pylab as plt
 import time
 import os, sys # for making the movie
-#from matplotlib.widgets import Slider, Button # for GUI
 from matplotlib.backends.backend_tkagg import *
 import mtTkinter as tk # for thread save GUI (from http://tkinter.unpythonic.net/wiki/mtTkinter)
 import sys
 import thread # for GUI
 
 def animate_interactive(data, t = [], dimOrder = (0,1,2),
-                        fps = 10.0, title = '', xlabel = '', ylabel = '',
-                        cBar = 0, interpol = 'nearest', colorTable = 'hot',
-                        aspectRatio = 'auto',
-                        movieFile = '', keepImages = False):
+                        fps = 10.0, title = '', xlabel = 'x', ylabel = 'y',
+                        fontsize = 24,
+                        cBar = 0,
+                        #interpol = 'nearest', colorTable = 'hot', aspectRatio = 'auto',
+                        arrowsX = np.array(0), arrowsY = np.array(0), arrowsRes = 10,
+                        arrowsPivot = 'mid', arrowsWidth = 0.002, arrowsScale = 5,
+                        arrowsColor = 'black', plotArrowsGrid = False,
+                        movieFile = '', bitrate = 1800, keepImages = False,
+                        **kwimshow):
     """
-    read 3D array and assemble an animation.
+    Assemble a 2D animation from a 3D array.
 
-    Options:
-
-     data --- array with time dependent slices (3d)
-     t --- time steps corresponding to the slices
-     dimOrder --- ordering of the dimensions in the array (t,x,y)
-     fps --- frames per second of the animation
-     title --- title of the plot
-     xlabel --- label of the x-axis
-     ylabel --- label of the y-axis
-     cBar --- determines how the colorbar should be change
-              (0 - no cahnge; 1 - keep extreme values constant; 2 - change extreme values)
-     interpol --- interpolation of the data points
-     colorTable --- colortable for ht plot
-     aspectRatio --- aspect ration of the plot
-     movieFile --- the movie file where the animation should be saved to,
-                   leave empty for no movie, requires 'mencoder' to be installed
-     keepImages --- if True the images for the movie creation are not deleted
-    """
+    call signature::
     
+      animate_interactive(data, t = [], dimOrder = (0,1,2),
+                        fps = 10.0, title = '', xlabel = 'x', ylabel = 'y',
+                        fontsize = 24,
+                        cBar = 0,
+                        #interpol = 'nearest', colorTable = 'hot', aspectRatio = 'auto',
+                        arrowsX = np.array(0), arrowsY = np.array(0), arrowsRes = 10,
+                        arrowsPivot = 'mid', arrowsWidth = 0.002, arrowsScale = 5,
+                        arrowsColor = 'black', plotArrowsGrid = False,
+                        movieFile = '', keepImages = False,
+                        **kwimshow)
+    
+    Assemble a 2D animation from a 3D array. *data* has to be a 3D array who's
+    time index has the same dimension as *t*. The time index of *data* as well
+    as its x and y indices can be changed via *dimOrder*.
+    
+    Keyword arguments:
+    
+      *dimOrder*: [ (i,j,k) ]
+        Ordering of the dimensions in the data array (t,x,y).
+        
+     *fps*:
+       Frames per second of the animation.
+       
+     *title*:
+       Title of the plot.
+       
+     *xlabel*:
+       Label of the x-axis.
+       
+     *ylabel*:
+       Label of the y-axis.
+       
+     *fontsize*:
+       Font size of the title, x and y label.
+       
+     *cBar*: [ 0 | 1 | 2 ]
+       Determines how the colorbar changes:
+       (0 - no cahnge; 1 - keep extreme values constant; 2 - change extreme values).
+              
+     *arrowsX*:
+       Data containing the x-component of the arrows.
+       
+     *arrowsy*:
+       Data containing the y-component of the arrows.
+       
+     *arrowsRes*:
+       Plot every arrowRes arrow.
+       
+     *arrowsPivot*: [ 'tail' | 'middle' | 'tip' ]
+       The part of the arrow that is at the grid point; the arrow rotates
+       about this point.
+       
+     *arrowsWidth*:
+       Width of the arrows.
+       
+     *arrowsScale*:
+       Scaling of the arrows.
+       
+     *arrowsColor*:
+       Color of the arrows.
+       
+     *plotArrowsGrid*: [ False | True ]
+       If 'True' the grid where the arrows are aligned to is shown.
+     
+     *movieFile*: [ None | string ]
+       The movie file where the animation should be saved to.
+       If 'None' no movie file is written. Requires 'mencoder' to be installed.
+     
+     *bitrate*:
+       Bitrate of the movie file. Set to higher value for higher quality.
+       
+     *keepImages*: [ False | True ]
+       If 'True' the images for the movie creation are not deleted.
+     
+     **kwimshow:
+       Remaining arguments are identical to those of pylab.imshow. Refer to that help.
+    """
+
+
     global tStep, sliderTime, pause
     
     
     # plot the current frame
     def plotFrame():
         global tStep, sliderTime
-    
-        ax.set_title('t={0}'.format(t[tStep]))
+        
+        if movieFile:
+            ax.set_title(title+r'$\quad$'+r'$t={0}$'.format(t[tStep]), fontsize = fontsize)
         
         if (cBar == 0):
             image.set_data(data[tStep,:,:])
@@ -61,10 +128,11 @@ def animate_interactive(data, t = [], dimOrder = (0,1,2),
             image.set_data(data[tStep,:,:])
             colorbar.set_clim(vmin=data[tStep,:,:].min(), vmax=data[tStep,:,:].max())
             colorbar.update_bruteforce(data[tStep,:,:])
+            
+        if plotArrows:
+            arrows.set_UVC(U = arrowsX[tStep,::arrowsRes,::arrowsRes], V = arrowsY[tStep,::arrowsRes,::arrowsRes])
 
         #manager.canvas.draw()
-        canvas.draw()
-        #print 'draw', tStep
                 
     
     # play the movie
@@ -91,63 +159,80 @@ def animate_interactive(data, t = [], dimOrder = (0,1,2),
             
     
     # call the play function as a seperate thread (for GUI)
-    def play_thread():
+    def play_thread(event):
         try:
             thread.start_new_thread(play, ("playThread", ))
         except:
             print "Error: unable to start play thread"
 
 
-    def pausing():               
+    def pausing(event):               
         global pause
         
         pause = True        
 
 
-    def reverse():
+    def reverse(event):
         global tStep, sliderTime
         
         tStep -= 1
         if tStep < 0:
             tStep = 0
-        #plotFrame()
         # plot the frame and update the time slider
         sliderTime.set_val(t[tStep])
 
         
-    def forward():
+    def forward(event):
         global tStep, sliderTime
         
         tStep += 1
         if tStep > len(t)-1:
             tStep = len(t)-1
-        #plotFrame()
         # plot the frame and update the time slider
         sliderTime.set_val(t[tStep])
 
     
     pause = False
+    plotArrows = False
     
-    # of no movie file is specified interacrive mode is switched on
-    interactive = True
-    if movieFile:
-        interactive = False
-        
     # check if the data has the right dimensions
     if (len(data.shape) != 3):
         print 'error: data dimensions are invalid: {0} instead of 3'.format(len(data.shape))
         return -1
-    
+        
     # transpose the data according to dimOrder
     unOrdered = data
     data = np.transpose(unOrdered, dimOrder)
     unOrdered = []
+    
+    # check if arrows should be plotted
+    if len(arrowsX.shape) == 3:
+        # transpose the data according to dimOrder
+        unOrdered = arrowsX
+        arrowsX = np.transpose(unOrdered, dimOrder)
+        unOrdered = []
+        if len(arrowsY.shape) == 3:
+            # transpose the data according to dimOrder
+            unOrdered = arrowsY
+            arrowsY = np.transpose(unOrdered, dimOrder)
+            unOrdered = []
+            
+            # check if the dimensions of the arrow arrays match each other
+            if ((len(arrowsX[:,0,0]) != len(arrowsY[:,0,0])) or (len(arrowsX[0,:,0]) != len(arrowsY[0,:,0])) or (len(arrowsX[0,0,:]) != len(arrowsY[0,0,:]))):
+                print 'error: dimensions of arrowX do not match with dimensions of arrowY'
+                return -1
+            else:
+                plotArrows = True
     
     # check if time array has the right length
     nT = len(t)
     if (nT != len(data[:,0,0])):
         print 'error: length of time array doesn\'t match length of data array'
         return -1
+        if plotArrows:
+            if (nT != len(arrowX[:,0,0]) or nT != len(arrowX[:,0,0])):
+                print 'error: length of time array doesn\'t match length of arrows array'
+                return -1
     
     # check if fps is positive
     if (fps < 0.0):
@@ -162,21 +247,89 @@ def animate_interactive(data, t = [], dimOrder = (0,1,2),
     dataMin = np.min(data)
     dataMax = np.max(data)
     
-    # setup the plot and the frame for the GUI
-    rootWindow = tk.Tk()
-    fig = plt.Figure()
-    ax = fig.add_subplot(111)
+    # setup the plot
+    if movieFile:
+        width = 8
+        height = 7
+        plt.rc("figure.subplot", left=(90/72.27)/width)
+        plt.rc("figure.subplot", right=(width-20/72.27)/width)
+        plt.rc("figure.subplot", bottom=(60/72.27)/height)
+        plt.rc("figure.subplot", top=(height-20/72.27)/height)
+        fig = plt.figure(figsize=(width, height))        
+        ax = plt.axes([0.11, 0.1, .90, .85])
+    else:
+        width = 8
+        height = 7
+        plt.rc("figure.subplot", left=(90/72.27)/width)
+        plt.rc("figure.subplot", right=(width-20/72.27)/width)
+        plt.rc("figure.subplot", bottom=(60/72.27)/height)
+        plt.rc("figure.subplot", top=(height-20/72.27)/height)
+        fig = plt.figure(figsize=(width, height))
+        ax = plt.axes([0.1, 0.25, .85, .70])
+        #ax = fig.add_subplot(111)
     
-    # create the time slider
-    if interactive == True:
+    ax.set_title(title, fontsize = fontsize)
+    ax.set_xlabel(xlabel, fontsize = fontsize)
+    ax.set_ylabel(ylabel, fontsize = fontsize)
+    plane = np.zeros((nX,nY))
+
+    # calibrate the displayed colors for the data range
+    image = ax.imshow(plane, vmin=dataMin, vmax=dataMax, **kwimshow)
+    colorbar = fig.colorbar(image)
+    
+    # plot the arrows
+    # TODO: add some more options
+    if plotArrows:
+        # prepare the mash grid where the arrows will be drawn
+        arrowGridX, arrowGridY = np.meshgrid(np.arange(0, len(data[0,:,0]), arrowsRes), np.arange(0, len(data[0,0,:]), arrowsRes))
+        arrows = ax.quiver(arrowGridX, arrowGridY, arrowsX[0,::arrowsRes,::arrowsRes], arrowsY[0,::arrowsRes,::arrowsRes], units = 'width', pivot = arrowsPivot, width = arrowsWidth, scale = arrowsScale, color = arrowsColor)
+        # plot the grid for the arrows
+        if plotArrowsGrid == True:
+            ax.plot(arrowGridX, arrowGridY, 'k.')
+        
+
+    # for real-time image display
+    #manager = plt.get_current_fig_manager()
+    #manager.show()
+
+
+    tStep = 0
+    if movieFile:
+        movieFiles = []
+        # start the animation
+        play('noThread')
+
+        # write the movie file        
+        mencodeCommand = "mencoder 'mf://"+movieFile+"*.png' -mf type=png:fps="+np.str(fps)+" -ovc lavc -lavcopts vcodec=mpeg4:vhq:vbitrate="+np.str(bitrate)+" -ffourcc MP4S -oac copy -o "+movieFile+".mpg"
+        os.system(mencodeCommand)
+        # clean up the image files
+        if (keepImages == False):
+            print 'cleaning up files'
+            for fname in movieFiles:
+                os.remove(fname)
+
+    else:
+        # set up the gui        
+        plt.ion()
+
+        axPlay = plt.axes([0.1, 0.05, 0.15, 0.05], axisbg='lightgoldenrodyellow')
+        buttonPlay = plt.Button(axPlay, 'play', color='lightgoldenrodyellow', hovercolor='0.975')
+        buttonPlay.on_clicked(play_thread)
+        axPause = plt.axes([0.3, 0.05, 0.15, 0.05], axisbg='lightgoldenrodyellow')
+        buttonPause = plt.Button(axPause, 'pause', color='lightgoldenrodyellow', hovercolor='0.975')
+        buttonPause.on_clicked(pausing)
+    
+        axReverse = plt.axes([0.5, 0.05, 0.15, 0.05], axisbg='lightgoldenrodyellow')
+        buttonReverse = plt.Button(axReverse, 'reverse', color='lightgoldenrodyellow', hovercolor='0.975')
+        buttonReverse.on_clicked(reverse)
+        axForward = plt.axes([0.7, 0.05, 0.15, 0.05], axisbg='lightgoldenrodyellow')
+        buttonForward = plt.Button(axForward, 'forward', color='lightgoldenrodyellow', hovercolor='0.975')
+        buttonForward.on_clicked(forward)
+        
+        # create the time slider
         fig.subplots_adjust(bottom=0.2)
-        #fig.axes.append(plt.axes([0.2, 0.1, 0.6, 0.03], axisbg='lightgoldenrodyellow'))
-        sliderTimeAxes = plt.axes([0.2, 0.1, 0.6, 0.03], axisbg='lightgoldenrodyellow')
-        #sliderTimeAxes = fig.add_axes([0.2, 0.1, 0.6, 0.03], axisbg='lightgoldenrodyellow')
-        #sliderTimeAxes = fig.gca()
-        #sliderTimeAxes = fig.axes[-1]
+        sliderTimeAxes = plt.axes([0.2, 0.12, 0.6, 0.03], axisbg='lightgoldenrodyellow')
         sliderTime = plt.Slider(sliderTimeAxes, 'time', t[0], t[-1], valinit = 0.0)
-        #sliderTimeAxes.set_figure(fig)
         def update(val):
             global tStep
             # find the closest time step to the slider time value
@@ -189,56 +342,5 @@ def animate_interactive(data, t = [], dimOrder = (0,1,2),
             plotFrame()
         sliderTime.on_changed(update)
     
-    canvas = FigureCanvasTkAgg(fig, master=rootWindow)
-    tkwidget = canvas.get_tk_widget()
-    tkwidget.pack(side=tk.TOP)
-    
-    ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    plane = np.zeros((nX,nY))
-
-    # calibrate the displayed colors for the data range
-    image = ax.imshow(plane, vmin=dataMin, vmax=dataMax, interpolation=interpol, cmap=colorTable, aspect=aspectRatio)
-    colorbar = fig.colorbar(image)
-
-    ## for real-time image display
-    #manager = plt.get_current_fig_manager()
-    #manager.show()
-    canvas.show()
-
-
-    tStep = 0
-    if movieFile:
-        movieFiles = []
-        # start the animation
-        play('noThread')
-
-        # write the movie file        
-        mencodeCommand = "mencoder 'mf://"+movieFile+"*.png' -mf type=png:fps="+np.str(fps)+" -ovc lavc -lavcopts vcodec=mpeg4:vhq:vbitrate=1800 -ffourcc MP4S -oac copy -o "+movieFile+".mpg"
-        os.system(mencodeCommand)
-        # clean up the image files
-        if (keepImages == False):
-            print 'cleaning up files'
-            for fname in movieFiles:
-                os.remove(fname)
-
-    else:
-        # set up the gui        
-        plt.ion()
-        # new GUI
-        frame = tk.Frame(rootWindow)
-        frame.pack(side=tk.BOTTOM)
-        
-        buttonPlay = tk.Button(frame, text="play", command=play_thread)
-        buttonPlay.pack(side=tk.LEFT)
-        buttonPause = tk.Button(frame, text="pause", command=pausing)
-        buttonPause.pack(side=tk.LEFT)
-        
-        buttonReverse = tk.Button(frame, text="reverse", command=reverse)
-        buttonReverse.pack(side=tk.LEFT)
-        buttonForward = tk.Button(frame, text="forward", command=forward)
-        buttonForward.pack(side=tk.LEFT)
-                
-        rootWindow.mainloop()
+        plt.show()
     print 'done'
