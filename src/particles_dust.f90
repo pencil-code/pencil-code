a41a477c59536f7ff38dc1a21075f9fa7857558e
Wladimir Lyra <wladimir.lyra@gmail.com>
2011-04-21 21:32:23 +0000

    Reinstated drag force reaction in polar coordinates. The mass density of a super-particle is mp_swarm/dvolume, where mp_swarm is the mass of a superparticle. This is not a constant (rhop_swarm) as in Cartesian equidistant grids, because the scale factors not being unity, dvolume=dVol_x*dVol_y*dVol_z depends on position.

--- src/particles_dust.f90
+++ src/particles_dust.f90
@@ -2658,12 +2658,9 @@ k_loop:   do while (.not. (k>npar_loc))
                       df(ixx,iyy,izz,iux:iuz)=df(ixx,iyy,izz,iux:iuz) - &
                           rhop_swarm*rho1_point*dragforce*weight
                     else
-                      call fatal_error("dvvt_dt_pencil", & 
-                           "back reaction not implemented for "//&
-                           "non-cartesian coordinates")
-                      !df(ixx,iyy,izz,iux:iuz)=df(ixx,iyy,izz,iux:iuz) - &
-                      !    rhop_swarm*box_volume/(nxgrid*nygrid*nzgrid)* &
-                      !    dvolume_1(ixx)*rho1_point*dragforce*weight
+                      df(ixx,iyy,izz,iux:iuz)=df(ixx,iyy,izz,iux:iuz) - &
+                          mp_swarm/(dvol_x(ixx)*dvol_y(iyy)*dvol_z(izz)) * &
+                          rho1_point*dragforce*weight
                     endif
                   enddo; enddo; enddo
 !
@@ -2735,12 +2732,9 @@ k_loop:   do while (.not. (k>npar_loc))
                       df(ixx,iyy,izz,iux:iuz)=df(ixx,iyy,izz,iux:iuz) - &
                           rhop_swarm*rho1_point*dragforce*weight
                     else
-                      call fatal_error("dvvt_dt_pencil", & 
-                           "back reaction not implemented for "//&
-                           "non-cartesian coordinates")
-                      !df(ixx,iyy,izz,iux:iuz)=df(ixx,iyy,izz,iux:iuz) - &
-                      !    rhop_swarm*box_volume/(nxgrid*nygrid*nzgrid)* &
-                      !    dvolume_1(ixx)*rho1_point*dragforce*weight
+                      df(ixx,iyy,izz,iux:iuz)=df(ixx,iyy,izz,iux:iuz) - &
+                          mp_swarm/(dvol_x(ixx)*dvol_y(iyy)*dvol_z(izz))* &
+                          rho1_point*dragforce*weight
                     endif
                   enddo; enddo; enddo
                 else
@@ -2752,12 +2746,9 @@ k_loop:   do while (.not. (k>npar_loc))
                     df(l,m,n,iux:iuz) = df(l,m,n,iux:iuz) - &
                         rhop_swarm*p%rho1(l-nghost)*dragforce
                   else
-                    call fatal_error("dvvt_dt_pencil", & 
-                         "back reaction not implemented for "//&
-                         "non-cartesian coordinates")
-                    !df(l,m,n,iux:iuz) = df(l,m,n,iux:iuz) - &
-                    !    rhop_swarm*box_volume/(nxgrid*nygrid*nzgrid)* &
-                    !    dvolume_1(l-nghost)*p%rho1(l-nghost)*dragforce
+                    df(l,m,n,iux:iuz) = df(l,m,n,iux:iuz) - &
+                        mp_swarm/(dvol_x(l)*dvol_y(m)*dvol_z(n))*&
+                        p%rho1(l-nghost)*dragforce
                   endif
                 endif
               endif
