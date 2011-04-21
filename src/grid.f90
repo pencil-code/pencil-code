41560cdb0308213cc4ed1684492846a50f09f0bb
Wladimir Lyra <wladimir.lyra@gmail.com>
2011-04-21 14:56:03 +0000

    lWlad => ldegenerate_direction. "lWlad" for a variable name is indeed rather silly

--- src/grid.f90
+++ src/grid.f90
@@ -22,7 +22,7 @@ module Grid
 !
 !  Special definition for degenerate directions
 !
-  logical :: lWlad=.false.
+  logical :: ldegenerate_direction=.false.
 !
   private
 !
@@ -734,7 +734,7 @@ module Grid
 !  Coordinate-related issues: nonuniform meshes, different coordinate systems
 !
 !  20-jul-10/wlad: moved here from register
-!  21-apr-11/axel: insert special lWlad option for degenerate directions
+!  21-apr-11/axel: insert special ldegenerate_directions option for degenerate directions
 !
       use Sub, only: remove_zprof
       use Mpicomm
@@ -888,7 +888,7 @@ module Grid
           dVol_x=x**2*xprim
         else
 !         dVol_x=1./3.*(xyz1(1)**3-xyz0(1)**3)
-          if (lWlad) then; dVol_x=1.; else; dVol_x=1./3.*(xyz1(1)**3-xyz0(1)**3); endif
+          if (ldegenerate_directions) then; dVol_x=1.; else; dVol_x=1./3.*(xyz1(1)**3-xyz0(1)**3); endif
         endif
 !
 !  Theta extent (if non-radially symmetric)
@@ -897,9 +897,8 @@ module Grid
           box_volume = box_volume*(-(cos(xyz1(2))  -cos(xyz0(2))))
           dVol_y=sinth*yprim
         else
-          box_volume = box_volume*2.
 !         dVol_y=2.
-          if (lWlad) then; dVol_y=1.; else; dVol_y=2.; endif
+          if (ldegenerate_directions) then; dVol_y=1.; else; dVol_y=2.; endif
         endif
 !
 !  phi extent (if non-axisymmetry)
@@ -908,9 +907,8 @@ module Grid
           box_volume = box_volume*Lxyz(3)
           dVol_z=zprim
         else
-          box_volume = box_volume*2.*pi
 !         dVol_z=2.*pi
-          if (lWlad) then; dVol_z=1.; else; dVol_z=2.*pi; endif
+          if (ldegenerate_directions) then; dVol_z=1.; else; dVol_z=2.*pi; endif
         endif
 !
 !  weighted coordinates for integration purposes
@@ -976,7 +974,7 @@ module Grid
           dVol_x=x*xprim
         else
 !         dVol_x=1./2.*(xyz1(1)**2-xyz0(1)**2)
-          if (lWlad) then; dVol_x=1.; else; dVol_x=1./2.*(xyz1(1)**2-xyz0(1)**2); endif
+          if (ldegenerate_directions) then; dVol_x=1.; else; dVol_x=1./2.*(xyz1(1)**2-xyz0(1)**2); endif
         endif
 !
 !  theta extent (non-cylindrically symmetric)
@@ -985,9 +983,8 @@ module Grid
           box_volume = box_volume*Lxyz(2)
           dVol_y=yprim
         else
-          box_volume = box_volume*2.*pi
 !         dVol_y=2.*pi
-          if (lWlad) then; dVol_y=1.; else; dVol_y=2.*pi; endif
+          if (ldegenerate_directions) then; dVol_y=1.; else; dVol_y=2.*pi; endif
         endif
 !
 !  z extent (vertically extended)
