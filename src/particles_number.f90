8d2a2ca12d826864b97099c5b08dcce3f0645407
Wladimir Lyra <wladimir.lyra@gmail.com>
2011-04-28 19:05:43 +0000

    ditto for particles number

--- src/particles_number.f90
+++ src/particles_number.f90
@@ -93,6 +93,10 @@ module Particles_number
       if (lroot) print*, 'initialize_particles_number: '// &
           'number density per particle np_swarm0=', np_swarm0
 !
+      if (.not(lcartesian_coords.and.(all(lequidist)))) call fatal_error( &
+           'initialize_particles_number', 'particles_number only '// &
+           'implemented for Cartesian equidistant grids.')
+!
       call keep_compiler_quiet(f)
       call keep_compiler_quiet(lstarting)
 !
