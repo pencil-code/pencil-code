8d66b78cd20d631434a1ccd479ed1f220e141b4d
Matthias Rheinhardt <mreinhardt@nordita.org>
2011-05-14 00:54:51 +0000

    MR: new variable n_spectra for number of requested spectra

--- src/power_spectrum.h
+++ src/power_spectrum.h
@@ -4,4 +4,4 @@
  public :: power, powerhel, powerscl, power_1d, power_2d, power_xy, pdf
  public :: power_phi,powerhel_phi, power_vec
  public :: read_power_spectrum_runpars,  write_power_spectrum_runpars
-
+ public :: n_spectra
