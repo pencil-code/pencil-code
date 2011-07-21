416070df5f5f81329baeac8e22eceac899b95baf
Boris Dintrans <boris.dintrans@irap.omp.eu>
2011-07-21 16:02:41 +0000

    generalization to files with several fields written in columns as
    z rho ss cs2 temp etc...

--- python/pencil/files/zprof.py
+++ python/pencil/files/zprof.py
@@ -1,46 +1,47 @@
 #$Id$
 
-import numpy as N
-import pencil as pc
+from numpy import zeros, asarray
+from pencil import read_dim
 
 class read_zprof:
-  """ 
+    """ 
     13-mar-2008/dintrans: coded
-    f = read_zprof(varname,datadir='data',dim=dim)
-    Read a vertical profile written in data/proc*/zprof_varname.dat
-  """
-
-  def __init__(self,varname,datadir='data/',dim=None):
-    """Constructor:
-         -----------
-
-         Params:
-         ------
-            varname = we want to read data/proc*/zprof_varname.dat files
-            datadir ='data/' (optionnal)
-            dim     = None (optional)
-         Returns:
-         -------
-            a read_zprof class with z and prof(z)
+    f = read_zprof(fname='zprof_hcond.dat',datadir='data/',dim=None,nfield=1)
+    Read vertical profiles written in data/proc*/fname
     """
 
-    if (dim==None): dim=pc.read_dim()
-    nz=dim.nzgrid/dim.nprocz
-    self.z=N.zeros(dim.nzgrid,'f')
-    self.prof=N.zeros(dim.nzgrid,'f')
+    def __init__(self,fname='zprof_hcond.dat',datadir='data/',dim=None,nfield=1):
+        """Constructor:
+            -----------
+            Params:
+            ------
+            fname   = we want to read data/proc*/fname files
+            datadir = 'data/' (optionnal)
+            dim     = None (optional)
+            nfield  = number of fields to be read (optional)
+            Returns:
+            -------
+            a class with z and profiles(z)
+        """
+
+        if (dim==None): dim=read_dim()
+        nz=dim.nzgrid/dim.nprocz
+        self.z=zeros(dim.nzgrid,'f')
+        self.prof=zeros((nfield,dim.nzgrid),'f')
 #
 #  loop over all processors and records in file
 #
-    izcount=0
-    for iprocz in range(0,dim.nprocz):
-      procname='/proc'+str(iprocz)
-      filename=datadir+procname+'/zprof_'+varname+'.dat'
-      file=open(filename,'r')
-      for i in range(nz):
-        st=file.readline()
-        data=N.asarray(st.split()).astype('f')
-        self.z[izcount]=data[0]
-        self.prof[izcount]=data[1]
-        izcount=izcount+1
-      file.close()
+        izcount=0
+        for iprocz in range(0,dim.nprocz):
+            procname='/proc'+str(iprocz)
+            filename=datadir+procname+'/'+fname
+            file=open(filename,'r')
+            for i in range(nz):
+                st=file.readline()
+                data=asarray(st.split()).astype('f')
+                self.z[izcount]=data[0]
+                for j in range(nfield):
+                    self.prof[j,izcount]=data[j+1]
+                izcount=izcount+1
+        file.close()
 
