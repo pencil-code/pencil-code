b8b85241b84d308be29892817f94817d2b7d956e
Sven Bingert <sven.bingert@gwdg.de>
2011-07-22 20:12:30 +0000

    removed pencils from the PROVIDED list which are not computed in that module

--- src/eos_idealgas.f90
+++ src/eos_idealgas.f90
@@ -13,7 +13,6 @@
 !
 ! PENCILS PROVIDED ss; gss(3); ee; pp; lnTT; cs2; cp; cp1; cp1tilde
 ! PENCILS PROVIDED glnTT(3); TT; TT1; gTT(3); yH; hss(3,3); hlnTT(3,3)
-! PENCILS PROVIDED TT_2; TT_3; TT_4
 ! PENCILS PROVIDED del2ss; del6ss; del2lnTT; cv1; del6lnTT; gamma
 ! PENCILS PROVIDED del2TT; del6TT; glnmumol(3); ppvap; csvap2
 ! PENCILS PROVIDED TTb; rhop; eth; geth(3); glneth(3); del2eth
