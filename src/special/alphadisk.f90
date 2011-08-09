da313221bdbb69bbddf5b66cc5f5c8d2bf7b50ec
Philippe Bourdin <git@Bourdin.ch>
2011-08-09 10:56:55 +0000

    Should at least compile now.

--- src/special/alphadisk.f90
+++ src/special/alphadisk.f90
@@ -89,7 +89,7 @@ module Special
 !
   implicit none
 !
-  include 'special.h'
+  include '../special.h'
 !
 !  Code constants
 !
@@ -1247,6 +1247,6 @@ endsubroutine read_special_run_pars
 !**  copies dummy routines from nospecial.f90 for any Special      **
 !**  routines not implemented in this file                         **
 !**                                                                **
-    include 'special_dummies.inc'
+    include '../special_dummies.inc'
 !********************************************************************
 endmodule Special
