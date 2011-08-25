1181e12aaf0b81fbb4c5929bbbe5e72093430763
Simon Candelaresi <iomsn1@gmail.com>
2011-08-25 12:20:45 +0000

    added power2vtk.py

--- python/pencil/__init__.py
+++ python/pencil/__init__.py
@@ -15,5 +15,6 @@ from pencil.files.zprof import *
 from pencil.files.power import *
 from pencil.files.animate_interactive import *
 from pencil.files.pc2vtk import *
+from pencil.files.power2vtk import *
 from pencil.math.derivatives import *
 from pencil.math.vector_multiplication import *
