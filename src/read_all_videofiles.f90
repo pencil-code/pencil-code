67ce47f1118e3fa0e1dd2265a09d611e4777b9b1
Wladimir Lyra <wladimir.lyra@gmail.com>
2012-01-25 22:48:38 +0000

    corrected typo

--- src/read_all_videofiles.f90
+++ src/read_all_videofiles.f90
@@ -6,7 +6,7 @@ program rvid_box
 !  read and combine slices from individual processor directories data
 !  /procN, write them to data/, where the can be used by used by
 !  rvid_box.pro. Adapated from read_videofiles.f90 but reads
-!  all files without wating for a user input.
+!  all files without waiting for a user input.
 !
 !  17-dec-10/bing: coded
 !
