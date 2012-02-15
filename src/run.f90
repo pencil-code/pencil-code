df8afe5a04dc2da26a7c548cfcc594bc25e8594a
Philippe Bourdin <git@Bourdin.ch>
2012-02-15 17:19:30 +0000

    Fixed indentation.

--- src/run.f90
+++ src/run.f90
@@ -142,25 +142,25 @@ program run
 ! Size of box at local processor. The if-statement is for 
 ! backward compatibility.
 !
-      if (all(lequidist)) then 
-        Lxyz_loc(1)=Lxyz(1)/nprocx
-        Lxyz_loc(2)=Lxyz(2)/nprocy
-        Lxyz_loc(3)=Lxyz(3)/nprocz
-        xyz0_loc(1)=xyz0(1)+ipx*Lxyz_loc(1) 
-        xyz0_loc(2)=xyz0(2)+ipy*Lxyz_loc(2)
-        xyz0_loc(3)=xyz0(3)+ipz*Lxyz_loc(3)
-        xyz1_loc(1)=xyz0_loc(1)+Lxyz_loc(1)
-        xyz1_loc(2)=xyz0_loc(2)+Lxyz_loc(2)
-        xyz1_loc(3)=xyz0_loc(3)+Lxyz_loc(3)
-      else
-        xyz0_loc(1)=x(l1) ; xyz1_loc(1)=x(l2)
-        xyz0_loc(2)=y(m1) ; xyz1_loc(2)=y(m2)
-        xyz0_loc(3)=z(n1) ; xyz1_loc(3)=z(n2)
+  if (all(lequidist)) then 
+    Lxyz_loc(1)=Lxyz(1)/nprocx
+    Lxyz_loc(2)=Lxyz(2)/nprocy
+    Lxyz_loc(3)=Lxyz(3)/nprocz
+    xyz0_loc(1)=xyz0(1)+ipx*Lxyz_loc(1) 
+    xyz0_loc(2)=xyz0(2)+ipy*Lxyz_loc(2)
+    xyz0_loc(3)=xyz0(3)+ipz*Lxyz_loc(3)
+    xyz1_loc(1)=xyz0_loc(1)+Lxyz_loc(1)
+    xyz1_loc(2)=xyz0_loc(2)+Lxyz_loc(2)
+    xyz1_loc(3)=xyz0_loc(3)+Lxyz_loc(3)
+  else
+    xyz0_loc(1)=x(l1) ; xyz1_loc(1)=x(l2)
+    xyz0_loc(2)=y(m1) ; xyz1_loc(2)=y(m2)
+    xyz0_loc(3)=z(n1) ; xyz1_loc(3)=z(n2)
 !
-        Lxyz_loc(1)=xyz1_loc(1) - xyz0_loc(1)
-        Lxyz_loc(2)=xyz1_loc(2) - xyz0_loc(3)
-        Lxyz_loc(3)=xyz1_loc(3) - xyz0_loc(3)
-      endif
+    Lxyz_loc(1)=xyz1_loc(1) - xyz0_loc(1)
+    Lxyz_loc(2)=xyz1_loc(2) - xyz0_loc(3)
+    Lxyz_loc(3)=xyz1_loc(3) - xyz0_loc(3)
+  endif
 !
 ! Calculate dimensionality
 !
