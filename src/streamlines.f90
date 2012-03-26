fcc01f8ca5666cff20b7d89ef45faab422af2076
Simon Candelaresi <iomsn1@gmail.com>
2012-03-26 10:26:28 +0000

    Works now in parallel.

--- src/streamlines.f90
+++ src/streamlines.f90
@@ -10,6 +10,7 @@ module Streamlines
   use Cdata
   use Cparam
   use Messages
+  use Sub, only: update_snaptime, read_snaptime
 !
   implicit none
 !
@@ -20,7 +21,6 @@ module Streamlines
   integer :: VV_RCV = 20
   integer :: FINISHED = 99
 ! the traced vector field
-  real, dimension (nx,ny,nz,3) :: vv
 ! the arrays with the values for x, y and z for all cores (global xyz)
   real, dimension(nxgrid) :: xg
   real, dimension(nygrid) :: yg
@@ -29,10 +29,52 @@ module Streamlines
   integer :: grid_pos_b(3)
 ! variables for the non-blocking mpi communication
   integer, dimension (MPI_STATUS_SIZE) :: status
-  integer :: request, flag, receive = 0, all_finished = 0
+  integer :: request, flag, receive = 0
+!
+  real, public :: ttracers
+  integer, public :: ntracers
+! Time value to be written together with the tracers.
+  real :: ttrace_write
+!
+  integer :: trace_sub = 1
+  character (len=labellen) :: trace_field = ''
+!
+! variables for the stream line tracing
+  real :: h_max = 0.4, h_min = 1e-4, l_max = 10., tol = 1e-4
+!
+  namelist /streamlines_run_pars/ &
+    trace_field, trace_sub, h_max, h_min, l_max, tol
 !
   contains
-!  
+!*********************************************************************** 
+  subroutine tracers_prepare
+!
+!  Prepare ltracers for writing tracers into tracers file
+!
+!  12-mar-12/simon: coded
+!
+    integer, save :: ifirst=0
+!
+    character (len=fnlen) :: file
+!
+!  Output tracer-data in 'ttracers' time intervals
+!
+    file = trim(datadir)//'/ttracers.dat'
+    if (ifirst==0) then
+      call read_snaptime(file,ttracers,ntracers,dtracers,t)
+      ifirst=1
+    endif
+!
+!  This routine sets ltracers=T whenever its time to write the tracers
+!
+    call update_snaptime(file,ttracers,ntracers,dtracers,t,ltracers)
+!
+!  Save current time so that the time that is written out is not
+!  from the next time step
+!
+    if (ltracers) ttrace_write = t
+!
+  endsubroutine tracers_prepare
 !*********************************************************************** 
   subroutine get_grid_pos(phys_pos, grid_pos, outside)
 !
@@ -87,7 +129,7 @@ module Streamlines
 !
   endsubroutine get_grid_pos
 !*********************************************************************** 
-  subroutine get_vector(grid_pos, vvb)
+  subroutine get_vector(grid_pos, vvb, vv)
 !
 ! Gets the vector field value from another core.
 !
@@ -95,6 +137,7 @@ module Streamlines
 !
     integer :: grid_pos(3), grid_pos_send(3)
     real, dimension(3) :: vvb, vvb_send
+    real, pointer, dimension (:,:,:,:) :: vv
     integer :: proc_id, x_proc, y_proc, z_proc, ierr
 !   variables for the non-blocking mpi communication
     integer, dimension (MPI_STATUS_SIZE) :: status_send, status_recv
@@ -175,19 +218,20 @@ module Streamlines
 !
   endsubroutine get_vector
 !***********************************************************************
-  subroutine trace_streamlines(f,tracers,n_tracers,h_max,h_min,l_max,tol,trace_field)
+  subroutine trace_streamlines(tracers,n_tracers,h_max,h_min,l_max,tol,vv)
 !
-!   trace stream lines of the vetor field stored in f(:,:,:,:)
+!   trace stream lines of the vetor field stored in vv
 !
 !   13-feb-12/simon: coded
 !
     use Sub
 !
-    real, dimension (mx,my,mz,mfarray) :: f
+!     real, dimension (mx,my,mz,mfarray) :: f
 !     real, dimension (nx*ny,7) :: tracers
     real, pointer, dimension (:,:) :: tracers
-    character (len=labellen) :: trace_field
-    integer :: n_tracers, tracer_idx, j, ierr, proc_idx
+    real, pointer, dimension (:,:,:,:) :: vv
+!     character (len=labellen) :: trace_field
+    integer :: n_tracers, tracer_idx, j, l, ierr, proc_idx
 !   the "borrowed" vector from the other core
     real, dimension (3) :: vvb, vvb_buf
     real :: h_max, h_min, l_max, tol, dh, dist2
@@ -196,19 +240,18 @@ module Streamlines
 !   current position on the grid
     integer :: grid_pos(3)
     integer :: loop_count, outside = 0
-!   filename for the tracer output
-    character(len=1024) :: filename
 !   array with all finished cores
     integer :: finished_tracing(nprocx*nprocy*nprocz)
 !   variables for the final non-blocking mpi communication
     integer :: request_finished_send(nprocx*nprocy*nprocz)
     integer :: request_finished_rcv(nprocx*nprocy*nprocz)
 !
-    intent(in) :: f,n_tracers,h_max,h_min,l_max,tol,trace_field
+    intent(in) :: n_tracers,h_max,h_min,l_max,tol,vv
 !
 !   tracing stream lines
 !
 !   compute the array with the global xyz values
+    call MPI_BARRIER(MPI_comm_world, ierr)
     do j=1,nxgrid
       xg(j) = x(1+nghost) - ipx*nx*dx + (j-1)*dx
     enddo
@@ -218,25 +261,6 @@ module Streamlines
     do j=1,nzgrid
       zg(j) = z(1+nghost) - ipz*nz*dz + (j-1)*dz
     enddo
-!
-!   TODO: implement for any vector field
-    if (trace_field == 'bb') then
-      do proc_idx=0,(nprocx*nprocy*nprocz-1)
-        if (proc_idx == iproc) then
-!         convert the magnetic vector potential into the magnetic field
-          do m=m1,m2
-            do n=n1,n2
-              call curl(f,iaa,vv(:,m-nghost,n-nghost,:))
-            enddo
-          enddo
-        endif
-        call MPI_BARRIER(MPI_comm_world, ierr)
-      enddo
-    endif
-!
-!   open the destination file
-    write(filename, "(A,I1.1,A)") 'data/proc', iproc, '/tracers.dat'
-    open(unit = 1, file = filename, form = "unformatted")
 !
 !   make sure all threads are synchronized
     call MPI_BARRIER(MPI_comm_world, ierr)
@@ -288,7 +312,7 @@ module Streamlines
         if (outside == 1) exit
         if (any(grid_pos <= 0) .or. (grid_pos(1) > nx) .or. &
             (grid_pos(2) > ny) .or. (grid_pos(3) > nz)) then
-          call get_vector(grid_pos, vvb)
+          call get_vector(grid_pos, vvb, vv)
           x_mid = tracers(tracer_idx,3:5) + 0.5*dh*vvb
         else
           x_mid = tracers(tracer_idx,3:5) + 0.5*dh*vv(grid_pos(1),grid_pos(2),grid_pos(3),:)
@@ -298,7 +322,7 @@ module Streamlines
         if (outside == 1) exit
         if (any(grid_pos <= 0) .or. (grid_pos(1) > nx) .or. &
             (grid_pos(2) > ny) .or. (grid_pos(3) > nz)) then
-          call get_vector(grid_pos, vvb)
+          call get_vector(grid_pos, vvb, vv)
           x_single = tracers(tracer_idx,3:5) + dh*vvb
         else
           x_single = tracers(tracer_idx,3:5) + dh*vv(grid_pos(1),grid_pos(2),grid_pos(3),:)
@@ -309,7 +333,7 @@ module Streamlines
         if (outside == 1) exit
         if (any(grid_pos <= 0) .or. (grid_pos(1) > nx) .or. &
             (grid_pos(2) > ny) .or. (grid_pos(3) > nz)) then
-          call get_vector(grid_pos, vvb)
+          call get_vector(grid_pos, vvb, vv)
           x_mid = tracers(tracer_idx,3:5) + 0.25*dh*vvb
         else
           x_mid = tracers(tracer_idx,3:5) + 0.25*dh*vv(grid_pos(1),grid_pos(2),grid_pos(3),:)
@@ -319,7 +343,7 @@ module Streamlines
         if (outside == 1) exit
         if (any(grid_pos <= 0) .or. (grid_pos(1) > nx) .or. &
             (grid_pos(2) > ny) .or. (grid_pos(3) > nz)) then
-          call get_vector(grid_pos, vvb)
+          call get_vector(grid_pos, vvb, vv)
           x_half = tracers(tracer_idx,3:5) + 0.5*dh*vvb
         else
           x_half = tracers(tracer_idx,3:5) + 0.5*dh*vv(grid_pos(1),grid_pos(2),grid_pos(3),:)
@@ -329,7 +353,7 @@ module Streamlines
         if (outside == 1) exit
         if (any(grid_pos <= 0) .or. (grid_pos(1) > nx) .or. &
             (grid_pos(2) > ny) .or. (grid_pos(3) > nz)) then
-          call get_vector(grid_pos, vvb)
+          call get_vector(grid_pos, vvb, vv)
           x_mid = x_half + 0.25*dh*vvb
         else
           x_mid = x_half + 0.25*dh*vv(grid_pos(1),grid_pos(2),grid_pos(3),:)
@@ -339,7 +363,7 @@ module Streamlines
         if (outside == 1) exit
         if (any(grid_pos <= 0) .or. (grid_pos(1) > nx) .or. &
             (grid_pos(2) > ny) .or. (grid_pos(3) > nz)) then
-          call get_vector(grid_pos, vvb)
+          call get_vector(grid_pos, vvb, vv)
           x_double = x_half + 0.5*dh*vvb
         else
           x_double = x_half + 0.5*dh*vv(grid_pos(1),grid_pos(2),grid_pos(3),:)
@@ -360,18 +384,14 @@ module Streamlines
           if (abs(dh) < h_min) dh = 2*dh
         endif
 !
+!         write(*,*) iproc, tracers(tracer_idx,:)
         if (tracers(tracer_idx, 6) >= l_max) exit
 !
         loop_count = loop_count + 1
       enddo
 !
-!     write into output file
-      if (n_tracers > 4) write(1) tracers(tracer_idx,:)
-      write(*,*) iproc, tracers(tracer_idx,:)
+!       write(*,*) iproc, tracers(tracer_idx,:), outside
     enddo
-!
-!     write(*,*) iproc, "finished field line tracing"
-    close(1)
 !
 !   Tell every other core that we have finished.
     finished_tracing(:) = 0
@@ -436,5 +456,66 @@ module Streamlines
 !     write(*,*) iproc, "all finished"
 !
   endsubroutine trace_streamlines
+!***********************************************************************
+  subroutine wtracers(f,path)
+!
+!   Write the tracers values to tracer.dat.
+!   This should be called during runtime.
+!
+!   12-mar-12/simon: coded
+!
+    use Sub
+!
+    real, dimension (mx,my,mz,mfarray) :: f
+    character(len=*) :: path
+    real, pointer, dimension (:,:) :: tracers
+!   the traced field
+    real, pointer, dimension (:,:,:,:) :: vv
+!   filename for the tracer output
+    character(len=1024) :: filename
+    integer :: j, k
+!
+!   allocate the memory for the tracers
+    allocate(tracers(nx*ny*trace_sub**2,7))
+!   allocate memory for the traced field
+    allocate(vv(nx,ny,nz,3))
+!
+!   prepare the traced field
+    if (lmagnetic) then
+      if (trace_field == 'bb' .and. ipz == 0) then
+!         write(*,*) iproc, "wtracers: curling aa"
+!       convert the magnetic vector potential into the magnetic field
+        do m=m1,m2
+          do n=n1,n2
+            call curl(f,iaa,vv(:,m-nghost,n-nghost,:))
+          enddo
+        enddo
+      endif
+    endif
+!   TODO: include other fields as well
+!     write(*,*) iproc, "allocation successful"
+!
+!   create the initial seeds at z(1+nghost)-ipz*nz*dz+dz
+    do j=1,nx*trace_sub
+      do k=1,ny*trace_sub
+        tracers(j+(k-1)*(nx*trace_sub),1) = x(1+nghost) + (dx/trace_sub)*(j-1)
+        tracers(j+(k-1)*(nx*trace_sub),2) = y(1+nghost) + (dy/trace_sub)*(k-1)
+        tracers(j+(k-1)*(nx*trace_sub),3) = tracers(j+(k-1)*(nx*trace_sub),1)
+        tracers(j+(k-1)*(nx*trace_sub),4) = tracers(j+(k-1)*(nx*trace_sub),2)
+        tracers(j+(k-1)*(nx*trace_sub),5) = z(1+nghost)-ipz*nz*dz+dz
+        tracers(j+(k-1)*(nx*trace_sub),6) = 0.
+        tracers(j+(k-1)*(nx*trace_sub),7) = 0.
+      enddo
+    enddo
+    write(filename, "(A,I1.1,A)") 'data/proc', iproc, '/tracers.dat'
+    open(unit = 1, file = filename, form = "unformatted", position = "append")
+!     write(*,*) iproc, "wtracers: call streamline tracer"
+    call trace_streamlines(tracers,nx*ny*trace_sub**2,h_max,h_min,l_max,tol,vv)
+    write(1) ttrace_write, tracers(:,:)
+    close(1)
+!
+    deallocate(tracers)
+    deallocate(vv)
+  end subroutine wtracers
 !***********************************************************************
 endmodule Streamlines
