6e7d59bbf1624efb0f18e18cf6dbe58ce549261a
Boris Dintrans <boris.dintrans@irap.omp.eu>
2012-05-22 08:24:41 +0000

    added the ability to plot a parallel setup in the z-direction

--- samples/1d-tests/implicit_diffusion/psetup.py
+++ samples/1d-tests/implicit_diffusion/psetup.py
@@ -3,11 +3,22 @@
 #  $Id$
 #  20-may-12/dintrans: coded
 #  Plot the initial setup (density, temperature, entropy and radia. cond. K)
+#  Work for both non-parallel and parallel runs in the z-direction
 #
 
 import pylab as P
+from pencil import read_dim
 
-z,rho,temp,ss,hcond=P.loadtxt('data/proc0/setup.dat',unpack=True,skiprows=1)
+dim=read_dim()
+
+z=[] ; rho=[] ; temp=[] ; ss=[] ; hcond=[]
+for i in range(dim.nprocz):
+    z0,rho0,temp0,ss0,hcond0=P.loadtxt('data/proc%i/setup.dat'%i,skiprows=1,unpack=True)
+    z=P.hstack((z,z0))
+    rho=P.hstack((rho,rho0))
+    temp=P.hstack((temp,temp0))
+    ss=P.hstack((ss,ss0))
+    hcond=P.hstack((hcond,hcond0))
 
 P.rc("lines", linewidth=2)
 P.subplot(221)
