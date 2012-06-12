c2032d23e35f410d75f7713905099dbc8abcf612
Dhrubaditya Mitra <dhruba.mitra@gmail.com>
2012-06-12 13:27:39 +0000

    checked in the full header file.

--- src/particles_potential.h
+++ src/particles_potential.h
@@ -1,4 +1,8 @@
 !  -*-f90-*-  (for emacs)    vim:set filetype=fortran:  (for vim)
-private
-
-public :: dvvp_dt_potential_pencil
+  private
+ 
+  public :: register_particles_potential, initialize_particles_potential
+  public :: dvvp_dt_potential_pencil
+  public :: read_particles_pot_init_pars, write_particles_pot_init_pars
+  public :: read_particles_pot_run_pars, write_particles_pot_run_pars
+  public :: rprint_particles_potential
