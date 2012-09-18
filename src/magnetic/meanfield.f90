ae6cddd10011a5944696cfed19a9da0f6c379386
Axel Brandenburg <brandenb@nordita.org>
2012-09-18 07:59:30 +0000

    adde uturbconst as meanfield_Beq_profile profile for alpha_quenching, but haven't checked it yet.

--- src/magnetic/meanfield.f90
+++ src/magnetic/meanfield.f90
@@ -754,9 +754,15 @@ module Magnetic_meanfield
 !  and should be improved using Beq.
 !
         if (alpha_quenching/=0.0) then
+          select case (meanfield_Beq_profile)
+          case ('uturbconst');
+            Beq21=p%rho1/(uturb**2)
+          case default;
+            Beq21=1./meanfield_Beq**2
+          endselect
           !alpha_quenching_tmp=alpha_quenching
-          alpha_quenching_tmp=alpha_quenching*sqrt(kf_x)
-          alpha_total=alpha_total/(1.+alpha_quenching_tmp*p%b2)
+          !alpha_quenching_tmp=alpha_quenching*sqrt(kf_x)
+          alpha_total=alpha_total/(1.+alpha_quenching_tmp*p%b2*Beq21)
         endif
 !
 !  Apply alpha effect; allow for anisotropy
