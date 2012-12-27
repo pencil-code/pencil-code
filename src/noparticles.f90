4c1939719f2168f4dbb40e4bb5bafb3700aa94d8
Wladimir Lyra <wladimir.lyra@gmail.com>
2012-12-27 16:29:20 +0000

    compute glnrho of particles

--- src/noparticles.f90
+++ src/noparticles.f90
@@ -9,7 +9,7 @@
 !
 ! CPARAM logical, parameter :: lparticles=.false.
 !
-! PENCILS PROVIDED rhop
+! PENCILS PROVIDED rhop;glnrhop(3)
 !
 !***************************************************************
 module Particles
