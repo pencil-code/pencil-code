00a9f44c149729989b33968ad1f74bdebf90948a
Axel Brandenburg <brandenb@nordita.org>
2013-01-31 18:40:58 +0000

    added bc_ss_temp3_z dummy

--- src/eos_temperature_ionization.f90
+++ src/eos_temperature_ionization.f90
@@ -1583,6 +1583,25 @@ module EquationOfState
 !
     endsubroutine bc_ss_temp2_z
 !***********************************************************************
+    subroutine bc_ss_temp3_z(f,topbot)
+!
+!  boundary condition for entropy: constant temperature
+!
+!  31-jan-2013/axel: dummy to bc_ss_temp3_z in eos_idealgas
+!
+      use Gravity, only: gravz
+!
+      character (len=3) :: topbot
+      real, dimension (mx,my,mz,mfarray) :: f
+      real :: tmp,dcs2bot
+      integer :: i
+!
+      if (ldebug) print*,'bc_ss_temp3_z: cs20,cs0=',cs20,cs0
+!
+      call fatal_error('bc_ss_temp3_z','invalid argument')
+!
+    endsubroutine bc_ss_temp3_z
+!***********************************************************************
     subroutine bc_ss_stemp_x(f,topbot)
 !
 !  boundary condition for entropy: symmetric temperature
