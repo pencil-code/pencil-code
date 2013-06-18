2258d0357cd5bd2b51456c12ceb44d52810fbac5
Chao-Chin Yang <ccyang@astro.lu.se>
2013-06-18 14:04:33 +0000

    Add dummy module for energy equation.

--- src/energy.h
+++ src/energy.h
@@ -5,7 +5,13 @@
   public :: read_energy_init_pars, write_energy_init_pars
   public :: read_energy_run_pars, write_energy_run_pars
   public :: rprint_energy, get_slices_energy
-  public :: init_ee, dee_dt, calc_lenergy_pars
+  public :: calc_lenergy_pars
+  public :: init_ee, dee_dt
   public :: pencil_criteria_energy, pencil_interdep_energy
-  public :: calc_pencils_energy, fill_farray_pressure
+  public :: calc_pencils_energy
+  public :: fill_farray_pressure
   public :: dynamical_thermal_diffusion
+  public :: impose_energy_floor
+  public :: split_update_energy
+  public :: expand_shands_energy
+  public :: get_slices_energy
---
+++ src/noenergy.f90
@@ -0,0 +1,207 @@
+! $Id$
+!
+!  Dummy module for energy equation.
+!
+!** AUTOMATIC CPARAM.INC GENERATION ****************************
+! Declare (for generation of cparam.inc) the number of f array
+! variables and auxiliary variables added by this module
+!
+! CPARAM logical, parameter :: lentropy = .false.
+! CPARAM logical, parameter :: ltemperature = .false.
+! CPARAM logical, parameter :: lthermal_energy = .false.
+!
+! MVAR CONTRIBUTION 0
+! MAUX CONTRIBUTION 0
+!
+! PENCILS PROVIDED Ma2; fpres(3); tcond
+!
+!***************************************************************
+module Energy
+!
+  use Cparam
+  use Cdata
+  use General, only: keep_compiler_quiet
+  use Messages
+!
+  implicit none
+!
+  include 'energy.h'
+!
+  contains
+!***********************************************************************
+    subroutine register_energy()
+!
+!  Identify version number.
+!
+      if (lroot) call svn_id("$Id")
+!
+    endsubroutine register_energy
+!***********************************************************************
+    subroutine initialize_energy(f,lstarting)
+!
+!  Called by run.f90 after reading parameters, but before the time loop.
+!
+      real, dimension (mx,my,mz,mfarray), intent(in) :: f
+      logical, intent(in) :: lstarting
+!
+      call keep_compiler_quiet(f)
+      call keep_compiler_quiet(lstarting)
+!
+    endsubroutine initialize_energy
+!***********************************************************************
+    subroutine read_energy_init_pars(unit,iostat)
+!
+      integer, intent(in) :: unit
+      integer, intent(inout), optional :: iostat
+!
+      call keep_compiler_quiet(unit)
+      if (present(iostat)) call keep_compiler_quiet(iostat)
+!
+    endsubroutine read_energy_init_pars
+!***********************************************************************
+    subroutine write_energy_init_pars(unit)
+!
+      integer, intent(in) :: unit
+!
+      call keep_compiler_quiet(unit)
+!
+    endsubroutine write_energy_init_pars
+!***********************************************************************
+    subroutine read_energy_run_pars(unit,iostat)
+!
+      integer, intent(in) :: unit
+      integer, intent(inout), optional :: iostat
+!
+      call keep_compiler_quiet(unit)
+      if (present(iostat)) call keep_compiler_quiet(iostat)
+!
+    endsubroutine read_energy_run_pars
+!***********************************************************************
+    subroutine write_energy_run_pars(unit)
+!
+      integer, intent(in) :: unit
+!
+      call keep_compiler_quiet(unit)
+!
+    endsubroutine write_energy_run_pars
+!***********************************************************************
+    subroutine init_ee(f)
+!
+      real, dimension (mx,my,mz,mfarray), intent(in) :: f
+!
+      call keep_compiler_quiet(f)
+!
+    endsubroutine init_ee
+!***********************************************************************
+    subroutine pencil_criteria_energy()
+!
+!  All pencils that the Entropy module depends on are specified here.
+!
+    endsubroutine pencil_criteria_energy
+!***********************************************************************
+    subroutine pencil_interdep_energy(lpencil_in)
+!
+!  Interdependency among pencils from the Entropy module is specified here.
+!
+      logical, dimension(npencils), intent(in) :: lpencil_in
+!
+      call keep_compiler_quiet(lpencil_in)
+!
+    endsubroutine pencil_interdep_energy
+!***********************************************************************
+    subroutine calc_pencils_energy(f,p)
+!
+!  Calculate Energy pencils.
+!  Most basic pencils should come first, as others may depend on them.
+!
+      real, dimension (mx,my,mz,mfarray), intent(in) :: f
+      type (pencil_case), intent(in) :: p
+!
+      call keep_compiler_quiet(f)
+      call keep_compiler_quiet(p)
+!
+    endsubroutine calc_pencils_energy
+!**********************************************************************
+    subroutine dee_dt(f,df,p)
+!
+!  Calculate right hand side of energy equation.
+!
+      call keep_compiler_quiet(f)
+      call keep_compiler_quiet(df)
+      call keep_compiler_quiet(p)
+!
+    endsubroutine dee_dt
+!***********************************************************************
+    subroutine calc_lenergy_pars(f)
+!
+!  dummy routine
+!
+      real, dimension (mx,my,mz,mfarray) :: f
+      intent(in) :: f
+!
+      call keep_compiler_quiet(f)
+!
+    endsubroutine calc_lenergy_pars
+!***********************************************************************
+    subroutine get_slices_energy(f,slices)
+!
+      real, dimension (mx,my,mz,mfarray) :: f
+      type (slice_data) :: slices
+!
+      call keep_compiler_quiet(f)
+      call keep_compiler_quiet(slices%ready)
+!
+    endsubroutine get_slices_energy
+!***********************************************************************
+    subroutine rprint_energy(lreset,lwrite)
+!
+!  Reads and registers print parameters relevant to entropy.
+!
+      logical :: lreset
+      logical, optional :: lwrite
+!
+      call keep_compiler_quiet(lreset)
+      if (present(lwrite)) call keep_compiler_quiet(lwrite)
+!
+    endsubroutine rprint_energy
+!***********************************************************************
+    subroutine fill_farray_pressure(f)
+!
+!  Fill f array with the pressure, to be able to calculate pressure gradient
+!  directly from the pressure.
+!
+!  18-feb-10/anders: dummy
+!
+      real, dimension (mx,my,mz,mfarray) :: f
+!
+      call keep_compiler_quiet(f)
+!
+    endsubroutine fill_farray_pressure
+!***********************************************************************
+    subroutine impose_energy_floor(f)
+!
+!  Dummy subroutine; may not be necessary for lnTT
+!
+      real, dimension(mx,my,mz,mfarray) :: f
+!
+      call keep_compiler_quiet(f)
+!
+    endsubroutine impose_energy_floor
+!***********************************************************************
+    subroutine split_update_energy(f)
+!
+!  Dummy subroutine
+!
+      real, dimension(mx,my,mz,mfarray), intent(inout) :: f
+!
+      call keep_compiler_quiet(f)
+!
+    endsubroutine
+!***********************************************************************
+    subroutine expand_shands_energy()
+!
+!  Presently dummy, for possible use
+!
+    endsubroutine expand_shands_energy
+!***********************************************************************
+endmodule Energy
