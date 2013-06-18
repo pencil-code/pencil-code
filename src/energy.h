7f2f357d25ce9c3e2edc1345e00152f44ce1fa3a
Sven Bingert <sven.bingert@gwdg.de>
2013-06-18 18:53:43 +0000

    mv file

--- src/energy.h
+++ src/energy.h
@@ -5,10 +5,10 @@
   public :: read_energy_init_pars, write_energy_init_pars
   public :: read_energy_run_pars, write_energy_run_pars
   public :: rprint_energy, get_slices_energy
-  public :: calc_lenergy_pars
-  public :: init_ee, dee_dt
+  public :: init_ss, dss_dt, calc_lenergy_pars
   public :: pencil_criteria_energy, pencil_interdep_energy
-  public :: calc_pencils_energy
+  public :: calc_pencils_energy, fill_farray_pressure
   public :: impose_energy_floor
+  public :: dynamical_thermal_diffusion
   public :: split_update_energy
   public :: expand_shands_energy
