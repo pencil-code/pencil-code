59d1ce4293c184d9330498a98619f5b508d58692
Anders Johansen <anders@astro.lu.se>
2013-06-18 14:06:30 +0000

    Commented out parameters for cparam.inc so that they do not appear twice.

--- src/noenergy.f90
+++ src/noenergy.f90
@@ -6,9 +6,9 @@
 ! Declare (for generation of cparam.inc) the number of f array
 ! variables and auxiliary variables added by this module
 !
-! CPARAM logical, parameter :: lentropy = .false.
-! CPARAM logical, parameter :: ltemperature = .false.
-! CPARAM logical, parameter :: lthermal_energy = .false.
+!! CPARAM logical, parameter :: lentropy = .false.
+!! CPARAM logical, parameter :: ltemperature = .false.
+!! CPARAM logical, parameter :: lthermal_energy = .false.
 !
 ! MVAR CONTRIBUTION 0
 ! MAUX CONTRIBUTION 0
