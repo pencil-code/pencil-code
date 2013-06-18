1e545fa9fd76aea53c78df8e3de8e21b3e53a480
Wladimir Lyra <wladimir.lyra@gmail.com>
2013-06-18 11:24:43 +0000

    include file for conductivity

---
+++ src/experimental/conductivity.h
@@ -0,0 +1,12 @@
+!  -*-f90-*-  (for emacs)    vim:set filetype=fortran:  (for vim)
+  private
+
+  public :: register_conductivity, initialize_conductivity
+  public :: read_conductivity_init_pars, write_conductivity_init_pars
+  public :: read_conductivity_run_pars, write_conductivity_run_pars
+  public :: rprint_conductivity
+  public :: pencil_criteria_conductivity, pencil_interdep_conductivity
+  public :: calc_pencils_conductivity
+  public :: dynamical_thermal_diffusion
+  public :: heat_conductivity
+
