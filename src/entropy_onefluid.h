4f40df8e0f1670bf74a67373411ae4a292211487
Sven Bingert <sven.bingert@gwdg.de>
2013-06-18 18:52:59 +0000

    mv file

---
+++ src/entropy_onefluid.h
@@ -0,0 +1,14 @@
+!  -*-f90-*-  (for emacs)    vim:set filetype=fortran:  (for vim)
+  private
+
+  public :: register_energy, initialize_energy
+  public :: read_energy_init_pars, write_energy_init_pars
+  public :: read_energy_run_pars, write_energy_run_pars
+  public :: rprint_energy, get_slices_energy
+  public :: calc_lenergy_pars
+  public :: init_ee, dee_dt
+  public :: pencil_criteria_energy, pencil_interdep_energy
+  public :: calc_pencils_energy
+  public :: impose_energy_floor
+  public :: split_update_energy
+  public :: expand_shands_energy
