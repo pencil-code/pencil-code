0f69d22fbf334160b6858d5c0d17108ada705ae3
Philippe Bourdin <git@Bourdin.ch>
2013-06-22 06:51:43 +0000

    Language corrections for the "Contact" page.

--- doc/www/contact.php
+++ doc/www/contact.php
@@ -4,7 +4,8 @@
  ?>
 <div class="centcolumnpad">
 <h2>Contact.</h2>
-The main developers and maintainers of this projects are:
+The current development team consists of:
+
 <ul>
 <li>Axel Brandenburg (brandenb&#64;nordita&#46;org)</li>
 <li>Sven Bingert</li>
@@ -17,13 +18,10 @@ The main developers and maintainers of this projects are:
 <li>Wladimir Lyra</li>
 <li>Dhruba Mitra</li>
 <li>Matthias Rheinhardt</li>
-
 </ul>
 
-All the  people involved in this project are listed <a href="http://code.google.com/p/pencil-code/people/list">here.</a>
-<br>
-For any problem in the code you can write to the <a href="https://groups.google.com/forum/?fromgroups#!forum/pencil-code-discuss"> mailing list.</a>
-
+All the people involved in this project are listed <a href="http://code.google.com/p/pencil-code/people/list">here</a>.<br>
+For detailed questions regarding the code, please contact our public <a href="https://groups.google.com/forum/?fromgroups#!forum/pencil-code-discuss">discussion mailing list</a>.
 </div>
 <?
 	include "footer.inc";
