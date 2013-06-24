267fb47cc1f77f21c18a2d6895813cfcf943414d
Philippe Bourdin <git@Bourdin.ch>
2013-06-24 20:38:26 +0000

    [web] Added links for the highlights.

--- doc/www/highlights/index.php
+++ doc/www/highlights/index.php
@@ -6,13 +6,13 @@
 <h2>Highlights</h2>
 Currently, we have the following highlights planned:
 <ul>
-	<li><a href="/highlights/">Isotropic turbulence</a> [AB]</li>
-	<li><a href="/highlights/">Helical MHD Dynamo</a> [AB]</li>
-	<li><a href="/highlights/">Stellar convection</a> [BD/DB]</li>
-	<li><a href="/highlights/">Magnetorotational instability</a> [AJ]</li>
-	<li><a href="/highlights/">Streaming instability</a> [DC/ML/AJ]</li>
-	<li><a href="/highlights/">Global cartesian discs</a> [WL]</li>
-	<li><a href="/highlights/">Particle flow around objects</a> [NH/DM]</li>
+	<li><a href="/highlights/isotropic_turbulence/">Isotropic turbulence</a> [AB]</li>
+	<li><a href="/samples/turbulence/helical-MHDturb32-4procs/">Helical MHD Dynamo</a> [AB]</li>
+	<li><a href="/highlights/stellar_convection/">Stellar convection</a> [BD/DB]</li>
+	<li><a href="/highlights/magnetorotational_instability/">Magnetorotational instability</a> [AJ]</li>
+	<li><a href="/highlights/streaming_instability/">Streaming instability</a> [DC/ML/AJ]</li>
+	<li><a href="/highlights/global_cartesian_discs/">Global cartesian discs</a> [WL]</li>
+	<li><a href="/highlights/particle_flow_around_objects/">Particle flow around objects</a> [NH/DM]</li>
 </ul>
 
 <u>This is a test page / template for the highlights subdirectory.</u>
