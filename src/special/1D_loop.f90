db1bd990137959f8e52eb957efe5d414bea21138
Sven Bingert <sven.bingert@gwdg.de>
2013-06-24 16:15:04 +0000

    [renaming]

--- src/special/1D_loop.f90
+++ src/special/1D_loop.f90
@@ -457,7 +457,7 @@ module Special
 !
     endsubroutine get_slices_special
 !***********************************************************************
-    subroutine special_calc_entropy(f,df,p)
+    subroutine special_calc_energy(f,df,p)
 !
 !  Calculate an additional 'special' term on the right hand side of the
 !  entropy equation.
@@ -501,7 +501,7 @@ module Special
           if (lfirst.and.ldt) dt1_max=max(dt1_max,hyper3_chi/0.01)
       endif
 !
-    endsubroutine special_calc_entropy
+    endsubroutine special_calc_energy
 !***********************************************************************
     subroutine special_calc_density(f,df,p)
 !
