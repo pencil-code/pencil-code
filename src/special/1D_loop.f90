67f9eab4381a1210aaf3618e73255a368c679341
Sven Bingert <sven.bingert@gwdg.de>
2013-06-24 16:16:51 +0000

    [renaming]

--- src/special/1D_loop.f90
+++ src/special/1D_loop.f90
@@ -864,7 +864,7 @@ module Special
 !
       real, dimension (nx) :: newton,tau_inv_tmp
 !
-      if (headtt) print*,'special_calc_entropy: newton cooling',tau_inv_newton
+      if (headtt) print*,'special_calc_energy: newton cooling',tau_inv_newton
 !
 !  Get reference temperature
       newton  = exp(lnTT_init_prof(l1:l2)-p%lnTT)-1.
