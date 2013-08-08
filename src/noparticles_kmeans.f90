aaf06bf907447ab89ea2008e725e699047e18181
Anders Johansen <anders@astro.lu.se>
2013-08-08 12:24:49 +0000

    Fatal error if called.

--- src/noparticles_kmeans.f90
+++ src/noparticles_kmeans.f90
@@ -1,9 +1,11 @@
 ! $Id: particles_coagulation.f90 19828 2012-11-27 09:58:06Z kalle.jansson.89 $
 !
-module Particles_kmeans
-!
 !  Dummy version of k-means vector quantization algorithm using the Forgy
 !  initialisation of the codebook.
+!
+module Particles_kmeans
+!
+  use Messages
 !
   contains
 !***********************************************************************
@@ -19,6 +21,8 @@ module Particles_kmeans
       real, dimension(dims,cb_size) :: codebk
       real, dimension(cb_size) :: codew
       logical :: do_split, do_norm, do_detail
+!
+      call fatal_error('ppcvq','you need to use the k-means module')
 !
       call keep_compiler_quiet(measure,dims,dim1,dim2,no_tr_vec)
       call keep_compiler_quiet(trainv)
