dc17b8544571d54db8197083f85d6c9bf8f21b81
Matthias Rheinhardt <mreinhardt@nordita.org>
2013-08-13 16:44:11 +0000

    MR: built in subroutine init_aatest

--- src/test_methods/testfield_general.f90
+++ src/test_methods/testfield_general.f90
@@ -14,14 +14,17 @@ module Testfield_general
   real, dimension(3)                         :: B_ext=(/0.,0.,0./)
   character (len=labellen), dimension(ninit) :: initaatest='zero'
   real,                     dimension(ninit) :: amplaatest=0.
+  real,                     dimension(ninit) :: kx_aatest,ky_aatest,kz_aatest, &
+                                                phasex_aatest,phasey_aatest,phasez_aatest
   logical                                    :: luxb_as_aux=.false.,ljxb_as_aux=.false.
-
-  namelist /testfield_init_pars/ &
-           B_ext,       &
-           initaatest,  &     !can be PROTECTED
-           amplaatest,  &     !        .
-           luxb_as_aux, &     !        .     
-           ljxb_as_aux        !        .
+  namelist /testfield_init_pars/          &
+           B_ext,                         &
+           luxb_as_aux,                   &     ! can be PROTECTED  
+           ljxb_as_aux,                   &     !        .
+           initaatest,                    &     !        .
+           amplaatest,                    &     !        .
+           kx_aatest,ky_aatest,kz_aatest, &
+           phasex_aatest,phasey_aatest,phasez_aatest
 !
 ! run parameters
 !
@@ -48,8 +51,7 @@ module Testfield_general
 !
 ! work variables
 !
-  real, dimension (nx,nz,3,njtest) :: uxbtestm
-  integer, dimension (njtest)      :: nuxb=0
+  integer, dimension (njtest) :: nuxb=0
 
   real    :: taainit=0.,bamp1=1.,bamp12=1.
   integer :: i1=1,i2=2,i3=3,i4=4,i5=5,i6=6,i7=7,i8=8,i9=9
@@ -58,6 +60,74 @@ module Testfield_general
   integer, private :: naainit
 !
   contains
+!***********************************************************************
+    subroutine init_aatest(f)
+!
+!  initialise testfield; called from start.f90
+!
+!   2-jun-05/axel: adapted from magnetic
+!
+      use Cdata
+      use Mpicomm
+      use Initcond
+      !!use Sub
+      use InitialCondition, only: initial_condition_aatest
+!
+      real, dimension (mx,my,mz,mfarray) :: f
+      integer :: j, indx, jtest, istart,ios
+!
+      do j=1,ninit
+
+      indx = index(initaatest(j),'-',BACK=.true.)+1
+      if (indx>1 .and. indx<=len(trim(initaatest(j)))) then
+!
+        read(initaatest(j)(indx:),'(i3)',IOSTAT=ios) jtest
+!
+        if (ios/=0) then
+          print*, 'init_aatest: Error when reading testfield index from initial condition ', j, '. Ignored.'
+          cycle
+        endif
+
+        if (jtest>0 .and. jtest<=njtest) then
+          istart = iaxtest+3*(jtest-1)
+        else
+          print*, 'init_aatest: Invalid testfield index employed in initial condition ', j, '. Ignored.'
+          cycle
+        endif
+
+      else
+        istart=0
+      endif
+
+      if (jtest>0 .and. jtest<=njtest) then
+
+        istart = iaxtest+3*(jtest-1)
+        select case (initaatest(j))
+
+        case ('zero'); f(:,:,:,iaatest:iaatest+ntestfield-1)=0.
+        case ('gaussian-noise'); call gaunoise(amplaatest(j),f,istart,iaztest+3*(jtest-1))
+        case ('sinwave-x'); if (istart>0) call sinwave(amplaatest(j),f,istart+1,kx=kx_aatest(j))   ! sets y-component!!
+!        case ('sinwave-x-1'); if (istart>0) call sinwave(amplaatest(j),f,iaxtest+0+1,kx=kx_aatest(j))        
+        case ('Beltrami-x'); if (istart>0) call beltrami(amplaatest(j),f,istart,kx=-kx_aatest(j),phase=phasex_aatest(j))
+        case ('Beltrami-z'); if (istart>0) call beltrami(amplaatest(j),f,istart,kz=-kz_aatest(j),phase=phasez_aatest(j))
+        case ('nothing'); !(do nothing)
+
+        case default
+        !
+        !  Catch unknown values
+        !
+          if (lroot) print*, 'init_aatest: check initaatest: ', trim(initaatest(j))
+          call stop_it("")
+
+        endselect
+      endif
+      enddo
+!
+!  Interface for user's own subroutine
+!
+      if (linitial_condition) call initial_condition_aatest(f)
+!
+    endsubroutine init_aatest
 !***********************************************************************
     subroutine rescaling_testfield(f)
 !
@@ -80,32 +150,33 @@ module Testfield_general
 !  reinitialize aatest periodically if requested
 !
       if (linit_aatest) then
-
+!
         file=trim(datadir)//'/tinit_aatest.dat'
-
+!
         if (lfirst_call) then
           call read_snaptime(trim(file),taainit,naainit,daainit,t)
           if (taainit==0 .or. taainit < t-daainit) taainit=t+daainit
           lfirst_call=.false.
         endif
 !
-!  Do only one xy plane at a time (for cache efficiency)
-!  Also: reset nuxb=0, which is used for time-averaged testfields
+!  Do only one xy plane at a time (for cache efficiency).
+!  Also: reset nuxb=0, which is used for time-averaged testfields.
+!  Do this for the full nuxb array.
 !
         if (t >= taainit) then
-
+!
           if (ltestfield_taver) nuxb=0
-
+!
           do jtest=1,njtest
-
+!
             iaxtest=iaatest+3*(jtest-1)
-
+!
             do j=iaxtest,iaxtest+2
               do nl=n1,n2
                 f(l1:l2,m1:m2,nl,j)=rescale_aatest(jtest)*f(l1:l2,m1:m2,nl,j)
               enddo
             enddo
-
+!
           enddo
           call update_snaptime(file,taainit,naainit,daainit,t,ltestfield_out)
         endif
@@ -113,7 +184,7 @@ module Testfield_general
 !
     endsubroutine rescaling_testfield
 !***********************************************************************
-    subroutine register_testfield()
+    subroutine register_testfield
 !
 !  Initialise variables which should know that we solve for the vector
 !  potential: iaatest, etc; increase nvar accordingly
@@ -137,6 +208,7 @@ module Testfield_general
       
       ntestfield=3*njtest
       nvar=nvar+ntestfield
+      iaztestpq=nvar
 !
       if ((ip<=8) .and. lroot) then
         print*, 'register_testfield: nvar = ', nvar
@@ -150,7 +222,7 @@ module Testfield_general
 !  Identify version number.
 !
       if (lroot) call svn_id( &
-           "$Id: testfield_meri.f90 19193 2012-06-30 12:55:46Z wdobler $")
+           "$Id: testfield_general.f90 19193 2013-06-27 12:55:46Z wdobler $")
 !
       if (nvar > mvar) then
         if (lroot) write(0,*) 'nvar = ', nvar, ', mvar = ', mvar
