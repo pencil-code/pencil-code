6cf185916bc6e841dcf7a0eebcc11fe844152396
Philippe Bourdin <git@Bourdin.ch>
2013-09-10 00:03:33 +0000

    Removed trailing spaces.

--- src/testfield.h
+++ src/testfield.h
@@ -9,4 +9,4 @@
   public :: init_aatest, daatest_dt
   public :: pencil_criteria_testfield, pencil_interdep_testfield
   public :: testfield_after_boundary
-  public :: rescaling_testfield               
+  public :: rescaling_testfield
