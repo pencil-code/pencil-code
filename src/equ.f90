4989a37cd4fe17fadff34c09a5ecf56bb894c0d7
Matthias Rheinhardt <mreinhardt@nordita.org>
2013-09-25 21:10:13 +0000

    MR: removed parameter p from call of testfield_after_boundary

--- src/equ.f90
+++ src/equ.f90
@@ -318,7 +318,7 @@ module Equ
       if (lforcing_cont)          call calc_lforcing_cont_pars(f)
       if (lpolymer)               call calc_polymer_after_boundary(f)
       if (ltestscalar)            call testscalar_after_boundary(f)
-      if (ltestfield)             call testfield_after_boundary(f,p)
+      if (ltestfield)             call testfield_after_boundary(f)
       if (lpscalar)               call pscalar_after_boundary(f)
       if (ldensity)               call calc_ldensity_pars(f)
       if (ltestflow)              call calc_ltestflow_nonlin_terms(f,df)
