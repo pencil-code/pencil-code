f7b7d35e8a5c6fb913c8898dd5a99c088c977885
Matthias Rheinhardt <mreinhardt@nordita.org>
2013-09-27 14:29:49 +0000

    MR: new parameters for nxgrid*nygrid etc.

--- src/cparam.f90
+++ src/cparam.f90
@@ -9,6 +9,7 @@ module Cparam
   include 'cparam.local'
 !
   integer, parameter :: nx=nxgrid/nprocx,ny=nygrid/nprocy,nz=nzgrid/nprocz
+  integer, parameter :: nxygrid=nxgrid*nygrid,nxzgrid=nxgrid*nzgrid,nyzgrid=nygrid*nzgrid
   integer, parameter :: nprocxy=nprocx*nprocy
   integer, parameter :: nprocyz=nprocy*nprocz
   integer, parameter :: nprocxz=nprocx*nprocz
