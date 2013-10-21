b118701887ac4b798232126cdf65713d2e9e8c66
Chao-Chin Yang <ccyang@unlv.edu>
2013-10-21 23:20:43 +0000

    Add function time_series.

--- PencilCode/plot.py
+++ PencilCode/plot.py
@@ -6,3 +6,27 @@
 # Chao-Chin Yang, 2013-10-22
 # Last Modification: $Id$
 #
+def time_series(datadir='./data', diagnostics='dt'):
+    """Plots diagnostic variable(s) as a function of time.
+
+    Keyword Arguments:
+        datadir:  Name of the data directory.
+        diagnostics:  (A list of) diagnostic variable(s).
+    """
+    #
+    # Chao-Chin Yang, 2013-10-22
+    #
+    from . import read
+    from pylab import plot, show
+
+    # Read the time series.
+    ts = read.time_series(datadir=datadir)
+
+    # Plot the diagnostics.
+    if type(diagnostics) is list:
+        for diag in diagnostics:
+            plot(ts.t, ts[diag])
+    else:
+        plot(ts.t, ts[diagnostics])
+    show()
+
