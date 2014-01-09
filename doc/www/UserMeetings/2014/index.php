971005bb36a98ce814f26357d68a1ced51d90298
Philippe Bourdin <git@Bourdin.ch>
2014-01-09 13:40:16 +0000

    Added meeting 2014.

---
+++ doc/www/UserMeetings/2014/index.php
@@ -0,0 +1,14 @@
+<?
+	 include "inc/header.inc";
+ ?>
+<div class="centcolumnpad">
+<h2>Pencil Code User Meeting 2014</h2>
+
+<li><a href="http://agenda.albanova.se/conferenceDisplay.py?confId=4213">Pencil Code User Meeting</a> (meeting page)
+<li><a href="/UserMeetings/2014/agenda.txt">agenda.txt</a>
+<br>
+<br>
+
+<?
+	include "inc/footer.inc";
+ ?>
---
+++ doc/www/meeting2014.php
@@ -0,0 +1,1 @@
+UserMeetings/2014/index.php
