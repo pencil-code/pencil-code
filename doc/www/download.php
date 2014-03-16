9de8ff9f83d5f113d53164b56674bd06e93f3f41
Axel Brandenburg <brandenb@nordita.org>
2014-03-16 20:47:57 +0000

    newer tarball

--- doc/www/download.php
+++ doc/www/download.php
@@ -11,7 +11,7 @@ If you just want to check out the latest version of the code use:
 If you have a Pencil Code username NAME at google-code then use:
 <div class="codescroll"><code>svn checkout https://pencil-code.googlecode.com/svn/trunk/ pencil-code --username NAME</code></div>
 
-The Pencil Code can also be obtained from a <a href="http://code.google.com/p/pencil-code/downloads/list">tar ball</a> (click <a href="http://norlx51.albanova.se/~brandenb/downloads/pencil-code-r18525.tar.gz">here</a> for a local copy r18525).
+The Pencil Code can also be obtained from a <a href="http://code.google.com/p/pencil-code/downloads/list">tar ball</a> (click <a href="http://norlx51.albanova.se/~brandenb/downloads/pencil-code-r21570.tar.gz">here</a> for a local copy r21570).
 </div>
 
 <div class="centerdivider"></div>
