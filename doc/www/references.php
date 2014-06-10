71b89b54127af7b8f0d1a8267fac9312b880fd6a
Philippe Bourdin <git@Bourdin.ch>
2014-06-10 09:45:01 +0000

    Added reference for the solar corona models.

--- doc/www/references.php
+++ doc/www/references.php
@@ -8,36 +8,42 @@
 <ul>
 
 <li class="separated">
-Brandenburg, A., &amp; Dobler, W.:&nbsp;2002, ``Hydromagnetic turbulence in computer simulations,'' <em>Comp. Phys. Comm.</em> <b>147</b>, 471-475
+Brandenburg, A., &amp; Dobler, W.: 2002, &quot;Hydromagnetic turbulence in computer simulations&quot;, <em>Comp. Phys. Comm.</em> <b>147</b>, 471-475
 (<a href="http://arXiv.org/abs/astro-ph/0111569"><i>astro-ph/0111569</i></a>,
 <a href="http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode=2002CoPhC.147..471B&amp;db_key=PHY&amp;high=3ba10b02e000421">ADS</a>,
 <a href="http://www.nordita.org/~brandenb/own/Bran+Dobl02.pdf">PDF</a>)
 </li>
 
 <li class="separated">
-Brandenburg, A.:&nbsp;2003, ``Computational aspects of astrophysical MHD and turbulence,'' in <em>Advances in nonlinear dynamos (The Fluid Mechanics of Astrophysics and Geophysics, Vol. <b>9</b>)</em>, ed. A. Ferriz-Mas &amp; M. N&#250;&#241;ez, Taylor &amp; Francis, London and New York, pp.&nbsp;269-344
+Brandenburg, A.: 2003, &quot;Computational aspects of astrophysical MHD and turbulence&quot;, in <em>Advances in nonlinear dynamos (The Fluid Mechanics of Astrophysics and Geophysics, Vol. <b>9</b>)</em>, ed. A. Ferriz-Mas &amp; M. N&#250;&#241;ez, Taylor &amp; Francis, London and New York, pp.&nbsp;269-344
 (<a href="http://arXiv.org/abs/astro-ph/0109497"><i>astro-ph/0109497</i></a>,
 <a href="http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode=2003and..book..269B&amp;amp;refs=CITATIONS&amp;amp;db_key=PHY">ADS</a>,
 <a href="http://www.nordita.org/~brandenb/own/Bran_comp03.pdf">PDF</a>)
 </li>
 
 <li class="separated">
-Haugen, N. E. L., Brandenburg, A., &amp; Dobler, W.:&nbsp;2004, ``Simulations of nonhelical hydromagnetic turbulence,'' <em>Phys. Rev. </em><b>E 70</b>, 016308, 1-14
+Haugen, N. E. L., Brandenburg, A., &amp; Dobler, W.: 2004, &quot;Simulations of nonhelical hydromagnetic turbulence&quot;, <em>Phys. Rev. </em><b>E 70</b>, 016308, 1-14
 (<a href="http://arXiv.org/abs/astro-ph/0307059"><i>astro-ph/0307059</i></a>,
 <a href="http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode=2004PhRvE..70a6308H&amp;amp;db_key=PHY&amp;amp;high=4080bca9ae20164">ADS</a>,
 <a href="http://www.nordita.org/~brandenb/own/Hau+Bran+Dobl04.pdf">PDF</a>)
 </li>
 
 <li class="separated">
-Dobler, W., Stix, M., &amp; Brandenburg, A.:&nbsp;2006, ``Convection and magnetic field generation in fully convective spheres,'' <em>Astrophys. J. </em><b>638</b>, 336-347
+Dobler, W., Stix, M., &amp; Brandenburg, A.: 2006, &quot;Convection and magnetic field generation in fully convective spheres&quot;, <em>Astrophys. J. </em><b>638</b>, 336-347
 (<a href="http://arXiv.org/abs/astro-ph/0410645"><i>astro-ph/0410645</i></a>,
 <a href="http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode=2006ApJ...638..336D&amp;amp;db_key=AST&amp;amp;data_type=HTML&amp;amp;format=&amp;amp;high=42689b430f26475">ADS</a>,
 <a href="http://www.nordita.org/~brandenb/own/Dobl+Stix+Bran06.pdf">PDF</a>)
 </li>
 
 <li class="separated">
-de Val-Borro, M. and 22 coauthors (incl. Lyra, W.):&nbsp;2006, ``A comparative study of disc-planet interaction,'' <em>Month. Not. Roy. Soc.</em><b>&nbsp;</b>
+de Val-Borro, M. and 22 coauthors (incl. Lyra, W.): 2006, &quot;A comparative study of disc-planet interaction&quot;, <em>Month. Not. Roy. Soc.</em>
 (<a href="http://arXiv.org/abs/astro-ph/0605237"><i>astro-ph/0605237</i></a>)
+</li>
+
+<li class="separated">
+Bourdin, Ph.-A., Bingert, S., &amp; Peter, H.: 2013, &quot;Observationally driven 3D magnetohydrodynamics model of the solar corona above an active region&quot; <em>A&amp;A </em><b>555</b>, A123
+(PDF: <a href="http://arXiv.org/abs/1305.5693"><i>arXiv:1305.5693</i></a>,
+<a href="http://adsabs.harvard.edu/abs/2013A%26A...555A.123B">ADS</a>)
 </li>
 
 </ul>
