d3eea8382976c21dbdfbe3dff4b9ee12fb348777
Philippe Bourdin <git@Bourdin.ch>
2014-06-10 09:50:58 +0000

    Small correction of HTML code.

--- doc/www/references.php
+++ doc/www/references.php
@@ -22,14 +22,14 @@ Brandenburg, A.: 2003, &quot;Computational aspects of astrophysical MHD and turb
 </li>
 
 <li class="separated">
-Haugen, N. E. L., Brandenburg, A., &amp; Dobler, W.: 2004, &quot;Simulations of nonhelical hydromagnetic turbulence&quot;, <em>Phys. Rev. </em><b>E 70</b>, 016308, 1-14
+Haugen, N. E. L., Brandenburg, A., &amp; Dobler, W.: 2004, &quot;Simulations of nonhelical hydromagnetic turbulence&quot;, <em>Phys. Rev.</em> <b>E 70</b>, 016308, 1-14
 (<a href="http://arXiv.org/abs/astro-ph/0307059"><i>astro-ph/0307059</i></a>,
 <a href="http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode=2004PhRvE..70a6308H&amp;amp;db_key=PHY&amp;amp;high=4080bca9ae20164">ADS</a>,
 <a href="http://www.nordita.org/~brandenb/own/Hau+Bran+Dobl04.pdf">PDF</a>)
 </li>
 
 <li class="separated">
-Dobler, W., Stix, M., &amp; Brandenburg, A.: 2006, &quot;Convection and magnetic field generation in fully convective spheres&quot;, <em>Astrophys. J. </em><b>638</b>, 336-347
+Dobler, W., Stix, M., &amp; Brandenburg, A.: 2006, &quot;Convection and magnetic field generation in fully convective spheres&quot;, <em>Astrophys. J.</em> <b>638</b>, 336-347
 (<a href="http://arXiv.org/abs/astro-ph/0410645"><i>astro-ph/0410645</i></a>,
 <a href="http://adsabs.harvard.edu/cgi-bin/nph-bib_query?bibcode=2006ApJ...638..336D&amp;amp;db_key=AST&amp;amp;data_type=HTML&amp;amp;format=&amp;amp;high=42689b430f26475">ADS</a>,
 <a href="http://www.nordita.org/~brandenb/own/Dobl+Stix+Bran06.pdf">PDF</a>)
@@ -41,7 +41,7 @@ de Val-Borro, M. and 22 coauthors (incl. Lyra, W.): 2006, &quot;A comparative st
 </li>
 
 <li class="separated">
-Bourdin, Ph.-A., Bingert, S., &amp; Peter, H.: 2013, &quot;Observationally driven 3D magnetohydrodynamics model of the solar corona above an active region&quot; <em>A&amp;A </em><b>555</b>, A123
+Bourdin, Ph.-A., Bingert, S., &amp; Peter, H.: 2013, &quot;Observationally driven 3D magnetohydrodynamics model of the solar corona above an active region&quot; <em>A&amp;A</em> <b>555</b>, A123
 (PDF: <a href="http://arXiv.org/abs/1305.5693"><i>arXiv:1305.5693</i></a>,
 <a href="http://adsabs.harvard.edu/abs/2013A%26A...555A.123B">ADS</a>)
 </li>
