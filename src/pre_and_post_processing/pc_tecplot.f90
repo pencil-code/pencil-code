703b6269c843cdf64526fae595a0062435502793
Philippe Bourdin <git@Bourdin.ch>
2014-07-10 14:34:30 +0000

    Reworked a bit: code formatting and support for distributed and collective varfiles.

--- src/pre_and_post_processing/pc_tecplot.f90
+++ src/pre_and_post_processing/pc_tecplot.f90
@@ -1,11 +1,12 @@
-! This is a tool to collect a distributed data cube and record them in tecplot
-! files. The files named "OutputN.dat"(N=0~iproc) means this tecplot format
-! data file is converted from var.dat in directory procN.
-! This file is based on the pc_collect.f90 writen by Bourdin.KIS.
+! This is a tool to collect distributed data file and convert them to a
+! tecplot file. The generated files are named "OutputN.dat"(N=[0...iproc]),
+! where each of them os created from var.dat in the /proc* directories.
+!
+! This file is derived from pc_collect.f90.
 !
 ! $Id$
 !***********************************************************************
-program pc_collect
+program pc_tecplot
 !
   use Cdata
   use Cparam, only: fnlen, intlen
@@ -30,9 +31,8 @@ program pc_collect
   real, dimension (mx,my,mz,mfarray) :: f
   real, dimension (:,:,:,:), allocatable :: gf
   real, dimension (mxgrid) :: gx, gy, gz
-  integer   ::  i, j, k
   logical :: ex
-  integer :: mvar_in, io_len, pz, alloc_err, start_pos, end_pos
+  integer :: mvar_in, io_len, px, py, pz, alloc_err, start_pos, end_pos
   integer(kind=8) :: rec_len, num_rec
   real :: t_sp, t_test   ! t in single precision for backwards compatibility
 !
@@ -81,8 +81,8 @@ program pc_collect
   call read_runpars()
 !
 !  Derived parameters (that may still be overwritten).
-!  [might better be put into another routine, possibly even in rparam or
-!  read_runpars]
+!  [might better be put into another routine, possibly even in read_startpars
+!  or read_runpars]
 !
   x0 = xyz0(1) ; y0 = xyz0(2) ; z0 = xyz0(3)
   Lx = Lxyz(1) ; Ly = Lxyz(2) ; Lz = Lxyz(3)
@@ -153,13 +153,15 @@ program pc_collect
   call initialize_modules(f, LSTARTING=.true.)
   lpencil_check_at_work = .false.
 !
-  if (IO_strategy == "collect_xy") then
+! Loop over processors
 !
-    gz = huge(1.0)
-    t_test = huge(1.0)
+  write (*,*) "IPZ-layer:"
+!
+  gz = huge(1.0)
+  t_test = huge(1.0)
 !
   do ipz = 0, nprocz-1
-    write (*,*) "IPZ-layer:"
+!
     write (*,*) ipz+1, " of ", nprocz
 !
     f = huge(1.0)
@@ -170,118 +172,181 @@ program pc_collect
     lfirst_proc_z = (ipz == 0)
     llast_proc_z = (ipz == nprocz-1)
 !
-! Take the shortcut, files are well prepared for direct combination
-! Set up directory names 'directory' and 'directory_snap'
+    if (IO_strategy == "collect_xy") then
+      ! Take the shortcut, files are well prepared for direct combination
 !
+      ! Set up directory names 'directory' and 'directory_snap'
       call directory_names()
 !
-! Read the data
-!
+      ! Read the data
       rec_len = int (mxgrid, kind=8) * int (mygrid, kind=8) * mz
       rec_len = rec_len * mvar_in * io_len
       open (lun_input, FILE=trim (directory_snap)//'/'//filename, access='direct', recl=rec_len, status='old')
       read (lun_input, rec=1) gf
       close (lun_input)
 !
+      ! Read additional information and check consistency of timestamp
+      rec_len = int (mxgrid, kind=8) * int (mygrid, kind=8)
+      num_rec = int (mz, kind=8) * int (mvar_in*sizeof_real(), kind=8)
+      open (lun_input, FILE=trim (directory_snap)//'/'//filename, FORM='unformatted', status='old')
+      call fseek_pos (lun_input, rec_len, num_rec, 0)
+      read (lun_input) t_sp
+      if (lroot) then
+        t_test = t_sp
+        read (lun_input) gx, gy, gz, dx, dy, dz
+      else
+        if (t_test /= t_sp) then
+          write (*,*) 'ERROR: '//trim(directory_snap)//'/'//trim(filename)//' IS INCONSISTENT: t=', t_sp
+          stop 1
+        endif
+      endif
+      close (lun_input)
+      t = t_sp
+!
 ! Write data in tecplot data format
 !
-      chproc=itoa(ipz)
-      open (lun_output, FILE=trim(directory_out)//'/'//'output'//chproc//'.dat', status='replace', FORM='FORMATTED')
-      write(lun_output,*) 'TITLE="Output"'
-      write(lun_output,*) 'variables="x","y","z","u","v","w","rho"'
-      if (dimensionality==2) then
-        write(lun_output,*) 'zone,','I=',mxgrid,',J=',mygrid,',K=',nz,',F=POINT'
-      elseif (dimensionality==3) then
-        write(lun_output,*) 'zone,','I=',mxgrid,',J=',mygrid,',K=',mzgrid,',F=POINT' 
-      endif
+      if (trim (filename) == 'var.dat') filename = 'var'
+      open (lun_output, FILE=trim (directory_out)//'/'//'output_'//trim (filename)//'.dat', status='replace', FORM='FORMATTED')
+      write (lun_output,*) 'TITLE="Output"'
+      write (lun_output,*) 'variables="x","y","z","u","v","w","rho"'
 !
-      if (dimensionality==2) then
-        start_pos = nghost + 1
-        end_pos = nghost + nz
-      elseif (dimensionality==3) then
-        start_pos = 1
-        end_pos = mz
-      endif
-      do k = start_pos, end_pos
-      do j=1,mygrid
-      do i=1,mxgrid
-          write (lun_output, 101) x(i),y(j),z(k),gf(i,j,k,1),gf(i,j,k,2),gf(i,j,k,3),gf(i,j,k,4)
-101     format(1X,7e16.8)
+      start_pos = nghost + 1
+      end_pos = nghost + nz
+      if ((dimensionality >= 3) .and. lfirst_proc_z) start_pos = 1
+      if ((dimensionality >= 3) .and. llast_proc_z) end_pos = mz
+      write (lun_output,*) 'zone,','I=',mxgrid,',J=',mygrid,',K=',mzgrid-2*(start_pos-1),',F=POINT'
+      do pz = start_pos, end_pos
+        do py = 1, mygrid
+          do px = 1, mxgrid
+            write (lun_output, '(1X,7e16.8)') x(px), y(py), z(pz), gf(i,j,pz,1:mvar_io)
+          enddo
         enddo
       enddo
-      enddo
-      close (lun_output)
-  enddo
-  endif
 !
-  if (IO_strategy == "dist") then
+      ! That is all we have to do
+      cycle
+    endif
 !
-  do ipz = 0, nprocz-1
-  do ipy = 0, nprocy-1
-  do ipx = 0, nprocx-1
-!
-    iproc = ipx + ipy * nprocx + ipz * nprocx*nprocy
-    write (*,*) "now reading",iproc
-    lroot = (iproc==root)
     gx = huge(1.0)
     gy = huge(1.0)
-    f  = huge(1.0)
-    gf = huge(1.0)
 !
-!  Set up directory names `directory' and `directory_snap'.
+    do ipy = 0, nprocy-1
+      do ipx = 0, nprocx-1
 !
-    call directory_names()
+        iproc = ipx + ipy * nprocx + ipz * nprocx*nprocy
+        lroot = (iproc==root)
+!
+!  Set up flags for leading processors in each possible direction and plane
+!
+        lfirst_proc_x = (ipx == 0)
+        lfirst_proc_y = (ipy == 0)
+        lfirst_proc_xy = lfirst_proc_x .and. lfirst_proc_y
+        lfirst_proc_yz = lfirst_proc_y .and. lfirst_proc_z
+        lfirst_proc_xz = lfirst_proc_x .and. lfirst_proc_z
+        lfirst_proc_xyz = lfirst_proc_x .and. lfirst_proc_y .and. lfirst_proc_z
+!
+!  Set up flags for trailing processors in each possible direction and plane
+!
+        llast_proc_x = (ipx == nprocx-1)
+        llast_proc_y = (ipy == nprocy-1)
+        llast_proc_xy = llast_proc_x .and. llast_proc_y
+        llast_proc_yz = llast_proc_y .and. llast_proc_z
+        llast_proc_xz = llast_proc_x .and. llast_proc_z
+        llast_proc_xyz = llast_proc_x .and. llast_proc_y .and. llast_proc_z
+!
+!  Set up directory names 'directory' and 'directory_snap'.
+!
+        call directory_names()
 !
 !  Read coordinates.
 !
-    if (ip<=6.and.lroot) print*, 'reading grid coordinates'
-    call rgrid ('grid.dat')
+        if ((ip<=6) .and. lroot) print*, 'reading grid coordinates'
+        call rgrid ('grid.dat')
 !
 ! Size of box at local processor. The if-statement is for
 ! backward compatibility.
 !
-    if (all(lequidist)) then
-      Lxyz_loc(1)=Lxyz(1)/nprocx
-      Lxyz_loc(2)=Lxyz(2)/nprocy
-      Lxyz_loc(3)=Lxyz(3)/nprocz
-      xyz0_loc(1)=xyz0(1)+ipx*Lxyz_loc(1)
-      xyz0_loc(2)=xyz0(2)+ipy*Lxyz_loc(2)
-      xyz0_loc(3)=xyz0(3)+ipz*Lxyz_loc(3)
-      xyz1_loc(1)=xyz0_loc(1)+Lxyz_loc(1)
-      xyz1_loc(2)=xyz0_loc(2)+Lxyz_loc(2)
-      xyz1_loc(3)=xyz0_loc(3)+Lxyz_loc(3)
-    else
-      xyz0_loc(1)=x(l1)
-      xyz0_loc(2)=y(m1)
-      xyz0_loc(3)=z(n1)
-      xyz1_loc(1)=x(l2)
-      xyz1_loc(2)=y(m2)
-      xyz1_loc(3)=z(n2)
-      Lxyz_loc(1)=xyz1_loc(1) - xyz0_loc(1)
-      Lxyz_loc(2)=xyz1_loc(2) - xyz0_loc(3)
-      Lxyz_loc(3)=xyz1_loc(3) - xyz0_loc(3)
-    endif
+        if (all(lequidist)) then
+          Lxyz_loc(1) = Lxyz(1) / nprocx
+          Lxyz_loc(2) = Lxyz(2) / nprocy
+          Lxyz_loc(3) = Lxyz(3) / nprocz
+          xyz0_loc(1) = xyz0(1) + ipx*Lxyz_loc(1)
+          xyz0_loc(2) = xyz0(2) + ipy*Lxyz_loc(2)
+          xyz0_loc(3) = xyz0(3) + ipz*Lxyz_loc(3)
+          xyz1_loc(1) = xyz0_loc(1) + Lxyz_loc(1)
+          xyz1_loc(2) = xyz0_loc(2) + Lxyz_loc(2)
+          xyz1_loc(3) = xyz0_loc(3) + Lxyz_loc(3)
+        else
+          xyz0_loc(1) = x(l1)
+          xyz0_loc(2) = y(m1)
+          xyz0_loc(3) = z(n1)
+          xyz1_loc(1) = x(l2)
+          xyz1_loc(2) = y(m2)
+          xyz1_loc(3) = z(n2)
+          Lxyz_loc(1) = xyz1_loc(1) - xyz0_loc(1)
+          Lxyz_loc(2) = xyz1_loc(2) - xyz0_loc(3)
+          Lxyz_loc(3) = xyz1_loc(3) - xyz0_loc(3)
+        endif
 !
 !  Need to re-initialize the local grid for each processor.
 !
-    call initialize_grid()
+        call initialize_grid()
 !
 !  Read data.
 !  Snapshot data are saved in the tmp subdirectory.
 !  This directory must exist, but may be linked to another disk.
 !
-    call rsnap (filename, f(:,:,:,1:mvar_in), mvar_in)
-    t_sp = t
+        call rsnap (filename, f(:,:,:,1:mvar_in), mvar_in)
+        t_sp = t
 !
-    if (lroot) t_test = t_sp
-    if (t_test /= t_sp) then
-      write (*,*) 'ERROR: '//trim(directory_snap)//'/'//trim(filename)//' IS INCONSISTENT: t=', t_sp
-      stop 1
-    endif
+        if (lroot) t_test = t_sp
+        if (t_test /= t_sp) then
+          write (*,*) 'ERROR: '//trim(directory_snap)//'/'//trim(filename)//' IS INCONSISTENT: t=', t_sp
+          stop 1
+        endif
 !
-! collect f in gf:
+        ! collect f in gf:
+        gf(1+ipx*nx:mx+ipx*nx,1+ipy*ny:my+ipy*ny,:,:) = f(:,:,:,1:mvar_io)
 !
-    gf(1+ipx*nx:mx+ipx*nx,1+ipy*ny:my+ipy*ny,1+ipz*nz:mz+ipz*nz,:) = f(:,:,:,1:mvar_io)
+        ! collect x coordinates:
+        gx(1+ipx*nx:mx+ipx*nx) = x
+        gdx_1(1+ipx*nx:mx+ipx*nx) = dx_1
+        gdx_tilde(1+ipx*nx:mx+ipx*nx) = dx_tilde
+!
+        ! collect y coordinates:
+        gy(1+ipy*ny:my+ipy*ny) = y
+        gdy_1(1+ipy*ny:my+ipy*ny) = dy_1
+        gdy_tilde(1+ipy*ny:my+ipy*ny) = dy_tilde
+!
+      enddo
+    enddo
+!
+    ! collect z coordinates:
+    gz(1+ipz*nz:mz+ipz*nz) = z
+    gdz_1(1+ipz*nz:mz+ipz*nz) = dz_1
+    gdz_tilde(1+ipz*nz:mz+ipz*nz) = dz_tilde
+!
+! Write data in tecplot data format
+!
+    chproc = itoa (ipz)
+    open (lun_output, FILE=trim (directory_out)//'/'//'output'//chproc//'.dat', status='replace', FORM='FORMATTED')
+    write (lun_output,*) 'TITLE="Output"'
+    write (lun_output,*) 'variables="x","y","z","u","v","w","rho"'
+!
+    start_pos = nghost + 1
+    end_pos = nghost + nz
+    if ((dimensionality >= 3) .and. lfirst_proc_z) start_pos = 1
+    if ((dimensionality >= 3) .and. llast_proc_z) end_pos = mz
+    write (lun_output,*) 'zone,','I=',mxgrid,',J=',mygrid,',K=',mzgrid-2*(start_pos-1),',F=POINT'
+    do pz = start_pos, end_pos
+      do py = 1, mygrid
+        do px = 1, mxgrid
+          write (lun_output, '(1X,7e16.8)') x(px), y(py), z(pz), gf(i,j,pz,1:mvar_io)
+        enddo
+      enddo
+    enddo
+!
+! Write data in tecplot data format
 !
     chproc=itoa(iproc)
     open (lun_output, FILE=trim(directory_out)//'/'//'output'//chproc//'.dat', status='replace', FORM='FORMATTED')
@@ -302,12 +367,11 @@ program pc_collect
     endif
 !
     do k = start_pos, end_pos
-    do j = 1+ipy*ny, my+ipy*ny
-    do i = 1+ipx*nx, mx+ipx*nx
-      write (lun_output,101) x(i-ipx*nx),y(j-ipy*ny),z(k-ipz*nz),&
-        gf(i,j,k,1),gf(i,j,k,2),gf(i,j,k,3),gf(i,j,k,4)
-    enddo
-    enddo
+      do j = 1+ipy*ny, my+ipy*ny
+        do i = 1+ipx*nx, mx+ipx*nx
+          write (lun_output, '(1X,7e16.8)') x(i-ipx*nx), y(j-ipy*ny), z(k-ipz*nz), gf(i,j,k,1:mvar_io)
+        enddo
+      enddo
     enddo
     close (lun_output)
   enddo
@@ -327,4 +391,4 @@ program pc_collect
   call fnames_clean_up()
   call vnames_clean_up()
 !
-endprogram pc_collect
+endprogram pc_tecplot
