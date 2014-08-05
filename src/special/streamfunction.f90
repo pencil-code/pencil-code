6f3438aec0ca396a591ae61f851f7589ac20ef7c
Wladimir Lyra <wladimir.lyra@gmail.com>
2014-08-05 01:02:24 +0000

    Flesh out the ice convection code with over-relaxation.

--- src/special/streamfunction.f90
+++ src/special/streamfunction.f90
@@ -31,7 +31,20 @@ module Special
 !  Code constants
 !
   real :: amplpsi=1d-5   !amplitude of initial perturbation for psi
-  real :: dummy=0.
+  real :: tolerance=1d-3
+  real :: alpha_sor=impossible
+!
+!  Stuff for calculating the Rayleigh number
+!
+  real ::  gravity_z = 1.3 ! gravity in m/s^2
+  real ::  rho0 = 917.     ! density in Kg/m^3
+  real ::  alpha = 1.65d-4 ! Thermal expansivity in K^-1
+  real ::  kappa = 1d-6    ! Thermal diffusivity in m^2/s
+  real ::  cp = 2000.      ! Specific heat in J Kg^-1 K^-1
+  real ::  T_m = 270.      ! Melting tempertature in K
+  real ::  eta_0 = 1d13    ! Viscosity at melting temperature in Pa s
+  real ::  Avisc = 4.00    ! constant
+  real ::  dslab_km = 20.  ! Ice shell thikness in km
 !
 !  These are the needed internal "pencils".
 !
@@ -41,16 +54,16 @@ module Special
   endtype InternalPencils
 !
   type (InternalPencils) :: q
-
-  real, dimension(nx) :: pencil_dummy
-!
-!  Shortcuts
 !
   character (len=labellen), dimension(ninit) :: initpsi='nothing'
 !
-  namelist /special_init_pars/ amplpsi
+  logical :: lprint_residual=.false.
 !
-  namelist /special_run_pars/ dummy
+  namelist /special_init_pars/ amplpsi,alpha_sor,Avisc,lprint_residual,tolerance
+!
+  namelist /special_run_pars/ amplpsi,alpha_sor,Avisc,lprint_residual,tolerance
+!
+  real :: Ra ! Rayleigh number
 !
   !real, dimension(:,:), allocatable :: dummy_table
 !
@@ -111,7 +124,12 @@ module Special
       real, dimension (mx,my,mz,mfarray) :: f
       logical :: lstarting
 !
-      if (.not.lstarting) call init_special(f,lstarting)
+      if (Lxyz(1)/nxgrid .ne. Lxyz(3)/nzgrid) then 
+        call fatal_error("initialize_special","dx ne dz")
+      endif
+!
+      if (alpha_sor == impossible) &
+           alpha_sor= 2./(1+pi/nxgrid)
 !
     endsubroutine initialize_special
 !***********************************************************************
@@ -123,11 +141,25 @@ module Special
 !  01-aug-11/wlad: adapted
 !
       use Initcond, only: gaunoise
+      !use General, only: random_number_wrapper
 !
       real, dimension (mx,my,mz,mfarray) :: f
       logical, optional :: lstarting_in
+      real :: dslab,delta_T
+      real, dimension (nx) :: r
+!
+!  Pre-calculate Rayleigh number
+!
+      delta_T=f(lpoint,mpoint,n1,iTT)-f(lpoint,mpoint,n2,iTT) !Tbot-Tsurf
+      dslab = dslab_km*1d3
+!
+      Ra = (gravity_z*rho0*alpha*delta_T*dslab**3)/(kappa*eta_0)
+!
+      if (lroot) print*,'Rayleigh number=',Ra
 !
       call gaunoise(amplpsi,f,ipsi)
+!
+      call solve_for_psi(f)
 !
     endsubroutine init_special
 !***********************************************************************
@@ -197,18 +229,187 @@ module Special
 !***********************************************************************
     subroutine solve_for_psi(f)
 !
-      use Initcond, only: gaunoise
-!
       real, dimension (mx,my,mz,mfarray) :: f   
+      real, dimension (mx,mz) :: psi,eta
+      integer :: icount
+      real :: residual,rms_psi
+!
+!  Initial residual
+!
+      residual=1e33
+!
+!  Start counter
 !      
-      !do n=n1,n2
-      !do m=m1,m2
-      !f(l1:l2,m,n,ipsi)=call gaunoise(ampluu(j),f,iux,iuz)
-      !enddo
-      !enddo
-      call gaunoise(1d-5,f,ipsi)
+      icount=0
+!
+      psi(l1:l2,n1:n2)=f(l1:l2,mpoint,n1:n2,ipsi)
+      call update_bounds_psi(psi)
+!
+      call get_rms_psi(psi,rms_psi)
+      call calc_viscosity(f,eta)
+!
+      do while (residual > tolerance*rms_psi)
+!
+!  Calculate psi via SOR
+!
+        call successive_over_relaxation(f,psi,eta,residual)
+        call update_bounds_psi(psi)
+!
+!  Calculate the rms of psi to check convergence. 
+!
+        call get_rms_psi(psi,rms_psi)
+!
+! Increase counter. 
+!
+        icount=icount+1
+        if (lprint_residual) &
+             print*, icount,residual,rms_psi
+!
+        if (icount >= maxit) &
+             call fatal_error("solve_for_psi","reached limit of iterations: did not converge.")
+!
+        !residual=0.
+      enddo
+!
+      do m=m1,m2
+        f(:,m,:,ipsi)=psi
+      enddo
 !
     endsubroutine solve_for_psi
+!***********************************************************************
+    subroutine successive_over_relaxation(f,psi,eta,residual)
+!
+      use Deriv, only: der
+!
+      real, dimension (mx,my,mz,mfarray) :: f   
+      real, dimension (mx,mz), intent(in) :: eta
+      real, dimension (mx,mz) :: psi,psi_old
+      real, dimension (nx,nz) :: tmp
+      real, dimension (nx) :: aa, bb, cc, u, aout
+      real :: v_1, v_2, v_3, v_4, v_5, v
+      real, dimension (nx) :: dx1, dTTdx
+      real :: v51,v52,v53,ddx,psi_ast,dpsi
+      real, intent(out) :: residual
+      integer :: i,inghost
+!
+      psi_old=psi
+!
+      do n=n1,n2
+        do m=m1,m2
+          dx1=dx_1(l1:l2)
+!
+          call f_function(eta,aout) ; aa =      aout/eta(l1:l2,n)
+          call g_function(eta,aout) ; bb =      aout/eta(l1:l2,n)
+          call der(f,iTT,dTTdx,1)   ; cc = -Ra*dTTdx/eta(l1:l2,n)
+          u = 20.*dx1**4 + 4.*bb*dx1**2
+!
+          do i=l1,l2
+            inghost=i-l1+1
+            ddx=dx1(inghost)
+            v_1 = ( psi(i,n+1) +    psi(i,n-1) -    psi(i+1,n) - psi(i-1,n)) * ddx**2
+            v_2 = ( psi(i,n+2) - 4.*psi(i,n+1) - 4.*psi(i,n-1) + psi(i,n-2)) * ddx**4
+            v_3 = ( psi(i+2,n) - 4.*psi(i+1,n) - 4.*psi(i-1,n) + psi(i-2,n)) * ddx**4
+            v_4 = (-psi(i-1,n) -    psi(i,n-1) +    psi(i-1,n-1)) * ddx**2
+!
+            v51 = psi(i+1,n+1) - 2.*psi(i,n+1) + psi(i-1,n+1)
+            v52 = psi(i+1,n  )                 + psi(i-1,n  )
+            v53 = psi(i+1,n-1) - 2.*psi(i,n-1) + psi(i-1,n-1)
+            v_5 = (v51 - 2.*v52 + v53) * ddx**4
+!
+            v = aa(inghost)*v_1 + v_2 + v_3 + 4.*bb(inghost)*v_4 + 2.*v_5
+!
+            psi_ast=(cc(inghost)-v)/u(inghost)
+!
+            dpsi = psi_ast - psi_old(i,n)
+            psi(i,n) = alpha_sor*dpsi + psi_old(i,n)
+          enddo
+!
+          !if (n==n2-1) then 
+          !  print*,v_1!(psi_old(l1:l2,n-1) - psi_old(l1+1:l2+1,n) - psi_old(l1-1:l2-1,n))*ddx**2
+          !  stop
+          !endif
+!
+        enddo
+      enddo
+!
+!  Psi updated. Now prepare for next iteration. Calculate residual.
+!
+      tmp=(psi(l1:l2,n1:n2) - psi_old(l1:l2,n1:n2))**2
+      residual = sqrt(sum(tmp))/(nxgrid*nzgrid)
+!
+    endsubroutine successive_over_relaxation
+!***********************************************************************
+    subroutine calc_viscosity(f,eta)
+!
+      real, dimension (mx,my,mz,mfarray) :: f
+      real, dimension (mx,mz), intent(out) :: eta
+!
+      eta = eta_0 * exp(Avisc * (T_m/f(:,mpoint,:,iTT) - 1.))
+!
+    endsubroutine calc_viscosity
+!***********************************************************************
+    subroutine f_function(a,aout)
+!
+!  Calculate F = dz2 - dx2
+!
+      real, dimension(mx,mz) :: a
+      real, dimension(nx) :: ax,az,aout
+!
+      ax = (a(l1+1:l2+1,n  ) - 2.*a(l1:l2,n) + a(l1-1:l2-1,n  )) * dx_1(l1:l2)**2
+      az = (a(l1  :l2  ,n+1) - 2.*a(l1:l2,n) + a(l1  :l2  ,n-1)) * dz_1(  n  )**2
+!
+      aout = az-ax
+!
+    endsubroutine f_function
+!***********************************************************************
+    subroutine g_function(a,aout)
+!
+!   Calculate G = dzdx
+!
+      real, dimension (mx,mz) :: a
+      real, dimension (nx) :: aout
+!
+      aout = ( (a(l1  :l2  ,n) - a(l1  :l2  ,n-1)) - &
+               (a(l1-1:l2-1,n) - a(l1-1:l2-1,n-1)) ) * dx_1(l1:l2)**2
+!
+    endsubroutine g_function
+!***********************************************************************
+    subroutine get_rms_psi(psi,rms_psi)
+!
+      real, dimension(mx,mz) :: psi
+      real, dimension(nx,nz) :: tmp
+      real :: rms_psi
+!
+      tmp=psi(l1:l2,n1:n2)**2
+      rms_psi = sqrt(sum(tmp))/(nx*nz)
+!
+    endsubroutine get_rms_psi
+!***********************************************************************
+    subroutine update_bounds_psi(psi)
+!
+      real, dimension(mx,mz) :: psi
+      integer :: i
+!
+!  Set boundary of psi - vertical, zero
+!
+      psi(:,n1)=0.
+      psi(:,n2)=0.
+!
+!  Zero also the second derivative
+!
+      do i=1,nghost
+        psi(:,n1-i) = -psi(:,n1+i)
+      enddo
+      do i=1,nghost
+        psi(:,n2+i) = -psi(:,n2-i)
+      enddo
+!
+!  Periodic in the lateral 
+!
+      psi(1   :l1-1,:) = psi(l2i:l2,:)
+      psi(l2+1:mx  ,:) = psi(l1:l1i,:)
+!      
+    endsubroutine update_bounds_psi
 !***********************************************************************
     subroutine special_calc_energy(f,df,p)
 !
