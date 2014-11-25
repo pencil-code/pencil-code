872c972c22fea616e892dba9fdb4cf00a167bcd3
Chao-Chin Yang <ccyang@unlv.edu>
2014-11-25 13:44:58 +0000

    Cosmetics.

--- PencilCode/find.py
+++ PencilCode/find.py
@@ -120,11 +120,12 @@ def time_average(datadir='./data', diagnostics=None, tmin=0, verbose=True):
             Directly print out the averages when True.
     """
     # Chao-Chin Yang, 2014-07-31
-
-    # Read the time series.
     from . import read
+    from collections import namedtuple
+    from numpy import sqrt
+    from scipy import integrate
+    # Read the time series.
     ts = read.time_series(datadir=datadir)
-
     # Check the time span.
     tmax = max(ts.t)
     if tmax <= tmin:
@@ -134,17 +135,13 @@ def time_average(datadir='./data', diagnostics=None, tmin=0, verbose=True):
         return None
     indices = ts.t >= tmin
     dtinv = 1 / (tmax - tmin)
-
     # Local function for conducting statistics.
     def stats(diag):
-        from scipy import integrate
-        from numpy import sqrt
         mean = dtinv * integrate.simps(ts[diag][indices], ts.t[indices])
         stddev = sqrt(dtinv * integrate.simps((ts[diag][indices] - mean)**2, ts.t[indices]))
         if verbose:
             print("<", diag, "> = ", mean, "+/-", stddev)
         return mean, stddev
-
     # Default diagnostics is all except it and t.
     if diagnostics is None:
         diagnostics = list(ts.dtype.names)
@@ -153,12 +150,10 @@ def time_average(datadir='./data', diagnostics=None, tmin=0, verbose=True):
                 diagnostics.remove(name)
             except ValueError:
                 pass
-
     # Find and return the time average and its standard deviation of each diagnostics.
     if type(diagnostics) is str:
         return stats(diagnostics)
     else:  # assuming diagnostics is iterable.
-        from collections import namedtuple
         Mean = namedtuple('Mean', diagnostics)
         StdDev = namedtuple('StandardDeviation', diagnostics)
         mean, stddev = zip(*(stats(diag) for diag in diagnostics))
