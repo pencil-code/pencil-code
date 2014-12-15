3594f01a49625debf8ee3cef22ec505393f1a237
Chao-Chin Yang <ccyang@astro.lu.se>
2014-12-15 17:51:21 +0000

    Generalize spline_coeff_clamped to spline_coeff.

--- src/general.f90
+++ src/general.f90
@@ -1456,40 +1456,61 @@ module General
 !
     endsubroutine spline
 !***********************************************************************
-    pure subroutine spline_coeff_clamped(y, yp1, yp2, b, c, d, err, msg)
+    pure subroutine spline_coeff(y, b, c, d, yp1, yp2, err, msg)
 !
-!  Calculates the coefficients of the cubic polynomials from the clamped
-!  spline interpolation.  The step size is assumed to be unity.  The
-!  polynomials are
+!  Calculates the coefficients of the cubic spline interpolants
 !
-!    S_i(x) = y_i + b_i (x - i) + c_i (x - i)^2 + d_i (x - i)^3,
+!    S_i(x) = y(i) + b(i)*(x-i) + c(i)*(x-i)^2 + d(i)*(x-i)^3,
 !
-!  for x in [i, i+1] and i = 1, 2, ..., n-1, which satisfies
+!  for x in [i, i+1] and with S_i(i) = y(i) and S_i(i+1) = y(i+1), where
+!  i = 1, 2, ..., n-1.
 !
-!    S'(1) = yp1 and S'(n) = yp2.
+!  If yp1 is present, S'(1) = yp1; S''(1) = 0, otherwise.
+!  If yp2 is present, S'(n) = yp2; S''(n) = 0, otherwise.
 !
-!  11-dec-14/ccyang: coded.
+!  15-dec-14/ccyang: coded.
 !
       real, dimension(:), intent(in) :: y
-      real, intent(in) :: yp1, yp2
       real, dimension(size(y)), intent(out) :: b, c, d
+      real, intent(in), optional :: yp1, yp2
       character(len=*), intent(out), optional :: msg
       logical, intent(out), optional :: err
 !
       real, parameter :: onethird = 1.0 / 3.0
-      real, dimension(size(y)) :: r
-      character(len=72) :: msg1
+      real, dimension(size(y)) :: p, q, r
+      character(len=linelen) :: msg1
       logical :: err1
       integer :: n
 !
 !  Solve the tridiagonal system for coefficient c's.
 !
       n = size(y)
-      r(1) = y(2) - y(1) - yp1
+      p = 4.0
+      q = 1.0
       r(2:n-1) = y(3:n) - 2.0 * y(2:n-1) + y(1:n-2)
-      r(n) = yp2 - y(n) + y(n-1)
+!
+!     Left boundary condition.
+!
+      left: if (present(yp1)) then
+        p(1) = 2.0
+        r(1) = y(2) - y(1) - yp1
+      else left
+        q(1) = 0.0
+        r(1) = 0.0
+      endif left
+!
+!     Right boundary condition
+!
+      right: if (present(yp2)) then
+        p(n) = 2.0
+        r(n) = yp2 - y(n) + y(n-1)
+      else right
+        q(n) = 0.0
+        r(n) = 0.0
+      endif right
+!
       r = 3.0 * r
-      call tridag(spread(1.0, 1, n), (/2.0, spread(4.0, 1, n-2), 2.0/), spread(1.0, 1, n), r, c, err1, msg1)
+      call tridag(q, p, q, r, c, err1, msg1)
       if (present(err)) err = err1
       if (present(msg)) msg = msg1
 !
@@ -1498,7 +1519,7 @@ module General
       b(1:n-1) = y(2:n) - y(1:n-1) - onethird * (c(2:n) + 2.0 * c(1:n-1))
       d(1:n-1) = onethird * (c(2:n) - c(1:n-1))
 !
-    endsubroutine spline_coeff_clamped
+    endsubroutine spline_coeff
 !***********************************************************************
     subroutine spline_coeff_periodic(y, b, c, d)
 !
