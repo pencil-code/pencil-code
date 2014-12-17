eda4a07b2fa0196aaa22fcb1cb59f7c5bd933a0e
Matthias Rheinhardt <mreinhardt@nordita.org>
2014-12-17 20:43:46 +0000

    MR: removed the 'illegal instruction' bug by trial and error

--- src/pre_and_post_processing/pc_collect_exp.f90
+++ src/pre_and_post_processing/pc_collect_exp.f90
@@ -12,7 +12,7 @@ program pc_collect
   use IO
   use Messages
   use Param_IO
-  use Register
+  !!use Register
   use Snapshot
   !!use Sub
   !!use Syscalls, only: sizeof_real
@@ -22,7 +22,6 @@ program pc_collect
   character (len=fnlen) :: filename
   character (len=*), parameter :: directory_out = 'data/allprocs'
 !
-  real, dimension (mx,my,mz,mfarray) :: f
   real, dimension (:,:,:,:), allocatable :: gf
   real, dimension (mxgrid) :: gx, gdx_1, gdx_tilde
   real, dimension (mygrid) :: gy, gdy_1, gdy_tilde
@@ -35,18 +34,6 @@ program pc_collect
   lstart = .true.
   lmpicomm = .false.
   root = 0
-  lroot = .true.
-  ipx = 0
-  ipy = 0
-  ipz = 0
-  ylneigh = 0
-  zlneigh = 0
-  yuneigh = 0
-  zuneigh = 0
-  llcorn = 0
-  lucorn = 0
-  uucorn = 0
-  ulcorn = 0
 !
   inquire (IOLENGTH=io_len) 1.0
 !
@@ -55,17 +42,12 @@ program pc_collect
   if (IO_strategy == "MPI-IO") call fatal_error ('pc_collect', &
       "Snapshots are already collected, when using the MPI-IO module.")
 !
-  write (*,*) 'Please enter the filename to convert (eg. var.dat, VAR1, ...):'
+  write(*,*) 'Please enter the filename to convert (eg. var.dat, VAR1, ...):'
   read (*,*) filename
 !
 !  Identify version.
 !
-  if (lroot) call svn_id( &
-      '$Id: pc_collect.f90 22542 2014-11-18 22:51:03Z Bourdin.KIS $')
-!
-!  Initialize the message subsystem, eg. color setting etc.
-!
-  call initialize_messages()
+   call svn_id('$Id: pc_collect.f90 22542 2014-11-18 22:51:03Z Bourdin.KIS $')
 !
 !  Read parameters from start.x (default values; may be overwritten by
 !  read_runpars).
@@ -76,38 +58,17 @@ program pc_collect
 !
   call read_runpars()
 !
-!  Derived parameters (that may still be overwritten).
-!  [might better be put into another routine, possibly even in read_startpars
-!  or read_runpars]
-!
-  x0 = xyz0(1)
-  y0 = xyz0(2)
-  z0 = xyz0(3)
-  Lx = Lxyz(1)
-  Ly = Lxyz(2)
-  Lz = Lxyz(3)
-!
 ! Calculate dimensionality
 !
   dimensionality = min(nxgrid-1,1) + min(nygrid-1,1) + min(nzgrid-1,1)
 !
-!  Register physics modules.
-!
-  !!!call register_modules()
-!
-!  Define the lenergy logical
-!
-  lenergy = lentropy .or. ltemperature .or. lthermal_energy
-!
   if (lwrite_aux .and. .not. lread_aux) then
-    if (lroot) then
-      print *, ''
-      print *, 'lwrite_aux=T but lread_aux=F'
-      print *, 'The code will write the auxiliary variables to allprocs/VARN'
-      print *, ' without having read them from proc*/VARN'
-      print *, ''
-      call fatal_error("pc_collect","Stop and check")
-    endif
+    print *, ''
+    print *, 'lwrite_aux=T but lread_aux=F'
+    print *, 'The code will write the auxiliary variables to allprocs/VARN'
+    print *, ' without having read them from proc*/VARN'
+    print *, ''
+    call fatal_error("pc_collect","Stop and check")
   endif
 !
 !  Will we write all slots of f?
@@ -129,17 +90,16 @@ program pc_collect
     mvar_in=mvar
   endif
 !
-  allocate (gf (mxgrid,mygrid,mz,mvar_io), stat=alloc_err)
+  allocate(gf(mxgrid,mygrid,mz,mvar_io), stat=alloc_err)
   if (alloc_err /= 0) call fatal_error ('pc_collect', 'Failed to allocate memory for gf.', .true.)
 !
 !  Print resolution and dimension of the simulation.
 !
-  if (lroot) write (*,'(a,i1,a)') ' This is a ', dimensionality, '-D run'
-  if (lroot) print *, 'nxgrid, nygrid, nzgrid=', nxgrid, nygrid, nzgrid
-  if (lroot) print *, 'Lx, Ly, Lz=', Lxyz
-  if (lroot) print *, '      Vbox=', Lxyz(1)*Lxyz(2)*Lxyz(3)
+  write (*,'(a,i1,a)') ' This is a ', dimensionality, '-D run'
+  print *, 'nxgrid, nygrid, nzgrid=', nxgrid, nygrid, nzgrid
+  print *, 'Lx, Ly, Lz=', Lxyz
+  print *, '      Vbox=', Lxyz(1)*Lxyz(2)*Lxyz(3)
 !
-  iproc = 0
   call directory_names()
   inquire (file=trim(directory_dist)//'/'//filename, exist=ex)
   if (.not. ex) call fatal_error ('pc_collect', 'File not found: '//trim(directory_dist)//'/'//filename, .true.)
@@ -155,130 +115,91 @@ program pc_collect
 !
   write (*,'("IPZ-layer:")')
 !
-  gz = huge(1.0)
-  t_test = huge(1.0)
+  gf = 0.
 
   do ipz = 0, nprocz-1
 !
     write (*,*) ipz+1, " of ", nprocz
 !
-    f = huge(1.0)
-    gf = huge(1.0)
-!
-    iproc = ipz * nprocx*nprocy
-    lroot = (iproc==root)
     lfirst_proc_z = (ipz == 0)
-    llast_proc_z = (ipz == nprocz-1)
+    llast_proc_z  = (ipz == nprocz-1)
 !
     if (IO_strategy == "collect_xy") then
-      ! Take the shortcut, files are well prepared for direct combination
 !
-      ! Set up directory names 'directory' and 'directory_snap'
-      !!!call directory_names()
+      iproc = ipz * nprocx*nprocy
+      lroot = (iproc==root)
 !
-      ! Read the data
+! Take the shortcut, files are well prepared for direct combination
+!
+! Set up directory names 'directory' and 'directory_snap'
+!
+      call directory_names()
+!
+! Read the data
+!
       rec_len = int (mxgrid, kind=8) * int (mygrid, kind=8) * mz
       rec_len = rec_len * mvar_in * io_len
       open (lun_input, FILE=trim (directory_snap)//'/'//filename, access='direct', recl=rec_len, status='old')
       read (lun_input, rec=1) gf
       close (lun_input)
 !
-      ! Read additional information and check consistency of timestamp
-      !!rec_len = int (mxgrid, kind=8) * int (mygrid, kind=8)
-      !!num_rec = int (mz, kind=8) * int (mvar_in*sizeof_real(), kind=8)
+! Read additional information and check consistency of timestamp
+!      
       open (lun_input, FILE=trim (directory_snap)//'/'//filename, FORM='unformatted', status='old', position='append')
-      backspace(lun_input) 
-      !open (lun_input, FILE=trim (directory_snap)//'/'//filename, FORM='unformatted', status='old')
-      !call fseek_pos (lun_input, rec_len, num_rec, 0)
+      backspace(lun_input); backspace(lun_input)
       read (lun_input) t_sp
+!
       if (lroot) then
-print*, 't_test=',t_test
         t_test = t_sp
         read (lun_input) gx, gy, gz, dx, dy, dz
-      else
-        if (t_test /= t_sp) then
-          write (*,*) 'ERROR: '//trim(directory_snap)//'/'//trim(filename)//' IS INCONSISTENT: t=', t_sp
-          stop 1
-        endif
+      elseif (t_test /= t_sp) then
+        write (*,*) 'ERROR: '//trim(directory_snap)//'/'//trim(filename)//' IS INCONSISTENT: t=', t_sp
+        stop 1
       endif
       close (lun_input)
+
       t = t_sp
 !
-      ! Write xy-layer
+! Write xy-layer
+!
       do pa = 1, mvar_io
-        start_pos = nghost + 1
-        end_pos = nghost + nz
-        if (lfirst_proc_z) start_pos = 1
-        if (llast_proc_z) end_pos = mz
+!
+        if (lfirst_proc_z) then
+          start_pos = 1
+        else
+          start_pos = nghost + 1
+        endif
+        if (llast_proc_z) then
+          end_pos = mz
+        else
+          end_pos = nghost + nz
+        endif
+!
         do pz = start_pos, end_pos
           write (lun_output, rec=pz+ipz*nz+(pa-1)*mzgrid) gf(:,:,pz,pa)
         enddo
+!
       enddo
 !
-      ! That is all we have to do
+! That is all we have to do
+!
       cycle
     endif
-!
-    gx = huge(1.0)
-    gy = huge(1.0)
 !
     do ipy = 0, nprocy-1
       do ipx = 0, nprocx-1
 !
         iproc = ipx + ipy * nprocx + ipz * nprocx*nprocy
         lroot = (iproc==root)
-!
-!  Set up flags for leading processors in each possible direction and plane
-!
-        lfirst_proc_x = (ipx == 0)
-        lfirst_proc_y = (ipy == 0)
-        lfirst_proc_xy = lfirst_proc_x .and. lfirst_proc_y
-        lfirst_proc_yz = lfirst_proc_y .and. lfirst_proc_z
-        lfirst_proc_xz = lfirst_proc_x .and. lfirst_proc_z
-        lfirst_proc_xyz = lfirst_proc_x .and. lfirst_proc_y .and. lfirst_proc_z
-!
-!  Set up flags for trailing processors in each possible direction and plane
-!
-        llast_proc_x = (ipx == nprocx-1)
-        llast_proc_y = (ipy == nprocy-1)
-        llast_proc_xy = llast_proc_x .and. llast_proc_y
-        llast_proc_yz = llast_proc_y .and. llast_proc_z
-        llast_proc_xz = llast_proc_x .and. llast_proc_z
-        llast_proc_xyz = llast_proc_x .and. llast_proc_y .and. llast_proc_z
-!
-!  Set up directory names `directory' and `directory_snap'.
-!
-        !!!call directory_names()
+
+! Set up directory names 'directory' and 'directory_snap'
+
+        call directory_names
 !
 !  Read coordinates.
 !
         if (ip<=6.and.lroot) print*, 'reading grid coordinates'
-        call rgrid ('grid.dat')
-!
-! Size of box at local processor. The if-statement is for
-! backward compatibility.
-!
-        if (all(lequidist)) then
-          Lxyz_loc(1)=Lxyz(1)/nprocx
-          Lxyz_loc(2)=Lxyz(2)/nprocy
-          Lxyz_loc(3)=Lxyz(3)/nprocz
-          xyz0_loc(1)=xyz0(1)+ipx*Lxyz_loc(1)
-          xyz0_loc(2)=xyz0(2)+ipy*Lxyz_loc(2)
-          xyz0_loc(3)=xyz0(3)+ipz*Lxyz_loc(3)
-          xyz1_loc(1)=xyz0_loc(1)+Lxyz_loc(1)
-          xyz1_loc(2)=xyz0_loc(2)+Lxyz_loc(2)
-          xyz1_loc(3)=xyz0_loc(3)+Lxyz_loc(3)
-        else
-          xyz0_loc(1)=x(l1)
-          xyz0_loc(2)=y(m1)
-          xyz0_loc(3)=z(n1)
-          xyz1_loc(1)=x(l2)
-          xyz1_loc(2)=y(m2)
-          xyz1_loc(3)=z(n2)
-          Lxyz_loc(1)=xyz1_loc(1) - xyz0_loc(1)
-          Lxyz_loc(2)=xyz1_loc(2) - xyz0_loc(3)
-          Lxyz_loc(3)=xyz1_loc(3) - xyz0_loc(3)
-        endif
+        call rgrid('grid.dat')
 !
 !  Need to re-initialize the local grid for each processor.
 !
@@ -288,76 +209,81 @@ print*, 't_test=',t_test
 !  Snapshot data are saved in the tmp subdirectory.
 !  This directory must exist, but may be linked to another disk.
 !
-        call rsnap (filename, f(:,:,:,1:mvar_in), mvar_in)
+        call rsnap(filename, gf(1+ipx*nx:mx+ipx*nx,1+ipy*ny:my+ipy*ny,:,1:mvar_in), mvar_in)
         t_sp = t
 !
-        if (lroot) t_test = t_sp
-        if (t_test /= t_sp) then
+        if (lroot) then
+          t_test = t_sp
+        elseif (t_test /= t_sp) then
           write (*,*) 'ERROR: '//trim(directory_snap)//'/'//trim(filename)//' IS INCONSISTENT: t=', t_sp
           stop 1
         endif
 !
-        ! collect f in gf:
-        gf(1+ipx*nx:mx+ipx*nx,1+ipy*ny:my+ipy*ny,:,:) = f(:,:,:,1:mvar_io)
+! collect x coordinates:
 !
-        ! collect x coordinates:
-        gx(1+ipx*nx:mx+ipx*nx) = x
-        gdx_1(1+ipx*nx:mx+ipx*nx) = dx_1
+        gx       (1+ipx*nx:mx+ipx*nx) = x
+        gdx_1    (1+ipx*nx:mx+ipx*nx) = dx_1
         gdx_tilde(1+ipx*nx:mx+ipx*nx) = dx_tilde
 !
-        ! collect y coordinates:
-        gy(1+ipy*ny:my+ipy*ny) = y
-        gdy_1(1+ipy*ny:my+ipy*ny) = dy_1
+! collect y coordinates:
+!
+        gy       (1+ipy*ny:my+ipy*ny) = y
+        gdy_1    (1+ipy*ny:my+ipy*ny) = dy_1
         gdy_tilde(1+ipy*ny:my+ipy*ny) = dy_tilde
 !
       enddo
     enddo
 !
-    ! collect z coordinates:
-    gz(1+ipz*nz:mz+ipz*nz) = z
-    gdz_1(1+ipz*nz:mz+ipz*nz) = dz_1
+! collect z coordinates:
+!
+    gz       (1+ipz*nz:mz+ipz*nz) = z
+    gdz_1    (1+ipz*nz:mz+ipz*nz) = dz_1
     gdz_tilde(1+ipz*nz:mz+ipz*nz) = dz_tilde
 !
-    ! write xy-layer:
+! write xy-layer:
+!
     do pa = 1, mvar_io
-      start_pos = nghost + 1
-      end_pos = nghost + nz
-      if (lfirst_proc_z) start_pos = 1
-      if (llast_proc_z) end_pos = mz
+
+      if (lfirst_proc_z) then
+        start_pos = 1
+      else
+        start_pos = nghost + 1
+      endif
+
+      if (llast_proc_z ) then
+        end_pos = mz
+      else
+        end_pos = nghost + nz
+      endif
+
       do pz = start_pos, end_pos
-        write (lun_output, rec=pz+ipz*nz+(pa-1)*mzgrid) gf(:,:,pz,pa)
+        write(lun_output, rec=pz+ipz*nz+(pa-1)*mzgrid) gf(:,:,pz,pa)
       enddo
+
     enddo
+!
   enddo
 !
-  ! write additional data:
-  close (lun_output)
+! write additional data:
+!
+  close(lun_output)
   open (lun_output, FILE=trim(directory_out)//'/'//filename, FORM='unformatted', position='append', status='old')
-  t_sp = t
-  write (lun_output) t_sp, gx, gy, gz, dx, dy, dz
-  close (lun_output)
+  write(lun_output) t, gx, gy, gz, dx, dy, dz
+  close(lun_output)
 !
   if (IO_strategy == 'dist') then
-    ! write global grid:
+!
+! write global grid:
+!
     open (lun_output, FILE=trim(directory_out)//'/grid.dat', FORM='unformatted', status='replace')
-    write (lun_output) t_sp, gx, gy, gz, dx, dy, dz
-    write (lun_output) dx, dy, dz
-    write (lun_output) Lx, Ly, Lz
-    write (lun_output) gdx_1, gdy_1, gdz_1
-    write (lun_output) gdx_tilde, gdy_tilde, gdz_tilde
-    close (lun_output)
+    write(lun_output) t_sp, gx, gy, gz, dx, dy, dz
+    write(lun_output) dx, dy, dz
+    write(lun_output) Lxyz
+    write(lun_output) gdx_1, gdy_1, gdz_1
+    write(lun_output) gdx_tilde, gdy_tilde, gdz_tilde
+    close(lun_output)
   endif
 !
   print *, 'Writing snapshot for time t =', t
-!
-!  Give all modules the possibility to exit properly.
-!
-  !!!call finalize_modules (f)
-!
-!  Free any allocated memory.
-!
-  deallocate (gf)
-  !!!call fnames_clean_up()
-  !!!call vnames_clean_up()
 !
 endprogram pc_collect
