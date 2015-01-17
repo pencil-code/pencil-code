d61efbb5b897c121e0cbfeda7b55d67f642669cd
Wladimir Lyra <wladimir.lyra@gmail.com>
2015-01-17 10:05:19 +0000

    Add f90 and vim hooks for color

--- src/unit_loc.h
+++ src/unit_loc.h
@@ -1,3 +1,5 @@
+!  -*-f90-*-  (for emacs)    vim:set filetype=fortran:  (for vim)  
+!
 ! Needed for switching between reading input data from external or internal files.
 ! If you use the preprocessor, remove all ! and set the appropriate compiler flag for preprocessor invocation;
 ! define f2003 if you want reading from internal files.
