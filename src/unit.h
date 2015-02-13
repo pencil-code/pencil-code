03e2c45d9c388e373e01edaaf7ec3666c85df176
Matthias Rheinhardt <mreinhardt@nordita.org>
2015-02-13 18:45:30 +0000

    MR: modified comments

--- src/unit.h
+++ src/unit.h
@@ -4,7 +4,7 @@
 ! If you use the preprocessor, remove all ! and set the appropriate compiler flag for preprocessor invocation;
 ! define f2003 if you want reading from internal files.
 ! Without preprocessor use, comment out 'integer :: unit' for reading from internal files 
-! and 'character(LEN=*) :: unit' for reading from external files.
+! and 'character(LEN=*) :: unit' for reading from external files, respectively.
 ! In any case one of the two declarations must finally be present.
 ! 
 !#IFDEF f2003
--- src/unit_loc.h
+++ src/unit_loc.h
@@ -4,7 +4,7 @@
 ! If you use the preprocessor, remove all ! and set the appropriate compiler flag for preprocessor invocation;
 ! define f2003 if you want reading from internal files.
 ! Without preprocessor use, comment out 'integer, parameter :: unit=1' for reading from internal files 
-! and 'character(LEN=*), allocatable :: unit' for reading from external files.
+! and 'character(LEN=*), allocatable :: unit' for reading from external files, respectively.
 ! In any case one of the two declarations must finally be present.
 !
 !#IFDEF f2003
