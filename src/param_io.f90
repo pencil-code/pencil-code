ca58fc71b6f517c0705664e69e9f30c2dd13f560
Petri Käpylä <petri.kapyla@aalto.fi>
2015-04-10 11:04:16 +0000

    MR&PJK: Added lproper_averages in init_pars

--- src/param_io.f90
+++ src/param_io.f90
@@ -96,7 +96,7 @@ module Param_IO
       lequatorz, zequator, lav_smallx, xav_max, niter_poisson, &
       lforce_shear_bc,lread_from_other_prec, &
       pipe_func, glnCrossSec0, CrossSec_x1, CrossSec_x2, CrossSec_w,&
-      lcorotational_frame, rcorot, &
+      lcorotational_frame, rcorot, lproper_averages, &
       ldirect_access
 !
   namelist /run_pars/ &
