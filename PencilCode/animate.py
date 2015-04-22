b9cc205336c3e6f324abb47ec1d4809932477eaa
Chao-Chin Yang <ccyang@unlv.edu>
2015-04-22 16:07:12 +0000

    Add slices() as a dispatcher.

--- PencilCode/animate.py
+++ PencilCode/animate.py
@@ -7,3 +7,33 @@
 #
 # Chao-Chin Yang, 2015-04-22
 #=======================================================================
+def slices(field, datadir='./data', **kwarg):
+    """Animate the video slices.
+
+    Positional Argument:
+        field
+            Field name.
+
+    Keyword Arguments:
+        datadir
+            Data directory.
+        **kwarg
+            Keyword arguments passed to the respective animator.
+    """
+    # Chao-Chin Yang, 2015-04-22
+    from . import read
+    # Get the dimensions, parameters, and grid.
+    dim = read.dimensions(datadir=datadir)
+    par = read.parameters(datadir=datadir)
+    grid = read.grid(datadir=datadir, trim=True)
+    # Dispatch.
+    ndim = (dim.nxgrid > 1) + (dim.nygrid > 1) + (dim.nzgrid > 1)
+    if ndim == 1:
+        raise NotImplementedError("1D run")
+    elif ndim == 2:
+        raise NotImplementedError("2D run")
+    elif ndim == 3:
+        raise NotImplementedError("3D run")
+#=======================================================================
+###### Local Functions ######
+#=======================================================================
