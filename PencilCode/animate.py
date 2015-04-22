617122b7b2d6eae3629d6a88fc11c755b1c4e23e
Chao-Chin Yang <ccyang@unlv.edu>
2015-04-22 16:09:21 +0000

    Also read the slices in slices() before dispatching.

--- PencilCode/animate.py
+++ PencilCode/animate.py
@@ -22,6 +22,8 @@ def slices(field, datadir='./data', **kwarg):
     """
     # Chao-Chin Yang, 2015-04-22
     from . import read
+    # Read the slices.
+    t, s = read.slices(datadir=datadir)
     # Get the dimensions, parameters, and grid.
     dim = read.dimensions(datadir=datadir)
     par = read.parameters(datadir=datadir)
