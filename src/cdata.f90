401f07a4cf1a56f5f4cd55147e36794d316fadae
mrheinhardt <mreinhardt@nordita.org>
2015-06-09 14:16:44 +0000

    MR: new quantities Area_* for areas of coordinate surfaces

--- src/cdata.f90
+++ src/cdata.f90
@@ -102,6 +102,7 @@ module Cdata
   real :: r_int=0.,r_ext=impossible   ! for spherical shell problems
   real :: r_int_border=impossible,r_ext_border=impossible
   real :: r_ref=1.,rsmooth=0.,box_volume=1.0
+  real :: Area_xy=1., Area_yz=1., Area_xz=1.
   real, dimension(3) :: k1xyz=0.
 !
 !  Time integration parameters.
