121ed4cfdb99d0f62ce47306226f2990c3d972b8
Axel Brandenburg <brandenb@nordita.org>
2015-07-01 19:30:51 +0200

    Mikhail, please check how this affects our results +!---    call multsv_mn(p%rho2,p%fpres,baroclinic) +!AB: corrected by Patrick Adams +        dAdt = dAdt-battery_term*p%fpres

--- src/magnetic.f90
+++ src/magnetic.f90
@@ -1229,6 +1229,7 @@ module Magnetic
             'Timestep is currently only sensitive to fourth order.')
 !
 !  if meanfield theory is invoked, we need to tell the other routines
+!  eta is also needed with the chiral fluids procedure.
 !
         if (lmagn_mf .or. lspecial) &
           call put_shared_variable('eta',eta,caller='initialize_magnetic')
@@ -3548,8 +3549,9 @@ module Magnetic
 !
       if (battery_term/=0.0) then
         if (headtt) print*,'daa_dt: battery_term=',battery_term
-        call multsv_mn(p%rho1,p%fpres,baroclinic)
-        dAdt = dAdt+battery_term*baroclinic
+!---    call multsv_mn(p%rho2,p%fpres,baroclinic)
+!AB: corrected by Patrick Adams
+        dAdt = dAdt-battery_term*p%fpres
         if (headtt.or.ldebug) print*,'daa_dt: max(battery_term) =',&
             battery_term*maxval(baroclinic)
       endif
