6eee3de4848beaf80830115928ddbe2172fd28a2
Frederick Gent <fred.gent.ncl@gmail.com>
2015-07-10 17:08:08 +0300

    Commented out obsolete public functions

--- src/interstellar.h
+++ src/interstellar.h
@@ -8,11 +8,11 @@
   public :: read_interstellar_init_pars, write_interstellar_init_pars
   public :: read_interstellar_run_pars,  write_interstellar_run_pars
   public :: pencil_criteria_interstellar
-  public :: interstellar_before_boundary
+!  public :: interstellar_before_boundary
   public :: calc_heat_cool_interstellar, check_SN
-  public :: calc_snr_damping
-  public :: calc_snr_damp_int
-  public :: calc_snr_unshock
+!  public :: calc_snr_damping
+!  public :: calc_snr_damp_int
+!  public :: calc_snr_unshock
   public :: input_persistent_interstellar, output_persistent_interstellar
   public :: addmassflux
 
