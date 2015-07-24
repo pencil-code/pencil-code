e5fe8e2700206e143cbbff5abdcf7a67988447b5
andreas-schreiber <andreas.schreiber88@googlemail.com>
2015-07-24 14:18:36 +0200

    fixed merge

--- python/pencil/files/__init__.py
+++ python/pencil/files/__init__.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-__all__ = ["var","ts","dim","param","slices","xyaver","yzaver","zaver","power","pc2vtk","post_processing","streamlines","tracers","kf","fixed_points","read_pvar"]
-=======
 __all__ = ["var","ts","dim","param","slices","xyaver","yzaver","zaver","power","pc2vtk","post_processing","streamlines","tracers","kf","fixed_points","read_pvar","particles_removed"]
->>>>>>> cbf2f33425f56dde5c1116bad48e062cc5b9e329
