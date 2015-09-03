d5be85f1462f36ed66f3235306afe96d8e742cbd
mrheinhardt <mreinhardt@nordita.org>
2015-09-03 17:28:41 +0000

    MR: corrected for lopacity

--- src/noopacity.f90
+++ src/noopacity.f90
@@ -6,7 +6,7 @@
 ! Declare (for generation of cparam.inc) the number of f array
 ! variables and auxiliary variables added by this module
 !
-! CPARAM logical, parameter :: lopacity = .true.
+! CPARAM logical, parameter :: lopacity = .false.
 !
 ! MVAR CONTRIBUTION 0
 ! MAUX CONTRIBUTION 0
