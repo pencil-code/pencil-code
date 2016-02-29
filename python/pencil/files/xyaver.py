7f6c0be6c0f9ae0d3122688be32c1c485f8a9d6f
Iomsn <iomsn1@gmail.com>
2016-02-29 22:21:26 +0000

    xyaver.py Fixed bug in reading routine.

--- python/pencil/files/xyaver.py
+++ python/pencil/files/xyaver.py
@@ -34,7 +34,7 @@ def read_xyaver(varfile='xyaverages.dat',datadir='data/', quiet=0):
     infile.close()
     
     #gotta be a better way to do this...
-    n_lines = int(check_output('wc '+datadir+'/'+varfile).split()[0])
+    n_lines = int(check_output(["wc", datadir+'/'+varfile]).split()[0])
 
     datafile = open(datadir+'/'+varfile)    
     n_vars = len(variables)
