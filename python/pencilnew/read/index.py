198e4c5592f54f48a624553125f0e3de5826994a
Iomsn <iomsn1@gmail.com>
2016-05-23 18:15:14 +0100

    Finished cleaning up index.py.

--- python/pencilnew/read/index.py
+++ python/pencilnew/read/index.py
@@ -9,13 +9,28 @@ Contains the index information.
 
 
 def index(*args, **kwargs):
-    """Read index.pro and return an Index object.
-
-    Params:
-    ------
-    data_dir='data/' (optionnal)
     """
-    return Index(*args, **kwargs)
+    Read Pencil Code index data from index.pro.
+
+    call signature:
+
+    read(self, data_dir='data/', param=None, dim=None)
+
+    Keyword arguments:
+
+    *data_dir*:
+      Directory where the data is stored.
+
+    *param*
+      Parameter object.
+
+    *dim*
+      Dimension object.
+    """
+
+    index_tmp = Index()
+    index_tmp.read(*args, **kwargs)
+    return index_tmp
 
 
 class Index(object):
@@ -33,7 +48,7 @@ class Index(object):
 
     def read(self, data_dir='data/', param=None, dim=None):
         """
-        Read Pencil Code index data.
+        Read Pencil Code index data from index.pro.
 
         call signature:
 
@@ -41,67 +56,40 @@ class Index(object):
 
         Keyword arguments:
 
-        *file_name*:
-          Name of the time series file.
-
         *data_dir*:
           Directory where the data is stored.
 
-        *quiet*
-          Flag for switching of output.
+        *param*
+          Parameter object.
 
-        *comment_char*
-          Comment character in the time series file.
+        *dim*
+          Dimension object.
         """
 
-
-
-
-    """
-     read index.pro and returns a read_index class which is composed
-     of a python dictionnary
-
-     Author: T. Gastine (tgastine@ast.obs-mip.fr)
-    """
-    def __init__(self, data_dir='data/', param=None, dim=None):
-        """Constructor:
-         -----------
-
-         Params:
-         ------
-            data_dir='data/' (optionnal)
-
-         Returns:
-         -------
-            a read_index class
-        """
         import os
-        from pencil import read_param, read_dim
+        import pencilnew.read as read
         
         if param is None:
-            param = read_param(data_dir=data_dir, quiet=True)
+            param = read.param(data_dir=data_dir, quiet=True)
         if dim is None:
-            dim = read_dim(data_dir=data_dir)
+            dim = read.dim(data_dir=data_dir)
 
         if param.lwrite_aux:
-            totalvars = dim.mvar+dim.maux
+            totalvars = dim.mvar + dim.maux
         else:
             totalvars = dim.mvar
 
-        f = open(os.path.join(data_dir,'index.pro'))
+        f = open(os.path.join(data_dir, 'index.pro'))
         for line in f.readlines():
             clean = line.strip()
-            name=clean.split('=')[0].strip().replace('[','').replace(']','')
+            name = clean.split('=')[0].strip().replace('[', '').replace(']', '')
             if (clean.split('=')[1].strip().startswith('intarr(370)')):
                 continue
-            val=int(clean.split('=')[1].strip())
-            #            print name,val
-            # need to compare val to totalvars as global indices
-            # may be present in index.pro
-            #            if (val != 0 and val <= totalvars and \
+            val = int(clean.split('=')[1].strip())
+
             if (val != 0  and val <= totalvars \
                 and not name.startswith('i_') and name.startswith('i')):
-                name=name.lstrip('i')
+                name = name.lstrip('i')
                 if (name == 'lnTT' and param.ltemperature_nolog):
                     name = 'tt'
-                self[name] = val
+                setattr(self, name, val)
