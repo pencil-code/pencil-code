d28098a50f14b21157f25a3e546323c0d9c907cc
Iomsn <iomsn1@gmail.com>
2016-05-23 17:57:17 +0100

    Started objectification of the routine.

--- python/pencilnew/read/index.py
+++ python/pencilnew/read/index.py
@@ -1,6 +1,11 @@
-#$Id$
-import os
-from pencil import read_param, read_dim
+# index.py
+#
+# Read the index.pro file.
+#
+# Authors: S. Candelaresi (iomsn1@gmail.com), T. Gastine (tgastine@ast.obs-mip.fr).
+"""
+Contains the index information.
+"""
 
 
 def index(*args, **kwargs):
@@ -8,46 +13,87 @@ def index(*args, **kwargs):
 
     Params:
     ------
-    datadir='data/' (optionnal)
+    data_dir='data/' (optionnal)
     """
     return Index(*args, **kwargs)
 
 
-class Index(dict):
+class Index(object):
+    """
+    Index -- holds pencil code index data.
+    """
+
+    def __init__(self):
+        """
+        Fill members with default values.
+        """
+
+        self.keys = []
+
+
+    def read(self, data_dir='data/', param=None, dim=None):
+        """
+        Read Pencil Code index data.
+
+        call signature:
+
+        read(self, data_dir='data/', param=None, dim=None)
+
+        Keyword arguments:
+
+        *file_name*:
+          Name of the time series file.
+
+        *data_dir*:
+          Directory where the data is stored.
+
+        *quiet*
+          Flag for switching of output.
+
+        *comment_char*
+          Comment character in the time series file.
+        """
+
+
+
+
     """
      read index.pro and returns a read_index class which is composed
      of a python dictionnary
 
      Author: T. Gastine (tgastine@ast.obs-mip.fr)
     """
-    def __init__(self, datadir='data/', param=None, dim=None):
+    def __init__(self, data_dir='data/', param=None, dim=None):
         """Constructor:
          -----------
 
          Params:
          ------
-            datadir='data/' (optionnal)
+            data_dir='data/' (optionnal)
 
          Returns:
          -------
             a read_index class
         """
+        import os
+        from pencil import read_param, read_dim
+        
         if param is None:
-            param = read_param(datadir=datadir, quiet=True)
+            param = read_param(data_dir=data_dir, quiet=True)
         if dim is None:
-            dim = read_dim(datadir=datadir)
+            dim = read_dim(data_dir=data_dir)
 
         if param.lwrite_aux:
             totalvars = dim.mvar+dim.maux
         else:
             totalvars = dim.mvar
 
-        f = open(os.path.join(datadir,'index.pro'))
+        f = open(os.path.join(data_dir,'index.pro'))
         for line in f.readlines():
             clean = line.strip()
             name=clean.split('=')[0].strip().replace('[','').replace(']','')
             if (clean.split('=')[1].strip().startswith('intarr(370)')):
-	      continue
+                continue
             val=int(clean.split('=')[1].strip())
             #            print name,val
             # need to compare val to totalvars as global indices
