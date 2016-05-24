420f87dd53feb6204f1a7cbe624cc8f58e32cf84
Dhrubaditya MITRA <dhrubaditya@users.noreply.github.com>
2016-05-24 12:34:10 +0200

    checking in the header file

---
+++ src/particles_lyapunov.h
@@ -0,0 +1,10 @@
+!  -*-f90-*-  (for emacs)    vim:set filetype=fortran:  (for vim)
+  private
+
+  public :: register_particles_lyapunov 
+  public :: initialize_particles_lyapunov,init_particles_lyapunov
+  public :: read_plyapunov_init_pars, write_plyapunov_init_pars
+  public :: read_plyapunov_run_pars, write_plyapunov_run_pars
+  public :: particles_stochastic_lyapunov
+  public :: rprint_particles_lyapunov
+  public :: dlyapunov_dt,dlyapunov_dt_pencil
