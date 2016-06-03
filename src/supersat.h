972c3c0565fdee2d95a747e43d43143479abb1f5
Axel Brandenburg <brandenb@nordita.org>
2016-06-03 15:00:40 -0600

    I think this was needed to fix the auto-test

--- src/supersat.h
+++ src/supersat.h
@@ -6,9 +6,9 @@
 
   public :: read_supersat_init_pars, write_supersat_init_pars
   public :: read_supersat_run_pars,  write_supersat_run_pars
-  public :: rprint_supersat, get_slices_supersat
+  public :: rprint_supersat !, get_slices_supersat
   public :: init_lncc, dlncc_dt
   public :: pencil_criteria_supersat, pencil_interdep_supersat
-  public :: calc_pencils_supersat
-  public :: supersat_after_boundary
-  public :: calc_msupersat
+  !public :: calc_pencils_supersat
+  !public :: supersat_after_boundary
+  !public :: calc_msupersat
