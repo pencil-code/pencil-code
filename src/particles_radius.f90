b80a42647d26351cf4fcc5a8583632ee241cc1bf
alexrichert <ajr327@psu.edu>
2016-07-14 01:30:39 -0400

    particles_radius: added an MPVAR for rpbeta particle pencil

--- src/particles_radius.f90
+++ src/particles_radius.f90
@@ -7,7 +7,7 @@
 ! Declare (for generation of cparam.inc) the number of f array
 ! variables and auxiliary variables added by this module
 !
-! MPVAR CONTRIBUTION 1
+! MPVAR CONTRIBUTION 2
 ! MPAUX CONTRIBUTION 1
 ! CPARAM logical, parameter :: lparticles_radius=.true.
 !
