1e86ee6ec989e5d8bac3dc8e1142d528c6137b27
Philippe Bourdin <git@Bourdin.ch>
2016-08-12 23:39:36 +0200

    Code formatting.

--- src/magnetic.f90
+++ src/magnetic.f90
@@ -3050,9 +3050,8 @@ module Magnetic
 ! reduced speed of light pencil
 !
      if (lpenc_loc(i_clight2)) then
-       clight2_zdep(n)=(0.5*(1.+tanh(z(n)/(-5*dz)))*   & 
-                       (c_light_cgs/unit_velocity-sqrt(va2max_jxb))+&
-                       sqrt(va2max_jxb))**2
+       clight2_zdep(n)=(0.5*(1.+tanh(z(n)/(-5*dz))) & 
+           * (c_light_cgs/unit_velocity-sqrt(va2max_jxb))+sqrt(va2max_jxb))**2
        p%clight2=clight2_zdep(n)
        p%gamma_A2=1./(1+p%va2/p%clight2)
      endif
