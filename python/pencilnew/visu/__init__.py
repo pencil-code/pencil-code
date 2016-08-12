edd528c929120771336c63de345404c022a87cd9
Jørgen R. Aarnes <jorgen.r.aarnes@ntnu.no>
2016-08-12 11:44:53 +0200

    updated init-file in visualisation part of pencilnew

--- python/pencilnew/visu/__init__.py
+++ python/pencilnew/visu/__init__.py
@@ -4,3 +4,9 @@
 ##
 ################
 
+from animate_multislices import * 
+from animate_slices_compareruns import *
+from animate_slices_maketomovie import * 
+from animate_slices import * 
+from animate_time_slices import * 
+
