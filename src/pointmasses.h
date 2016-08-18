85178af8690adbc11b0e144a24844430b4beb753
Wladimir Lyra <wladimir.lyra@gmail.com>
2016-08-17 23:59:53 -0700

    include file for pointmasses

---
+++ src/pointmasses.h
@@ -0,0 +1,17 @@
+!  -*-f90-*-  (for emacs)    vim:set filetype=fortran:  (for vim)
+  private
+ 
+  public :: register_pointmasses, initialize_pointmasses
+  public :: init_pointmasses
+  public :: pencil_criteria_pointmasses, pencil_interdep_pointmasses
+  public :: calc_pencils_pointmasses
+  public :: pointmasses_pde_pencil,pointmasses_pde
+  public :: rprint_pointmasses
+  public :: read_pointmasses_init_pars, write_pointmasses_init_pars
+  public :: read_pointmasses_run_pars, write_pointmasses_run_pars
+  public :: get_totalmass
+  public :: pointmasses_read_snapshot
+  public :: pointmasses_write_snapshot,pointmasses_write_qdim
+  public :: pointmasses_timestep_first,pointmasses_timestep_second
+  public :: boundconds_pointmasses
+
