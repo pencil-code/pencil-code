e2d6f8c2be1550cff4acbe192c905ed5bd93e011
Wladimir Lyra <wladimir.lyra@gmail.com>
2016-09-28 14:38:13 -0700

    Map particles to density using tsc, in python.

--- python/pencil/files/__init__.py
+++ python/pencil/files/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["var","ts","sn","dim","param","slices","xyaver","yzaver","xzaver","zaver","power","pc2vtk","post_processing","streamlines","tracers","kf","fixed_points","read_pvar","particles_removed", "animate_interactive"]
+__all__ = ["var","ts","sn","dim","param","slices","xyaver","yzaver","xzaver","zaver","power","pc2vtk","post_processing","streamlines","tracers","kf","fixed_points","read_pvar","particles_removed", "animate_interactive","particles_to_density"]
--- python/pencil/files/particles_to_density.py
+++ python/pencil/files/particles_to_density.py
@@ -7,39 +7,60 @@
 ##
 import numpy as np
 import os.path
-#Pencil routines
 from pencil.files.dim import read_dim
-from pencil.files.grid import read_grid
 import sys
 
-def particles_to_density(xp,yp,zp,x,y,z):
+def particles_to_density(xxp,yyp,zzp,x,y,z):
     dim = read_dim()
     if dim.precision == 'D':
         precision = 'd'
     else:
         precision = 'f'
+    nnp = np.zeros([dim.mz,dim.my,dim.mx])
+                
+    npar=len(xxp)
+    
+    for k in range(npar):
 
-    npar=len(xp)
-    #g=read_grid()
+        xp=xxp[k]
+        yp=yyp[k]
+        zp=zzp[k]
 
-    if (dim.nx > 1): 
-        dx=x[1]-x[0]
-    else:
-        dx=1.0
-        
-    if (dim.ny > 1):
-        dy=y[1]-y[0]
-    else:
-        dy=1.0
-        
-    if (dim.nz > 1):
-        dz=z[1]-z[0]
-    else:
-        dz=1.0
+        ix0=find_index_bisect(xp,x)
+        iy0=find_index_bisect(yp,y)
+        iz0=find_index_bisect(zp,z)
 
-    dx_1=1.0/dx
-    dy_1=1.0/dy
-    dz_1=1.0/dz
+        ixx0=ix0-1
+        ixx1=ix0+1
+
+        iyy0=iy0-1
+        iyy1=iy0+1
+
+        izz0=iz0-1
+        izz1=iz0+1
+
+        if (dim.nx > 1):
+            dx=x[ix0]-x[ixx0]
+        else:
+            dx=1.0
+
+        if (dim.ny > 1):
+            dy=y[iy0]-y[iyy0]
+        else:
+            dy=1.0
+
+        if (dim.nz > 1):
+            dz=z[iz0]-z[izz0]
+        else:
+            dz=1.0
+
+        dx_1=1.0/dx
+        dy_1=1.0/dy
+        dz_1=1.0/dz
+#
+        dx_2=1.0/dx**2
+        dy_2=1.0/dy**2
+        dz_2=1.0/dz**2
     
 #if (not ghost) then begin
 #  mx=nx+6 & l1=3 & l2=l1+nx-1
@@ -56,36 +77,55 @@ def particles_to_density(xp,yp,zp,x,y,z):
 #  for m=m2+1,my-1,+1 do y2[m]=y2[m-1]+dy
 #  y=y2
 #;                                                                                         
-    if (dim.nz == 1): 
-        for n in np.arange(3):
-            nn=n+1
-            z[dim.n1-nn]=z[dim.n1]-nn*dz
-            z[dim.n2+nn]=z[dim.n2]+nn*dz
+#    if (dim.nz == 1): 
+#        for n in np.arange(3):
+#            nn=n+1
+#            z[dim.n1-nn]=z[dim.n1]-nn*dz
+#            z[dim.n2+nn]=z[dim.n2]+nn*dz
 
+        for ixx in np.arange(ixx0,ixx1+1):
+            for iyy in np.arange(iyy0,iyy1+1):
+                for izz in np.arange(izz0,izz1+1):
 
-    nnp=np.zeros((dim.mx,dim.my,dim.mz)) #(dim.mx,dim.my,dim.mz))
+                    if ( ((ixx-ix0) == -1) or ((ixx-ix0) == +1) ):
+                        weight_x = 1.125 - 1.5*abs(xp-x[ixx])  *dx_1 + 0.5*abs(xp-x[ixx])**2*dx_2
+                    else:
+                        if (dim.nx != 1): weight_x = 0.75 - (xp-x[ixx])**2*dx_2
 
-    for k in np.arange(npar): 
+                    if ( ((iyy-iy0) == -1) or ((iyy-iy0) == +1) ):
+                        weight_y = 1.125 - 1.5*abs(yp-y[iyy])  *dy_1 + 0.5*abs(yp-y[iyy])**2*dy_2
+                    else:
+                        if (dim.ny != 1): weight_y = 0.75 - (yp-y[iyy])**2*dy_2
 
-        ix = np.round((xp[k]-x[0])*dx_1)
-        iy = np.round((yp[k]-y[0])*dy_1)
-        iz = np.round((zp[k]-z[0])*dz_1)
+                    if ( ((izz-iz0) == -1) or ((izz-iz0) == +1) ):
+                        weight_z = 1.125 - 1.5*abs(zp-z[izz])  *dz_1 + 0.5*abs(zp-z[izz])**2*dz_2
+                    else:
+                        if (dim.nz != 1): weight_z = 0.75 - (zp-z[izz])**2*dz_2
 
-        if (ix == dim.l2+1): ix=ix-1
-        if (iy == dim.m2+1): iy=iy-1
-        if (iz == dim.n2+1): iz=iz-1
-        if (ix == dim.l1-1): ix=ix+1
-        if (iy == dim.m1-1): iy=iy+1
-        if (iz == dim.n1-1): iz=iz+1
+                    weight=1.0
+                    if (dim.nx != 1): weight=weight*weight_x
+                    if (dim.ny != 1): weight=weight*weight_y
+                    if (dim.nz != 1): weight=weight*weight_z
+#                                                                                                                                                                                                         
+                    nnp[izz,iyy,ixx]=nnp[izz,iyy,ixx] + weight
 
-##  Particles are assigned to the nearest grid point.
-
-        nnp[ix,iy,iz]=nnp[ix,iy,iz]+1.0
-
-    #print 'nnp minmax=',nnp.min(), nnp.max()
-    nnp=nnp[dim.l1:dim.l2+1,dim.m1:dim.m2+1,dim.n1:dim.n2+1]
-
-    if (dim.nz == 1): nnp=nnp[:,:,0]
-##  Convert from count to density.
-#
     return nnp
+#---------------------------------------------------------------
+def find_index_bisect(qpar,q):
+
+    jl=0
+    ju=len(q)-1
+
+    while ((ju-jl)>1):
+        jm=(ju+jl)/2
+        if (qpar > q[jm]):
+            jl=jm
+        else:
+            ju=jm
+
+    if (qpar-q[jl] <= q[ju]-qpar):
+        iq0=jl
+    else:
+        iq0=ju
+
+    return iq0
