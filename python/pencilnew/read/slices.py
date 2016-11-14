1220fa362a0e8cf04cdefd93127f82267a4ffb20
Iomsn <iomsn1@gmail.com>
2016-11-14 11:47:09 +0000

    Objectified the read slices routine.

--- python/pencilnew/read/slices.py
+++ python/pencilnew/read/slices.py
@@ -56,7 +56,6 @@ class SliceSeries(object):
         import numpy as np
 
         self.t = np.array([])
-        self.slices = []
 
 
     def read(self, field='', extension='', data_dir='data', proc=-1,
@@ -95,65 +94,101 @@ class SliceSeries(object):
         from scipy.io import FortranFile
         from pencilnew import read
 
+        # Define the directory that contains the slice files.
+        if proc < 0:
+            slice_dir = data_dir
+        else:
+            slice_dir = os.path.join(data_dir, 'proc{0}'.format(proc))
+
+        # Initialize the fields list.
+        if field:
+            if isinstance(field, list):
+                field_list = field
+            else:
+                field_list = [field]
+        else:
+            # Find the existing fields.
+            field_list = []
+            for file_name in os.listdir(slice_dir):
+                if (file_name[:6] == 'slice_'):
+                    field_list.append(file_name.split('.')[0][6:])
+            # Remove duplicates.
+            field_list = list(set(field_list))
+
+        # Initialize the extensions list.
         if extension:
             if isinstance(extension, list):
                 extension_list = extension
             else:
                 extension_list = [extension]
         else:
-            print('error')
-            #Find the existing extensions.
-            #for file_name in os.listdir(data_dir):
-                #if (file_name[:6] == 'slice_') and 
-            
-        
-        # Compose the file name according to field and extension.
-        data_dir = os.path.expanduser(data_dir)
-        if proc < 0:
-            file_name = os.path.join(data_dir, 'slice_'+field+'.'+extension)
-        else:
-            file_name = os.path.join(data_dir, 'proc{0}'.format(proc),
-                                     'slice_'+field+'.'+extension)
+            # Find the existing extensions.
+            extension_list = []
+            for file_name in os.listdir(slice_dir):
+                if (file_name[:6] == 'slice_'):
+                    extension_list.append(file_name.split('.')[1])
+            # Remove duplicates.
+            extension_list = list(set(extension_list))
 
-        dim = read.dim(data_dir, proc)
-        if dim.precision == 'D':
-            precision = 'd'
-        else:
-            precision = 'f'
+        class Foo():
+            pass
 
-        # Set up slice plane.
-        if (extension == 'xy' or extension == 'Xy'):
-            hsize = dim.nx
-            vsize = dim.ny
-        if (extension == 'xz'):
-            hsize = dim.nx
-            vsize = dim.nz
-        if (extension == 'yz'):
-            hsize = dim.ny
-            vsize = dim.nz
+        for extension in extension_list:
+            # This one will store the data.
+            ext_object = Foo()
 
-        infile = FortranFile(file_name)
+            for field in field_list:
+                # Compose the file name according to field and extension.
+                data_dir = os.path.expanduser(data_dir)
+                if proc < 0:
+                    file_name = os.path.join(data_dir, 'slice_'+field+'.'+extension)
+                else:
+                    file_name = os.path.join(data_dir, 'proc{0}'.format(proc),
+                                             'slice_'+field+'.'+extension)
 
-        islice = 0
-        self.t = np.zeros(1, dtype=precision)
-        self.slices = np.zeros(1, dtype=precision)
+                dim = read.dim(data_dir, proc)
+                if dim.precision == 'D':
+                    precision = 'd'
+                else:
+                    precision = 'f'
 
-        while True:
-            try:
-                raw_data = infile.read_record(dtype=precision)
-            except ValueError:
-                break
-            except TypeError:
-                break
+                # Set up slice plane.
+                if (extension == 'xy' or extension == 'Xy'):
+                    hsize = dim.nx
+                    vsize = dim.ny
+                if (extension == 'xz'):
+                    hsize = dim.nx
+                    vsize = dim.nz
+                if (extension == 'yz'):
+                    hsize = dim.ny
+                    vsize = dim.nz
 
-            if old_file:
-                self.t = np.concatenate((self.t, raw_data[-1:]))
-                self.slices = np.concatenate((self.slices, raw_data[:-1]))
-            else:
-                self.t = np.concatenate((self.t, raw_data[-2:-1]))
-                self.slices = np.concatenate((self.slices, raw_data[:-2]))
-            islice += 1
+                infile = FortranFile(file_name)
 
-        # Reshape and remove first entry.
-        self.t = self.t[1:]
-        self.slices = self.slices[1:].reshape(islice, vsize, hsize)
+                islice = 0
+                self.t = np.zeros(1, dtype=precision)
+                slice_series = np.zeros(1, dtype=precision)
+
+                while True:
+                    try:
+                        raw_data = infile.read_record(dtype=precision)
+                    except ValueError:
+                        break
+                    except TypeError:
+                        break
+
+                    if old_file:
+                        self.t = np.concatenate((self.t, raw_data[-1:]))
+                        slice_series = np.concatenate((slice_series, raw_data[:-1]))
+                    else:
+                        self.t = np.concatenate((self.t, raw_data[-2:-1]))
+                        slice_series = np.concatenate((slice_series, raw_data[:-2]))
+                    islice += 1
+
+                # Reshape and remove first entry.
+                self.t = self.t[1:]
+                slice_series = slice_series[1:].reshape(islice, vsize, hsize)
+                setattr(ext_object, field, slice_series)
+
+            setattr(self, extension, ext_object)
+
