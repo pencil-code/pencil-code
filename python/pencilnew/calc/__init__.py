d2cf37857f7ed94971fd8040cb22e60a43cebf77
andreas-schreiber <andreas.schreiber88@googlemail.com>
2016-11-22 16:35:22 +0100

    rename

--- python/pencilnew/calc/__init__.py
+++ python/pencilnew/calc/__init__.py
@@ -7,5 +7,5 @@
 from streamlines import *
 
 ####### working with particles and grid data
-from part_to_grid_2d import *             # bin particle quantities to a grid
+from part_to_grid import *             # bin particle quantities to a grid
 from fill_gaps_in_grid import *
