6b1ca172b124777bff8ae595c40be88842da7557
Wladimir Lyra <wladimir.lyra@gmail.com>
2016-11-23 11:14:21 -0800

    Add a make_movie with conversion from cylindrical to Cartesian

--- python/pencil/files/slices.py
+++ python/pencil/files/slices.py
@@ -548,6 +548,115 @@ def make_movie(field='uu1', datadir='data/', proc=-1, extension='xz',
     os.system("rm _tmp*.png")
     infile.close()
 
+def make_movie_cart(field='uu1', datadir='data/', proc=-1, extension='xz',
+               format='native', tmin=0., tmax=1.e38, amin=0., amax=1.,
+               transform='', oldfile=False):
+    """
+    read 2D slice files and assemble an animation in a mpg movie.
+
+    Quickly written from the example at
+    http://matplotlib.sourceforge.net/faq/howto_faq.html
+
+    Options:
+
+     field --- which variable to slice
+     datadir --- path to data directory
+     proc --- an integer giving the processor to read a slice from
+     extension --- which plane of xy,xz,yz,Xz. for 2D this should be overwritten.
+     format --- endian. one of little, big, or native (default)
+     tmin --- start time
+     tmax --- end time
+     amin --- minimum value for image scaling
+     amax --- maximum value for image scaling
+     transform --- insert arbitrary numerical code to modify the slice
+    """
+
+    import pylab as plt
+    import sys
+    from pencil.files.var import read_var
+
+    mkmvvar = read_var(trimall=True)	
+    r2d,phi2d = np.meshgrid(mkmvvar.x,mkmvvar.y)
+    x2d=r2d*np.cos(phi2d)
+    y2d=r2d*np.sin(phi2d)
+
+    datadir = os.path.expanduser(datadir)
+    if proc < 0:
+        filename = datadir + '/slice_' + field + '.' + extension
+    else:
+        filename = datadir + '/proc' + \
+            str(proc) + '/slice_' + field + '.' + extension
+
+    # Read the global dimensions.
+    dim = read_dim(datadir, proc)
+    if dim.precision == 'D':
+        precision = 'd'
+    else:
+        precision = 'f'
+
+    # Set up slice plane.
+    if extension == 'xy' or extension == 'Xy':
+        hsize = dim.nx
+        vsize = dim.ny
+    if extension == 'xz':
+	print('only works for xy')
+	sys.stop
+    if extension == 'yz':
+        print('only works for xy')
+	sys.stop
+    plane = np.zeros((vsize, hsize), dtype=precision)
+
+    infile = npfile(filename, endian=format)
+
+    files = []
+    fig = plt.figure(figsize=(5, 10))
+    ax = fig.add_subplot(111)
+
+    ifirst = True
+    islice = 0
+    while True:
+        try:
+            raw_data = infile.fort_read(precision)
+        except ValueError:
+            break
+        except TypeError:
+            break
+
+        if oldfile:
+            t = raw_data[-1]
+            plane = raw_data[:-1].reshape(vsize, hsize)
+        else:
+            t = raw_data[-2]
+            plane = raw_data[:-2].reshape(vsize, hsize)
+
+        if transform:
+            exec('plane = plane' + transform)
+
+        if t > tmin and t < tmax:
+	    ax.set_aspect('equal')
+            ax.cla()
+            ax.contourf(x2d, y2d, plane, 256)
+            fname = '_tmp%03d.png' % islice
+            print('Saving frame' + fname)
+            fig.savefig(fname)
+            files.append(fname)
+
+            if ifirst:
+                #print "----islice----------t---------min-------max-------delta" # Python 2
+                print("----islice----------t---------min-------max-------delta")
+            #print "%10i %10.3e %10.3e %10.3e %10.3e" % \ # Python 2
+                #(islice, t, plane.min(), plane.max(), plane.max() - plane.min()) # Python 2
+            print("{0:10} {1:10.3e} {2:10.3e} {3:10.3e} {4:10.3e}".format(islice, t, plane.min(), plane.max(), plane.max() - plane.min()))
+
+            ifirst = False
+            islice += 1
+
+    #print 'Making movie animation.mpg - this make take a while'
+    print('Making movie animation.mpg - this make take a while')
+    # SC: Not all systems use mencoder. Need to change this into ffmpeg.
+    os.system("mencoder 'mf://_tmp*.png' -mf type=png:fps=24 -ovc lavc -lavcopts vcodec=wmv2 -oac copy -o animation.mpg")
+    os.system("rm _tmp*.png")
+    infile.close()
 
 
 
