6716183e7db15f5e3a45aadf2082de4c67923144
andreas-schreiber <andreas.schreiber88@googlemail.com>
2016-11-29 15:37:52 +0100

    fill gaps method now works perfectly file with n dim data

--- python/pencilnew/calc/fill_gaps_in_grid.py
+++ python/pencilnew/calc/fill_gaps_in_grid.py
@@ -1,6 +1,8 @@
-def fill_gaps_in_grid(array, key='nan', method='cubic'):
+def fill_gaps_in_grid(array, key='nan', steps=['interpolate', 'extrapolate'], order=1, DEBUG=True):
     """Interpolates nans by nearest neighbor method to fill gaps in arrays.
     Beware this method does not invoke bondaries correctly!
+    This method does not work with array beeing a vector field! Hence, use componentwise
+    and stitch together manually!
 
     Args:
         - array:        array with nans inside
@@ -21,34 +23,65 @@ def fill_gaps_in_grid(array, key='nan', method='cubic'):
 
     import numpy as np
 
+    def get_columns_with_cell_idx(matrix, cell_idx):
+        slicing = [[e if i!=j else slice(None) for j,e in enumerate(cell_idx)] for i in range(np.size(cell_idx))]
+        return [matrix[c] for c in slicing]
+
     ######## type cast array into np.array
     array = np.array(array)
 
-    grid_x, grid_y, grid_z = np.mgrid[0:]
+    ######## Two iteration steps: first interpolate, then extrapolate
+    for step in steps:
+        ######## get indexes where array entries match key
+        if key == 'nan' or np.isnan(key):
+            idx_bad = np.isnan(array)
+            coords_bad = np.argwhere(idx_bad)
 
+            idx_good = ~np.isnan(array)
+            coords_good = np.argwhere(idx_good)
 
-    ######## get indexes where array entries match key
-    if key == 'nan' or np.isnan(key):
-        idx_bad = np.isnan(array)
-        coords_bad = np.argwhere(idx_bad)
+        else:
+            idx_bad = np.argwhere(array == key)
+            idx_good = array != key
 
-        idx_good = ~np.isnan(array)
-        coords_good = np.argwhere(idx_good)
+        ####### Step 1: Interpolate
+        if step == 'interpolate':
+            from scipy.interpolate import LinearNDInterpolator
 
-    else:
-        idx_bad = np.argwhere(array == key)
-        idx_good = array != key
+            f = LinearNDInterpolator(coords_good, array[idx_good])
+            for x_bad in coords_bad:
+                y_bad = f(*x_bad)
+                if DEBUG: print('Interpolating '+str(x_bad)+' entry to '+str(y_bad))
+                array.__setitem__(tuple(x_bad), y_bad)
 
+        ####### Step 2: Extrapolate
+        elif step == 'extrapolate':
+            from scipy.interpolate import InterpolatedUnivariateSpline
 
-    ######## replace each of these entries from indx will interpolated values
-    # A does not work :(
-    # from scipy.interpolate import griddata
-    # array[idx_bad] = griddata(points=coords_good, values=array[idx_good], xi=coords_bad, method=method)
+            for x_bad in coords_bad:
+                ## get columns in all axis having a certain nan position inside
+                columns = get_columns_with_cell_idx(array, x_bad)
 
-    # B
-    from scipy.interpolate import interpn
-    newArray = interpn(points=coords_good, values=array[idx_good], xi=coords_bad)
+                ## extrapolate at x_bad for each column and store in val
+                vals = []
+                for pos, column in zip(x_bad, columns):
+                    xi=[]; yi=[]
 
-    if key in newArray: print('! WARNING: Still '+str(key)+' found inside interpolated array!')
+                    ## clean up all additional locations with key or nan inside of column
+                    for x,y in enumerate(column):
+                        if not ( (y == key) or (np.isnan(y)) ):
+                            xi.append(x); yi.append(y)
+
+                    s = InterpolatedUnivariateSpline(xi, yi, k=order)
+                    vals.append(s(pos))
+
+                y_bad = np.mean(vals)
+                if DEBUG: print('Extrapolating '+str(x_bad)+' entry to '+str(y_bad))
+                array.__setitem__(tuple(x_bad), y_bad)      ## replace in array with mean extrapolated value
+
+        else:
+            print('! ERROR: Could not identify operation step "'+step+'"!')
+
+    if key in array: print('! WARNING: Still '+str(key)+' found inside interpolated array! Something went wrong...')
 
     return array
