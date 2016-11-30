a67b9e456efc7d18b7ea4a13f31d80cf806a19e4
andreas-schreiber <andreas.schreiber88@googlemail.com>
2016-11-30 16:24:11 +0100

    moved vtk to exporter

--- python/pencilnew/export/__init__.py
+++ python/pencilnew/export/__init__.py
@@ -1,6 +1,8 @@
 ################
 ##
-##	io
+##	exporter
 ##
 ################
 
+####### vtk
+from array2d_to_vtk import *
--- python/pencilnew/vtk/__init__.py
+++
@@ -1,7 +0,0 @@
-################
-##
-##	vtk
-##
-################
-
-from array2d_to_vtk import array2d_to_vtk
