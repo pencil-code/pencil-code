0e7789d77119181839bdf22b03ee10b10cc70bb8
Iomsn <iomsn1@gmail.com>
2017-01-09 17:01:43 +0000

    Rewrote derivative modules for python to comply with modern standards.

--- python/pencilnew/math/derivatives/der.py
+++ python/pencilnew/math/derivatives/der.py
@@ -1,20 +1,24 @@
-#
-# $Id$
+# der.py
 #
 """
-this is a wrapper for the actual derivatives, currently 6th order with
+This is a wrapper for the actual derivatives, currently 6th order with
 ghost zones included (pencil-code style).
 """
-import der_6th_order_w_ghosts as der
-xder = der.xder_6th
-yder = der.yder_6th
-zder = der.zder_6th
 
-xder2 = der.xder2_6th
-yder2 = der.yder2_6th
-zder2 = der.zder2_6th
 
-xder6 = der.xder6_6th
-yder6 = der.yder6_6th
-zder6 = der.zder6_6th
+from pencilnew.math.derivatives.der_6th_order_w_ghosts import \
+    xder_6th, yder_6th, zder_6th, xder2_6th, yder2_6th, zder2_6th, \
+    xder6_6th, yder6_6th, zder6_6th
+
+xder = xder_6th
+yder = yder_6th
+zder = zder_6th
+
+xder2 = xder2_6th
+yder2 = yder2_6th
+zder2 = zder2_6th
+
+xder6 = xder6_6th
+yder6 = yder6_6th
+zder6 = zder6_6th
 
