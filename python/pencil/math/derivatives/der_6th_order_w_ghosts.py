26672ab126345dd3f871bf687cc20ef63e81abde
Wladimir Lyra <wladimir.lyra@gmail.com>
2017-01-23 00:31:38 -0600

    use numpy gradient to calcualte dx, so that it works for non-equidistant grids.

--- python/pencil/math/derivatives/der_6th_order_w_ghosts.py
+++ python/pencil/math/derivatives/der_6th_order_w_ghosts.py
@@ -22,14 +22,16 @@ def xder_6th(f,dx,x=[],y=[],z=[]):
         print("%s dimension arrays not handled." % (str(f.ndim)))
         raise ValueError
 
+    dx=N.gradient(x)
     dx2 = 1./(60.*dx)
     dfdx = N.zeros_like(f)
     l1 = 3
     l2 = f.shape[-1]-3
     if (l2 > l1):
-        dfdx[...,l1:l2] = dx2*( +45.*(f[...,l1+1:l2+1]-f[...,l1-1:l2-1])
-                                -9.*(f[...,l1+2:l2+2]-f[...,l1-2:l2-2])
-                                +(f[...,l1+3:l2+3]-f[...,l1-3:l2-3]) )
+        for l in range(l1,l2):
+            dfdx[...,l] = dx2[l]*( +45.*(f[...,l+1]-f[...,l-1])
+                                     -9.*(f[...,l+2]-f[...,l-2])
+                                     +(f[...,l+3]-f[...,l-3]) )
     else:
         dfdx = 0.
     return dfdx
@@ -42,15 +44,17 @@ def yder_6th(f,dy,x=[],y=[],z=[]):
 
     param=read_param(quiet=True)
 
+    dy=N.gradient(y)
     dy2 = 1./(60.*dy)
     dfdy = N.zeros_like(f)
     m1 = 3
     m2 = f.shape[-2]-3
 
     if (m2 > m1):
-        dfdy[...,m1:m2,:] = dy2*( +45.*(f[...,m1+1:m2+1,:]-f[...,m1-1:m2-1,:]) 
-                                  -9.*(f[...,m1+2:m2+2,:]-f[...,m1-2:m2-2,:]) 
-                                  +(f[...,m1+3:m2+3,:]-f[...,m1-3:m2-3,:]) )
+        for m in range(m1,m2):
+            dfdy[...,m,:] = dy2[m]*( +45.*(f[...,m+1,:]-f[...,m-1,:]) 
+                                      -9.*(f[...,m+2,:]-f[...,m-2,:]) 
+                                      +(f[...,m+3,:]-f[...,m-3,:]) )
     else:
         dfdy = 0.
     if param.coord_system == ('cylindric' or 'spherical'):
@@ -69,6 +73,7 @@ def zder_6th(f,dz,x=[],y=[],z=[],run2D=False):
 
     param=read_param(quiet=True)
 
+    dz=N.gradient(z)
     dz2 = 1./(60.*dz)
     dfdz = N.zeros_like(f)
     n1 = 3
@@ -77,22 +82,21 @@ def zder_6th(f,dz,x=[],y=[],z=[],run2D=False):
     else:
         n2 = f.shape[-3]-3
 
+
     if (n2 > n1):
        if (run2D):
           # f[...,z,x] or f[...,z,y]
-          dfdz[...,n1:n2,:] = dz2*(+45.*(f[...,n1+1:n2+1,:]
-                              -f[...,n1-1:n2-1,:])
-                              -9.*(f[...,n1+2:n2+2,:]
-                              -f[...,n1-2:n2-2,:]) 
-                              +(f[...,n1+3:n2+3,:]-f[...,n1-3:n2-3,:]) )
+           for n in range(n1,n2): 
+               dfdz[...,n,:] = dz2[n]*(+45.*(f[...,n+1,:]-f[...,n-1,:])
+                                        -9.*(f[...,n+2,:]-f[...,n-2,:]) 
+                                           +(f[...,n+3,:]-f[...,n-3,:]) )
 
        else:
           # f[...,z,y,x]
-          dfdz[...,n1:n2,:,:] = dz2*(+45.*(f[...,n1+1:n2+1,:,:]
-                                -f[...,n1-1:n2-1,:,:])
-                                -9.*(f[...,n1+2:n2+2,:,:]
-                                -f[...,n1-2:n2-2,:,:]) 
-                                +(f[...,n1+3:n2+3,:,:]-f[...,n1-3:n2-3,:,:]) )
+           for n in range(n1,n2): 
+               dfdz[...,n,:,:] = dz2[n]*(+45.*(f[...,n+1,:,:]-f[...,n-1,:,:])
+                                          -9.*(f[...,n+2,:,:]-f[...,n-2,:,:]) 
+                                             +(f[...,n+3,:,:]-f[...,n-3,:,:]) )
     else:
         dfdz=0
     if param.coord_system == 'spherical':
@@ -115,11 +119,12 @@ def xder2_6th(f,dx,x=[],y=[],z=[]):
         print("%s dimension arrays not handled." % (str(f.ndim)))
         raise ValueError
 
-
-    dx2 = 1./(180.*dx**2.)
-    dfdx = N.zeros_like(f)
     l1 = 3
     l2 = f.shape[-1]-3
+    dx = N.gradient(x[l1:l2])
+    dx2 = 1./(180.*dx**2.)
+    dfdx = N.zeros_like(f)
+
     if (l2 > l1):
         dfdx[...,l1:l2] = dx2*(-490.*f[...,l1:l2]
                               +270.*(f[...,l1-1:l2-1]+f[...,l1+1:l2+1])
@@ -138,15 +143,22 @@ def yder2_6th(f,dy,x=[],y=[],z=[]):
     
     param=read_param(quiet=True)
 
-    dy2 = 1./(180.*dy**2.)
-    dfdy = N.zeros_like(f)
     m1 = 3
     m2 = f.shape[-2]-3
+    dy = N.gradient(y) #[m1:m2])
+    dy2 = 1./(180.*dy**2.)
+    dfdy = N.zeros_like(f)
+
     if (m2 > m1):
-        dfdy[...,m1:m2,:] = dy2*(-490.*f[...,m1:m2,:]
-                              +270.*(f[...,m1-1:m2-1,:]+f[...,m1+1:m2+1,:])
-                              - 27.*(f[...,m1-2:m2-2,:]+f[...,m1+2:m2+2,:])
-                              +  2.*(f[...,m1-3:m2-3,:]+f[...,m1+3:m2+3,:]) )
+        for m in range(m1,m2+1):
+            dfdy[...,m,:] = dy2[m]*(-490.*f[...,m,:]
+                                     +270.*(f[...,m-1,:]+f[...,m+1,:])
+                                     - 27.*(f[...,m-2,:]+f[...,m+2,:])
+                                     +  2.*(f[...,m-3,:]+f[...,m+3,:]) )
+        #dfdy[...,m1:m2,:] = dy2*(-490.*f[...,m1:m2,:]
+        #                      +270.*(f[...,m1-1:m2-1,:]+f[...,m1+1:m2+1,:])
+        #                      - 27.*(f[...,m1-2:m2-2,:]+f[...,m1+2:m2+2,:])
+        #                      +  2.*(f[...,m1-3:m2-3,:]+f[...,m1+3:m2+3,:]) )
     else:
         dfdy = 0.
     if param.coord_system == ('cylindric' or 'spherical'):
@@ -165,10 +177,12 @@ def zder2_6th(f,dz,x=[],y=[],z=[]):
 
     param=read_param(quiet=True)
 
-    dz2 = 1./(180.*dz**2.)
-    dfdz = N.zeros_like(f)
     n1 = 3
     n2 = f.shape[-3]-3
+    dz = N.gradient(z[n1:n2])
+    dz2 = 1./(180.*dz**2.)
+    dfdz = N.zeros_like(f)
+
     if (n2 > n1):
         dfdz[...,n1:n2,:,:] = dz2*(-490.*f[...,n1:n2,:,:]
                               +270.*(f[...,n1-1:n2-1,:,:]+f[...,n1+1:n2+1,:,:])
@@ -195,10 +209,12 @@ def xder6_6th(f,dx,x=[],y=[],z=[]):
         print("%s dimension arrays not handled." % (str(f.ndim)))
         raise ValueError
 
-    fac=1/dx**6
-    d6fdx = N.zeros_like(f)
     l1 = 3
     l2 = f.shape[-1] - 3
+    dx = N.gradient(x[l1:l2])
+    fac=1/dx**6
+    d6fdx = N.zeros_like(f)
+
     if (l2 > l1):
         d6fdx[...,l1:l2] = fac*(- 20.0* f[...,l1:l2] 
                      + 15.0*(f[...,l1+1:l2+1]+f[...,l1-1:l2-1]) 
@@ -213,9 +229,11 @@ def yder6_6th(f,dy,x=[],y=[],z=[]):
         print("%s dimension arrays not handled." % (str(f.ndim)))
         raise ValueError
 
-    fac=1/dy**6
     m1 = 3
     m2 = f.shape[-2] - 3
+
+    dy = N.gradient(y[m1:m2])
+    fac=1/dy**6
     d6fdy = N.zeros_like(f)
 
     if (m2 > m1):
@@ -230,9 +248,12 @@ def zder6_6th(f,dz,x=[],y=[],z=[]):
     if (f.ndim != 3 and f.ndim != 4):
         print("%s dimension arrays not handled." % (str(f.ndim)))
         raise ValueError
-    fac=1/dz**6
+
     n1 = 3
     n2 = f.shape[-3] - 3
+    dz = N.gradient(z[n1:n2])
+    fac=1/dz**6
+
     d6fdy = N.zeros_like(f)
 
     if (n2 > n1):
