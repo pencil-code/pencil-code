b6126d1dc8af4766e26a72a5d4ecc6931782014a
mrheinhardt <mreinhardt@nordita.org>
2017-06-24 01:34:00 +0300

    MR: added missing headers

---
+++ src/cuda/src/ddiagsextern.cuh
@@ -0,0 +1,10 @@
+// extern declerations for the diagnostic quantities on the device
+
+extern float *d_umax, *d_umin;
+extern float *d_urms;
+extern float *d_uxrms, *d_uyrms, *d_uzrms;
+extern float *d_rhorms, *d_rhomax, *d_rhomin;
+extern float *d_uxmax, *d_uymax, *d_uzmax;
+extern float *d_uxmin, *d_uymin, *d_uzmin;
+extern float *d_partial_result; 
+
---
+++ src/cuda/src/dfdfextern.cuh
@@ -0,0 +1,5 @@
+// extern declarations for arrays containing the variables and the rhss on the device 
+
+extern float *d_lnrho, *d_uu_x, *d_uu_y, *d_uu_z;
+extern float *d_w_lnrho, *d_w_uu_x, *d_w_uu_y, *d_w_uu_z;
+extern float *d_lnrho_dest, *d_uu_x_dest, *d_uu_y_dest, *d_uu_z_dest;
