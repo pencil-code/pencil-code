61ae04983855e60d02a041ca6bada0808a794066
mrheinhardt <mreinhardt@nordita.org>
2017-07-10 19:42:42 +0300

    MR: simplified declaration of externals; removed unneeded globals

--- src/cuda/src/timestep.cuh
+++ src/cuda/src/timestep.cuh
@@ -3,10 +3,7 @@
 float timestep_cuda(float* d_umax, float* d_partial_result, float* d_uu_x, float* d_uu_y, float* d_uu_z);
 
 void timeseries_diagnostics_cuda(/*float* d_umax, float* d_umin, float* d_urms, float* d_uxrms, 
-                                 float* d_uyrms, float* d_uzrms, float* d_rhorms,
-                                 float* d_rhomax, float* d_uxmax, float* d_uymax, float* d_uzmax,
-                                 float* d_rhomin, float* d_uxmin, float* d_uymin, float* d_uzmin,*/
+                                 float* d_uyrms, float* d_uzrms, float* d_rhorms,*/
                                  int step, float dt, double t);
-void max_diffus();
-void max_advec();
-
+void get_maxscal_from_device(float & maxscal,float *d_src);
+void get_minscal_from_device(float & minscal,float *d_src);
