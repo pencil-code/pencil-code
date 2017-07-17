53c8664c2a443343c2a6424c2160b7fbf2a57981
mrheinhardt <mreinhardt@nordita.org>
2017-07-17 18:21:10 +0300

    MR: removed unneeded public variable

--- src/nomagnetic.f90
+++ src/nomagnetic.f90
@@ -29,7 +29,7 @@ module Magnetic
   real, dimension (nz,3) :: bbmz,jjmz
   real :: inertial_length=0.,linertial_2
   logical :: lelectron_inertia=.false.
-  logical :: lcalc_aamean,lcalc_aameanz=.false.
+  logical :: lcalc_aameanz=.false.
   logical, dimension(7) :: lresi_dep=.false. 
   logical :: lcovariant_magnetic=.false.
 !
