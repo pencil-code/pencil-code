7664d8e1b91fa37799c7656764ba7e35b0509724
Andreas Schreiber <aschreib@isaac1.bc.rzg.mpg.de>
2017-08-21 16:25:53 +0200

    fixed natural sorting algorithm in pencilnew. now also can return reverse order!

--- python/pencilnew/math/natural_sort.py
+++ python/pencilnew/math/natural_sort.py
@@ -1,13 +1,14 @@
 
-def natural_sort(l):
+def natural_sort(l, reverse=False):
     """Method sorts list l of float numbers in strings in a natural way.
 
     Args:
         l:      list of strings, strings will be converted to float, then sorted
     """
     import re
+    from .is_number import is_number
 
-    convert = lambda text: int(text) if text.isdigit() else text.lower()
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
-    
-    return sorted(l, key = alphanum_key)
+    convert = lambda text: float(text) if is_number(text) else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9.]+)', str(key)) ]
+
+    return sorted(l, key = alphanum_key, reverse = reverse)
