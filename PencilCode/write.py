b6c8e2bcc850623e14698321c56373def8c8b5e7
Chao-Chin Yang <ccyang@unlv.edu>
2017-08-24 13:58:21 +0000

    Initiate module write for writing data in VTK.

---
+++ PencilCode/write.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# Last Modification: $Id$
+#=======================================================================
+# write.py
+#
+# Facilities for writing the Pencil Code data in VTK.
+#
+# Chao-Chin Yang, 2017-08-24
+#=======================================================================
