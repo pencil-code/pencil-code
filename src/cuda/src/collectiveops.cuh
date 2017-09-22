8dac6f1da601909da6a142e556e6a5d094f92b35
mrheinhardt <mreinhardt@nordita.org>
2017-09-22 21:58:42 +0300

    MR: adapted to calculation of linear means and to multi-CPU setups

--- src/cuda/src/collectiveops.cuh
+++ src/cuda/src/collectiveops.cuh
@@ -9,8 +9,8 @@ void max_scal_cuda(float* d_scal_max, float* d_partial_result, float* d_scal);
 
 void min_scal_cuda(float* d_scal_min, float* d_partial_result, float* d_scal);
 
-void vec_rms_cuda(float* d_vec_rms, float* d_partial_result, float* d_vec_x, float* d_vec_y, float* d_vec_z);
+void vec_rms_cuda(float* d_vec_rms, float* d_partial_result, float* d_vec_x, float* d_vec_y, float* d_vec_z, bool root=true);
 
-void scal_rms_cuda(float* d_scal_rms, float* d_partial_result, float* d_scal);
+void scal_rms_cuda(float* d_scal_rms, float* d_partial_result, float* d_scal, bool sqr=true, bool root=true);
 
-void scal_exp_rms_cuda(float* d_scal_rms, float* d_partial_result, float* d_scal);
+void scal_exp_rms_cuda(float* d_scal_rms, float* d_partial_result, float* d_scal, bool sqr=true, bool root=true);
