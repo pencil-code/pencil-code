6a66918f37aaa2167e991f0c062e4e73ced2f870
mrheinhardt <mreinhardt@nordita.org>
2017-10-09 17:02:25 +0300

    MR: added to calm the autotest

---
+++ src/noparticles_persistence.f90
@@ -0,0 +1,126 @@
+! $Id$
+!
+! This module calculates the probability distribution function
+! the particles that have moved a certain distance away from their
+! initial position. 
+!
+!** AUTOMATIC CPARAM.INC GENERATION ****************************
+!
+! Declare (for generation of cparam.inc) the number of f array
+! variables and auxiliary variables added by this module
+!
+! CPARAM logical, parameter :: lparticles_persistence=.true.
+!
+! MPAUX CONTRIBUTION 4
+!
+!***************************************************************
+module Particles_persistence
+!
+  use Cdata
+  use Messages
+  use Particles_cdata
+  use Particles_map
+  use Particles_mpicomm
+!
+  implicit none
+!
+  include 'particles_persistence.h'
+!
+contains
+!***********************************************************************
+    subroutine register_particles_persistence
+!
+!  Dummy.
+!
+    endsubroutine register_particles_persistence
+!***********************************************************************
+    subroutine initialize_particles_persistence(f)
+!
+!  Dummy.
+!
+      use General, only: keep_compiler_quiet
+!
+      real, dimension (mx,my,mz,mfarray) :: f
+!
+      call keep_compiler_quiet(f)
+!
+    endsubroutine initialize_particles_persistence
+!***********************************************************************
+    subroutine init_particles_persistence(fp)
+!
+      use General, only: keep_compiler_quiet
+!
+      real, dimension (mpar_loc,mparray), intent (out) :: fp
+!
+      call keep_compiler_quiet(fp)
+!      
+    endsubroutine init_particles_persistence
+!***********************************************************************
+    subroutine dpersist_dt(f,df,fp,dfp,ineargrid)
+!
+      use General, only: keep_compiler_quiet
+!
+      real, dimension (mx,my,mz,mfarray), intent (in) :: f
+      real, dimension (mx,my,mz,mvar), intent (inout) :: df
+      real, dimension (mpar_loc,mparray) :: fp
+      real, dimension (mpar_loc,mpvar), intent (inout) :: dfp
+      integer, dimension (mpar_loc,3), intent (in) :: ineargrid
+!
+      call keep_compiler_quiet(f,df)
+      call keep_compiler_quiet(fp,dfp)
+      call keep_compiler_quiet(ineargrid)
+!
+    endsubroutine dpersist_dt
+!***********************************************************************
+    subroutine read_ppersist_init_pars(iostat)
+!
+      use General, only: keep_compiler_quiet
+!
+      integer, intent(out) :: iostat
+!
+      call keep_compiler_quiet(iostat)
+!
+    endsubroutine read_ppersist_init_pars
+!***********************************************************************
+    subroutine write_ppersist_init_pars(unit)
+!
+      use General, only: keep_compiler_quiet
+!
+      integer, intent(in) :: unit
+!
+      call keep_compiler_quiet(unit)
+!
+    endsubroutine write_ppersist_init_pars
+!***********************************************************************
+    subroutine read_ppersist_run_pars(iostat)
+!
+      use General, only: keep_compiler_quiet
+!
+      integer, intent(out) :: iostat
+!
+      call keep_compiler_quiet(iostat)
+!
+    endsubroutine read_ppersist_run_pars
+!***********************************************************************
+    subroutine write_ppersist_run_pars(unit)
+!
+      use General, only: keep_compiler_quiet
+!
+      integer, intent(in) :: unit
+!
+      call keep_compiler_quiet(unit)
+!
+    endsubroutine write_ppersist_run_pars
+!***********************************************************************
+    subroutine rprint_particles_persist(lreset,lwrite)
+!
+      use General, only: keep_compiler_quiet
+!
+      logical :: lreset
+      logical, optional :: lwrite
+!
+      call keep_compiler_quiet(lreset,lwrite)
+!
+    endsubroutine rprint_particles_persist
+!***********************************************************************
+endmodule Particles_persistence 
---
+++ src/particles_persistence.h
@@ -0,0 +1,6 @@
+  public :: initialize_particles_persistence
+  public :: register_particles_persistence
+  public :: init_particles_persistence
+  public :: dpersist_dt
+  public :: read_ppersist_init_pars
+  public :: rprint_particles_persist
