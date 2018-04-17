60d0fed81c745c590be86092f98fe80098b24a13
mrheinhardt <mreinhardt@nordita.org>
2018-04-18 00:43:47 +0300

    MR: more functions

--- src/slices_methods.f90
+++ src/slices_methods.f90
@@ -8,8 +8,6 @@ module Slices_methods
 !
   implicit none
 !
-  include 'slices_methods.h'
-!
   interface assign_slices_scal
     module procedure assign_slices_f_scal
     module procedure assign_slices_sep_scal
@@ -53,16 +51,23 @@ module Slices_methods
 
     endsubroutine assign_slices_sep_scal
 !***********************************************************************
-    subroutine assign_slices_sep_vec(slices,xy,xz,yz,xy2,xy3,xy4,xz2)
+    subroutine assign_slices_sep_vec(slices,xy,xz,yz,xy2,xy3,xy4,xz2,ncomp)
 !
 !  Assignment of slice pointers according to slice selection switches.
 !
 !  12-apr-17/MR: coded 
 !
-    type (slice_data) :: slices
-    real, dimension(:,:,:), target :: xy,xz,yz,xy2,xy3,xy4,xz2
+    use General, only: ioptest
+!
+    type (slice_data)             , intent(INOUT):: slices
+    real, dimension(:,:,:), target, intent(OUT)  :: xy,xz,yz,xy2,xy3,xy4,xz2
+    integer, optional             , intent(IN)   :: ncomp
 
-    if (slices%index>=3) then
+    integer :: nc
+
+    nc=ioptest(ncomp,3)
+
+    if (slices%index>=nc) then
       slices%ready=.false.
     else
       slices%index=slices%index+1
@@ -75,7 +80,7 @@ module Slices_methods
       if (lwrite_slice_xy4) slices%xy4=>xy4(:,:,slices%index)
       if (lwrite_slice_xz2) slices%xz2=>xz2(:,:,slices%index)
 
-      if (slices%index<=3) slices%ready=.true.
+      if (slices%index<=nc) slices%ready=.true.
     endif
 
     endsubroutine assign_slices_sep_vec
@@ -86,9 +91,9 @@ module Slices_methods
 !
 !  12-apr-17/MR: coded 
 !
-      type (slice_data) :: slices
-      real, dimension(:,:,:,:) :: f
-      integer :: ind
+      type (slice_data)          , intent(OUT):: slices
+      real, dimension(mx,my,mz,*), intent(IN) :: f
+      integer                    , intent(IN) :: ind
 
       if (lwrite_slice_yz) &
         slices%yz=f(ix_loc,m1:m2  ,n1:n2  ,ind)
@@ -105,24 +110,30 @@ module Slices_methods
       if (lwrite_slice_xz2) &
         slices%xz2=f(l1:l2,iy2_loc,n1:n2  ,ind)
 
+      slices%ready=.true.
+
     endsubroutine assign_slices_f_scal
 !***********************************************************************
-    subroutine assign_slices_f_vec(slices,f,ind)
+    subroutine assign_slices_f_vec(slices,f,ind,ncomp)
 !
 !  Assignment of slice pointers into f-array according to slice selection
 !  switches.
 !
 !  12-apr-17/MR: coded 
 !
-    use General, only: transform_thph_yy_other
+    use General, only: transform_thph_yy_other, ioptest
 
-    type (slice_data) :: slices
-    real, dimension(:,:,:,:) :: f
-    integer :: ind
+    type (slice_data)       , intent(INOUT):: slices
+    real, dimension(:,:,:,:), intent(IN)   :: f
+    integer                 , intent(IN)   :: ind
+    integer, optional       , intent(IN)   :: ncomp
 
     real, dimension(:,:,:,:), allocatable, save :: transformed
+    integer :: nc
 
-    if (slices%index>=3) then
+    nc=ioptest(ncomp,3)
+
+    if (slices%index>=nc) then
       slices%ready=.false.
     else
       slices%index=slices%index+1
@@ -160,7 +171,7 @@ module Slices_methods
       if (lwrite_slice_xz2) &
         slices%xz2=f(l1:l2,iy2_loc,n1:n2  ,ind-1+slices%index)
 
-      if (slices%index<=3) slices%ready=.true.
+      if (slices%index<=nc) slices%ready=.true.
     endif
 
     endsubroutine assign_slices_f_vec
@@ -171,8 +182,8 @@ module Slices_methods
 !
 !  12-apr-17/MR: coded 
 !
-      real, dimension(nx) :: pencil
-      real, dimension(:,:) :: xy,xz,yz,xy2,xy3,xy4,xz2
+      real, dimension(nx) , intent(IN) :: pencil
+      real, dimension(:,:), intent(OUT):: xy,xz,yz,xy2,xy3,xy4,xz2
 
       if (n==iz_loc)  xy(:,m-m1+1)=pencil
       if (m==iy_loc)  xz(:,n-n1+1)=pencil
@@ -184,23 +195,27 @@ module Slices_methods
 
     endsubroutine store_slices_scal
 !***********************************************************************
-    subroutine store_slices_vec(pencil,xy,xz,yz,xy2,xy3,xy4,xz2)
+    subroutine store_slices_vec(pencil,xy,xz,yz,xy2,xy3,xy4,xz2,ncomp)
 !
 !  Stores data from pencil in auxiliary arrays when needed.
 !
 !  12-apr-17/MR: coded 
 !
-      use General, only: transform_thph_yy
+      use General, only: transform_thph_yy, ioptest
 
-      real, dimension(nx,3) :: pencil
-      real, dimension(:,:,:) :: xy,xz,yz,xy2,xy3,xy4,xz2
+      real, dimension(:,:)  , intent(IN) :: pencil
+      real, dimension(:,:,:), intent(OUT):: xy,xz,yz,xy2,xy3,xy4,xz2
+      integer, optional     , intent(IN) :: ncomp
 
+      integer :: nc
       real, dimension(1,3) :: tmp
+      
+      nc=ioptest(ncomp,3)
 !
 !  Note: i[xyz][234]*_loc is the index with respect to array with ghost zones.
 !
       if (lwrite_slice_yz) then
-        if (lyang) then
+        if (lyang.and.nc==3) then
 !
 !  On Yang grid: transform theta and phi components of oo to Yin-grid basis.
 !
@@ -227,7 +242,7 @@ module Slices_methods
 !
 !  12-apr-17/MR: coded 
 !
-      real, dimension(:,:), allocatable :: xy,xy2,xy3,xy4,xz,xz2,yz
+      real,dimension(:,:),allocatable,intent(INOUT):: xy,xy2,xy3,xy4,xz,xz2,yz
 
       if (lwrite_slice_yz .and..not.allocated(yz)) allocate(yz(ny,nz))
       if (lwrite_slice_xz .and..not.allocated(xz)) allocate(xz(nx,nz))
@@ -239,23 +254,100 @@ module Slices_methods
 
     endsubroutine alloc_slice_buffers_scal
 !***********************************************************************
-    subroutine alloc_slice_buffers_vec(xy,xz,yz,xy2,xy3,xy4,xz2)
+    subroutine alloc_slice_buffers_vec(xy,xz,yz,xy2,xy3,xy4,xz2,ncomp)
 !
 !  Allocation of auxiliary arrays for slices according to slice selection
 !  switches.
 !
 !  12-apr-17/MR: coded 
 !
-      real, dimension(:,:,:), allocatable :: xy,xy2,xy3,xy4,xz,xz2,yz
+      use General, only: ioptest
+!
+      real,dimension(:,:,:),allocatable,intent(INOUT):: xy,xy2,xy3,xy4,xz,xz2,yz
+      integer, optional,                intent(IN)   :: ncomp
 
-      if (lwrite_slice_yz .and..not.allocated(yz)) allocate(yz(ny,nz,3))
-      if (lwrite_slice_xz .and..not.allocated(xz)) allocate(xz(nx,nz,3))
-      if (lwrite_slice_xy .and..not.allocated(xy)) allocate(xy(nx,ny,3))
-      if (lwrite_slice_xy2.and..not.allocated(xy2)) allocate(xy2(nx,ny,3))            
-      if (lwrite_slice_xy3.and..not.allocated(xy3)) allocate(xy3(nx,ny,3))      
-      if (lwrite_slice_xy4.and..not.allocated(xy4)) allocate(xy4(nx,ny,3))
-      if (lwrite_slice_xz2.and..not.allocated(xz2)) allocate(xz2(nx,nz,3))
+      integer :: nc
+
+      nc=ioptest(ncomp,3)
+
+      if (lwrite_slice_yz .and..not.allocated(yz)) allocate(yz(ny,nz,nc))
+      if (lwrite_slice_xz .and..not.allocated(xz)) allocate(xz(nx,nz,nc))
+      if (lwrite_slice_xy .and..not.allocated(xy)) allocate(xy(nx,ny,nc))
+      if (lwrite_slice_xy2.and..not.allocated(xy2)) allocate(xy2(nx,ny,nc))            
+      if (lwrite_slice_xy3.and..not.allocated(xy3)) allocate(xy3(nx,ny,nc))      
+      if (lwrite_slice_xy4.and..not.allocated(xy4)) allocate(xy4(nx,ny,nc))
+      if (lwrite_slice_xz2.and..not.allocated(xz2)) allocate(xz2(nx,nz,nc))
 
     endsubroutine alloc_slice_buffers_vec
+!***********************************************************************
+    subroutine process_slices(slices,func)
+!
+!  Assignment of slice pointers according to slice selection switches.
+!
+!  12-apr-17/MR: coded 
+!
+      type(slice_data), intent(INOUT):: slices
+      character(LEN=40),intent(IN)   :: func
+
+      if (trim(func)=='exp') then
+        if (lwrite_slice_yz ) slices%yz =exp(slices%yz)
+        if (lwrite_slice_xz ) slices%xz =exp(slices%xz)
+        if (lwrite_slice_xy ) slices%xy =exp(slices%xy)
+        if (lwrite_slice_xy2) slices%xy2=exp(slices%xy2)
+        if (lwrite_slice_xy3) slices%xy3=exp(slices%xy3)
+        if (lwrite_slice_xy4) slices%xy4=exp(slices%xy4)
+        if (lwrite_slice_xz2) slices%xz2=exp(slices%xz2)
+      elseif (trim(func)=='alog') then
+        if (lwrite_slice_yz ) slices%yz =alog(slices%yz)
+        if (lwrite_slice_xz ) slices%xz =alog(slices%xz)
+        if (lwrite_slice_xy ) slices%xy =alog(slices%xy)
+        if (lwrite_slice_xy2) slices%xy2=alog(slices%xy2)
+        if (lwrite_slice_xy3) slices%xy3=alog(slices%xy3)
+        if (lwrite_slice_xy4) slices%xy4=alog(slices%xy4)
+        if (lwrite_slice_xz2) slices%xz2=alog(slices%xz2)
+      elseif (trim(func)=='abs') then
+        if (lwrite_slice_yz ) slices%yz =abs(slices%yz)
+        if (lwrite_slice_xz ) slices%xz =abs(slices%xz)
+        if (lwrite_slice_xy ) slices%xy =abs(slices%xy)
+        if (lwrite_slice_xy2) slices%xy2=abs(slices%xy2)
+        if (lwrite_slice_xy3) slices%xy3=abs(slices%xy3)
+        if (lwrite_slice_xy4) slices%xy4=abs(slices%xy4)
+        if (lwrite_slice_xz2) slices%xz2=abs(slices%xz2)
+      endif
+
+    endsubroutine process_slices
+!***********************************************************************
+    subroutine addto_slices(slices,pencil)
+!
+!  Adds pencil data to slices according to slice selection switches.
+!
+!  12-apr-17/MR: coded 
+!
+      type (slice_data),  intent(INOUT):: slices
+      real, dimension(nx),intent(IN)   :: pencil
+
+      integer :: i
+
+      if (lwrite_slice_yz ) slices%yz=slices%yz+pencil(ix_loc-l1+1)
+      if (lwrite_slice_xz ) then
+        do i=1,nz; slices%xz (:,i)=slices%xz (:,i)+pencil; enddo
+      endif
+      if (lwrite_slice_xy ) then
+        do i=1,ny; slices%xy (:,i)=slices%xy (:,i)+pencil; enddo
+      endif
+      if (lwrite_slice_xy2) then
+        do i=1,ny; slices%xy2(:,i)=slices%xy2(:,i)+pencil; enddo
+      endif
+      if (lwrite_slice_xy3) then
+        do i=1,ny; slices%xy3(:,i)=slices%xy3(:,i)+pencil; enddo
+      endif
+      if (lwrite_slice_xy4) then
+        do i=1,ny; slices%xy4(:,i)=slices%xy4(:,i)+pencil; enddo
+      endif
+      if (lwrite_slice_xz2) then
+        do i=1,nz; slices%xz2(:,i)=slices%xz2(:,i)+pencil; enddo
+      endif
+
+    endsubroutine addto_slices
 !***********************************************************************
 endmodule Slices_methods
