0984a58e56cd4d351ed1336c1f82ecf1e7ed3ddb
mrheinhardt <mreinhardt@nordita.org>
2018-04-19 20:53:50 +0300

    MR: adaptations to PC

--- src/cuda/src_new/gpu/gpu.cc
+++ src/cuda/src_new/gpu/gpu.cc
@@ -21,31 +21,39 @@
 
 //Memory management interface
 GPUInitFunc               GPUInit               = &init_cuda_generic;
+GPUInitializeFunc         GPUInitialize         = &initialize_cuda_generic;
 GPUDestroyFunc            GPUDestroy            = &destroy_cuda_generic;
 GPULoadFunc               GPULoad               = &load_grid_cuda_generic;  //Load from host to device
 GPUStoreFunc              GPUStore              = &store_grid_cuda_generic; //Store from device to host
-GPULoadForcingParamsFunc  GPULoadForcingParams  = &load_forcing_params_cuda_generic;
 GPULoadOuterHalosFunc     GPULoadOuterHalos     = &load_outer_halos_cuda_generic;
 GPUStoreInternalHalosFunc GPUStoreInternalHalos = &store_internal_halos_cuda_generic;
-
+#ifdef FORCING
+#ifdef GPU_ASTAROTH
+GPUUpdateForcingCoefsFunc GPUUpdateForcingCoefs = &update_forcing_coefs_cuda_generic;
+#else
+GPULoadForcingParamsFunc  GPULoadForcingParams  = &load_forcing_params_cuda_generic;
+#endif
+#endif
 
 //GPU solver interface
 GPUIntegrateFunc     GPUIntegrate     = &integrate_cuda_generic;
 GPUIntegrateStepFunc GPUIntegrateStep = &integrate_step_cuda_generic;
 GPUBoundcondStepFunc GPUBoundcondStep = &boundcond_step_cuda_generic;
-GPUReduceFunc        GPUReduce        = &reduce_cuda_generic;
 
 //Misc
+GPUReduceFunc    GPUReduce   = NULL;
 GPUGetSliceFunc  GPUGetSlice = &get_slice_cuda_generic;
 
 //Names for grid types. TODO note. Possibly error prone, but how else to define these at compile time
-    //without additional init functions or malloc/frees?
-const char* impl_type_names[] = {"CUDA_GENERIC"};/*,
+//without additional init functions or malloc/frees?
+
+const char* impl_type_names[] = {"CUDA_GENERIC",
                                  "CUDA_19P",
                                  "CUDA_55P",
-                                 "CUDA_MAXWELL"};*/
+                                 "CUDA_MAXWELL",
+                                 "CUDA_FOR_PENCIL"};
 
-#define DO_MINIMAL_BUILD
+//#define DO_MINIMAL_BUILD
 #ifdef DO_MINIMAL_BUILD
     void GPUSelectImplementation(ImplType type) 
     { 
@@ -69,7 +77,9 @@ const char* impl_type_names[] = {"CUDA_GENERIC"};/*,
             GPUDestroy              = &destroy_cuda_generic;
             GPULoad                 = &load_grid_cuda_generic;  //Load from host to device
             GPUStore                = &store_grid_cuda_generic; //Store from device to host
+            #ifdef FORCING
             GPULoadForcingParams    = &load_forcing_params_cuda_generic;
+            #endif
             GPULoadOuterHalos       = &load_outer_halos_cuda_generic;
             GPUStoreInternalHalos   = &store_internal_halos_cuda_generic;
 
@@ -77,11 +87,12 @@ const char* impl_type_names[] = {"CUDA_GENERIC"};/*,
             GPUIntegrate     = &integrate_cuda_generic;
             GPUBoundcondStep = &boundcond_step_cuda_generic;
             GPUIntegrateStep = &integrate_step_cuda_generic;
+#ifndef GPU_ASTAROTH
             GPUReduce        = &reduce_cuda_generic;
-
+#endif
             //Misc
             GPUGetSlice = &get_slice_cuda_generic;
-        } /*else if (type == CUDA_19P) {
+        }/*else if (type == CUDA_19P) {
             //Memory management interface
             GPUInit    = &init_cuda_19p;
             GPUDestroy = &destroy_cuda_19p;
@@ -129,7 +140,30 @@ const char* impl_type_names[] = {"CUDA_GENERIC"};/*,
 
             //Misc
             GPUGetSlice = &get_slice_cuda_maxwell;
-        }*/ else {
+        }*/ else if (type == CUDA_FOR_PENCIL) {
+            //Memory management interface
+            GPUInit              = &init_cuda_generic;
+	    GPUInitialize        = &initialize_cuda_generic;
+            GPUDestroy           = &destroy_cuda_generic;
+            GPULoad              = &load_grid_cuda_generic;
+            GPUStore             = &store_grid_cuda_generic;
+#ifdef FORCING
+            GPULoadForcingParams = &load_forcing_params_cuda_generic;
+            GPUUpdateForcingCoefs= &update_forcing_coefs_cuda_generic;
+#endif
+            GPULoadOuterHalos    = &load_outer_halos_cuda_generic;
+            GPUStoreInternalHalos= &store_internal_halos_cuda_generic;
+
+            //GPU solver interface
+            GPUIntegrate     = NULL;                   // only substeps called from PC
+            GPUBoundcondStep = NULL;     	       // boundary conditions set in PC
+            GPUIntegrateStep = &integrate_step_cuda_generic;
+#ifdef GPU_ASTAROTH
+            GPUReduce        = &reduce_cuda_PC;
+#endif
+            //Misc
+            GPUGetSlice = NULL;
+        } else {
             CRASH("Invalid implementation type!");
         }
     }
--- src/cuda/src_new/gpu/gpu.h
+++ src/cuda/src_new/gpu/gpu.h
@@ -6,6 +6,7 @@
 #include "common/grid.h"
 #include "common/slice.h"
 #include "common/forcing.h"
+#include "diagnostics/diagnostics.h"
 
 
 typedef enum {
@@ -13,6 +14,7 @@ typedef enum {
     CUDA_19P,
     CUDA_55P,
     CUDA_MAXWELL,
+    CUDA_FOR_PENCIL,
     NUM_IMPLEMENTATIONS
 } ImplType;
 
@@ -22,35 +24,34 @@ extern const char* impl_type_names[];
 /*
 NOTE: All of the following functions operate on all GPUs on the node unless otherwise stated.
 
-
 GPUInitFunc: 
 	Starting point of all GPU computation. Handles the allocation and
-initialization of *all memory needed on all GPUs in the node*. In other words,
-setups everything GPU-side so that calling any other GPU interface function
-afterwards does not result in illegal memory accesses. 
+        initialization of *all memory needed on all GPUs in the node*. In other words,
+        setups everything GPU-side so that calling any other GPU interface function
+        afterwards does not result in illegal memory accesses. 
 
 GPUDestroyFunc:
 	Opposite of GPUInitFunc. Frees all GPU allocations and resets all devices in
-the node.
+        the node.
 
 GPULoadFunc:
 	Decomposes and loads the whole host grid g (as defined in "common/grid.h")
-into GPU memories.
+        into GPU memories.
 
 GPUStoreFunc:
 	Combines and stores the grids in GPU memories into the host grid g.
 
 GPULoadForcingParamsFunc:
 	Takes a ForcingParams struct and copies its contents into the constant
-memories.
+        memories.
 
 GPULoadOuterHalosFunc:
 	Similar to GPULoadFunc, but loads only the ghost zone of the host grid into
-appropriate locations in GPU memory. TODO review this description.
+        appropriate locations in GPU memory. TODO review this description.
 
 GPUStoreInternalHalosFunc:
 	Similar to GPUStoreFunc, but combines the ghost zones of the GPU grid and
-stores them back to the ghost zone of the host grid. TODO review this description.
+        stores them back to the ghost zone of the host grid. TODO review this description.
 
 GPUIntegrateFunc:
 	Does the full RK3 integration (including boundary conditions)
@@ -63,26 +64,28 @@ GPUBoundcondStepFunc:
 
 GPUReduceFunc:
 	Performs a reduction on the GPU grids. The possible ReductTypes and GridTypes
-can be found in "common/datatypes.h" (subject to change) and "common/grid.h".
-Usage f.ex. GPUReduce(MAX_SCAL_UU, LNRHO); finds the maximum in the lnrho arrays
-on the GPUs.
+        can be found in "common/datatypes.h" (subject to change) and "common/grid.h".
+        Usage f.ex. GPUReduce(MAX_SCAL_UU, LNRHO); finds the maximum in the lnrho arrays
+        on the GPUs.
 
 GPUGetSliceFunc:
 	Similar to GPUStoreFunc, but instead stores a 2D slices to the Slice struct
-given as parameter
+        given as parameter
 */
 ////////////////////////////////////////////////////////////////////////////////
 //Memory management functions
 //Single-node
 typedef void (*GPUInitFunc)(CParamConfig* cparamconf, RunConfig* runconf);
+typedef void (*GPUInitializeFunc)(CParamConfig* cparamconf, RunConfig* runconf, const Grid & h_grid);
 typedef void (*GPUDestroyFunc)();
 typedef void (*GPULoadFunc)(Grid* g);
 typedef void (*GPUStoreFunc)(Grid* g);
 typedef void (*GPULoadForcingParamsFunc)(ForcingParams* fp);
+typedef void (*GPUUpdateForcingCoefsFunc)(ForcingParams* fp);
+
 //Multi-node
 typedef void (*GPULoadOuterHalosFunc)(Grid* g, real* halo);
 typedef void (*GPUStoreInternalHalosFunc)(Grid* g, real* halo);
-
 
 //GPU solver functions
 typedef void (*GPUIntegrateFunc)(real dt);
@@ -93,15 +96,16 @@ typedef real (*GPUReduceFunc)(ReductType t, GridType a);
 //Misc GPU functions
 typedef void (*GPUGetSliceFunc)(Slice* s);
 
-
 //Memory management interface
 extern GPUInitFunc               GPUInit;
+extern GPUInitializeFunc         GPUInitialize;
 extern GPUDestroyFunc            GPUDestroy;
-extern GPULoadFunc               GPULoad;  //Load from host to device
-extern GPUStoreFunc              GPUStore; //Store from device to host
+extern GPULoadFunc               GPULoad;             //Load from host to device
+extern GPUStoreFunc              GPUStore;            //Store from device to host
 extern GPULoadForcingParamsFunc  GPULoadForcingParams;
+extern GPUUpdateForcingCoefsFunc GPUUpdateForcingCoefs;
 extern GPULoadOuterHalosFunc     GPULoadOuterHalos;
-extern GPUStoreInternalHalosFunc GPULoadInternalHalos;
+extern GPUStoreInternalHalosFunc GPUStoreInternalHalos;
 
 //GPU solver interface
 extern GPUIntegrateFunc     GPUIntegrate;
@@ -112,83 +116,3 @@ extern GPUReduceFunc        GPUReduce;
 //Misc
 extern GPUGetSliceFunc  GPUGetSlice;
 void GPUSelectImplementation(ImplType type);
-///////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
