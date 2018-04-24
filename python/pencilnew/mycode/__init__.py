781e05faa0b9b85b7c0501ee67c8c595d5ad3718
luoyh15 <18810663237@163.com>
2018-04-24 20:20:32 +0800

    add mycode

---
+++ python/pencilnew/mycode/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 20:17:58 2018
+
+@author: luoyh
+"""
+
+################
+##
+##	io
+##
+################
+
+## animations
+from .animate_slices import *
+
+
+
