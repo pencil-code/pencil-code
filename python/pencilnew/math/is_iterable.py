28f71cec198a434f5de3a7e9abe3d4c673ca9544
Iomsn <iomsn1@gmail.com>
2018-05-11 22:16:20 +0100

    Removed redundant files in pencilnew/math, since those routines are now in pencilnew/math/general.py.

--- python/pencilnew/math/is_iterable.py
+++
@@ -1,9 +0,0 @@
-
-def is_iterable(i):
-    """ Checks if i is an iterable. """
-    try:
-        some_object_iterator = iter(i)
-        return True
-    except TypeError:
-        return False
-    return False
--- python/pencilnew/math/round_next_magnitude.py
+++
@@ -1,7 +0,0 @@
-def round_next_magnitude(num):
-    """Rounds a number up to its next magnitude.
-    e.g. 4,56 will be rounded to 10
-    """
-    import numpy as np
-
-    return np.ceil(num/10.)*10.
