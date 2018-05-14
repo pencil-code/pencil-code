059314b4e93636263be0b0875963c5c71ff4eb3a
mrheinhardt <mreinhardt@nordita.org>
2018-05-14 19:30:22 +0300

    MR: pushpars2c added

--- src/hydro.f90
+++ src/hydro.f90
@@ -6627,6 +6627,15 @@ module Hydro
 !
     endsubroutine calc_gradu
 !***********************************************************************
+    subroutine pushpars2c(p_par)
+
+    integer, parameter :: n_pars=1
+    integer(KIND=ikind8), dimension(n_pars) :: p_par
+
+    call copy_addr_c(lpressuregradient_gas,p_par(1))  ! int
+
+    endsubroutine pushpars2c
+!***********************************************************************
     subroutine push2c(p_idiag)
 
     use Diagnostics, only: set_type
