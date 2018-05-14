pencil-code/pencil-code

commit 327b6047d7fa5866bf2a6ce7689ee4704e9c5bf9

MR: corrected name of push2c; calls of initialize_timestep

src/run.f90
@@ -93,7 +93,8 @@ program run
   use Testfield,       only: rescaling_testfield
   use TestPerturb,     only: testperturb_begin, testperturb_finalize
   use Timeavg
-  use Timestep,        only: time_step
+  use Timestep,        only: time_step, initialize_timestep
+
 !
   implicit none
 !
@@ -468,6 +469,7 @@ program run
 !  initialization. And final pre-timestepping setup.
 !  (must be done before need_XXXX can be used, for example)
 !
+  call initialize_timestep
   call initialize_modules(f)
 !
   if (it1d==impossible_int) then
@@ -537,7 +539,8 @@ program run
 !  see idiag_timeperstep).
 !
   if (lroot) then
-    time1=mpiwtime()
+    !time1=mpiwtime()
+    time1=0
     time_last_diagnostic=time1
     icount=0
     it_last_diagnostic=icount
@@ -612,6 +615,7 @@ program run
         if (lhydro_kinematic)    call hydro_clean_up
         if (lsolid_cells)        call solid_cells_clean_up
         call rprint_list(LRESET=.true.) !(Re-read output list)
+        call initialize_timestep
         call initialize_modules(f)
         if (lparticles) then
           call particles_rprint_list(.false.)

src/hydro.f90
@@ -6636,38 +6636,38 @@ module Hydro
 
     endsubroutine pushpars2c
 !***********************************************************************
-    subroutine push2c(p_idiag)
+    subroutine pushdiags2c(p_diag)
 
     use Diagnostics, only: set_type
 
     integer, parameter :: ndiags=12
-    integer(KIND=ikind8), dimension(ndiags) :: p_idiag
+    integer(KIND=ikind8), dimension(ndiags) :: p_diag
 
-    call copy_addr_c(idiag_urms,p_idiag(1))
+    call copy_addr_c(idiag_urms,p_diag(1))
     call set_type(idiag_urms,lsqrt=.true.)
-    call copy_addr_c(idiag_uxrms,p_idiag(2))
+    call copy_addr_c(idiag_uxrms,p_diag(2))
     call set_type(idiag_uxrms,lsqrt=.true.)
-    call copy_addr_c(idiag_uyrms,p_idiag(3))
+    call copy_addr_c(idiag_uyrms,p_diag(3))
     call set_type(idiag_uyrms,lsqrt=.true.)
-    call copy_addr_c(idiag_uzrms,p_idiag(4))
+    call copy_addr_c(idiag_uzrms,p_diag(4))
     call set_type(idiag_uzrms,lsqrt=.true.)
-    call copy_addr_c(idiag_umax,p_idiag(5))
+    call copy_addr_c(idiag_umax,p_diag(5))
     call set_type(idiag_umax,lmax=.true.)
-    call copy_addr_c(idiag_umin,p_idiag(6))
+    call copy_addr_c(idiag_umin,p_diag(6))
     call set_type(idiag_umin,lmin=.true.)
-    call copy_addr_c(idiag_uxmin,p_idiag(7))
+    call copy_addr_c(idiag_uxmin,p_diag(7))
     call set_type(idiag_uxmin,lmin=.true.)
-    call copy_addr_c(idiag_uymin,p_idiag(8))
+    call copy_addr_c(idiag_uymin,p_diag(8))
     call set_type(idiag_uymin,lmin=.true.)
-    call copy_addr_c(idiag_uzmin,p_idiag(9))
+    call copy_addr_c(idiag_uzmin,p_diag(9))
     call set_type(idiag_uzmin,lmin=.true.)
-    call copy_addr_c(idiag_uxmax,p_idiag(10))
+    call copy_addr_c(idiag_uxmax,p_diag(10))
     call set_type(idiag_uxmax,lmax=.true.)
-    call copy_addr_c(idiag_uymax,p_idiag(11))
+    call copy_addr_c(idiag_uymax,p_diag(11))
     call set_type(idiag_uymax,lmax=.true.)
-    call copy_addr_c(idiag_uzmax,p_idiag(12))
+    call copy_addr_c(idiag_uzmax,p_diag(12))
     call set_type(idiag_uzmax,lmax=.true.)
 
-    endsubroutine push2c
+    endsubroutine pushdiags2c
 !***********************************************************************
 endmodule Hydro

commit 9048f14aecb2276a7728e51c293880cb047cce98

Fix a bug introduced in commit 327b6047.
This bug made the runtime parameter max_walltime useless.

src/run.f90
@@ -539,8 +539,7 @@ program run
 !  see idiag_timeperstep).
 !
   if (lroot) then
-    !time1=mpiwtime()
-    time1=0
+    time1=mpiwtime()
     time_last_diagnostic=time1
     icount=0
     it_last_diagnostic=icount