3e4cb50c200d440446a58cfdaa69cea831cceaef
Iomsn <iomsn1@gmail.com>
2018-05-16 17:38:41 +0100

    Added option for cylindrical and spherical coordinates for div, curl and grad in pencilnew.

--- python/pencilnew/math/derivatives/div_grad_curl.py
+++ python/pencilnew/math/derivatives/div_grad_curl.py
@@ -1,4 +1,3 @@
-
 # div_grad_curl.py
 #
 # Contains the vector calculus derivatives.
@@ -7,65 +6,159 @@ Compute the divergence, gradient and curl.
 """
 
 
-def div(f, dx, dy, dz):
+def div(f, dx, dy, dz, x=None, y=None, coordinate_system='cartesian'):
     """
-    Take divervenge of pencil code vector array f.
-    """
+    Take divervenge of pencil code vector array f in various coordinate systems.
 
-    from pencilnew.math.derivatives.der import xder, yder, zder
+    Keyword arguments:
 
-    if (f.ndim != 4):
-        print("div: must have vector 4-D array f[mvar ,mz, my, mx] for divergence.")
-        raise ValueError
+    *f*:
+      Pencil code vector array f.
 
-    return xder(f[0, ...], dx) + yder(f[1, ...], dy) + zder(f[2, ...], dz)
+    *dx, dy, dz*:
+      Grid spacing in the three dimensions.
 
+    *x, y*:
+      Radial (x) and polar (y) coordinates.
 
-def grad(f, dx, dy, dz):
-    """
-    Take the curl of a pencil code scalar array f.
+    *coordinate_system*:
+      Coordinate system under which to take the divergence.
+      Takes 'cartesian', 'cylindrical' and 'spherical'.
     """
 
     import numpy as np
-    from pencilnew.math.derivatives.der import xder, yder, zder
+    from der import xder, yder, zder
 
-    if (f.ndim != 3):
+    if f.ndim != 4:
+        print("div: must have vector 4-D array f[mvar, mz, my, mx] for divergence.")
+        raise ValueError
+
+    if coordinate_system == 'cartesian':
+        return xder(f[0], dx) + yder(f[1], dy) + zder(f[2], dz)
+
+    if coordinate_system == 'cylindrical':
+        if not x:
+            print('ERROR: need to specify x (radius) for cylindrical coordinates.')
+            raise ValueError
+        return xder(x*f[0], dx)/x + yder(f[1], dy)/x + zder(f[2], dz)
+
+    if coordinate_system == 'spherical':
+        if not x or not y:
+            print('ERROR: need to specify x (radius) and y (polar angle) for spherical coordinates.')
+            raise ValueError
+        return xder(x**2*f[0], dx)/x**2 + yder(np.sin(y)*f[1], dy)/(x*np.sin(y)) + zder(f[2], dz)/(x*np.sin(y))
+
+    print('ERROR: could not recognize coordinate system {0}'.format(coordinate_system))
+    raise ValueError
+
+
+def grad(f, dx, dy, dz, x=None, y=None, coordinate_system='cartesian'):
+    """
+    Take the curl of a pencil code scalar array f in various coordinate systems.
+
+    Keyword arguments:
+
+    *f*:
+      Pencil code scalar array f.
+
+    *dx, dy, dz*:
+      Grid spacing in the three dimensions.
+
+    *x, y*:
+      Radial (x) and polar (y) coordinates.
+
+    *coordinate_system*:
+      Coordinate system under which to take the divergence.
+      Takes 'cartesian', 'cylindrical' and 'spherical'.
+    """
+
+    import numpy as np
+    from der import xder, yder, zder
+
+    if f.ndim != 3:
         print("grad: must have scalar 3-D array f[mz, my, mx] for gradient.")
         raise ValueError
 
     grad_value = np.empty((3,) + f.shape)
-    grad_value[0, ...] = xder(f, dx)
-    grad_value[1, ...] = yder(f, dy)
-    grad_value[2, ...] = zder(f, dz)
+
+    if coordinate_system == 'cartesian':
+        grad_value[0, ...] = xder(f, dx)
+        grad_value[1, ...] = yder(f, dy)
+        grad_value[2, ...] = zder(f, dz)
+
+    if coordinate_system == 'cylindrical':
+        if not x:
+            print('ERROR: need to specify x (radius) for cylindrical coordinates.')
+            raise ValueError
+        grad_value[0, ...] = xder(f, dx)
+        grad_value[1, ...] = yder(f, dy)/x
+        grad_value[2, ...] = zder(f, dz)
+
+    if coordinate_system == 'spherical':
+        if not x or not y:
+            print('ERROR: need to specify x (radius) and y (polar angle) for spherical coordinates.')
+            raise ValueError
+        grad_value[0, ...] = xder(f, dx)
+        grad_value[1, ...] = yder(f, dy)/x
+        grad_value[2, ...] = zder(f, dz)/(x*np.sin(y))
 
     return grad_value
 
 
 def curl(f, dx, dy, dz, x=None, y=None, run2D=False, coordinate_system='cartesian'):
     """
-    Take the curl of a pencil code vector array.
-    The run2D parameter deals with pure 2-D snapshots (solved the (x,z)-plane pb).
+    Take the curl of a pencil code vector array f in various coordinate systems.
+
+    Keyword arguments:
+
+    *f*:
+      Pencil code scalar array f.
+
+    *dx, dy, dz*:
+      Grid spacing in the three dimensions.
+
+    *x, y*:
+      Radial (x) and polar (y) coordinates.
+
+    *run2D*:
+      Deals with pure 2-D snapshots (solved the (x,z)-plane pb).
+      !Only for Cartesian grids at the moment!
+
+    *coordinate_system*:
+      Coordinate system under which to take the divergence.
+      Takes 'cartesian', 'cylindrical' and 'spherical'.
+      !Does not work for 2d runs yet!
     """
 
     import numpy as np
-    from pencilnew.math.derivatives.der import xder, yder, zder
+    from der import xder, yder, zder
 
-    if (f.shape[0] != 3):
-        print("curl: must have vector 4-D array f[3,mz,my,mx] for curl.")
+    if f.shape[0] != 3:
+        print("curl: must have vector 4-D array f[3, mz, my, mx] for curl.")
         raise ValueError
 
     curl_value = np.empty_like(f)
     if (dy != 0. and dz != 0.):
-        # 3-D case	
+        # 3-D case
         if coordinate_system == 'cartesian':
-            curl_value[0, ...] = yder(f[2, ...], dy) - zder(f[1, ...], dz)
-            curl_value[1, ...] = zder(f[0, ...], dz) - xder(f[2, ...], dx)
-            curl_value[2, ...] = xder(f[1, ...], dx) - yder(f[0, ...], dy)
+            curl_value[0, ...] = yder(f[2], dy) - zder(f[1], dz)
+            curl_value[1, ...] = zder(f[0], dz) - xder(f[2], dx)
+            curl_value[2, ...] = xder(f[1], dx) - yder(f[0], dy)
         if coordinate_system == 'cylindrical':
-            curl_value[0, ...] = (1/x)*yder(f[2, ...], dy) - zder(f[1, ...], dz)
-            curl_value[1, ...] = zder(f[0, ...], dz) - xder(f[2, ...], dx)
-            curl_value[2, ...] = (1/x)*xder(x*f[1, ...], dx) - (1/x)*yder(f[0, ...], dy)
-    elif (dy == 0.):
+            if not x:
+                print('ERROR: need to specify x (radius) for cylindrical coordinates.')
+                raise ValueError
+            curl_value[0, ...] = (1/x)*yder(f[2], dy) - zder(f[1], dz)
+            curl_value[1, ...] = zder(f[0], dz) - xder(f[2], dx)
+            curl_value[2, ...] = (1/x)*xder(x*f[1], dx) - (1/x)*yder(f[0], dy)
+        if coordinate_system == 'spherical':
+            if not x or not y:
+                print('ERROR: need to specify x (radius) and y (polar angle) for spherical coordinates.')
+                raise ValueError
+            curl_value[0, ...] = (yder(np.sin(y)*f[2], dy) - zder(f[1], dz))/(x*np.sin(y))
+            curl_value[1, ...] = (zder(f[0], dz)/np.sin(y) - xder(x*f[2], dx))/x
+            curl_value[2, ...] = (xder(x*f[1], dx) - yder(f[0], dy))/x
+    elif dy == 0.:
         # 2-D case in the xz-plane
         curl_value[0, ...] = zder(f, dz, run2D)[0, ...] - xder(f, dx)[2, ...]
     else:
@@ -82,30 +175,29 @@ def curl2(f, dx, dy, dz):
     """
 
     import numpy as np
-    from pencilnew.math.derivatives.der import xder, yder, zder, xder2, yder2, zder2
+    from der import xder, yder, zder, xder2, yder2, zder2
 
     if (f.ndim != 4 or f.shape[0] != 3):
-        print("curl2: must have vector 4-D array f[3,mz,my,mx] for curl2.")
+        print("curl2: must have vector 4-D array f[3, mz, my, mx] for curl2.")
         raise ValueError
 
     curl2_value = np.empty(f.shape)
-    curl2_value[0, ...] = xder(yder(f[1, ...], dy) + zder(f[2, ...], dz), dx) \
-                          -yder2(f[0, ...], dy) - zder2(f[0, ...], dz)
-    curl2_value[1, ...] = yder(xder(f[0, ...], dx) + zder(f[2, ...], dz), dy) \
-                          -xder2(f[1, ...], dx) - zder2(f[1, ...], dz)
-    curl2_value[2, ...] = zder(xder(f[0, ...], dx) + yder(f[1, ...], dy), dz) \
-                          -xder2(f[2, ...], dx) - yder2(f[2, ...], dy)
+    curl2_value[0, ...] = xder(yder(f[1], dy) + zder(f[2], dz), dx) \
+                          -yder2(f[0], dy) - zder2(f[0], dz)
+    curl2_value[1, ...] = yder(xder(f[0], dx) + zder(f[2], dz), dy) \
+                          -xder2(f[1], dx) - zder2(f[1], dz)
+    curl2_value[2, ...] = zder(xder(f[0], dx) + yder(f[1], dy), dz) \
+                          -xder2(f[2], dx) - yder2(f[2], dy)
 
     return curl2
 
 
 def del2(f, dx, dy, dz):
     """
-    Calculate del6 (defined here as d^6/dx^6 + d^6/dy^6 + d^6/dz^6, rather
-    than del2^3) of a scalar f for hyperdiffusion.
+    Calculate del2.
     """
 
-    from pencilnew.math.derivatives.der import xder2, yder2, zder2
+    from der import xder2, yder2, zder2
 
     return xder2(f, dx) + yder2(f, dy) + zder2(f, dz)
 
@@ -116,6 +208,6 @@ def del6(f, dx, dy, dz):
     than del2^3) of a scalar f for hyperdiffusion.
     """
 
-    from pencilnew.math.derivatives.der import xder6, yder6, zder6
+    from der import xder6, yder6, zder6
 
     return xder6(f, dx) + yder6(f, dy) + zder6(f, dz)
