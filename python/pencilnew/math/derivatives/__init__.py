c8d1f85251c2d440dc41dfc31393db0c6ec24215
Iomsn <iomsn1@gmail.com>
2018-05-16 18:55:33 +0100

    Cleaned up __init__.py in pencilnew/math/derivatives.

--- python/pencilnew/math/derivatives/__init__.py
+++ python/pencilnew/math/derivatives/__init__.py
@@ -1,8 +1,7 @@
-from .der import *
-from .div_grad_curl import *
-from .simple_centered import simple_centered
+'''
+Differential operators.
+'''
 
-__all__ = ["xder","yder","zder",
-           "xder2","yder2","zder2",
-           "xder6","yder6","zder6",
-           "div","curl","grad","curl2","del2","del6"]
+from .der import xder, yder, zder, xder2, yder2, zder2, xder6, yder6, zder6
+from .div_grad_curl import div, curl, grad, curl2, del2, del6
+from .simple_centered import simple_centered
