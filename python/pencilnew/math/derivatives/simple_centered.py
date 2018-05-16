94cd4685c6e49e5fc62ed24eca8f78f7c782adba
Iomsn <iomsn1@gmail.com>
2018-05-16 18:56:09 +0100

    Cleaned up simple_centered derivatives in pencilnew.

--- python/pencilnew/math/derivatives/simple_centered.py
+++ python/pencilnew/math/derivatives/simple_centered.py
@@ -1,19 +1,26 @@
+# simple_centered.py
+#
+"""
+Contains simple centered differential operators.
+"""
+
 
 def simple_centered(x, y):
-  """
-  calculate dy by center differencing using array slices
-  """
-  import numpy as np
+    """
+    Calculate dy by center differencing using array slices
+    """
 
-  try:
-      dy = np.zeros(y.shape,np.float) #we know it will be this size
-      dy[1:-1] = (y[2:] - y[0:-2])/(x[2:] - x[0:-2])
-      dy[0] = (y[1]-y[0])/(x[1]-x[0])
-      dy[-1] = (y[-1] - y[-2])/(x[-1] - x[-2])
+    import numpy as np
 
-  except:
-      print('!! ERROR: Something is wrong with your input:')
-      print('!!        Shape x: '+str(x.shape))
-      print('!!        Shape y: '+str(y.shape))
+    try:
+        # We know it will be this size.
+        dy = np.zeros(y.shape, np.float)
+        dy[1:-1] = (y[2:] - y[0:-2])/(x[2:] - x[0:-2])
+        dy[0] = (y[1] - y[0])/(x[1] - x[0])
+        dy[-1] = (y[-1] - y[-2])/(x[-1] - x[-2])
+    except:
+        print('!! ERROR: Something is wrong with your input:')
+        print('!!        Shape x: '+str(x.shape))
+        print('!!        Shape y: '+str(y.shape))
 
-  return dy
+    return dy
