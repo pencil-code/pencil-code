868482844aa544d96fe488359355dfd5de6c2be0
Iomsn <iomsn1@gmail.com>
2018-05-25 19:26:44 +0100

    Added domain integration in pencilnew.

--- python/pencilnew/math/__init__.py
+++ python/pencilnew/math/__init__.py
@@ -5,9 +5,9 @@ Basic mathematical operators, including derivatives.
 from .vector_multiplication import dot, dot2, cross
 from .general import is_number, is_int, is_float, is_iterable
 from .general import log_range, round_next_magnitude, natural_sort
+from .transform import pospolar2cart, velpolar2cart
+from .integration import integrate
+del(integration)
 
-# old. TODO: update
 from . import derivatives
 
-# coordinate transformation
-from .transform import *
