1c206e8ab5c011382b4227cce973dce909f2b2c8
mrheinhardt <mreinhardt@nordita.org>
2018-09-11 22:25:47 +0300

    MR: new identifier for special/meanfield_e_tensor

--- src/record_types.h
+++ src/record_types.h
@@ -52,3 +52,6 @@ integer, parameter :: id_record_MAGNETIC_AMPL    = 312
 ! Shear
 integer, parameter :: id_record_SHEAR_DELTA_Y    = 320
 
+! Special
+integer, parameter :: id_record_SPECIAL_ILOAD    = 330
+
