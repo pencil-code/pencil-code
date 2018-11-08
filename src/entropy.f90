496388cfdadcf1715ec01c6894c926c9c4da47d2
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2018-11-08 15:27:10 +0100

    Added sanity check.

--- src/entropy.f90
+++ src/entropy.f90
@@ -816,7 +816,11 @@ module Energy
 !
 !  Read entropy profile (used for cooling to reference profile)
 !
-      if (lcooling_ss_mz) call input_profile('ss_mz','z',ss_mz,mz)
+      if (lcooling_ss_mz) then
+        if (.not. lentropy .and. .not. ltemperature) &
+            call fatal_error('cooling_ss_mz','ss cooling requires entropy or temperature')
+        call input_profile('ss_mz','z',ss_mz,mz)
+      endif
 !
 !  Initialize heat conduction.
 !
