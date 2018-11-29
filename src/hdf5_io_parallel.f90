8bbc094481af6260ef2141c66f39d0be01e0a348
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2018-11-29 14:37:20 +0100

    Identified that the 'timeavg' module triggers the warning about missing f-array indices in 'samples/mdwarf'.

--- src/hdf5_io_parallel.f90
+++ src/hdf5_io_parallel.f90
@@ -1617,7 +1617,8 @@ module HDF5_IO
 !
       if (lroot .and. (index_get == '') .and. (max_reported < ivar)) then
         ! known broken MVAR/MAUX contributions:
-        ! 'selfgravity_logspirals' without 'particles_selfgravity'
+        ! 'samples/selfgravity_logspirals' without 'particles_selfgravity'
+        ! 'samples/mdwarf' with 'timeavg'
         call warning ('index_get', 'f-array index #'//trim (itoa (ivar))//' not found!')
         if (max_reported == -1) then
           call warning ('index_get', &
