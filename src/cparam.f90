3d8364af35be7041efb906e2050ff2537929ce39
Axel Brandenburg <brandenb@nordita.org>
2019-02-17 00:22:03 -0700

    added sqrt2pi=sqrt2*sqrtpi

--- src/cparam.f90
+++ src/cparam.f90
@@ -154,6 +154,7 @@ module Cparam
   real, parameter :: pi_1=1./pi,pi4_1=pi**(-4),pi5_1=pi**(-5)
   real, parameter :: sqrtpi=1.77245385090551602729816748334115
   real, parameter :: sqrt2=1.41421356237309504880168872420970
+  real, parameter :: sqrt2pi=sqrt2*sqrtpi
   real, parameter :: four_pi_over_three=4.0/3.0*pi
   real, parameter :: onethird=1./3., fourthird=4./3.
   real, parameter :: one_over_sqrt3=0.577350269189625764509148780501958
