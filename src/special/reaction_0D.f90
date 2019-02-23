096843af2c00e9863b472d6596af4892140890e7
Axel Brandenburg <brandenb@nordita.org>
2019-02-22 23:32:52 -0700

    current

--- src/special/reaction_0D.f90
+++ src/special/reaction_0D.f90
@@ -31,22 +31,49 @@ module Special
    integer :: ispecial=0,ispecial1=0,ispecial2=0
 !
   ! input parameters
-  integer :: AA0=0
-  character(len=50) :: init_qq='racemic'
+  integer :: AA0=10, DD0=0, LL0=0
+  character(len=50) :: init_qq='set'
   namelist /special_init_pars/ &
-    init_qq, AA0
+    init_qq, AA0, DD0, LL0
 !
   ! run parameters
-  real :: kp=.1, km=.1, kC=0., kX=.1
+  real :: kp=.0, km=.0, kC=.9, kX=.1
+  real, dimension (nx) :: Ntot
+  logical :: lcompute_kC=.false.
   namelist /special_run_pars/ &
-    kp, km, kC, kX
+    kp, km, kC, kX, lcompute_kC
 !
 ! other variables (needs to be consistent with reset list below)
 !
+  integer :: idiag_eem=0  ! DIAG_DOC: $\langle \eta \rangle$
+  integer :: idiag_ee1m=0 ! DIAG_DOC: $\langle |\eta| \rangle$
+  integer :: idiag_ee2m=0 ! DIAG_DOC: $\langle \eta^2 \rangle$
+  integer :: idiag_ee3m=0 ! DIAG_DOC: $\langle \eta^3 \rangle$
+  integer :: idiag_ee4m=0 ! DIAG_DOC: $\langle \eta^4 \rangle$
+  integer :: idiag_ee10=0 ! DIAG_DOC: $\langle \eta_{10\%} \rangle$
+  integer :: idiag_ee50=0 ! DIAG_DOC: $\langle \eta_{50\%} \rangle$
+  integer :: idiag_ee90=0 ! DIAG_DOC: $\langle \eta_{90\%} \rangle$
+  integer :: idiag_ee99=0 ! DIAG_DOC: $\langle \eta_{99\%} \rangle$
   integer :: idiag_AAm=0  ! DIAG_DOC: $\langle [A] \rangle$
   integer :: idiag_DDm=0  ! DIAG_DOC: $\langle [D] \rangle$
   integer :: idiag_LLm=0  ! DIAG_DOC: $\langle [L] \rangle$
+  integer :: idiag_DLm=0  ! DIAG_DOC: $\langle [D]+[L] \rangle$
   integer :: idiag_kC=0   ! DIAG_DOC: $k_C$
+  integer :: idiag_A1=0   ! DIAG_DOC: $A_1$
+  integer :: idiag_A2=0   ! DIAG_DOC: $A_2$
+  integer :: idiag_A3=0   ! DIAG_DOC: $A_3$
+  integer :: idiag_A4=0   ! DIAG_DOC: $A_4$
+  integer :: idiag_A5=0   ! DIAG_DOC: $A_5$
+  integer :: idiag_D1=0   ! DIAG_DOC: $D_1$
+  integer :: idiag_D2=0   ! DIAG_DOC: $D_2$
+  integer :: idiag_D3=0   ! DIAG_DOC: $D_3$
+  integer :: idiag_D4=0   ! DIAG_DOC: $D_4$
+  integer :: idiag_D5=0   ! DIAG_DOC: $D_5$
+  integer :: idiag_L1=0   ! DIAG_DOC: $L_1$
+  integer :: idiag_L2=0   ! DIAG_DOC: $L_2$
+  integer :: idiag_L3=0   ! DIAG_DOC: $L_3$
+  integer :: idiag_L4=0   ! DIAG_DOC: $L_4$
+  integer :: idiag_L5=0   ! DIAG_DOC: $L_5$
 !
   contains
 !
@@ -56,7 +83,7 @@ module Special
 !  Configure pre-initialised (i.e. before parameter read) variables
 !  which should be know to be able to evaluate
 !
-!  6-oct-03/tony: coded
+!  19-feb-2019/axel: coded
 !
       use FArrayManager
 !
@@ -75,13 +102,17 @@ module Special
 !
 !  called by run.f90 after reading parameters, but before the time loop
 !
-!  06-oct-03/tony: coded
+!  19-feb-2019/axel: coded
 !
       real, dimension (mx,my,mz,mfarray) :: f
 !
 !  Cannot have kp+km+kX > 1.
 !
-      if ((kp+km+kX)>1.) call fatal_error('reaction_0D','initialize_special')
+      if ((kp+km+kC+kX)>1.) call fatal_error('initialize_special','kp+km+kC+kX > 1')
+!
+!  Compute total number of molecules of all populations.
+!
+      Ntot=f(l1:l2,m1,n1,iqq)+f(l1:l2,m1,n1,iqq+1)+f(l1:l2,m1,n1,iqq+2)
 !
 !  Initialize any module variables which are parameter dependent
 !
@@ -92,7 +123,7 @@ module Special
     subroutine init_special(f)
 !
 !  initialise special condition; called from start.f90
-!  06-oct-2003/tony: coded
+!  19-feb-2019/axel: coded
 !
       use Mpicomm
       use Sub
@@ -101,15 +132,15 @@ module Special
 !
       intent(inout) :: f
 !
-!  initial condition
+!  Initial condition; same for every population.
 !
       select case (init_qq)
         case ('nothing'); if (lroot) print*,'init_qq: nothing'
         case ('racemic'); f(:,:,:,iqq)=AA0
-        case ('all_equal')
-          f(:,:,:,iqq+1)=nx*AA0/3
-          f(:,:,:,iqq+2)=nx*AA0/3
-          f(:,:,:,iqq)=nx-f(:,:,:,iqq+1)-f(:,:,:,iqq+2)
+        case ('set')
+          f(:,:,:,iqq  )=AA0
+          f(:,:,:,iqq+1)=DD0
+          f(:,:,:,iqq+2)=LL0
 !
         case default
           !
@@ -171,7 +202,7 @@ module Special
 !  several precalculated Pencils of information are passed if for
 !  efficiency.
 !
-!   06-oct-03/tony: coded
+!  19-feb-2019/axel: coded
 !
       use General, only: random_number_wrapper
       use Diagnostics
@@ -181,11 +212,11 @@ module Special
       real, dimension (mx,my,mz,mfarray) :: f
       real, dimension (mx,my,mz,mvar) :: df
       real, dimension (nx) :: rrr
-      real ::     d1, d2, d3, d4, d5, d6, d7
-      real :: r0, r1, r2, r3, r4, r5, r6, r7
-      real :: Dm, Lm, Dm_per_proc,Lm_per_proc
-      integer, dimension (nx) :: dqA, dqD, dqL
-      integer, dimension (nx) :: qA, qD, qL
+      real, dimension (nx) ::     d1, d2, d3, d4, d5, d6, d7
+      real, dimension (nx) :: r0, r1, r2, r3, r4, r5, r6, r7
+      real, dimension (nx) :: qA, qD, qL
+      real, dimension (nx) :: dqA, dqD, dqL
+      real, dimension (nx) :: ee, ee1, ee10, ee50, ee90, ee99
       type (pencil_case) :: p
 !
       intent(in) :: f,p
@@ -197,39 +228,29 @@ module Special
 !
       if (headtt.or.ldebug) print*,'dspecial_dt: SOLVE dSPECIAL_dt'
 !
-!  Define state vector
+!  Define state vector.
+!  The positions l1-l2 refer to different populations.
 !
       qA=f(l1:l2,m,n,1)
       qD=f(l1:l2,m,n,2)
       qL=f(l1:l2,m,n,3)
 !
-!  For autocatalysis, need to know the mean number of molecules.
-!
-      if (kC/=0.) then
-        Dm_per_proc=sum(qD)
-        Lm_per_proc=sum(qL)
-        call mpireduce_sum(Dm_per_proc,Dm,1)
-        call mpireduce_sum(Lm_per_proc,Lm,1)
-        call mpibcast_real(Dm)
-        call mpibcast_real(Lm)
-      endif
-!
 !  Recompute kC
 !
-      if (kC/=0..and.(Dm+Lm)/=0.) kC=(1.-(kp+km+kX))*nx/(Dm+Lm)
+      !if (lcompute_kC.and.(Dm+Lm)/=0.) kC=(1.-(kp+km+kX))*nx/(Dm+Lm)
 !
 !  Determine boundary points
 !
-      d1=.5*kp
-      d2=   kp
-      d3=.5*km
-      d4=   km
-      d5=   kC*Dm/nx
-      d6=   kC*Lm/nx
-      d7=   kX
+      d1=kp*.5
+      d2=kp*.5
+      d3=km*.5
+      d4=km*.5
+      d5=kC*qD/Ntot
+      d6=kC*qL/Ntot
+      d7=kX
 !
       r0=0.
-      r1=   d1
+      r1=r0+d1
       r2=r1+d2
       r3=r2+d3
       r4=r3+d4
@@ -240,14 +261,18 @@ module Special
 !  Produce random numbers
 !
       call random_number_wrapper(rrr)
-print*,'AXEL-: ',r4, r5, r6, r7
-print*,'AXEL0: ',rrr
+!
+!  Initialize to zero
+!
+        dqA=0
+        dqD=0
+        dqL=0
 !
 !  Spontaneous formation of chiral molecule *or*
 !  autocatalysis of D. 
 !
       where (( (rrr >= r0 .and. rrr < r1) .or. &
-                   (rrr >= r4 .and. rrr < r5) ) .and. qA /= 0)
+               (rrr >= r4 .and. rrr < r5) ) .and. qA /= 0)
         dqA=-1
         dqD=+1
         dqL= 0
@@ -257,17 +282,21 @@ print*,'AXEL0: ',rrr
 !  autocatalysis of L. 
 !
       where (( (rrr >= r1 .and. rrr < r2) .or. &
-                   (rrr >= r5 .and. rrr < r6) ) .and. qA /= 0)
+               (rrr >= r5 .and. rrr < r6) ) .and. qA /= 0)
         dqA=-1
         dqD= 0
         dqL=+1
       endwhere
+!
+!  Spontaneous racemizatio of D.
 !
       where ((rrr >= r2 .and. rrr < r3) .and. qD > 0)
         dqA= 1
         dqD=-1
         dqL= 0
       endwhere
+!
+!  Spontaneous racemizatio of L.
 !
       where ((rrr >= r3 .and. rrr < r4) .and. qL > 0)
         dqA= 1
@@ -286,17 +315,57 @@ print*,'AXEL0: ',rrr
       df(l1:l2,m,n,1)=df(l1:l2,m,n,1)+dqA
       df(l1:l2,m,n,2)=df(l1:l2,m,n,2)+dqD
       df(l1:l2,m,n,3)=df(l1:l2,m,n,3)+dqL
-print*,'AXEL1: ',qA
-print*,'AXEL2: ',dqA
-print*
 !
 !  diagnostics
 !
       if (ldiagnos) then
-        if (idiag_AAm/=0) call sum_mn_name(f(l1:l2,m,n,1),idiag_AAm)
-        if (idiag_DDm/=0) call sum_mn_name(f(l1:l2,m,n,2),idiag_DDm)
-        if (idiag_LLm/=0) call sum_mn_name(f(l1:l2,m,n,3),idiag_LLm)
+        where (qD+qL/=0.)
+          ee=(qD-qL)/(qD+qL)
+        elsewhere
+          ee=0.
+        endwhere
+        ee1=abs(ee)
+        if (idiag_ee1m/=0) call sum_mn_name(ee   ,idiag_eem)
+        if (idiag_ee1m/=0) call sum_mn_name(ee1  ,idiag_ee1m)
+        if (idiag_ee2m/=0) call sum_mn_name(ee**2,idiag_ee2m)
+        if (idiag_ee3m/=0) call sum_mn_name(ee**3,idiag_ee3m)
+        if (idiag_ee4m/=0) call sum_mn_name(ee**4,idiag_ee4m)
+        if (idiag_AAm/=0) call sum_mn_name(qA/Ntot,idiag_AAm)
+        if (idiag_DDm/=0) call sum_mn_name(qD/Ntot,idiag_DDm)
+        if (idiag_LLm/=0) call sum_mn_name(qL/Ntot,idiag_LLm)
+        if (idiag_DLm/=0) call sum_mn_name((qD+qL)/Ntot,idiag_DLm)
         if (idiag_kC /=0) call   save_name(kC            ,idiag_kC)
+        if (idiag_A1 /=0) call   save_name(f(l1+0 ,m,n,1),idiag_A1)
+        if (idiag_A2 /=0) call   save_name(f(l1+1 ,m,n,1),idiag_A2)
+        if (idiag_A3 /=0) call   save_name(f(l1+2 ,m,n,1),idiag_A3)
+        if (idiag_A4 /=0) call   save_name(f(l1+3 ,m,n,1),idiag_A4)
+        if (idiag_A5 /=0) call   save_name(f(l1+4 ,m,n,1),idiag_A5)
+        if (idiag_D1 /=0) call   save_name(f(l1+0 ,m,n,2),idiag_D1)
+        if (idiag_D2 /=0) call   save_name(f(l1+1 ,m,n,2),idiag_D2)
+        if (idiag_D3 /=0) call   save_name(f(l1+2 ,m,n,2),idiag_D3)
+        if (idiag_D4 /=0) call   save_name(f(l1+3 ,m,n,2),idiag_D4)
+        if (idiag_D5 /=0) call   save_name(f(l1+4 ,m,n,2),idiag_D5)
+        if (idiag_L1 /=0) call   save_name(f(l1+0 ,m,n,3),idiag_L1)
+        if (idiag_L2 /=0) call   save_name(f(l1+1 ,m,n,3),idiag_L2)
+        if (idiag_L3 /=0) call   save_name(f(l1+2 ,m,n,3),idiag_L3)
+        if (idiag_L4 /=0) call   save_name(f(l1+3 ,m,n,3),idiag_L4)
+        if (idiag_L5 /=0) call   save_name(f(l1+4 ,m,n,3),idiag_L5)
+        if (idiag_ee10/=0) then
+          where (ee1>.1); ee10=1.; elsewhere; ee10=0.; endwhere
+          call sum_mn_name(ee10,idiag_ee10)
+        endif
+        if (idiag_ee50/=0) then
+          where (ee1>.5); ee50=1.; elsewhere; ee50=0.; endwhere
+          call sum_mn_name(ee50,idiag_ee50)
+        endif
+        if (idiag_ee90/=0) then
+          where (ee1>.9); ee90=1.; elsewhere; ee90=0.; endwhere
+          call sum_mn_name(ee90,idiag_ee90)
+        endif
+        if (idiag_ee99/=0) then
+          where (ee1>.99); ee99=1.; elsewhere; ee99=0.; endwhere
+          call sum_mn_name(ee99,idiag_ee99)
+        endif
       endif
 !
     endsubroutine dspecial_dt
@@ -341,7 +410,7 @@ print*
 !
 !  reads and registers print parameters relevant to special
 !
-!   06-oct-03/tony: coded
+!  19-feb-2019/axel: coded
 !
       use Diagnostics
       use FArrayManager, only: farray_index_append
@@ -360,14 +429,44 @@ print*
 !  (this needs to be consistent with what is defined above!)
 !
       if (lreset) then
-        idiag_AAm=0; idiag_DDm=0; idiag_LLm=0; idiag_kC=0
+        idiag_AAm=0; idiag_DDm=0; idiag_LLm=0; idiag_DLm=0; idiag_kC=0
+        idiag_A1=0; idiag_A2=0; idiag_A3=0; idiag_A4=0; idiag_A5=0
+        idiag_D1=0; idiag_D2=0; idiag_D3=0; idiag_D4=0; idiag_D5=0
+        idiag_L1=0; idiag_L2=0; idiag_L3=0; idiag_L4=0; idiag_L5=0
+        idiag_eem=0; idiag_ee1m=0; idiag_ee2m=0; idiag_ee3m=0; idiag_ee4m=0
+        idiag_ee10=0; idiag_ee50=0; idiag_ee90=0; idiag_ee99=0
       endif
 !
       do iname=1,nname
+        call parse_name(iname,cname(iname),cform(iname),'eem',idiag_eem)
+        call parse_name(iname,cname(iname),cform(iname),'ee1m',idiag_ee1m)
+        call parse_name(iname,cname(iname),cform(iname),'ee2m',idiag_ee2m)
+        call parse_name(iname,cname(iname),cform(iname),'ee3m',idiag_ee3m)
+        call parse_name(iname,cname(iname),cform(iname),'ee4m',idiag_ee4m)
+        call parse_name(iname,cname(iname),cform(iname),'ee10',idiag_ee10)
+        call parse_name(iname,cname(iname),cform(iname),'ee50',idiag_ee50)
+        call parse_name(iname,cname(iname),cform(iname),'ee90',idiag_ee90)
+        call parse_name(iname,cname(iname),cform(iname),'ee99',idiag_ee99)
         call parse_name(iname,cname(iname),cform(iname),'AAm',idiag_AAm)
         call parse_name(iname,cname(iname),cform(iname),'DDm',idiag_DDm)
         call parse_name(iname,cname(iname),cform(iname),'LLm',idiag_LLm)
+        call parse_name(iname,cname(iname),cform(iname),'DLm',idiag_DLm)
         call parse_name(iname,cname(iname),cform(iname),'kC',idiag_kC)
+        call parse_name(iname,cname(iname),cform(iname),'A1',idiag_A1)
+        call parse_name(iname,cname(iname),cform(iname),'A2',idiag_A2)
+        call parse_name(iname,cname(iname),cform(iname),'A3',idiag_A3)
+        call parse_name(iname,cname(iname),cform(iname),'A4',idiag_A4)
+        call parse_name(iname,cname(iname),cform(iname),'A5',idiag_A5)
+        call parse_name(iname,cname(iname),cform(iname),'D1',idiag_D1)
+        call parse_name(iname,cname(iname),cform(iname),'D2',idiag_D2)
+        call parse_name(iname,cname(iname),cform(iname),'D3',idiag_D3)
+        call parse_name(iname,cname(iname),cform(iname),'D4',idiag_D4)
+        call parse_name(iname,cname(iname),cform(iname),'D5',idiag_D5)
+        call parse_name(iname,cname(iname),cform(iname),'L1',idiag_L1)
+        call parse_name(iname,cname(iname),cform(iname),'L2',idiag_L2)
+        call parse_name(iname,cname(iname),cform(iname),'L3',idiag_L3)
+        call parse_name(iname,cname(iname),cform(iname),'L4',idiag_L4)
+        call parse_name(iname,cname(iname),cform(iname),'L5',idiag_L5)
       enddo
 !
     endsubroutine rprint_special
