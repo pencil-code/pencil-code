cbd2ae54ac8545b7f4f2ebc1409de79533ba162a
iomsn <iomsn1@googlemail.com>
2019-04-11 15:39:38 +0100

    Added 4th order accuracy derivatives for third order derivatives. Added triple curl operator for cylindrical coordinates. Useful for Laplacian(vorticity) or Laplacian(B). Still need to implement for Cartesian coordinates and spherical.

--- python/pencilnew/math/derivatives/__init__.py
+++ python/pencilnew/math/derivatives/__init__.py
@@ -2,6 +2,6 @@
 Differential operators.
 '''
 
-from .der import xder, yder, zder, xder2, yder2, zder2, xder6, yder6, zder6
-from .div_grad_curl import div, curl, grad, curl2, del2, del6
+from .der import xder, yder, zder, xder2, yder2, zder2, xder3, yder3, zder3, xder6, yder6, zder6
+from .div_grad_curl import div, curl, grad, curl2, del2, curl3, del6
 from .simple_centered import simple_centered
--- python/pencilnew/math/derivatives/der.py
+++ python/pencilnew/math/derivatives/der.py
@@ -9,6 +9,8 @@ ghost zones included (pencil-code style).
 from .der_6th_order_w_ghosts import \
     xder_6th, yder_6th, zder_6th, xder2_6th, yder2_6th, zder2_6th, \
     xder6_6th, yder6_6th, zder6_6th
+from .der_4th_order_w_ghosts import \
+    xder3_4th, yder3_4th, zder3_4th
 
 xder = xder_6th
 yder = yder_6th
@@ -17,6 +19,10 @@ zder = zder_6th
 xder2 = xder2_6th
 yder2 = yder2_6th
 zder2 = zder2_6th
+
+xder3 = xder3_4th
+yder3 = yder3_4th
+zder3 = zder3_4th
 
 xder6 = xder6_6th
 yder6 = yder6_6th
