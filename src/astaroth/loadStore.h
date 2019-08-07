e5ff7c2c77acd01ea258731c4eeeaeb0e33f3576
mrheinhardt <matthias.rheinhardt@aalto.fi>
2019-08-07 22:06:33 +0300

    MR: header for loadStore.cc

---
+++ src/astaroth/loadStore.h
@@ -0,0 +1,9 @@
+void initLoadStore();
+void finalLoadStore();
+void loadOuterFront(AcMesh& mesh);
+void loadOuterBack(AcMesh& mesh);
+void loadOuterBottom(AcMesh& mesh);
+void loadOuterTop(AcMesh& mesh);
+void loadOuterLeft(AcMesh& mesh);
+void loadOuterRight(AcMesh& mesh);
+
