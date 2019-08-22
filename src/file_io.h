9e2f5f6f27166345162327a747393a794407028a
mrheinhardt <mreinhardt@nordita.org>
2019-08-22 22:39:19 +0300

    MR: added flush_file

--- src/file_io.h
+++ src/file_io.h
@@ -3,6 +3,6 @@
   public :: parallel_unit, parallel_unit_vec, parallel_open, parallel_close, parallel_rewind
   public :: find_namelist, read_namelist, fseek_pos, backskip_to_time, read_zaver
   public :: file_remove, file_exists, file_size, delete_file, count_lines, parallel_file_exists, parallel_count_lines
-  public :: list_files
+  public :: list_files, flush_file
 !
   include 'record_types.h'
