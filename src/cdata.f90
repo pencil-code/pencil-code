5e3406706e789134b8a6a640f8c28c20adf0a998
Axel Brandenburg <brandenb@nordita.org>
2019-08-28 17:45:07 +0200

    added vector mode, as well as projected and unprojected stress tensor spectra

--- src/cdata.f90
+++ src/cdata.f90
@@ -530,7 +530,7 @@ module Cdata
   logical :: ub_spec=.false., Lor_spec=.false., EMF_spec=.false., Tra_spec=.false.
   logical :: GWs_spec=.false., GWh_spec=.false., GWm_spec=.false., Str_spec=.false.
   logical :: GWd_spec=.false., GWe_spec=.false., GWf_spec=.false., GWg_spec=.false.
-  logical :: SCL_spec=.false.
+  logical :: SCL_spec=.false., VCT_spec=.false., Tpq_spec=.false.
   logical :: har_spec=.false.,hav_spec=.false.
   logical :: oned=.false.,twod=.false.
   logical :: ab_phispec=.false.,ou_phispec=.false.
