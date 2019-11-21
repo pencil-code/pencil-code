6907d1a825ca13819ad879ca5d16a9e9fd3c97e4
iomsn <iomsn1@googlemail.com>
2019-11-21 11:55:56 +0000

    Removed redundant comments.

--- python/pencilnew/export/__init__.py
+++ python/pencilnew/export/__init__.py
@@ -1,11 +1,7 @@
-################
-##
-##	exporter
-##
-################
+'''
+Mostly routines for exporting data into different formats.
+'''
 
-####### vtk
 from .arrays2d_to_vtk import arrays2d_to_vtk
 from .particles_to_vtk import particles_to_vtk, ParticlesVtk
-####### write hdf5
 from .create_h5 import create_aver_sph
