668b0135c2153380124de87a2c685720cfebba1f
mrheinhardt <mreinhardt@nordita.org>
2019-12-04 15:13:24 +0200

    MR: made nprocs public

--- src/mpicomm.h
+++ src/mpicomm.h
@@ -69,7 +69,7 @@
   public :: radboundary_yz_periodic_ray, radboundary_zx_periodic_ray
 
 ! Variables
-  public :: ipx, ipy, ipz, lroot, iproc, mpi_precision
+  public :: ipx, ipy, ipz, lroot, iproc, mpi_precision, nprocs
   public :: lfirst_proc_x, lfirst_proc_y, lfirst_proc_z, lfirst_proc_xy, lfirst_proc_yz, lfirst_proc_xz, lfirst_proc_xyz
   public :: llast_proc_x, llast_proc_y, llast_proc_z, llast_proc_xy, llast_proc_yz, llast_proc_xz, llast_proc_xyz
   public :: MPI_COMM_WORLD, MPI_COMM_GRID, MPI_COMM_PENCIL, MPI_COMM_XYPLANE, MPI_COMM_XZPLANE, MPI_COMM_YZPLANE, &
