0ed6df099a739cc36ee9f2539be249571f979015
wlyra <wladimir.lyra@gmail.com>
2020-02-29 17:11:43 -0700

    Initial condition for shallow water run

---
+++ src/initial_condition/shallow_water_bottom.f90
@@ -0,0 +1,103 @@
+! $Id$
+!
+!** AUTOMATIC CPARAM.INC GENERATION ****************************
+! Declare (for generation of cparam.inc) the number of f array
+! variables and auxiliary variables added by this module
+!
+! CPARAM logical, parameter :: linitial_condition = .true.
+!
+!***************************************************************
+!
+!
+module InitialCondition
+!
+  use Cparam
+  use Cdata
+  use General, only: keep_compiler_quiet
+  use Messages
+!
+  implicit none
+!
+  include '../initial_condition.h'
+!
+  real :: eta0, k_eta, x0_drop, y0_drop
+!
+  namelist /initial_condition_pars/  eta0, k_eta, x0_drop, y0_drop
+!
+  contains
+!***********************************************************************
+    subroutine register_initial_condition()
+!
+!  Register variables associated with this module; likely none.
+!
+!  07-may-09/wlad: coded
+!
+      if (lroot) call svn_id( &
+           "$Id$")
+!
+    endsubroutine register_initial_condition
+!***********************************************************************
+    subroutine initialize_initial_condition(f)
+!
+!  Initialize any module variables which are parameter dependent.
+!
+!  07-may-09/wlad: coded
+!
+      real, dimension (mx,my,mz,mfarray) :: f
+!
+      call keep_compiler_quiet(f)
+!
+    endsubroutine initialize_initial_condition
+!***********************************************************************
+    subroutine initial_condition_lnrho(f)
+!
+!  Initial condition given by 
+!
+!     h = eta + Lb
+!     rho is eta       
+!
+!  with eta=a*exp(-k*(x-x0)**2)
+!
+      real, dimension (mx,my,mz,mfarray), intent(inout) :: f
+      real, dimension (nx) :: eta
+!
+      do n=n1,n2
+        do m=m1,m2
+          eta = eta0 * exp(-k_eta * ( & 
+               (x(l1:l2)-x0_drop)**2 + (y(m)-y0_drop)**2 & 
+               ))
+          f(l1:l2,m,n,ilnrho) = log(eta)
+        enddo
+      enddo
+!
+    endsubroutine initial_condition_lnrho
+!***********************************************************************
+    subroutine read_initial_condition_pars(iostat)
+!
+      use File_io, only: parallel_unit
+!
+      integer, intent(out) :: iostat
+!
+      read(parallel_unit, NML=initial_condition_pars, IOSTAT=iostat)
+!
+    endsubroutine read_initial_condition_pars
+!***********************************************************************
+    subroutine write_initial_condition_pars(unit)
+!
+      integer, intent(in) :: unit
+!
+      write(unit, NML=initial_condition_pars)
+!
+    endsubroutine write_initial_condition_pars
+!***********************************************************************
+!
+!********************************************************************
+!************        DO NOT DELETE THE FOLLOWING       **************
+!********************************************************************
+!**  This is an automatically generated include file that creates  **
+!**  copies dummy routines from noinitial_condition.f90 for any    **
+!**  InitialCondition routines not implemented in this file        **
+!**                                                                **
+    include '../initial_condition_dummies.inc'
+!********************************************************************
+endmodule InitialCondition
