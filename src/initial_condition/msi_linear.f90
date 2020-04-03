2fa1f718ea4125ffb9aa55251185e9287fb7d2e4
Chao-Chin Yang <ccyang@unlv.edu>
2020-04-02 23:59:51 -0500

    Centralize src/.

---
+++ src/initial_condition/msi_linear.f90
@@ -0,0 +1,387 @@
+! $Id$
+!
+!  This module sets up nonlinear streaming instability with
+!  multiple particle species.
+!
+!** AUTOMATIC CPARAM.INC GENERATION ****************************
+! Declare (for generation of cparam.inc) the number of f array
+! variables and auxiliary variables added by this module
+!
+! CPARAM logical, parameter :: linitial_condition = .true.
+!
+!***************************************************************
+module InitialCondition
+!
+  use Cparam
+  use Cdata
+  use General, only: keep_compiler_quiet
+  use Messages
+  use Particles_cdata
+  use Particles_sub, only: dragforce_equi_multispecies 
+!
+  implicit none
+!
+  include '../initial_condition.h'
+!
+! Module Variables
+!
+  real, dimension(npar_species) :: taus = 0.0, eps0 = 0.0, vpx0 = 0.0, vpy0 = 0.0
+  real :: eta_vK = 0.0, ux0 = 0.0, uy0 = 0.0
+!
+! Input Parameters
+!
+  complex, dimension(4*(npar_species+1)) :: si_ev = (0.0, 0.0)
+  logical :: ltaus_log_center = .true.
+  real :: logtausmin = -4.0, logtausmax = -1.0
+  real :: dlnndlntaus = -4.0
+  real :: dlnrhodlnr = -0.1
+  real :: si_kx = 0.0, si_kz = 0.0, si_amp = 1E-6
+!
+  namelist /initial_condition_pars/ &
+    ltaus_log_center, logtausmin, logtausmax, dlnndlntaus, dlnrhodlnr, si_kx, si_kz, si_ev, si_amp
+!
+  contains
+!***********************************************************************
+    subroutine initialize_initial_condition(f)
+!
+! Initialize any module variables which are parameter dependent.
+!
+! 10-mar-20/ccyang: coded
+!
+      use EquationOfState, only: cs0
+!
+      real, dimension(mx,my,mz,mfarray), intent(in) :: f
+!
+      integer :: i
+      real :: dlogtaus
+!
+      call keep_compiler_quiet(f)
+!
+! Assemble stopping times.
+!
+      dlogtaus = (logtausmax - logtausmin) / real(npar_species)
+      gettaus: if (ltaus_log_center) then
+        taus = logtausmin + real((/ (i, i = 1, npar_species) /) - 0.5) * dlogtaus
+        if (lroot) print *, "initialize_initial_condition: log(taus) = ", taus
+        taus = 10.0**taus
+      else gettaus
+        taus = 0.5 * 10.0**logtausmin * (10.0**(real((/ (i, i = 0, npar_species - 1) /)) * dlogtaus) + &
+                                         10.0**(real((/ (i, i = 1, npar_species) /)) * dlogtaus))
+        if (lroot) print *, "initialize_initial_condition: taus = ", taus
+      endif gettaus
+!
+! Evaluate the radial pressure gradient support.
+!
+      eta_vK = -0.5 * dlnrhodlnr * cs0
+      if (lroot) print*, 'initialize_initial_condition: eta * v_K = ', eta_vK
+!
+! Find the density ratio for each species.
+!
+      eps0 = taus**(4.0 + dlnndlntaus)
+      eps0 = eps_dtog / sum(eps0) * eps0
+!
+! Find the equilibrium velocities.
+!
+      call dragforce_equi_multispecies(npar_species, taus, eps0, eta_vK, vpx0, vpy0, ux0, uy0)
+      eqvel: if (lroot) then
+        print *, "initialize_initial_condition: ux0, uy0 = ", ux0, uy0
+        print *, "initialize_initial_condition: vpx0 = ", vpx0
+        print *, "initialize_initial_condition: vpy0 = ", vpy0
+      endif eqvel
+!
+! Save the equilibrium velocities to a file.
+!
+      open(10, file="data/multisp_drag_eq.dat", form="unformatted", action="write")
+      write(10) ux0, uy0, vpx0, vpy0
+      close(10)
+!
+    endsubroutine initialize_initial_condition
+!***********************************************************************
+    subroutine initial_condition_uu(f)
+!
+! Initialize the velocity field.
+!
+!  30-mar-20/ccyang: coded
+!
+      real, dimension(mx,my,mz,mfarray), intent(inout) :: f
+!
+      real, dimension(nx) :: argx, sinkx, coskx, dux, duy, duz
+      real :: argz, sinkz, coskz
+!
+! Assign the equilibrium velocities to the gas
+!
+      f(l1:l2,m1:m2,n1:n2,iux) = f(l1:l2,m1:m2,n1:n2,iux) + ux0
+      f(l1:l2,m1:m2,n1:n2,iuy) = f(l1:l2,m1:m2,n1:n2,iuy) + uy0
+!
+! Perturb the gas.
+!
+      argx = si_kx * x(l1:l2)
+      sinkx = sin(argx)
+      coskx = cos(argx)
+!
+      duz = si_amp * eta_vK
+      dux = duz * (real(si_ev(1)) * coskx - aimag(si_ev(1)) * sinkx)
+      duy = duz * (real(si_ev(2)) * coskx - aimag(si_ev(2)) * sinkx)
+      duz = duz * (real(si_ev(3)) * sinkx + aimag(si_ev(3)) * coskx)
+!
+      nloop: do n = n1, n2
+        argz = si_kz * z(n)
+        sinkz = sin(argz)
+        coskz = cos(argz)
+!
+        mloop: do m = m1, m2
+          f(l1:l2,m,n,iux) = f(l1:l2,m,n,iux) + dux * coskz
+          f(l1:l2,m,n,iuy) = f(l1:l2,m,n,iuy) + duy * coskz
+          f(l1:l2,m,n,iuz) = f(l1:l2,m,n,iuz) - duz * sinkz
+        enddo mloop
+      enddo nloop
+!
+    endsubroutine initial_condition_uu
+!***********************************************************************
+    subroutine initial_condition_lnrho(f)
+!
+! Initialize logarithmic density.
+!
+!  30-mar-20/ccyang: coded
+!
+      use EquationOfState, only: rho0
+!
+      real, dimension(mx,my,mz,mfarray), intent(inout) :: f
+!
+      real, dimension(nx) :: argx, drho
+      real :: coskz
+!
+! Perturb the gas density.
+!
+      argx = si_kx * x(l1:l2)
+      drho = si_amp * rho0 * (real(si_ev(4)) * cos(argx) - aimag(si_ev(4)) * sin(argx))
+!
+      nloop: do n = n1, n2
+        coskz = cos(si_kz * z(n))
+        do m = m1, m2
+          f(l1:l2,m,n,irho) = rho0 + drho * coskz
+        enddo
+      enddo nloop
+!
+! Convert to logarithmic density.
+!
+      f(l1:l2,m1:m2,n1:n2,ilnrho) = log(f(l1:l2,m1:m2,n1:n2,irho))
+!
+    endsubroutine initial_condition_lnrho
+!***********************************************************************
+    subroutine initial_condition_xxp(f, fp)
+!
+! Initialize particles' positions.
+!
+! 30-mar-20/ccyang: coded
+!
+      real, dimension(mx,my,mz,mfarray), intent(in) :: f
+      real, dimension(:,:), intent(inout) :: fp
+!
+      integer, dimension(npar_species) :: ip
+      real, dimension(npar_species) :: ar, ai, a1, a2, a3
+      integer :: npps, npx, npz, ix, iz, is, k
+      real :: dxp, dzp, xp, yp, zp, dxp1, dzp1
+      real :: argx, argz
+      real :: sinp, sinm, cosp, cosm
+      real :: sinp2, sinm2, cosp2, cosm2
+      real :: sin2kz, cos2kx, sin2kx
+      real :: c1x, c1z, c2x, c2z
+!
+      call keep_compiler_quiet(f)
+!
+! Sanity checks
+!
+      proc: if (mod(npar, ncpus) /= 0 .or. npar_loc /= npar / ncpus) then
+        if (lroot) print *, "initialize_initial_condition: npar, ncpus, npar_loc = ", npar, ncpus, npar_loc
+        call fatal_error("initialize_initial_condition", "particles are not evenly distributed among processors")
+      endif proc
+!
+      species: if (mod(npar_loc, npar_species) /= 0) then
+        if (lroot) print *, "initialize_initial_condition: npar_loc, npar_species = ", npar_loc, npar_species
+        call fatal_error("initialize_initial_condition", "particle species are not evenly divided")
+      endif species
+!
+! Find the initial ID for each species.
+!
+      npps = npar_loc / npar_species
+      ip = (/ ((k - 1) * (npar / npar_species) + iproc * npps, k = 1, npar_species) /)
+!
+! Find the spacing between particles.
+!
+      getnp: if (nzgrid > 1) then
+        npx = nint(sqrt(Lxyz_loc(1) * real(npps) / Lxyz_loc(3)))
+        npz = npps / npx
+      else getnp
+        npx = npps
+        npz = 1
+      endif getnp
+!
+      grid: if (npx * npz /= npps) then
+        if (lroot) print *, "initialize_initial_condition: Lx_loc, Lz_loc = ", Lxyz_loc(1), Lxyz_loc(3)
+        if (lroot) print *, "initialize_initial_condition: npps, npx, npz = ", npps, npx, npz
+        call fatal_error("initialize_initial_condition", "cannot find equal spacing between particles")
+      endif grid
+!
+      dxp = Lxyz_loc(1) / real(npx)
+      dzp = Lxyz_loc(3) / real(npz)
+      if (lroot) print *, "initialize_initial_condition: npx, npz = ", npx, npz
+      if (lroot) print *, "initialize_initial_condition: dxp, dzp = ", dxp, dzp
+!
+! Compute repeated constants.
+!
+      c1x = si_kx**2 + si_kz**2
+      c2x = c1x**2
+      coeff: if (c1x > 0.0) then
+        c1x = 0.5 / c1x
+        c2x = 1.0 / c2x
+      endif coeff
+      c1z = c1x * si_kz
+      c2z = c2x * si_kz**3
+      c1x = c1x * si_kx
+      c2x = c2x * si_kx**3
+!
+      ar = si_amp * real(si_ev(8::4)) / eps0
+      ai = si_amp * aimag(si_ev(8::4)) / eps0
+      a1 = 0.25 * (ar**2 - ai**2)
+      a2 = 0.5 * ar * ai
+      a3 = 0.25 * (ar**2 + ai**2)
+!
+! Assign particle positions and IDs.
+!
+      k = 0
+      yp = xyz0(2) + 0.5 * Lxyz(2)
+!
+      zloop: do iz = 1, npz
+        zp = xyz0_loc(3) + (real(iz) - 0.5) * dzp
+        argz = si_kz * zp
+        sin2kz = sin(2.0 * argz)
+!
+        xloop: do ix = 1, npx
+          xp = xyz0_loc(1) + (real(ix) - 0.5) * dxp
+          argx = si_kx * xp
+          cos2kx = cos(2.0 * argx)
+          sin2kx = sin(2.0 * argx)
+!
+          sinp = sin(argx + argz)
+          sinm = sin(argx - argz)
+          cosp = cos(argx + argz)
+          cosm = cos(argx - argz)
+!
+          sinp2 = sin(2.0 * (argx + argz))
+          sinm2 = sin(2.0 * (argx - argz))
+          cosp2 = cos(2.0 * (argx + argz))
+          cosm2 = cos(2.0 * (argx - argz))
+!
+          sloop: do is = 1, npar_species
+            dxp1 = -c1x * (ar(is) * (sinp + sinm) + ai(is) * (cosp + cosm) &
+                         - a1(is) * (sinp2 + sinm2) - a2(is) * (cosp2 + cosm2)) &
+                   + c2x * (a2(is) * cos2kx + a1(is) * sin2kx)
+            dzp1 = -c1z * (ar(is) * (sinp - sinm) + ai(is) * (cosp - cosm) &
+                         - a1(is) * (sinp2 - sinm2) - a2(is) * (cosp2 - cosm2)) &
+                   + c2z * a3(is) * sin2kz
+!
+            k = k + 1
+            fp(k,ixp) = xp + dxp1
+            fp(k,iyp) = yp
+            fp(k,izp) = zp + dzp1
+            ip(is) = ip(is) + 1
+            ipar(k) = ip(is)
+          enddo sloop
+        enddo xloop
+      enddo zloop
+!
+    endsubroutine initial_condition_xxp
+!***********************************************************************
+    subroutine initial_condition_vvp(f, fp)
+!
+! Initialize particles' mass and velocity.
+!
+! 30-mar-20/ccyang: coded
+!
+      use EquationOfState, only: rho0
+      use SharedVariables, only: get_shared_variable
+!
+      real, dimension(mx,my,mz,mfarray), intent(in) :: f
+      real, dimension(:,:), intent(inout) :: fp
+!
+      real, dimension(:), pointer :: tausp_species, tausp1_species
+!
+      real, dimension(npar_species) :: rhopj, vpx, vpy
+      real :: argx, argz, sinkx, coskx, sinkz, coskz
+      real :: dv, dvpx, dvpy, dvpz
+!
+      integer :: k, p, i
+      real :: ux, uy, hgas, zp
+!
+      call keep_compiler_quiet(f)
+!
+! Find the mass of each particle.
+!
+      rhopj = rho0 / real(npar / (npar_species * nxgrid * nygrid * nzgrid)) * eps0
+      if (lroot) print *, "initial_condition_vvp: rhopj = ", rhopj
+!
+! Assign the mass and velocity of each particle.
+!
+      dv = si_amp * eta_vK
+      ploop: do k = 1, npar_loc
+        argx = si_kx * fp(k,ixp)
+        argz = si_kz * fp(k,izp)
+        sinkx = sin(argx)
+        coskx = cos(argx)
+        sinkz = sin(argz)
+        coskz = cos(argz)
+!
+        p = npar_species * (ipar(k) - 1) / npar + 1
+        i = 4 * p + 1
+        dvpx = dv * (real(si_ev(i)) * coskx - aimag(si_ev(i)) * sinkx) * coskz
+        dvpy = dv * (real(si_ev(i+1)) * coskx - aimag(si_ev(i+1)) * sinkx) * coskz
+        dvpz = -dv * (real(si_ev(i+2)) * sinkx + aimag(si_ev(i+2)) * coskx) * sinkz
+!
+        fp(k,irhopswarm) = rhopj(p)
+        fp(k,ivpx) = fp(k,ivpx) + vpx0(p) + dvpx
+        fp(k,ivpy) = fp(k,ivpy) + vpy0(p) + dvpy
+        fp(k,ivpz) = fp(k,ivpz) + dvpz
+      enddo ploop
+!
+! Override the stopping times in particles_dust.
+!
+      multisp: if (npar_species > 1) then
+        call get_shared_variable("tausp_species", tausp_species)
+        call get_shared_variable("tausp1_species", tausp1_species)
+        tausp_species = taus / omega
+        tausp1_species = omega / taus
+        if (lroot) print *, "initial_condition_vvp: override tausp_species = ", tausp_species
+      endif multisp
+!
+    endsubroutine initial_condition_vvp
+!***********************************************************************
+    subroutine read_initial_condition_pars(iostat)
+!
+      use File_io, only: parallel_unit
+!
+      integer, intent(out) :: iostat
+!
+      read(parallel_unit, NML=initial_condition_pars, IOSTAT=iostat)
+!
+    endsubroutine read_initial_condition_pars
+!***********************************************************************
+    subroutine write_initial_condition_pars(unit)
+!
+      integer, intent(in) :: unit
+!
+      write(unit, NML=initial_condition_pars)
+!
+    endsubroutine write_initial_condition_pars
+!***********************************************************************
+!
+!********************************************************************
+!************        DO NOT DELETE THE FOLLOWING       **************
+!********************************************************************
+!**  This is an automatically generated include file that creates  **
+!**  copies dummy routines from noinitial_condition.f90 for any    **
+!**  InitialCondition routines not implemented in this file        **
+!**                                                                **
+    include '../initial_condition_dummies.inc'
+!********************************************************************
+endmodule InitialCondition
---
+++ src/special/multisp_drag_eq.f90
@@ -0,0 +1,287 @@
+! $Id$
+!
+!  This module provide a way for users to specify custom
+!  (i.e. not in the standard Pencil Code) physics, diagnostics etc.
+!
+!** AUTOMATIC CPARAM.INC GENERATION ****************************
+! Declare (for generation of special_dummies.inc) the number of f array
+! variables and auxiliary variables added by this module
+!
+! CPARAM logical, parameter :: lspecial = .true.
+!
+! MVAR CONTRIBUTION 0
+! MAUX CONTRIBUTION 0
+!
+!***************************************************************
+module Special
+!
+  use Cparam
+  use Cdata
+  use General, only: keep_compiler_quiet
+  use Messages, only: svn_id, fatal_error
+!
+  implicit none
+!
+  include "../special.h"
+!
+! Module Variables
+!
+  real, dimension(npar_species) :: vpx0 = 0.0, vpy0 = 0.0
+  real :: ux0 = 0.0, uy0 = 0.0
+!
+! Diagnostic variables (needs to be consistent with reset list below).
+!
+  integer :: idiag_rduxm = 0
+  integer :: idiag_rduym = 0
+  integer :: idiag_rdux2m = 0
+  integer :: idiag_rduy2m = 0
+  integer :: idiag_rduxduym = 0
+  integer :: idiag_ruzduxm = 0
+  integer :: idiag_ruzduym = 0
+!
+  integer :: idiag_drhopm = 0
+  integer :: idiag_drhop2m = 0
+  integer :: idiag_rhopdvpxm = 0
+  integer :: idiag_rhopdvpym = 0
+  integer :: idiag_rhopdvpx2m = 0
+  integer :: idiag_rhopdvpy2m = 0
+  integer :: idiag_rhopvpz2m = 0
+!
+  contains
+!****************************************************************************
+    subroutine initialize_special(f)
+!
+!  Called after reading parameters, but before the time loop.
+!
+!  04-oct-19/ccyang: coded
+!
+      real, dimension(mx,my,mz,mfarray), intent(in) :: f
+!
+      call keep_compiler_quiet(f)
+      if (lstart) return
+!
+!  Read the equilibrium velocities from the initial conditions.
+!
+      open(10, file="data/multisp_drag_eq.dat", form="unformatted", action="read")
+      read(10) ux0, uy0, vpx0, vpy0
+      close(10)
+!
+!  Log the velocities.
+!
+      info: if (lroot) then
+        print *, "initialize_special: ux0, uy0 = ", ux0, uy0
+        print *, "initialize_special: vpx0 = ", vpx0
+        print *, "initialize_special: vpy0 = ", vpy0
+      endif info
+!
+    endsubroutine initialize_special
+!***********************************************************************
+    subroutine pencil_criteria_special
+!
+!  All pencils that this special module depends on are specified here.
+!
+!  04-oct-19/ccyang: coded
+!
+      gas: if (idiag_rduxm /= 0 .or. idiag_rduym /= 0 .or. &
+               idiag_rdux2m /= 0 .or. idiag_rduy2m /= 0 .or. &
+               idiag_rduxduym /= 0 .or. idiag_ruzduxm /= 0 .or. idiag_ruzduym /= 0) then
+        lpenc_diagnos(i_rho) = .true.
+        lpenc_diagnos(i_uu) = .true.
+      endif gas
+!
+      if (idiag_drhopm /= 0 .or. idiag_drhop2m /= 0) lpenc_diagnos(i_rhop) = .true.
+!
+    endsubroutine pencil_criteria_special
+!***********************************************************************
+    subroutine dspecial_dt(f, df, p)
+!
+!  calculate right hand side of ONE OR MORE extra coupled PDEs
+!  along the 'current' Pencil, i.e. f(l1:l2,m,n) where
+!  m,n are global variables looped over in equ.f90
+!
+!  Due to the multi-step Runge Kutta timestepping used one MUST always
+!  add to the present contents of the df array.  NEVER reset it to zero.
+!
+!  Several precalculated Pencils of information are passed for
+!  efficiency.
+!
+!  04-oct-19/ccyang: coded
+!
+      use Diagnostics, only: sum_mn_name
+      use EquationOfState, only: rho0
+      use Particles_cdata, only: eps_dtog
+!
+      real, dimension(mx,my,mz,mfarray), intent(in) :: f
+      real, dimension(mx,my,mz,mvar), intent(in) :: df
+      type(pencil_case), intent(in) :: p
+!
+      real, dimension(nx) :: dux, duy, drhop
+!
+      call keep_compiler_quiet(f, df)
+!
+!  Identify module and boundary conditions.
+!
+      if (headtt.or.ldebug) print *, "dspecial_dt: special diagnostics"
+!
+!  Diagnostics
+!
+      diag: if (ldiagnos) then
+        dux = p%uu(:,1) - ux0
+        duy = p%uu(:,2) - uy0
+        if (idiag_rduxm /= 0) call sum_mn_name(p%rho * dux, idiag_rduxm)
+        if (idiag_rduym /= 0) call sum_mn_name(p%rho * duy, idiag_rduym)
+        if (idiag_rdux2m /= 0) call sum_mn_name(p%rho * dux**2, idiag_rdux2m)
+        if (idiag_rduy2m /= 0) call sum_mn_name(p%rho * duy**2, idiag_rduy2m)
+        if (idiag_rduxduym /= 0) call sum_mn_name(p%rho * dux * duy, idiag_rduxduym)
+        if (idiag_ruzduxm /= 0) call sum_mn_name(p%rho * p%uu(:,3) * dux, idiag_ruzduxm)
+        if (idiag_ruzduym /= 0) call sum_mn_name(p%rho * p%uu(:,3) * duy, idiag_ruzduym)
+!
+        drhop = p%rhop - eps_dtog * rho0
+        if (idiag_drhopm /= 0) call sum_mn_name(drhop, idiag_drhopm)
+        if (idiag_drhop2m /= 0) call sum_mn_name(drhop**2, idiag_drhop2m)
+      endif diag
+!
+    endsubroutine dspecial_dt
+!***********************************************************************
+    subroutine special_calc_particles(f, df, fp, dfp, ineargrid)
+!
+!  Called before the loop, in case some particle value is needed
+!  for the special density/hydro/magnetic/entropy.
+!
+!  04-oct-19/ccyang: coded
+!
+      use Particles_sub, only: sum_par_name
+      use Particles_cdata, only: ipar, npar_loc, ivpx, ivpy, ivpz, irhopswarm
+!
+      real, dimension(mx,my,mz,mfarray), intent(in) :: f
+      real, dimension(mx,my,mz,mvar), intent(in) :: df
+      real, dimension(:,:), intent(in) :: fp, dfp
+      integer, dimension(:,:), intent(in) :: ineargrid
+!
+      real, dimension(npar_loc) :: dvpx, dvpy
+      integer :: k, jspec
+!
+      call keep_compiler_quiet(f, df)
+      call keep_compiler_quiet(dfp)
+      call keep_compiler_quiet(ineargrid)
+!
+      diag: if (ldiagnos) then
+!
+!  Compute the deviation from the equilibrium velocities.
+!
+        vpeq: do k = 1, npar_loc
+          jspec = npar_species * (ipar(k) - 1) / npar + 1
+          dvpx(k) = fp(k,ivpx) - vpx0(jspec)
+          dvpy(k) = fp(k,ivpy) - vpy0(jspec)
+        enddo vpeq
+!
+!  Compute the diagnostics
+!
+        rhop: if (lparticles_density) then
+          if (idiag_rhopdvpxm /= 0) call sum_par_name(fp(1:npar_loc,irhopswarm) * dvpx, idiag_rhopdvpxm)
+          if (idiag_rhopdvpym /= 0) call sum_par_name(fp(1:npar_loc,irhopswarm) * dvpy, idiag_rhopdvpym)
+          if (idiag_rhopdvpx2m /= 0) call sum_par_name(fp(1:npar_loc,irhopswarm) * dvpx**2, idiag_rhopdvpx2m)
+          if (idiag_rhopdvpy2m /= 0) call sum_par_name(fp(1:npar_loc,irhopswarm) * dvpy**2, idiag_rhopdvpy2m)
+          if (idiag_rhopvpz2m /= 0) call sum_par_name(fp(1:npar_loc,irhopswarm) * fp(1:npar_loc,ivpz)**2, idiag_rhopvpz2m)
+        endif rhop
+!
+      endif diag
+!
+    endsubroutine special_calc_particles
+!***********************************************************************
+    subroutine rprint_special(lreset, lwrite)
+!
+!  Reads and registers print parameters relevant to special.
+!
+!  04-oct-19/ccyang: coded
+!
+      use Diagnostics, only: parse_name
+      use FArrayManager, only: farray_index_append
+!
+      logical, intent(in) :: lreset
+      logical, intent(in), optional :: lwrite
+!
+      logical :: lwr
+      integer :: iname
+!
+      lwr = .false.
+      if (present(lwrite)) lwr = lwrite
+!
+!  Reset everything in case of reset
+!
+      reset: if (lreset) then
+        idiag_rduxm = 0
+        idiag_rduym = 0
+        idiag_rdux2m = 0
+        idiag_rduy2m = 0
+        idiag_rduxduym = 0
+        idiag_ruzduxm = 0
+        idiag_ruzduym = 0
+!
+        idiag_drhopm = 0
+        idiag_drhop2m = 0
+        idiag_rhopdvpxm = 0
+        idiag_rhopdvpym = 0
+        idiag_rhopdvpx2m = 0
+        idiag_rhopdvpy2m = 0
+        idiag_rhopvpz2m = 0
+      endif reset
+!
+!  Turn on requested diagnostics.
+!
+      diag: do iname = 1, nname
+        call parse_name(iname, cname(iname), cform(iname), "rduxm", idiag_rduxm)
+        call parse_name(iname, cname(iname), cform(iname), "rduym", idiag_rduym)
+        call parse_name(iname, cname(iname), cform(iname), "rdux2m", idiag_rdux2m)
+        call parse_name(iname, cname(iname), cform(iname), "rduy2m", idiag_rduy2m)
+        call parse_name(iname, cname(iname), cform(iname), "rduxduym", idiag_rduxduym)
+        call parse_name(iname, cname(iname), cform(iname), "ruzduxm", idiag_ruzduxm)
+        call parse_name(iname, cname(iname), cform(iname), "ruzduym", idiag_ruzduym)
+!
+        call parse_name(iname, cname(iname), cform(iname), "drhopm", idiag_drhopm)
+        call parse_name(iname, cname(iname), cform(iname), "drhop2m", idiag_drhop2m)
+        call parse_name(iname, cname(iname), cform(iname), "rhopdvpxm", idiag_rhopdvpxm)
+        call parse_name(iname, cname(iname), cform(iname), "rhopdvpym", idiag_rhopdvpym)
+        call parse_name(iname, cname(iname), cform(iname), "rhopdvpx2m", idiag_rhopdvpx2m)
+        call parse_name(iname, cname(iname), cform(iname), "rhopdvpy2m", idiag_rhopdvpy2m)
+        call parse_name(iname, cname(iname), cform(iname), "rhopvpz2m", idiag_rhopvpz2m)
+      enddo diag
+!
+!  Write column where which magnetic variable is stored
+!
+      wr: if (lwr) then
+        call farray_index_append("idiag_rduxm", idiag_rduxm)
+        call farray_index_append("idiag_rduym", idiag_rduym)
+        call farray_index_append("idiag_rdux2m", idiag_rdux2m)
+        call farray_index_append("idiag_rduy2m", idiag_rduy2m)
+        call farray_index_append("idiag_rduxduym", idiag_rduxduym)
+        call farray_index_append("idiag_ruzduxm", idiag_ruzduxm)
+        call farray_index_append("idiag_ruzduym", idiag_ruzduym)
+!
+        call farray_index_append("idiag_drhopm", idiag_drhopm)
+        call farray_index_append("idiag_drhop2m", idiag_drhop2m)
+        call farray_index_append("idiag_rhopdvpxm", idiag_rhopdvpxm)
+        call farray_index_append("idiag_rhopdvpym", idiag_rhopdvpym)
+        call farray_index_append("idiag_rhopdvpx2m", idiag_rhopdvpx2m)
+        call farray_index_append("idiag_rhopdvpy2m", idiag_rhopdvpy2m)
+        call farray_index_append("idiag_rhopvpz2m", idiag_rhopvpz2m)
+      endif wr
+!
+    endsubroutine rprint_special
+!***********************************************************************
+!***********************************************************************
+!
+!***********************************************************************
+!***********************************************************************
+!********************************************************************
+!
+!********************************************************************
+!************        DO NOT DELETE THE FOLLOWING       **************
+!********************************************************************
+!**  This is an automatically generated include file that creates  **
+!**  copies dummy routines from nospecial.f90 for any Special      **
+!**  routines not implemented in this file                         **
+!**                                                                **
+    include '../special_dummies.inc'
+!********************************************************************
+endmodule Special
