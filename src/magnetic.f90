90445c5ab2454b19894f379a4438d9cadf3a96bc
mrheinhardt <mreinhardt@nordita.org>
2020-05-05 04:53:24 +0300

    MR: quenching of anisotropic eta by A_rms for automatic determination of critical value

--- src/magnetic.f90
+++ src/magnetic.f90
@@ -126,6 +126,7 @@ module Magnetic
   real :: eta1_aniso_ratio=impossible, eta1_aniso=impossible
   real :: eta1_aniso_r=0.0, eta1_aniso_d=0.0
   real :: eta_shock=0.0, eta_shock2=0.0, alp_aniso=0.0
+  real :: quench_aniso=impossible
   real :: eta_va=0., eta_j=0., eta_j2=0., eta_jrho=0., eta_min=0., &
           etaj20=0., va_min=0., vArms=1.
   real :: rhomin_jxb=0.0, va2max_jxb=0.0, va2max_boris=0.0,cmin=0.0
@@ -170,7 +171,7 @@ module Magnetic
   logical :: lresi_eta_const=.false.
   logical :: lresi_eta_tdep=.false.
   logical :: lresi_sqrtrhoeta_const=.false.
-  logical :: lresi_eta_aniso=.false.
+  logical :: lresi_eta_aniso=.false., lquench_eta_aniso=.false.
   logical :: lresi_etaSS=.false.
   logical :: lresi_hyper2=.false.
   logical :: lresi_hyper3=.false.
@@ -350,7 +351,7 @@ module Magnetic
       eta_xwidth, eta_ywidth, eta_zwidth, eta_rwidth, &
       eta_zwidth2, eta_xwidth0, eta_xwidth1, eta_rwidth0, eta_rwidth1, &
       eta_z0, eta_z1, eta_y0, eta_y1, eta_x0, eta_x1, eta_r0, eta_r1, &
-      eta1_aniso_ratio, eta1_aniso, eta1_aniso_r, eta1_aniso_d, alp_aniso, &
+      eta1_aniso_ratio, eta1_aniso, eta1_aniso_r, eta1_aniso_d, alp_aniso, quench_aniso, &
       eta_spitzer, borderaa, &
       eta_aniso_hyper3, lelectron_inertia, inertial_length, &
       lbext_curvilinear, lbb_as_aux, lbb_as_comaux, lB_ext_in_comaux, ljj_as_aux, &
@@ -685,6 +686,7 @@ module Magnetic
                                 ! DIAG_DOC: $\eta\sim J^2 / \rho$
   integer :: idiag_etajrhomax=0 ! DIAG_DOC: Max of artificial resistivity
                                 ! DIAG_DOC: $\eta\sim J / \rho$
+  integer :: idiag_etaaniso=0   ! DIAG_DOC: $\eta_1$
   integer :: idiag_cosjbm=0     ! DIAG_DOC: $\left<\Jv\cdot\Bv/(|\Jv|\,|\Bv|)\right>$
   integer :: idiag_coshjbm=0    ! DIAG_DOC:
   integer :: idiag_jparallelm=0 ! DIAG_DOC: Mean value of the component
@@ -917,7 +919,7 @@ module Magnetic
                          diffus_eta=0.,diffus_eta2=0.,diffus_eta3=0.,advec_va2=0.
   real, dimension(nx,3) :: fres,uxbb
   real, dimension(nzgrid) :: eta_zgrid = 0.0
-  real :: eta_shock_jump1,eta_tdep
+  real :: eta_shock_jump1,eta_tdep,Arms
 !
   contains
 !***********************************************************************
@@ -1501,6 +1503,11 @@ module Magnetic
         if (lmagn_mf .or. lspecial) call put_shared_variable('eta',eta)
 
       endif
+!
+!  Quenching of \eta by rms of magnetic vector potential?
+!
+      lquench_eta_aniso=(quench_aniso/=impossible)
+      if (.not.lquench_eta_aniso) idiag_etaaniso=0
 !
 !  precalculating fixed (on timescales larger than tau) vectorpotential
 !
@@ -3972,6 +3979,7 @@ module Magnetic
         else
           prof=eta1_aniso*(1.-step(x(l1:l2),eta1_aniso_r,eta1_aniso_d))
         endif
+        if (lquench_eta_aniso) prof=prof/(1.+quench_aniso*Arms)
         fres(:,1)=fres(:,1)-prof*cosalp*(cosalp*p%jj(:,1)+sinalp*p%jj(:,2))
         fres(:,2)=fres(:,2)-prof*sinalp*(cosalp*p%jj(:,1)+sinalp*p%jj(:,2))
         if (lfirst.and.ldt) diffus_eta=diffus_eta+eta1_aniso
@@ -5374,6 +5382,7 @@ module Magnetic
         call sum_mn_name(eta_smag,idiag_etasmagm)
         if (idiag_etasmagmin/=0) call max_mn_name(-eta_smag,idiag_etasmagmin,lneg=.true.)
         call max_mn_name(eta_smag,idiag_etasmagmax)
+        call save_name(eta1_aniso/(1.+quench_aniso*Arms),idiag_etaaniso)
       endif
       call max_mn_name(p%etava,idiag_etavamax)
       call max_mn_name(p%etaj,idiag_etajmax)
@@ -6053,6 +6062,7 @@ module Magnetic
       use Boundcond, only: update_ghosts
       use Diagnostics, only: save_name
       use Density, only: calc_pencils_density
+      use Mpicomm, only: mpiallreduce_sum
 
       real, dimension(mx,my,mz,mfarray), intent(inout) :: f
 
@@ -6123,6 +6133,14 @@ module Magnetic
           jjmz(:,2)=-d2aamz(:,2)
           jjmz(:,3)=0.
         endif
+      endif
+!
+!  Calculate Arms for quenching.
+!
+      if (lquench_eta_aniso) then
+        Arms=sum(f(l1:l2,m1:m2,n1:n2,iaa:iaa+2)**2)  ! requires equidistant grid
+        call mpiallreduce_sum(Arms,fact)
+        Arms=sqrt(fact/nwgrid)
       endif
 !
 !  Remove mean field (y average).
@@ -8903,6 +8921,7 @@ module Magnetic
         idiag_brmsx=0; idiag_brmsz=0
         idiag_etavamax=0; idiag_etajmax=0; idiag_etaj2max=0; idiag_etajrhomax=0
         idiag_hjrms=0;idiag_hjbm=0;idiag_coshjbm=0
+        idiag_etasmagm=0; idiag_etaaniso=0
         idiag_cosjbm=0;idiag_jparallelm=0;idiag_jperpm=0
         idiag_hjparallelm=0;idiag_hjperpm=0
         idiag_vmagfricmax=0; idiag_vmagfricrms=0; idiag_vmagfricmz=0
@@ -9157,6 +9176,7 @@ module Magnetic
         call parse_name(iname,cname(iname),cform(iname),'etajmax',idiag_etajmax)
         call parse_name(iname,cname(iname),cform(iname),'etaj2max',idiag_etaj2max)
         call parse_name(iname,cname(iname),cform(iname),'etajrhomax',idiag_etajrhomax)
+        call parse_name(iname,cname(iname),cform(iname),'etaaniso',idiag_etaaniso)
         call parse_name(iname,cname(iname),cform(iname),'cosjbm',idiag_cosjbm)
         call parse_name(iname,cname(iname),cform(iname),'jparallelm',idiag_jparallelm)
         call parse_name(iname,cname(iname),cform(iname),'jperpm',idiag_jperpm)
