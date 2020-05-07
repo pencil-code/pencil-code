54cc9d21b350f5f2ed87a554c4abdaf9d1a011e7
fredgent <fred.gent.ncl@gmail.com>
2020-05-07 18:12:51 +0300

    Apply optimal cpu array and streamlined grid.h5 write

--- python/pencil/sim/remesh.py
+++ python/pencil/sim/remesh.py
@@ -1,31 +1,24 @@
 # remesh.py
 #
-# Read VAR files from existing mature simulation located in the source_path
-# directory to be remeshed into newly initialised target simulation of new
-# grid and processor layout located in the target_path directory.
-# 
-# First set up new run compile and start it.
-# To interpolate the old data into new shape call interp_var with string
-# arguments specifying source_path and target_path directories.
-# Then use the resulting fnew array and target_path as arguments for
-# distribute_fort to overwrite the var.dat in the process tree
-# 
-#
-# NB: to include particles or additional farray variables the scripts will
-#     need to be modified
-#     the interpolation does not handle switching the system of coordinates
-#     for periodic boundaries the arrays are stretched or shrunk to maintain
-#     continuity on the boundaries. This is the default.
-#     Not implemented: clipping the data or extending e.g. repeat the array in
-#     the periodic directions
-#     Not implemented reading or writing farray in hdf5 format
-#
+# 05-may-20
 # Author: F. Gent (fred.gent.ncl@gmail.com).
 #
+""" Remesh mature simulation snapshot with [nx,ny,nz] dimensions onto new
+    simulation with new grid dimensions and optionally alternate cpu layout
+    copying the base simulation files, existing start output files.
+ 
+    uses:
+      local_remesh to apply the interpolation onto a variable array
+      get_dstgrid to derive the new grid layout
+      src2dst_remesh to create the new simulation object and files
+"""
 import numpy as np
 from scipy.interpolate import interp1d
 from ..math.derivatives import grad
 from ..io import open_h5, group_h5, dataset_h5
+from fileinput import input
+from sys import stdout
+import subprocess as sub
 
 def local_remesh(var,
            xsrc, ysrc, zsrc, xdst, ydst, zdst, quiet=True
@@ -37,10 +30,10 @@ def local_remesh(var,
       snapshot scalar numpy array of shape [mz,my,mx].
 
     *xsrc, ysrc, zsrc:
-      hdf5 grid object from source simulation.
+      grid x,y,z arrays from source simulation.
 
     *xdst, ydst, zdst*:
-      hdf5 grid object for destination simulation.
+      grid x,y,z arrays for destination simulation.
 
     *quiet*
       Flag for switching of output.
@@ -59,21 +52,30 @@ def local_remesh(var,
 
     return tmp
 
-def get_dstgrid(srch5, srcpar, dsth5, ncpus=[1,1,1],
+def get_dstgrid(srch5, srcpar, dsth5, ncpus=[1,1,1], 
                 multxyz=[2,2,2], fracxyz=[1,1,1], srcghost=3, dstghost=3,
-                dstprecision=[b'D'], lsymmetric=True
+                dtype=np.float64, lsymmetric=True, quiet=True
                ):
     """
     Keyword arguments:
 
-    *srcgrid*:
+    *srch5*:
       hdf5 object from source simulation.
 
     *srcpar*:
       simulation param dictionary object from source simulation.
 
     *dsth5*:
-      hdf5 object for destination simulation data
+      hdf5 object for destination simulation data.
+
+    *ncpus*:
+      array of nprocx, nprocy, and nprocz to apply for new simulation.
+
+    *multxyz*:
+      factors by which to multiply old sim dimensions yxz order.
+
+    *fracxyz*
+      factors by which to divide old sim dimensions yxz order.
 
     *srcghost*
       Number of ghost zones from the source order of accuracy (mx-nx)/2
@@ -81,34 +83,27 @@ def get_dstgrid(srch5, srcpar, dsth5, ncpus=[1,1,1],
     *dstghost*
       Number of ghost zones for the destination order of accuracy (mx-nx)/2
 
+    *dtype*
+      Precision used in destination simulation. Default double.
+
+    *lsymmetric*
+      Option to make non-periodic grid symmetric about old sim centre. 
+      Otherwise the lower boundary is retained from old sim grid.
+
     *quiet*
       Flag for switching of output.
 
-    *dstprecision*
-      Precision used in destination simulation. Default double.
-
     """
+    # TBA
     # check prime factorization of the result and display for proc options
     # if using fft check options for grid and cpu layout
     # handling non-equidistant grids tba
-    if dstprecision[0] == b'D':
-        dtype = np.float64
-    elif dstprecision[0] == b'S':
-        dtype = np.float32
-    else:
-        print('precision '+dstprecision+' not valid')
-        return 1
 
+    # copy settings from srcsim and revise with changes to dstsim var.h5
     srcsets=srch5['settings']
-    if not dsth5.__contains__('settings'):
-        sets = dsth5.create_group('settings')
-        for key in srcsets.keys():
-            sets.create_dataset(key, data=srcsets[key][()])
-    else:
-        sets = dsth5['settings']
-        for key in srcsets.keys():
-            if not sets.__contains__(key):
-                sets.create_dataset(key, data=srcsets[key][()])
+    sets = group_h5(dsth5, 'settings', mode='a')
+    for key in srcsets.keys():
+        dset = dataset_h5(sets, key, data=srcsets[key][()], mode='a')
     #update grid dimensions
     sets['nx'][()] = int(srcsets['nx'][()]*multxyz[0]/fracxyz[0])
     sets['mx'][()] = sets['nx'][()] + 2*dstghost
@@ -126,40 +121,33 @@ def get_dstgrid(srch5, srcpar, dsth5, ncpus=[1,1,1],
         sets['nprocx'][()] = ncpus[0]
         sets['nprocy'][()] = ncpus[1]
         sets['nprocz'][()] = ncpus[2]
-    #calculate the new grid arrays
+    #copy the grid from the srcsim to dstsim var.h5 and grid.h5
     srcgrid=srch5['grid']
-    if not dsth5.__contains__('grid'):
-        grid = dsth5.create_group('grid')
-        for key in srcgrid.keys():
-            grid.create_dataset(key, data=srcgrid[key][()], dtype=dtype)
-    else:
-        grid = dsth5['grid']
-        for key in srcgrid.keys():
-            if not grid.__contains__(key):
-                grid.create_dataset(key, data=srcgrid[key][()])
+    grid = group_h5(dsth5, 'grid', mode='a')
+    for key in srcgrid.keys():
+        dset = dataset_h5(grid, key, data=srcgrid[key][()], mode='a')
+    #replace grid data changed for dstsim
     for ii,mm in [[0,'mx'],[1,'my'],[2,'mz']]:
         if not srcpar['lequidist'][ii]:
             print('get_dstgrid WARNING: non-equidistant grid not implemented\n',
-                  'continuing with equidistant grid. Please correct omission')
+                  'continuing with equidistant grid.\n',
+                  'Please implement non-equidistant grid options.')
         if not sets[mm][()] == srcsets[mm][()]:
             #assuming for now par.lxyz is the same
             mstr = mm[1]
-            grid['d'+mstr][()] = dtype(srcgrid['d'+mstr][()]/
-                             multxyz[ii]*fracxyz[ii])
+            grid['d'+mstr][()] = dtype((srcgrid[mstr][-srcghost]-
+                                srcgrid[mstr][srcghost])/(sets['n'+mstr][()]-1))
             grid.__delitem__(mstr)
             grid.create_dataset(mstr, (sets[mm][()],), dtype=dtype)
-            if not srcpar['lperi'][ii] and lsymmetric:
-                grid[mstr][dstghost:-dstghost] = np.linspace(
-                                srcgrid[mstr][srcghost]-grid['d'+mstr][()],
-                                srcgrid[mstr][-srcghost-1],
-                                sets['n'+mstr][()],dtype=dtype
-                                ) + dtype(0.5*grid['d'+mstr][()])
-            else:
-                grid[mstr][dstghost:-dstghost] = np.linspace(
-                                srcgrid[mstr][srcghost]-grid['d'+mstr][()],
-                                srcgrid[mstr][-srcghost-1],
-                                sets['n'+mstr][()],dtype=dtype
-                                )
+            grid[mstr][dstghost:-dstghost] = np.linspace(
+                            srcgrid[mstr][srcghost]-grid['d'+mstr][()],
+                            srcgrid[mstr][-srcghost-1],
+                            sets['n'+mstr][()],dtype=dtype
+                            )
+            if srcpar['lshift_origin'][ii] or lsymmetric:
+                grid[mstr][dstghost:-dstghost] += dtype(0.5*grid['d'+mstr][()])
+            elif srcpar['lshift_origin_lower'][ii]:
+                grid[mstr][dstghost:-dstghost] -= dtype(0.5*grid['d'+mstr][()])
             for jj in range(0,dstghost):
                 grid[mstr][jj] = grid[mstr][dstghost] -\
                                     (dstghost-jj)*grid['d'+mstr][()]
@@ -169,9 +157,12 @@ def get_dstgrid(srch5, srcpar, dsth5, ncpus=[1,1,1],
                 grid['L'+mstr][()] = srcgrid['L'+mstr][()] + grid['d'+mstr][()]
                 grid['O'+mstr][()] = srcgrid['O'+mstr][()] -\
                                                          0.5*grid['d'+mstr][()]
-            grid['d'+mstr+'_1'][()] = 1./grid['d'+mstr][()]
-            grid['d'+mstr+'_tilde'][()] = np.gradient(grid['d'+mstr+'_1'][()],
-                                               grid['d'+mstr][()])
+            grid.__delitem__('d'+mstr+'_1')
+            grid.create_dataset('d'+mstr+'_1', 
+                               data=1./np.gradient(grid[mstr][()]), dtype=dtype)
+            grid.__delitem__('d'+mstr+'_tilde')
+            grid.create_dataset('d'+mstr+'_tilde', 
+                         data=np.gradient(grid['d'+mstr+'_1'][()]), dtype=dtype)
  
 def src2dst_remesh(src, dst,
                    h5in='var.h5', h5out='var.h5',
@@ -179,17 +170,18 @@ def src2dst_remesh(src, dst,
                    srcdatadir='data/allprocs', dstdatadir='data/allprocs',
                    dstprecision=[b'D'], lsymmetric=True, quiet=True,
                    check_grid=True, OVERWRITE=False, optionals=True,
-                   rename_submit_script=False, nmin=8, ncpus=[1,1,1],
-                   start_optionals=False
+                   rename_submit_script=False, nmin=8, MBmin=5.0, ncpus=[1,1,1],
+                   start_optionals=False, hostfile=None, submit_new=False
                   ):
     import h5py
     from .. import read
     from os.path import join
     import os
-    from ..io import mkdir	
+    from ..io import mkdir
     from . import is_sim_dir, simulation
-    from ..math import common_factors
+    from ..math import cpu_optimal
 
+    # set dtype from precision
     if dstprecision[0] == b'D':
         dtype = np.float64
     elif dstprecision[0] == b'S':
@@ -218,20 +210,32 @@ def src2dst_remesh(src, dst,
                              rename_submit_script=rename_submit_script)
     with open_h5(join(srcsim.path,srcdatadir,h5in),mode='r') as srch5:
         with open_h5(join(dstsim.path,dstdatadir,h5out), mode='w') as dsth5:
-            get_dstgrid(srch5, srcsim.param, dsth5,
+            #apply settings and grid to dst h5 files
+            get_dstgrid(srch5, srcsim.param, dsth5, ncpus=ncpus,
                         multxyz=multxyz, fracxyz=fracxyz, srcghost=srcghost,
-                        dstghost=dstghost, ncpus=ncpus,
-                        dstprecision=dstprecision, lsymmetric=lsymmetric)
+                        dstghost=dstghost, dtype=dtype, lsymmetric=lsymmetric,
+                        quiet=quiet)
+            #use settings to determine available proc dist then set ncpus 
+            factors = cpu_optimal(
+                   dsth5['settings/nx'][0],
+                   dsth5['settings/ny'][0],
+                   dsth5['settings/nz'][0],
+                   mvar=dsth5['settings/mvar'][0],
+                   maux=dsth5['settings/maux'][0],
+                   par=srcsim.param, nmin=nmin, MBmin=MBmin)
+            print('remesh check grid: optional cpus upto min grid of'+
+                  'nmin={}\n'.format(nmin)+
+                  'cpu options {}\n'.format(factors)+
+                  'new mesh: {}, {}, {}\n'.format(dsth5['settings/nx'][0],
+                  dsth5['settings/ny'][0], dsth5['settings/nz'][0])+
+                 'To execute remesh set "check_grid=False".')
             if check_grid:
-                factors = common_factors(dsth5['settings/nx'][0],
-                                         dsth5['settings/nx'][0],
-                                         dsth5['settings/nz'][0])
-                print('remesh check grid: optional cpus upto min grid of'+
-                      ' {}\n'.format(nmin)+'cpu options {}\n'.format(factors)+
-                      'new mesh: {}, {}, {}\n'.format(dsth5['settings/nx'][0],
-                      dsth5['settings/ny'][0], dsth5['settings/nz'][0])+
-                     'To execute remesh set "check_grid=False".')
                 return 1
+            if ncpus == [1,1,1]:
+                ncpus = [factors[1][0],factors[1][1],factors[1][2]]
+                dsth5['settings/nprocx'][0] = ncpus[0]
+                dsth5['settings/nprocy'][0] = ncpus[1]
+                dsth5['settings/nprocz'][0] = ncpus[2]
             group = group_h5(dsth5, 'unit', mode='w')
             for key in srch5['unit'].keys():
                 if type(srch5['unit'][key][()]) == np.float64 or\
@@ -243,9 +247,14 @@ def src2dst_remesh(src, dst,
                     dset = dataset_h5(group, key, mode='w',
                                       data=srch5['unit'][key][()],
                                       overwrite=True)
+            gridh5 = open_h5(join(dstsim.datadir,'grid.h5'), mode='w')
+            dsth5.copy('settings', gridh5)
+            dsth5.copy('grid', gridh5)
+            dsth5.copy('unit', gridh5)
+            gridh5.close()
+            nprocs = ncpus[0]*ncpus[1]*ncpus[2]
             if 'persist' in srch5.keys():
                 group = group_h5(dsth5, 'persist', mode='w')
-                nprocs = ncpus[0]*ncpus[1]*ncpus[2]
                 for key in srch5['persist'].keys():
                     tmp = np.zeros(nprocs)
                     tmp[:] = srch5['persist'][key][0]
@@ -256,18 +265,40 @@ def src2dst_remesh(src, dst,
                     else:
                         dset = dataset_h5(group, key, mode='w',
                                           data=tmp, overwrite=True)
-            dst = dataset_h5(dsth5, 'time', mode='w',
+            dset = dataset_h5(dsth5, 'time', mode='w',
                              data=srch5['time'][()], dtype=dtype)
             srcgrid = srch5['grid']
             dstgrid = dsth5['grid']
             group = group_h5(dsth5, 'data', mode='w')
             for key in srch5['data'].keys():
                 print('remeshing '+key)     
-                var = local_remesh(srch5['data'][key][()], 
-                                   srcgrid['x'], srcgrid['y'], srcgrid['z'],
-                                   dstgrid['x'], dstgrid['y'], dstgrid['z'],
-                                   quiet=quiet )
+                var = local_remesh(
+                    srch5['data'][key][()], 
+                    srch5['grid']['x'], srch5['grid']['y'], srch5['grid']['z'],
+                    dsth5['grid']['x'], dsth5['grid']['y'], dsth5['grid']['z'],
+                    quiet=quiet )
                 print('writing '+key+' shape {}'.format(var.shape))
-                dst = dataset_h5(group, key, mode='w', data=var,
+                dset = dataset_h5(group, key, mode='w', data=var,
                                  overwrite=True, dtype=dtype)
+    dstsim.update()
+    dstsim.change_value_in_file('src/cparam.local','ncpus', str(nprocs))
+    dstsim.change_value_in_file('src/cparam.local','nprocx',str(ncpus[0]))
+    dstsim.change_value_in_file('src/cparam.local','nprocy',str(ncpus[1]))
+    dstsim.change_value_in_file('src/cparam.local','nprocz',str(ncpus[2]))
+    dstsim.change_value_in_file('src/cparam.local','nxgrid',str(dstsim.dim.nxgrid))
+    #dstsim.change_value_in_file('src/cparam.local','nygrid',str(dstsim.dim.nygrid))
+    dstsim.change_value_in_file('src/cparam.local','nzgrid',str(dstsim.dim.nzgrid))
+
+    #cmd = 'source '+join(srcsim.path,'src','.moduleinfo')
+    #os.system(cmd)
+    #os.chdir(dstsim.path)
+    #cmd = 'pc_setupsrc; make cleann'
+    #os.system(cmd)
+    #cmd = 'pc_build'
+    #if hostfile: cmd = cmd + ' -f '+hostfile
+    #process = sub.Popen(cmd.split(),stdout=sub.PIPE)
+    #process = sub.Popen(cmd.split(),stdout=sub.PIPE)
+    #output, error = process.communicate()
+    #print(cmd,output,error)
+ 
 # remains to copy other files and edit param files
