d492328859b5a5b41a88316ca4d78ea151c401c3
fredgent <fred.gent.ncl@gmail.com>
2020-05-13 10:57:35 +0300

    gradient and curl completed in k space

--- python/pencil/math/Helmholtz.py
+++ python/pencil/math/Helmholtz.py
@@ -11,7 +11,8 @@ import numpy as np
 from ..math import dot, dot2, cross
 from ..math.derivatives import div, curl, grad
 
-def helmholtz_fft(field, grid, params, nghost=3, nonperi_bc=None, field_scalar=[]):
+def helmholtz_fft(tot_field, grid, params, nghost=3,
+                  lno_mean=False, nonperi_bc=None, field_scalar=[], s=None):
     """
     Creates the decomposition vector pair for the supplied vector field.
 
@@ -21,7 +22,7 @@ def helmholtz_fft(field, grid, params, nghost=3, nonperi_bc=None, field_scalar=[
  
     Keyword arguments:
 
-     *field*:
+     *tot_field*:
        Vector field of dimension [3,mz,my,mx], which is decomposed.
 
      *grid*:
@@ -30,20 +31,46 @@ def helmholtz_fft(field, grid, params, nghost=3, nonperi_bc=None, field_scalar=[
      *params*:
        Simulation Params object with domain dimensions Lx, Ly and Lz.
 
+     *nghost*:
+       Number of ghost zones to exclude from the fft.
+
+     *lno_mean*:
+       Exclude any mean flow from the decomposition - should drop anyway.
+
+     *nonperi_bc:
+       String if not None with boundary condition label.
+       How to apply the bc needs to be implemented as required.
+
      *field_scalar
-       Scalar field (density) as debug tool for energy comparison
+       Scalar field (density) as debug tool for energy comparison.
+
+     *s*:
+      List of three integers if not None for fft dimension.
+      If none the dimension of the field [nz,ny,nx] is used.
 
     """
+    if lno_mean:
+        # exclude volume mean flows
+        field = np.zeros_like(tot_field)
+        for j in range(0,3):
+            field[j] = tot_field[j] - tot_field[j].mean()
+    else:
+        field = tot_field
+    #use mean speed and grid spacing in normalization of div/curl check
+    amp_field_1 = 1./np.sqrt(dot2(field)).mean()
     nz,ny,nx = field[:,nghost:-nghost,nghost:-nghost,nghost:-nghost].shape[-3],\
                field[:,nghost:-nghost,nghost:-nghost,nghost:-nghost].shape[-2],\
                field[:,nghost:-nghost,nghost:-nghost,nghost:-nghost].shape[-1]
-    nz2,ny2,nx2 = int(nz/2),int(ny/2),int(nx/2)
+    invs = [nz,ny,nx]
+    if not s:
+        s =  [nz,ny,nx]
+    knz,kny,knx = s[0], s[1], s[2]
+    nz2,ny2,nx2 = int(knz/2),int(kny/2),int(knx/2)
     #derive wavenumbers k scaled to dimension of simulation domain
-    kk = np.empty(shape=
-                  field[:,nghost:-nghost,nghost:-nghost,nghost:-nghost].shape)
-    k0 = np.arange(nx); k0[nx2:] = -k0[nx2-1::-1]-1
-    k1 = np.arange(ny); k1[ny2:] = -k1[ny2-1::-1]-1
-    k2 = np.arange(nz); k2[nz2:] = -k2[nz2-1::-1]-1
+    kk = np.empty(shape=[3,s[0],s[1],s[2]])
+    k0 = np.arange(knx); k0[nx2:] = -k0[nx2-1::-1]-1
+    k1 = np.arange(kny); k1[ny2:] = -k1[ny2-1::-1]-1
+    k2 = np.arange(knz); k2[nz2:] = -k2[nz2-1::-1]-1
     for j in range(0,k0.size):
         kk[0,:,:,j] = k0[j]*2*np.pi/params.lxyz[0]
     for j in range(0,k1.size):
@@ -52,47 +79,33 @@ def helmholtz_fft(field, grid, params, nghost=3, nonperi_bc=None, field_scalar=[
         kk[2,j,:,:] = k2[j]*2*np.pi/params.lxyz[2]
     knorm = dot2(kk)
     #apply fast Fourier transform to the vector field
-    kfield = np.empty(shape=kk.shape, dtype=complex)
+    kfield = np.empty(shape=[3,s[0],s[1],s[2]], dtype=complex)
     for j in range(0,3):
         kfield[j] = np.fft.fftn(
-                          field[j,nghost:-nghost,nghost:-nghost,nghost:-nghost])
+                    field[j,nghost:-nghost,nghost:-nghost,nghost:-nghost],s=s
+                                )
+    #reverse fft to obtain the scalar potential
+    pfield = -1j*dot(kk,kfield)
+    pfield[np.where(knorm==0)] = 0.
+    #reverse fft to obtain the vector potential
+    rfield =  1j*cross(kk,kfield)
+    for j in range(3):
+        rfield[j][np.where(knorm==0)] = 0.
     #avoid division by zero
     knorm[np.where(knorm==0)] = 1.
-    #reverse fft to obtain the scalar potential
-    pfield = -1j*dot(kk,kfield)/knorm
-    scalar_pot = np.zeros_like(field[0])
-    scalar_pot[nghost:-nghost,nghost:-nghost,nghost:-nghost] =\
-                                                       np.fft.ifftn(pfield).real
-    #reverse fft to obtain the vector potential
-    rfield =  1j*cross(kk,kfield)/knorm
-    vector_pot = np.zeros_like(field[:])
-    for j in range(0,3):
-        vector_pot[j,nghost:-nghost,nghost:-nghost,nghost:-nghost] =\
-                                                    np.fft.ifftn(rfield[j]).real
-    #apply the periodic boundary conditions for the ghost zones:
-    scalar_pot[:nghost] = scalar_pot[-2*nghost:-nghost]
-    scalar_pot[-nghost:] = scalar_pot[nghost:2*nghost]
-    scalar_pot[:,:nghost] = scalar_pot[:,-2*nghost:-nghost]
-    scalar_pot[:,-nghost:] = scalar_pot[:,nghost:2*nghost]
-    scalar_pot[:,:,:nghost] = scalar_pot[:,:,-2*nghost:-nghost]
-    scalar_pot[:,:,-nghost:] = scalar_pot[:,:,nghost:2*nghost]
-    vector_pot[:,:nghost] = vector_pot[:,-2*nghost:-nghost]
-    vector_pot[:,-nghost:] = vector_pot[:,nghost:2*nghost]
-    vector_pot[:,:,:nghost] = vector_pot[:,:,-2*nghost:-nghost]
-    vector_pot[:,:,-nghost:] = vector_pot[:,:,nghost:2*nghost]
-    vector_pot[:,:,:,:nghost] = vector_pot[:,:,:,-2*nghost:-nghost]
-    vector_pot[:,:,:,-nghost:] = vector_pot[:,:,:,nghost:2*nghost]
+    pfield /= knorm
+    for j in range(3):
+        rfield[j] /= knorm
     if nonperi_bc:
         print('Please implement new nonperi_bc not yet implemented.\n',
               'Applying periodic boundary conditions for now.')
     pot_field = np.zeros_like(field)
     rot_field = np.zeros_like(field)
-    pot_field[:,nghost:-nghost,nghost:-nghost,nghost:-nghost] =\
-       grad(scalar_pot, grid.dx, grid.dy, grid.dz
-                               )[:,nghost:-nghost,nghost:-nghost,nghost:-nghost]
-    rot_field[:,nghost:-nghost,nghost:-nghost,nghost:-nghost] =\
-       curl(vector_pot, grid.dx, grid.dy, grid.dz
-                               )[:,nghost:-nghost,nghost:-nghost,nghost:-nghost]
+    for j in range(0,3):
+        pot_field[j,nghost:-nghost,nghost:-nghost,nghost:-nghost] =\
+                                      np.fft.ifftn(1j*pfield*kk[j],s=invs).real
+        rot_field[j,nghost:-nghost,nghost:-nghost,nghost:-nghost] =\
+                               np.fft.ifftn(cross(1j*kk,rfield)[j],s=invs).real
     #apply the periodic boundary conditions for the ghost zones:
     for j in range(0,3):
         pot_field[j, :nghost,:,:] = pot_field[j,-2*nghost:-nghost,:,:]
@@ -110,17 +123,6 @@ def helmholtz_fft(field, grid, params, nghost=3, nonperi_bc=None, field_scalar=[
     if nonperi_bc:
         print('Please implement new nonperi_bc not yet implemented.\n',
               'Applying periodic boundary conditions for now.')
-    #check div and curl approximate/equal zero
-    print('Max {} and mean {} of abs(curl(pot field))'.format(np.abs(
-          curl(pot_field, grid.dx, grid.dy, grid.dz)
-              )[:,nghost:-nghost,nghost:-nghost,nghost:-nghost].max(), np.abs(
-          curl(pot_field, grid.dx, grid.dy, grid.dz)
-              )[:,nghost:-nghost,nghost:-nghost,nghost:-nghost].mean()))
-    print('Max {} and mean {} of abs(div(rot field))'.format(np.abs(
-          div(rot_field, grid.dx, grid.dy, grid.dz)
-             )[nghost:-nghost,nghost:-nghost,nghost:-nghost].max(), np.abs(
-          div(rot_field, grid.dx, grid.dy, grid.dz)
-             )[nghost:-nghost,nghost:-nghost,nghost:-nghost].mean()))
     #compare internal energy of original and sum of decomposed vectors
     rot2 = dot2(rot_field)[nghost:-nghost,nghost:-nghost,nghost:-nghost]
     pot2 = dot2(pot_field)[nghost:-nghost,nghost:-nghost,nghost:-nghost]
@@ -132,19 +134,20 @@ def helmholtz_fft(field, grid, params, nghost=3, nonperi_bc=None, field_scalar=[
         field2 *= field_scalar[nghost:-nghost,nghost:-nghost,nghost:-nghost]
     print('mean total field energy {} mean summed component energy {}'.format(
           np.mean(field2), np.mean(rot2+pot2)))
-    #rescale rot and pot to normalize energy sum of decomposed vectors
-    eratio = np.sqrt(np.mean(field2)/np.mean(rot2+pot2))
-    rot_field *= eratio
-    pot_field *= eratio
-    rot2 = dot2(rot_field)[nghost:-nghost,nghost:-nghost,nghost:-nghost]
-    pot2 = dot2(pot_field)[nghost:-nghost,nghost:-nghost,nghost:-nghost]
-    field2 = dot2(field)[nghost:-nghost,nghost:-nghost,nghost:-nghost]
-    if len(field_scalar) > 0:
-        rot2 *= field_scalar[nghost:-nghost,nghost:-nghost,nghost:-nghost]
-        pot2 *= field_scalar[nghost:-nghost,nghost:-nghost,nghost:-nghost]
-        field2 *= field_scalar[nghost:-nghost,nghost:-nghost,nghost:-nghost]
-    print('normalized mean total field energy '+
-          '{} mean summed component energy {}'.format(
-          np.mean(field2), np.mean(rot2+pot2)))
-    return rot_field, pot_field
-
+    #check div and curl approximate/equal zero
+    print('Max {} and mean {} of abs(curl(pot field))'.format(
+          max(grid.dx, grid.dy, grid.dz)*amp_field_1*
+          np.sqrt(dot2(curl(pot_field, grid.dx, grid.dy, grid.dz))
+                 )[nghost:-nghost,nghost:-nghost,nghost:-nghost].max(),
+          max(grid.dx, grid.dy, grid.dz)*amp_field_1*
+          np.sqrt(dot2(curl(pot_field, grid.dx, grid.dy, grid.dz))
+                 )[nghost:-nghost,nghost:-nghost,nghost:-nghost].mean()))
+    print('Max {} and mean {} of abs(div(rot field))'.format(
+          max(grid.dx, grid.dy, grid.dz)*amp_field_1*
+          np.abs(div(rot_field, grid.dx, grid.dy, grid.dz)
+             )[nghost:-nghost,nghost:-nghost,nghost:-nghost].max(),
+          max(grid.dx, grid.dy, grid.dz)*amp_field_1*
+          np.abs(div(rot_field, grid.dx, grid.dy, grid.dz)
+             )[nghost:-nghost,nghost:-nghost,nghost:-nghost].mean()))
+    ret_opt = [rot_field, pot_field]
+    return ret_opt
