e24347b96954722f38b054e17728fcaf841da503
Wladimir Lyra <wladimir.lyra@gmail.com>
2020-06-17 12:32:51 -0600

    change name of the shallow water initial condition from shallow_water_bottom.f90 -> shallow_water_init.f90
    * introduced planetary radius (a) as parameter
    * set planetary radius as default to the unit of length (also sets gamma=1 is Omega=1, gamma=Omega/a**2)

--- src/initial_condition/shallow_water_init.f90
+++ src/initial_condition/shallow_water_init.f90
@@ -20,14 +20,16 @@ module InitialCondition
 !
   include '../initial_condition.h'
 !
-  real :: eta0, k_eta, x0_drop, y0_drop
-  real :: Omega_SB=1.0,gamma_parameter
+  real :: eta0=0.0, k_eta, x0_drop, y0_drop
+  real :: Omega_SB=1.0,gamma_parameter=1.0
 !
   character (len=labellen), dimension(ninit) :: init_shallow_density='nothing'
   character (len=labellen), dimension(ninit) :: init_shallow_hydro='nothing'
 !
   namelist /initial_condition_pars/  eta0, k_eta, x0_drop, y0_drop, Omega_SB, &
-       init_shallow_density,init_shallow_hydro,gamma_parameter, a_code
+       init_shallow_density,init_shallow_hydro,gamma_parameter
+!
+  real :: planetary_radius=impossible ! derives from gamma_parameter and Omega
 !
   contains
 !***********************************************************************
@@ -50,6 +52,10 @@ module InitialCondition
 !
       real, dimension (mx,my,mz,mfarray) :: f
 !
+!  gamma_parameter = Omega/a**2 where a is the planetary radius
+!
+      planetary_radius = sqrt(Omega_SB/gamma_parameter)
+!
       call keep_compiler_quiet(f)
 !
     endsubroutine initialize_initial_condition
@@ -71,12 +77,13 @@ module InitialCondition
 !
         case('solid-body')   
 !
-! gamma_parameter = Omega/a**2 where a is the planetary radius
-!
           do n=n1,n2
             do m=m1,m2
+!
+! Equilibrium -> gh = gh0 + 3/2*Omega**2 * r**2 - Omega**2/(4*a**2) r**4
+!
                r2 = x(l1:l2)**2 + y(m)**2
-               eta = eta0 + Omega_SB**2 * (1.5*r2 - (0.25/a_code**2)*gamma_parameter * r2**2)
+               eta = eta0 + Omega_SB**2*r2 * (1.5 - 0.25*gamma_parameter/Omega_SB * r2)
                f(l1:l2,m,n,ilnrho) = log(eta)
             enddo
           enddo
