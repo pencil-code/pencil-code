9a73840c9edf34d987e76c6651853f5b22c5ee67
fredgent <fred.gent.ncl@gmail.com>
2020-07-16 09:00:53 +0300

    include hyper diffusion even if laplacian diffusion present

--- python/pencil/calc/Reynolds.py
+++ python/pencil/calc/Reynolds.py
@@ -13,7 +13,8 @@ from ..math import dot, dot2, cross
 from ..math.derivatives import div, curl, curl2, grad, del2, del6
 import numpy as np
 
-def fluid_reynolds(uu, param, grid, lnrho=list(), shock=list(), nghost=3):
+def fluid_reynolds(uu, param, grid, lnrho=list(), shock=list(), nghost=3,
+                   lmix=True):
     """
     Computes the fluid Reynolds number from the advective and effective
     viscous expressions in the momentum equation.
@@ -41,6 +42,9 @@ def fluid_reynolds(uu, param, grid, lnrho=list(), shock=list(), nghost=3):
 
      *nghost*:
        The number of ghost zones appropriate to the order of accuracy
+
+     *lmix*:
+       Option not to include hyper values when Laplacian values present
     """
     #viscous forces
     th2 = 2./3
@@ -58,10 +62,18 @@ def fluid_reynolds(uu, param, grid, lnrho=list(), shock=list(), nghost=3):
             lhyper3 = True
  
     if ldel2:
+        if lhyper3:
+            lhyper3 = lhyper3==lmix
         del2u = np.zeros_like(uu)
         for j in range(0,3):
             del2u[j] = del2(uu[j],grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,
                             coordinate_system=param.coord_system)
+            del2u[j,       : nghost, nghost:-nghost, nghost:-nghost] = del2u[j,-2*nghost: -nghost,   nghost: -nghost,   nghost: -nghost]
+            del2u[j,-nghost:       , nghost:-nghost, nghost:-nghost] = del2u[j,   nghost:2*nghost,   nghost: -nghost,   nghost: -nghost]
+            del2u[j, nghost:-nghost,       : nghost, nghost:-nghost] = del2u[j,   nghost: -nghost,-2*nghost: -nghost,   nghost: -nghost]
+            del2u[j, nghost:-nghost,-nghost:       , nghost:-nghost] = del2u[j,   nghost: -nghost,   nghost:2*nghost,   nghost: -nghost]
+            del2u[j, nghost:-nghost, nghost:-nghost,       : nghost] = del2u[j,   nghost: -nghost,   nghost: -nghost,-2*nghost: -nghost]
+            del2u[j, nghost:-nghost, nghost:-nghost,-nghost:       ] = del2u[j,   nghost: -nghost,   nghost: -nghost,   nghost:2*nghost]
         for ivisc in param.ivisc:
             ivisc = str.strip(ivisc,'\n')
             if 'nu-const' not in ivisc and 'shock' not in ivisc\
@@ -72,22 +84,50 @@ def fluid_reynolds(uu, param, grid, lnrho=list(), shock=list(), nghost=3):
         del(del2u)
     tmp0 = grad(uu[0],grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,
                 coordinate_system=param.coord_system)
+    for j in range(0,3):
+        tmp0[j, :nghost,nghost:-nghost,nghost:-nghost] = tmp0[j,-2*nghost:-nghost,nghost:-nghost,nghost:-nghost]
+        tmp0[j,-nghost:,nghost:-nghost,nghost:-nghost] = tmp0[j, nghost: 2*nghost,nghost:-nghost,nghost:-nghost]
+        tmp0[j,nghost:-nghost, :nghost,nghost:-nghost] = tmp0[j,nghost:-nghost,-2*nghost:-nghost,nghost:-nghost]
+        tmp0[j,nghost:-nghost,-nghost:,nghost:-nghost] = tmp0[j,nghost:-nghost, nghost: 2*nghost,nghost:-nghost]
+        tmp0[j,nghost:-nghost,nghost:-nghost, :nghost] = tmp0[j,nghost:-nghost,nghost:-nghost,-2*nghost:-nghost]
+        tmp0[j,nghost:-nghost,nghost:-nghost,-nghost:] = tmp0[j,nghost:-nghost,nghost:-nghost, nghost: 2*nghost]
     tmp1 = grad(uu[1],grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,
                 coordinate_system=param.coord_system)
+    for j in range(0,3):
+        tmp1[j, :nghost,nghost:-nghost,nghost:-nghost] = tmp1[j,-2*nghost:-nghost,nghost:-nghost,nghost:-nghost]
+        tmp1[j,-nghost:,nghost:-nghost,nghost:-nghost] = tmp1[j, nghost: 2*nghost,nghost:-nghost,nghost:-nghost]
+        tmp1[j,nghost:-nghost, :nghost,nghost:-nghost] = tmp1[j,nghost:-nghost,-2*nghost:-nghost,nghost:-nghost]
+        tmp1[j,nghost:-nghost,-nghost:,nghost:-nghost] = tmp1[j,nghost:-nghost, nghost: 2*nghost,nghost:-nghost]
+        tmp1[j,nghost:-nghost,nghost:-nghost, :nghost] = tmp1[j,nghost:-nghost,nghost:-nghost,-2*nghost:-nghost]
+        tmp1[j,nghost:-nghost,nghost:-nghost,-nghost:] = tmp1[j,nghost:-nghost,nghost:-nghost, nghost: 2*nghost]
     tmp2 = grad(uu[2],grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,
                 coordinate_system=param.coord_system)
+    for j in range(0,3):
+        tmp2[j, :nghost,nghost:-nghost,nghost:-nghost] = tmp2[j,-2*nghost:-nghost,nghost:-nghost,nghost:-nghost]
+        tmp2[j,-nghost:,nghost:-nghost,nghost:-nghost] = tmp2[j, nghost: 2*nghost,nghost:-nghost,nghost:-nghost]
+        tmp2[j,nghost:-nghost, :nghost,nghost:-nghost] = tmp2[j,nghost:-nghost,-2*nghost:-nghost,nghost:-nghost]
+        tmp2[j,nghost:-nghost,-nghost:,nghost:-nghost] = tmp2[j,nghost:-nghost, nghost: 2*nghost,nghost:-nghost]
+        tmp2[j,nghost:-nghost,nghost:-nghost, :nghost] = tmp2[j,nghost:-nghost,nghost:-nghost,-2*nghost:-nghost]
+        tmp2[j,nghost:-nghost,nghost:-nghost,-nghost:] = tmp2[j,nghost:-nghost,nghost:-nghost, nghost: 2*nghost]
     #effect of compressibility             
     if len(lnrho) > 0:
         divu = div(uu,grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,
                    coordinate_system=param.coord_system)
-        divu[ :nghost,:,:] = divu[-2*nghost:-nghost,:,:]
-        divu[-nghost:,:,:] = divu[ nghost: 2*nghost,:,:]
-        divu[:, :nghost,:] = divu[:,-2*nghost:-nghost,:]
-        divu[:,-nghost:,:] = divu[:, nghost: 2*nghost,:]
-        divu[:,:, :nghost] = divu[:,:,-2*nghost:-nghost]
-        divu[:,:,-nghost:] = divu[:,:, nghost: 2*nghost]
+        divu[ :nghost,nghost:-nghost,nghost:-nghost] = divu[-2*nghost:-nghost,nghost:-nghost,nghost:-nghost]
+        divu[-nghost:,nghost:-nghost,nghost:-nghost] = divu[ nghost: 2*nghost,nghost:-nghost,nghost:-nghost]
+        divu[nghost:-nghost, :nghost,nghost:-nghost] = divu[nghost:-nghost,-2*nghost:-nghost,nghost:-nghost]
+        divu[nghost:-nghost,-nghost:,nghost:-nghost] = divu[nghost:-nghost, nghost: 2*nghost,nghost:-nghost]
+        divu[nghost:-nghost,nghost:-nghost, :nghost] = divu[nghost:-nghost,nghost:-nghost,-2*nghost:-nghost]
+        divu[nghost:-nghost,nghost:-nghost,-nghost:] = divu[nghost:-nghost,nghost:-nghost, nghost: 2*nghost]
         gradlnrho = grad(lnrho,grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,
                          coordinate_system=param.coord_system)
+        for j in range(0,3):
+            gradlnrho[j, :nghost,nghost:-nghost,nghost:-nghost] = gradlnrho[j,-2*nghost:-nghost,nghost:-nghost,nghost:-nghost]
+            gradlnrho[j,-nghost:,nghost:-nghost,nghost:-nghost] = gradlnrho[j, nghost: 2*nghost,nghost:-nghost,nghost:-nghost]
+            gradlnrho[j,nghost:-nghost, :nghost,nghost:-nghost] = gradlnrho[j,nghost:-nghost,-2*nghost:-nghost,nghost:-nghost]
+            gradlnrho[j,nghost:-nghost,-nghost:,nghost:-nghost] = gradlnrho[j,nghost:-nghost, nghost: 2*nghost,nghost:-nghost]
+            gradlnrho[j,nghost:-nghost,nghost:-nghost, :nghost] = gradlnrho[j,nghost:-nghost,nghost:-nghost,-2*nghost:-nghost]
+            gradlnrho[j,nghost:-nghost,nghost:-nghost,-nghost:] = gradlnrho[j,nghost:-nghost,nghost:-nghost, nghost: 2*nghost]
         Sglnrho = np.zeros_like(uu)
         Sglnrho[0] = dot(tmp0,gradlnrho) +\
                         (tmp0[0]+tmp1[0]+tmp2[0]-th2*divu)*gradlnrho[0] 
@@ -97,6 +137,13 @@ def fluid_reynolds(uu, param, grid, lnrho=list(), shock=list(), nghost=3):
                         (tmp0[2]+tmp1[2]+tmp2[2]-th2*divu)*gradlnrho[2]
         graddivu = grad(divu,grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,
                         coordinate_system=param.coord_system)
+        for j in range(0,3):
+            graddivu[j, :nghost,nghost:-nghost,nghost:-nghost] = graddivu[j,-2*nghost:-nghost,nghost:-nghost,nghost:-nghost]
+            graddivu[j,-nghost:,nghost:-nghost,nghost:-nghost] = graddivu[j, nghost: 2*nghost,nghost:-nghost,nghost:-nghost]
+            graddivu[j,nghost:-nghost, :nghost,nghost:-nghost] = graddivu[j,nghost:-nghost,-2*nghost:-nghost,nghost:-nghost]
+            graddivu[j,nghost:-nghost,-nghost:,nghost:-nghost] = graddivu[j,nghost:-nghost, nghost: 2*nghost,nghost:-nghost]
+            graddivu[j,nghost:-nghost,nghost:-nghost, :nghost] = graddivu[j,nghost:-nghost,nghost:-nghost,-2*nghost:-nghost]
+            graddivu[j,nghost:-nghost,nghost:-nghost,-nghost:] = graddivu[j,nghost:-nghost,nghost:-nghost, nghost: 2*nghost]
         fvisc = fvisc + param.nu*(th1*graddivu+Sglnrho)
         del(Sglnrho)
     elif param.ldensity:
@@ -108,25 +155,44 @@ def fluid_reynolds(uu, param, grid, lnrho=list(), shock=list(), nghost=3):
             print('fluid_reynolds WARNING: no shock provided\n'+
                   'rate of strain tensor likely incomplete')
         else:
+            shock[ :nghost,nghost:-nghost,nghost:-nghost] = shock[-2*nghost:-nghost,nghost:-nghost,nghost:-nghost]
+            shock[-nghost:,nghost:-nghost,nghost:-nghost] = shock[ nghost: 2*nghost,nghost:-nghost,nghost:-nghost]
+            shock[nghost:-nghost, :nghost,nghost:-nghost] = shock[nghost:-nghost,-2*nghost:-nghost,nghost:-nghost]
+            shock[nghost:-nghost,-nghost:,nghost:-nghost] = shock[nghost:-nghost, nghost: 2*nghost,nghost:-nghost]
+            shock[nghost:-nghost,nghost:-nghost, :nghost] = shock[nghost:-nghost,nghost:-nghost,-2*nghost:-nghost]
+            shock[nghost:-nghost,nghost:-nghost,-nghost:] = shock[nghost:-nghost,nghost:-nghost, nghost: 2*nghost]
             divugradlnrho = np.zeros_like(uu)
             gradshock = grad(shock,grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,
                              coordinate_system=param.coord_system)
+            for j in range(0,3):
+                gradshock[j, :nghost,nghost:-nghost,nghost:-nghost] = gradshock[j,-2*nghost:-nghost,nghost:-nghost,nghost:-nghost]
+                gradshock[j,-nghost:,nghost:-nghost,nghost:-nghost] = gradshock[j, nghost: 2*nghost,nghost:-nghost,nghost:-nghost]
+                gradshock[j,nghost:-nghost, :nghost,nghost:-nghost] = gradshock[j,nghost:-nghost,-2*nghost:-nghost,nghost:-nghost]
+                gradshock[j,nghost:-nghost,-nghost:,nghost:-nghost] = gradshock[j,nghost:-nghost, nghost: 2*nghost,nghost:-nghost]
+                gradshock[j,nghost:-nghost,nghost:-nghost, :nghost] = gradshock[j,nghost:-nghost,nghost:-nghost,-2*nghost:-nghost]
+                gradshock[j,nghost:-nghost,nghost:-nghost,-nghost:] = gradshock[j,nghost:-nghost,nghost:-nghost, nghost: 2*nghost]
             for j in range(0,3):
                 divugradlnrho[j] = param.nu_shock*divu*gradshock[j] +\
                           param.nu_shock*shock*(divu*gradlnrho[j] + graddivu[j])
             del(divu,gradshock,gradlnrho,graddivu)
             fvisc = fvisc + divugradlnrho
             del(divugradlnrho)
-    if lhyper3 and not ldel2:
+    if lhyper3:
         #deluij5 = np.zeros_like([uu,uu,uu])
         #uij5glnrho to be included
         del6u = np.zeros_like(uu)
         for j in range(0,3):
             del6u[j] = del6(uu[j],grid.dx,grid.dy,grid.dz)
+            del6u[j, :nghost,nghost:-nghost,nghost:-nghost] = del6u[j,-2*nghost:-nghost,nghost:-nghost,nghost:-nghost]
+            del6u[j,-nghost:,nghost:-nghost,nghost:-nghost] = del6u[j, nghost: 2*nghost,nghost:-nghost,nghost:-nghost]
+            del6u[j,nghost:-nghost, :nghost,nghost:-nghost] = del6u[j,nghost:-nghost,-2*nghost:-nghost,nghost:-nghost]
+            del6u[j,nghost:-nghost,-nghost:,nghost:-nghost] = del6u[j,nghost:-nghost, nghost: 2*nghost,nghost:-nghost]
+            del6u[j,nghost:-nghost,nghost:-nghost, :nghost] = del6u[j,nghost:-nghost,nghost:-nghost,-2*nghost:-nghost]
+            del6u[j,nghost:-nghost,nghost:-nghost,-nghost:] = del6u[j,nghost:-nghost,nghost:-nghost, nghost: 2*nghost]
             #del6 for non-cartesian tba
             #del6u[j] = del6(uu[j],grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,
             #                coordinate_system=param.coord_system)
-            fvisc = fvisc + param.nu_hyper3*del6u/4**6
+            fvisc = fvisc + param.nu_hyper3*del6u
         del(del6u)
     fvisc2 = np.sqrt(dot2(fvisc))
     #advective forces
@@ -148,7 +214,8 @@ def fluid_reynolds(uu, param, grid, lnrho=list(), shock=list(), nghost=3):
         print('Re undefined')
     return Re
 
-def magnetic_reynolds(uu, param, grid, aa=list(), bb=list(), jj=list(), nghost=3):
+def magnetic_reynolds(uu, param, grid, aa=list(), bb=list(), jj=list(),
+                      nghost=3, lmix=True):
     """
     Computes the magnetic Reynolds number from the advective and effective
     resistive expressions in the induction equation.
@@ -179,6 +246,9 @@ def magnetic_reynolds(uu, param, grid, aa=list(), bb=list(), jj=list(), nghost=3
 
      *nghost*:
        The number of ghost zones appropriate to the order of accuracy
+
+     *lmix*:
+       Option not to include hyper values when Laplacian values present
     """
     if len(bb) ==0 and len(aa) ==0 and len(jj) ==0:
         print('magnetic_reynolds WARNING: no aa, bb nor jj provided\n'+
@@ -193,6 +263,8 @@ def magnetic_reynolds(uu, param, grid, aa=list(), bb=list(), jj=list(), nghost=3
             lhyper3 = True
     fresi = np.zeros_like(uu)
     if lres:
+        if lhyper3:
+            lhyper3 = lhyper3==lmix
         if len(jj) == 0:
             if len(aa) == 0:
                 print('magnetic_reynolds WARNING: calculating jj without aa\n',
@@ -202,6 +274,13 @@ def magnetic_reynolds(uu, param, grid, aa=list(), bb=list(), jj=list(), nghost=3
             else:
                 jj = curl2(aa,grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,    
                             coordinate_system=param.coord_system)
+            for j in range(0,3):
+                jj[j, :nghost,:,:] = jj[j,-2*nghost:-nghost,:,:]
+                jj[j,-nghost:,:,:] = jj[j, nghost: 2*nghost,:,:]
+                jj[j,:, :nghost,:] = jj[j,:,-2*nghost:-nghost,:]
+                jj[j,:,-nghost:,:] = jj[j,:, nghost: 2*nghost,:]
+                jj[j,:,:, :nghost] = jj[j,:,:,-2*nghost:-nghost]
+                jj[j,:,:,-nghost:] = jj[j,:,:, nghost: 2*nghost]
         fresi = fresi + param.eta*param.mu0*jj
         for iresi in param.iresistivity:
             iresi = str.strip(iresi,'\n')
@@ -209,7 +288,7 @@ def magnetic_reynolds(uu, param, grid, aa=list(), bb=list(), jj=list(), nghost=3
                                         and len(iresi) > 0:
                 print('magnetic_reynolds WARNING: '+iresi+' not implemented\n'+
                       'terms may be missing from the standard resistive forces')
-    if lhyper3 and not lres:
+    if lhyper3:
         if len(aa) == 0:
             print('magnetic_reynolds WARNING: no aa provided\n'+
                   'aa must be provided for hyper resistivity')
@@ -218,11 +297,17 @@ def magnetic_reynolds(uu, param, grid, aa=list(), bb=list(), jj=list(), nghost=3
             del6a = np.zeros_like(aa)
             for j in range(0,3):
                 del6a[j] = del6(aa[j],grid.dx,grid.dy,grid.dz)
+                del6a[j, :nghost,:,:] = del6a[j,-2*nghost:-nghost,:,:]
+                del6a[j,-nghost:,:,:] = del6a[j, nghost: 2*nghost,:,:]
+                del6a[j,:, :nghost,:] = del6a[j,:,-2*nghost:-nghost,:]
+                del6a[j,:,-nghost:,:] = del6a[j,:, nghost: 2*nghost,:]
+                del6a[j,:,:, :nghost] = del6a[j,:,:,-2*nghost:-nghost]
+                del6a[j,:,:,-nghost:] = del6a[j,:,:, nghost: 2*nghost]
                 #del6 for non-cartesian tba
                 #del6a[j] = del6(aa[j],grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,
                 #                coordinate_system=param.coord_system)
-            #effective at l > 5 grid.dx
-            fresi = fresi + param.eta_hyper3*del6a/4**6
+            #effective at l > 5 grid.dx? 
+            fresi = fresi + param.eta_hyper3*del6a
             del(del6a)
     fresi2 = np.sqrt(dot2(fresi))
     del(fresi)
@@ -235,6 +320,13 @@ def magnetic_reynolds(uu, param, grid, aa=list(), bb=list(), jj=list(), nghost=3
         else:
             bb = curl(aa,grid.dx,grid.dy,grid.dz,x=grid.x,y=grid.y,    
                       coordinate_system=param.coord_system)
+            for j in range(0,3):
+                bb[j, :nghost,:,:] = bb[j,-2*nghost:-nghost,:,:]
+                bb[j,-nghost:,:,:] = bb[j, nghost: 2*nghost,:,:]
+                bb[j,:, :nghost,:] = bb[j,:,-2*nghost:-nghost,:]
+                bb[j,:,-nghost:,:] = bb[j,:, nghost: 2*nghost,:]
+                bb[j,:,:, :nghost] = bb[j,:,:,-2*nghost:-nghost]
+                bb[j,:,:,-nghost:] = bb[j,:,:, nghost: 2*nghost]
     advec = cross(uu,bb)
     advec2 = np.sqrt(dot2(advec))
     del(advec)
