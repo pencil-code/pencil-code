045936db4fb08000cd0dd1d079d40105b247661f
fredgent <fred.gent.ncl@gmail.com>
2020-09-09 12:30:55 +0300

    Added option to set stripe to mkdir

--- python/pencil/io/mkdir.py
+++ python/pencil/io/mkdir.py
@@ -7,15 +7,21 @@
 #####################################################################################
 
 
-def mkdir(destination, rank=0):
-  ##
-  ## external modules
-  import os
-  
-  ## 
-  ## create dir if not existing
-  if not os.path.exists(destination):
-    if rank == 0:
-        os.makedirs(destination)
-    return True
-  return False
+def mkdir(destination, rank=0, lfs=False, MB=1, count=1):
+    ##
+    ## external modules
+    import os
+    import subprocess as sub
+    
+    ## 
+    ## create dir if not existing
+    if not os.path.exists(destination):
+        if rank == 0:
+            os.makedirs(destination)
+            if lfs:
+                cmd = 'lfs setstripe -S {}M -c {} '.format(MB,count)+destination
+                process = sub.Popen(cmd.split(),stdout=sub.PIPE)
+                output, error = process.communicate()
+                print(cmd,output,error)
+        return True
+    return False
