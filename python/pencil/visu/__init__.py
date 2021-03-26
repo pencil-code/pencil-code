1d6ad55fe7d209b02ae50cc517d068cdabaa3de0
iomsn <iomsn1@googlemail.com>
2021-03-26 12:43:25 +0000

    Removed animate_time_slices from __init__ is visu.

--- python/pencil/visu/__init__.py
+++ python/pencil/visu/__init__.py
@@ -8,7 +8,6 @@ from .animate_multislices import *
 from .animate_slices_compareruns import *
 from .animate_slices_maketomovie import *
 from .animate_slices import *
-from .animate_time_slices import *
 
 ## line integral convolution
 from .lic import *
