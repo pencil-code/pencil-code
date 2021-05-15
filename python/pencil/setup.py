feca6f9a29b7e8577a95bed17a2abd5f71741827
Illa <illa.rivero.losada@gmail.com>
2021-05-15 16:28:44 -0600

    more files for documentation

---
+++ python/pencil/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+        name='pencil',
+        version='2.0',
+        description='Python for pencil postprocessing scripts',
+        packages=['backpack','calc','diag','export','io', 'ism_dyn', 'math', 'read', 'sim', 'tool_kit', 'util', 'visu'],
+)
