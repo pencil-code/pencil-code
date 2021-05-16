c022ddde8adffa49ddc453e99635e9616209baf4
Illa <illa.rivero.losada@gmail.com>
2021-05-16 14:08:00 -0600

    documentation

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -16,6 +16,15 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+# -- Mocking ---------
+from mock import Mock as MagicMock
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.linalg', 'scipy.signal']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- Project information -----------------------------------------------------
 
