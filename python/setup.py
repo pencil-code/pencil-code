271b8d4ee00a5602c43666fa682b279d733518b2
Illa <illa.rivero.losada@gmail.com>
2021-05-21 09:50:18 -0600

    docs

--- python/setup.py
+++ python/setup.py
@@ -4,6 +4,6 @@ setup(
         name='pencil',
         version='2.0',
         description='Python for pencil postprocessing scripts',
-        packages=['pencil', 'pencil.backpack','pencil.calc','pencil.diag','pencil.export','pencil.pio', 'pencil.ism_dyn', 'pencil.pmath', 'pencil.read', 'pencil.sim', 'pencil.tool_kit', 'pencil.util', 'pencil.visu'],
+        packages=['pencil', 'pencil.backpack','pencil.calc','pencil.diag','pencil.export','pencil.pio', 'pencil.ism_dyn', 'pencil.math', 'pencil.read', 'pencil.sim', 'pencil.tool_kit', 'pencil.util', 'pencil.visu'],
         package_dir={'': 'python'}
 )
