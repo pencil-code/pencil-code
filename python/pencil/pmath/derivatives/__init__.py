fe7e789b3388d751ed5e0bca0d9960020640475c
Illa <illa.rivero.losada@gmail.com>
2021-05-21 09:46:14 -0600

    documentation

--- python/pencil/pmath/derivatives/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Differential operators in different coordinate systems.
-"""
-
-from .der import xder, yder, zder, xder2, yder2, zder2, xder3, yder3, zder3, xder5, yder5, zder5, xder6, yder6, zder6
-from .div_grad_curl import div, curl, grad, curl2, del2, curl3, del6, gij, traceless_strain
-from .simple_centered import simple_centered
