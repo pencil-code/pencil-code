2eba69e31ed2076cac9e8fb55a4c459018fc7280
iomsn <iomsn1@googlemail.com>
2021-09-13 18:05:18 +0100

    Added scalar laplace in spherical coordinates. Coded by Glasgow student Hamish Ablett.

--- python/pencil/math/laplace_solver.py
+++ python/pencil/math/laplace_solver.py
@@ -64,7 +64,7 @@ def laplace_vector_cartesian(bx, by, bz, dx, dy, dz, niter=200):
 
     Parameters
     ----------
-     *bx, by, bz*: ndarray of shape [nz, ny, nx]
+     *bx, by, bz*: ndarrays of shape [nz, ny, nx]
          Boundary conditions on exterior points.
          Keep the inner points 0.
 
@@ -145,7 +145,7 @@ def laplace_vector_cylindrical(br, btheta, bz, r, theta, z, niter=200):
 
     Parameters
     ----------
-     *br, btheta, bz*: ndarray of shape [nz, ny, nx]
+     *br, btheta, bz*: ndarrays of shape [nz, ny, nx]
          Boundary conditions on exterior points.
          Keep the inner points 0.
 
@@ -189,10 +189,9 @@ def laplace_vector_cylindrical(br, btheta, bz, r, theta, z, niter=200):
     return np.array([R, Theta, bz])
 
 
-
 def laplace_scalar_spherical(bc, r, theta, phi, niter=200):
     '''
-    Solve the vector Laplace equation in spherical coordinates in 3 dimensions
+    Solve the scalar Laplace equation in spherical coordinates in 3 dimensions
     using finite differences.
 
     Signature:
@@ -218,22 +217,6 @@ def laplace_scalar_spherical(bc, r, theta, phi, niter=200):
     Returns
     ----------
     ndarray with the same shape as bc, representing solution to the Laplace equation.
-    '''
-
-    '''
-    x = np.linspace(1, 10, 101)
-    y = np.linspace(0.1, 1, 101)
-    z = np.linspace(0, 2*np.pi, 101)
-    dx = x[1] - x[0]
-    dy = y[1] - y[0]
-    dz = z[1] - z[0]
-    xx, yy, zz = np.meshgrid(x, y, z, indexing='ij')
-    bx = np.zeros_like(xx)
-    by = np.zeros_like(xx)
-    bz = np.exp(-(xx-2)**2-81*(yy-0.5)**2) - np.exp(-(xx-8)**2-81*(yy-0.5)**2)
-    bz = np.swapaxes(bz, 0, 2)
-    bz[1:, :, :] = 0
-    solution = pc.math.laplace_scalar_spherical(bz, x, y, z, niter=100)
     '''
 
     import numpy as np
@@ -263,92 +246,102 @@ def laplace_scalar_spherical(bc, r, theta, phi, niter=200):
     return uu
 
 
-#def laplace_spherical_vector(br, btheta, bphi, r, theta, phi):
-#    '''
-#    Solve the vector form of the Laplace equation in spherical coordinates using finite differences.
-#    
-#    Call signature:
-#    
-#    br, btheta, bphi: 3D ndarrays containing boundary conditions on exterior points. If the interior points are non-zero, the
-#              solution may not be correct. 0th index corresponds to smallest radis; theta increases as index increases.
-#              
-#    r, theta, phi: 1D coordinate arrays of r, theta and phi where the convention is taken that theta ranges from 0 to pi, phi
-#                   ranges from 0 to 2pi. Note that singularities arise in the equation when theta = 0. This may produce 
-#                   unintended results. 
-#                   
-#    Returns 3 3D ndarrays representing the components of the solution to the vector Laplace equation in spherical coordinates.
-#    '''
-#    
-#    import numpy as np
-#    
-#    theta_matrix, radius_matrix, phi_matrix = np.meshgrid(theta, r, phi)
-#    h = r[1] - r[0]
-#    h_prime = theta[1] - theta[0]
-#    h_doubleprime = phi[1] - phi[0]
-#    
-#    fraction_r = 1/( 
-#                    2 * (1/h**2 + 1/(h_prime * radius_matrix)**2
-#                          + 1/(h_doubleprime * radius_matrix * np.sin(theta_matrix))**2 + 1/radius_matrix**2)
-#                   )
-#    
-#    fraction_theta = 1/(
-#                        2 * (1/h**2 + 1/(h_prime * radius_matrix)**2 + 1/(h_doubleprime * radius_matrix * np.sin(theta_matrix))**2
-#                        + 1/(radius_matrix * np.sin(theta_matrix))**2)
-#                        )
-#    
-#    fraction_phi = fraction_theta
-#        
-#    def pre(C):
-#        '''
-#        This function simply returns the first five terms in each of the differential equations to increase readability.
-#        The parameter C is simply the most recent iteration of the unknown.
-#        '''
-#        return np.nan_to_num((
-#                1/(h * radius_matrix) * (np.roll(C, -1, 0) - np.roll(C, 1, 0)) 
-#                + 1/h**2 * (np.roll(C, -1, 0) + np.roll(C, 1, 0))
-#                + 1/(h_prime * radius_matrix)**2 * (np.roll(C, -1, 1) + np.roll(C, 1, 1))
-#                + 1/(h_doubleprime * radius_matrix * np.sin(theta_matrix))**2 * (np.roll(C, -1, 2) + np.roll(C, 1, 2))
-#                + 1/(2 * h_prime * radius_matrix**2 * np.tan(theta_matrix)) * (np.roll(C, -1, 1) + np.roll(C, 1, 1))
-#                ))
-#    
-#    
-#    
-#    iteration = 0
-#    while iteration <= 200:
-#        iteration += 1
-#        R = br.copy()
-#        Theta = btheta.copy()
-#        Phi = bphi.copy()
-#    
-#        R = fraction_r * np.nan_to_num((
-#                                        pre(R)
-#                                        - 1/(h_prime * radius_matrix**2) * (np.roll(Theta, -1, 1) - np.roll(Theta, 1, 1))
-#                                        - 1/(h_doubleprime * radius_matrix**2 * np.sin(theta_matrix)) 
-#                                        * (np.roll(Phi, -1, 1) - np.roll(Phi, 1, 1))
-#                                        - 2/(radius_matrix**2 * np.nan_to_num(np.tan(theta_matrix))) * Theta
-#                                        ))
-#                        
-#        Theta = fraction_theta * np.nan_to_num ((
-#                                                  pre(Theta)
-#                                                  - 1/(np.nan_to_num(np.tan(theta_matrix)) * h_doubleprime * radius_matrix**2 * np.sin(theta_matrix)) * (np.roll(Phi, -1, 2) - np.roll(Phi, 1, 2))
-#                                                  + 1/(radius_matrix**2 * h_prime) * (np.roll(R, -1, 1) - np.roll(R, 1, 1))
-#                                 ))
-#        
-#        Phi = fraction_phi * np.nan_to_num((
-#                                              pre(Phi)
-#                                              - 1/(h_doubleprime * radius_matrix**2 * np.sin(theta_matrix)) * (np.roll(R,-1,2)-np.roll(R,1,2))
-#                                              + 1/(np.nan_to_num(np.tan(theta_matrix)) * radius_matrix**2 * h_doubleprime * np.sin(theta_matrix)) * (np.roll(Theta, -1, 1) - np.roll(Theta, 1, 1))
-#                             ))
-#    
-#        br[1:-1, :, :] = R[1:-1, :, :]
-#        btheta[:, 1:-1, :] = Theta[:, 1:-1, :]
-#        bphi[:, 1:-1, :] = Phi[:, 1:-1, :]
-#        
-#    return R, Theta, Phi
-#
-#
-#
-#
+
+def laplace_vector_spherical(br, btheta, bphi, r, theta, phi, niter=200):
+    '''
+    Solve the scalar Laplace equation in spherical coordinates in 3 dimensions
+    using finite differences.
+
+    Signature:
+
+    laplace_vector_spherical(br, btheta, bphi, r, theta, phi, niter=200)
+
+    Parameters
+    ----------
+     *bc, btheta, bphi*: ndarray of shape [nz, ny, nx]
+         Boundary conditions on exterior points.
+         Keep the inner points 0.
+
+    r, theta, phi: ndarrays of shape [nx], [ny] and [nz]
+        1D coordinate arrays of r, theta and phi.
+        The convention is taken that theta ranges from 0 to pi and
+        phi ranges from 0 to 2pi.
+        Note that singularities arise in the equation when theta = 0 and r = 0.
+        This may produce unintended results.
+
+    *niter*: int
+        Number of iterations.
+
+    Returns
+    ----------
+    ndarray with the shape [3, nz, ny, nx], representing solution to the Laplace equation.
+    '''
+
+    '''
+    x = np.linspace(1, 10, 101)
+    y = np.linspace(0.1, 1, 101)
+    z = np.linspace(0, 2*np.pi, 101)
+    dx = x[1] - x[0]
+    dy = y[1] - y[0]
+    dz = z[1] - z[0]
+    xx, yy, zz = np.meshgrid(x, y, z, indexing='ij')
+    bx = np.zeros_like(xx)
+    by = np.zeros_like(xx)
+    bz = np.exp(-(xx-2)**2-81*(yy-0.5)**2) - np.exp(-(xx-8)**2-81*(yy-0.5)**2)
+    bz = np.swapaxes(bz, 0, 2)
+    bz[1:, :, :] = 0
+    solution = pc.math.laplace_vector_spherical(bx, by, bz, x, y, z, niter=100)
+    '''
+
+    import numpy as np
+
+    theta_matrix, radius_matrix, phi_matrix = np.meshgrid(theta, r, phi)
+    dr = r[1] - r[0]
+    dtheta = theta[1] - theta[0]
+    dphi = phi[1] - phi[0]
+
+    fraction_r = 1/(2*(1/dr**2 + 1/(dtheta*radius_matrix)**2 +
+                       1/(dphi*radius_matrix*np.sin(theta_matrix))**2 + 1/radius_matrix**2))
+    fraction_theta = 1/(2*(1/dr**2 + 1/(dtheta*radius_matrix)**2 + 1/(dphi*radius_matrix*np.sin(theta_matrix))**2 +
+                           1/(radius_matrix*np.sin(theta_matrix))**2))
+    fraction_phi = fraction_theta
+
+    def pre(coord):
+        '''
+        This function simply returns the first five terms in each of the differentialequations
+        The parameter coord is simply the most recent iteration of the unknown.
+        '''
+
+        return np.nan_to_num((1/(dr*radius_matrix)*(np.roll(coord, -1, 0) - np.roll(coord, 1, 0)) +
+                              1/dr**2 * (np.roll(coord, -1, 0) + np.roll(coord, 1, 0)) +
+                              1/(dtheta*radius_matrix)**2 * (np.roll(coord, -1, 1) + np.roll(coord, 1, 1)) +
+                              1/(dphi*radius_matrix*np.sin(theta_matrix))**2 * (np.roll(coord, -1, 2) + np.roll(coord, 1, 2)) +
+                              1/(2*dtheta*radius_matrix**2*np.tan(theta_matrix))*(np.roll(coord, -1, 1) + np.roll(coord, 1, 1))))
+
+    iteration = 0
+    while iteration <= niter:
+        iteration += 1
+        R = br.copy()
+        Theta = btheta.copy()
+        Phi = bphi.copy()
+
+        R = fraction_r*np.nan_to_num((pre(R) - 1/(dtheta*radius_matrix**2)*(np.roll(Theta, -1, 1) - np.roll(Theta, 1, 1)) -
+                                      1/(dphi*radius_matrix**2*np.sin(theta_matrix))*(np.roll(Phi, -1, 1) - np.roll(Phi, 1, 1)) -
+                                      2/(radius_matrix**2*np.nan_to_num(np.tan(theta_matrix)))*Theta))
+        Theta = fraction_theta * np.nan_to_num((pre(Theta) - 1/(np.nan_to_num(np.tan(theta_matrix))*
+                                                 dphi*radius_matrix**2*np.sin(theta_matrix))*(np.roll(Phi, -1, 2) - np.roll(Phi, 1, 2)) +
+                                                1/(radius_matrix**2 * dr) * (np.roll(R, -1, 1) - np.roll(R, 1, 1))))
+        Phi = fraction_phi * np.nan_to_num((pre(Phi) - 1/(dphi*radius_matrix**2*np.sin(theta_matrix))*(np.roll(R, -1, 2)-np.roll(R, 1, 2)) +
+                                            1/(np.nan_to_num(np.tan(theta_matrix))*radius_matrix**2*dphi*np.sin(theta_matrix))*(np.roll(Theta, -1, 1) - np.roll(Theta, 1, 1))))
+
+        br[1:-1, :, :] = R[1:-1, :, :]
+        btheta[:, 1:-1, :] = Theta[:, 1:-1, :]
+        bphi[:, 1:-1, :] = Phi[:, 1:-1, :]
+
+    return np.array([R, Theta, Phi])
+
+
+
 #def poisson_cartesian_vector(bx, by, bz, x, y, z, hx, hy, hz):
 #    '''
 #    Solves the (vector) form of the Poisson equation in 3D Cartesian coordinates, $\nabla^2 u = h$, using finite differences.
