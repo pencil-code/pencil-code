b108e9be9d7b40125b3b65d7b1d2369258d1abb0
Illa <illa.rivero.losada@gmail.com>
2021-09-20 14:06:22 -0500

    Fixing error in latex configuration

--- doc/readthedocs/conf.py
+++ doc/readthedocs/conf.py
@@ -155,7 +155,7 @@ htmlhelp_basename = "PencilCodeDOC"
 
 
 # -- Options for LaTeX output ------------------------------------------------
-latex_engine = "xelatex"
+latex_engine = "pdflatex"
 
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
