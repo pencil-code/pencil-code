1f8339f73d05a42b8e34f3d2d9b8355c3ab4e205
fredgent <fred.gent.ncl@gmail.com>
2022-01-18 11:14:02 +0200

    Removed load h5py when not present

--- python/pencil/__init__.py
+++ python/pencil/__init__.py
@@ -17,7 +17,6 @@ except:
         "Error: You need to install h5py library doing 'pip3 install h5py' (Python 3) \
            or 'pip install h5py' (Python 2)."
     )
-    import h5py
 
 # Load sub-modules.
 from . import io
