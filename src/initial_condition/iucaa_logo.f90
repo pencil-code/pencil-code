fdda3cb79a11ee76c625e39a0f4aa1279f1ac2d8
iomsn <iomsn1@googlemail.com>
2022-03-30 18:28:50 +0100

    Added twist to the iucaa plot.

--- src/initial_condition/iucaa_logo.f90
+++ src/initial_condition/iucaa_logo.f90
@@ -26,16 +26,17 @@ module InitialCondition
 ! C = offset from the center of the knot, should be > 1
 ! D = extention coefficient for the z direction
 ! phase = relative phase of the z-variation
+! twist = internal twist of the flux tube
 ! [xyz]scale = scale in each dimension
 ! [xyz]shift = shift in each dimension in a 2*pi box
 
-  real :: ampl=1.0,width_ring=0.3, C=2.0, D=1.5, phase=4./3.
-  real :: xscale = 1.0, yscale = 1.0, zscale = 1.0
-  real :: xshift = 0.2, yshift = 0.5, zshift = 0.0
+  real :: ampl=1.0, width_ring=0.3, C=2.0, D=1.5, phase=4./3., twist=0
+  real :: xscale=1.0, yscale=1.0, zscale=1.0
+  real :: xshift=0.0, yshift=0.0, zshift=0.0
   character (len=labellen) :: prof='constant'
 !
   namelist /initial_condition_pars/ &
-      ampl,width_ring,prof,C,D,xscale,yscale,zscale,xshift,yshift,zshift,phase
+      ampl,width_ring,prof,C,D,xscale,yscale,zscale,xshift,yshift,zshift,phase,twist
 !
   contains
 !***********************************************************************
@@ -86,10 +87,6 @@ module InitialCondition
 !  04-aug-10/simon: coded
 !
 !  Magnetic flux ring which has the form of a n-foil knot.
-!  NB: Curerently this works only for one CPU. For multi CPU usage
-!  initialize on one CPU and do the remeshing.
-!
-!  Created 2011-01-17 by Simon Candelaresi (Iomsn)
 !
       use Mpicomm, only: stop_it
       use Poisson
@@ -98,9 +95,10 @@ module InitialCondition
       real, dimension (mx,my,mz,mfarray) :: f
       real :: knot_param, circle_param, circle_radius
       real :: delta_knot_param, delta_circle_param, delta_circle_radius
-      real, dimension(3) :: knot_pos, circle_pos, tangent, normal
+      real, dimension(3) :: knot_pos, circle_pos, tangent, normal, twist_vector
       integer :: domain_width, domain_depth, domain_height
       integer :: l, j, ju
+      real :: dist
       ! The next 2 variables are used for the uncurling.
       real, dimension (nx,ny,nz,3) :: jj, tmpJ  ! This is phi for poisson.f90
       
@@ -117,7 +115,6 @@ module InitialCondition
       delta_circle_radius = delta_circle_param
 !
       knot_param = 0.
-      
 !
 !  loop which moves along the n-foil knot
 !
@@ -165,6 +162,9 @@ module InitialCondition
 !
           do
             if (circle_param .gt. 2.*pi) exit
+!
+!  Compute the position on the finite size flux tube.
+!
             circle_pos(1) = knot_pos(1) + circle_radius * &
             ((tangent(1)*tangent(1)*(1-cos(circle_param))+cos(circle_param))*normal(1) + &
             (tangent(1)*tangent(2)*(1-cos(circle_param))-tangent(3)*sin(circle_param))*normal(2) + &
@@ -178,17 +178,55 @@ module InitialCondition
             (tangent(2)*tangent(3)*(1-cos(circle_param))+tangent(1)*sin(circle_param))*normal(2) + &
             (tangent(3)*tangent(3)*(1-cos(circle_param))+cos(circle_param))*normal(3))
 !
+!  Compute the twist vector for the magnetic field.
+            twist_vector(1) = tangent(2)*(circle_pos(3)-knot_pos(3)) - tangent(3)*(circle_pos(2)-knot_pos(2))
+            twist_vector(2) = tangent(3)*(circle_pos(1)-knot_pos(1)) - tangent(1)*(circle_pos(3)-knot_pos(3))
+            twist_vector(3) = tangent(1)*(circle_pos(3)-knot_pos(3)) - tangent(2)*(circle_pos(1)-knot_pos(1))
+            twist_vector = twist_vector*circle_radius*twist
+            if (sqrt((circle_pos(1)-knot_pos(1))**2 + (circle_pos(2)-knot_pos(2))**2 + &
+            (circle_pos(3)-knot_pos(3))**2) > 0) then
+                twist_vector = twist_vector/sqrt((circle_pos(1)-knot_pos(1))**2 + &
+                (circle_pos(2)-knot_pos(2))**2 + &
+                (circle_pos(3)-knot_pos(3))**2)
+            endif
+!
+!
+!  Scale and shift the circle position.
+!
+            circle_pos(1) = circle_pos(1)*xscale + xshift
+            circle_pos(2) = circle_pos(2)*yscale + yshift
+            circle_pos(3) = circle_pos(3)*zscale + zshift
+!
 !  Find the corresponding mesh point to this position.
 !
-            l = nint((circle_pos(1)+xshift)/(2.*pi)*domain_width*xscale + domain_width/2.0)
-            m = nint((circle_pos(2)+yshift)/(2.*pi)*domain_depth*yscale + domain_depth/2.0)
-            n = nint((circle_pos(3)+zshift)/(2.*pi)*domain_height*zscale + domain_height/2.0)
+            dist = x(l2+nghost) - x(1)
+            do l = 1, l2+nghost
+                if (abs(x(l) - circle_pos(1)) > dist) exit
+                dist = abs(x(l) - circle_pos(1))
+            end do
+            dist = y(m2+nghost) - y(1)
+            do m = 1, m2+nghost
+                if (abs(y(m) - circle_pos(2)) > dist) exit
+                dist = abs(y(m) - circle_pos(2))
+            end do
+            dist = z(n2+nghost) - z(1)
+            do n = 1, n2+nghost
+                if (abs(z(n) - circle_pos(3)) > dist) exit
+                dist = abs(z(n) - circle_pos(3))
+            end do
 !
 !  Write the magnetic field B.
 !  Note that B is written in the f-array where A is stored. This is
 !  corrected further in the code.
 !
-            f(l,m,n,iax:iaz) = tangent*ampl
+!              f(l,m,n,iax:iaz) = tangent*ampl
+            if (l >= 1 .and. l <= l2+nghost .and. m >= 1 .and. m <= m2+nghost .and. n >= 1 .and. n <= n2+nghost) then
+                if (prof == 'const') then
+                    f(l,m,n,iax:iaz) = (tangent + twist_vector)*ampl
+                else if (prof == 'smooth') then     
+                    f(l,m,n,iax:iaz) = (tangent+twist_vector)*ampl*(1-(circle_radius/(width_ring/2.))**4)**4
+                endif
+            endif
             circle_param = circle_param + delta_circle_param
           enddo
           circle_radius = circle_radius + delta_circle_radius
