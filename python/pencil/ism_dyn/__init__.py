cf8eaf97fe6afd6851e2e67780d56e89fe2bb2c3
fredgent <fred.gent.ncl@gmail.com>
2022-04-06 14:47:53 +0300

    Explicit imports

--- python/pencil/ism_dyn/__init__.py
+++ python/pencil/ism_dyn/__init__.py
@@ -2,9 +2,9 @@
 Diagnostics for interstellar-medium dynamo simulations.
 """
 
-from .derived_h5 import *
+from .derived_h5 import derive_data, calc_derived_data, is_vector, der_limits, under_limits
 from .get_masks import *
 from .get_stats import *
-from .rhs_terms import *
+from .rhs_terms import rhs_data, calc_rhs_data
 from .ism_cooling import *
 from .ism_sedov_taylor import *
