8dcd8cb98a51be36db8aaa9bec3a34c0418e24c9
fredgent <fred.gent.ncl@gmail.com>
2022-09-22 13:30:54 +0300

    Use fortran overloaded copy_addr, rather than c copy_addr_c

--- src/pscalar_nolog.f90
+++ src/pscalar_nolog.f90
@@ -1120,7 +1120,7 @@ module Pscalar
     integer, parameter :: n_pars=1
     integer(KIND=ikind8), dimension(n_pars) :: p_par
 
-    call copy_addr_c(pscalar_diff,p_par(1))
+    call copy_addr(pscalar_diff,p_par(1))
 
     endsubroutine pushpars2c
 !***********************************************************************
