Python: modified fort2h5 to deal with r-slices and phi-averages. Also adapted related read/io routines and fixed some bugs.

python/pencil/io/snapshot.py
@@ -66,7 +66,7 @@ def write_snapshot(
 
     *lshear*:
       Flag for the shear.
-    
+
     *dx, dy, dz* : float
       Grid spacing (for calculation in the case of nonequidistant grids, see subroutine grid.f90/construct_grid)
     """
@@ -1048,6 +1048,11 @@ def write_h5_averages(
             n1 = dim.ny
             nn = procdim.ny
         n2 = dim.nx
+        if file_name == "phi":
+            nproc = dim.nprocz
+            n1 = dim.z
+            n2 = dim.nx/2.
+
         # number of iterations to record
     if not nt:
         nt = aver.t.shape[0]
@@ -1097,43 +1102,50 @@ def write_h5_averages(
                 comm=comm,
                 size=size,
             )
+        if file_name == "phi":
+            file_name = "phiavg"
         for key in aver.__getattribute__(file_name).__dict__.keys():
-            data = aver.__getattribute__(file_name).__getattribute__(key)
-            if file_name == "y" or file_name == "z":
-                data = np.swapaxes(data, 1, 2)
-            for it in range(0, nt):
-                if aver_by_proc:
-                    dataset_h5(
-                        ds[str(it)],
-                        key,
-                        status=state,
-                        shape=(n1, n2),
-                        dtype=precision,
-                        overwrite=overwrite,
-                        rank=rank,
-                        comm=comm,
-                        size=size,
-                    )
-                else:
-                    dataset_h5(
-                        ds[str(it)],
-                        key,
-                        status=state,
-                        shape=data[0].shape,
-                        dtype=precision,
-                        overwrite=overwrite,
-                        rank=rank,
-                        comm=comm,
-                        size=size,
-                    )
+            if not key == "key":
+                data = aver.__getattribute__(file_name).__getattribute__(key)
+                #if file_name == "y" or file_name == "z":
+                #    data = np.swapaxes(data, 1, 2)
+                for it in range(0, nt):
+                    if aver_by_proc:
+                        dataset_h5(
+                            ds[str(it)],
+                            key,
+                            status=state,
+                            shape=(n1, n2),
+                            dtype=precision,
+                            overwrite=overwrite,
+                            rank=rank,
+                            comm=comm,
+                            size=size,
+                        )
+                    else:
+                        try:
+                            data.shape
+                            dataset_h5(
+                                ds[str(it)],
+                                key,
+                                status=state,
+                                shape=data[0].shape,
+                                dtype=precision,
+                                overwrite=overwrite,
+                                rank=rank,
+                                comm=comm,
+                                size=size,
+                            )
+                        except:
+                            continue
         for it in indx:
             ds[str(it)]["time"][:] = aver.t[it - indx[0]]
         for key in aver.__getattribute__(file_name).__dict__.keys():
             # key needs to be broadcast as order of keys may vary on each process
             # causing segmentation fault
             data = aver.__getattribute__(file_name).__getattribute__(key)
-            if file_name == "y" or file_name == "z":
-                data = np.swapaxes(data, 1, 2)
+            #if file_name == "y" or file_name == "z":
+            #    data = np.swapaxes(data, 1, 2)
             if not quiet:
                 print("writing", key, "on rank", rank)
                 sys.stdout.flush()
@@ -1141,7 +1153,11 @@ def write_h5_averages(
                 if aver_by_proc:
                     ds[str(it)][key][proc * nn : (proc + 1) * nn] = data[it - indx[0]]
                 else:
-                    ds[str(it)][key][:] = data[it - indx[0]]
+                    try:
+                        data.shape
+                        ds[str(it)][key][:] = data[it - indx[0]]
+                    except:
+                        continue
     if not quiet:
         print(filename + " written on rank {}".format(rank))
         sys.stdout.flush()
@@ -1225,7 +1241,7 @@ def write_h5_slices(
     # open file for writing data
     nt = vslice.t.shape[0]
     for extension in vslice.__dict__.keys():
-        if not extension in "t":
+        if not (extension == "t" or extension == "coordinate" or extension == "position"):
             for field in vslice.__getattribute__(extension).__dict__.keys():
                 filename = join(datadir, field + "_" + extension + ".h5")
                 if append:

python/pencil/read/phiaverages.py
@@ -58,7 +58,7 @@ class Averages(object):
             print(i)
 
     def read(
-        self, datadir="data", avg_file="1", var_index=-1, iter_list=None, precision="f"
+        self, datadir="data", avg_file=None, var_index=-1, iter_list=None, precision="f"
     ):
         """
          read(datadir="data", avg_file="1", var_index=-1, iter_list=None, precision="f")
@@ -86,6 +86,8 @@ class Averages(object):
         """
 
         import os
+        import numpy as np
+        from pencil import read
 
         l_h5 = False
 
@@ -101,9 +103,18 @@ class Averages(object):
         else:
             if os.path.exists("data/averages/phiavg.list"):
                 in_file_name_list.append("data/averages/phiavg.list")
-                aver_file_name_list.append(
+                if avg_file:
+                    aver_file_name_list.append(
                     os.path.join("averages", "PHIAVG" + avg_file)
-                )
+                    )
+                else:
+                    nphiavg = sum(1 for line in open('data/averages/phiavg.files'))
+                    in_file_name_list = []
+                    for phi_index in range(1,nphiavg):
+                        in_file_name_list.append("data/averages/phiavg.list")
+                        aver_file_name_list.append(
+                        os.path.join("averages", "PHIAVG" + str(phi_index))
+                        )
         if not in_file_name_list:
             print("error: invalid plane name")
             sys.stdout.flush()
@@ -112,6 +123,13 @@ class Averages(object):
         class Foo(object):
             pass
 
+        glob_dim = read.dim(datadir)
+        nu = glob_dim.nx / 2
+        nv = glob_dim.nz
+        t_list = np.zeros(shape=(len(aver_file_name_list)))
+        raw_data_list = np.zeros(shape=(len(aver_file_name_list),42,int(nv),int(nu)))
+
+        ii=0
         for in_file_name, aver_file_name in zip(in_file_name_list, aver_file_name_list):
             # This one will store the data.
             ext_object = Foo()
@@ -135,20 +153,24 @@ class Averages(object):
                 l_h5=l_h5,
             )
 
-            # Add the raw data to self.
-            var_idx = 0
-            for var in variables:
-                if var_index >= 0:
-                    if var_idx == var_index:
-                        setattr(ext_object, var.strip(), raw_data[:, ...])
-                else:
-                    setattr(ext_object, var.strip(), raw_data[var_idx, :, ...])
-                var_idx += 1
+            t_list[ii] = t
+            raw_data_list[ii,:] = raw_data
+            ii=ii+1
 
-            self.t = t
-            self.r_cyl = r_cyl
-            self.z_cyl = z_cyl
-            self.phiavg = ext_object
+        # Add the raw data to self.
+        var_idx = 0
+        for var in variables:
+            if var_index >= 0:
+                if var_idx == var_index:
+                    setattr(ext_object, var.strip(), raw_data_list[:,:, ...])
+            else:
+                setattr(ext_object, var.strip(), raw_data_list[:,var_idx, :, ...])
+            var_idx += 1
+
+        self.t = t_list
+        self.r_cyl = r_cyl
+        self.z_cyl = z_cyl
+        self.phiavg = ext_object
 
         return 0
 
@@ -200,7 +222,6 @@ class Averages(object):
 
             # Prepare the raw data.
             raw_data = []
-            t = []
 
             # Read records
             # path=os.path.join(datadir, aver_file_name)