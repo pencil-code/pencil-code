c09f9ad8c6118ead7d4678aab0b9c590aba53615
mrheinhardt <mreinhardt@nordita.org>
2022-12-02 17:53:35 +0200

    MR: allocate df only if nt>0

--- src/farray_alloc.f90
+++ src/farray_alloc.f90
@@ -16,19 +16,27 @@
 
   integer :: stat
 
-    allocate( f(mx,my,mz,nvar+naux+nscratch+nglobal),STAT=stat)
-    if (stat>0) call fatal_error('farray_alloc','Could not allocate memory for f')
-    allocate(df(mx,my,mz,nvar),STAT=stat)
-    if (stat>0) call fatal_error('farray_alloc','Could not allocate memory for df')
+    !allocate( f(mx,my,mz,nvar+naux+nscratch+nglobal),STAT=stat)
+    !if (stat>0) call fatal_error('farray_alloc','Could not allocate memory for f')
+    !allocate(df(mx,my,mz,nvar),STAT=stat)
+    !if (stat>0) call fatal_error('farray_alloc','Could not allocate memory for df')
     !!print*, 'stat,mfarry,nfarray=',stat,mvar,maux,mscratch,mglobal,nvar,naux,nscratch,nglobal
 
     !mvar=nvar; maux=naux; maux_com=naux_com; mscratch=nscratch; mglobal=nglobal
+
+    allocate(f(mx,my,mz,mfarray),STAT=stat)
+    if (stat>0) call fatal_error('farray_alloc','Could not allocate memory for f')
+    if (nt>0) then
+      allocate(df(mx,my,mz,mvar),STAT=stat)
+      if (stat>0) call fatal_error('farray_alloc','Could not allocate memory for df')
+    endif
 
   endsubroutine initialize
 !******************************************************************************
   subroutine finalize
 
-    deallocate(f,df) 
+    deallocate(f) 
+    if (allocated(df)) deallocate(df) 
 
   endsubroutine finalize
 !******************************************************************************
