39042ff10b2a265a6ac6aba5215c25501913461b
Frederick Gent <fred.gent.ncl@gmail.com>
2022-12-13 09:32:35 +0200

    Use int64 to avoid integer overflow

--- python/pencil/math/primes.py
+++ python/pencil/math/primes.py
@@ -28,7 +28,7 @@ def prime_factors(n):
 def divisors(factors, nmax, nmin=8):
     divisors = list()
     nn = len(factors)
-    n = factors[0]
+    n = np.int64(factors[0])
     divisors.append(n)
     for i in range(0, nn - 1):
         n = factors[i - 1] * n
@@ -73,6 +73,8 @@ def cpu_optimal(
     minghosts=7,
     quiet=True,
     size=1,
+    remesh=False,
+    nsize=None
 ):
     xdiv, ydiv, zdiv = common_factors(nx, ny, nz, nmin=nmin)
     if not quiet:
@@ -80,10 +82,11 @@ def cpu_optimal(
 
     nvar = mvar + maux
     # size of farray in MB
-    nsize = 8 * nvar * nx * ny * nz / 1024 ** 2
+    if not nsize:
+        nsize = 8 * nvar * np.int64(nx) * ny * nz / 1024 ** 2
     ncpu_max = int(nsize / MBmin) + 1
     if size > 1:
-        ncpu_max = min(size, ncpu_max)
+        ncpu_max = max(size, ncpu_max)
     if not quiet:
         print("ncpu_max", ncpu_max)
     cpu_list = list()
@@ -120,15 +123,31 @@ def cpu_optimal(
     nprocx_last = 1
     nprocy_last = 1
     nprocz_last = 1
-    npxyz = nx + ny + nz
-    for iprocz in zdiv:
-        for iprocy in ydiv:
-            for iprocx in xdiv:
-                if np.mod(ncpu_max, iprocx * iprocy * iprocz) <= 0:
-                    if nx/iprocx + ny/iprocy + nz/iprocz < npxyz:
-                        npxyz = nx/iprocx + ny/iprocy + nz/iprocz
-                        nprocz_last = iprocz; nprocy_last = iprocy; nprocx_last = iprocx;
+    if remesh:
+        npxyz = np.int64(nx) * np.int64(ny) * np.int64(nz)
+        for iprocx in xdiv:
+            for iprocy in ydiv:
+                for iprocz in zdiv:
+                    if np.mod(np.int64(ncpu_max), np.int64(iprocx) * iprocy * iprocz) == 0:
+                        if np.int64(nx)/iprocx * np.int64(ny)/iprocy * np.int64(nz)/iprocz < npxyz:
+                            npxyz = np.int64(nx)/iprocx * np.int64(ny)/iprocy * np.int64(nz)/iprocz
+                            nprocz_last = iprocz; nprocy_last = iprocy; nprocx_last = iprocx;
+    else:
+        npxyz = nx + ny + nz
+        for iprocx in xdiv:
+            for iprocy in ydiv:
+                for iprocz in zdiv:
+                    nprocz_last=iprocz
+                    if nx/iprocx + ny/iprocy + nz/iprocz <= npxyz:
+                        if np.mod(ncpu_max, iprocx * iprocy * iprocz) == 0:
+                            nprocz_last = iprocz
+                            npxyz = nz/nprocz_last + ny/nprocy_last + nx/nprocx_last
+                        if nx/nprocx_last + ny/iprocy + nz/nprocz_last < npxyz:
+                            nprocy_last = iprocy
+                            npxyz = nz/nprocz_last + ny/nprocy_last + nx/nprocx_last
+                        if nz/nprocz_last + ny/nprocy_last + nx/iprocx < npxyz:
+                            nprocx_last = iprocx
     nprocx = nprocx_last
     nprocy = nprocy_last
     nprocz = nprocz_last
-    return [xdiv, ydiv, zdiv], [int(nprocx), int(nprocy), int(nprocz)]
+    return [xdiv, ydiv, zdiv], [int(nprocx), int(nprocy), int(nprocz)], ncpu_max
