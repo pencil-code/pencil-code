448618248cecf6b7f224538832d3b67ec717fd43
Mayank Pathak <pathakmayank1998@gmail.com>
2023-04-06 14:33:03 +0530

    Added initial magnetic field in disk+corona.f90

--- src/initial_condition/disk+corona.f90
+++ src/initial_condition/disk+corona.f90
@@ -83,10 +83,10 @@ module InitialCondition
   include '../initial_condition.h'
   real :: l0_d, l_d1, psipn0, psipn1, psi0, psi1, lnrho0_c
   real :: m0, r0_d, rs=1.0, apara, h_d, dsteep, ngamma, &
-          rho0_c, cs0_c, Tc, rpos
+          rho0_c, cs0_c, Tc, rpos,psi0d,pbeta,amplaa
 !
-  namelist /initial_condition_pars/ r0_d, apara, & 
-  h_d, rho0_c, dsteep, ngamma, cs0_c, Tc, rpos
+  namelist /initial_condition_pars/ r0_d, apara,amplaa, & 
+  h_d, rho0_c, dsteep, ngamma, cs0_c, Tc, rpos, pbeta
 !
   contains
 !***********************************************************************
@@ -142,14 +142,15 @@ module InitialCondition
       use FArrayManager,   only: farray_use_global
       use Sub,             only: get_radial_distance, location_in_proc
       use SharedVariables, only: get_shared_variable
-
+      
       real, dimension (mx,my,mz,mfarray), optional, intent(inout):: f
       real, dimension (:,:),              optional, intent(out)  :: profiles
-      real, dimension (mx, my, mz) :: psi, masked, xmesh, tmasked
+      real, dimension (mx, my, mz) :: psi, rcut, masked, xmesh, tmasked, masked3
       real, dimension (mx) :: rr_sph, rr_cyl
-      real, dimension (nx) :: rho_d, lnrho_c, psipn, l_d, vphi_d, lnT_d
+      real, dimension (nx) :: rho_d, lnrho_c, psipn, l_d, vphi_d, lnT_d, aphi
+      real, dimension (nx) :: dzphi_d, drphi_d, pg, dzrho_d, drrho_d, kphi, pg_c, pbeta_c
       real, pointer :: g0
-      real :: cp1, m00
+      real :: cp1, m00, rpn=1.2, pg0, drphi_d0, drrho_d0
       integer :: lpos, mpos, npos, procno,procnomax
 
 ! Some constants
@@ -161,7 +162,7 @@ module InitialCondition
       l_d1=l0_d*(2.0*rs/r0_d)**apara
       psipn0=-g0/(r0_d-rs)
       psipn1=-g0/rs
-!      psi0=psipn0+1.0/(2.0-2.0*apara)*(l0_d/r0_d)**2.0
+      psi0d=psipn0+1.0/(2.0-2.0*apara)*(l0_d/r0_d)**2.0
       psi1=psipn1+1.0/(2.0-2.0*apara)*(l_d1/(2.0*rs))**2.0
 !    
       call get_cp1(cp1) 
@@ -170,7 +171,7 @@ module InitialCondition
         do m=m1,m2
           xmesh(:,m,n)=x
           call get_radial_distance(rr_sph,rr_cyl)
-!
+          rcut(:,m,n)=rr_sph          
 ! Specific angular momentum
 !        
           l_d=l0_d*(rr_cyl(l1:l2)/r0_d)**apara
@@ -208,11 +209,16 @@ module InitialCondition
              elsewhere
                tmasked=1.0
             end where mask2
+      mask3: where (rcut .gt. rpn)
+               masked3=1.0
+            elsewhere
+               masked3=0.0
+            end where mask3
 !
       do n=n1,n2
         do m=m1,m2
           call get_radial_distance(rr_sph,rr_cyl)
-!
+       
 ! Specific angular momentum
 !
           l_d=l0_d*(rr_cyl(l1:l2)/r0_d)**apara
@@ -220,13 +226,41 @@ module InitialCondition
 ! Pseudo-Newtonian potential
 !
         psipn=-g0/sqrt((rr_sph(l1:l2)-rs)**2+(1e-3*rs)**2)
-
+        
 !  Disk Density
-        
+                  
         rho_d=exp(lnrho0)*(1.0-gamma*masked(l1:l2,m,n)*(psi(l1:l2,m,n)-psi0)/(cs0**2*(ngamma+1.0)))**ngamma
-        
+!        rho_d=exp(lnrho0)*(1.0-gamma*(psi(l1:l2,m,n)-psi0d)/(cs0**2.0*(ngamma+1.0)))**ngamma
 ! Corona Density
-        lnrho_c=lnrho0_c-(psipn-psipn1)*gamma/cs0_c**2
+        lnrho_c=lnrho0_c-masked3(l1:l2,m,n)*(psipn-psipn1)*gamma/cs0_c**2
+!        print *, minval(psipn-psipn1), maxval(psipn-psipn1)
+        
+       ! dzphi_d=g0/((rr_sph(l1:l2)-rs)**2+(1e-3*rs)**2)*z(n)/sqrt(rr_sph(l1:l2)**2.0+(1e-3*rs)**2.0)
+        
+        !drphi_d=g0/((rr_sph(l1:l2)-rs)**2+(1e-3*rs)**2)*rr_cyl(l1:l2)/sqrt(rr_sph(l1:l2)**2.0+&
+         !     (1e-3*rs)**2)-l_d**2.0/((1-apara)*sqrt(rr_cyl(l1:l2)**2+(1e-3*rs)**2)**3.0)
+        
+        pg=exp(lnrho0)**(-1.0/ngamma)*cs0**2.0/gamma*(rho_d)**(1.0+1.0/ngamma)
+        pg_c=exp(lnrho_c)*cs0_c**2.0/gamma
+       ! dzrho_d=-gamma*dzphi_d/(cs0**2.0*(ngamma+1.0))*exp(lnrho0)*ngamma*(1.0-&
+       !         gamma*(psi(l1:l2,m,n)-psi0)/(cs0**2*(ngamma+1.0)))**(ngamma-1.0)
+        
+       ! drrho_d=-gamma*drphi_d/(cs0**2.0*(ngamma+1.0))*exp(lnrho0)*ngamma*(1.0-&
+        !        gamma*(psi(l1:l2,m,n)-psi0)/(cs0**2*(ngamma+1.0)))**(ngamma-1.0)
+        
+        !drphi_d0=g0/((r0_d*rs-rs)**2+(1e-3*rs)**2)*40.0*rs/sqrt((r0_d*rs)**2.0+&
+         !     (1e-3*rs)**2)-l0_d**2.0/((1-apara)*sqrt((r0_d*rs)**2+(1e-3*rs)**2)**3.0)
+
+        !pg0=exp(lnrho0)**(-1.0/ngamma)*cs0**2.0/gamma*(exp(lnrho0))**(1.0+1.0/ngamma)
+
+        !drrho_d0=-gamma*drphi_d0/(cs0**2.0*(ngamma+1.0))*exp(lnrho0)*ngamma*(1.0/&
+        !        (cs0**2*(ngamma+1.0)))**(ngamma-1.0)
+        aphi=amplaa*rpos**2.0/(rpos**2.0+(x(l1:l2)**2.0+z(n)**2.0))**1.5*(1+15.0*rpos**2.0*&
+                x(l1:l2)**2.0/(8.0*(rpos**2.0+(x(l1:l2)**2.0+z(n)**2.0))**2.0))
+        
+        !kphi=sqrt(2.0*5.08e-20*pg0/(pbeta*(drrho_d0)**2.0))
+        !f(l1:l2,m,n,iay)=kphi*x(l1:l2)*rho_d*masked(l1:l2,m,n)
+        f(l1:l2,m,n,iay)=aphi*x(l1:l2)*masked(l1:l2,m,n)
         f(l1:l2,m,n,ilnrho) = log(rho_d+exp(lnrho_c))
         vphi_d=l_d*x(l1:l2)/(rr_cyl(l1:l2)**2+(1e-3*rs)**2)*masked(l1:l2,m,n) 
         f(l1:l2,m,n,iuy) = vphi_d  
@@ -263,37 +297,11 @@ module InitialCondition
 !  Initialize entropy.
 !
 !  07-may-09/wlad: coded
-
-!/mayank
-      !use Mpicomm, only: mpibcast
-      !use EquationOfState, only: get_cp1, cs0, cs20, cs2bot, cs2top, rho0, lnrho0, &
-                             !gamma, gamma1, gamma_m1
-!      use FArrayManager,   only: farray_use_global
-!      use Sub,             only: get_radial_distance, location_in_proc, smooth
-!      use SharedVariables, only: get_shared_variable
-
+! 
       real, dimension (mx,my,mz,mfarray), intent(inout) :: f
- !     real, dimension (mx, my, mz) :: psi, xmesh, masked, tmasked
-  !    real, dimension (mx) :: rr_sph, rr_cyl
-   !   real, dimension (nx) :: rho_d, psipn, l_d, lnT_d
-    !  real, pointer :: g0
-     ! real :: cp1
-      !integer :: lpos, mpos, npos
-
-      !call get_shared_variable('g0',g0)
-      !lnrho0_c=log(rho0_c)
-      !rs=2*g0/c_light**2
-      !l0_d=sqrt(g0*r0_d**3.0)/(r0_d-rs)
-!      l_d1=l0_d*(2.0*rs/r0_d)**aparan=n1,n2
-        !do m=m1,m2
-
-        !rho_d=exp(lnrho0)*(1.0-gamma*masked(l1:l2,m,n)*(psi(l1:l2,m,n)-psi0)/(cs0**2*(ngamma+1.0)))**ngamma
-        !lnT_d=(log(rho_d)-lnrho0)/ngamma+log(cs0**2.0/(gamma_m1/cp1))
-        !f(l1:l2,m,n,ilnTT)=log(exp(lnT_d)+Tc*tmasked(l1:l2,m,n))
-
-       ! enddo
-      !enddo
+! 
          call keep_compiler_quiet(f)
+! 
     endsubroutine initial_condition_ss
 !***********************************************************************
     subroutine initial_condition_aa(f)
@@ -303,8 +311,7 @@ module InitialCondition
 !  07-may-09/wlad: coded
 !
       real, dimension (mx,my,mz,mfarray), intent(inout) :: f
-!
-!  SAMPLE IMPLEMENTATION
+
 !
       call keep_compiler_quiet(f)
 !
