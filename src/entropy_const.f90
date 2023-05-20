210c078ce8f486f9a9798112df05f626ee9881b5
mrheinhardt <mreinhardt@nordita.org>
2023-05-20 04:49:36 +0300

    MR: updated (was not even usable); added dummyinc mechansim

--- src/entropy_const.f90
+++ src/entropy_const.f90
@@ -16,12 +16,14 @@
 ! Declare (for generation of cparam.inc) the number of f array
 ! variables and auxiliary variables added by this module
 !
+! CPARAM logical, parameter :: lentropy = .true.
 ! CPARAM logical, parameter :: ltemperature = .false.
 ! CPARAM logical, parameter :: lthermal_energy = .false.
 !
 ! MVAR CONTRIBUTION 0
 ! MAUX CONTRIBUTION 0
 !
+! PENCILS PROVIDED Ma2; fpres(3); sglnTT(3)
 !***************************************************************
 !
 module Energy
@@ -47,7 +49,6 @@ module Energy
   logical :: lcalc_heatcond_constchi=.false.
   logical :: lenergy_slope_limited=.false.
   character (len=labellen), dimension(ninit) :: initss='nothing'
-  integer :: pushpars2c  ! should be procedure pointer (F2003)
 !
   ! input parameters
   namelist /entropy_init_pars/ &
@@ -57,7 +58,8 @@ module Energy
   namelist /entropy_run_pars/ &
       dummy
 !
-  ! other variables (needs to be consistent with reset list below)
+! diagnostics (needs to be consistent with reset list below)
+!
   integer :: idiag_dtc=0,idiag_ethm=0,idiag_ethdivum=0,idiag_ssm=0
   integer :: idiag_ugradpm=0,idiag_ethtot=0,idiag_dtchi=0,idiag_ssmphi=0
 !
@@ -70,8 +72,6 @@ module Energy
 !
 !  6-nov-01/wolf: coded
 !
-      lentropy = .true.
-!
 !  identify version number
 !
       if (lroot) call svn_id( &
@@ -79,7 +79,7 @@ module Energy
 !
     endsubroutine register_energy
 !***********************************************************************
-    subroutine initialize_energy
+    subroutine initialize_energy(f)
 !
 !  called by run.f90 after reading parameters, but before the time loop
 !
@@ -87,10 +87,13 @@ module Energy
 !
       use Cdata
 !
+      real, dimension (mx,my,mz,mfarray) :: f
+!
       integer :: ierr
 !
-  ! run parameters
-  namelist /entropy_initialize_pars/ &
+! run parameters
+!
+      namelist /entropy_initialize_pars/ &
        iss_profile,xss_corona,wss_corona,ss_corona
 !
 !  read initialization parameters
@@ -114,6 +117,7 @@ module Energy
         print*,'initialize_energy: wss_corona,ss_corona=',xss_corona,wss_corona,ss_corona
       endif
 !
+      call keep_compiler_quiet(f)
       return
 99    if (lroot) print*,'i/o error'
 !
@@ -125,10 +129,10 @@ module Energy
 !  07-nov-2001/wolf: coded
 !  24-nov-2002/tony: renamed for consistancy (i.e. init_[variable name])
 !
-      use Cdata
-!
       real, dimension (mx,my,mz,mfarray) :: f
 !
+      integer :: j
+
       do j=1,ninit
         select case (initss(j))
 !
@@ -140,6 +144,14 @@ module Energy
 !
     endsubroutine init_energy
 !***********************************************************************
+    subroutine energy_before_boundary(f)
+
+      real, dimension (mx,my,mz,mfarray), intent(INOUT) :: f
+
+      call keep_compiler_quiet(f)
+
+    endsubroutine energy_before_boundary
+!***********************************************************************
     subroutine energy_after_boundary(f)
 
       real, dimension (mx,my,mz,mfarray), intent(INOUT) :: f
@@ -147,39 +159,62 @@ module Energy
       call keep_compiler_quiet(f)
 
       if (lenergy_slope_limited) &
-        call fatal_error('energy_after_boundary', &
-                         'Slope-limited diffusion not implemented')
+        call not_implemented('energy_after_boundary','slope-limited diffusion')
 
     endsubroutine energy_after_boundary
 !***********************************************************************
-    subroutine denergy_dt(f,df,uu,glnrho,divu,rho1,lnrho,cs2,TT1,shock,gshock,bb,bij)
+    subroutine pencil_criteria_energy
+!
+    endsubroutine pencil_criteria_energy
+!***********************************************************************
+    subroutine pencil_interdep_energy(lpencil_in)
+!
+!  Interdependency among pencils from the Energy module is specified here.
+!
+      logical, dimension (npencils) :: lpencil_in
+
+      call keep_compiler_quiet(lpencil_in)
+
+    endsubroutine pencil_interdep_energy
+!***********************************************************************
+    subroutine calc_pencils_energy(f,p)
+!
+!  Calculate Energy pencils.
+!  Most basic pencils should come first, as others may depend on them.
+!
+      real, dimension (mx,my,mz,mfarray) :: f
+      type (pencil_case) :: p
+
+      call keep_compiler_quiet(f)
+      call keep_compiler_quiet(p)
+
+    endsubroutine calc_pencils_energy
+!***********************************************************************
+    subroutine denergy_dt(f,df,p)
 !
 !  28-mar-02/axel: dummy routine, adapted from entropy.f90 of 6-nov-01.
 !  19-may-02/axel: added isothermal pressure gradient
 !   9-jun-02/axel: pressure gradient added to du/dt already here
 !
-      use Density
-      use Diagnostics
-      use Sub
+      use DensityMethods, only: getlnrho
       use EquationOfState, only: pressure_gradient
 !
       real, dimension (mx,my,mz,mfarray) :: f
       real, dimension (mx,my,mz,mvar) :: df
-      real, dimension (nx,3,3) :: bij
-      real, dimension (nx,3) :: uu,glnrho,gshock,bb
-      real, dimension (nx) :: lnrho,rho1,divu,cs2,TT1,uglnrho,shock,rho
-      real, dimension (nx) :: ss,cp1tilde
+      type(pencil_case) :: p
+ 
+      real, dimension (nx,3) :: glnrho
+      real, dimension (nx) :: lnrho,cs2,cp1tilde
       integer :: j,ju
 !
-      intent(in) :: f,uu,glnrho,rho1,shock,gshock
-      intent(out) :: cs2,TT1  !(df is dummy)
+      intent(in) :: f
 !
 !  sound speed squared and inverse temperature
 !
-      TT1=0.
-      ss=profxss
-      call pressure_gradient(f,lnrho,cs2,cp1tilde)
-      print*, it, m, n, maxval(cs2), maxval(ss)
+      call pressure_gradient(f,cs2,cp1tilde)
+      call getlnrho(f(:,m,n,ilnrho),lnrho)
+
+      if (headtt) print*, it, m, n, maxval(cs2), maxval(profxss)
 !
 !  ``cs2/dx^2'' for timestep
 !
@@ -193,49 +228,42 @@ module Energy
         df(l1:l2,m,n,iuy)=df(l1:l2,m,n,iuy)-cs2*(glnrho(:,2))
         df(l1:l2,m,n,iuz)=df(l1:l2,m,n,iuz)-cs2*(glnrho(:,3))
       endif
-!
-!  Calculate energy related diagnostics
-!
-      if (ldiagnos) then
-        if (idiag_dtc/=0) &
-            call max_mn_name(sqrt(advec_cs2)/cdt,idiag_dtc,l_dt=.true.)
-        if (idiag_ugradpm/=0) then
-          call dot_mn(uu,glnrho,uglnrho)
-          call sum_mn_name(rho*cs2*uglnrho,idiag_ugradpm)
-        endif
-      endif
-!
-      call keep_compiler_quiet(f,df)
-      call keep_compiler_quiet(uu)
-      call keep_compiler_quiet(divu)
-      call keep_compiler_quiet(rho1)
-      call keep_compiler_quiet(shock)
-      call keep_compiler_quiet(gshock)
-      call keep_compiler_quiet(bb)
-      call keep_compiler_quiet(bij)
-!
+
+      call calc_diagnostics_energy(f,p)
+
     endsubroutine denergy_dt
 !***********************************************************************
     subroutine calc_diagnostics_energy(f,p)
 
+      use Diagnostics
+      use Sub, only: dot_mn
+
       real, dimension (mx,my,mz,mfarray) :: f
       type(pencil_case) :: p
-
+!
+!  Calculate energy related diagnostics
+!
+      if (ldiagnos) then
+        if (idiag_dtc/=0) call max_mn_name(sqrt(advec_cs2)/cdt,idiag_dtc,l_dt=.true.)
+        if (idiag_ugradpm/=0) then
+          call dot_mn(p%uu,p%glnrho,p%uglnrho)
+          call sum_mn_name(p%rho*p%cs2*p%uglnrho,idiag_ugradpm)
+        endif
+      endif
+!
       call keep_compiler_quiet(f)
-      call keep_compiler_quiet(p)
-
+!
     endsubroutine calc_diagnostics_energy
 !***********************************************************************
-    subroutine energy_after_boundary(f)
+    subroutine update_char_vel_energy(f)
 !
-!  dummy routine
-!
+!  Updates characteristic velocity for slope-limited diffusion.
+
       real, dimension (mx,my,mz,mfarray) :: f
-      intent(in) :: f
-!
+
       call keep_compiler_quiet(f)
-!
-    endsubroutine energy_after_boundary
+
+    endsubroutine update_char_vel_energy
 !***********************************************************************
     subroutine rprint_energy(lreset,lwrite)
 !
@@ -268,8 +296,7 @@ module Energy
         call parse_name(iname,cname(iname),cform(iname),'dtc',idiag_dtc)
         call parse_name(iname,cname(iname),cform(iname),'dtchi',idiag_dtchi)
         call parse_name(iname,cname(iname),cform(iname),'ethtot',idiag_ethtot)
-        call parse_name(iname,cname(iname),cform(iname),&
-            'ethdivum',idiag_ethdivum)
+        call parse_name(iname,cname(iname),cform(iname),'ethdivum',idiag_ethdivum)
         call parse_name(iname,cname(iname),cform(iname),'ethm',idiag_ethm)
         call parse_name(iname,cname(iname),cform(iname),'ssm',idiag_ssm)
         call parse_name(iname,cname(iname),cform(iname),'ugradpm',idiag_ugradpm)
@@ -292,119 +319,15 @@ module Energy
 !
     endsubroutine rprint_energy
 !***********************************************************************
-    subroutine fill_farray_pressure(f)
-!
-!  18-feb-10/anders: dummy
-!
-      real, dimension (mx,my,mz,mfarray) :: f
-!
-      call keep_compiler_quiet(f)
-!
-    endsubroutine fill_farray_pressure
-!***********************************************************************
-    subroutine impose_energy_floor(f)
-!
-!  Dummy subroutine
-!
-      real, dimension(mx,my,mz,mfarray) :: f
-!
-      call keep_compiler_quiet(f)
-!
-    endsubroutine impose_energy_floor
-!***********************************************************************
-    subroutine dynamical_thermal_diffusion(uc)
-!
-!  Dummy subroutine
-!
-      real, intent(in) :: uc
-!
-      call keep_compiler_quiet(uc)
-!
-    endsubroutine dynamical_thermal_diffusion
-!***********************************************************************
-    subroutine split_update_energy(f)
-!
-!  Dummy subroutine
-!
-      real, dimension(mx,my,mz,mfarray), intent(inout) :: f
-!
-      call keep_compiler_quiet(f)
-!
-    endsubroutine
-!***********************************************************************
-    subroutine expand_shands_energy
-!
-!  Presently dummy, for possible use
-!
-    endsubroutine expand_shands_energy
-!***********************************************************************
-    subroutine energy_after_timestep(f,df,dtsub)
-!
-      real, dimension(mx,my,mz,mfarray) :: f
-      real, dimension(mx,my,mz,mvar) :: df
-      real :: dtsub
-!
-      call keep_compiler_quiet(f,df)
-      call keep_compiler_quiet(dtsub)
-!
-    endsubroutine energy_after_timestep
-!***********************************************************************    
-    subroutine update_char_vel_energy(f)
-!
-! TB implemented.
-!
-!   25-sep-15/MR+joern: coded
-!
-      real, dimension (mx,my,mz,mfarray), intent(in) :: f
-
-      call keep_compiler_quiet(f)
-
-      call warning('update_char_vel_energy', &
-           'characteristic velocity not yet implemented for entropy_const')
-
-    endsubroutine update_char_vel_energy
-!***********************************************************************
-    subroutine heatcond_TT_2d(TT, hcond, dhcond)
-!
-! dummy
-!
-      implicit none
-!
-      real, dimension(:,:), intent(in) :: TT
-      real, dimension(:,:), intent(out) :: hcond
-      real, dimension(:,:), optional :: dhcond
-
-      call keep_compiler_quiet(TT,hcond,dhcond)
-
-    endsubroutine heatcond_TT_2d
-!***********************************************************************
-    subroutine heatcond_TT_1d(TT, hcond, dhcond)
-!
-! dummy
-!
-      implicit none
-!
-      real, dimension(:), intent(in) :: TT
-      real, dimension(:), intent(out) :: hcond
-      real, dimension(:), optional :: dhcond
-
-      call keep_compiler_quiet(TT,hcond,dhcond)
-
-    endsubroutine heatcond_TT_1d
-!***********************************************************************
-    subroutine heatcond_TT_0d(TT, hcond, dhcond)
-!
-! dummy
-!
-      implicit none
-!
-      real, intent(in) :: TT
-      real, intent(out) :: hcond
-      real, optional :: dhcond
-
-      call keep_compiler_quiet(TT,hcond,dhcond)
-
-    endsubroutine heatcond_TT_0d
+!********************************************************************
+!********************************************************************
+!************        DO NOT DELETE THE FOLLOWING        *************
+!********************************************************************
+!**  This is an automatically generated include file that creates  **
+!**  copies dummy routines from nospecial.f90 for any Special      **
+!**  routines not implemented in this file                         **
+!**                                                                **
+    include 'energy_common.inc'
 !***********************************************************************
 endmodule Energy
 
