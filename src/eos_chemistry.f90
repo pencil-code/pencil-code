c31d1a47ffa4265113c14e64536b3f709c97fc06
mrheinhardt <mreinhardt@nordita.org>
2023-06-20 18:59:38 +0300

    MR: reinstated providing of (yet no defining) of pencils gamma; gamma_m1

--- src/eos_chemistry.f90
+++ src/eos_chemistry.f90
@@ -18,7 +18,7 @@
 !
 ! PENCILS PROVIDED hss(3,3); hlnTT(3,3); del2ss; del6ss; del6lnTT
 ! PENCILS PROVIDED yH; ee; ss; delta; glnmumol(3); ppvap; csvap2; cs2
-! PENCILS PROVIDED cp1tilde; cp
+! PENCILS PROVIDED cp1tilde; cp; gamma; gamma_m1
 ! PENCILS PROVIDED rho_anel; gradcp(3)
 !
 !
