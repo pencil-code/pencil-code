53f9e68fe33ddab352f55261a31c71ed2b59582b
fredgent <fred.gent.ncl@gmail.com>
2023-06-29 14:48:26 +0300

    403 pc comment corrected

--- src/eos_chemistry.f90
+++ src/eos_chemistry.f90
@@ -1387,7 +1387,7 @@ module EquationOfState
 !  responsive to interior dynamics.
 !  06-jun-22/fred update to allow setting scale height in start.in or run.in
 !  default is density_scale_factor=impossible so that scale_factor is 0.9, assuming
-!  unit_length = 1 kpc and scale is 400 pc. To change scale height add to
+!  unit_length = 1 kpc and scale is 403 pc. To change scale height add to
 !  start_pars or run_pars density_scale_factor=... in dimensionless units
 !  Copied from eos_ionization written for entropy - may need revision
 !  Currently not correct for energy variable
