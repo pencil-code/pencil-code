8408cc5cfc6b8143d2cf4dd9fc91adbe7fb4fbab
fredgent <fred.gent.ncl@gmail.com>
2023-08-22 12:01:15 +0300

    Improved updating substep and accuracy thresholds

--- src/timestep_rkf.f90
+++ src/timestep_rkf.f90
@@ -14,7 +14,7 @@ module Timestep
   real, parameter :: safety      =  0.9
   real, parameter :: dt_decrease = -0.25
   real, parameter :: dt_increase = -0.20
-  real            :: errcon
+  real            :: errcon, errcon25
 !
   contains
 !
@@ -32,16 +32,19 @@ module Timestep
 !
       if (dt==0.) then
         call warning('initialize_timestep','dt=0 not appropriate for Runge-Kutta-Fehlberg'// &
-                     'set to 1e-4')
-        dt=1e-4
+                     'set to 1e-6')
+        dt=1e-6
       endif
 !
 ! General error condition
 !
       errcon = (5.0/safety)**(1.0/dt_increase)
+      errcon25 = (eps_rkf*errcon)**2.5
 !
+      !ldt is set after read_persistent in rsnap, so dt0==0 used to read, but ldt=F for write
       ldt=.false.
-      dt0=dt
+      !overwrite the persistent time_step from dt0 in run.in
+      if (dt0/=0.) dt=dt0
 
     endsubroutine initialize_timestep
 !***********************************************************************
@@ -58,8 +61,8 @@ module Timestep
       real, dimension (mx,my,mz,mvar) :: df
       type (pencil_case) :: p
 !
-      real :: errmax, tnew
-      real :: dt_temp, dt_next
+      real :: errmax, dt_temp
+      double precision :: tnew, told
       integer :: j,i
 !
 !  dt_beta_ts may be needed in other modules (like Dustdensity) for fixed dt
@@ -67,40 +70,43 @@ module Timestep
 !      if (.not. ldt) dt_beta_ts=dt*beta_ts
 !
       lfirst=.true.
-      do i=1,10
+      told=t
+      do i=1,20
         ! Do a Runge-Kutta step
         call rkck(f, df, p, errmax)
         ! Step succeeded so exit
         if (errmax <= safety) exit
         ! Step didn't succeed so decrease the time step
-!        print*,"Decreasing",errmax,dt
         dt_temp = safety*dt*(errmax**dt_decrease)
+        !if (lroot) print*,"Decreasing dt, dt_temp, errmax",dt,dt_temp,errmax
         ! Don't decrease the time step by more than a factor of ten
         dt = sign(max(abs(dt_temp), 0.1*abs(dt)), dt)
-!print*,'AXEL: ',dt,dt_temp,errmax,safety
-        tnew=t+dt
-        if (tnew == t) then
+        tnew=told+dt
+        if (tnew == told) then
           ! Guard against infinitesimal time steps
           call warning('time_step','Timestep underflow in Runge-Kutta-Fehlberg')
           ! Increase dt???
           exit
         endif
+        t=told
       enddo
+      !if (abs(t - (told + dt)) > errcon25) then
+      !  call warning('time_step','dt is not equal to sum of dtsub')
+      !  if (lroot) print*,"t, told, dt, t-told",t, told, dt, t-told
+      !endif
+      call update_after_substep(f,df,dt,.true.)
+!
+! Time step to try next time
 !
       if (errmax > errcon) then
-        ! Increase the time step
-        dt_next = safety*dt*(errmax**dt_increase)
+        ! Decrease the time step
+        dt = safety*dt*(errmax**dt_increase)
       else
         ! But not by more than a factor of 5
-        dt_next = 5.0*dt
+        dt = 5.0*dt
       endif
-!print*,'AXEL2: ',dt,dt_temp,errmax,safety
 !
       if (ip<=6) print*,'TIMESTEP: iproc,dt=',iproc_world,dt
-! Increase time
-      t = t+dt
-! Time step to try next time
-      dt = dt_next
 !
 !  Time evolution of grid variables
 !  (do this loop in pencils, for cache efficiency)
@@ -116,8 +122,9 @@ module Timestep
 ! Explicit fifth order Runge--Kutta--Fehlberg time stepping
 !
       use Mpicomm, only: mpiallreduce_max,MPI_COMM_WORLD
-      use Equ, only: pde
+      use Equ, only: pde, impose_floors_ceilings
       use Shear, only: advance_shear
+      use Boundcond, only: update_ghosts
 !
 ! RK parameters by Cash and Karp
 !
@@ -165,7 +172,7 @@ module Timestep
 !   call pde(f + b21*k(:,:,:,:,1), k(:,:,:,:,2), p)
       real, dimension (mx,my,mz,mfarray) :: tmp
       real, dimension(nx) :: scal, err
-      real :: errmax, errmaxs,dt_tmp
+      real :: errmax, errmaxs, dtsub
       integer :: j
 !
       df=0.
@@ -173,23 +180,31 @@ module Timestep
       errmaxs=0.
       k=0.
 !
+!  FIRST SUBSTEP
+!
       call pde(f, k(:,:,:,:,1), p)
+      dtsub = dt*(c1-dc1)
       if (lshear) then
         tmp = f
-        dt_tmp = dt*c1
-        call advance_shear(tmp, k(:,:,:,:,1), dt_tmp)
+        call impose_floors_ceilings(tmp)
+        call update_ghosts(tmp)  ! Necessary for non-FFT advection but unnecessarily overloading FFT advection
+        call advance_shear(tmp, k(:,:,:,:,1), dtsub)
       endif
       do j=1,mvar; do n=n1,n2; do m=m1,m2
         k(l1:l2,m,n,j,1) = dt*k(l1:l2,m,n,j,1)
       enddo; enddo; enddo
 !
-!print*, 'k,f', maxval(abs(k(l1:l2,m1:m2,n1:n2,1:mvar,1))), maxval(abs(f(l1:l2,m1:m2,n1:n2,:)))
       lfirst=.false.
 !
 !  Transferring auxiliaries from f to tmp.
 !
       if (mfarray>mvar) tmp(:,:,:,mvar+1:mfarray) = f(:,:,:,mvar+1:mfarray)
       tmp(:,:,:,1:mvar) = f(:,:,:,1:mvar) + b21*k(:,:,:,:,1)
+!
+      call update_after_substep(tmp,k(:,:,:,:,1),dtsub,.false.)
+      t = t + dtsub
+!
+!  DUMMY SUBSTEP
 !
       call pde(tmp, k(:,:,:,:,2), p)
       do j=1,mvar; do n=n1,n2; do m=m1,m2
@@ -198,11 +213,17 @@ module Timestep
 !
       tmp(:,:,:,1:mvar) = f(:,:,:,1:mvar) + b31*k(:,:,:,:,1) &
                                           + b32*k(:,:,:,:,2)
+      call update_after_substep(tmp,k(:,:,:,:,2),0.,.false.)
+!
+!
+!  SECOND SUBSTEP
 !
       call pde(tmp, k(:,:,:,:,3), p)
+      dtsub = dt*(c3-dc3)
       if (lshear) then
-        dt_tmp = dt*c2
-        call advance_shear(tmp, k(:,:,:,:,3), dt_tmp)
+        call impose_floors_ceilings(tmp)
+        call update_ghosts(tmp)
+        call advance_shear(tmp, k(:,:,:,:,3), dtsub)
       endif
       do j=1,mvar; do n=n1,n2; do m=m1,m2
         k(l1:l2,m,n,j,3) = dt*k(l1:l2,m,n,j,3)
@@ -212,10 +233,17 @@ module Timestep
                                           + b42*k(:,:,:,:,2) &
                                           + b43*k(:,:,:,:,3)
 !
+      call update_after_substep(tmp,k(:,:,:,:,3),dtsub,.false.)
+      t = t + dtsub
+!
+!  THIRD SUBSTEP
+!
       call pde(tmp, k(:,:,:,:,4), p)
+      dtsub = dt*(c4-dc4)
       if (lshear) then
-        dt_tmp = dt*c3
-        call advance_shear(tmp, k(:,:,:,:,4), dt_tmp)
+        call impose_floors_ceilings(tmp)
+        call update_ghosts(tmp)
+        call advance_shear(tmp, k(:,:,:,:,4), dtsub)
       endif
       do j=1,mvar; do n=n1,n2; do m=m1,m2
         k(l1:l2,m,n,j,4) = dt*k(l1:l2,m,n,j,4)
@@ -226,10 +254,17 @@ module Timestep
                                           + b53*k(:,:,:,:,3) &
                                           + b54*k(:,:,:,:,4)
 !
+      call update_after_substep(tmp,k(:,:,:,:,4),dtsub,.false.)
+      t = t + dtsub
+!
+!  FOURTH SUBSTEP
+!
       call pde(tmp, k(:,:,:,:,5), p)
+      dtsub = dt*(c5-dc5)
       if (lshear) then
-        dt_tmp = dt*c4
-        call advance_shear(tmp, k(:,:,:,:,5), dt_tmp)
+        call impose_floors_ceilings(tmp)
+        call update_ghosts(tmp)
+        call advance_shear(tmp, k(:,:,:,:,5), dtsub)
       endif
       do j=1,mvar; do n=n1,n2; do m=m1,m2
         k(l1:l2,m,n,j,5) = dt*k(l1:l2,m,n,j,5)
@@ -241,9 +276,15 @@ module Timestep
                                           + b64*k(:,:,:,:,4) &
                                           + b65*k(:,:,:,:,5)
 !
-      call pde(tmp, df, p)
+      call update_after_substep(tmp,k(:,:,:,:,5),dtsub,.false.)
+      t = t + dtsub
 !
-!print*, 'df,tmp', maxval(abs(df)), maxval(abs(tmp))
+!  FIFTH SUBSTEP
+!
+      call pde(tmp, df, p)
+      dtsub = dt*(c6-dc6)
+      t = t + dtsub
+!
       do j=1,mvar; do n=n1,n2; do m=m1,m2
         df(l1:l2,m,n,j) = dt*df(l1:l2,m,n,j)
 !
@@ -271,7 +312,8 @@ module Timestep
           errmaxs = max(maxval(abs(err/f(l1:l2,m,n,j))),errmaxs)
         case ('cons_err')
           ! Constant error
-          scal = max(abs(f(l1:l2,m,n,j)), 1e-8)
+          ! Constrain error below impossible and limit accuracy above errcon25
+          scal = min(max(abs(f(l1:l2,m,n,j)), errcon25),impossible)
           errmaxs = max(maxval(abs(err/scal)),errmaxs)
           !
         case ('none')
@@ -280,7 +322,6 @@ module Timestep
 !
       enddo; enddo;
 
-!print*, 'j,errmaxs=', j,errmaxs
       enddo
 !
 !  Transferring auxiliaries from tmp to f.
@@ -294,6 +335,47 @@ module Timestep
       call mpiallreduce_max(errmaxs,errmax,MPI_COMM_WORLD)
 !
     endsubroutine rkck
+!***********************************************************************
+    subroutine update_after_substep(f,df,dtsub,llast)
+!
+!   Hooks for modifying f and df after the timestep is performed.
+!
+!  12-03-17/wlyra: coded
+!  28-03-17/MR: removed update_ghosts; checks for already communicated variables enabled.
+!
+      use Density,  only: density_after_timestep
+      use Hydro,    only: hydro_after_timestep
+      use Energy,   only: energy_after_timestep
+      use Magnetic, only: magnetic_after_timestep
+      use Special,  only: special_after_timestep
+      use Particles_main, only: particles_special_after_dtsub
+!
+      logical, intent(in) :: llast
+      real, dimension (mx,my,mz,mfarray) :: f
+      real, dimension (mx,my,mz,mvar) :: df
+      real :: dtsub
+!
+!  Enables checks to avoid unnecessary communication
+!
+      ighosts_updated=0
+!
+!  Dispatch to respective modules. The module which communicates
+!  the biggest number of variables should come first here.
+!
+      if (lhydro)    call hydro_after_timestep   (f,df,dtsub)
+      if (lmagnetic) call magnetic_after_timestep(f,df,dtsub)
+      if (lenergy)   call energy_after_timestep  (f,df,dtsub)
+      if (ldensity)  call density_after_timestep (f,df,dtsub)
+      if (lspecial) then
+        call special_after_timestep(f, df, dtsub, llast)
+        if (lparticles) call particles_special_after_dtsub(f, dtsub)
+      endif
+!
+!  Disables checks.
+!
+      ighosts_updated=-1
+!
+    endsubroutine update_after_substep
 !***********************************************************************
     subroutine pushpars2c(p_par)
 
