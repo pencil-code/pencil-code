6984144c1e077b40df7938c40ff83419f6a0af2a
iomsn <iomsn1@googlemail.com>
2023-08-28 18:46:22 +0100

    Added bz-correction term for the magnetic flux.

--- src/initial_condition/hopf_kink.f90
+++ src/initial_condition/hopf_kink.f90
@@ -1,17 +1,10 @@
 ! $Id$
 !
-<<<<<<< HEAD
-! Create an initial magnetic field of the Hopf kink type
-! as describe in Finkelstein, Weil (1978) (DOI: https://doi.org/10.1007/BF00680372),
-! section 2.2 C.
-! Only works in Cartesian coordinates.
-=======
 ! Initial condition for the hopf kink magnetic field as
 ! discribed by Finkelstein and Weil (1978), eqs. in 2.2 C,
 ! DOI: 10.1007/BF00680372.
 
 ! Set the single magnetic field or array of fields.
->>>>>>> Added hopf kink initial magnetic field.
 !
 !** AUTOMATIC CPARAM.INC GENERATION ****************************
 ! Declare (for generation of cparam.inc) the number of f array
@@ -42,9 +35,10 @@ module InitialCondition
   real :: ampl = 1.0
   character (len=labellen) :: configuration = 'single'
   integer :: sign_pair = -1
+  logical :: bz_correction = .false.
 
   namelist /initial_condition_pars/ &
-      ampl, configuration, sign_pair
+      ampl, configuration, sign_pair, bz_correction
 !
   contains
 !***********************************************************************
@@ -103,6 +97,9 @@ module InitialCondition
       logical :: pair
       real, dimension (400) :: s ! array for the integration
       real, dimension (nx,ny,nz,3) :: jj, tmpJ  ! This is phi for poisson.f90
+      ! Variables used for the div(B) = 0correction outside the sphere.
+      integer :: z_0_idx
+      real :: z_0, rho_0, psi_0, B_z_0, rho_section, psi_in, rho_integral, slope
 !
 ! Prepare for a pair.
 !
@@ -116,6 +113,7 @@ module InitialCondition
       endif
 
       do pair_idx=1,pair_final
+        ! Add the field inside the sphere.
         do m=m1,m2
           do n=n1,n2
             do l=l1,l2
@@ -155,7 +153,72 @@ module InitialCondition
             enddo
           enddo
           z_shift = -z_shift
-        enddo
+          
+          ! Add the vertical field outside the spherefor div(B) = 0 condition.          
+          if bz_correction then
+            ! Perform some preliminarycalculations on the equator
+            ! We use these numbers for thecorrections outside the sphere.
+            z_0_idx = Int(mz/2)
+            z_0 = z(z_0_idx)
+            rho_0 = sqrt(1**2 - z_0**2)
+            psi_0 = 0
+            ! Compute the magnetic flux though the equator part.
+            do m=m1,m2
+                do l=l1,l2
+                    if (x(l)**2 + y(m)**2 <= rho_0**2) then
+                        psi_0 = psi_0 + f(l,m,z_0_idx,iaz)*dx*dy
+                    endif
+                enddo
+            enddo
+            
+            ! Compute the magnetic field at the sphere's boundary.            
+            do s_idx = 1, 400, 1
+                s(s_idx) = 1.0/400*s_idx
+            enddo
+            integration = sum(sin(2*pi*sin(pi*s**3/2)/2)**2/s)*(s(2)-s(1))
+            B_z_0 = ampl*exp(-4*integration)
+            
+            ! Peroform the corrections for everyhorizontal slice.
+            do n=n1,n2
+                ! Circular radius of the sphere on this xy-slice.
+                if (1**2 - z(n)**2 >= 0) then
+                    rho_section = sqrt(1**2 - z(n)**2)
+                else
+                    rho_section = 0.0
+                endif
+                
+                ! Compute the flux within the section.
+                psi_in = 0
+                do m=m1,m2
+                    do l=l1,l2
+                        if (x(l)**2 + y(m)**2 <= rho_section**2) then
+                            psi_in = psi_in + f(l,m,n,iaz)*dx*dy
+                        endif
+                    enddo
+                enddo
+                
+                ! Adjust the magnetic field outside section by linearly increasing it.
+                rho_integral = 0
+                do m=m1,m2
+                    do l=l1,l2
+                        rho_integral = rho_integral + sqrt(x(l)**2 + y(m)**2)*dx*dy
+                    enddo
+                enddo
+                slope = (psi_0 - B_z_0*Lx*Ly + 2*pi*rho_section**2*B_z_0) / (rho_integral - 2.0/3*pi*rho_section**3)
+                do m=m1,m2
+                    do l=l1,l2
+                        if (x(l)**2 + y(m)**2 > rho_section**2) then
+                            f(l,m,n,iaz) = (sqrt(x(l)**2 + y(m)**2) - rho_section)*slope + B_z_0
+                        endif
+                    enddo
+                enddo
+            enddo
+          endif
+      enddo
+!
+!   Removed the mean magnetic field in the z-direction for the uncurling.
+    write(*,*) "add mean B_z in start.in under &magnetic_init_pars as b_ext = 0, 0, ", sum(f(:,:,:,iaz))/size(f(:,:,:,iaz))
+    f(:,:,:,iaz) = f(:,:,:,iaz) - sum(f(:,:,:,iaz))/size(f(:,:,:,iaz))
 !
 !  Compute curl(B) = J for the Poisson solver
       do m=m1,m2
@@ -174,7 +237,7 @@ module InitialCondition
           ju=iaa-1+j
           f(l1:l2,m1:m2,n1:n2,ju) = tmpJ(:,:,:,j)
       enddo
-!      
+     
     endsubroutine initial_condition_aa
 !***********************************************************************
     subroutine initial_condition_ss(f)
