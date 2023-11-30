pencil-code/pencil-code

commit d8d5d6c6751a28cfc33c4624ef55f9e430e425ad

MR: use output_ode at right place

src/io_mpi2.f90
@@ -441,10 +441,10 @@ module Io
           endif
         endif
 
-        if (lode) call output_ode(file)
-
       endif
 !
+      if (lode) output_ode(file)
+
     endsubroutine output_snap
 !***********************************************************************
     subroutine output_snap_finalize

src/io_collect.f90
@@ -279,9 +279,10 @@ module Io
           deallocate (gx, gy, gz)
         endif
 
-        if (lode) call output_ode(file)
       endif
 !
+      if (lode) call output_ode(file)
+
     endsubroutine output_snap
 !***********************************************************************
     subroutine output_snap_finalize

src/io_collect_xy.f90
@@ -198,9 +198,9 @@ module Io
           if (lroot) write (lun_output) gx, gy, gz, dx, dy, dz
         endif
 !
-        if (lode) call output_ode(file)
-
       endif
+
+      if (lode) call output_ode(file)
 
     endsubroutine output_snap
 !***********************************************************************

src/io_collect_xy_f2003.f90
@@ -204,8 +204,9 @@ module Io
         call collect_grid (dx_tilde, dy_tilde, dz_tilde, gx, gy, gz)
         if (lroot) write (lun_output) gx, gy, gz
 !
-        if (lode) call output_ode(file)
       endif
+
+      if (lode) output_ode(file)
 
     endsubroutine output_snap
 !***********************************************************************

commit 99d8d5355a7b233da9c7a1e2756e6389ac4618b0

Fix a syntax error introduced in commit d8d5d6c67.
Missing call.

src/io_mpi2.f90
@@ -443,8 +443,8 @@ module Io
 
       endif
 !
-      if (lode) output_ode(file)
-
+      if (lode) call output_ode(file)
+!
     endsubroutine output_snap
 !***********************************************************************
     subroutine output_snap_finalize