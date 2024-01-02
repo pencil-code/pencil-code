de89a51b7df0cd35e9c0e703f376ac4a35fbbbdb
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-01-02 14:39:37 +0530

    Python: dims.py: remove redundant checks for existence of io_strategy

--- python/pencil/read/dims.py
+++ python/pencil/read/dims.py
@@ -96,16 +96,10 @@ class Dim(object):
         import glob
         import os
 
-        lh5 = False
         if not param:
             param = read.param(datadir=datadir, quiet=True)
-        if hasattr(param, "io_strategy"):
-            if param.io_strategy == "HDF5":
-                lh5 = True
-        # Keep this for sims that were converted from Fortran to hdf5
-        if os.path.exists(os.path.join(datadir, "grid.h5")):
-            lh5 = True
-        if lh5:
+
+        if param.io_strategy == "HDF5":
             import h5py
 
             if down:
