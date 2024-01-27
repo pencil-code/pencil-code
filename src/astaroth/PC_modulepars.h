b4470038ccd75efba6093b7d0f1b581b341edda0
Matthias Rheinhardt <matthias.rheinhardt@aalto.fi>
2024-01-28 00:45:22 +0200

    MR: remove setup-specific

--- src/astaroth/PC_modulepars.h
+++
@@ -1,8 +0,0 @@
-// automatically generated; do not edit!
-#include "../hydro_pars_c.h"
-#include "../density_pars_c.h"
-#include "../magnetic_pars_c.h"
-#include "../energy_pars_c.h"
-#include "../equationofstate_pars_c.h"
-#include "../viscosity_pars_c.h"
-#include "../timestep_pars_c.h"
--- src/astaroth/PC_modulesources.h
+++
@@ -1,9 +0,0 @@
-MODULESOURCES= \
-../hydro.f90 \
-../density.f90 \
-../magnetic.f90 \
-../entropy.f90 \
-../eos_idealgas.f90 \
-../viscosity.f90 \
-../timestep.f90 \
- 
