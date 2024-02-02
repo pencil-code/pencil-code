3d2154cfc8f42984acdceabf13bf523efd9236f3
Touko Puro <>
2024-02-02 10:42:59 +0200

    remove pars_c.h from src

--- src/hydro_pars_c.h
+++
@@ -1,2 +0,0 @@
-__hydro_MOD_pushpars2c(p_par_hydro);
-     config.int_params[AC_lpressuregradient_gas]=lpressuregradient_gas; // [1-1] lpressuregradient_gas int
--- src/timestep_pars_c.h
+++
@@ -1,2 +0,0 @@
-    !  config.profiles[AC_alpha_ts]=alpha_ts; // [1-1] alpha_ts real(3)
-    !  config.profiles[AC_beta_ts]=beta_ts; // [2-1] beta_ts real(3)
