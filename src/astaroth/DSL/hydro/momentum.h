f8f9d4ec8721f724c5120898bd77d299e10d8f4a
mrheinhardt <matthias.rheinhardt@aalto.fi>
2024-04-03 14:45:43 +0200

    MR: branches in momentum.h

--- src/astaroth/DSL/hydro/momentum.h
+++ src/astaroth/DSL/hydro/momentum.h
@@ -1,11 +1,20 @@
+#if LMAGNETIC
     j =  (gradient_of_divergence(AA) - veclaplace(AA))/mu0
     rho1 = exp(-value(LNRHO))
-
+#endif
+#if LENTROPY
     cs2 = cs20 * exp(gamma * value(SS) / cp + gamma_m1 * (value(LNRHO) - lnrho0))
+#endif
 
     return - gradients(UU) * vecvalue(UU)
-           - cs2 * (gradient(SS)/cp + gradient(LNRHO))    //???
+#if LENTROPY
+           - cs2 * (gradient(SS)/cp + gradient(LNRHO))
+#else
+	   - cs20 * gradient(LNRHO)
+#endif
+#if LMAGNETIC
            + rho1 * cross(j, curl(AA))
+#endif
            + nu * (veclaplace(UU) + (1./3.) * gradient_of_divergence(UU) + 2. * stress_tensor(UU) * gradient(LNRHO))
-           + zeta * gradient_of_divergence(UU)                                                 // | overloaded?
+           + zeta * gradient_of_divergence(UU)
 
