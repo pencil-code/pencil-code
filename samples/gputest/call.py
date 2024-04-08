8c1f36e186d5980402656ee114ef40a9afdc4355
ToxPuro <touko.puro@aalto.fi>
2024-04-08 14:49:53 +0300

    stuff to allow PC to be called from Python for ODOP monitoring

---
+++ samples/gputest/call.py
@@ -0,0 +1,18 @@
+from ctypes import*
+from odop.odop_obs import OdopObs
+from qoa4ml.common import ODOP_PATH
+
+so_file = "./src/libPC.so"
+my_funcs = CDLL(so_file)
+
+def main():
+    odop_obs = OdopObs(config_path=ODOP_PATH + "config/odop_obs_conf.yaml")
+    odop_obs.start()
+
+    my_funcs.main()
+
+    odop_obs.stop()
+
+if __name__ == "__main__":
+    main()
+
