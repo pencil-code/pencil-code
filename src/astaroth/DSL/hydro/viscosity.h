5ce97ecb1e627ae15dc1de02beff0c26d1b15e96
fredgent <fred.gent.ncl@gmail.com>
2024-06-04 15:28:26 +0300

    Added hyper diffusion

---
+++ src/astaroth/DSL/hydro/viscosity.h
@@ -0,0 +1,7 @@
+if LVISC_NU_CONST {
+rhs += nu * (veclaplace(UU) + (1./3.) * gradient_of_divergence(UU) + 2. * stress_tensor(UU) * gradient(LNRHO))
+   + zeta * gradient_of_divergence(UU)}
+if LVISC_HYPER3_NU_CONST {
+   rhs += nu_hyper3 * (del6s(UU) + u_dot_grad_vec(gij5(UU),gradient(LNRHO))
+   }
+
---
+++ src/astaroth/DSL/magnetic/magnetic_diffusivity.h
@@ -0,0 +1,6 @@
+if LRESI_ETA_CONST {
+   rhs += eta * veclaplace(AA)
+}
+if LRESI_HYPER3 {
+        rhs += eta_hyper3 * del6s(AA)
+}
