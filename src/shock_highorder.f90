3d1a704a32e64d256c58114291d18ca28adb9178
Matthias Rheinhardt <matthias.rheinhardt@aalto.fi>
2024-06-05 17:57:34 +0300

    MR: exported missing dtfactor

--- src/shock_highorder.f90
+++ src/shock_highorder.f90
@@ -852,7 +852,7 @@ module Shock
 
     use Syscalls, only: copy_addr
 
-    integer, parameter :: n_pars=6
+    integer, parameter :: n_pars=7
     integer(KIND=ikind8), dimension(n_pars) :: p_par
 
     call copy_addr(ishock_max   ,p_par(1))  ! int
@@ -860,7 +860,8 @@ module Shock
     call copy_addr(shock_linear ,p_par(3))
     call copy_addr(shock_div_pow,p_par(4))
     call copy_addr(dt_div_pow   ,p_par(5))
-    call copy_addr(con_bias     ,p_par(6))
+    call copy_addr(dtfactor     ,p_par(6))
+    call copy_addr(con_bias     ,p_par(7))
 
     endsubroutine pushpars2c
 !***********************************************************************
