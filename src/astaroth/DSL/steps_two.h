fcf7bb0595952ea0426628f174a5b021d29d9572
ToxPuro <touko.puro@aalto.fi>
2024-08-18 23:15:24 +0300

    fix Z symmetric bc example

--- src/astaroth/DSL/steps_two.h
+++ src/astaroth/DSL/steps_two.h
@@ -18,20 +18,25 @@ BoundConds boundconds{
 //  bc_sym_z(BOUNDARY_Z_TOP, 1.0,AC_top,UUY,false)
 //  bc_sym_z(BOUNDARY_Z_TOP, 1.0,AC_top,UUZ,false)
 //
-//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,UUX,false)
-//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,UUY,false)
-//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,UUZ,false)
 //
 //  bc_sym_z(BOUNDARY_Z_TOP, 1.0,AC_top,AAX,false)
 //  bc_sym_z(BOUNDARY_Z_TOP, 1.0,AC_top,AAY,false)
 //  bc_sym_z(BOUNDARY_Z_TOP, 1.0,AC_top,AAZ,false)
 //
-//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,SS,false)
 //  bc_sym_z(BOUNDARY_Z_TOP, 1.0,AC_top,SS,false)
-//
-//  bc_sym_z(BOUNDARY_Z_TOP, 1.0,AC_top,RHO,false)
 //  bc_sym_z(BOUNDARY_Z_TOP, 1.0,AC_top,RHO,false)
 //
+//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,UUX,false)
+//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,UUY,false)
+//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,UUZ,false)
+//
+//
+//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,AAX,false)
+//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,AAY,false)
+//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,AAZ,false)
+//
+//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,SS,false)
+//  bc_sym_z(BOUNDARY_Z_BOT, 1.0,AC_bot,RHO,false)
 //}
 ////TP example 2
 //BoundConds boundconds{
