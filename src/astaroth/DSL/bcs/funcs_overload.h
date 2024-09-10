85afceccbb9f98a57ce4fd8e9ddaa3f3cbe62d06
Matthias Rheinhardt <mreinhar@puhti-login11.bullx>
2024-09-10 16:30:16 +0300

    MR: missing files

---
+++ src/astaroth/DSL/bcs/funcs_overload.h
@@ -0,0 +1,14 @@
+bc_sym_x(AC_TOP_BOT topbot,VtxBuffer field,real sgn)
+{
+  bc_sym_x(topbot,field,sgn,false)
+}
+
+bc_sym_y(AC_TOP_BOT topbot,VtxBuffer field,real sgn)
+{
+  bc_sym_y(topbot,field,sgn,false)
+}
+
+bc_sym_z(AC_TOP_BOT topbot,VtxBuffer field,real sgn)
+{
+  bc_sym_z(topbot,field,sgn,false)
+}
