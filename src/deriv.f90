4296e4c48cb13014fb667195d4bdebcca5aa001e
Philippe-A. Bourdin <git@Bourdin.ch>
2024-10-16 15:39:56 +0000

    Saving many more multiplications in the 5th derivative.

--- src/deriv.f90
+++ src/deriv.f90
@@ -889,26 +889,26 @@ module Deriv
       if (j==1) then
         if (nxgrid/=1) then
           if (igndx) then
-            fac=1.0
+            fac=(1.0/2)
           else
-            fac=1/dx**5
+            fac=(1.0/2)*1/dx**5
           endif
-          df=fac*(+  2.5*(f(l1+1:l2+1,m,n,k)-f(l1-1:l2-1,m,n,k)) &
-                  -  2.0*(f(l1+2:l2+2,m,n,k)-f(l1-2:l2-2,m,n,k)) &
-                  +  0.5*(f(l1+3:l2+3,m,n,k)-f(l1-3:l2-3,m,n,k)))
+          df=fac*(+  5.0*(f(l1+1:l2+1,m,n,k)-f(l1-1:l2-1,m,n,k)) &
+                  -  4.0*(f(l1+2:l2+2,m,n,k)-f(l1-2:l2-2,m,n,k)) &
+                  +      (f(l1+3:l2+3,m,n,k)-f(l1-3:l2-3,m,n,k)))
         else
           df=0.
         endif
       elseif (j==2) then
         if (nygrid/=1) then
           if (igndx) then
-            fac=1.0
+            fac=(1.0/2)
           else
-            fac=1/dy**5
+            fac=(1.0/2)*1/dy**5
           endif
-          df=fac*(+  2.5*(f(l1:l2,m+1,n,k)-f(l1:l2,m-1,n,k)) &
-                  -  2.0*(f(l1:l2,m+2,n,k)-f(l1:l2,m-2,n,k)) &
-                  +  0.5*(f(l1:l2,m+3,n,k)-f(l1:l2,m-3,n,k)))
+          df=fac*(+  5.0*(f(l1:l2,m+1,n,k)-f(l1:l2,m-1,n,k)) &
+                  -  4.0*(f(l1:l2,m+2,n,k)-f(l1:l2,m-2,n,k)) &
+                  +      (f(l1:l2,m+3,n,k)-f(l1:l2,m-3,n,k)))
           if (lcylindrical_coords) df=df*rcyl_mn1**5
 !MR: spherical missing
         else
@@ -917,13 +917,13 @@ module Deriv
       elseif (j==3) then
         if (nzgrid/=1) then
           if (igndx) then
-            fac=1.0
+            fac=(1.0/2)
           else
-            fac=1/dz**5
+            fac=(1.0/2)*1/dz**5
           endif
-          df=fac*(+  2.5*(f(l1:l2,m,n+1,k)-f(l1:l2,m,n-1,k)) &
-                  -  2.0*(f(l1:l2,m,n+2,k)-f(l1:l2,m,n-2,k)) &
-                  +  0.5*(f(l1:l2,m,n+3,k)-f(l1:l2,m,n-3,k)))
+          df=fac*(+  5.0*(f(l1:l2,m,n+1,k)-f(l1:l2,m,n-1,k)) &
+                  -  4.0*(f(l1:l2,m,n+2,k)-f(l1:l2,m,n-2,k)) &
+                  +      (f(l1:l2,m,n+3,k)-f(l1:l2,m,n-3,k)))
 !MR: spherical/coarse missing
         else
           df=0.
