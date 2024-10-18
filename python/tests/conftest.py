6b6e5282dafaf3fef48f175ada448195b0f6324d
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-10-18 20:40:21 +0530

    Python: conftest.py: set PYTHONPATH to get the standalone tests to pass
    If I don't do this, the standalone tests don't find the location of
    Pencil's Python modules (assuming the user has not sourced sourceme.sh).

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -2,3 +2,6 @@ import os
 import pathlib
 
 os.environ["PENCIL_HOME"] = str(pathlib.Path(__file__).parent/"../..")
+
+#The following seems dirty, but is needed for subprocesses to be initialized with the correct Python path
+os.environ["PYTHONPATH"] = str(pathlib.Path(__file__).parent/"..")
