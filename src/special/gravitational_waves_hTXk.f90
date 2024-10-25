eb049643d11b83bdf788f39fb1428aa5bd3bf297
Axel Brandenburg <brandenb@nordita.org>
2024-10-25 21:12:19 +0000

    added rotation

--- src/special/gravitational_waves_hTXk.f90
+++ src/special/gravitational_waves_hTXk.f90
@@ -119,6 +119,7 @@ module Special
   logical :: lno_noise_GW=.false., lfactors_GW=.false.,lcomp_GWs_k=.false.,lcomp_GWh_k=.false.
   logical :: lnot_amp_GW=.true.
   logical :: llogbranch_GW=.false., ldouble_GW=.false., lLighthill=.false.
+  logical :: lrandomize_e1_e2=.false.
   real, dimension(3,3) :: ij_table
   real :: c_light2=1., delk=0., tdelk=0., tau_delk=1.
   real :: tstress_ramp=0., stress_upscale_rate=0., stress_upscale_exp=0., tturnoff=1.
@@ -179,7 +180,7 @@ module Special
     initGW, reinitialize_GW, rescale_GW, &
     lggTX_as_aux, lhhTX_as_aux, lremove_mean_hij, lremove_mean_gij, &
     vx_boost, vy_boost, vz_boost, & 
-    lboost, boost_method, &
+    lboost, boost_method, lrandomize_e1_e2, &
     lstress, lstress_ramp, tstress_ramp, &
     lstress_upscale, stress_upscale_rate, stress_upscale_exp, &
     linflation, lreheating_GW, lmatter_GW, ldark_energy_GW, &
@@ -800,6 +801,8 @@ module Special
           call fatal_error("init_special: No such value for initGW:" &
               ,trim(initGW))
       endselect
+print*,'AXEL: f(nghost+2,nghost+2,nghost+2,ihhT  )=',f(nghost+2,nghost+2,nghost+2,ihhT  )
+print*,'AXEL: f(nghost+2,nghost+2,nghost+2,ihhX  )=',f(nghost+2,nghost+2,nghost+2,ihhX  )
 !
       if (lStress_as_aux) then
         f(:,:,:,iStressT)=0.
@@ -1313,6 +1316,8 @@ module Special
 !  16-oct-19/MR: carved out from special_calc_spectra
 !
       use Fourier, only: kx_fft, ky_fft, kz_fft
+      use General, only: random_number_wrapper
+      use Sub, only: cross, dot
 !
       real, dimension (mx,my,mz,mfarray) :: f
 !
@@ -1326,8 +1331,9 @@ module Special
       real :: k1_boost, k1sqr_boost, k2_boost,k2sqr_boost,k3_boost, k3sqr_boost
       real :: ksqr_boost, ksqrt_boost, one_over_k_boost
       real, dimension (3) :: e1_boost, e2_boost, vboost, kvec_boost, khat_boost
+      real, dimension (3) :: ee1_boost, ee2_boost
       real, dimension (6) :: e_T_boost, e_X_boost
-      real :: eTT, eTX, eXT, eXX
+      real :: eTT, eTX, eXT, eXX, phi, tmp, s, c, c1, kk1, kk2, kk3
       real :: gamma_boost, v_boostsqr, kdotv
       real :: SCL_re_boost, SCL_im_boost, VCT_re_boost, VCT_im_boost
       real :: hhT_boost, hhTim_boost, hhX_boost, hhXim_boost
@@ -1339,7 +1345,7 @@ module Special
       real :: DT_a, DT_b, DX_a, DX_b, khat_xhat_a, khat_xhat_b, DT_a_sum, DT_b_sum, DX_a_sum, DX_b_sum
       real :: khat_xhat_a_boost, khat_xhat_b_boost, DT_a_sum_boost, DT_b_sum_boost, DX_a_sum_boost, DX_b_sum_boost
       real, dimension (6) :: e_T, e_X
-      real, dimension (3) :: e1, e2
+      real, dimension (3) :: e1, e2, ee1, ee2
 !
       spectra%GWs=0.; spectra%GWshel=0.
       spectra%GWh=0.; spectra%GWhhel=0.
@@ -1439,6 +1445,35 @@ module Special
                   e2=(/k1*k3,k2*k3,-(k1sqr+k2sqr)/)
                 endif
               endif
+!
+!  possibility to randomize e1 and e2
+!
+              if (lrandomize_e1_e2) then
+                call random_number_wrapper(phi); phi = phi*2*pi
+                s=sin(phi)
+                c=cos(phi)
+                c1=1.-c
+                kk1=k1*one_over_k
+                kk2=k2*one_over_k
+                kk3=k3*one_over_k
+!
+                ee1(1)=(kk1**2*c1+c)*e1(1) + (kk1*kk2*c1-kk3*s)*e1(2) + (kk1*kk3*c1+kk2*s)*e1(3)
+                ee2(1)=(kk1**2*c1+c)*e2(1) + (kk1*kk2*c1-kk3*s)*e2(2) + (kk1*kk3*c1+kk2*s)*e2(3)
+!
+                ee1(2)=(kk1*kk2*c1+kk3*s)*e1(1) + (kk2**2*c1+c)*e1(2) + (kk2*kk3*c1-kk1*s)*e1(3)
+                ee2(2)=(kk1*kk2*c1+kk3*s)*e2(1) + (kk2**2*c1+c)*e2(2) + (kk2*kk3*c1-kk1*s)*e2(3)
+!
+                ee1(3)=(kk1*kk3*c1-kk2*s)*e1(3) + (kk2*kk3*c1+kk1*s)*e1(2) + (kk3**2*c1+c)*e1(3)
+                ee2(3)=(kk1*kk3*c1-kk2*s)*e2(3) + (kk2*kk3*c1+kk1*s)*e2(2) + (kk3**2*c1+c)*e2(3)
+!
+                e1=ee1
+                e2=ee2
+              endif
+!
+call dot (kvec,e1,tmp) ; print*,'AXEL1: kxe1=',tmp
+call dot (kvec,e2,tmp) ; print*,'AXEL1: kxe2=',tmp
+call dot (e1,e2,tmp) ; print*,'AXEL1: e1xe2=',tmp
+!
               e1=e1/sqrt(e1(1)**2+e1(2)**2+e1(3)**2)
               e2=e2/sqrt(e2(1)**2+e2(2)**2+e2(3)**2)
 !
@@ -1522,10 +1557,27 @@ module Special
                   endif
                 endif
 !
+!  possibility to randomize e1 and e2
+!
+              if (lrandomize_e1_e2) then
+                call random_number_wrapper(phi); phi = phi*2*pi
+                ee1_boost=+cos(phi)*e1_boost+sin(phi)*e2_boost
+                ee2_boost=-sin(phi)*e1_boost+cos(phi)*e2_boost
+                e1_boost=ee1_boost
+                e2_boost=ee2_boost
+              endif
+!
 !  normalize boosted e1 and e2 vectors
 !
                 e1_boost=e1_boost/sqrt(e1_boost(1)**2+e1_boost(2)**2+e1_boost(3)**2)
                 e2_boost=e2_boost/sqrt(e2_boost(1)**2+e2_boost(2)**2+e2_boost(3)**2)
+!
+!  check
+!
+call dot (kvec_boost,e1_boost,tmp) ; print*,'AXEL2: kxe1_boost=',tmp
+call dot (kvec_boost,e2_boost,tmp) ; print*,'AXEL2: kxe2_boost=',tmp
+call dot (e1_boost,e2_boost,tmp) ; print*,'AXEL2: e1xe2_boost=',tmp
+print*
 !
 !  compute e_T_boost and e_X_boost
 !
@@ -1572,6 +1624,7 @@ module Special
 !endif
                 enddo
                 enddo
+if (abs(k1) <3 .and. abs(k2) <3 .and. abs(k3) <3) print*,'AXEL: eXT=',k1,k2,k3,eXT
 !
 !  apply transformation from unboosted to boosted h and g, Eq.(64) from Emma's notes
 !  Do first h, but this could also be switcheable
