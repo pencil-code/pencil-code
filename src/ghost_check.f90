fca89d9fce713b640ffb862e991be8657a21f806
mrheinhardt <matthias.rheinhardt@aalto.fi>
2024-11-22 12:47:54 +0100

    MR: avoid units 100-101

--- src/ghost_check.f90
+++ src/ghost_check.f90
@@ -43,7 +43,7 @@ module Ghost_check
           do py = 1, num_y
             do px = 1, num_x
               if (a(px,py,pz,pa) /= b(px,py,pz,pa)) then
-                write (100+iproc,*) msg, ' => ', px, py, pz, pa, ' : ', a(px,py,pz,pa), b(px,py,pz,pa)
+                write (200+iproc,*) msg, ' => ', px, py, pz, pa, ' : ', a(px,py,pz,pa), b(px,py,pz,pa)
                 blocks_equal = .false.
               endif
             enddo
@@ -94,19 +94,19 @@ module Ghost_check
       ok = .true.
 !
       ! check in X:
-      write (100+iproc,*) 'Checking X-direction:'
+      write (200+iproc,*) 'Checking X-direction:'
       do py = 1, nprocy
         do pz = 1, nprocz
           if (nprocx == 1) then
             if (lperi(1)) then
-              write (100+iproc,*) 'Subdomain ', 1, py, pz
+              write (200+iproc,*) 'Subdomain ', 1, py, pz
               middle = global(:,(py-1)*my+1:py*my,(pz-1)*mz+1:pz*mz,:)
               ok = ok .and. blocks_equal ("X  l2i:l2 <>  1:l1-1", middle(l2i:l2,m1:m2,n1:n2,:), middle(1:l1-1,m1:m2,n1:n2,:))
               ok = ok .and. blocks_equal ("X l2+1:mx <> l1:l1i ", middle(l2+1:mx,m1:m2,n1:n2,:), middle(l1:l1i,m1:m2,n1:n2,:))
             endif
           else
             do px = 1, nprocx
-              write (100+iproc,*) 'Subdomain ', px, py, pz
+              write (200+iproc,*) 'Subdomain ', px, py, pz
               lx = px - 1
               if (lperi(1) .and. (lx < 1)) lx = lx + nprocx
               ux = px + 1
@@ -128,19 +128,19 @@ module Ghost_check
       enddo
 !
       ! check in Y:
-      write (100+iproc,*) 'Checking Y-direction:'
+      write (200+iproc,*) 'Checking Y-direction:'
       do px = 1, nprocx
         do pz = 1, nprocz
           if (nprocy == 1) then
             if (lperi(2)) then
-              write (100+iproc,*) 'Subdomain ', px, 1, pz
+              write (200+iproc,*) 'Subdomain ', px, 1, pz
               middle = global((px-1)*mx+1:px*mx,:,(pz-1)*mz+1:pz*mz,:)
               ok = ok .and. blocks_equal ("Y  m2i:m2 <>  1:m1-1", middle(l1:l2,m2i:m2,n1:n2,:), middle(l1:l2,1:m1-1,n1:n2,:))
               ok = ok .and. blocks_equal ("Y m2+1:my <> m1:m1i ", middle(l1:l2,m2+1:my,n1:n2,:), middle(l1:l2,m1:m1i,n1:n2,:))
             endif
           else
             do py = 1, nprocy
-              write (100+iproc,*) 'Subdomain ', px, py, pz
+              write (200+iproc,*) 'Subdomain ', px, py, pz
               ly = ipy - 1
               if (lperi(2) .and. (ly < 1)) ly = ly + nprocy
               uy = ipy + 1
@@ -162,19 +162,19 @@ module Ghost_check
       enddo
 !
       ! check in z:
-      write (100+iproc,*) 'Checking Z-direction:'
+      write (200+iproc,*) 'Checking Z-direction:'
       do px = 1, nprocx
         do py = 1, nprocy
           if (nprocz == 1) then
             if (lperi(3)) then
-              write (100+iproc,*) 'Subdomain ', px, py, 1
+              write (200+iproc,*) 'Subdomain ', px, py, 1
               middle = global((px-1)*mx+1:px*mx,(py-1)*my+1:py*my,:,:)
               ok = ok .and. blocks_equal ("Z  n2i:n2 <>  1:n1-1", middle(l1:l2,m1:m2,n2i:n2,:), middle(l1:l2,m1:m2,1:n1-1,:))
               ok = ok .and. blocks_equal ("Z n2+1:mz <> n1:n1i ", middle(l1:l2,m1:m2,n2+1:mz,:), middle(l1:l2,m1:m2,n1:n1i,:))
             endif
           else
             do pz = 1, nprocz
-              write (100+iproc,*) 'Subdomain ', px, py, pz
+              write (200+iproc,*) 'Subdomain ', px, py, pz
               lz = pz - 1
               if (lperi(3) .and. (lz < 1)) lz = lz + nprocz
               uz = pz + 1
