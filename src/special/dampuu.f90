37c44ad100c14d3d40cfb46eae6fa666b5c02dfc
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-11-27 15:27:24 +0530

    special/dampuu: fix mistake in damping profile
    Earlier damping profile was zero everywhere.
    
    Am now also applying a ceiling to the damping profile to avoid strange
    effects in the corners of the domain

--- src/special/dampuu.f90
+++ src/special/dampuu.f90
@@ -57,10 +57,11 @@ module Special
       if (y_2==impossible) y_2=xyz1(2)
       if (z_2==impossible) z_2=xyz1(3)
 !
-      tauinv_prof = spread(spread(step(x,x_1,-w)*step(x,x_2,w),2,my),3,mz) &
-                   *spread(spread(step(y,y_1,-w)*step(y,y_2,w),1,mx),3,mz) &
-                   *spread(spread(step(z,z_1,-w)*step(z,z_2,w),1,mx),2,my) &
-                   /tau
+      tauinv_prof = spread(spread(step(x,x_1,-w)+step(x,x_2,w),2,my),3,mz) &
+                   +spread(spread(step(y,y_1,-w)+step(y,y_2,w),1,mx),3,mz) &
+                   +spread(spread(step(z,z_1,-w)+step(z,z_2,w),1,mx),2,my)
+      where (tauinv_prof>1) tauinv_prof = 1 !avoid damping being too strong in the corners
+      tauinv_prof = tauinv_prof/tau
 !
       if (ldamp_rho) then
         if (ilnrho==0) call fatal_error('initialize_special', 'could not find density variable to be damped')
