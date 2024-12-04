727fc518086c07b7978020f1adce090cde93ae5f
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-12-04 16:10:44 +0530

    eos_idealgas_vapor: include eos_params.h

--- src/experimental/eos_idealgas_vapor.f90
+++ src/experimental/eos_idealgas_vapor.f90
@@ -30,12 +30,8 @@ module EquationOfState
   implicit none
 !
   include '../eos.h'
+  include '../eos_params.h'
 !
-  integer, parameter :: ilnrho_ss=1, ilnrho_ee=2, ilnrho_pp=3
-  integer, parameter :: ilnrho_lnTT=4, ilnrho_cs2=5
-  integer, parameter :: irho_cs2=6, irho_ss=7, irho_lnTT=8, ilnrho_TT=9
-  integer, parameter :: irho_TT=10, ipp_ss=11, ipp_cs2=12
-  integer, parameter :: irho_eth=13, ilnrho_eth=14
   integer :: iglobal_cs2, iglobal_glnTT
   real, dimension(nchemspec,18) :: species_constants
   real :: Rgas_unit_sys=1.0
