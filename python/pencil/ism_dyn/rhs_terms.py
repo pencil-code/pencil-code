c887ce666a64667a7b0f77f99705d94289e25d52
Kishore Gopalakrishnan <kishore96@gmail.com>
2025-02-07 21:39:12 +0530

    Python: sim_dyn/rhs_terms: use pc_print

--- python/pencil/ism_dyn/rhs_terms.py
+++ python/pencil/ism_dyn/rhs_terms.py
@@ -14,27 +14,14 @@
       compute "structure" functions as required
 """
 
-try:
-    from mpi4py import MPI
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-    l_mpi = True
-    l_mpi = l_mpi and (size != 1)
-except ImportError:
-    rank = 0
-    size = 1
-    comm = None
-    l_mpi = False
-
 import numpy as np
+from pencil.util import pc_print
 from pencil.math import dot, dot2, cross, cpu_optimal
 from pencil.math.derivatives import curl, div, curl2, grad, del2
 try:
     from pencil.calc import grav_profile
 except:
-    if rank == 0:
-        print("grav_profile not defined: setting to zero")
+    pc_print("grav_profile not defined: setting to zero")
     def grav_profile(grav, x, y, z):
         if "z" in grav:
             return np.zeros_like(z)
