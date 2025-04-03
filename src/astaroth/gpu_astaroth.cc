44b56f0fb2de9a22fd602237425a017713a9439f
ToxPuro <touko.puro@aalto.fi>
2025-04-03 19:36:47 +0300

    log in the interface after completing reloading

--- src/astaroth/gpu_astaroth.cc
+++ src/astaroth/gpu_astaroth.cc
@@ -1430,6 +1430,7 @@ reloadConfig()
   //TP: restore the vtxbuf values before quitting grid
   loadFarray();
 #endif
+  acLogFromRootProc(rank, "DONE initializeGPU\n");
 }
 /***********************************************************************************************/
 void loadFarray()
