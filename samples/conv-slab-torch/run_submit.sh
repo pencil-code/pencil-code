318fa4bcf1b83cb3fe5b5d7ec47c9b3cfac8cd3a
Touko Puro <toukopur@puhti-login15.bullx>
2025-04-14 13:18:18 +0300

    make scripts for running inside the container more robust to directory changees

--- samples/conv-slab-torch/run_submit.sh
+++ samples/conv-slab-torch/run_submit.sh
@@ -14,7 +14,7 @@
 
 #TP: for god knows for what reason a blank data folder gets generated on top of the correct data folder after running
 #TP: and even more weirdly if one deletes the blank folder the correct one reappears!
-apptainer exec --nv -B $PENCIL_HOME:$PENCIL_HOME -B scripts:/opt/scripts -B /scratch/project_2000403/toukopur/data:$PENCIL_HOME/samples/conv-slab-torch/data -B /scratch/project_2000403/toukopur/data:/data torchfort_bisonflex.sif bash rm -rf data
+apptainer exec --nv -B $PENCIL_HOME:$PENCIL_HOME -B scripts:/opt/scripts -B /scratch/project_2000403/toukopur/data:$PWD/data -B /scratch/project_2000403/toukopur/data:/data torchfort_bisonflex.sif bash rm -rf data
 
-apptainer exec --nv -B $PENCIL_HOME:$PENCIL_HOME -B scripts:/opt/scripts -B /scratch/project_2000403/toukopur/data:$PENCIL_HOME/samples/conv-slab-torch/data -B /scratch/project_2000403/toukopur/data:/data torchfort_bisonflex.sif bash /opt/scripts/run.sh
+apptainer exec --nv -B $PENCIL_HOME:$PENCIL_HOME -B scripts:/opt/scripts -B /scratch/project_2000403/toukopur/data:$PWD/data -B /scratch/project_2000403/toukopur/data:/data torchfort_bisonflex.sif bash /opt/scripts/run.sh
 
