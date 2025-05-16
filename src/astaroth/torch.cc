b4d00f9f5317643e9fa5433412fb02827cabece4
Matthias Rheinhardt <mreinhar@puhti-login12.bullx>
2025-05-17 01:45:22 +0300

    MR: precision as a parameter to *CAPI

--- src/astaroth/torch.cc
+++ src/astaroth/torch.cc
@@ -5,8 +5,9 @@
 #include <stdio.h>
 #include <mpi.h>
 
-extern "C" void torch_trainCAPI(int sub_dims[3], float* input, float* label, float* loss_val){
+void torch_trainCAPI(int sub_dims[3], float* input, float* label, float* loss_val, bool dble=false){
 
+        torchfort_datatype_t precision = dble ? TORCHFORT_DOUBLE : TORCHFORT_FLOAT;
 	torchfort_result_t result = torchfort_set_manual_seed(943442);
 
 //	std::ofstream myFile;
@@ -17,7 +18,7 @@ extern "C" void torch_trainCAPI(int sub_dims[3], float* input, float* label, flo
 	//printf("Calling c API");
 	int64_t input_shape[5] = {1, 3, sub_dims[2], sub_dims[1], sub_dims[0]};
 	int64_t label_shape[5] = {1, 6, sub_dims[2], sub_dims[1], sub_dims[0]};
-  	torchfort_result_t res = torchfort_train("stationary", input, 5, input_shape, label, 5, label_shape, loss_val, TORCHFORT_FLOAT, 0);
+  	torchfort_result_t res = torchfort_train("stationary", input, 5, input_shape, label, 5, label_shape, loss_val, precision, 0);
 
  	if (res != TORCHFORT_RESULT_SUCCESS)
  	{
@@ -25,13 +26,14 @@ extern "C" void torch_trainCAPI(int sub_dims[3], float* input, float* label, flo
  	}
 }
 
-extern "C" void torch_inferCAPI(int sub_dims[3], float* input, float* label){
+void torch_inferCAPI(int sub_dims[3], float* input, float* label, bool dble=false){
 
+        torchfort_datatype_t precision = dble ? TORCHFORT_DOUBLE : TORCHFORT_FLOAT;
 	torchfort_result_t result = torchfort_set_manual_seed(943442);
 
 	int64_t input_shape[5] = {1, 3, sub_dims[2], sub_dims[1], sub_dims[0]};
 	int64_t label_shape[5] = {1, 6, sub_dims[2], sub_dims[1], sub_dims[0]};
-	torchfort_result_t res = torchfort_inference("stationary", input, 5, input_shape, label, 5, label_shape, TORCHFORT_FLOAT, 0);
+	torchfort_result_t res = torchfort_inference("stationary", input, 5, input_shape, label, 5, label_shape, precision, 0);
 
 
  	if (res != TORCHFORT_RESULT_SUCCESS)
@@ -40,7 +42,7 @@ extern "C" void torch_inferCAPI(int sub_dims[3], float* input, float* label){
  	}
 }
 
-extern "C" void torch_createmodel(const char* name, const char* config_fname,MPI_Comm mpi_comm, int device){
+void torch_createmodel(const char* name, const char* config_fname,MPI_Comm mpi_comm, int device){
 	
 	torchfort_result_t result = torchfort_set_manual_seed(943442);
 	torchfort_result_t res = torchfort_create_distributed_model("cnn", "data/training/unet_torchscript.pt", MPI_COMM_WORLD, device);
