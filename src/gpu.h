d603488b98626223d843673f8197224483bb830f
mrheinhardt <matthias.rheinhardt@aalto.fi>
2025-05-18 18:16:38 +0200

    fix the get_ptr_gpu call in training_torchfort (needed to be 5-dimensional since the batch size is also one of the dimensions)

--- src/gpu.h
+++ src/gpu.h
@@ -1,5 +1,6 @@
   public :: gpu_init, register_GPU, initialize_GPU, finalize_GPU, get_farray_ptr_gpu, rhs_GPU, &
             copy_farray_from_GPU, &
-            load_farray_to_GPU, reload_GPU_config, update_on_gpu, get_ptr_GPU, get_ptr_GPU_for_training, &
-	    gpu_set_dt, test_rhs_gpu
+            test_rhs_gpu, load_farray_to_GPU, reload_GPU_config, update_on_gpu, get_ptr_GPU,get_ptr_GPU_for_training, &
+	    gpu_set_dt
+
   private
