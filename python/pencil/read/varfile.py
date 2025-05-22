Correct "pp" calculation

python/pencil/read/varfile.py
@@ -957,9 +957,7 @@ class DataCube(object):
                 cp = param.cp
                 gamma = param.gamma
                 cv = cp / gamma
-                cs20 = param.cs0 ** 2
                 lnrho0 = np.log(param.rho0)
-                rho0 = param.rho0
                 if hasattr(self, "lnrho"):
                     lnrho = self.lnrho
                 elif hasattr(self, "rho"):
@@ -967,12 +965,11 @@ class DataCube(object):
                 else:
                     raise AttributeError("Problem in magic: missing rho or lnrho variable")
                 if hasattr(self, "ss"):
-                    setattr(
-                        self,
-                        "pp",
-                            (rho0 * cs20 / gamma)
-                            * np.exp(gamma * (self.ss + lnrho - lnrho0))
-                    )
+                    cs20 = param.cs0 ** 2
+                    lnTT0 = np.log(cs20 / (cp * (gamma - 1.0)))
+                    setattr(self, "pp",
+                           (cp - cv) * np.exp(lnTT0 + gamma / cp * self.ss
+                                    + gamma * lnrho - (gamma - 1.0) * lnrho0))
                 elif hasattr(self, "lntt"):
                     setattr(self, "pp", (cp - cv) * np.exp(self.lnTT + lnrho))
                 elif hasattr(self, "tt"):