41c8b310cc488db9e148c79d24bc5208725dfa7c
Frederick Gent <fred.gent.ncl@gmail.com>
2025-06-03 14:00:40 +0300

    Disambiguate cool variable

--- src/astaroth/DSL/entropy/heatcool.h
+++ src/astaroth/DSL/entropy/heatcool.h
@@ -1,11 +1,11 @@
 //checked 18.6.
-cool=0.0
+interstellar_cool=0.0
 for i in 0:ncool-1
 {
     if (lncoolT[i] <= lnTT && lnTT < lncoolT[i+1]) {
-      cool = exp(lncoolH[i]+lnrho+lnTT*coolB[i])     // +=?
+      interstellar_cool = exp(lncoolH[i]+lnrho+lnTT*coolB[i])     // +=?
     }
 }
 heat = GammaUV*0.5*(1.0+tanh(cUV*(T0UV-TT)))
 	
-rhs += heat - cool   // division by TT deferred to heat_ss.h
+rhs += heat - interstellar_cool   // division by TT deferred to heat_ss.h
