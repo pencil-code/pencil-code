pencil-code/pencil-code

commit 761c70f940d12401b0bf6ce59ab735cb42e53835

MR: improved Python FortranFile; adaptations for divergence shock diff; minor tweaks

python/pencil/read/grids.py
@@ -266,7 +266,26 @@ class Grid(object):
         self.Lz = Lz
 
 @copy_docstring(Grid.read)
-def grid(*args, **kwargs):
-    grid_tmp = Grid()
-    grid_tmp.read(*args, **kwargs)
-    return grid_tmp
+    def grid(*args, **kwargs):
+        grid_tmp = Grid()
+        grid_tmp.read(*args, **kwargs)
+        return grid_tmp
+
+    def restrict(self,irange_x,irange_y,irange_z):
+        """
+        Restrict the grid arrays according to the index ranges irange_[xyz]
+        """
+        if irange_x != None:
+            self.x = self.x[irange_x[0]:irange_x[1]]
+            self.dx_1 = self.dx_1[irange_x[0]:irange_x[1]]
+            self.dx_tilde = self.dx_tilde[irange_x[0]:irange_x[1]]
+
+        if irange_y != None:
+            self.y = self.y[irange_y[0]:irange_y[1]]
+            self.dy_1 = self.dy_1[irange_y[0]:irange_y[1]]
+            self.dy_tilde = self.dy_tilde[irange_y[0]:irange_y[1]]
+
+        if irange_z != None:
+            self.z = self.z[irange_z[0]:irange_z[1]]
+            self.dz_1 = self.dz_1[irange_z[0]:irange_z[1]]
+            self.dz_tilde = self.dz_tilde[irange_z[0]:irange_z[1]]

bin/pc_auto-test
@@ -698,8 +698,6 @@ if ($nodelist) {
         test_rundir("$d", $makeopts);
     }
 }
-
-
 # ---------------------------------------------------------------------- #
 sub check_timeout_found {
     # Check if the timeout command exists on the system.
@@ -920,7 +918,6 @@ sub my_ok {
     if ($summarize_t) {
         record_time($phase, $dt);
     }
-
 }
 # ---------------------------------------------------------------------- #
 sub record_time {
@@ -949,9 +946,7 @@ sub find_test_dirs {
           },
           @rootdirs);
 
-
 #      $File::Find::prune = 1;
-
 }
 # ---------------------------------------------------------------------- #
 sub FF_wanted {
@@ -2118,7 +2113,6 @@ sub get_maintainers {
     return @emails;
 }
 # ---------------------------------------------------------------------- #
-
 ## Summarize results
 END {
 
@@ -2247,5 +2241,3 @@ END {
         exit 0;
     }
 }
-
-# End of file pc_auto-test

bin/pc_newrun
@@ -55,7 +55,7 @@ else
   fi
 fi
 #
-#  save current working directory and make new run directory and go there.
+#  Save current working directory and make new run directory and go there.
 #
 parentdir=`pwd`
 echo "Parent directory: " $parentdir
@@ -75,9 +75,8 @@ targetdir=`pwd`
 #  the new one is generated
 #
 $PENCIL_HOME/bin/pc_mkdatadir
-
 #
-#  write source directory name into file new_from.dir
+#  Write source directory name into file new_from.dir
 #
 [ -e data/new_from.dir ] || touch data/new_from.dir
 (echo $olddir; /bin/date) >> data/new_from.dir
@@ -106,7 +105,6 @@ $PENCIL_HOME/bin/pc_mkdatadir
 #  This gives the possibility to link your own routines
 #  into src (pc_setupsrc below won't overwrite those)
 #
-
   [ -f $parentdir/bin/pc_setupsrc.local ] && $parentdir/bin/pc_setupsrc.local
 #
 #  continue with the usual setup procedure
@@ -162,13 +160,12 @@ if [ -n "`ls *.py 2>/dev/null`" ]; then cp *.py $targetdir; fi
 [ -f src/.moduleinfo ] && cp src/.moduleinfo $targetdir/src
 [ -f src/.config-files ] && cp src/.config-files $targetdir/src
 [ -d src/astaroth/DSL/local ] && cp src/astaroth/DSL/local/* $targetdir/src/astaroth/DSL/local
-
 #
-#  if same_source, then link all *.x files from the original directory.
+#  if same_source, then link all *.x and *.so files from the original directory.
 #  (We don't want to use a link to the entire src directory, because
 #  then a run directory that is checked in would not possess a record
 #  of it's own src/*.local files.)
-#  Also: copy $olddir/src/*.inc, which are needed for remeshing.
+#  Also: copy $olddir/src/*.inc, which are needed for remeshing.  MR: really?
 #
 if [ $same_source ]; then
   cd $targetdir/src
@@ -189,11 +186,31 @@ if [ $same_source ]; then
     lnk=`readlink $olddir/src/special.so`
     if [ "$lnk" != "" ]; then ln -s $lnk .; fi
 
-    lnk=`readlink $olddir/src/astaroth/libastaroth_dbl.so`
-    if [ "$lnk" != "" ]; then ln -s $lnk astaroth/.; fi
+    if [ -e $olddir/src/astaroth ]; then
 
+      lnk=`readlink $olddir/src/astaroth/libastaroth_dbl.so`
+      if [ "$lnk" != "" ]; then ln -s $lnk astaroth/.; fi
+
+      lnk=`readlink $olddir/src/astaroth/libastaroth_sgl.so`
+      if [ "$lnk" != "" ]; then ln -s $lnk astaroth/.; fi
+
+      if [ -e $olddir/src/astaroth/submodule/build ]; then
+        mkdir -p astaroth/submodule/build/src/core/kernels
+        mkdir astaroth/submodule/build/runtime_compilation
+        mkdir -p astaroth/submodule/build/acc-runtime/acc
+        lnk=`readlink $olddir/src/astaroth/submodule/build/src/core/libastaroth_core.so`
+        if [ "$lnk" != "" ]; then ln -s  $lnk ./astaroth/submodule/build/src/core; fi 
+        lnk=`readlink $olddir/src/astaroth/submodule/build/src/core/kernels/libkernels.so`
+        if [ "$lnk" != "" ]; then ln -s  $lnk ./astaroth/submodule/build/src/core/kernels; fi
+        lnk=`readlink $olddir/src/astaroth/submodule/build/runtime_compilation/libacc_runtime_compiler.so`
+        if [ "$lnk" != "" ]; then ln -s  $lnk ./astaroth/submodule/build/runtime_compilation; fi
+        lnk=`readlink $olddir/src/astaroth/submodule/build/acc-runtime/acc/acc`
+        if [ "$lnk" != "" ]; then ln -s  $lnk ./astaroth/submodule/build/acc-runtime/acc; fi
+      fi
+    fi
   else
     cd $olddir;
+    echo Building minimal helper set ...
     if [ -e src/.config-files ]; then 
       pc_build -l -q -s -t helper_min >& /dev/null;
     else
@@ -205,11 +222,16 @@ if [ $same_source ]; then
     if [ -n "`ls $olddir/src/astaroth/*.so 2>/dev/null`" ]; then ln -s $olddir/src/astaroth/*.so ./astaroth ; fi
     if [ -n "`ls $olddir/src/astaroth/submodule/build/src/core/*.so 2>/dev/null`" ]; then
       if [ ! -n "`ls astaroth/submodule/build/src/core 2>/dev/null`" ]; then mkdir -p astaroth/submodule/build/src/core ; fi
-      ln -s $olddir/src/astaroth/submodule/build/src/core/*.so astaroth/submodule/build/src/core
+      cp -s $olddir/src/astaroth/submodule/build/src/core/*.so ./astaroth/submodule/build/src/core
+    fi
+    if [ -n "`ls $olddir/src/astaroth/submodule/build/runtime_compilation 2>/dev/null`" ]; then
+      if [ ! -n "`ls astaroth/submodule/build/runtime_compilation 2>/dev/null`" ]; then mkdir -p astaroth/submodule/build/runtime_compilation ; fi
+      cp -s $olddir/src/astaroth/submodule/build/runtime_compilation/libacc_runtime_compiler.so ./astaroth/submodule/build/runtime_compilation
     fi
     if [ -n "`ls $olddir/src/astaroth/submodule/build/src/core/kernels/*.so 2>/dev/null`" ]; then
       if [ ! -n "`ls astaroth/submodule/build/src/core/kernels 2>/dev/null`" ]; then mkdir -p astaroth/submodule/build/src/core/kernels ; fi
       ln -s $olddir/src/astaroth/submodule/build/src/core/kernels/*.so astaroth/submodule/build/src/core/kernels
+      cp -s $olddir/src/astaroth/submodule/build/src/core/kernels/*.so astaroth/submodule/build/src/core/kernels
     fi
   fi
   cp $olddir/src/*.inc .

bin/pc_setupsrc
@@ -400,24 +400,6 @@ link_initial_condition_files()
   fi
 }
 #
-make_cuda_dir()
-{
-## cuda subdirectory
-  [ -h $src/cuda ] && rm $src/cuda
-  adddir $src/cuda
-## cuda/src_new subdirectory
-  [ -h $src/cuda/src_new ] && rm $src/cuda/src_new
-  adddir $src/cuda/src_new
-## subdirectories of src_new
-  adddir $src/cuda/src_new/utils
-  adddir $src/cuda/src_new/common
-  adddir $src/cuda/src_new/gpu
-  adddir $src/cuda/src_new/diagnostics
-  adddir $src/cuda/src_new/gpu/cuda
-  adddir $src/cuda/src_new/gpu/cuda/core
-  adddir $src/cuda/src_new/gpu/cuda/generic
-}
-#
 make_astaroth_dir()
 {
 ## astaroth subdirectory
@@ -430,10 +412,8 @@ make_astaroth_dir()
   adddir $src/astaroth/submodule
 ## subdirectories of submodule
   adddir $src/astaroth/submodule/acc-runtime 
-  adddir $src/astaroth/submodule/acc-comm
   adddir $src/astaroth/submodule/acc-runtime/samples
   adddir $src/astaroth/submodule/acc-runtime/samples/mhd
-  adddir $src/astaroth/submodule/acc-runtime/samples/mhd_modular
   adddir $src/astaroth/submodule/acc-runtime/samples/gputest
 }
 #
@@ -455,17 +435,14 @@ link_astaroth_files()
   old_dir="`pwd`"
   cd $src/astaroth
   to_link=
-#  len=`expr match "$srcdir/cuda/astaroth/" '.*astaroth/'`
   for file in $srcdir/astaroth/*.cc $srcdir/astaroth/*.h $srcdir/astaroth/Makefile* $srcdir/astaroth/*.txt $srcdir/astaroth/*.sed $srcdir/astaroth/DSL/* \
               $srcdir/astaroth/submodule/CMakeLists.txt $srcdir/astaroth/submodule/include $srcdir/astaroth/submodule/stdlib \
-              $srcdir/astaroth/submodule/acc-comm \
               $srcdir/astaroth/submodule/acc-runtime/CMakeLists.txt \
               $srcdir/astaroth/submodule/acc-runtime/acc \
               $srcdir/astaroth/submodule/acc-runtime/api \
               $srcdir/astaroth/submodule/acc-runtime/built-in \
               $srcdir/astaroth/submodule/acc-runtime/samples/mhd/* \
               $srcdir/astaroth/submodule/acc-runtime/samples/gputest/* \
-              $srcdir/astaroth/submodule/acc-runtime/samples/mhd_modular/* \
               $srcdir/astaroth/submodule/src \
               $srcdir/astaroth/submodule/runtime_compilation
   do
@@ -480,54 +457,6 @@ link_astaroth_files()
     ln -s $file `echo $file | sed -r 's/^.*astaroth\///'`
   done
   
-  cd $old_dir
-}
-#
-link_cuda_files()
-{
-#
-#  LINK all *.* to local src/cuda directory
-#
-  echo "Linking files in '$src/cuda'."
-  old_dir="`pwd`"
-  cd $src/cuda
-  n=0
-  to_link=
-  for file in $srcdir/cuda/src/*.cc $srcdir/cuda/src/*.cuh $srcdir/cuda/src/*.cpp $srcdir/cuda/src/*.h $srcdir/cuda/src/makefile $srcdir/cuda/src/makefile.* $srcdir/cuda/src/*.conf 
-  do
-    if [ -f $file ] || [ -d $file ]; then
-      if [ -h "`basename $file`" ]; then
-        rm -f "`basename $file`"
-        to_link="$to_link $file"
-        #ln -s $file .
-      elif [ -f "`basename $file`" ] \
-        || [ -d "`basename $file`" ]; then
-        n="`expr $n + 1`"
-        existf="$existf`basename $file` "
-      else
-        to_link="$to_link $file"
-        #ln -s $file .
-      fi
-    else
-      echo "No such file: $file"
-    fi
-  done
-  [ "$to_link" ] && ln -s $to_link .
-
-  echo "Linking files in '$src/cuda/src_new'."
-  cd $src/cuda/src_new
-  to_link=
-  len=`expr match "$srcdir/cuda/src_new/" '.*src_new/'`
-  for file in $srcdir/cuda/src_new/*.cc $srcdir/cuda/src_new/*.txt $srcdir/cuda/src_new/Makefile* $srcdir/cuda/src_new/utils/*.h $srcdir/cuda/src_new/common/*.cc $srcdir/cuda/src_new/common/*.txt $srcdir/cuda/src_new/common/*.h $srcdir/cuda/src_new/gpu/*.cc $srcdir/cuda/src_new/gpu/*.txt $srcdir/cuda/src_new/gpu/*.h $srcdir/cuda/src_new/diagnostics/*.h $srcdir/cuda/src_new/diagnostics/*.cc $srcdir/cuda/src_new/gpu/cuda/*.cc* $srcdir/cuda/src_new/gpu/cuda/core/*.cc* $srcdir/cuda/src_new/gpu/cuda/generic/*.cu*
-  do
-    file_for_link $file
-  done
-  for file in $to_link
-  do 
-#    ln -s $file ${file:$len}
-    ln -s $file `echo $file | sed -r 's/^.*src_new\///'`
-  done
-
   cd $old_dir
 }
 #

bin/run.csh
@@ -201,6 +201,7 @@ else
   echo "$mpirun $mpirunops $npops $mpirunops2 $run_x $x_ops"
   echo "$mpirun $mpirunops $npops $mpirunops2 $run_x $x_ops" >> pc_commands.log
   time  $mpirun $mpirunops $npops $mpirunops2 $run_x $x_ops
+  #time $mpirun $mpirunops $npops $mpirunops2 valgrind --leak-check=full $run_x $x_ops
   #time $mpirun --bind-to core:overload-allowed $mpirunops $npops $mpirunops2 $run_x $x_ops
   #time $mpirun -x LD_LIBRARY_PATH -x PATH $mpirunops $npops $mpirunops2 $run_x $x_ops
 endif

python/pencil/read/fortran_file.py
@@ -1,0 +1,179 @@
+import scipy
+from scipy.io import FortranFile
+import numpy as np
+
+class FortranFileExt(FortranFile):
+
+    def read_record(self, *dtypes, **kwargs):
+        """
+        Reads a record of a given type from the file.
+
+        Parameters
+        ----------
+        *dtypes : dtypes, optional
+            Data type(s) specifying the size and endianness of the data.
+
+        Returns
+        -------
+        data : ndarray
+            A 1-D array object.
+
+        Raises
+        ------
+        FortranEOFError
+            To signal that no further records are available
+        FortranFormattingError
+            To signal that the end of the file was encountered
+            part-way through a record
+
+        Notes
+        -----
+        If the record contains a multidimensional array, you can specify
+        the size in the dtype. For example::
+
+            INTEGER var(5,4)
+
+        can be read with::
+
+            read_record('(4,5)i4').T
+
+        Note that this function does **not** assume the file data is in Fortran
+        column major order, so you need to (i) swap the order of dimensions
+        when reading and (ii) transpose the resulting array.
+
+        Alternatively, you can read the data as a 1-D array and handle the
+        ordering yourself. For example::
+
+            read_record('i4').reshape(5, 4, order='F')
+
+        For records that contain several variables or mixed types (as opposed
+        to single scalar or array types), give them as separate arguments::
+
+            double precision :: a
+            integer :: b
+            write(1) a, b
+
+            record = f.read_record('<f4', '<i4')
+            a = record[0]  # first number
+            b = record[1]  # second number
+
+        and if any of the variables are arrays, the shape can be specified as
+        the third item in the relevant dtype::
+
+            double precision :: a
+            integer :: b(3,4)
+            write(1) a, b
+
+            record = f.read_record('<f4', np.dtype(('<i4', (4, 3))))
+            a = record[0]
+            b = record[1].T
+
+        NumPy also supports a short syntax for this kind of type::
+
+            record = f.read_record('<f4', '(3,3)<i4')
+
+        See Also
+        --------
+        read_reals
+        read_ints
+
+        """
+        dtype = kwargs.pop('dtype', None)
+        if kwargs:
+            raise ValueError(f"Unknown keyword arguments {tuple(kwargs.keys())}")
+
+        if dtype is not None:
+            dtypes = dtypes + (dtype,)
+        elif not dtypes:
+            raise ValueError('Must specify at least one dtype')
+
+        dtypes = tuple(np.dtype(dtype) for dtype in dtypes)
+        block_size = sum(dtype.itemsize for dtype in dtypes)   # size of one block of data defined by dtypes
+
+        total_size = 0
+        rec_sizes = np.array([],dtype=int)
+        start_pos = self._fp.tell()
+        num_subrecords = 0
+
+        while True:
+            first_size = self._read_size(eof_ok=True)
+            size = first_size
+            if size < 0:
+                size = -size
+
+            rec_sizes=np.append(rec_sizes,size)
+            total_size += size
+            num_subrecords += 1
+
+            maxoffset = 2**31-1
+            num_reps, remainder = divmod(size, maxoffset)
+            for i in range(num_reps):
+                self._fp.seek(maxoffset,1)
+
+            self._fp.seek(remainder,1)
+            second_size = abs(self._read_size())
+            #print("FIRST-SECOND",first_size,second_size)
+
+            if size != second_size:
+                raise ValueError('Sizes do not agree in the header and footer for '
+                                 'this record - check header dtype')
+            if first_size > 0:
+                break
+
+        end_pos = self._fp.tell()
+
+        num_blocks, remainder = divmod(total_size, block_size)  # num_blocks: number of blocks in logical record
+        if remainder != 0:
+            raise ValueError(f'Size obtained ({total_size}) is not a multiple of the '
+                             f'dtypes given ({block_size}).')
+
+        if len(dtypes) != 1 and total_size != block_size:       # if more than one type and num_blocks>1  -  yet unclear, see Fortran array of structures
+            # Fortran does not write mixed type array items in interleaved order,
+            # and it's not possible to guess the sizes of the arrays that were written.
+            # The user must specify the exact sizes of each of the arrays.
+            raise ValueError(f'Size obtained ({total_size}) does not match with the '
+                             f'expected size ({block_size}) of multi-item record')
+        #print('num_subrecords,total_size,rec_sizes,start_pos=', num_subrecords,total_size,rec_sizes,start_pos)
+
+        self._fp.seek(start_pos)                       # reset file pointer to original record head
+        if num_subrecords > 1:                         # combine all subrecords in tmparray
+            tmparr = bytearray(total_size)
+            istart = 0
+            for i in range(num_subrecords):
+                first_size = self._read_size(eof_ok=True)
+                iend = istart+rec_sizes[i]
+                r = self._fp.read(rec_sizes[i])
+                if len(r) != rec_sizes[i]:
+                    raise FortranFormattingError(
+                        "End of file in the middle of a record")
+                tmparr[istart:iend] = r
+                second_size = self._read_size(eof_ok=True)
+                istart=iend
+
+        data = []
+        for dtype in dtypes:
+            if num_subrecords == 1:
+                first_size = self._read_size(eof_ok=True)
+                r = np.fromfile(self._fp, dtype=dtype, count=num_blocks)
+                if len(r) != num_blocks:
+                    raise FortranFormattingError(
+                        "End of file in the middle of a record")
+                if dtype.shape != ():
+                    # Squeeze outmost block dimension for array items
+                    if num_blocks == 1:
+                        assert r.shape == (1,) + dtype.shape
+                        r = r[0]
+
+                data.append(r)
+            else:
+                #print('DTYPE,NUM_BLOCKS=',dtype,num_blocks)
+                r = np.frombuffer(tmparr, dtype=dtype, count=num_blocks)
+                data.append(r)
+
+        self._fp.seek(end_pos)                       # reset file pointer behind original record end
+
+        # Unpack result
+        if len(dtypes) == 1:
+            return data[0]
+        else:
+            return tuple(data)

python/pencil/read/varfile.py
@@ -292,9 +292,12 @@ class DataCube(object):
         """
 
         import os
-        from scipy.io import FortranFile
+        #from scipy.io import FortranFile
+        from .fortran_file import FortranFileExt
+
         from pencil.math.derivatives import curl, curl2
         from pencil import read
+
         if precision=="h":
             precision = "half"
         if timing:
@@ -358,6 +361,8 @@ class DataCube(object):
             index = read.index(datadir=datadir)
             grid = read.grid(datadir=datadir, quiet=True) # we can't use sim.grid because we want the untrimmed one
 
+        if param.io_strategy[-1] == '/': param.io_strategy = param.io_strategy[:-1]    # because of Python 3.10.10 bug!
+
         if var_file[0:2].lower() == "og":
             dim = read.ogdim(datadir, proc)
         elif var_file[0:4] == "VARd":
@@ -383,15 +388,6 @@ class DataCube(object):
             import h5py
 
             run2D = param.lwrite_2d
-
-            # Set up the global array.
-            if not run2D:
-                self.f = np.zeros((total_vars, dim.mz, dim.my, dim.mx), dtype=precision)
-            else:
-                if dim.ny == 1:
-                    self.f = np.zeros((total_vars, dim.mz, dim.mx), dtype=precision)
-                else:
-                    self.f = np.zeros((total_vars, dim.my, dim.mx), dtype=precision)
 
             if not var_file:
                 if ivar < 0:
@@ -439,6 +435,9 @@ class DataCube(object):
                         irange_z = (max(irange_z[0],0),min(irange_z[1],tmp["settings/mz"][0]))
                 mz = irange_z[1]-irange_z[0]
                 z = (tmp["grid/z"][irange_z[0]:irange_z[1]]).astype(precision)
+
+                if grid != None:
+                    grid.restrict(irange_x,irange_y,irange_z)
 
                 # Set up the global array.
                 if run2D:
@@ -548,9 +547,9 @@ class DataCube(object):
                 myloc = procdim.my
                 mzloc = procdim.mz
 
-                # Read the data.
+                # Read the data: f-array
                 file_name = os.path.join(datadir, directory, var_file)
-                infile = FortranFile(file_name)
+                infile = FortranFileExt(file_name,header_dtype=np.int32)
                 if not run2D:
                     f_loc = (infile.read_record(dtype=read_precision)).astype(precision)
                     f_loc = f_loc.reshape((-1, mzloc, myloc, mxloc))
@@ -562,7 +561,11 @@ class DataCube(object):
                     else:
                         f_loc = (infile.read_record(dtype=read_precision)).astype(precision)
                         f_loc = f_loc.reshape((-1, myloc, mxloc))
+
+                # Read the data: time, coordinates, etc.
                 raw_etc = (infile.read_record(dtype=read_precision)).astype(precision)
+
+                # Read the data: persistent variables
                 if lpersist and directory==proc_dirs[0]:
                     persist(self, infile=infile, precision=read_precision, quiet=quiet)
                 infile.close()
@@ -643,11 +646,13 @@ class DataCube(object):
                             self.f[i0z:i1z, i0y:i1y, i0x:i1x] = f_loc[
                                 i0zloc:i1zloc, i0yloc:i1yloc, i0xloc:i1xloc
                             ]
-                else:
+                else:                    # reading from a single processor
                     self.f = f_loc
                     x = x_loc
                     y = y_loc
                     z = z_loc
+                    if grid != None:     # overwrite global grid by local grid to enable "magic" calculations
+                        grid = read.grid(datadir=datadir,proc=proc)
         else:
             raise NotImplementedError(
                 "IO strategy {} not supported by the Python module.".format(

python/pencil/visu/pv_volume_plotter.py
@@ -78,6 +78,10 @@ from tqdm import tqdm
 import pyvista as pv
 import pencil as pc
 import numpy as np
+import os
+
+os.environ['PYVISTA_USE_IPYVTK'] = 'true'
+os.environ['PYVISTA_USE_PANEL'] = 'true'
 
 # Python libraries
 from dataclasses import dataclass, asdict
@@ -438,10 +442,10 @@ class Plot3DSettings:
     mesh_cmap: str
         matplotlib compatible colormap for the mesh
     scalar_sbar_pos_x: float
-        The percentage (0 to 1) along the windows’s horizontal direction to place
+        The percentage (0 to 1) along the window's horizontal direction to place
         the bottom left corner of the colorbar. If None it is placed automatically.
     scalar_sbar_pos_y: float
-        The percentage (0 to 1) along the windows’s vertical direction to place
+        The percentage (0 to 1) along the window's vertical direction to place
         the bottom left corner of the colorbar. If None it is placed automatically.
 
     Scalarbar: Vectors | Streamlines
@@ -455,10 +459,10 @@ class Plot3DSettings:
     field_cmap: str
         Colormap for the plotter streamlines / vectors
     field_sbar_pos_x: float
-        The percentage (0 to 1) along the windows’s horizontal direction to place
+        The percentage (0 to 1) along the window's horizontal direction to place
         the bottom left corner of the colorbar. If None it is placed automatically.
     field_sbar_pos_y: float
-        The percentage (0 to 1) along the windows’s vertical direction to place
+        The percentage (0 to 1) along the window's vertical direction to place
         the bottom left corner of the colorbar. If None it is placed automatically.
 
     Orbit gif
@@ -572,6 +576,14 @@ class Plot3DSettings:
     ### Slices
     normal: str = "x"
     origin: tuple = None
+
+    ### Subdomain
+    range_x: tuple=None
+    range_y: tuple=None
+    range_z: tuple=None
+    irange_x: tuple=None
+    irange_y: tuple=None
+    irange_z: tuple=None
 
     def __post_init__(self):
         if self.stream_params == None:
@@ -761,12 +773,18 @@ class Pyvista3DPlot:
         scalar_key,
         datadir="./data",
         precision="f",
-        magic="bb",
+        magic="None",
         ivar=-1,
         coordinates="cartesian",
         outputdir=f"./stream_output",
         settings=Plot3DSettings(),
         debug=False,
+        irange_x=None,
+        irange_y=None,
+        irange_z=None,
+        range_x=None,
+        range_y=None,
+        range_z=None,
     ):
         """
         Initialization for the Pyvista3DPlot object.
@@ -834,7 +852,9 @@ class Pyvista3DPlot:
         self.vector_key = vector_key
 
         self.var = pc.read.var(
-            datadir=datadir, precision=precision, trimall=True, magic=magic, ivar=ivar
+            datadir=datadir, precision=precision, trimall=True, magic=magic, ivar=ivar,
+            irange_x=irange_x, irange_y=irange_y, irange_z=irange_z,
+            range_x=range_x, range_y=range_y, range_z=range_z
         )
         self.grid = pc.read.grid(datadir=datadir, trim=True)
 
@@ -1075,17 +1095,16 @@ class Pyvista3DPlot:
             del surfaces[i]
 
         lims = [self.mesh[scalars].min(), self.mesh[scalars].max()]
-        self.plotter = pv.Plotter(window_size=self.settings.window_size)
+        self.plotter = pv.Plotter(window_size=self.settings.window_size,off_screen=self.settings.off_screen)
         self.__plotterSettings(self.settings)
 
         for surf in surfaces:
             self.plotter.add_mesh(surf, cmap=cmap, clim=lims)
 
-        print(
-            '\n--> Pan around the camera to wanted angle, then press "q" to save image!\n'
-        )
+        if not self.settings.off_screen:
+            print('\n--> Pan around the camera to wanted angle, then press "q" to save image!\n')
         self.plotter.show(auto_close=False)
-        self.plotter.screenshot(filename=f"{filename}.{self.settings.imageformat}")
+        self.plotter.screenshot(filename=self.outputdir / f"{filename}.{self.settings.imageformat}")
 
     def scalars(self):
         """
@@ -1166,7 +1185,7 @@ class Pyvista3DPlot:
             del surfaces[i]
 
         surface = surfaces[0].copy()
-        self.plotter = pv.Plotter(window_size=self.settings.window_size)
+        self.plotter = pv.Plotter(window_size=self.settings.window_size,off_screen=self.settings.off_screen)
         self.__plotterSettings(self.settings)
         self.plotter.open_gif(str(self.outputdir / filename))
         self.plotter.enable_depth_peeling()
@@ -1192,9 +1211,8 @@ class Pyvista3DPlot:
             # self.plotter.add_mesh(self.mesh.outline_corners(), color='k')
             self.plotter.add_mesh(self.mesh.outline(), color="k")
 
-        print(
-            '\n--> Pan around the camera to wanted angle, then press "q" to produce the movie!\n'
-        )
+        if not self.settings.off_screen:
+            print('\n--> Pan around the camera to wanted angle, then press "q" to produce the movie!\n')
         self.plotter.show(auto_close=False)
         print(f"Starting to create the isovalue gif, this might take a moment!")
         with tqdm(total=2 * len(surfaces), desc="Moving isovalue rendering:") as pbar:
@@ -1224,8 +1242,8 @@ class Pyvista3DPlot:
             window_size=settings.window_size, title="Plot Preview"
         )
         scalar_bar_args = {
-            "width": settings.sbar_width,
-            "height": settings.sbar_height,
+            "width": settings.cbar_width,
+            "height": settings.cbar_height,
             "vertical": settings.vertical_sbar,
         }
         self.plotter.add_mesh(
@@ -1237,9 +1255,7 @@ class Pyvista3DPlot:
         )
         self.plotter.show_bounds(color="black", location="outer")
 
-        print(
-            "--> NOTE! In spreview only the mesh is added by default, not vectors | streamlines are shown!"
-        )
+        print("--> NOTE! In spreview only the mesh is added by default, vectors|streamlines are not shown!")
 
         plotPreview(self.plotter)
 

commit 44cf36e6fb043ee81bcf2b523f4e20096f9cac17

Python: fix wrong indentation
Was broken by commit 761c70f940d12401b0bf6ce59ab735cb42e53835 .

Matthias, please at least run the minimal set of Python tests before
pushing changes to the Python module
(python/tests/test-python-modules.py). It just takes 12 seconds on my
machine.

python/pencil/read/grids.py
@@ -265,12 +265,6 @@ class Grid(object):
         self.Ly = Ly
         self.Lz = Lz
 
-@copy_docstring(Grid.read)
-    def grid(*args, **kwargs):
-        grid_tmp = Grid()
-        grid_tmp.read(*args, **kwargs)
-        return grid_tmp
-
     def restrict(self,irange_x,irange_y,irange_z):
         """
         Restrict the grid arrays according to the index ranges irange_[xyz]
@@ -289,3 +283,10 @@ class Grid(object):
             self.z = self.z[irange_z[0]:irange_z[1]]
             self.dz_1 = self.dz_1[irange_z[0]:irange_z[1]]
             self.dz_tilde = self.dz_tilde[irange_z[0]:irange_z[1]]
+
+
+@copy_docstring(Grid.read)
+def grid(*args, **kwargs):
+    grid_tmp = Grid()
+    grid_tmp.read(*args, **kwargs)
+    return grid_tmp