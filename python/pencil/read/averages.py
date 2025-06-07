Python: Averages.__read_h5_aver: fix bug in reading 2D HDF5 averages
At least on my system,
```
list(tmp[str(itlist[0])].keys())[0]
```
returns `time`, which does not have the same shape as the actual data
arrays (e.g. ux2mxz).

This was broken in commit f44050e89375fb8b3c8e7d765cf09461d8c05d0f .
I think what I have written now should account for the issue Fred was
trying to fix in that commit.

python/pencil/read/averages.py
@@ -499,11 +499,18 @@ class Averages(object):
             else:
                 start_time = time.time()
 
-                testkey = list(tmp[str(itlist[0])].keys())[0]
-                data_shape = [len(itlist), *tmp[str(itlist[0])][testkey].shape]
-                t = np.zeros(data_shape[0], dtype=precision)
+                # testkey = list(tmp[str(itlist[0])].keys())[0] #Fred
+                # testkey = var_names[0] #Kishore
+                # data_shape = [len(itlist), *tmp[str(itlist[0])][testkey].shape]
+                # t = np.zeros(data_shape[0], dtype=precision)
+                # Kishore: Fred, you replaced var_names[0] by the above, but this breaks the reading of 2D averages (e.g. yaver). Can you please check if what I have now done below addresses your concern?
+                if len(var_names) > 0:
+                    data_shape = [len(itlist), *tmp[f"{itlist[0]}/{var_names[0]}"].shape]
                 for var in var_names:
                     setattr(ext_object, var, np.zeros(data_shape, dtype=precision))
+
+                t = np.zeros(len(itlist), dtype=precision)
+
                 for t_idx, tmp_idx in enumerate(itlist):
                     t[t_idx] = tmp[f"{tmp_idx}/time"][()]
                     for var in var_names: