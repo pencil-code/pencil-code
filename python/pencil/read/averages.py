56ddc4506bcb745f6033d5a80613ca94619598e0
Kishore Gopalakrishnan <kishore96@gmail.com>
2025-06-09 16:55:49 +0530

    Averages.read: note on axis ordering of 2D averages
    Why, oh why?

--- python/pencil/read/averages.py
+++ python/pencil/read/averages.py
@@ -129,6 +129,12 @@ class Averages(object):
         >>> aver = pc.read.aver()
         >>> avr.lnrho.shape
         (134, 134, 134)
+
+        Notes
+        -----
+        Due to unfortunate historical decisions, the axis ordering of the 2D
+        averages (yaver and zaver) depends on the IO module used. For yaver
+        with io_dist the ordering is [t,x,z], while with io_hdf5 it is [t,z,x].
         """
 
 
