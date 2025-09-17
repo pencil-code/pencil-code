65fd98285fab4bd934a7b67cda1baf7865f6a35f
purotouko <touko.puro@aalto.com>
2025-09-17 13:15:40 +0300

    Have to include headers for dynamic loading

--- src/astaroth/gpu_astaroth.cc
+++ src/astaroth/gpu_astaroth.cc
@@ -17,6 +17,10 @@
 #include <sys/resource.h>
 #include <fstream>
 
+#if AC_RUNTIME_COMPILATION
+#include <dlfcn.h>
+#endif
+
 //TP: defined here since mpi.h can have its own definition of DOUBLE_PRECISION
 //    and we don't want to conflict it with it. This is at least true on my laptop
 #if AC_DOUBLE_PRECISION
