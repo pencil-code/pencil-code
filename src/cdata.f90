b22658c3c6c096871c1f03270b685b876f601033
purotouko <touko.puro@aalto.com>
2025-09-17 18:20:20 +0300

    Also maxadvec has to be private

--- src/cdata.f90
+++ src/cdata.f90
@@ -887,7 +887,7 @@ module Cdata
 !$omp threadprivate(fname,fnamex,fnamey,fnamez,fnamer,fnamexy,fnamexz,fnamerz,fname_keep,fname_sound,ncountsz)
 !$omp threadprivate(l1dphiavg, l1davgfirst, l2davgfirst, ldiagnos,lout, l1davg, l2davg, lout_sound, lvideo)
 !$omp threadprivate(t,tspec,tdiagnos,t1ddiagnos,t2davgfirst,tslice,tsound,itdiagnos,dtdiagnos,eps_rkf_diagnos)
-!$omp threadprivate(maxdiffus)
+!$omp threadprivate(maxdiffus,maxadvec)
 !
 ! For use in offloaded code:
 !!$omp declare target(ldensity_nolog,l2,m2,n2)
