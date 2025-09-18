7b4238fdbf1744b69afbf38b762eb94ac6347985
purotouko <touko.puro@aalto.com>
2025-09-18 14:37:54 +0300

    make forcing compile with transpiled code

--- src/astaroth/DSL/forcing/getforce.h
+++ src/astaroth/DSL/forcing/getforce.h
@@ -1,7 +1,7 @@
 get_deltacorr_force(int step_num) {
   real3 force = real3(0.,0.,0.)
 #ifdef LFORCING
-  if ( !lforcing_cont ) {
+  if ( !AC_lforcing_cont__mod__cdata ) {
     if (step_num==2) {force = forcing()}
   }
 #endif
