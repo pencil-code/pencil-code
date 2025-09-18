918b41fdd371bb57049d9670ff653952f1ea6216
ToxPuro <touko.puro@aalto.fi>
2025-09-18 17:33:41 +0300

    Fix typo

--- src/astaroth/DSL/polymer.h
+++ src/astaroth/DSL/polymer.h
@@ -6,7 +6,7 @@ Kernel calc_poly_fr()
 			+ value(Field(AC_ip22__mod__polymer))
 			+ value(Field(AC_ip33__mod__polymer))
 	fenep_l2 = AC_fenep_l__mod__polymer*AC_fenep_l__mod__polymer
-	write(F_POLY_FR,(fenep_l2-3)/(fenep_l-rsqr))
+	write(F_POLY_FR,(fenep_l2-3)/(fenep_l2-rsqr))
 }
 #else
 Kernel calc_poly_fr(){}
