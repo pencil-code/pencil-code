d71793671bc6d8b73d5f2f05ec5599d803e02ab1
purotouko <touko.puro@aalto.com>
2025-09-18 18:12:27 +0300

    index-1 in DSL when coming from Fortran

--- src/astaroth/DSL/polymer.h
+++ src/astaroth/DSL/polymer.h
@@ -2,9 +2,9 @@
 Kernel calc_poly_fr()
 {
 	const real rsqr = 
-			  value(Field(AC_ip11__mod__polymer))
-			+ value(Field(AC_ip22__mod__polymer))
-			+ value(Field(AC_ip33__mod__polymer))
+			  value(Field(AC_ip11__mod__polymer-1))
+			+ value(Field(AC_ip22__mod__polymer-1))
+			+ value(Field(AC_ip33__mod__polymer-1))
 	fenep_l2 = AC_fenep_l__mod__polymer*AC_fenep_l__mod__polymer
 	write(F_POLY_FR,(fenep_l2-3)/(fenep_l2-rsqr))
 }
