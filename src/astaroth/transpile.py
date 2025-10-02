5c12e5f92f0f932a132db138709b2cc9530bfd03
purotouko <touko.puro@aalto.com>
2025-10-02 20:01:04 +0300

    need to set GENERATE_DSL_CODE=on only when want to explicitly to regenerate the rhs; rhs is regenerated automatically if the modulesources have changed

--- src/astaroth/transpile.py
+++ src/astaroth/transpile.py
@@ -7,6 +7,16 @@ def main():
     file = "equ.f90"
     PC_HOME = os.getenv("PENCIL_HOME")
     command = f"python3 {PC_HOME}/fortran-parser/parse.py -f rhs_cpu -F {path}/src/{file} -o -s --sample-dir {path} --dir {path}/src"
+    
+    sources_are_different = False
+    if not os.path.exists("PC_modulesources_old.h"):
+        sources_are_different = True
+    else:
+        sources_are_different = os.system("diff PC_modulesources.h PC_modulesources_old.h")
+    skip_generation = os.path.exists("DSL/local/mhdsolver.ac") and not sources_are_different
+    if skip_generation:
+        return
+
     os.system(command)
     os.system("mv mhdsolver-rhs.inc DSL/local/mhdsolver.ac")
     os.system("mv cparam.h DSL/local")
