82a8fc22e4c5e2a8df4555dac655a6000c32b81e
purotouko <touko.puro@aalto.com>
2025-10-02 14:10:25 +0300

    Make run script more generic

--- samples/conv-slab-torch/run_submit.sh
+++ samples/conv-slab-torch/run_submit.sh
@@ -13,24 +13,24 @@
 #SBATCH --output=train.out
 
 
-rm /scratch/project_2000403/sgiridha/data_conv-slab-torch/data/training/stationary.pt
+rm /scratch/project_2000403/$USER/data_conv-slab-torch/data/training/stationary.pt
 
 #TP: for god knows for what reason a blank data folder gets generated on top of the correct data folder after running
 #TP: and even more weirdly if one deletes the blank folder the correct one reappears!
-#apptainer exec --nv -B $PENCIL_HOME:$PENCIL_HOME -B scripts:/opt/scripts -B /scratch/project_2000403/sgiridha/data:$PWD/data -B /scratch/project_2000403/sgiridha/data_conv-slab-torch:/data torchfort_bisonflex.sif bash rm -rf data
+#apptainer exec --nv -B $PENCIL_HOME:$PENCIL_HOME -B scripts:/opt/scripts -B /scratch/project_2000403/$USER/data:$PWD/data -B /scratch/project_2000403/$USER/data_conv-slab-torch:/data torchfort_bisonflex.sif bash rm -rf data
 
 #bash clean.sh
 
 #apptainer exec --nv -B $PENCIL_HOME:$PENCIL_HOME -B scripts:/opt/scripts -B /scratch/project_2000403/toukopur/data:$PWD/data -B /scratch/project_2000403/toukopur/data:/data torchfort_bisonflex.sif bash
-#apptainer exec --nv -B $PENCIL_HOME:$PENCIL_HOME -B scripts:/opt/scripts -B /scratch/project_2000403/sgiridha/data:$PWD/data -B /scratch/project_2000403/sgiridha/data:/data torchfort_bisonflex.sif bash /opt/scripts/run.sh
+#apptainer exec --nv -B $PENCIL_HOME:$PENCIL_HOME -B scripts:/opt/scripts -B /scratch/project_2000403/$USER/data:$PWD/data -B /scratch/project_2000403/$USER/data:/data torchfort_bisonflex.sif bash /opt/scripts/run.sh
 
 
 
-apptainer exec --nv -B $PENCIL_HOME:/users/$USER/pencil-code -B scripts:/opt/scripts -B /scratch/project_2000403/sgiridha/data_conv-slab-torch/data:$PENCIL_HOME/samples/conv-slab-torch/data -B /scratch/project_2000403/sgiridha/data_conv-slab-torch/data:/data -B /scratch/project_2000403/sgiridha/snapshots:/$PENCIL_HOME/samples/conv-slab-torch/snapshots -B /scratch/project_2000403/sgiridha/snapshots:/snapshots torchfort_bisonflex.sif bash rm -rf data bash rm -rf snapshots
+apptainer exec --nv -B $PENCIL_HOME:/users/$USER/pencil-code -B scripts:/opt/scripts -B /scratch/project_2000403/$USER/data_conv-slab-torch/data:$PENCIL_HOME/samples/conv-slab-torch/data -B /scratch/project_2000403/$USER/data_conv-slab-torch/data:/data -B /scratch/project_2000403/$USER/snapshots:/$PENCIL_HOME/samples/conv-slab-torch/snapshots -B /scratch/project_2000403/$USER/snapshots:/snapshots torchfort_bisonflex.sif bash rm -rf data bash rm -rf snapshots
 
 
-apptainer exec --nv -B $PENCIL_HOME:/users/$USER/pencil-code -B scripts:/opt/scripts -B /scratch/project_2000403/sgiridha/data_conv-slab-torch/data:$PENCIL_HOME/samples/conv-slab-torch/data -B /scratch/project_2000403/sgiridha/data_conv-slab-torch/data:/data -B /scratch/project_2000403/sgiridha/snapshots:/$PENCIL_HOME/samples/conv-slab-torch/snapshots -B /scratch/project_2000403/sgiridha/snapshots:/snapshots torchfort_bisonflex.sif bash
+apptainer exec --nv -B $PENCIL_HOME:/users/$USER/pencil-code -B scripts:/opt/scripts -B /scratch/project_2000403/$USER/data_conv-slab-torch/data:$PENCIL_HOME/samples/conv-slab-torch/data -B /scratch/project_2000403/$USER/data_conv-slab-torch/data:/data -B /scratch/project_2000403/$USER/snapshots:/$PENCIL_HOME/samples/conv-slab-torch/snapshots -B /scratch/project_2000403/$USER/snapshots:/snapshots torchfort_bisonflex.sif bash
 
 
 
-apptainer exec --nv -B $PENCIL_HOME:/users/$USER/pencil-code -B /appl:/appl -B scripts:/opt/scripts -B /scratch/project_2000403/sgiridha/data_conv-slab-torch/data:$PENCIL_HOME/samples/conv-slab-torch/data -B /scratch/project_2000403/sgiridha/data_conv-slab-torch/data:/data -B /scratch/project_2000403/sgiridha/snapshots:/$PENCIL_HOME/samples/conv-slab-torch/snapshots -B /scratch/project_2000403/sgiridha/snapshots:/snapshots torchfort_bisonflex.sif bash /opt/scripts/run.sh
+apptainer exec --nv -B $PENCIL_HOME:/users/$USER/pencil-code -B /appl:/appl -B scripts:/opt/scripts -B /scratch/project_2000403/$USER/data_conv-slab-torch/data:$PENCIL_HOME/samples/conv-slab-torch/data -B /scratch/project_2000403/$USER/data_conv-slab-torch/data:/data -B /scratch/project_2000403/$USER/snapshots:/$PENCIL_HOME/samples/conv-slab-torch/snapshots -B /scratch/project_2000403/$USER/snapshots:/snapshots torchfort_bisonflex.sif bash /opt/scripts/run.sh
