db215f83b3f003f737209db7347af3fef154b6a0
purotouko <touko.puro@aalto.com>
2025-10-03 18:57:01 +0300

    calculate sound speed for sld with entropy on

--- src/run.f90
+++ src/run.f90
@@ -1506,7 +1506,8 @@ endsubroutine helper_loop
     call copy_addr(lcpu_timestep_on_gpu,p_par(1177)) ! bool
     call copy_addr(lac_sparse_autotuning,p_par(1178)) ! bool
     call copy_addr(lskip_rtime_compilation,p_par(1179)) ! bool
-    call copy_addr(lread_all_vars_from_device,p_par(1180)) ! bool
+    call copy_addr(lmultithread,p_par(1180)) ! bool
+    call copy_addr(lread_all_vars_from_device,p_par(1181)) ! bool
     
     call copy_addr_dble(sigma_thomson,p_par(1185))
     call copy_addr_dble(c_light,p_par(1188))
