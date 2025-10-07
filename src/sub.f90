6f49e6dd204d5be2d7bf0b8fbbbd450211af2e6d
purotouko <touko.puro@aalto.com>
2025-10-07 14:34:29 +0300

    Write out isnan since we do not always have it

--- src/sub.f90
+++ src/sub.f90
@@ -9335,7 +9335,8 @@ if (notanumber(f(ll,mm,2:mz-2,iff))) print*, 'DIFFZ:k,ll,mm=', k,ll,mm
       character (len=*), optional :: caller
       integer :: has_nan_local,has_nan_global
 
-      has_nan_local = merge(1,0,any(isnan(f)))
+      !isnan is written out since we cannot always depend on it
+      has_nan_local = merge(1,0,any(f > huge_real .or. f /= f))
       call mpireduce_max_int(has_nan_local,has_nan_global)
       if(has_nan_global == 1) then
               if(.not. present(caller)) then
