2826662e3811e03177a71762f3d0d1a67d7a95d2
purotouko <touko.puro@aalto.com>
2025-10-13 02:37:36 +0300

    set_ghosts_for_onesided_derivs on GPU

--- src/astaroth/DSL/bcs/funcs_full.h
+++ src/astaroth/DSL/bcs/funcs_full.h
@@ -4040,3 +4040,10 @@ bc_set_nfr_x(AcBoundary boundary, AC_TOP_BOT topbot,Field j)
   else {
   }
 } 
+
+set_ghosts_for_onesided_derivs(AcBoundary boundary,AC_TOP_BOT topbot, Field j, int idir)
+{
+	suppress_unused_warning(topbot)
+	suppress_unused_warning(idir)
+	ac_set_ghosts_for_onesided_derivs(boundary,j)
+}
