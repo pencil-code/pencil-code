a2156a5b627b850cff9ed900b8e0dd9a758e6a00
iomsn <iomsn1@googlemail.com>
2025-10-13 14:19:52 +0100

    Improved and correcte visualization in tutorial.

--- python/tutorials/visualize/plot_time_series.py
+++ python/tutorials/visualize/plot_time_series.py
@@ -1,6 +1,5 @@
 # Import all the necessary libraries.
 
-import numpy as np
 import matplotlib.pyplot as plt
 import pencil as pc
 
@@ -8,12 +7,16 @@ import pencil as pc
 ts = pc.read.ts()
 
 # Plot the kinetic energies: fluid and particle.
-plt.plot(ts.t, ts.ekin, linestyle='-', color='r', linewidth=2, label=r'$E_{\rm kin}$')
-plt.plot(ts.t, ts.ekinp, linestyle='--', color='green', linewidth=2, label=r'$E_{\rm kin}^{\rm p}$')
+plt.plot(ts.t, ts.uxmax, linestyle='-', color='r', linewidth=2,
+         label=r'$u_x^{\rm max}$')
+plt.plot(ts.t, ts.uymax, linestyle='', color='green', marker='x',
+         markersize=12, label=r'$u_y^{\rm max}$')
+plt.plot(ts.t, ts.uzmax, linestyle='--', color=(1, 0, 1), linewidth=2,
+         label=r'$u_z^{\rm max}$')
 
 # Add lables.
 plt.xlabel(r'$t$')
-plt.ylabel(r'$\rho$')
+plt.ylabel(r'$u^{\rm max}$')
 
 # Add a legend.
 plt.legend(loc=0, shadow=False, fancybox=False, numpoints=1)
@@ -23,4 +26,4 @@ frame.set_facecolor('1.0')
 leg.draw_frame(False)
 
 # Save the figure.
-plt.savefig('rho.pdf')
+plt.savefig('u_max.pdf')
