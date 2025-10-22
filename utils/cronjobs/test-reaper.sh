43614fc307c834dabd7c3e68395ab3e3c5f145cc
Philippe-A. Bourdin <git@Bourdin.ch>
2025-10-22 21:45:12 +0000

    Added some minimal documentation.

--- utils/cronjobs/test-reaper.sh
+++ utils/cronjobs/test-reaper.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Sample /etc/crontab entry:
+# 59 *		* * *   pencil	/home/pencil/run-test.sh
+
+# kill all processes that ran for 3 hours or more:
 killall -9 -q --older-than 3h -u pencil
