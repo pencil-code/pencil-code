fcb8ce53b7b16973bdb9990686c829acda42982f
AlbertoRoper <alberto.roper.pol@gmail.com>
2025-10-30 19:36:58 +0100

    updated compilation files and pc_mkspecial routine to rename special appearances in special module during compilation

--- utils/axel/pc_mkspecial.sh
+++ utils/axel/pc_mkspecial.sh
@@ -1,5 +1,9 @@
-sed -i -e '/module  *procedure/ ! s/^\( *module  *\)[a-zA-Z0-9_]* */\1Special/' \
-              -e 's/^\( *endmodule  *\)[a-zA-Z0-9_]* *$/\1Special/' \
-              -e "s/[a-zA-Z0-9_]*\.inc/special_dummies.inc/" \
-              -e "s/namelist *\/[a-zA-Z0-9_]*[a-zA-Z0-9]_\([ir][a-z]*_pars\)/namelist \/special_\1/" \
-              -e "s/NML *= *[a-zA-Z0-9_]*_\([a-z][a-z]*_pars\)/NML=special_\1/" $1
+for file in $1; do
+    sed -e '/module  *procedure/ ! s/^\( *module  *\)[a-zA-Z0-9_]* */\1Special/' \
+                -e 's/^\( *endmodule  *\)[a-zA-Z0-9_]* *$/\1Special/' \
+                -e "s/[a-zA-Z0-9_]*\.inc/special_dummies.inc/" \
+                -e "s/namelist *\/[a-zA-Z0-9_]*[a-zA-Z0-9]_\([ir][a-z]*_pars\)/namelist \/special_\1/" \
+                -e "s/NML *= *[a-zA-Z0-9_]*_\([a-z][a-z]*_pars\)/NML=special_\1/" $file > $file.tmp
+
+    mv -f $file.tmp $PENCIL_HOME/src/$file
+done
