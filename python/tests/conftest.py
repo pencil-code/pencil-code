83f2648287d4b891cdfc1a6eee0a922532fcfd6b
Kishore G. <kishore96@gmail.com>
2025-11-07 17:13:12 +0530

    Python: tests: add fixture datadir_helical_MHDTurb

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -79,3 +79,21 @@ def datadir_helical_MHDTurb_HDF5():
     sim.run(bashrc=False, cleardata=True, raise_errors=True)
 
     return sim.datadir
+
+@pytest.fixture(scope='session')
+def datadir_helical_MHDTurb():
+    rundir = get_rundir("samples/helical-MHDturb")
+    sim = pc.sim.get(rundir, quiet=True)
+
+    if sim is False:
+        raise RuntimeError(f"Could not get simulation in {rundir}")
+
+    sim.compile(
+        bashrc=False,
+        cleanall=False,
+        autoclean=True,
+        raise_errors=True,
+        )
+    sim.run(bashrc=False, cleardata=True, raise_errors=True)
+
+    return sim.datadir
