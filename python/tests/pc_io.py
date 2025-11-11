e7dbc82423355a5fe43470f2726eddf1edd1b2f3
Kishore G. <kishore96@gmail.com>
2025-11-11 15:03:09 +0530

    Python: tests: pc_io: add tests for write_h5_snapshot and write_snapshot

--- python/tests/pc_io.py
+++ python/tests/pc_io.py
@@ -6,7 +6,16 @@ Test functions in pc.io
 """
 
 import os
-from pencil.io import get_value_from_file
+import shutil
+import pytest
+import numpy as np
+
+from pencil.io import (
+    get_value_from_file,
+    write_h5_snapshot,
+    write_snapshot,
+    )
+from pencil import read
 from test_utils import make_test, assert_equal, get_rundir
 
 
@@ -28,3 +37,63 @@ def test_get_value_from_file() -> None:
     assert_equal(get_r("gravz"), -1)
     assert_equal(get_r("gravz_profile"), "const")
     assert_equal(get_r("lupw_lnrho"), True)
+
+@pytest.mark.integration
+def test_write_snapshot(datadir_helical_MHDTurb, temp_datadir):
+    src_datadir = datadir_helical_MHDTurb
+    dest_datadir = shutil.copytree(src_datadir, temp_datadir/"data")
+
+    for f in dest_datadir.glob("proc*/var.dat"):
+        f.unlink()
+
+    src_var = read.var(datadir=src_datadir, trimall=True)
+    src_dim = read.dim(datadir=src_datadir)
+
+    if src_dim.precision == "D":
+        precision = "d"
+    else:
+        precision = "f"
+
+    dest_dim = read.dim(datadir=dest_datadir)
+    write_snapshot(
+        src_var.f,
+        file_name = "var.dat",
+        datadir = dest_datadir,
+        nprocx = dest_dim.nprocx,
+        nprocy = dest_dim.nprocy,
+        nprocz = dest_dim.nprocz,
+        precision = precision,
+        nghost=3,
+        )
+
+    dest_var = read.var(datadir=dest_datadir, trimall=True)
+    assert np.all(src_var.f == dest_var.f)
+
+@pytest.mark.integration
+def test_write_h5_snapshot(datadir_helical_MHDTurb_HDF5, temp_datadir):
+    src_datadir = datadir_helical_MHDTurb_HDF5
+    dest_datadir = shutil.copytree(src_datadir, temp_datadir/"data")
+
+    dest_varfile = dest_datadir/"allprocs/var.h5"
+    dest_varfile.unlink()
+
+    src_var = read.var(datadir=src_datadir, trimall=True)
+    src_dim = read.dim(datadir=src_datadir)
+
+    if src_dim.precision == "D":
+        precision = "d"
+    else:
+        precision = "f"
+
+    write_h5_snapshot(
+        src_var.f,
+        file_name = "var.h5",
+        precision = precision,
+        nghost=3,
+        lghosts=False,
+        sim_datadir=dest_datadir,
+        datadir = dest_datadir/"allprocs",
+        )
+
+    dest_var = read.var(datadir=dest_datadir, trimall=True)
+    assert np.all(src_var.f == dest_var.f)
