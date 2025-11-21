971b11739458055f95c6576738e93baeb112cd32
Touko Puro <tpuro@norlx51.nordita.org>
2025-11-21 15:43:16 +0100

    add update_fortran.py that is supposed to be automatically update Fortran sources to be compatible with the GPU whenever the rhs's change

--- src/astaroth/transpile.py
+++ src/astaroth/transpile.py
@@ -1,12 +1,21 @@
+import argparse
 import os
 from pathlib import Path
 def main():
+    argparser = argparse.ArgumentParser(description="Wrapper",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    argparser.add_argument("--modify_source_code",default=False,action="store_true", help="Whether to modify the fortran source code to include the pushpars etc. automatically")
     current = Path.cwd()
+
+    args = argparser.parse_args()
+    config = vars(args)
+    modify_source_code = config["modify_source_code"]
     sample_dir  = current.parents[1]
     path = str(sample_dir)
     file = "equ.f90"
     PC_HOME = os.getenv("PENCIL_HOME")
     command = f"python3 {PC_HOME}/fortran-parser/parse.py -f rhs_cpu -F {path}/src/{file} -o -s --sample-dir {path} --dir {path}/src"
+    if(modify_source_code): command += " --modify_source_code"
     
     sources_are_different = False
     if not os.path.exists("PC_modulesources_old.h"):
---
+++ src/astaroth/update_fortran.py
@@ -0,0 +1,18 @@
+import os
+import argparse
+def main():
+    argparser = argparse.ArgumentParser(description="Update *.f90 files to be GPU ready",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    argparser.add_argument("-p", "--pencil-home", help="Where PC is installed", required=True)
+    args = argparser.parse_args()
+    config = vars(args)
+    PC_HOME = config["pencil_home"]
+    os.environ["PENCIL_USER"] = "GPU_AUTOTEST"
+    source_command = f"cd {PC_HOME} && . ./sourceme.sh"
+    submodule_command = "git submodule update --init --remote"
+    build_command = f"cd {PC_HOME}/samples/1d-tests/sine-Gordon_doublet && pc_build -f GNU-GCC_MPI+GNU-GCC_GPU+GNU-GCC_debug MODIFY_SOURCE_CODE=on"
+    command = f"{source_command} && {submodule_command} && {build_command}"
+    os.system(command)
+
+if __name__ == "__main__":
+    main()
