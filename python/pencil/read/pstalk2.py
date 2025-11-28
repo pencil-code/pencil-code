7693f90a3e1903dcc63ad5caa53faf3a5c73e273
Wladimir Lyra <wladimir.lyra@gmail.com>
2025-11-28 14:50:14 +0100

    fix use of single and nstalk

--- python/pencil/read/pstalk2.py
+++ python/pencil/read/pstalk2.py
@@ -1,16 +1,14 @@
-
+A
 # pstalk2.py
 #
 # Read the stalker files.
-# Returns fs.ipar, fs.xp, etc.
+# Returns fs.ipar, fs.xp, etc. with dot-access.
 #
-# python translation of Anders' pstalker IDL script.
-# Called it "2" because there is another pstalk, which 
-# uses the IDL-python bridge (although that one does not 
-# seem to work).
+# Python translation of Anders Johansen's pc_read_pstalk IDL script,
+# adapted to use the Pencil Python I/O.
 #
 # W. Lyra (wlyra@nmsu.edu)
-# 27 nov 2025: 
+# 27 Nov 2025
 #
 import os
 import glob
@@ -19,6 +17,7 @@ import numpy as np
 import h5py
 
 from pencil import read
+
 
 class Struct(dict):
     """Simple dict-like container with attribute (dot) access."""
@@ -30,6 +29,7 @@ class Struct(dict):
 
     def __setattr__(self, name, value):
         self[name] = value
+
 
 def pstalk2(
     datadir="data",
@@ -45,9 +45,9 @@ def pstalk2(
     Read Pencil Code particle stalker data (PSTALK) and return a Struct with
     dot-access fields:
 
-        obj.t      : (nout,)
-        obj.ipar   : (nstalk,)
-        obj.<field>: (nstalk, nout) for each field in particles_stalker_header.dat
+        fs.t      : (nout,)
+        fs.ipar   : (nstalk,)
+        fs.<field>: (nstalk, nout) for each field in particles_stalker_header.dat
 
     Parameters
     ----------
@@ -62,15 +62,19 @@ def pstalk2(
     quiet : bool
         If True, suppress most logging.
     noutmax : int or None
-        Maximum number of times to read. None => use all available.
+        Maximum number of output times to read. None => use all available.
+        Semantics match IDL: min(noutmax, noutmaxfile), with -1 meaning "all".
     single : bool
-        If True, store data in float32; otherwise float64.
+        If True, store output data in float32; otherwise float64.
+        (Binary precision is auto-detected and then cast.)
     nstalk : int or None
-        Max number of stalked particles. Defaults to pdim.npar_stalk.
+        Max number of stalked particles to keep. Defaults to pdim.npar_stalk.
+        In non-sink mode, particles with index >= nstalk are ignored.
+        In sink-particle mode, nstalk may be increased internally if needed.
 
     Returns
     -------
-    obj : Struct
+    fs : Struct
         Container with fields t, ipar, and one field per stalk quantity.
     """
     datadir = os.path.expanduser(datadir)
@@ -99,9 +103,9 @@ def pstalk2(
             )
 
         num_files = len(h5_files)
-        dtype = np.float32 if single else np.float64
+        out_dtype = np.float32 if single else np.float64
 
-        stalk_obj = Struct()
+        fs = Struct()
         quantities = None
 
         for pos, fname in enumerate(h5_files):
@@ -115,26 +119,25 @@ def pstalk2(
                     stalk_group = f["stalker"]
                     quantities = list(stalk_group.keys())
 
-                    stalk_obj.t = np.full(num_files, np.nan, dtype=dtype)
-                    stalk_obj.ipar = np.array(stalk_group["ID"], dtype=np.int64)
+                    fs.t = np.full(num_files, np.nan, dtype=out_dtype)
+                    fs.ipar = np.array(stalk_group["ID"], dtype=np.int64)
 
                     for q in quantities:
                         if q.upper() == "ID":
                             continue
-                        stalk_obj[q] = np.full(
-                            (num_part, num_files), np.nan, dtype=dtype
+                        fs[q] = np.full(
+                            (num_part, num_files), np.nan, dtype=out_dtype
                         )
 
                 for q in quantities:
                     if q.upper() == "ID":
                         continue
-                    stalk_obj[q][:, pos] = np.array(
-                        f["stalker"][q], dtype=dtype, copy=False
-                    )
+                    data = np.array(f["stalker"][q])
+                    fs[q][:, pos] = data.astype(out_dtype, copy=False)
 
-                stalk_obj.t[pos] = t_val
+                fs.t[pos] = out_dtype(t_val)
 
-        return stalk_obj
+        return fs
 
     # ------------------------------------------------------------------
     # Legacy binary PSTALK (particles_stalker.dat)
@@ -146,10 +149,10 @@ def pstalk2(
         )
         return Struct()
 
-    # Do we stalk sink particles?
-    lstalk_sink_particles = int(
-        getattr(param, "lstalk_sink_particles", 0)
-    )
+    # In the current IDL version, sink-particle stalking is effectively disabled:
+    # lstalk_sink_particles = 0 ; param.lstalk_sink_particles
+    # To match that behavior, we set it to 0 here as well.
+    lstalk_sink_particles = 0
 
     # ------------------------------------------------------------------
     # Read tstalk.dat -> (tout, noutmaxfile)
@@ -181,12 +184,12 @@ def pstalk2(
         print("at", nout, "times")
 
     # ------------------------------------------------------------------
-    # Allocate arrays
+    # Allocate arrays (output dtype controlled by `single`)
     # ------------------------------------------------------------------
-    dtype = np.float32 if single else np.float64
+    out_dtype = np.float32 if single else np.float64
 
-    t = np.zeros(nout, dtype=dtype)                         # (nout,)
-    array = np.zeros((nfields, nstalk, nout), dtype=dtype)  # (nfields, nstalk, nout)
+    t = np.zeros(nout, dtype=out_dtype)                         # (nout,)
+    array = np.zeros((nfields, nstalk, nout), dtype=out_dtype)  # (nfields, nstalk, nout)
 
     if lstalk_sink_particles:
         ipar_stalk = np.full((nstalk, nout), -1, dtype=np.int64)
@@ -195,22 +198,24 @@ def pstalk2(
         ipar_stalk = np.arange(nstalk, dtype=np.int64)
 
     # ------------------------------------------------------------------
-    # Fortran unformatted record helpers
+    # Fortran unformatted record helpers (auto-detect float precision)
     # ------------------------------------------------------------------
     endian = ">" if swap_endian else "<"
-    float_fmt = "f" if single else "d"
-    float_size = 4 if single else 8
 
     def read_record(fh):
-        """Read one Fortran unformatted sequential record and return payload bytes."""
+        """Read one Fortran unformatted sequential record and return (payload, nbytes)."""
         header_bytes = fh.read(4)
         if not header_bytes:
-            return None
+            return None, 0
         (nbytes,) = struct.unpack(endian + "i", header_bytes)
         payload = fh.read(nbytes)
-        # trailing record length
-        fh.read(4)
-        return payload
+        fh.read(4)  # trailing record length
+        return payload, nbytes
+
+    # These will be auto-detected from the first rec1 we see:
+    read_float_fmt = None      # 'f' or 'd'
+    read_float_size = None     # 4 or 8
+    binary_float_dtype = None  # np.float32 or np.float64
 
     # ------------------------------------------------------------------
     # Loop over processors and read particles_stalker.dat
@@ -234,23 +239,41 @@ def pstalk2(
 
         with open(fname, "rb") as fh:
             while ntread < nout:
-                rec1 = read_record(fh)
+                rec1, nbytes1 = read_record(fh)
                 if rec1 is None:
                     break  # EOF
 
+                # Auto-detect float precision from record length rec1:
+                # rec1 contains: t_loc (float or double) + npar_stalk_loc (int32)
+                if binary_float_dtype is None:
+                    if nbytes1 == 8:      # 4 (float32) + 4 (int32)
+                        read_float_fmt = "f"
+                        read_float_size = 4
+                        binary_float_dtype = np.float32
+                    elif nbytes1 == 12:   # 8 (float64) + 4 (int32)
+                        read_float_fmt = "d"
+                        read_float_size = 8
+                        binary_float_dtype = np.float64
+                    else:
+                        raise RuntimeError(
+                            f"pstalk2: cannot infer float size from record length {nbytes1}"
+                        )
+
                 # Record 1: t_loc (float) + npar_stalk_loc (int32)
-                t_loc = struct.unpack(endian + float_fmt, rec1[:float_size])[0]
+                t_loc_raw = struct.unpack(
+                    endian + read_float_fmt, rec1[:read_float_size]
+                )[0]
                 npar_stalk_loc = struct.unpack(
-                    endian + "i", rec1[float_size : float_size + 4]
+                    endian + "i", rec1[read_float_size : read_float_size + 4]
                 )[0]
 
                 ipar_loc = None
                 array_loc = None
 
-                # If npar_stalk_loc > 0, we must read records 2 and 3
+                # If npar_stalk_loc > 0, read records 2 and 3
                 if npar_stalk_loc > 0:
                     # Record 2: ipar_loc (npar_stalk_loc ints)
-                    rec2 = read_record(fh)
+                    rec2, _ = read_record(fh)
                     if rec2 is None:
                         break
                     ipar_loc = np.frombuffer(
@@ -258,31 +281,42 @@ def pstalk2(
                     ).astype(np.int64)
 
                     # Record 3: array_loc (nfields * npar_stalk_loc floats)
-                    rec3 = read_record(fh)
+                    rec3, _ = read_record(fh)
                     if rec3 is None:
                         break
-                    array_loc = np.frombuffer(
+                    array_loc_native = np.frombuffer(
                         rec3,
-                        dtype=dtype,
+                        dtype=binary_float_dtype,
                         count=nfields * npar_stalk_loc,
                     ).reshape((nfields, npar_stalk_loc), order="F")
 
+                    # Cast to output dtype if needed
+                    if array_loc_native.dtype != out_dtype:
+                        array_loc = array_loc_native.astype(out_dtype)
+                    else:
+                        array_loc = array_loc_native
+
                 # Store if we are at/after it0
                 if it >= it0:
+                    out_it = it - it0
+                    if out_it >= nout:
+                        # we already filled requested outputs for this proc
+                        break
+
                     if (it1 != -1) and (it % it1 == 0) and (not quiet):
-                        print(iproc, it, t_loc, npar_stalk_loc)
+                        print(iproc, it, t_loc_raw, npar_stalk_loc)
 
                     if npar_stalk_loc > 0:
                         if not lstalk_sink_particles:
                             idx = ipar_loc - 1  # 1-based -> 0-based
 
-                            # clamp to [0, nstalk)
+                            # Clamp to [0, nstalk) so user can restrict number of particles
                             mask = (idx >= 0) & (idx < nstalk)
                             if np.any(mask):
-                                array[:, idx[mask], it - it0] = array_loc[:, mask]
-                                # particles with index >= nstalk are ignored
+                                array[:, idx[mask], out_it] = array_loc[:, mask]
                         else:
-                            start = npar_stalk_read[it - it0]
+                            # Sink-particle mode (currently not used in IDL, but logic kept)
+                            start = npar_stalk_read[out_it]
                             stop = start + npar_stalk_loc
 
                             if stop > nstalk:
@@ -291,7 +325,7 @@ def pstalk2(
                                 ipar2 = ipar_stalk
 
                                 array = np.zeros(
-                                    (nfields, nstalk2, nout), dtype=dtype
+                                    (nfields, nstalk2, nout), dtype=out_dtype
                                 )
                                 ipar_stalk = np.full(
                                     (nstalk2, nout), -1, dtype=np.int64
@@ -301,17 +335,18 @@ def pstalk2(
                                 ipar_stalk[:nstalk, :] = ipar2
                                 nstalk = nstalk2
 
-                            array[:, start:stop, it - it0] = array_loc
-                            ipar_stalk[start:stop, it - it0] = ipar_loc
-                            npar_stalk_read[it - it0] += npar_stalk_loc
+                            array[:, start:stop, out_it] = array_loc
+                            ipar_stalk[start:stop, out_it] = ipar_loc
+                            npar_stalk_read[out_it] += npar_stalk_loc
 
-                    t[it - it0] = t_loc
+                    # Cast t_loc to output dtype when storing
+                    t[out_it] = out_dtype(t_loc_raw)
                     ntread += 1
 
                 it += 1
 
     # ------------------------------------------------------------------
-    # Sink-particle reordering / trimming
+    # Sink-particle reordering / trimming (not active if lstalk_sink_particles==0)
     # ------------------------------------------------------------------
     if lstalk_sink_particles:
         array2 = array
@@ -329,7 +364,7 @@ def pstalk2(
 
             if ipar_unique.size > nstalk:
                 array = np.zeros(
-                    (nfields, ipar_unique.size, nout), dtype=dtype
+                    (nfields, ipar_unique.size, nout), dtype=out_dtype
                 )
 
             for k, ip in enumerate(ipar_unique):
@@ -352,12 +387,12 @@ def pstalk2(
     # ------------------------------------------------------------------
     # Build result Struct with dot-access
     # ------------------------------------------------------------------
-    obj = Struct()
-    obj.t = t
-    obj.ipar = ipar_stalk
+    fs = Struct()
+    fs.t = t
+    fs.ipar = ipar_stalk
 
     # reshape each field -> (nstalk, nout)
     for i, name in enumerate(fields):
-        obj[name] = array[i, :, :]
+        fs[name] = array[i, :, :]
 
-    return obj
+    return fs
