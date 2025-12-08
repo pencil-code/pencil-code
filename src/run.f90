86b0f1d52591b73455b6c1e78fdfc65368544997
Kishore G. <kishore96@gmail.com>
2025-12-08 11:43:19 +0530

    run.f90: code style and comment

--- src/run.f90
+++ src/run.f90
@@ -1208,31 +1208,35 @@ endsubroutine helper_loop
                                wall_clock_time/icount/nw/1.0e-6
         write(*,'(A,1pG14.7)') ' Rhs wall clock time/timestep/local meshpoint [microsec] =', &
                                rhs_sum_time/icount/nw/1.0e-6
-        if (lverbose_performance_log) write(*,'(A,1pG14.7)') &
-          ' After substep wall clock time/timestep/local meshpoint [microsec] =', &
-                               after_substep_sum_time/icount/nw/1.0e-6
-        if (lverbose_performance_log) write(*,'(A,1pG14.7)') &
-          ' Before boundary wall clock time/timestep/local meshpoint [microsec] =', &
-                               before_boundary_sum_time/icount/nw/1.0e-6
-        if (lverbose_performance_log) write(*,'(A,1pG14.7)') &
-          ' Diagnostics wall clock time/timestep/local meshpoint [microsec] =', &
-                               time_doing_diagnostics/icount/nw/1.0e-6
-        if (lverbose_performance_log) write(*,'(A,1pG14.7)') &
-          ' Timestep wall clock time/timestep/local meshpoint [microsec] =', &
-                               time_in_timestep/icount/nw/1.0e-6
-        if (lverbose_performance_log) write(*,'(A,1pG14.7)') &
-          ' Timestep wall clock time/timestep/meshpoint [microsec] =', &
-                               time_in_timestep/icount/nw/ncpus/1.0e-6
-        if (lverbose_performance_log) write(*,'(A,1pG14.7)') &
-          ' Diagnostics wall clock time/timestep/meshpoint [microsec] =', &
-                               time_doing_diagnostics/icount/nw/ncpus/1.0e-6
-        if (lverbose_performance_log) write(*,'(A,1pG14.7)') &
-          ' Rhs wall clock time/timestep/meshpoint [microsec] =', &
-                               rhs_sum_time/icount/nw/ncpus/1.0e-6
-        if (lverbose_performance_log) write(*,'(A,1pG14.7)') &
-          ' After substep wall clock time/timestep/meshpoint [microsec] =', &
-                               after_substep_sum_time/icount/nw/ncpus/1.0e-6
-
+! 2025-Dec-08/Kishore: Touko, I would suggest using the existing variable `ip` (which
+! controls some other diagnostic outputs as well), rather than introducing a new
+! flag.
+        if (lverbose_performance_log) then
+          write(*,'(A,1pG14.7)') &
+            ' After substep wall clock time/timestep/local meshpoint [microsec] =', &
+            after_substep_sum_time/icount/nw/1.0e-6
+          write(*,'(A,1pG14.7)') &
+            ' Before boundary wall clock time/timestep/local meshpoint [microsec] =', &
+            before_boundary_sum_time/icount/nw/1.0e-6
+          write(*,'(A,1pG14.7)') &
+            ' Diagnostics wall clock time/timestep/local meshpoint [microsec] =', &
+            time_doing_diagnostics/icount/nw/1.0e-6
+          write(*,'(A,1pG14.7)') &
+            ' Timestep wall clock time/timestep/local meshpoint [microsec] =', &
+            time_in_timestep/icount/nw/1.0e-6
+          write(*,'(A,1pG14.7)') &
+            ' Timestep wall clock time/timestep/meshpoint [microsec] =', &
+            time_in_timestep/icount/nw/ncpus/1.0e-6
+          write(*,'(A,1pG14.7)') &
+            ' Diagnostics wall clock time/timestep/meshpoint [microsec] =', &
+            time_doing_diagnostics/icount/nw/ncpus/1.0e-6
+          write(*,'(A,1pG14.7)') &
+            ' Rhs wall clock time/timestep/meshpoint [microsec] =', &
+            rhs_sum_time/icount/nw/ncpus/1.0e-6
+          write(*,'(A,1pG14.7)') &
+            ' After substep wall clock time/timestep/meshpoint [microsec] =', &
+            after_substep_sum_time/icount/nw/ncpus/1.0e-6
+        endif
       endif
     endif
   endif
