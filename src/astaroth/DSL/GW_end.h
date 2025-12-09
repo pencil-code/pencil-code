60ddfde72e69d64b348c77d477e3658659a842d5
ToxPuro <touko.puro@aalto.fi>
2025-12-09 14:11:42 +0100

    commit GW_end.h

---
+++ src/astaroth/DSL/GW_end.h
@@ -0,0 +1,11 @@
+#if LGRAVITATIONAL_WAVES_HTXK
+if(AC_lsplit_gw_rhs_from_rest_on_gpu__mod__gravitational_waves_htxk)
+{
+	write(F_STRESS_0,DF_STRESS_0)
+	write(F_STRESS_1,DF_STRESS_1)
+	write(F_STRESS_2,DF_STRESS_2)
+	write(F_STRESS_3,DF_STRESS_3)
+	write(F_STRESS_4,DF_STRESS_4)
+	write(F_STRESS_5,DF_STRESS_5)
+}
+#endif
