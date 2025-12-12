c8191b0bbf210f0e7a0ef7e7420b5beaa4870ba3
Kishore G. <kishore96@gmail.com>
2025-12-12 11:17:47 +0530

    python/tests/script_auto-tests.py: prevent runaway processes
    pc_auto-test can hang for a variety of reasons. Some reasons I have
    faced so far:
    - locked HDF5 files
    - bugs in the perl scripts

--- python/tests/script_autotests.py
+++ python/tests/script_autotests.py
@@ -58,7 +58,8 @@ for var in [
 def test_script_pcautotest(path, pytestconfig):
     rundir = pathlib.Path(get_rundir(path))
     res = subprocess.run(
-        f"pc_auto-test --no-lock --auto-clean --script-tests=python '{rundir}'",
+        f"""pc_auto-test --no-lock --auto-clean --script-tests=python \
+                --time-limit=10m '{rundir}'""",
         env=env,
         shell=True,
         universal_newlines=True,
