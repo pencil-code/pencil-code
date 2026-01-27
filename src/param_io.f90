aaa2599721fbe0b9220831d1270b55f418ecc6d5
Touko Puro <ToxPuro>
2026-01-27 13:34:11 +0200

    fix loptional check since loptional is optional might not be present

--- src/param_io.f90
+++ src/param_io.f90
@@ -573,9 +573,7 @@ module Param_IO
         call read_namelist(read_hydro_init_pars          ,'hydro'          ,lhydro, loptional)
         call read_namelist(read_density_init_pars        ,'density'        ,ldensity, loptional)
         call read_namelist(read_gravity_init_pars        ,'grav'           ,lgrav, loptional)
-        !TP: We want to always set gravitational_constant via selfgravity namelist.
-        !    So it is always enabled but optional if using the no-implementation
-        call read_namelist(read_selfgravity_init_pars    ,'selfgrav'       ,.true., loptional .or. .not. lselfgravity)
+        call read_namelist(read_selfgravity_init_pars    ,'selfgrav'       ,.true., loptest(loptional) .or. (.not. lselfgravity))
         call read_namelist(read_poisson_init_pars        ,'poisson'        ,lpoisson, loptional)
         call read_namelist(read_energy_init_pars         ,'entropy'        ,lenergy, loptional)
         call read_namelist(read_magnetic_init_pars       ,'magnetic'       ,lmagnetic, loptional)
