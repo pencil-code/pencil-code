7aba2eb7bff62f1623500417a551c2222206d16b
Kishore G. <kishore96@gmail.com>
2026-02-06 11:53:33 +0530

    Python: Averages: change .keys method to return a list.
    This was decided in the latest Python meeting.
    
    This reverts commit 6c97ee9903ac5364adbfab78b5aa7b76c8cf8420 (Fred Gent).

--- python/pencil/read/averages.py
+++ python/pencil/read/averages.py
@@ -38,11 +38,12 @@ class Averages(object):
             warnings.warn("Mismatch between the times of different kinds of averages (usually happens when 1D and 2D averages are stored at different times). Please use the t attributes of the respective planes (e.g. av.xy.t, rather than av.t).")
         self._t = arr
 
+    @property
     def keys(self):
-        for i in self.__dict__.keys():
-            if i == "_t":
-               i = "t"
-            print(i)
+        ks = list(self.__dict__.keys())
+        ks.remove("_t")
+        ks.append("t")
+        return ks
 
     def read(
         self,
@@ -792,7 +793,9 @@ class _Plane():
     """
     Used to store the averages in a particular plane
     """
+    @property
     def keys(self):
-        for i in self.__dict__.keys():
-            if not i == "keys":
-               print(i)
+        ks = list(self.__dict__.keys())
+        if "keys" in ks:
+            ks.remove("ks")
+        return ks
