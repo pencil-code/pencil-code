Python: TimeSeries.read: fix loss of precision
This only matters if the user specifies a precision higher than double
(e.g. 'g' -> np.longdouble).

python/pencil/read/timeseries.py
@@ -101,7 +101,7 @@ class TimeSeries(object):
                     self.keys = keys_new
             else:
                 try:
-                    row = np.array(list(map(float, re.split(" +", line.strip(" \n")))))
+                    row = np.array(list(re.split(" +", line.strip(" \n"))), dtype=precision)
                     data[nlines, :] = row
                     nlines += 1
                 except ValueError: