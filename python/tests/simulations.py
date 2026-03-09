89ad39995360702a4ee5eab157ccc3bde9123b39
Kishore G. <kishore96.work@gmail.com>
2026-03-09 14:58:40 +0530

    Python: tests/simulations: spaces for indentation
    Feels perverted, but I suppose one must follow the rules.

--- python/tests/simulations.py
+++ python/tests/simulations.py
@@ -7,30 +7,30 @@ from pencil.sim import Simulations
 from test_utils import get_rundir
 
 samples = [
-	"samples/helical-MHDturb_HDF5",
-	"samples/conv-slab_cp_2",
-	]
+    "samples/helical-MHDturb_HDF5",
+    "samples/conv-slab_cp_2",
+    ]
 
 simdirs = [get_rundir(sample) for sample in samples]
 
 def test_Sims_from_tuple():
-	sims = Simulations(*simdirs)
-	assert len(sims.sims) == len(simdirs)
+    sims = Simulations(*simdirs)
+    assert len(sims.sims) == len(simdirs)
 
 def test_Sims_from_list():
-	sims = Simulations(simdirs)
-	assert len(sims.sims) == len(simdirs)
+    sims = Simulations(simdirs)
+    assert len(sims.sims) == len(simdirs)
 
 @pytest.mark.xfail
 def test_Sims_is_iterable():
-	sims = Simulations(*simdirs)
-	
-	assert len(sims) == len(simdirs)
-	
-	for simdir in sims:
-		pass
+    sims = Simulations(*simdirs)
+    
+    assert len(sims) == len(simdirs)
+    
+    for simdir in sims:
+        pass
 
 @pytest.mark.xfail
 def test_Sims_kwarg():
-	sims = Simulations(*simdirs, quiet=True)
-	assert len(sims.sims) == len(simdirs)
+    sims = Simulations(*simdirs, quiet=True)
+    assert len(sims.sims) == len(simdirs)
