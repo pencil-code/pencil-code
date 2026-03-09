bbf4943c180a1a0068e9dd79c9be491e1f0369ae
Kishore G. <kishore96.work@gmail.com>
2026-03-09 14:49:53 +0530

    Python: add tests for pc.sim.Simulations
    Also some xfail tests to document the expected behaviour.

---
+++ python/tests/simulations.py
@@ -0,0 +1,37 @@
+"""
+Tests for the `Simulations` object
+"""
+
+import pytest
+from pencil.sim import Simulations
+from test_utils import get_rundir
+
+samples = [
+	"samples/helical-MHDturb_HDF5",
+	"samples/conv-slab_cp_2",
+	]
+
+simdirs = [get_rundir(sample) for sample in samples]
+
+@pytest.mark.xfail
+def test_Sims_from_tuple():
+	sims = Simulations(*simdirs)
+	assert len(sims.sims) == len(simdirs)
+
+def test_Sims_from_list():
+	sims = Simulations(simdirs)
+	assert len(sims.sims) == len(simdirs)
+
+@pytest.mark.xfail
+def test_Sims_is_iterable():
+	sims = Simulations(*simdirs)
+	
+	assert len(sims) == len(simdirs)
+	
+	for simdir in sims:
+		pass
+
+@pytest.mark.xfail
+def test_Sims_kwarg():
+	sims = Simulations(*simdirs, quiet=True)
+	assert len(sims.sims) == len(simdirs)
