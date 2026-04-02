5cf262d2c18c1fc7ee0bff9ccbb2c8affc64ad57
Kishore G. <kishore96.work@gmail.com>
2026-04-02 12:05:22 +0530

    Python: add test for Simulation.param object
    Check that both dict-style and object-style access work.

---
+++ python/tests/simulation.py
@@ -0,0 +1,16 @@
+"""
+Tests for the Simulation object.
+Note that many aspects of the Simulation object (e.g. sim.bash, sim.run) are
+already used while setting up the other tests.
+"""
+
+from test_utils import require_sample, get_rundir
+from pencil.sim import Simulation
+
+@require_sample("samples/helical-MHDturb")
+def test_sim_param(datadir_helical_MHDTurb):
+    simdir = get_rundir("samples/helical-MHDturb")
+    sim = Simulation(path=simdir, hard=True, quiet=True)
+
+    assert sim.param.lmagnetic is True
+    assert sim.param['initaa'] == sim.param.initaa
