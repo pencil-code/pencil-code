14753bd33a660c3134cf4cef4f2a4befa4c10339
Kishore G. <kishore96.work@gmail.com>
2026-04-08 14:46:57 +0530

    Python: Simulation.update: set conflicts_quiet=quiet

--- python/pencil/sim/simulation.py
+++ python/pencil/sim/simulation.py
@@ -553,7 +553,12 @@ class Simulation:
                 if not quiet: print("~ Reading param.nml.. ")
 
                 try:
-                    self.param = param(quiet=quiet, datadir=self.datadir, keep_nested=True)
+                    self.param = param(
+                        quiet=quiet,
+                        datadir=self.datadir,
+                        keep_nested=True,
+                        conflicts_quiet=quiet,
+                        )
                 except Exception as e:
                     warnings.warn(f"! ERROR: ({e}) while reading param.nml for {self.path}")
                     self.param = False
