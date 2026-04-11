01917fe53f6bf614e72f5849a6b267bbd768c133
Kishore G <kishore96@gmail.com>
2026-04-11 17:31:06 +0530

    sourceme.sh: fix repository location in check

--- sourceme.sh
+++ sourceme.sh
@@ -102,7 +102,7 @@ if [ -n "$PENCIL_HOME" -a -d "$PENCIL_HOME/.git" ]; then
 #
 # Enforce basic pull policy to "rebase".
 #
-  if [ "$(git config get pull.rebase)" = "false" ]; then
+  if [ "$(git -C $PENCIL_HOME config get pull.rebase)" = "false" ]; then
     echo !!!WARNING - you have \"rebase = false\" settings in your .git/config!!!
     echo !!!Pull strategy should always be \"--rebase\" on all branches!!!
     echo !!!Changing it to \"true\"!!!
