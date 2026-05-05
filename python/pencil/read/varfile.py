889fc01c7cbff2557fd3a78e6dba99a1f5d54465
Kishore G. <kishore96.work@gmail.com>
2026-05-05 10:26:45 +0530

    Python: DataCube: rm debug statement
    It seems this print statement was unintentionally added by me.

--- python/pencil/read/varfile.py
+++ python/pencil/read/varfile.py
@@ -233,8 +233,6 @@ class DataCube(object):
             inds = sorted([v for k, v in self._index.items()])
             m = {v: i+1 for i,v in enumerate(inds)}
             self._index = {k:m[v] for k,v in self._index.items()}
-            #TP: put on comment since Python was complaining about improper syntax
-            #print(f"{self._index = }") #debug
         else:
             self._index = index.__dict__
 
