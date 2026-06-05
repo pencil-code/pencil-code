36009ade454682529fcbb32cc5ac942c89511c6c
Kishore G. <kishore96.work@gmail.com>
2026-06-05 10:46:23 +0530

    noderiv: calc_coeffs_1: fix dimensions of grid
    Should fix compilation of the axionSU2back sample on my machine:
    ```
    grid.f90:3067:25:
    
     3067 |       call calc_coeffs_1(dc,coeffs(:,BOT))
          |                         1
    Error: Actual argument contains too few elements for dummy argument
    ‘grid’ (0/2) at (1)
    ```
    
    Relevant previous commit: ef17c109206c94d951134f67836e2292bb3c0a04
    
    I'm pretty sure the sample was working in that commit, but something
    seems to have changed in the interim.

--- src/noderiv.f90
+++ src/noderiv.f90
@@ -34,7 +34,7 @@ module Deriv
 ! 
       !real, dimension(-2:3), intent(in ) :: grid
       !real, dimension(-3:3), intent(out) :: coeffs
-      real, dimension(-0:1), intent(in ) :: grid
+      real, dimension(1:0), intent(in ) :: grid
       real, dimension(-0:0), intent(out) :: coeffs
 !
       if (lroot) print*,'calc_coeffs_1 is not evaluated'
