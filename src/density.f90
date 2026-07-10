3fa2bf45a8fbead0677244a16f403f8906b57986
ToxPuro <touko.puro@aalto.fi>
2026-07-10 13:47:07 +0300

    fix density

--- src/density.f90
+++ src/density.f90
@@ -4652,6 +4652,8 @@ module Density
 
     call copy_addr(kap_tdep,p_par(85))
     call copy_addr(ldiff_kap_tdep,p_par(86)) ! bool
+    call copy_addr(cs201,p_par(87))
+    call copy_addr(cs20_corr,p_par(88))
 
     endsubroutine pushpars2c
 !***********************************************************************
