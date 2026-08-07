dee382f3eee75caeb1be61fb794e28a914ad91f0
ToxPuro <touko.puro@aalto.fi>
2026-08-07 14:00:07 +0300

    0 is for success

--- src/astaroth/torch.cc
+++ src/astaroth/torch.cc
@@ -19,7 +19,7 @@ bool
 initialize_torch()
 {
 	const torchfort_result_t res = torchfort_set_manual_seed(943442);
-	return res == TORCHFORT_RESULT_SUCCESS;
+	return res != TORCHFORT_RESULT_SUCCESS;
 }
 /***********************************************************************************************/
 bool torch_train_CAPI(int sub_dims[3], AcReal* input, AcReal* label, AcReal* loss_val,
@@ -29,7 +29,7 @@ bool torch_train_CAPI(int sub_dims[3], AcReal* input, AcReal* label, AcReal* los
 	int64_t input_shape[5] = {1, input_fields,  sub_dims[2], sub_dims[1], sub_dims[0]};
 	int64_t label_shape[5] = {1, output_fields, sub_dims[2], sub_dims[1], sub_dims[0]};
   	const torchfort_result_t res = torchfort_train(model_name, input, 5, input_shape, label, 5, label_shape, loss_val, TORCH_PRECISION, 0);
-	return res == TORCHFORT_RESULT_SUCCESS;
+	return res != TORCHFORT_RESULT_SUCCESS;
 }
 
 /***********************************************************************************************/
@@ -49,40 +49,40 @@ bool torch_infer_CAPI(int sub_dims[3], AcReal* input, AcReal* label,
 	}
 */
 	res = torchfort_inference(model_name, input, 5, input_shape, label, 5, label_shape, TORCH_PRECISION, 0);
-	return res == TORCHFORT_RESULT_SUCCESS;
+	return res != TORCHFORT_RESULT_SUCCESS;
 }
 
 /***********************************************************************************************/
 bool torch_create_model_CAPI(const char* name, const char* config_fname, int device){
 	
 	const torchfort_result_t res = torchfort_create_model(name, config_fname, device);
-	return res == TORCHFORT_RESULT_SUCCESS;
+	return res != TORCHFORT_RESULT_SUCCESS;
 }
 
 /***********************************************************************************************/
 bool torch_create_distributed_model_CAPI(const char* name, const char* config_fname, MPI_Comm mpi_comm, int device){
 	
 	const torchfort_result_t res = torchfort_create_distributed_model(name, config_fname, mpi_comm, device);
-	return res == TORCHFORT_RESULT_SUCCESS;
+	return res != TORCHFORT_RESULT_SUCCESS;
 }
 /***********************************************************************************************/
 bool torch_load_CAPI(const char* name, const char* fname){
 	const torchfort_result_t res = torchfort_load_model(name, fname);
-	return res == TORCHFORT_RESULT_SUCCESS;
+	return res != TORCHFORT_RESULT_SUCCESS;
 }
 /***********************************************************************************************/
 bool torch_load_checkpoint_CAPI(const char* name, const char* checkpoint_dir, int64_t* step_train, int64_t* step_inference){
 	const torchfort_result_t res = torchfort_load_checkpoint(name, checkpoint_dir, step_train, step_inference);
-	return res == TORCHFORT_RESULT_SUCCESS;
+	return res != TORCHFORT_RESULT_SUCCESS;
 }
 /***********************************************************************************************/
 bool torch_save_model_CAPI(const char* name, const char* fname){
 	const torchfort_result_t res = torchfort_save_model(name, fname);
-	return res == TORCHFORT_RESULT_SUCCESS;
+	return res != TORCHFORT_RESULT_SUCCESS;
 }
 /***********************************************************************************************/
 bool torch_save_checkpoint_CAPI(const char* name, const char* checkpoint_dir){
 	const torchfort_result_t res = torchfort_save_checkpoint(name, checkpoint_dir);
-	return res == TORCHFORT_RESULT_SUCCESS;
+	return res != TORCHFORT_RESULT_SUCCESS;
 }
 /***********************************************************************************************/
