078793a794ebed36f20a67f388fa61ca00fd0c5b
Matthias Rheinhardt <mreinhardt@nordita.org>
2026-08-31 20:13:00 +0000

    MR: removed unneeded dependency

--- src/nocosmicrayflux.f90
+++ src/nocosmicrayflux.f90
@@ -124,8 +124,6 @@ module Cosmicrayflux
 !***********************************************************************
     subroutine pushpars2c(p_par)
 
-    use Syscalls, only: copy_addr
-
     integer, parameter :: n_pars=0
     integer(KIND=ikind8), dimension(n_pars) :: p_par
 
