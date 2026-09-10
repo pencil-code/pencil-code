61b553f70cd0456cda7e75a4cb4a0b1e409e2b8d
ToxPuro <touko.puro@aalto.fi>
2026-09-10 15:15:12 +0300

    limit the number of module routines: rescaling happens in after the last substep

--- src/hydro.f90
+++ src/hydro.f90
@@ -4784,30 +4784,7 @@ module Hydro
 !
       intent(inout) :: f
 !
-      real, dimension (mx,3) :: ss
-      real, dimension (mx) :: ss2
-      integer :: j, l_ind, m_ind, n_ind
 !
-      if (llorentz_limiter.or.lvel_limiter) then
-         do n_ind=1,mz
-         do m_ind=1,my
-           ss=f(:,m_ind,n_ind,iux:iuz)
-           call dot2_mx(ss,ss2)
-           do j=iux,iuz
-             if (llorentz_limiter) then
-               f(:,m_ind,n_ind,j)=f(:,m_ind,n_ind,j)/sqrt(1.+ss2)
-             endif
-             if (lvel_limiter) then
-               do l_ind=1,mx
-                 if (ss2(l_ind)>max_vel**2) then
-                   f(l_ind,m_ind,n_ind,j)=f(l_ind,m_ind,n_ind,j)*max_vel/sqrt(ss2(l_ind))
-                 endif
-               enddo
-             endif
-           enddo
-       enddo
-       enddo
-      endif
 !
     endsubroutine rescaling_velocity
 !*******************************************************************************
@@ -8384,6 +8361,10 @@ module Hydro
       real, contiguous, dimension(:,:,:,:) :: f
       real, contiguous, dimension(:,:,:,:) :: df
       real :: dt_sub
+
+      real, dimension (mx,3) :: ss
+      real, dimension (mx) :: ss2
+      integer :: j, l_ind, m_ind, n_ind
 !
       logical :: lwrite_debug=.false.
       integer :: iorder_z=2
@@ -8444,6 +8425,27 @@ module Hydro
       if (ldiagnos.and.othresh_per_orms/=0.) then
         call vecout_finalize(41,trim(directory)//'/ovec',novec)
         call calc_othresh
+      endif
+
+      if (llast .and. (llorentz_limiter.or.lvel_limiter)) then
+         do n_ind=1,mz
+         do m_ind=1,my
+           ss=f(:,m_ind,n_ind,iux:iuz)
+           call dot2_mx(ss,ss2)
+           do j=iux,iuz
+             if (llorentz_limiter) then
+               f(:,m_ind,n_ind,j)=f(:,m_ind,n_ind,j)/sqrt(1.+ss2)
+             endif
+             if (lvel_limiter) then
+               do l_ind=1,mx
+                 if (ss2(l_ind)>max_vel**2) then
+                   f(l_ind,m_ind,n_ind,j)=f(l_ind,m_ind,n_ind,j)*max_vel/sqrt(ss2(l_ind))
+                 endif
+               enddo
+             endif
+           enddo
+       enddo
+       enddo
       endif
 !
     endsubroutine hydro_after_timestep
--- src/hydro.h
+++ src/hydro.h
@@ -26,7 +26,6 @@
   public :: load_variables_to_gpu_hydro
   public :: hydro_save_diagnostic_controls
   public :: hydro_restore_diagnostic_controls
-  public :: rescaling_velocity
 !
 ! WL: SHOULDN'T BE PUBLIC!
 !
