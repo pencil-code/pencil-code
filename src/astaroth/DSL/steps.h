41542e860850511ced633e46d06508298e3f068b
ToxPuro <touko.puro@aalto.fi>
2026-09-18 16:43:40 +0200

    we need time in twopass_solve_final because of the new forcing

--- src/astaroth/DSL/steps.h
+++ src/astaroth/DSL/steps.h
@@ -29,7 +29,7 @@ ComputeSteps AC_rhs(boundconds)
 	shock_2_max(AC_step_num)
 	shock_3_smooth(AC_step_num)
         twopass_solve_intermediate(AC_step_num,AC_dt,AC_t,AC_lrmv)
-        twopass_solve_final(AC_step_num,AC_dt)
+        twopass_solve_final(AC_step_num,AC_t,AC_dt)
 }
 
 ComputeSteps AC_GW_rhs(boundconds)
